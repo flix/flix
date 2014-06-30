1edc51072510352736d76f27d213d3be79266cdb
Magnus Madsen <magnusm@cs.au.dk>
2014-06-30 14:40:14 -0400

    .

--- src/util/output/StringFormat.scala
+++ src/util/output/StringFormat.scala
@@ -3,13 +3,26 @@ package util.output
 import impl.logic._
 
 object StringFormat {
-  def format(p: Program): String = p.facts.map(format).mkString("\n") + "\n\n" + p.rules.map(format).mkString(", ")
+  def format(p: Program): String = p.facts.map(format).mkString("\n") + "\n\n" + p.rules.map(format).mkString("\n")
 
-  def format(h: HornClause): String = s"${format(h.head)} :- ${h.body.map(format(_).mkString(", "))}\n"
+  def format(h: HornClause): String =
+    if (h.isFact)
+      format(h.head) + "."
+    else
+      format(h.head) + " :- " + h.body.map(format).mkString(", ") + "."
 
-  def format(p: Predicate): String = format(p.name) + "(" + p.terms.map(format) + ")"
+  def format(p: Predicate): String = format(p.name) + "(" + p.terms.map(format).mkString(", ") + ")"
 
-  def format(t: Term): String = t.toString
+  def format(t: Term): String = t match {
+    case Term.Constant(v) => format(v)
+    case Term.Variable(Symbol.VariableSymbol(v)) => v
+    case tt => tt.toString
+  }
+
+  def format(v: Value): String = v match {
+    case Value.String(s) => s
+    case vv => vv.toString
+  }
 
   def format(s: Symbol): String = s match {
     case Symbol.PredicateSymbol(x) => x
