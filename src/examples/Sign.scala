d9765a0e66edbcc28de4a4974abd3891b8d50e55
Magnus Madsen <magnusm@cs.au.dk>
2014-07-03 12:58:23 -0400

    .

--- src/examples/Sign.scala
+++ src/examples/Sign.scala
@@ -4,64 +4,65 @@ import impl.logic._
 import syntax.Symbols._
 
 object Sign {
-  def main(args: Array[String]): Unit = {
 
-    val Lattice = Type.Variant(IndexedSeq(
-      Type.Constructor0("Top"),
-      Type.Constructor0("Neg"), Type.Constructor0("Zero"), Type.Constructor0("Pos"),
-      Type.Constructor0("Bot")
-    ))
+  val Lattice = Type.Variant(IndexedSeq(
+    Type.Constructor0("Top"),
+    Type.Constructor0("Neg"), Type.Constructor0("Zero"), Type.Constructor0("Pos"),
+    Type.Constructor0("Bot")
+  ))
 
-    val Top = Term.Constant(Value.Constructor0("Top"))
-    val Neg = Term.Constant(Value.Constructor0("Neg"))
-    val Zero = Term.Constant(Value.Constructor0("Zero"))
-    val Pos = Term.Constant(Value.Constructor0("Pos"))
-    val Bot = Term.Constant(Value.Constructor0("Bot"))
+  val Top = Term.Constant(Value.Constructor0("Top"))
+  val Neg = Term.Constant(Value.Constructor0("Neg"))
+  val Zero = Term.Constant(Value.Constructor0("Zero"))
+  val Pos = Term.Constant(Value.Constructor0("Pos"))
+  val Bot = Term.Constant(Value.Constructor0("Bot"))
 
-    val Leq = Set(
-      HornClause(Predicate("Sign.Leq", List(Bot, Term.Variable("_"))), Set.empty),
-      HornClause(Predicate("Sign.Leq", List(Neg, Neg)), Set.empty),
-      HornClause(Predicate("Sign.Leq", List(Zero, Zero)), Set.empty),
-      HornClause(Predicate("Sign.Leq", List(Pos, Pos)), Set.empty),
-      HornClause(Predicate("Sign.Leq", List(Term.Variable("_"), Top)), Set.empty)
-    )
+  val Leq = Set(
+    HornClause(Predicate("Sign.Leq", List(Bot, Term.Variable("_"))), Set.empty),
+    HornClause(Predicate("Sign.Leq", List(Neg, Neg)), Set.empty),
+    HornClause(Predicate("Sign.Leq", List(Zero, Zero)), Set.empty),
+    HornClause(Predicate("Sign.Leq", List(Pos, Pos)), Set.empty),
+    HornClause(Predicate("Sign.Leq", List(Term.Variable("_"), Top)), Set.empty)
+  )
 
-    val Join = Set(
-      HornClause(Predicate("Sign.Leq", List(Bot, Term.Variable("x"), Term.Variable("x"))), Set.empty),
-      HornClause(Predicate("Sign.Leq", List(Term.Variable("x"), Bot, Term.Variable("x"))), Set.empty),
+  val Join = Set(
+    HornClause(Predicate("Sign.Leq", List(Bot, Term.Variable("x"), Term.Variable("x"))), Set.empty),
+    HornClause(Predicate("Sign.Leq", List(Term.Variable("x"), Bot, Term.Variable("x"))), Set.empty),
 
-      HornClause(Predicate("Sign.Leq", List(Neg, Neg, Neg)), Set.empty),
-      HornClause(Predicate("Sign.Leq", List(Neg, Zero, Top)), Set.empty),
-      HornClause(Predicate("Sign.Leq", List(Neg, Pos, Top)), Set.empty),
+    HornClause(Predicate("Sign.Leq", List(Neg, Neg, Neg)), Set.empty),
+    HornClause(Predicate("Sign.Leq", List(Neg, Zero, Top)), Set.empty),
+    HornClause(Predicate("Sign.Leq", List(Neg, Pos, Top)), Set.empty),
 
-      HornClause(Predicate("Sign.Leq", List(Zero, Neg, Top)), Set.empty),
-      HornClause(Predicate("Sign.Leq", List(Zero, Zero, Zero)), Set.empty),
-      HornClause(Predicate("Sign.Leq", List(Zero, Pos, Top)), Set.empty),
+    HornClause(Predicate("Sign.Leq", List(Zero, Neg, Top)), Set.empty),
+    HornClause(Predicate("Sign.Leq", List(Zero, Zero, Zero)), Set.empty),
+    HornClause(Predicate("Sign.Leq", List(Zero, Pos, Top)), Set.empty),
 
-      HornClause(Predicate("Sign.Leq", List(Pos, Neg, Top)), Set.empty),
-      HornClause(Predicate("Sign.Leq", List(Pos, Zero, Top)), Set.empty),
-      HornClause(Predicate("Sign.Leq", List(Pos, Pos, Pos)), Set.empty),
+    HornClause(Predicate("Sign.Leq", List(Pos, Neg, Top)), Set.empty),
+    HornClause(Predicate("Sign.Leq", List(Pos, Zero, Top)), Set.empty),
+    HornClause(Predicate("Sign.Leq", List(Pos, Pos, Pos)), Set.empty),
 
-      HornClause(Predicate("Sign.Leq", List(Term.Variable("_"), Top, Top)), Set.empty),
-      HornClause(Predicate("Sign.Leq", List(Top, Term.Variable("_"), Top)), Set.empty)
-    )
+    HornClause(Predicate("Sign.Leq", List(Term.Variable("_"), Top, Top)), Set.empty),
+    HornClause(Predicate("Sign.Leq", List(Top, Term.Variable("_"), Top)), Set.empty)
+  )
 
-    // Sign.Sum(Bot, _, Bot).
-    // Sign.Sum(_, Bot, Bot).
-    // Sign.Sum(Neg, Neg, Neg).
-    // Sign.Sum(Neg, Zero, Neg).
-    // Sign.Sum(Neg, Pos, Top).
-    // Sign.Sum(Zero, Neg, Neg).
-    // Sign.sum(Zero, Zero, Zero).
-    // Sign.Sum(Zero, Pos, Pos).
-    // Sign.Sum(Pos, Neg, Top).
-    // Sign.Sum(Pos, Zero, Pos).
-    // Sign.Sum(Pos, Pos, Pos).
-    // Sign.Sum(Top, _, Top).
-    // Sign.Sum(_, Top, Top).
+  val Sum = Set(
+    HornClause(Predicate("Sign.Sum", List(Bot, Term.Variable("_"), Bot)), Set.empty),
+    HornClause(Predicate("Sign.Sum", List(Term.Variable("_"), Bot, Bot)), Set.empty),
 
-    val Sum = Set(
+    HornClause(Predicate("Sign.Sum", List(Neg, Neg, Neg)), Set.empty),
+    HornClause(Predicate("Sign.Sum", List(Neg, Zero, Neg)), Set.empty),
+    HornClause(Predicate("Sign.Sum", List(Neg, Pos, Top)), Set.empty),
 
-    )
-  }
+    HornClause(Predicate("Sign.Sum", List(Zero, Neg, Neg)), Set.empty),
+    HornClause(Predicate("Sign.Sum", List(Zero, Zero, Zero)), Set.empty),
+    HornClause(Predicate("Sign.Sum", List(Zero, Pos, Pos)), Set.empty),
+
+    HornClause(Predicate("Sign.Sum", List(Pos, Neg, Top)), Set.empty),
+    HornClause(Predicate("Sign.Sum", List(Pos, Zero, Pos)), Set.empty),
+    HornClause(Predicate("Sign.Sum", List(Pos, Pos, Pos)), Set.empty),
+
+    HornClause(Predicate("Sign.Sum", List(Top, Term.Variable("_"), Top)), Set.empty),
+    HornClause(Predicate("Sign.Sum", List(Term.Variable("_"), Top, Top)), Set.empty)
+  )
+
 }
---
+++ src/examples/SignAndParity.scala
@@ -0,0 +1,8 @@
+package examples
+
+object SignAndParity {
+  def main(args: Array[String]): Unit = {
+    // TODO Should somehow be able to combine the two lattices....
+
+  }
+}
