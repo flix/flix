cbe2da54cd66f8b18ba36e7e4fd6fb2898839b60
Magnus Madsen <magnusm@cs.au.dk>
2014-07-07 11:16:51 -0400

    .

--- src/domains/Interval.scala
+++ src/domains/Interval.scala
@@ -1,27 +1,54 @@
 package domains
 
+import impl.logic._
+import syntax.Symbols._
+
 object Interval {
-  def main(args: Array[String]): Unit = {
 
-    // TODO: Intervals
+  val Elements = Type.Variant(IndexedSeq(
+    Type.Constructor0("Top"),
+    Type.Constructor2("Range", Type.Int, Type.Int), // TODO: Need additional constraint...
+    Type.Constructor0("Bot")
+  ))
 
-    // Lattice Interval = Bot | Top | Range(Int, Int).
+  val Top = Term.Constant(Value.Constructor0("Top"))
+  val Bot = Term.Constant(Value.Constructor0("Bot"))
 
-    // Interval.Leq(Bot, _).
-    // Interval.Leq(Range(b1, e1), Range(b2, e2)) :- b2 <= b1, e2 >= e1.
-    // Interval.Leq(_, Top).
+  val LeqSymbol = Symbol.PredicateSymbol("Interval.Leq")
+  val JoinSymbol = Symbol.PredicateSymbol("Interval.Join")
+  val SumSymbol = Symbol.PredicateSymbol("Interval.Sum")
 
-    // Interval.Join(Bot, x, x).
-    // Interval.Join(x, Bot, x).
-    // Interval.Join(Range(b1, e1), Range(b2, e2), Range(b3, e3)) :- (max(e1, e2) - min(b1, b2)) <= 10.
-    // Interval.Join(Range(b1, e1), Range(b2, e2), Top) :- (max(e1, e2) - min(b1, b2)) > 10.
-    // Interval.Join(Top, _, Top).
-    // Interval.Join(_, Top, Top).
+  val Leq = Set(
+    HornClause(Predicate(LeqSymbol, List(Bot, Term.Variable("_"))), Set.empty),
 
-    // Interval.Sum(Bot, _, Bot).
-    // Interval.Sum(_, Bot, Bot).
-    // Interval.Sum(Range(b1, e1), Range(b2, e2), Range(b1 + b2, e1 + e2)).
-    // Interval.Sum(Top, _, Top).
-    // Interval.Sum(_, Top, Top).
-  }
+    HornClause(
+      head = Predicate(LeqSymbol, List(
+        Term.Constructor2(Symbol.NamedSymbol("Range"), Term.Variable("b1"), Term.Variable("e1")),
+        Term.Constructor2(Symbol.NamedSymbol("Range"), Term.Variable("b2"), Term.Variable("e2"))
+      )),
+      body = Set(
+        Predicate("Int.Leq", List(Term.Variable("b2"), Term.Variable("b1"))),
+        Predicate("Int.Leq", List(Term.Variable("e1"), Term.Variable("e2")))
+      )),
+
+    HornClause(Predicate(LeqSymbol, List(Term.Variable("_"), Top)), Set.empty)
+  )
+
+  // Interval.Leq(Bot, _).
+  // Interval.Leq(Range(b1, e1), Range(b2, e2)) :- b2 <= b1, e2 >= e1.
+  // Interval.Leq(_, Top).
+
+  // Interval.Join(Bot, x, x).
+  // Interval.Join(x, Bot, x).
+  // Interval.Join(Range(b1, e1), Range(b2, e2), Range(b3, e3)) :- (max(e1, e2) - min(b1, b2)) <= 10.
+  // Interval.Join(Range(b1, e1), Range(b2, e2), Top) :- (max(e1, e2) - min(b1, b2)) > 10.
+  // Interval.Join(Top, _, Top).
+  // Interval.Join(_, Top, Top).
+
+  // Interval.Sum(Bot, _, Bot).
+  // Interval.Sum(_, Bot, Bot).
+  // Interval.Sum(Range(b1, e1), Range(b2, e2), Range(b1 + b2, e1 + e2)).
+  // Interval.Sum(Top, _, Top).
+  // Interval.Sum(_, Top, Top).
+
 }
