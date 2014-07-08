7a4d0f4c18e2ae0bc32edc6dab617b1e45f16c54
Magnus Madsen <magnusm@cs.au.dk>
2014-07-08 18:44:16 -0400

    .

--- src/examples/RandomGraph01.scala
+++ src/examples/RandomGraph01.scala
@@ -8,8 +8,8 @@ import scala.util.Random
 
 object RandomGraph01 {
 
-  val NumberOfVertices = 100
-  val NumberOfEdges = 10000
+  val NumberOfVertices = 5
+  val NumberOfEdges = 10
 
   val Random = new Random()
 
@@ -19,23 +19,32 @@ object RandomGraph01 {
   def main(args: Array[String]): Unit = {
 
     val EdgeSymbol = Symbol.PredicateSymbol("Edge")
+    val CycleSymbol = Symbol.PredicateSymbol("Cycle")
 
     val facts = Edges.toList map {
       case (x, y) => HornClause(Predicate(EdgeSymbol, List(Term.Constant(Value.Int(x)), Term.Constant(Value.Int(y)))))
     }
 
-    // E(x, z) :- E(x, y), E(y, z).
     val clauses = List(
+      // E(x, z) :- E(x, y), E(y, z).
       HornClause(head =
         Predicate(EdgeSymbol, List(Term.Variable("x"), Term.Variable("z"))),
         body = List(
           Predicate(EdgeSymbol, List(Term.Variable("x"), Term.Variable("y"))),
           Predicate(EdgeSymbol, List(Term.Variable("y"), Term.Variable("z")))
+        )),
+
+      // C(x) :- E(x, x).
+      HornClause(head =
+        Predicate(CycleSymbol, List(Term.Variable("x"))),
+        body = List(
+          Predicate(EdgeSymbol, List(Term.Variable("x"), Term.Variable("x")))
         ))
     )
 
     val interpretations = Map(
-      EdgeSymbol -> Interpretation.Relation(Representation.Data)
+      EdgeSymbol -> Interpretation.Relation(Representation.Data),
+      CycleSymbol -> Interpretation.Relation(Representation.Data)
     )
 
     val program = Program(facts ::: clauses, interpretations)
