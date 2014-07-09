77cf1af2e85a478839f81afb91671bedf6759ce9
Magnus Madsen <magnusm@cs.au.dk>
2014-07-09 14:28:05 -0400

    .

--- src/domains/Parity.scala
+++ src/domains/Parity.scala
@@ -7,13 +7,12 @@ object Parity {
 
   val Elements = Type.Variant(IndexedSeq(
     Type.Constructor0("Parity.Top"),
-    Type.Constructor0("Parity.Odd"), Type.Constructor0("Parity.Zero"), Type.Constructor0("Parity.Even"),
+    Type.Constructor0("Parity.Odd"), Type.Constructor0("Parity.Even"),
     Type.Constructor0("Parity.Bot")
   ))
 
   val Top = Term.Constructor0("Parity.Top")
   val Odd = Term.Constructor0("Parity.Odd")
-  val Zero = Term.Constructor0("Parity.Zero")
   val Even = Term.Constructor0("Parity.Even")
   val Bot = Term.Constructor0("Parity.Bot")
 
@@ -24,7 +23,6 @@ object Parity {
   val Leq = List(
     HornClause(Predicate(LeqSymbol, List(Bot, Term.Variable("_")))),
     HornClause(Predicate(LeqSymbol, List(Odd, Odd))),
-    HornClause(Predicate(LeqSymbol, List(Zero, Zero))),
     HornClause(Predicate(LeqSymbol, List(Even, Even))),
     HornClause(Predicate(LeqSymbol, List(Term.Variable("_"), Top)))
   )
@@ -32,9 +30,13 @@ object Parity {
   val Join = List(
     HornClause(Predicate(JoinSymbol, List(Bot, Term.Variable("x"), Term.Variable("x")))),
     HornClause(Predicate(JoinSymbol, List(Term.Variable("x"), Bot, Term.Variable("x")))),
+
     HornClause(Predicate(JoinSymbol, List(Odd, Odd, Odd))),
-    HornClause(Predicate(JoinSymbol, List(Zero, Zero, Zero))),
+    HornClause(Predicate(JoinSymbol, List(Odd, Even, Top))),
+
     HornClause(Predicate(JoinSymbol, List(Even, Even, Even))),
+    HornClause(Predicate(JoinSymbol, List(Even, Odd, Top))),
+
     HornClause(Predicate(JoinSymbol, List(Top, Term.Variable("_"), Top))),
     HornClause(Predicate(JoinSymbol, List(Term.Variable("_"), Top, Top)))
   )
@@ -42,15 +44,13 @@ object Parity {
   val Sum = List(
     HornClause(Predicate(SumSymbol, List(Bot, Term.Variable("_"), Bot))),
     HornClause(Predicate(SumSymbol, List(Term.Variable("_"), Bot, Bot))),
+
     HornClause(Predicate(SumSymbol, List(Odd, Odd, Even))),
-    HornClause(Predicate(SumSymbol, List(Odd, Zero, Odd))),
     HornClause(Predicate(SumSymbol, List(Odd, Even, Odd))),
-    HornClause(Predicate(SumSymbol, List(Zero, Odd, Odd))),
-    HornClause(Predicate(SumSymbol, List(Zero, Zero, Zero))),
-    HornClause(Predicate(SumSymbol, List(Zero, Even, Even))),
+
     HornClause(Predicate(SumSymbol, List(Even, Odd, Odd))),
-    HornClause(Predicate(SumSymbol, List(Even, Zero, Even))),
     HornClause(Predicate(SumSymbol, List(Even, Even, Even))),
+
     HornClause(Predicate(SumSymbol, List(Top, Term.Variable("_"), Top))),
     HornClause(Predicate(SumSymbol, List(Term.Variable("_"), Top, Top)))
   )
