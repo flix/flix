1603cfe082370b93078854b609dae31e06bc36ba
Magnus Madsen <magnusm@cs.au.dk>
2014-08-24 16:08:57 +0200

    .

--- src/impl/logic/Pattern.scala
+++ src/impl/logic/Pattern.scala
@@ -1,6 +1,32 @@
 package impl.logic
 
-class Pattern {
- // TODO: For Tuples.
+sealed trait Pattern
+
+object Pattern {
+
+  /**
+   * A variable pattern.
+   */
+  case class Var(s: Symbol.VariableSymbol) extends Pattern
+
+  /**
+   * A 2-tuple pattern.
+   */
+  case class Tuple2(p1: Pattern, p2: Pattern) extends Pattern
+
+  /**
+   * A 3-tuple pattern.
+   */
+  case class Tuple3(p1: Pattern, p2: Pattern, p3: Pattern) extends Pattern
+
+  /**
+   * A 4-tuple pattern.
+   */
+  case class Tuple4(p1: Pattern, p2: Pattern, p3: Pattern, p4: Pattern) extends Pattern
+
+  /**
+   * A 5-tuple pattern.
+   */
+  case class Tuple5(p1: Pattern, p2: Pattern, p3: Pattern, p4: Pattern, p5: Pattern) extends Pattern
 
 }
