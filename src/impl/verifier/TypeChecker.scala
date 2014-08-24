cf45a1e549759db4bb5bda96dcdd2b1268c1e823
Magnus Madsen <magnusm@cs.au.dk>
2014-08-24 12:06:20 +0200

    .

--- src/impl/verifier/TypeChecker.scala
+++ src/impl/verifier/TypeChecker.scala
@@ -26,31 +26,33 @@ object TypeChecker {
       val typ1 = typecheck(t1, typenv)
       val typ2 = typecheck(t2, typenv)
       val typ3 = typecheck(t3, typenv)
-      assertType(Type.Bool, typ1, t)
+      assertType(Type.Bool, typ1, t1)
       assertType(typ2, typ3, t)
 
     case Term.UnaryOp(op, t1) =>
       val typ1 = typecheck(t1, typenv)
       op match {
-        case UnaryOperator.Not => assertType(Type.Bool, typ1, t)
-        case UnaryOperator.UnaryPlus => assertType(Type.Int, typ1, t)
-        case UnaryOperator.UnaryMinus => assertType(Type.Int, typ1, t)
+        case UnaryOperator.Not => assertType(Type.Bool, typ1, t1)
+        case UnaryOperator.UnaryPlus => assertType(Type.Int, typ1, t1)
+        case UnaryOperator.UnaryMinus => assertType(Type.Int, typ1, t1)
       }
 
     case Term.BinaryOp(op, t1, t2) =>
       val typ1 = typecheck(t1, typenv)
       val typ2 = typecheck(t2, typenv)
       op match {
-        case BinaryOperator.Plus => assertTypes(expected = List(Type.Int, Type.Int), actual = List(typ1, typ2), result = Type.Int, t)
-        case BinaryOperator.Minus => assertTypes(expected = List(Type.Int, Type.Int), actual = List(typ1, typ2), result = Type.Int, t)
-        case BinaryOperator.Times => assertTypes(expected = List(Type.Int, Type.Int), actual = List(typ1, typ2), result = Type.Int, t)
-        case BinaryOperator.Divide => assertTypes(expected = List(Type.Int, Type.Int), actual = List(typ1, typ2), result = Type.Int, t)
-        case BinaryOperator.Modulo => assertTypes(expected = List(Type.Int, Type.Int), actual = List(typ1, typ2), result = Type.Int, t)
-        case BinaryOperator.Less => assertTypes(expected = List(Type.Int, Type.Int), actual = List(typ1, typ2), result = Type.Bool, t)
-        case BinaryOperator.LessEqual => assertTypes(expected = List(Type.Int, Type.Int), actual = List(typ1, typ2), result = Type.Bool, t)
-        case BinaryOperator.Greater => assertTypes(expected = List(Type.Int, Type.Int), actual = List(typ1, typ2), result = Type.Bool, t)
-        case BinaryOperator.GreaterEqual => assertTypes(expected = List(Type.Int, Type.Int), actual = List(typ1, typ2), result = Type.Bool, t)
-        case BinaryOperator.Equal | BinaryOperator.NotEqual => assertType(typ1, typ2, t)
+        case BinaryOperator.Plus | BinaryOperator.Minus | BinaryOperator.Times | BinaryOperator.Divide | BinaryOperator.Modulo =>
+          assertType(Type.Int, typ1, t1)
+          assertType(Type.Int, typ2, t2)
+          Type.Int
+
+        case BinaryOperator.Less | BinaryOperator.LessEqual | BinaryOperator.Greater | BinaryOperator.GreaterEqual | BinaryOperator.GreaterEqual =>
+          assertType(Type.Int, typ1, t1)
+          assertType(Type.Int, typ2, t2)
+          Type.Int
+
+        case BinaryOperator.Equal | BinaryOperator.NotEqual =>
+          assertType(typ1, typ2, t)
       }
   }
 
@@ -63,15 +65,4 @@ object TypeChecker {
       actual
     else
       throw Error.TypingError(expected, actual, t)
-
-  /**
-   * Asserts that the given `expected` types are equal to the `actual` types for the given term `t`.
-   * Otherwise, throws an exception.
-   */
-  private def assertTypes(expected: List[Type], actual: List[Type], result: Type, t: Term): Type = {
-    for ((ee, tt) <- expected zip actual) {
-      assertType(ee, tt, t)
-    }
-    result
-  }
 }
