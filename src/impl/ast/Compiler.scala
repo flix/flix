afe0aca0cc41bc7f2184fe6e7e2758dca9517956
Magnus Madsen <magnusm@cs.au.dk>
2014-08-25 15:18:04 +0200

    .

--- src/impl/ast/Compiler.scala
+++ src/impl/ast/Compiler.scala
@@ -11,36 +11,38 @@ object Compiler {
     val ast = Parser.parse(file)
     println(ast)
 
-    println(parse(ast))
+    println(compile(ast))
   }
 
-  def parse(e: List[SExp]): Unit = {
-    e map parseDeclaration
+  def compile(e: List[SExp]): Unit = {
+    e map compileDeclaration
   }
 
-  def parseDeclaration(e: SExp): Unit = e match {
-    case SExp.Lst(SExp.Keyword("def-type") :: Literal.Name(n) :: typ :: Nil) => parseType(typ)
-    case SExp.Lst(SExp.Keyword("def-bot") :: Literal.Name(n) :: v :: Nil) => parseValue(v)
+  def compileDeclaration(e: SExp): Unit = e match {
+    case SExp.Lst(SExp.Keyword("def-type") :: Literal.Name(n) :: typ :: Nil) => compileType(typ)
+    case SExp.Lst(SExp.Keyword("def-bot") :: Literal.Name(n) :: v :: Nil) => compileValue(v)
     case SExp.Lst(SExp.Keyword("def-leq") :: Literal.Name(n) :: args :: body :: Nil) =>
     case SExp.Lst(SExp.Keyword("def-lub") :: Literal.Name(n) :: args :: body :: Nil) =>
-    case SExp.Lst(SExp.Keyword("def-height") :: Literal.Name(n) :: args :: body :: Nil) =>
+    case SExp.Lst(SExp.Keyword("def-height") :: Literal.Name(n) :: args :: body :: Nil) => compileTerm(body)
     case SExp.Lst(SExp.Keyword("def-fun") :: Literal.Name(n) :: args :: body :: Nil) =>
     case SExp.Lst(SExp.Keyword("rule") :: head :: tail) =>
   }
 
-  def parseType(e: SExp): Type = e match {
+  def compileType(e: SExp): Type = e match {
     case Literal.Name(s) => Type.Tagged(Symbol.NamedSymbol(s), Type.Unit)
-    case SExp.Lst(xs) => Type.Sum(xs.map(parseType))
+    case SExp.Lst(xs) => Type.Sum(xs.map(compileType))
   }
 
-  def parseTerm(e: SExp): Term = ???
+  def compileTerm(e: SExp): Term = e match {
+    case SExp.Lst(SExp.Keyword("match") :: exp :: cases :: Nil) => ???
+  }
 
-  def parseValue(e: SExp): Value = e match {
+  def compileValue(e: SExp): Value = e match {
     case Literal.Name(n) => Value.Tagged(Symbol.NamedSymbol(n), Value.Unit, Type.Sum(List.empty))
     case Literal.Bool(b) => Value.Bool(b)
     case Literal.Int(i) => Value.Int(i)
     case Literal.Str(s) => Value.Str(s)
-    case SExp.Lst(Literal.Name(s) :: e1 :: Nil) => Value.Tagged(Symbol.NamedSymbol(s), parseValue(e1), ???)
+    case SExp.Lst(Literal.Name(s) :: e1 :: Nil) => Value.Tagged(Symbol.NamedSymbol(s), compileValue(e1), ???)
 
     case SExp.Lst(a :: b :: Nil) => ???
   }
