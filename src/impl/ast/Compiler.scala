da27a740eae6503d63594c2e2928aaa909ff4aad
Magnus Madsen <magnusm@cs.au.dk>
2014-08-25 15:58:01 +0200

    .

--- src/impl/ast/Compiler.scala
+++ src/impl/ast/Compiler.scala
@@ -19,17 +19,17 @@ object Compiler {
   }
 
   def compileDeclaration(e: SExp): Unit = e match {
-    case SExp.Lst(SExp.Keyword("def-type") :: Literal.Name(n) :: typ :: Nil) => compileType(typ)
-    case SExp.Lst(SExp.Keyword("def-bot") :: Literal.Name(n) :: v :: Nil) => compileValue(v)
-    case SExp.Lst(SExp.Keyword("def-leq") :: Literal.Name(n) :: args :: body :: Nil) =>
-    case SExp.Lst(SExp.Keyword("def-lub") :: Literal.Name(n) :: args :: body :: Nil) =>
-    case SExp.Lst(SExp.Keyword("def-height") :: Literal.Name(n) :: args :: body :: Nil) => compileTerm(body)
-    case SExp.Lst(SExp.Keyword("def-fun") :: Literal.Name(n) :: args :: body :: Nil) =>
+    case SExp.Lst(SExp.Keyword("def-type") :: SExp.Name(n) :: typ :: Nil) => compileType(typ)
+    case SExp.Lst(SExp.Keyword("def-bot") :: SExp.Name(n) :: v :: Nil) => compileValue(v)
+    case SExp.Lst(SExp.Keyword("def-leq") :: SExp.Name(n) :: args :: body :: Nil) =>
+    case SExp.Lst(SExp.Keyword("def-lub") :: SExp.Name(n) :: args :: body :: Nil) =>
+    case SExp.Lst(SExp.Keyword("def-height") :: SExp.Name(n) :: args :: body :: Nil) => compileTerm(body)
+    case SExp.Lst(SExp.Keyword("def-fun") :: SExp.Name(n) :: args :: body :: Nil) =>
     case SExp.Lst(SExp.Keyword("rule") :: head :: tail) =>
   }
 
   def compileType(e: SExp): Type = e match {
-    case Literal.Name(s) => Type.Tagged(Symbol.NamedSymbol(s), Type.Unit)
+    case SExp.Name(s) => Type.Tagged(Symbol.NamedSymbol(s), Type.Unit)
     case SExp.Lst(xs) => Type.Sum(xs.map(compileType))
   }
 
@@ -37,13 +37,19 @@ object Compiler {
     case SExp.Lst(SExp.Keyword("match") :: exp :: cases :: Nil) => ???
   }
 
-  def compileValue(e: SExp): Value = e match {
-    case Literal.Name(n) => Value.Tagged(Symbol.NamedSymbol(n), Value.Unit, Type.Sum(List.empty))
-    case Literal.Bool(b) => Value.Bool(b)
-    case Literal.Int(i) => Value.Int(i)
-    case Literal.Str(s) => Value.Str(s)
-    case SExp.Lst(Literal.Name(s) :: e1 :: Nil) => Value.Tagged(Symbol.NamedSymbol(s), compileValue(e1), ???)
+  def compilePattern(s: SExp): Pattern = ???
 
-    case SExp.Lst(a :: b :: Nil) => ???
+
+  def compileValue(e: SExp): Value = e match {
+    case SExp.Unit => Value.Unit
+    case SExp.Bool(b) => Value.Bool(b)
+    case SExp.Int(i) => Value.Int(i)
+    case SExp.Str(s) => Value.Str(s)
+    case SExp.Name(n) => Value.Tagged(Symbol.NamedSymbol(n), Value.Unit, Type.Sum(List.empty)) // TODO: Type
+    case SExp.Lst(List(SExp.Name(s), e1)) => Value.Tagged(Symbol.NamedSymbol(s), compileValue(e1), ???) // TODO: Type
+    case SExp.Lst(List(e1, e2)) => Value.Tuple2(compileValue(e1), compileValue(e2))
+    case SExp.Lst(List(e1, e2, e3)) => Value.Tuple3(compileValue(e1), compileValue(e2), compileValue(e3))
+    case SExp.Lst(List(e1, e2, e3, e4)) => Value.Tuple4(compileValue(e1), compileValue(e2), compileValue(e3), compileValue(e4))
+    case SExp.Lst(List(e1, e2, e3, e4, e5)) => Value.Tuple5(compileValue(e1), compileValue(e2), compileValue(e3), compileValue(e4), compileValue(e5))
   }
 }
--- src/impl/ast/SExp.scala
+++ src/impl/ast/SExp.scala
@@ -6,20 +6,18 @@ object SExp {
 
   case class Keyword(s: String) extends SExp
 
-  case class Variable(x: java.lang.String) extends SExp
-
-  case class Lst(xs: List[SExp]) extends SExp
-
-}
-
-trait Literal
-
-object Literal {
-  case class Name(s: java.lang.String) extends SExp
+  case object Unit extends SExp
 
   case class Bool(b: scala.Boolean) extends SExp
 
   case class Int(i: scala.Int) extends SExp
 
   case class Str(s: java.lang.String) extends SExp
+
+  case class Name(s: java.lang.String) extends SExp
+
+  case class Variable(x: java.lang.String) extends SExp
+
+  case class Lst(xs: List[SExp]) extends SExp
+
 }
