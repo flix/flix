f0703a4c1bc057394e1e68a15315edbea4311ef0
Magnus Madsen <magnusm@cs.au.dk>
2014-08-27 15:48:13 +0200

    .

--- src/impl/verifier/SmtExp.scala
+++ src/impl/verifier/SmtExp.scala
@@ -1,33 +1,15 @@
 package impl.verifier
 
 import impl.logic.Symbol
-import syntax.Symbols._
 
 /**
  * An SMT-LIB formula.
  */
 sealed trait SmtExp {
-  def fmt(indent: Int): String = this match {
-    case SmtExp.Var(s) => s.fmt
-    case SmtExp.And(formulae) => "(and " + formulae.map(_.fmt(indent)).mkString(" ") + ")"
-    case SmtExp.Or(formulae) => "(or \n" + "    " * (indent + 1) + formulae.map(_.fmt(indent + 1)).mkString("\n" + "    " * (indent + 1)) + ")"
-    case SmtExp.Implies(antecedent, consequent) => "(=> " + antecedent.fmt(indent) + " " + consequent.fmt(indent) + ")"
-    case SmtExp.Eq(lhs, rhs) => "(= " + lhs.fmt(indent) + " " + rhs.fmt(indent) + ")"
-  }
+  def fmt(indent: Int): String = ???
 }
 
 object SmtExp {
-
-  /**
-   * A literal.
-   */
-  case class Literal(token: String) extends SmtExp
-
-
-  /**
-   * A literal.
-   */
-  case class Lst(xs: List[SmtExp]) extends SmtExp
 
   /**
    * A variable.
@@ -35,23 +17,14 @@ object SmtExp {
   case class Var(v: Symbol.VariableSymbol) extends SmtExp
 
   /**
-   * A equality formula.
+   * A literal.
    */
-  case class Eq(left: SmtExp, right: SmtExp) extends SmtExp
+  case class Literal(token: String) extends SmtExp
 
   /**
-   * A conjunction of formulae.
+   * A literal.
    */
-  case class And(formulae: List[SmtExp]) extends SmtExp
+  case class Lst(xs: List[SmtExp]) extends SmtExp
 
-  /**
-   * A disjunction of formulae.
-   */
-  case class Or(formulae: List[SmtExp]) extends SmtExp
-
-  /**
-   * An implication of antecedent => consequent.
-   */
-  case class Implies(antecedent: SmtExp, consequent: SmtExp) extends SmtExp
 
 }
--- src/impl/verifier/Verifier.scala
+++ src/impl/verifier/Verifier.scala
@@ -4,8 +4,6 @@ import java.io.{File, PrintWriter}
 
 import impl.logic.Symbol.{PredicateSymbol => PSym, VariableSymbol => VSym}
 import impl.logic._
-import impl.runtime.Unification
-import syntax.Symbols._
 
 object Verifier {
 
@@ -32,28 +30,28 @@ object Verifier {
     }
   }
 
-//     * Verification conditions for the lattice ordering: Leq.
-//     */
-//    run(lattice, "Leq is reflexivity", LatticeLeq.reflexivity(lattice.name, lattice.leq))
-//    run(lattice, "Leq is antisymmetric", LatticeLeq.antiSymmetri(lattice.name, lattice.leq))
-//    run(lattice, "Leq is transitive", LatticeLeq.transitivity(lattice.name, lattice.leq))
-//    run(lattice, "Leq has a bottom element", LatticeLeq.leastElement(lattice.name, lattice.bot, lattice.leq))
-//
-//    /**
-//     * Verification conditions for the lattice least-upper-bound: Lub.
-//     */
-//    run(lattice, "Lub is an upper bound", LatticeLub.upperBound(lattice.name, lattice.leq, lattice.lub))
-//    run(lattice, "Lub is a least upper bound", LatticeLub.leastUpperBound(lattice.name, lattice.leq, lattice.lub))
-//
-//    /**
-//     * Verification conditions for the lattice height.
-//     */
-//    run(lattice, "The height function is strictly decreasing", LatticeHeight.strictlyDecreasing(lattice.name, lattice.height, lattice.leq))
-//    run(lattice, "The height function is always non-negative", LatticeHeight.nonNegative(lattice.name, lattice.height))
-//
-//    for (s <- lattice.funcs) {
-//      run(lattice, s.fmt + " is monotone", Transfer.isMonotone2(lattice.name, s, lattice.leq))
-//    }
+  //     * Verification conditions for the lattice ordering: Leq.
+  //     */
+  //    run(lattice, "Leq is reflexivity", LatticeLeq.reflexivity(lattice.name, lattice.leq))
+  //    run(lattice, "Leq is antisymmetric", LatticeLeq.antiSymmetri(lattice.name, lattice.leq))
+  //    run(lattice, "Leq is transitive", LatticeLeq.transitivity(lattice.name, lattice.leq))
+  //    run(lattice, "Leq has a bottom element", LatticeLeq.leastElement(lattice.name, lattice.bot, lattice.leq))
+  //
+  //    /**
+  //     * Verification conditions for the lattice least-upper-bound: Lub.
+  //     */
+  //    run(lattice, "Lub is an upper bound", LatticeLub.upperBound(lattice.name, lattice.leq, lattice.lub))
+  //    run(lattice, "Lub is a least upper bound", LatticeLub.leastUpperBound(lattice.name, lattice.leq, lattice.lub))
+  //
+  //    /**
+  //     * Verification conditions for the lattice height.
+  //     */
+  //    run(lattice, "The height function is strictly decreasing", LatticeHeight.strictlyDecreasing(lattice.name, lattice.height, lattice.leq))
+  //    run(lattice, "The height function is always non-negative", LatticeHeight.nonNegative(lattice.name, lattice.height))
+  //
+  //    for (s <- lattice.funcs) {
+  //      run(lattice, s.fmt + " is monotone", Transfer.isMonotone2(lattice.name, s, lattice.leq))
+  //    }
 
 
   /**
@@ -82,19 +80,37 @@ object Verifier {
     }
   }
 
+  //  (declare-datatypes () ((Tuple2_Sort (Tuple2 (x Int) (y Int)))))
+  //  (declare-const z Tuple2_Sort)
+  //  (assert (= z (Tuple2 1 2)))
+  //  (check-sat)
+  //  (get-model)
+
   def compileTerm(t: Term): SmtExp = t match {
-    case Term.Unit =>  SmtExp.Literal("unit")
+    case Term.Unit => SmtExp.Literal("unit")
     case Term.Bool(b) => SmtExp.Literal(b.toString)
     case Term.Int(i) => SmtExp.Literal(i.toString)
     case Term.Str(s) => SmtExp.Literal(s.hashCode.toString) // TODO: Need a better way to map string constants to integers.
 
     case Term.Var(x) => SmtExp.Var(x)
-      
-    case Term.Tagged(x, t1, _) => SmtExp.Lst(List(SmtExp.Literal(x.s), compileTerm(t1)))
-    case Term.Tuple2(t1, t2) => ???
+
+    case Term.Tagged(x, t1, _) =>             List(SmtExp.Literal(x.s), compileTerm(t1))
+    case Term.Tuple2(t1, t2) =>               List(SmtExp.Literal("Tuple2"), compileTerm(t1), compileTerm(t2))
+    case Term.Tuple3(t1, t2, t3) =>           List(SmtExp.Literal("Tuple3"), compileTerm(t1), compileTerm(t2), compileTerm(t3))
+    case Term.Tuple4(t1, t2, t3, t4) =>       List(SmtExp.Literal("Tuple4"), compileTerm(t1), compileTerm(t2), compileTerm(t3), compileTerm(t4))
+    case Term.Tuple5(t1, t2, t3, t4, t5) =>   List(SmtExp.Literal("Tuple5"), compileTerm(t1), compileTerm(t2), compileTerm(t3), compileTerm(t4), compileTerm(t5))
 
   }
 
-  def compileType(typ: Type): SmtExp = ???
+  def compileType(typ: Type): SmtExp = typ match {
+    case Type.Tagged(s, typ1) => ???
+    case Type.Tuple2(typ1, typ2) => ???
+    case Type.Tuple3(typ1, typ2, typ3) => ???
+    case Type.Tuple4(typ1, typ2, typ3, typ4) => ???
+    case Type.Tuple5(typ1, typ2, typ3, typ4, typ5) => ???
+  }
+
+  import scala.language.implicitConversions
+  private implicit def lift(xs: List[SmtExp]): SmtExp = SmtExp.Lst(xs)
 
 }
