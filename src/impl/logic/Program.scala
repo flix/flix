771beb84ac557f8bdeb67349e3b9ef23662e0704
Magnus Madsen <magnusm@cs.au.dk>
2014-09-22 12:31:00 +0200

    .

--- src/impl/logic/Program.scala
+++ src/impl/logic/Program.scala
@@ -8,14 +8,14 @@ case class Program(declarations: List[Declaration], constraints: List[Constraint
    * Returns the list of facts.
    */
   def facts: List[Constraint.Fact] = constraints collect {
-    case f@Constraint.Fact(head) => f
+    case f: Constraint.Fact => f
   }
 
   /**
    * Returns the list of rules.
    */
   def rules: List[Constraint.Rule] = constraints collect {
-    case r@Constraint.Rule(head, body) => r
+    case r: Constraint.Rule => r
   }
 
   /**
