d53a7112e3c317770452ea4ee0de135327d35e48
Ondrej Lhotak <olhotak@uwaterloo.ca>
2014-11-18 11:16:08 +0100

    initial Scala API

---
+++ src/api/Flix.scala
@@ -0,0 +1,16 @@
+package api
+
+import impl.ast._
+import impl.runtime._
+
+class Flix {
+  def apply(s: String) = Parser.parse(s).foreach(compiler.compileDeclaration(_))
+  def solve(options: Options = Options(propagation = Propagation.Full, simplify = Simplify.Enable)) = {
+    solver = new Solver(compiler.outputProgram, options)
+    solver.solve
+  }
+  def print() = solver.print
+
+  val compiler = new Compiler
+  var solver: Solver = null
+}
