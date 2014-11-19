494b65c1795208c381dcd7014bfe08275b78e161
Magnus Madsen <magnusm@cs.au.dk>
2014-11-19 22:57:52 +0100

    .

--- test/TestParser.scala
+++ test/TestParser.scala
@@ -5,9 +5,31 @@ import scala.collection.immutable.Seq
 class TestParser extends FunSuite {
 
   test("Parser.Type01") {
+    val s = """type t = Unit;"""
+    val a = Ast.Root(Seq(Ast.Declaration.TypeDecl("t", Ast.Type.Unit)))
+
+    assertResult(a)(Parsing.parse(s))
+  }
+
+  test("Parser.Type02") {
     val s = """type t = Bool;"""
     val a = Ast.Root(Seq(Ast.Declaration.TypeDecl("t", Ast.Type.Bool)))
 
     assertResult(a)(Parsing.parse(s))
   }
+
+  test("Parser.Type03") {
+    val s = """type t = Int;"""
+    val a = Ast.Root(Seq(Ast.Declaration.TypeDecl("t", Ast.Type.Int)))
+
+    assertResult(a)(Parsing.parse(s))
+  }
+
+  test("Parser.Type04") {
+    val s = """type t = Str;"""
+    val a = Ast.Root(Seq(Ast.Declaration.TypeDecl("t", Ast.Type.Str)))
+
+    assertResult(a)(Parsing.parse(s))
+  }
+
 }
