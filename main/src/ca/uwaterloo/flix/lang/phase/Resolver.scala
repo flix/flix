1af8229baa3150562130af9ad44bc148a7b3b1cb
Magnus Madsen <magnusm@cs.au.dk>
2015-09-17 17:44:26 -0400

    .

--- main/src/ca/uwaterloo/flix/lang/phase/Resolver.scala
+++ main/src/ca/uwaterloo/flix/lang/phase/Resolver.scala
@@ -2,6 +2,7 @@ package ca.uwaterloo.flix.lang.phase
 
 import ca.uwaterloo.flix.lang.ast.{WeededAst, ResolvedAst, ParsedAst}
 import util.Validation
+import util.Validation._
 
 object Resolver {
 
@@ -11,29 +12,54 @@ object Resolver {
 
   object ResolverError {
 
-    case class UnknownPredicate(name: ParsedAst.QName) extends ResolverError
+    //case class UnknownPredicate(name: ParsedAst.QName) extends ResolverError
 
-    case class IllegalReference(expect: String, actual: String) extends ResolverError
+    //case class IllegalReference(expect: String, actual: String) extends ResolverError
 
   }
 
-  /**
-   * - Resolve proposition vs predicate.
-   * - Resolve named types
-   */
+  def resolve(ast: WeededAst.Root): Validation[ResolvedAst.Root, ResolverError] = {
 
-  def symbols(ast: ParsedAst.Root): Map[ParsedAst.QName, ParsedAst] = ???
 
-  def link(p: ParsedAst.Predicate, symbols: Map[ParsedAst.QName, ParsedAst]): ResolvedAst.Predicate =
-    symbols.get(p.name) match {
-      case None => ??? //UnknownPredicate(p.name)
-      case Some(d: ParsedAst.Definition.Function) => ResolvedAst.Predicate.Functional()
-      case Some(d: ParsedAst.Definition.Relation) => ResolvedAst.Predicate.Relational()
-      case Some(otherDecl) => ??? // IllegalReference("Relation", otherDecl)
+    ???
+  }
+
+  object Declaration {
+
+    def symbols(wd: WeededAst.Declaration, namespace: List[String]): Validation[Map[ResolvedAst.RName, WeededAst.Definition], ResolverError] = wd match {
+      case WeededAst.Declaration.Namespace(ParsedAst.QName(parts, location), body) =>
+        
+        ???
+
+      case d: WeededAst.Declaration.Fact => Map.empty[ResolvedAst.RName, WeededAst.Definition].toSuccess // nop
+      case d: WeededAst.Declaration.Rule => Map.empty[ResolvedAst.RName, WeededAst.Definition].toSuccess // nop
+      case d: WeededAst.Definition => Definition.symbols(d)
     }
 
-  def resolve(ast: WeededAst.Root): Validation[ResolvedAst.Root, ResolverError] = {
-   ???
+
+    def link(p: ParsedAst.Predicate, globals: Map[ParsedAst.QName, ParsedAst]): ResolvedAst.Predicate =
+      globals.get(p.name) match {
+        case None => ??? //UnknownPredicate(p.name)
+        case Some(d: ParsedAst.Definition.Function) => ResolvedAst.Predicate.Functional()
+        case Some(d: ParsedAst.Definition.Relation) => ResolvedAst.Predicate.Relational()
+        case Some(otherDecl) => ??? // IllegalReference("Relation", otherDecl)
+      }
+  }
+
+  object Definition {
+
+    def symbols(d: WeededAst.Definition): Validation[Map[ResolvedAst.RName, WeededAst.Definition], ResolverError] = d match {
+      case _ => ???
+    }
+
+  }
+
+  object Literal {
+
+  }
+
+  object Expression {
+
   }
 
 
