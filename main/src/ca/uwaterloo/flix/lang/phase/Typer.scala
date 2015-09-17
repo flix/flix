325ae751387ced2bc5fc8796e34b7725f523e566
Magnus Madsen <magnusm@cs.au.dk>
2015-09-17 12:41:41 -0400

    .

--- main/src/ca/uwaterloo/flix/lang/phase/Typer.scala
+++ main/src/ca/uwaterloo/flix/lang/phase/Typer.scala
@@ -18,28 +18,37 @@ object Typer {
 
   def typecheck(rast: ResolvedAst) = ???
 
-  // TODO: locals: Map[String, TypedAst.Type], globals: Map[???, TypedAst.Type]
-  def typeExpression(rast: ResolvedAst.Expression): Validation[TypedAst.Expression, TypeError] = rast match {
-    case ResolvedAst.Expression.Var(ident) => ??? // pull type out of map.
-
-    case ResolvedAst.Expression.Ref(name, decl) => ??? // todo then the declaration must have a type...
-
-    case ResolvedAst.Expression.Let(ident, value, body) =>
-      typeExpression(value) map {
-        case valueTpe => typeExpression(body) // pass ident.name -> valueTpe
-      }
-      ???
-
-    case ResolvedAst.Expression.IfThenElse(re1, re2, re3) =>
-      @@(typeExpression(re1), typeExpression(re2), typeExpression(re3)) flatMap {
-        case (e1, e2, e3) =>
-          val conditionType = expect(TypedAst.Type.Bool)(e1.tpe)
-          val expressionType = expectEqual(e2.tpe, e3.tpe)
-          #@(conditionType, expressionType) map {
-            case tpe => TypedAst.Expression.IfThenElse(e1, e2, e3, tpe)
-          }
-      }
+  object Literal {
+    // def typer()
   }
+
+  object Expression {
+
+    // TODO: locals: Map[String, TypedAst.Type], globals: Map[???, TypedAst.Type]
+    def typer(rast: ResolvedAst.Expression): Validation[TypedAst.Expression, TypeError] = rast match {
+      case ResolvedAst.Expression.Var(ident) => ??? // pull type out of map.
+
+      case ResolvedAst.Expression.Ref(name, decl) => ??? // todo then the declaration must have a type...
+
+      case ResolvedAst.Expression.Let(ident, value, body) =>
+        typer(value) map {
+          case valueTpe => typer(body) // pass ident.name -> valueTpe
+        }
+        ???
+
+      case ResolvedAst.Expression.IfThenElse(re1, re2, re3) =>
+        @@(typer(re1), typer(re2), typer(re3)) flatMap {
+          case (e1, e2, e3) =>
+            val conditionType = expect(TypedAst.Type.Bool)(e1.tpe)
+            val expressionType = expectEqual(e2.tpe, e3.tpe)
+            #@(conditionType, expressionType) map {
+              case tpe => TypedAst.Expression.IfThenElse(e1, e2, e3, tpe)
+            }
+        }
+    }
+
+  }
+
 
   def expect(tpe1: TypedAst.Type)(tpe2: TypedAst.Type): Validation[TypedAst.Type, TypeError] = ???
 
