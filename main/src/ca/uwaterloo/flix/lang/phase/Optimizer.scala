6969cbff657cb860efa9b3b5536dd814c3c8dcd9
Magnus Madsen <magnusm@cs.au.dk>
2015-09-18 12:05:52 -0400

    .

--- main/src/ca/uwaterloo/flix/lang/phase/Optimizer.scala
+++ main/src/ca/uwaterloo/flix/lang/phase/Optimizer.scala
@@ -7,6 +7,8 @@ object Optimizer {
   // - Inline all expressions.
   // - Rewrite all lambdas to be single argument.
   // - Rewrite to a normal form (ANF)?
+  // - Rewrite pattern matches?
+  // - Reduce the number of unary and binary operators?
   // -
   // - Bit encodings?
   // - High-level optimizations?
