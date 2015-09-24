6998d823bcd47ac75199bdaa504c35550f58531b
Ming-Ho Yee <mh@mhyee.com>
2015-09-24 10:29:11 -0400

    Make it explicit that we want a runtime exception if the Value conversions fail

--- main/src/ca/uwaterloo/flix/runtime/Value.scala
+++ main/src/ca/uwaterloo/flix/runtime/Value.scala
@@ -3,11 +3,20 @@ package ca.uwaterloo.flix.runtime
 import ca.uwaterloo.flix.lang.ast.Name
 
 sealed trait Value {
-  def toBool: Boolean = this.asInstanceOf[Value.Bool].b
+  def toBool: Boolean = this match {
+    case Value.Bool(b) => b
+    case _ => throw new RuntimeException("Expected a Value.Bool.")
+  }
 
-  def toInt: Int = this.asInstanceOf[Value.Int].i
+  def toInt: Int = this match {
+    case Value.Int(i) => i
+    case _ => throw new RuntimeException("Expected a Value.Int.")
+  }
 
-  def toStr: String = this.asInstanceOf[Value.Str].s
+  def toStr: String = this match {
+    case Value.Str(s) => s
+    case _ => throw new RuntimeException("Expected a Value.Str.")
+  }
 }
 
 object Value {
