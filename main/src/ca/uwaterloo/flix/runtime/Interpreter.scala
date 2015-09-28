fff8390bac110eb04be97ae352cf0b7004facfcc
Ming-Ho Yee <mh@mhyee.com>
2015-09-28 11:47:06 -0400

    First attempt at implementing Expression.Match and the unifier

--- main/src/ca/uwaterloo/flix/runtime/Interpreter.scala
+++ main/src/ca/uwaterloo/flix/runtime/Interpreter.scala
@@ -1,6 +1,6 @@
 package ca.uwaterloo.flix.runtime
 
-import ca.uwaterloo.flix.lang.ast.TypedAst.{Expression, Literal, Type, FormalArg, Root}
+import ca.uwaterloo.flix.lang.ast.TypedAst.{Expression, Literal, Pattern, Type, FormalArg, Root}
 import ca.uwaterloo.flix.lang.ast.{ParsedAst, BinaryOperator, UnaryOperator}
 
 object Interpreter {
@@ -21,8 +21,8 @@ object Interpreter {
             eval(body, root, newEnv)
           case _ => assert(false, "Expected a function."); Value.Unit
         }
-      case Expression.Unary(op, exp, _) => apply(op, eval(exp, root, env))
-      case Expression.Binary(op, exp1, exp2, _) => apply(op, eval(exp1, root, env), eval(exp2, root, env))
+      case Expression.Unary(op, exp, _) => evalUnary(op, eval(exp, root, env))
+      case Expression.Binary(op, exp1, exp2, _) => evalBinary(op, eval(exp1, root, env), eval(exp2, root, env))
       case Expression.IfThenElse(exp1, exp2, exp3, tpe) =>
         val cond = eval(exp1, root, env).toBool
         if (cond) eval(exp2, root, env) else eval(exp3, root, env)
@@ -32,12 +32,17 @@ object Interpreter {
           body, Type.Function(List(value.tpe), tpe))
         val desugared = Expression.Apply(func, List(value), tpe)
         eval(desugared, root, env)
-      case Expression.Match(exp, rules, _) => ??? // TODO(mhyee): Implement this. Need to write a Unify function
+      case Expression.Match(exp, rules, _) =>
+        val value = eval(exp, root, env)
+        matchRule(rules, value) match {
+          case Some((matchExp, matchEnv)) => eval(matchExp, root, env ++ matchEnv)
+          case None => throw new RuntimeException(s"Unmatched value $value.")
+        }
       case Expression.Tag(name, ident, exp, _) => Value.Tag(name, ident.name, eval(exp, root, env))
       case Expression.Tuple(elms, _) => Value.Tuple(elms.map(e => eval(e, root, env)))
       case Expression.Error(location, tpe) => location.path match {
-        case Some(path) => throw new RuntimeException(s"Error at $path:${location.line}:${location.column}")
-        case None => throw new RuntimeException("Error at unknown location")
+        case Some(path) => throw new RuntimeException(s"Error at $path:${location.line}:${location.column}.")
+        case None => throw new RuntimeException("Error at unknown location.")
       }
     }
   }
@@ -51,13 +56,13 @@ object Interpreter {
     case Literal.Tuple(elms, _) => Value.Tuple(elms.map(evalLit))
   }
 
-  private def apply(op: UnaryOperator, v: Value): Value = op match {
+  private def evalUnary(op: UnaryOperator, v: Value): Value = op match {
     case UnaryOperator.Not => Value.Bool(!v.toBool)
     case UnaryOperator.UnaryPlus => Value.Int(+v.toInt)
     case UnaryOperator.UnaryMinus => Value.Int(-v.toInt)
   }
 
-  private def apply(op: BinaryOperator, v1: Value, v2: Value): Value = op match {
+  private def evalBinary(op: BinaryOperator, v1: Value, v2: Value): Value = op match {
     case BinaryOperator.Plus => Value.Int(v1.toInt + v2.toInt)
     case BinaryOperator.Minus => Value.Int(v1.toInt - v2.toInt)
     case BinaryOperator.Times => Value.Int(v1.toInt * v2.toInt)
@@ -75,5 +80,27 @@ object Interpreter {
     case BinaryOperator.Maximum => Value.Int(math.max(v1.toInt, v2.toInt))
     case BinaryOperator.Union | BinaryOperator.Subset =>
       assert(false, "Can't have union or subset operators."); Value.Unit
+  }
+
+  private def matchRule(rules: List[(Pattern, Expression)], value: Value): Option[(Expression, Env)] = rules match {
+    case (pattern, exp) :: rest => unify(pattern, value) match {
+      case Some(env) => Some((exp, env))
+      case None => matchRule(rest, value)
+    }
+    case Nil => None
+  }
+
+  private def unify(pattern: Pattern, value: Value): Option[Env] = (pattern, value) match {
+    case (Pattern.Wildcard(_), _) => Some(Map())
+    case (Pattern.Var(ident, _), _) => Some(Map(ident -> value))
+    case (Pattern.Lit(lit, _), _) if evalLit(lit) == value => Some(Map())
+    case (Pattern.Tag(name1, ident1, innerPat, _), Value.Tag(name2, ident2, innerVal))
+      if name1 == name2 && ident1.name == ident2 => unify(innerPat, innerVal)
+    case (Pattern.Tuple(pats, _), Value.Tuple(vals)) =>
+      val envs = pats.zip(vals).map { case (p, v) => unify(p, v) }.collect { case Some(e) => e }
+      if (pats.size == envs.size)
+        Some(envs.foldLeft(Map[ParsedAst.Ident, Value]()) { case (acc, newEnv) => acc ++ newEnv })
+      else None
+    case _ => None
   }
 }
