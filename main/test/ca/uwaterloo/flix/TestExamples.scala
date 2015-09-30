5510a794018ae3cb8b2593af40f64c0061956ec7
Magnus Madsen <magnusm@cs.au.dk>
2015-09-30 16:08:03 -0400

    .

--- main/test/ca/uwaterloo/flix/TestExamples.scala
+++ main/test/ca/uwaterloo/flix/TestExamples.scala
@@ -8,16 +8,29 @@ import org.scalatest.FunSuite
 
 class TestExamples extends FunSuite {
 
+  // TODO: Figure out proper interface for the compiler and runtime.
+
+  /////////////////////////////////////////////////////////////////////////////
+  // Algorithms                                                              //
+  /////////////////////////////////////////////////////////////////////////////
+  test("Cycles.flix") {
+    Compiler.compile(Paths.get("./examples/algorithms/Cycles.flix"))
+  }
+
+  /////////////////////////////////////////////////////////////////////////////
+  // Models                                                                  //
+  /////////////////////////////////////////////////////////////////////////////
+
   test("Bank.flix") {
     Compiler.compile(Paths.get("./examples/models/Bank.flix"))
   }
 
   test("Company.flix") {
-    Compiler.compile(Paths.get("./examples/Company.flix"))
+    Compiler.compile(Paths.get("./examples/models/Company.flix"))
   }
 
   test("University.flix") {
-    Compiler.compile(Paths.get("./examples/University.flix"))
+    Compiler.compile(Paths.get("./examples/models/University.flix"))
   }
 
 }
