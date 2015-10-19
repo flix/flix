766cac19d1d489b4ac26b4f7e4bc27b98562f915
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2015-10-19 11:05:06 -0400

    .

--- main/src/ca/uwaterloo/flix/Flix.scala
+++ main/src/ca/uwaterloo/flix/Flix.scala
@@ -1,16 +1,34 @@
 package ca.uwaterloo.flix
 
-import java.nio.file.{InvalidPathException, Files, Paths}
+import java.nio.file.{Path, InvalidPathException, Files, Paths}
 
 import ca.uwaterloo.flix.language.Compiler
+import ca.uwaterloo.flix.runtime.{Model, Solver}
+import ca.uwaterloo.flix.util.Validation
+import ca.uwaterloo.flix.util.Validation._
 
 object Flix {
+
+  trait FlixError {
+
+  }
 
   def solve(s: String): Unit = {
     if (isValidPath(s))
       Compiler.compile(Paths.get(s))
     else
       Compiler.compile(s)
+  }
+
+  def solve(paths: Traversable[Path]): Validation[Model, FlixError] = {
+    val ast = Compiler.compile(paths)
+
+    implicit val sCtx = Solver.SolverContext(ast.get)
+
+    val solver = new Solver()
+    val model = solver.solve()
+
+    model.toSuccess
   }
 
   private def isValidPath(s: String): Boolean = try {
