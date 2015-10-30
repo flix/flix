bac6da6d6f863ef4562d0b1c2c6eb5816bc20511
Ming-Ho Yee <mh@mhyee.com>
2015-10-29 20:18:08 -0400

    Add some tests

--- main/src/ca/uwaterloo/flix/util/misc/SampleLattice.java
+++ main/src/ca/uwaterloo/flix/util/misc/SampleLattice.java
@@ -7,19 +7,15 @@ public class SampleLattice {
     public static String TOP = "<<top>>";
 
     public static boolean leq(String e1, String e2) {
-        return e1.equals(e2);
+        return e1.compareTo(e2) <= 0;
     }
 
     public static boolean testBool(boolean b1, boolean b2) {
         return b1 && b2;
     }
 
-    public static String concat(String e1, String e2) {
-        return e1 + e2;
-    }
-
     public static String lub(String e1, String e2) {
-        return "";
+        return e1 + e2;
     }
 
     public static String glb(String e1, String e2) {
