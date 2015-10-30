a3a11d3c7e82487d20c3efa02e579e9c8753ac59
Ming-Ho Yee <mh@mhyee.com>
2015-10-30 16:14:54 -0400

    Remove tests from Parity.flix and SampleLattice.java.
    Move them to NativeTest.flix and NativeTest.java.
    
    In these tests, we demonstrate calling native methods with a mix of Java
    and Flix values. Boxed ints and bools are also handled.

--- main/src/ca/uwaterloo/flix/util/misc/SampleLattice.java
+++ main/src/ca/uwaterloo/flix/util/misc/SampleLattice.java
@@ -7,18 +7,15 @@ public class SampleLattice {
     public static String TOP = "<<top>>";
 
     public static boolean leq(String e1, String e2) {
-        return e1.compareTo(e2) <= 0;
-    }
-
-    public static boolean testBool(boolean b1, boolean b2) {
-        return b1 && b2;
+        return false;
     }
 
     public static String lub(String e1, String e2) {
-        return e1 + e2;
+        return "";
     }
 
     public static String glb(String e1, String e2) {
         return "";
     }
+
 }
