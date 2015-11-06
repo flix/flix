40496278d138a833829f9e2b3b68ac39c6300931
Ming-Ho Yee <mh@mhyee.com>
2015-11-06 13:56:04 -0500

    Add some tests for Set.
    These are sets of Native types.

--- main/src/ca/uwaterloo/flix/util/misc/ScalaNative.scala
+++ main/src/ca/uwaterloo/flix/util/misc/ScalaNative.scala
@@ -4,6 +4,7 @@ object ScalaNative {
   // Note that these values are compiled as a static method
   val strTuple2 = ("foo", "bar")
   val tuple6 = (true, false, 1, 5, ("foo", "bar"), 42)
+  val set = Set(1, 2, 3)
 
   def mkTuple2(t1: Int, t2: Int) = (t1, t2)
   def incrTuple2(t: (Int, Int)) = (t._1 + 1, t._2 + 1)
@@ -16,6 +17,6 @@ object ScalaNative {
 
   def reverseTuple6(t: (Boolean, Boolean, Int, Int, (String, String), Int)) = (t._6, t._5, t._4, t._3, t._2, t._1)
 
-  val set = Set(1, 2, 3)
-  def mapper(s: Set[Int], i: Int) = s.map(_ + i)
+  def mkSet(i: Int) = (0 until i).toSet
+  def incrSetBy(s: Set[Int], i: Int) = s.map(_ + i)
 }
