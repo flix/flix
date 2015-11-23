09ebf39b2e97e4801333660623fd08f1dd315b49
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2015-11-23 17:14:46 -0500

    Debugger

--- main/src/ca/uwaterloo/flix/runtime/debugger/web/js/app.jsx
+++ main/src/ca/uwaterloo/flix/runtime/debugger/web/js/app.jsx
@@ -331,6 +331,55 @@ var RelationPage = React.createClass({
     }
 });
 
+
+var randomScalingFactor = function () {
+    return Math.round(Math.random() * 100)
+};
+var lineChartData = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+        {
+            label: "My First dataset",
+            fillColor: "rgba(220,220,220,0.2)",
+            strokeColor: "rgba(220,220,220,1)",
+            pointColor: "rgba(220,220,220,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
+            data: [randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor()]
+        },
+        {
+            label: "My Second dataset",
+            fillColor: "rgba(151,187,205,0.2)",
+            strokeColor: "rgba(151,187,205,1)",
+            pointColor: "rgba(151,187,205,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(151,187,205,1)",
+            data: [randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor()]
+        }
+    ]
+};
+
+
+var MyComponent = React.createClass({
+    componentDidMount: function () {
+        console.log("hello world")
+        var ctx = this.canvasRef.getContext("2d");
+        this.myLine = new Chart(ctx).Line(lineChartData, {
+            responsive: false
+        });
+    },
+
+    componentWillUnmount: function () {
+        this.myLine.destroy();
+    },
+
+    render: function () {
+        return <canvas ref={(ref) => this.canvasRef = ref}/>
+    }
+});
+
 /**
  * Phases page.
  */
@@ -347,6 +396,8 @@ var PhasesPage = React.createClass({
             <div>
                 <Head name="Performance / Phases"/>
                 <Table table={table}/>
+
+                <MyComponent/>
             </div>
         );
     }
