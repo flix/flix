f513077fdc56811742f9a70c7e711cc26400f78e
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2015-11-23 17:31:41 -0500

    Debugger

--- main/src/ca/uwaterloo/flix/runtime/debugger/web/js/app.jsx
+++ main/src/ca/uwaterloo/flix/runtime/debugger/web/js/app.jsx
@@ -331,33 +331,27 @@ var RelationPage = React.createClass({
     }
 });
 
-
-var randomScalingFactor = function () {
-    return Math.round(Math.random() * 100)
-};
-var barChartData = {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
-    datasets: [
-        {
-            fillColor: "rgba(220,220,220,0.5)",
-            strokeColor: "rgba(220,220,220,0.8)",
-            highlightFill: "rgba(220,220,220,0.75)",
-            highlightStroke: "rgba(220,220,220,1)",
-            data: [randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor()]
-        },
-        {
-            fillColor: "rgba(151,187,205,0.5)",
-            strokeColor: "rgba(151,187,205,0.8)",
-            highlightFill: "rgba(151,187,205,0.75)",
-            highlightStroke: "rgba(151,187,205,1)",
-            data: [randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor()]
-        }
-    ]
-}
-
-
 var BarChart = React.createClass({
     componentDidMount: function () {
+
+
+        var randomScalingFactor = function () {
+            return Math.round(Math.random() * 100)
+        };
+        var barChartData = {
+            labels: ["January", "February", "March", "April", "May", "June", "July"],
+            datasets: [
+                {
+                    fillColor: "rgba(151,187,205,0.5)",
+                    strokeColor: "rgba(151,187,205,0.8)",
+                    highlightFill: "rgba(151,187,205,0.75)",
+                    highlightStroke: "rgba(151,187,205,1)",
+                    data: [randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor()]
+                }
+            ]
+        }
+
+
         console.log("hello world")
         var ctx = this.canvasRef.getContext("2d");
         this.myLine = new Chart(ctx).Bar(barChartData, {
