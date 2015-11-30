52517ac7bde8a2dae093d25737a48befdd9600a9
Magnus Madsen <magnusm@cs.au.dk>
2015-11-30 14:59:37 -0500

    Skeleton for CodeGenIR.

--- main/src/ca/uwaterloo/flix/language/backend/ir/CodeGenIR.scala
+++ main/src/ca/uwaterloo/flix/language/backend/ir/CodeGenIR.scala
@@ -27,22 +27,27 @@ object CodeGenIR {
 
     case class Int(literal: Int, tpe: CodeGenIR.Type, loc: SourceLocation) extends CodeGenIR.Expression
 
+
     case class Var(localVar: CodeGenIR.LocalVar, tpe: CodeGenIR.Type, loc: SourceLocation) extends CodeGenIR.Expression
 
-    case class Ref(name: Name.Resolved, tpe: CodeGenIR.Type, loc: SourceLocation) extends CodeGenIR.Expression
 
     case class Apply(name: Name.Resolved, args: List[CodeGenIR.Expression], tpe: CodeGenIR.Type, loc: SourceLocation) extends CodeGenIR.Expression
 
+
     case class Let(localVar: CodeGenIR.LocalVar, exp1: CodeGenIR.Expression, exp2: CodeGenIR.Expression, tpe: CodeGenIR.Type, loc: SourceLocation) extends CodeGenIR.Expression
+
 
     case class Unary(op: UnaryOperator, exp: CodeGenIR.Expression, tpe: CodeGenIR.Type, loc: SourceLocation) extends CodeGenIR.Expression
 
+
     case class Binary(op: BinaryOperator, exp1: CodeGenIR.Expression, exp2: CodeGenIR.Expression, tpe: CodeGenIR.Type, loc: SourceLocation) extends CodeGenIR.Expression
+
 
     case class IfThenElse(exp1: CodeGenIR.Expression, exp2: CodeGenIR.Expression, exp3: CodeGenIR.Expression, tpe: CodeGenIR.Type, loc: SourceLocation) extends CodeGenIR.Expression
 
     // TODO: ElementAt( (1,2), 0) = 1
     case class ElementAt(exp1: CodeGenIR.Expression, exp2: CodeGenIR.Expression, tpe: CodeGenIR.Type, loc: SourceLocation) extends CodeGenIR.Expression
+
 
     case class Tag(tag: TagName, exp: CodeGenIR.Expression, tpe: CodeGenIR.Type, loc: SourceLocation) extends CodeGenIR.Expression
 
@@ -50,8 +55,7 @@ object CodeGenIR {
 
     case class Tuple(elms: List[CodeGenIR.Expression], tpe: CodeGenIR.Type, loc: SourceLocation) extends CodeGenIR.Expression
 
-    case class Set(elms: List[CodeGenIR.Expression], tpe: CodeGenIR.Type.Set, loc: SourceLocation) extends CodeGenIR.Expression
-
+    case class Set(elms: List[CodeGenIR.Expression], tpe: CodeGenIR.Type, loc: SourceLocation) extends CodeGenIR.Expression
 
     case class Error(loc: SourceLocation) extends CodeGenIR.Expression
 
@@ -61,11 +65,35 @@ object CodeGenIR {
 
   object Type {
 
-    case object Int extends CodeGenIR.Type
+    case object Int32 extends CodeGenIR.Type
 
-    case class Set() extends CodeGenIR.Type
+    /**
+     * A type that represents an offset to a string in the value pool.
+     */
+    case object StrRef extends CodeGenIR.Type
 
-    // TODO: Function
+    /**
+     * A type that represents an offset to a tag in the value pool.
+     */
+    case object TagRef extends CodeGenIR.Type
+
+    /**
+     * A type that represents an offset to a tuple in the value pool.
+     */
+    case object TupleRef extends CodeGenIR.Type
+
+    /**
+     * A type that represents an offset to a set in the value pool.
+     */
+    case object SetRef extends CodeGenIR.Type
+
+    /**
+     * A type that represents an offset to a map in the value pool.
+     */
+    case object MapRef extends CodeGenIR.Type
+
+
+    case class Lambda(args: List[CodeGenIR.Type], retTpe: CodeGenIR.Type) extends CodeGenIR.Type
 
   }
 
