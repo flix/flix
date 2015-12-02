bb3c961c9de18d5776eddd779400db3a508d8f78
Ming-Ho Yee <mh@mhyee.com>
2015-12-02 17:39:26 -0500

    Refactor to a functional style that explicitly passes visitors around

--- main/src/ca/uwaterloo/flix/language/backend/phase/Codegen.scala
+++ main/src/ca/uwaterloo/flix/language/backend/phase/Codegen.scala
@@ -1,12 +1,13 @@
 package ca.uwaterloo.flix.language.backend.phase
 
-import ca.uwaterloo.flix.language.backend.ir.CodeGenIR.{Definition}
+import ca.uwaterloo.flix.language.backend.ir.CodeGenIR.Definition
 
 import org.objectweb.asm._
 import org.objectweb.asm.Opcodes._
 import org.objectweb.asm.util.CheckClassAdapter
 
 object Codegen {
+  // Example code that will be deleted later
   def genTestAsm(): Array[Byte] = {
     val cw = new ClassWriter(ClassWriter.COMPUTE_FRAMES)
     val cv = new CheckClassAdapter(cw)
@@ -38,19 +39,50 @@ object Codegen {
 
     cw.toByteArray
   }
-}
 
-// TODO: What other pieces of the interface/pipeline do we need?
-// How is Codegen called? Where do we put the generated methods? How are the generated methods called?
-// TODO: Constructor should take a list of (Flix) functions? Does it need anything else? Flix file = class name?
-class Codegen(name: String, definitions: List[Definition]) {
-  private val functions = definitions.collect { case f: Definition.Function => f }
+  /*
+   * Given a list of Flix definitions, compile the definitions to bytecode and put them in a JVM class.
+   * For now, we put all definitions in a single class: ca.uwaterloo.flix.runtime.compiled.FlixDefinitions.
+   * The Flix function A::B::C::foo is compiled as the method A$B$C$foo.
+   */
+  def compile(definitions: List[Definition]): Array[Byte] = {
+    val functions = definitions.collect { case f: Definition.Function => f }
+    val classWriter = new ClassWriter(ClassWriter.COMPUTE_FRAMES)
+    val visitor = new CheckClassAdapter(classWriter)
 
-  private val classWriter = new ClassWriter(0)
-  // Note: CheckClassAdapter requires us to manually calculate maxs and frames
-  private val visitor = new CheckClassAdapter(classWriter)
+    // Initialize the visitor to create a class
+    visitor.visit(V1_7, ACC_PUBLIC + ACC_SUPER, "ca/uwaterloo/flix/runtime/compiled/FlixDefinitions", null, "java/lang/Object", null)
 
-  def generateMethod(function: Definition.Function): Unit = {
+    // Generate the constructor for the class
+    compileConstructor(visitor)
+
+    // Generate code for each of the Flix functions
+    functions.foreach { f => compileFunction(visitor, f) }
+
+    // Finish the traversal and convert to a byte array
+    visitor.visitEnd()
+    classWriter.toByteArray
+  }
+
+  /*
+   * Generate the constructor.
+   */
+  def compileConstructor(visitor: ClassVisitor): Unit = {
+    val mv = visitor.visitMethod(ACC_PUBLIC, "<init>", "()V", null, null)
+    mv.visitCode()
+    mv.visitVarInsn(ALOAD, 0)
+    mv.visitMethodInsn(INVOKESPECIAL, "java/lang/Object", "<init>", "()V", false)
+    mv.visitInsn(RETURN)
+    mv.visitMaxs(1, 1)
+    mv.visitEnd()
+  }
+
+  /*
+   * Given a definition for a Flix function, generate bytecode.
+   * The Flix function A::B::C::foo is compiled as the method A$B$C$foo.
+   */
+  def compileFunction(visitor: ClassVisitor, function: Definition.Function): Unit = {
+    // TODO: Proper function name and signature
     val mv = visitor.visitMethod(ACC_PUBLIC + ACC_STATIC, "f", "()V", null, null)
     mv.visitCode()
 
@@ -59,30 +91,5 @@ class Codegen(name: String, definitions: List[Definition]) {
     mv.visitInsn(RETURN)  // TODO: Proper return instruction
     mv.visitMaxs(0, 0)    // TODO: Calculate maxs
     mv.visitEnd()
-  }
-
-  lazy val bytecode: Array[Byte] = {
-    // Initialize the visitor to create "public class ca.uwaterloo.flix.TestAsm extends java.lang.Object"
-    // with no generic types and no interfaces
-    // TODO: Proper name for the class
-    visitor.visit(V1_7, ACC_PUBLIC + ACC_SUPER, "ca/uwaterloo/flix/TestAsm", null, "java/lang/Object", null)
-
-    // Generate the constructor for the class
-    {
-      val mv = visitor.visitMethod(ACC_PUBLIC, "<init>", "()V", null, null)
-      mv.visitCode()
-      mv.visitVarInsn(ALOAD, 0)
-      mv.visitMethodInsn(INVOKESPECIAL, "java/lang/Object", "<init>", "()V", false)
-      mv.visitInsn(RETURN)
-      mv.visitMaxs(1, 1)
-      mv.visitEnd()
-    }
-
-    // Generate code for each of the Flix functions
-    functions.foreach(generateMethod)
-
-    // Finish the traversal and convert to a byte array
-    visitor.visitEnd()
-    classWriter.toByteArray
   }
 }
