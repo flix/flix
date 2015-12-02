e6198af1a49c88abe5b4735d557e4b6e5c2ed02c
Ming-Ho Yee <mh@mhyee.com>
2015-12-02 12:04:53 -0500

    Some more sketching and TODOs

--- main/src/ca/uwaterloo/flix/language/backend/phase/Codegen.scala
+++ main/src/ca/uwaterloo/flix/language/backend/phase/Codegen.scala
@@ -5,7 +5,6 @@ import org.objectweb.asm.Opcodes._
 import org.objectweb.asm.util.CheckClassAdapter
 
 object Codegen {
-
   def genTestAsm(): Array[Byte] = {
     val cw = new ClassWriter(0)
     val cv = new CheckClassAdapter(cw)
@@ -37,16 +36,32 @@ object Codegen {
 
     cw.toByteArray
   }
-
 }
 
+// TODO: What other pieces of the interface/pipeline do we need?
+// How is Codegen called? Where do we put the generated methods? How are the generated methods called?
+// TODO: Constructor should take a list of (Flix) functions? Does it need anything else? Flix file = class name?
 class Codegen {
   private val classWriter = new ClassWriter(0)
+  // Note: CheckClassAdapter requires us to manually calculate maxs and frames
   private val visitor = new CheckClassAdapter(classWriter)
+
+  // TODO: generateMethod needs to take a (Flix) function, so it can get the name, signature, and body expression
+  def generateMethod(): Unit = {
+    val mv = visitor.visitMethod(ACC_PUBLIC + ACC_STATIC, "f", "()V", null, null)
+    mv.visitCode()
+
+    // TODO: Method body
+
+    mv.visitInsn(RETURN)  // TODO: Proper return instruction
+    mv.visitMaxs(0, 0)    // TODO: Calculate maxs
+    mv.visitEnd()
+  }
 
   lazy val bytecode: Array[Byte] = {
     // Initialize the visitor to create "public class ca.uwaterloo.flix.TestAsm extends java.lang.Object"
     // with no generic types and no interfaces
+    // TODO: Proper name for the class
     visitor.visit(V1_7, ACC_PUBLIC + ACC_SUPER, "ca/uwaterloo/flix/TestAsm", null, "java/lang/Object", null)
 
     // Generate the constructor for the class
@@ -60,7 +75,7 @@ class Codegen {
       mv.visitEnd()
     }
 
-    // TODO: Generate code for each of the methods
+    // TODO: Generate code for each of the Flix functions
 
     // Finish the traversal and convert to a byte array
     visitor.visitEnd()
