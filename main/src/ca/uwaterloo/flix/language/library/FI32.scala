fbe62571a23c17fc6715393fb3ff838896952786
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2015-12-25 02:48:50 +0100

    WIP Library.

---
+++ main/src/ca/uwaterloo/flix/language/library/FI32.scala
@@ -0,0 +1,13 @@
+package ca.uwaterloo.flix.language.library
+
+object FI32 {
+  // TODO:
+  // minValue
+  // maxValue
+  // abs
+  // min/max
+  // isPositive/isNegative?
+  
+
+  // https://doc.rust-lang.org/std/primitive.i64.html
+}
