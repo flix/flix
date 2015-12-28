0f608ed5ae34f6a8ced1411812289537662a5a75
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2015-12-28 01:24:41 +0100

    WIP Library.

---
+++ main/src/ca/uwaterloo/flix/language/library/FInt32.scala
@@ -0,0 +1,36 @@
+package ca.uwaterloo.flix.language.library
+
+object FInt32 {
+
+  sealed trait Int32Operator {
+
+  }
+
+  // TODO:
+  // isPositive/isNegative?
+
+  object minValue extends Int32Operator {
+
+  }
+
+  object maxValue extends Int32Operator {
+
+  }
+
+  object abs extends Int32Operator {
+
+  }
+
+  object min extends Int32Operator {
+
+  }
+
+  object max extends Int32Operator {
+
+  }
+
+
+  // https://doc.rust-lang.org/std/primitive.i64.html
+  // https://docs.oracle.com/javase/7/docs/api/java/lang/Integer.html
+
+}
