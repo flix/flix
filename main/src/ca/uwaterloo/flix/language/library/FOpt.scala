e4e5f02e92bf12f61c68e228bde786b0d12a8977
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2015-12-29 01:21:14 +0100

    WIP Library.

--- main/src/ca/uwaterloo/flix/language/library/FOpt.scala
+++ main/src/ca/uwaterloo/flix/language/library/FOpt.scala
@@ -8,34 +8,30 @@ import scala.collection.immutable
 
 object FOpt {
 
-  // TODO: Implement using `null` and iterators.
-
-  // TODO: Create cheatsheet
-
   /**
-    * All option operations.
+    * All Opt operations.
     */
   val Ops: immutable.Map[Name.Resolved, OptOperator] = List(
-  // TODO: None/Some
-  // TODO: isNone
-  // TODO: isSome
+    // TODO: None/Some
+    // TODO: isNone
+    // TODO: isSome
 
-  // TODO: null
-  // TODO: get
-  // TODO: getOrElse
-  // TODO:  or
-  // TODO: orElse: (Opt[A], Opt[A] => Opt[A]
-  // TODO: filter
-  // TODO: exists
-  // TODO: forall
+    // TODO: null
+    // TODO: get
+    // TODO: getOrElse
+    // TODO:  or
+    // TODO: orElse: (Opt[A], Opt[A] => Opt[A]
+    // TODO: filter
+    // TODO: exists
+    // TODO: forall
 
-  // TODO: map2: ((Opt, Opt) => A, Opt, Opt) => Opt
-  // TODO: flatMap2: see above
-  // TODO: toSet
-  // TODO: toList
+    // TODO: map2: ((Opt, Opt) => A, Opt, Opt) => Opt
+    // TODO: flatMap2: see above
 
-    "Opt::map" -> map,
-    "Opt::flatMap" -> flatMap
+    "Opt/map" -> map,
+    "Opt/flatMap" -> flatMap,
+    "Opt/toList" -> toList,
+    "Opt/toSet" -> toSet
   ).map {
     case (name, op) => Name.Resolved.mk(name) -> op
   }.toMap
@@ -54,18 +50,20 @@ object FOpt {
   /////////////////////////////////////////////////////////////////////////////
   // Basic Operations                                                        //
   /////////////////////////////////////////////////////////////////////////////
-  /**
-    * The `map : (Opt[A], A => B) => Opt[B]` function.
-    */
   object map extends OptOperator {
-    val tpe = (Opt(A), A ~> B) ~> Opt(B)
+    val tpe = (A ~> B, Opt(A)) ~> Opt(B)
   }
 
-  /**
-    * The `flatMap : (Opt[A], A => Opt[B]) => Opt[B]` function.
-    */
   object flatMap extends OptOperator {
-    val tpe = (Opt(A), A ~> Opt(B)) ~> Opt(B)
+    val tpe = (A ~> Opt(B), Opt(A)) ~> Opt(B)
+  }
+
+  object toList extends OptOperator {
+    val tpe = Opt(A) ~> Lst(A)
+  }
+
+  object toSet extends OptOperator {
+    val tpe = Opt(A) ~> Set(A)
   }
 
 }
