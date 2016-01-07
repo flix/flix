c39c833e84452fa93273f1dcce2d3c066688efc7
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2016-01-07 18:13:11 -0500

    WIP simplifier.

--- main/src/ca/uwaterloo/flix/runtime/PartialEvaluator.scala
+++ main/src/ca/uwaterloo/flix/runtime/PartialEvaluator.scala
@@ -1,46 +1,46 @@
-package ca.uwaterloo.flix.language.backend.phase
+package ca.uwaterloo.flix.runtime
 
-import ca.uwaterloo.flix.language.ast.{UnaryOperator, BinaryOperator, TypedAst}
 import ca.uwaterloo.flix.language.ast.TypedAst.Expression._
 import ca.uwaterloo.flix.language.ast.TypedAst.Literal
-
-// TODO: Look into symbolic evaluation.
+import ca.uwaterloo.flix.language.ast.{BinaryOperator, TypedAst, UnaryOperator}
+import ca.uwaterloo.flix.language.backend.ir.ReducedIR
 
 object PartialEvaluator {
 
-  def eval(e: TypedAst.Expression): TypedAst.Expression =
-    eval(e, x => x)
+  type Cont = TypedAst.Expression => TypedAst.Expression
 
-  def eval(e: TypedAst.Expression, k: TypedAst.Expression => TypedAst.Expression): TypedAst.Expression = e match {
-    case Lit(literal, tpe, loc) => k(e)
+  // TODO: Introduce type for k.
+  // TODO: pass env
+  def eval(exp0: TypedAst.Expression, env0: Map[Int, ReducedIR.Expression], k: Cont): TypedAst.Expression = exp0 match {
+    case Lit(literal, tpe, loc) => k(exp0)
 
-    case Unary(UnaryOperator.Minus, exp, _, _) => eval(exp, e => e match {
+    case Unary(UnaryOperator.Minus, exp, _, _) => eval(exp, env0, {
       case Lit(Literal.Int(i, loc2), tpe, loc1) => k(Lit(Literal.Int(-i, loc2), tpe, loc1))
     })
 
     case exp@Binary(BinaryOperator.Or, exp1, exp2, _, _) =>
       // partially evaluate exp1
-      eval(exp1, e1 => e1 match {
+      eval(exp1, env0, e1 => e1 match {
         // Case 1: exp1 is true. The result is true.
         case Lit(Literal.Bool(true, loc), tpe, _) => k(Lit(Literal.Bool(lit = true, loc), tpe, loc))
         // Case 2: exp1 is false. The result is exp2.
-        case Lit(Literal.Bool(false, _), _, _) => eval(exp2, k)
+        case Lit(Literal.Bool(false, _), _, _) => eval(exp2, env0, k)
         // Case 3: exp1 is residual. Partially evaluate exp2.
-        case _ => eval(exp2, e2 => e2 match {
+        case _ => eval(exp2, env0, e2 => e2 match {
           // Case 3.1: exp2 is true. The result is true.
           case Lit(Literal.Bool(true, loc), tpe, _) => k(Lit(Literal.Bool(lit = true, loc), tpe, loc))
-          // Case 3.2: exp2 is false or residual. Reconstruct the term.
+          // Case 3.2: exp2 is false or residual. Reconstruct the term. // TODO:  This seems incorrect
           case _ => exp.copy(exp1 = e1, exp2 = e2)
         })
       })
 
     case exp@Binary(BinaryOperator.And, exp1, exp2, _, _) =>
       // partially evaluate exp1
-      eval(exp1, e1 => e1 match {
+      eval(exp1, env0, e1 => e1 match {
         // Case 1: exp1 is false. The result is false.
         case Lit(Literal.Bool(false, loc), tpe, _) => k(Lit(Literal.Bool(lit = false, loc), tpe, loc))
         // Case 2: exp1 is true. The result is exp2.
-        case Lit(Literal.Bool(true, loc), tpe, _) => eval(exp2, k)
+        case Lit(Literal.Bool(true, loc), tpe, _) => eval(exp2, env0, k)
         // Case 3: exp1 is residual. Partially evaluate exp2.
         case _ => ???
       })
