f0adbb40b2bb2ec9a514f5ff273cfb3ea75f740b
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2016-01-25 22:52:10 -0500

    WIP api

--- main/src/ca/uwaterloo/flix/api/IModel.scala
+++ main/src/ca/uwaterloo/flix/api/IModel.scala
@@ -5,22 +5,49 @@ package ca.uwaterloo.flix.api
   */
 trait IModel {
 
+  /**
+    * Returns an iterable over all constant names in the program.
+    */
+  def getConstants: java.lang.Iterable[String]
 
+  /**
+    * Returns an iterable over all relation names in the program.
+    */
+  def getRelations: java.lang.Iterable[String]
 
+  /**
+    * Returns an iterable over all lattice names in the program.
+    */
+  def getLattices: java.lang.Iterable[String]
+
+  /**
+    * Returns the constant with the given fully qualified `name`.
+    *
+    * @throws IllegalArgumentException if no such constant exists.
+    */
+  def getConstant(name: String): IValue
+
+  /**
+    * Returns an iterable over the relation with the given fully qualified `name`.
+    *
+    * @throws IllegalArgumentException if no such relation exists.
+    */
   def getRelation(name: String): java.lang.Iterable[Array[IValue]]
 
+  /**
+    * Returns an iterable over the lattice with the given fully qualified `name`.
+    *
+    * @throws IllegalArgumentException if no such lattice exists.
+    */
   def getLattice(name: String): java.lang.Iterable[Array[IValue]]
 
-
-  // def isFact()
-  // def isEDBFact()
-  // def isIDBFact()
-
-
-
-  //  root: TypedAst.Root,
-  //  constants: Map[Name.Resolved, Value],
-  //  relations: Map[Name.Resolved, Iterator[List[Value]]], // TODO: Replace with Iterable.
-  //  lattices: Map[Name.Resolved, Iterator[(List[Value], List[Value])]]
+  /**
+    * Returns `true` if the given `fact` exists in the relation or lattice with the given `name`.
+    *
+    * @param name the fully qualified name of the relation or lattice.
+    * @param fact the fact as an array of values.
+    * @throws IllegalArgumentException if no relation or lattice exists with the given `name`.
+    */
+  def isFact(name: String, fact: Array[IValue]): Boolean
 
 }
