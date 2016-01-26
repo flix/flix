a908b4f73d6f0e4febd816ac28b4539279ffa8e0
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2016-01-25 22:36:01 -0500

    WIP api

--- main/src/ca/uwaterloo/flix/api/IModel.scala
+++ main/src/ca/uwaterloo/flix/api/IModel.scala
@@ -5,4 +5,22 @@ package ca.uwaterloo.flix.api
   */
 trait IModel {
 
+
+
+  def getRelation(name: String): java.lang.Iterable[Array[IValue]]
+
+  def getLattice(name: String): java.lang.Iterable[Array[IValue]]
+
+
+  // def isFact()
+  // def isEDBFact()
+  // def isIDBFact()
+
+
+
+  //  root: TypedAst.Root,
+  //  constants: Map[Name.Resolved, Value],
+  //  relations: Map[Name.Resolved, Iterator[List[Value]]], // TODO: Replace with Iterable.
+  //  lattices: Map[Name.Resolved, Iterator[(List[Value], List[Value])]]
+
 }
---
+++ main/src/ca/uwaterloo/flix/api/WrappedModel.scala
@@ -0,0 +1,9 @@
+package ca.uwaterloo.flix.api
+
+import ca.uwaterloo.flix.runtime.Model
+
+import scala.collection.immutable
+
+protected class WrappedModel(private val model: Model) {
+
+}
