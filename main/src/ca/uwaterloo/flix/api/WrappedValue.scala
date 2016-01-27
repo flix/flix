21340f241630a8b8b7ffa445321ed5cf0bdf025b
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2016-01-26 20:15:01 -0500

    WIP api

--- main/src/ca/uwaterloo/flix/api/WrappedValue.scala
+++ main/src/ca/uwaterloo/flix/api/WrappedValue.scala
@@ -15,82 +15,91 @@ protected final class WrappedValue(val ref: AnyRef) extends IValue {
   }
 
   // TODO: Maybe this needs to be part of the value???
+  @deprecated
   def getType: IType = ref match {
     case Value.Unit => new WrappedType(Type.Unit)
     case Value.True => new WrappedType(Type.Bool)
     case Value.False => new WrappedType(Type.Bool)
     case v: Value.Int => new WrappedType(Type.Int32)
     case v: Value.Str => new WrappedType(Type.Str)
-    case _ => throw new UnsupportedOperationException(s"Unexpected value: $ref")
+    case _ => throw new UnsupportedOperationException(s"Unexpected value: '$ref'.")
   }
 
   def getBool: Boolean = ref match {
     case Value.True => true
     case Value.False => false
-    case _ => throw new UnsupportedOperationException(s"Unexpected value: $ref")
+    case o: java.lang.Boolean => o.booleanValue()
+    case _ => throw new UnsupportedOperationException(s"Unexpected value: '$ref'.")
   }
 
-  def isTrue: Boolean = ref match {
-    case Value.True => true
-    case _ => false
+  def isTrue: Boolean = getBool
+
+  def isFalse: Boolean = !getBool
+
+  def getChar: Char = ref match {
+    case o: java.lang.Character => o.charValue()
+    case _ => throw new UnsupportedOperationException(s"Unexpected value: '$ref'.")
   }
 
-  def isFalse: Boolean = ref match {
-    case Value.False => true
-    case _ => false
+  def getInt8: Byte = ref match {
+    case o: java.lang.Byte => o.byteValue()
+    case _ => throw new UnsupportedOperationException(s"Unexpected value: '$ref'.")
   }
 
-  def getChar: Char = throw new UnsupportedOperationException("Not Yet Implemented. Sorry.") // TODO
-
-  def getInt8: Byte = throw new UnsupportedOperationException("Not Yet Implemented. Sorry.")  // TODO
-
-  def getInt16: Short = throw new UnsupportedOperationException("Not Yet Implemented. Sorry.")  // TODO
+  def getInt16: Short = ref match {
+    case o: java.lang.Short => o.shortValue()
+    case _ => throw new UnsupportedOperationException(s"Unexpected value: '$ref'.")
+  }
 
   def getInt32: Int = ref match {
-    case v: Value.Int => v.i
-    case _ => throw new UnsupportedOperationException(s"Unexpected value: $ref")
+    case o: java.lang.Integer => o.intValue()
+    case _ => throw new UnsupportedOperationException(s"Unexpected value: '$ref'.")
   }
 
-  def getInt64: Long = throw new UnsupportedOperationException("Not Yet Implemented. Sorry.")
+  def getInt64: Long = ref match {
+    case o: java.lang.Long => o.longValue()
+    case _ => throw new UnsupportedOperationException(s"Unexpected value: '$ref'.")
+  }
 
   def getStr: String = ref match {
     case v: Value.Str => v.s
-    case _ => throw new UnsupportedOperationException(s"Unexpected value: $ref")
+    case o: java.lang.String => o
+    case _ => throw new UnsupportedOperationException(s"Unexpected value: '$ref'.")
   }
 
   def getTuple: Array[IValue] = ref match {
     case v: Value.Tuple => v.elms.map(e => new WrappedValue(e))
-    case _ => throw new UnsupportedOperationException(s"Unexpected value: $ref")
+    case o: Array[AnyRef] => o.map(e => new WrappedValue(e))
+    case _ => throw new UnsupportedOperationException(s"Unexpected value: '$ref'.")
   }
-
 
   def getEnumName: String = ref match {
     case v: Value.Tag => v.enum.parts.mkString("::")
-    case _ => throw new UnsupportedOperationException(s"Unexpected value: $ref")
+    case _ => throw new UnsupportedOperationException(s"Unexpected value: '$ref'.")
   }
 
   def getTagName: String = ref match {
     case v: Value.Tag => v.tag
-    case _ => throw new UnsupportedOperationException(s"Unexpected value: $ref")
+    case _ => throw new UnsupportedOperationException(s"Unexpected value: '$ref'.")
   }
 
   def getTagValue: IValue = ref match {
     case v: Value.Tag => new WrappedValue(v.value)
-    case _ => throw new UnsupportedOperationException(s"Unexpected value: $ref")
+    case _ => throw new UnsupportedOperationException(s"Unexpected value: '$ref'.")
   }
 
   def getNativeObj: AnyRef = ref match {
     case v: Value.Native => v.value
-    case _ => throw new UnsupportedOperationException(s"Unexpected value: $ref")
+    case o => o
   }
 
-  def getJavaOpt: java.util.Optional[IValue] = throw new UnsupportedOperationException("Not Yet Implemented. Sorry.")  // TODO
+  def getJavaOpt: java.util.Optional[IValue] = throw new UnsupportedOperationException("Not Yet Implemented. Sorry.") // TODO
 
   def getScalaOpt: scala.Option[IValue] = throw new UnsupportedOperationException("Not Yet Implemented. Sorry.") // TODO
 
-  def getJavaList: java.util.Set[IValue] = throw new UnsupportedOperationException("Not Yet Implemented. Sorry.")  // TODO
+  def getJavaList: java.util.Set[IValue] = throw new UnsupportedOperationException("Not Yet Implemented. Sorry.") // TODO
 
-  def getScalaList: immutable.List[IValue] = throw new UnsupportedOperationException("Not Yet Implemented. Sorry.")  // TODO
+  def getScalaList: immutable.List[IValue] = throw new UnsupportedOperationException("Not Yet Implemented. Sorry.") // TODO
 
   def getJavaSet: java.util.Set[IValue] = ref match {
     case v: Value.Set => {
@@ -100,17 +109,17 @@ protected final class WrappedValue(val ref: AnyRef) extends IValue {
       }
       r
     }
-    case _ => throw new UnsupportedOperationException(s"Unexpected value: $ref")
+    case _ => throw new UnsupportedOperationException(s"Unexpected value: '$ref'.")
   }
 
   def getScalaSet: immutable.Set[IValue] = ref match {
     case v: Value.Set => v.elms.map(e => new WrappedValue(e))
-    case _ => throw new UnsupportedOperationException(s"Unexpected value: $ref")
+    case _ => throw new UnsupportedOperationException(s"Unexpected value: '$ref'.")
   }
 
   def getJavaMap: java.util.Map[IValue, IValue] = throw new UnsupportedOperationException("Not Yet Implemented. Sorry.") // TODO
 
-  def getScalaMap: immutable.Map[IValue, IValue] = throw new UnsupportedOperationException("Not Yet Implemented. Sorry.")  // TODO
+  def getScalaMap: immutable.Map[IValue, IValue] = throw new UnsupportedOperationException("Not Yet Implemented. Sorry.") // TODO
 
   override def equals(other: Any): Boolean = other match {
     case that: WrappedValue => ref == that.ref
