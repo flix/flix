001401be428f6677c9a305ca0680136916a255ab
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2016-02-02 20:45:14 -0500

    WIP verifier.

--- main/src/ca/uwaterloo/flix/runtime/PartialEvaluator.scala
+++ main/src/ca/uwaterloo/flix/runtime/PartialEvaluator.scala
@@ -9,9 +9,14 @@ import ca.uwaterloo.flix.runtime.Interpreter.InternalRuntimeError
 object PartialEvaluator {
 
   /**
-    * The type of the continuation used by the partial evaluator.
+    * The type of the continuation used by eval.
     */
   type Cont = Expression => Expression
+
+  /**
+    * The type of the continuation used by eval2.
+    */
+  type Cont2 = (Expression, Expression) => Expression
 
   /**
     * Partially evaluates the given expression `exp0` under the given environment `env0`
@@ -61,7 +66,7 @@ object PartialEvaluator {
         * Var Expressions/
         */
       case Var(name, offset, tpe, loc) => env0.get(name.name) match {
-        case None => throw new InternalRuntimeError(s"Unknown name: '$name'.")
+        case None => throw new InternalRuntimeError(s"Unresolved variable: '$name'.")
         case Some(e) => eval(e, env0, k)
       }
 
@@ -69,7 +74,7 @@ object PartialEvaluator {
         * Ref Expressions.
         */
       case Ref(name, tpe, loc) => root.constants.get(name) match {
-        case None => throw new InternalRuntimeError(s"Unknown name: '$name'.")
+        case None => throw new InternalRuntimeError(s"Unresolved reference: '$name'.")
         case Some(defn) => k(defn.exp)
       }
 
@@ -95,7 +100,10 @@ object PartialEvaluator {
           * Unary Minus.
           */
         case UnaryOperator.Minus => eval(exp, env0, {
+          case Int8(i) => k(Int8(byte(-i)))
+          case Int16(i) => k(Int16(short(-i)))
           case Int32(i) => k(Int32(-i))
+          case Int64(i) => k(Int64(-i))
           case residual => k(residual)
         })
 
@@ -103,7 +111,10 @@ object PartialEvaluator {
           * Unary Bitwise Negation.
           */
         case UnaryOperator.BitwiseNegate => eval(exp, env0, {
-          case Int32(i) => Int32(~i)
+          case Int8(i) => k(Int8(byte(~i)))
+          case Int16(i) => k(Int16(short(~i)))
+          case Int32(i) => k(Int32(~i))
+          case Int64(i) => k(Int64(~i))
           case residual => k(residual)
         })
       }
@@ -114,21 +125,30 @@ object PartialEvaluator {
       case Binary(op, exp1, exp2, tpe, loc) => op match {
 
         /**
-          * Arithmetic Plus.
+          * Arithmetic Addition.
           */
         case BinaryOperator.Plus =>
-          // Partially evaluate exp1.
-          eval(exp1, env0, {
-            case e1 =>
-              // Partially evaluate exp2.
-              eval(exp2, env0, {
-                case e2 => (e1, e2) match {
-                  case (Int32(x), Int32(y)) => k(Int32(x + y))
-                  case (Int32(0), _) => k(e2)
-                  case (_, Int32(0)) => k(e1)
-                  case (r1, r2) => k(Binary(op, r1, r2, tpe, loc))
-                }
-              })
+          // Partially evaluate exp1 and exp2.
+          eval2(exp1, exp2, env0, {
+            // Concrete execution.
+            case (Int8(x), Int8(y)) => k(Int8(byte(x + y)))
+            case (Int16(x), Int16(y)) => k(Int16(short(x + y)))
+            case (Int32(x), Int32(y)) => k(Int32(x + y))
+            case (Int64(x), Int64(y)) => k(Int64(x + y))
+
+            // Identity laws.
+            case (Int8(0), y) => k(y)
+            case (Int16(0), y) => k(y)
+            case (Int32(0), y) => k(y)
+            case (Int64(0), y) => k(y)
+
+            case (x, Int8(0)) => k(x)
+            case (x, Int16(0)) => k(x)
+            case (x, Int32(0)) => k(x)
+            case (x, Int64(0)) => k(x)
+
+            // Reconstruction
+            case (r1, r2) => k(Binary(op, r1, r2, tpe, loc))
           })
 
         case BinaryOperator.Minus => ??? // TODO
@@ -389,6 +409,16 @@ object PartialEvaluator {
       case o: StoreInt32 => ??? // TODO: To be eliminated from this phase.
     }
 
+    /**
+      * Overloaded eval that partially evaluates the two arguments `exp1` and `exp2` under the environment `env0`.
+      */
+    def eval2(exp1: Expression, exp2: Expression, env0: Map[String, Expression], k: Cont2): Expression =
+      eval(exp1, env0, {
+        case e1 => eval(exp2, env0, {
+          case e2 => k(e1, e2)
+        })
+      })
+
     eval(exp0, env0, x => x)
   }
 
@@ -493,6 +523,15 @@ object PartialEvaluator {
     case Unit => Unit
   }
 
+  /**
+    * Short-hand for casting an Int to a Byte.
+    */
+  private def byte(i: Int): Byte = i.asInstanceOf[Byte]
+
+  /**
+    * Short-hand for casting an Int to a Short.
+    */
+  private def short(i: Int): Short = i.asInstanceOf[Short]
 
   // http://www.lshift.net/blog/2007/06/11/folds-and-continuation-passing-style/
 
