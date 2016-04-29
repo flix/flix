6b51f8574d38e8ba6b8650418a6650c26da0930c
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2016-04-29 14:02:27 -0400

    WIP typer2.

--- main/src/ca/uwaterloo/flix/language/phase/Typer2.scala
+++ main/src/ca/uwaterloo/flix/language/phase/Typer2.scala
@@ -2,72 +2,126 @@ package ca.uwaterloo.flix.language.phase
 
 import ca.uwaterloo.flix.language.ast._
 
+import scala.collection.mutable
+
 object Typer2 {
 
-  case class EqType(t1: Type, t2: Type)
+  sealed trait TypeConstraint
 
-  case class EqTypeOneOf(t1: Type, t2: Set[Type])
+  object TypeConstraint {
 
+    case class Eq(t1: Type, t2: Type) extends TypeConstraint
+
+    case class OneOf(t1: Type, t2: Set[Type]) extends TypeConstraint
+
+  }
+
+  /**
+    * Phase 1: Constraint Generation.
+    */
   object ConstraintGeneration {
 
-
     object Expressions {
-
-      // TODO: Use mutable list of type constraints.
 
       /**
         * Generates type constraints for the given expression `exp0`.
         */
-      def gen(exp0: TypedAst.Expression, tenv: Map[Name.Ident, Type])(implicit genSym: GenSym): (List[EqType], Type) = exp0 match {
+      def gen(exp0: TypedAst.Expression)(implicit genSym: GenSym): Unit = {
 
-        case TypedAst.Expression.Lit(TypedAst.Literal.Int32(_, _), _, _) => (Nil, Type.Int32)
+        /*
+         * A mutable set to hold the collected constraints.
+         */
+        val constraints = mutable.Set.empty[TypeConstraint]
 
-        case TypedAst.Expression.Var(ident, _, _) =>
-          ??? // TODO Look up in type environment.
+        /**
+          * Generates type constraints for the given expression `e0` under the given type environment `tenv`.
+          */
+        def visit(e0: TypedAst.Expression, tenv: Map[Name.Ident, Type]): Type = e0 match {
+          /*
+           * Literal Expressions.
+           */
+          case TypedAst.Expression.Lit(lit, _, _) => lit match {
+            case TypedAst.Literal.Unit(loc) => Type.Unit
+            case TypedAst.Literal.Bool(_, _) => Type.Bool
+            case TypedAst.Literal.Char(_, _) => Type.Char
+            case TypedAst.Literal.Float32(_, _) => Type.Float32
+            case TypedAst.Literal.Float64(_, _) => Type.Float64
+          }
 
-        case TypedAst.Expression.Let(ident, exp1, exp2, tpe, _) =>
-          // update type environment and recurse
-          ???
+          case TypedAst.Expression.Var(ident, _, _) =>
+            ??? // TODO Look up in type environment.
 
-        case TypedAst.Expression.Binary(op, e1, e2, _, _) => op match {
-          case BinaryOperator.Plus =>
-            // TODO: Use EqTypeOneOf.
+          case TypedAst.Expression.Let(ident, exp1, exp2, tpe, _) =>
+            // update type environment and recurse
             ???
 
-          case _: ComparisonOperator =>
-            val (c1, tpe1) = gen(e1, tenv)
-            val (c2, tpe2) = gen(e2, tenv)
 
-            (EqType(tpe1, tpe2) :: c1 ++ c2, Type.Bool)
+          /*
+           * Unary Expressions.
+           */
+          case TypedAst.Expression.Unary(op, e1, _, _) => op match {
+            case UnaryOperator.LogicalNot =>
+              val t1 = visit(e1, tenv)
+              constraints += TypeConstraint.Eq(t1, Type.Bool)
+              Type.Bool
+
+            case UnaryOperator.Plus =>
+
+              ???
+
+            case UnaryOperator.Minus => ???
+            case UnaryOperator.BitwiseNegate => ???
+          }
+
+
+          /*
+           * Binary Expressions.
+           */
+          case TypedAst.Expression.Binary(op, e1, e2, _, _) => op match {
+            case BinaryOperator.Plus =>
+              // TODO: Use EqTypeOneOf.
+              ???
+
+            case _: ComparisonOperator =>
+              val tpe1 = visit(e1, tenv)
+              val tpe2 = visit(e2, tenv)
+
+              constraints += TypeConstraint.Eq(tpe1, tpe2)
+
+              Type.Bool
+          }
+
+          case TypedAst.Expression.Apply(exp1, args, _, _) =>
+            val tpe1 = visit(exp1, tenv)
+
+            val r = Type.Var(genSym.fresh2().name) // TODO
+            //(TypeConstraint.EqType(tpe1, Type.Lambda(tpes, r)) :: c1 ::: cs.flatten, r)
+            r
+
+          case TypedAst.Expression.IfThenElse(e1, e2, e3, _, loc) =>
+            val tpe1 = visit(e1, tenv)
+            val tpe2 = visit(e2, tenv)
+            val tpe3 = visit(e3, tenv)
+            //(TypeConstraint.EqType(tpe1, Type.Bool) :: TypeConstraint.EqType(tpe2, tpe3) :: c1 ++ c2 ++ c3, tpe2)
+            tpe2
         }
 
-        case TypedAst.Expression.Apply(exp1, args, _, _) =>
-          val (c1, tpe1) = gen(exp1, tenv)
-          val (cs, tpes) = args.map {
-            case e => gen(e, tenv)
-          }.unzip
 
-          val r = Type.Var(genSym.fresh2().name) // TODO
-          (EqType(tpe1, Type.Lambda(tpes, r)) :: c1 ::: cs.flatten, r)
+        visit(exp0, Map.empty)
 
-        case TypedAst.Expression.IfThenElse(e1, e2, e3, _, loc) =>
-          val (c1, tpe1) = gen(e1, tenv)
-          val (c2, tpe2) = gen(e2, tenv)
-          val (c3, tpe3) = gen(e3, tenv)
-          (EqType(tpe1, Type.Bool) :: EqType(tpe2, tpe3) :: c1 ++ c2 ++ c3, tpe2)
       }
-
     }
 
   }
 
-
+  /**
+    * Phase 2: Unification.
+    */
   object Unification {
 
     // TODO: Possibly return map from names to types.
-    def unify(cs: List[EqType]): List[EqType] = ???
+    def unify(cs: List[TypeConstraint.Eq]): List[TypeConstraint.Eq] = ???
 
   }
-
 
 }
