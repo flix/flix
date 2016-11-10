414268ff688e9fbbe775f2069e5eba5db1a9bc5e
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2016-11-10 00:13:28 -0500

    Fixed sorting order.

--- main/src/resources/documentation/app.jsx
+++ main/src/resources/documentation/app.jsx
@@ -294,11 +294,56 @@ function intersperse(arr, sep) {
 function lexicographic(a, b) {
     var x = a.name;
     var y = b.name;
-    var cmp = x.localeCompare(y);
-    if (cmp < 0) {
-        return x.length - y.length;
+    return naturalSort(x, y);
+}
+
+/*
+ * Natural Sort algorithm for Javascript - Version 0.7 - Released under MIT license
+ * Author: Jim Palmer (based on chunking idea from Dave Koelle)
+ */
+function naturalSort(a, b, options) {
+    var re = /(^-?[0-9]+(\.?[0-9]*)[df]?e?[0-9]?$|^0x[0-9a-f]+$|[0-9]+)/gi,
+        sre = /(^[ ]*|[ ]*$)/g,
+        dre = /(^([\w ]+,?[\w ]+)?[\w ]+,?[\w ]+\d+:\d+(:\d+)?[\w ]?|^\d{1,4}[\/\-]\d{1,4}[\/\-]\d{1,4}|^\w+, \w+ \d+, \d{4})/,
+        hre = /^0x[0-9a-f]+$/i,
+        ore = /^0/,
+        options = options || {},
+        i = function (s) {
+            return options.insensitive && ('' + s).toLowerCase() || '' + s
+        },
+        // convert all to strings strip whitespace
+        x = i(a).replace(sre, '') || '',
+        y = i(b).replace(sre, '') || '',
+        // chunk/tokenize
+        xN = x.replace(re, '\0$1\0').replace(/\0$/, '').replace(/^\0/, '').split('\0'),
+        yN = y.replace(re, '\0$1\0').replace(/\0$/, '').replace(/^\0/, '').split('\0'),
+        // numeric, hex or date detection
+        xD = parseInt(x.match(hre)) || (xN.length !== 1 && x.match(dre) && Date.parse(x)),
+        yD = parseInt(y.match(hre)) || xD && y.match(dre) && Date.parse(y) || null,
+        oFxNcL, oFyNcL,
+        mult = options.desc ? -1 : 1;
+    // first try and sort Hex codes or Dates
+    if (yD)
+        if (xD < yD) return -1 * mult;
+        else if (xD > yD) return 1 * mult;
+    // natural sorting through split numeric strings and default strings
+    for (var cLoc = 0, numS = Math.max(xN.length, yN.length); cLoc < numS; cLoc++) {
+        // find floats not starting with '0', string or 0 if not defined (Clint Priest)
+        oFxNcL = !(xN[cLoc] || '').match(ore) && parseFloat(xN[cLoc]) || xN[cLoc] || 0;
+        oFyNcL = !(yN[cLoc] || '').match(ore) && parseFloat(yN[cLoc]) || yN[cLoc] || 0;
+        // handle numeric vs string comparison - number < string - (Kyle Adams)
+        if (isNaN(oFxNcL) !== isNaN(oFyNcL)) {
+            return (isNaN(oFxNcL) ? 1 : -1) * mult;
+        }
+        // rely on string comparison if different types - i.e. '02' < 2 != '02' < '2'
+        else if (typeof oFxNcL !== typeof oFyNcL) {
+            oFxNcL += '';
+            oFyNcL += '';
+        }
+        if (oFxNcL < oFyNcL) return -1 * mult;
+        if (oFxNcL > oFyNcL) return 1 * mult;
     }
-    return cmp;
+    return 0;
 }
 
 /**
