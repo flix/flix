5feaed4c8c0afb593538abc4925de4626a4ff686
liampalmer43 <liampalmer43@gmail.com>
2017-02-17 00:25:40 -0500

    Unit Tests Int16

--- main/test/ca/uwaterloo/flix/library/LibrarySuite.scala
+++ main/test/ca/uwaterloo/flix/library/LibrarySuite.scala
@@ -22,6 +22,7 @@ import org.scalatest.{ParallelTestExecution, Suites}
 class LibrarySuite extends Suites(
   new FlixTest("TestPrelude", "main/test/ca/uwaterloo/flix/library/TestPrelude.flix"),
   new FlixTest("TestInt8", "main/test/ca/uwaterloo/flix/library/TestInt8.flix"),
+  new FlixTest("TestInt16", "main/test/ca/uwaterloo/flix/library/TestInt16.flix"),
   new FlixTest("TestBigInt", "main/test/ca/uwaterloo/flix/library/TestBigInt.flix"),
   new FlixTest("TestFloat32", "main/test/ca/uwaterloo/flix/library/TestFloat32.flix"),
   new FlixTest("TestFloat64", "main/test/ca/uwaterloo/flix/library/TestFloat64.flix"),
