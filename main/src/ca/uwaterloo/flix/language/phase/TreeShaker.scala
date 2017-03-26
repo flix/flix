3355fc4f895c7e538c69e7d40240517cb8723a5f
liampalmer43 <liampalmer43@gmail.com>
2017-03-25 22:44:22 -0400

    Initial Progress on TreeShaker

--- main/src/ca/uwaterloo/flix/language/phase/TreeShaker.scala
+++ main/src/ca/uwaterloo/flix/language/phase/TreeShaker.scala
@@ -18,9 +18,12 @@ package ca.uwaterloo.flix.language.phase
 
 import ca.uwaterloo.flix.api.Flix
 import ca.uwaterloo.flix.language.CompilationError
-import ca.uwaterloo.flix.language.ast.SimplifiedAst
+import ca.uwaterloo.flix.language.ast.{SimplifiedAst, Symbol}
+import ca.uwaterloo.flix.language.ast.SimplifiedAst.{Expression, Definition}
 import ca.uwaterloo.flix.util.Validation
 import ca.uwaterloo.flix.util.Validation._
+
+import scala.collection.mutable
 
 /**
   * The Tree Shaking phase removes all unused function definitions.
@@ -31,6 +34,7 @@ import ca.uwaterloo.flix.util.Validation._
   * (b) Appears in a fact or a rule as a filter/transfer function.
   * (c) Appears in a function which itself is reachable.
   */
+
 object TreeShaker extends Phase[SimplifiedAst.Root, SimplifiedAst.Root] {
 
   /**
@@ -38,9 +42,129 @@ object TreeShaker extends Phase[SimplifiedAst.Root, SimplifiedAst.Root] {
     */
   def run(root: SimplifiedAst.Root)(implicit flix: Flix): Validation[SimplifiedAst.Root, CompilationError] = {
 
-    // TODO: Implement.
+    /**
+      * A queue of function definitions to be processed recursively.
+      *
+      * For example, if the queue contains the entry:
+      *
+      * -   f
+      *
+      * it means that the function definition f should be considered to determine new reachable functions.
+      */
+    val queue: mutable.Queue[Definition.Constant] = mutable.Queue.empty
 
-    root.toSuccess
+    /**
+      * A map used to collect reachable functions (symbols and the corresponding definitions).
+      */
+    val reachableFunctions: mutable.Map[Symbol.DefnSym, Definition.Constant] = mutable.Map.empty
+
+    /**
+      * Returns true iff the function definition `defn` is initially reachable.
+      *
+      * That is, returns true iff `defn` satisfies one of the following:
+      *
+      *   (a) Appears in the global namespaces, takes zero arguments, and is not marked as synthetic.
+      *
+      *   (b) Appears in a fact or a rule as a filter/transfer function.
+      */
+    def firstReachable(defn: Definition.Constant): Boolean = {
+      val global = defn.sym.namespace.isEmpty
+      val noArguments = defn.formals.isEmpty
+      val notSynthetic = !defn.isSynthetic
+
+      global && noArguments && notSynthetic
+    }
+
+    /**
+      * Searches the given expression `expression` for reachable functions.
+      */
+    def visitExp(expression: Expression): Unit =  expression match {
+      case Expression.Ref(sym, tpe, loc) => newRef(sym)
+      case Expression.Lambda(args, body, tpe, loc) => visitExp(body)
+      case Expression.MkClosure(lambda, freeVars, tpe, loc) => visitExp(lambda)
+      case Expression.MkClosureRef(ref, freeVars, tpe, loc) => visitExp(ref)
+      case Expression.ApplyRef(sym, args, tpe, loc) =>
+        newRef(sym)
+        args.foreach(e => visitExp(e))
+      case Expression.ApplyTail(sym, formals, actuals, tpe, loc) =>
+        newRef(sym)
+        actuals.foreach(e => visitExp(e))
+      case Expression.Apply(exp, args, tpe, loc) =>
+        visitExp(exp)
+        args.foreach(e => visitExp(e))
+      case Expression.Unary(op, exp, tpe, loc) => visitExp(exp)
+      case Expression.Binary(op, exp1, exp2, tpe, loc) =>
+        visitExp(exp1)
+        visitExp(exp2)
+      case Expression.IfThenElse(exp1, exp2, exp3, tpe, loc) =>
+        visitExp(exp1)
+        visitExp(exp2)
+        visitExp(exp3)
+      case Expression.Let(sym, exp1, exp2, tpe, loc) =>
+        visitExp(exp1)
+        visitExp(exp2)
+      case Expression.Is(exp, tag, loc) => visitExp(exp)
+      case Expression.Tag(sym, tag, exp, tpe, loc) => visitExp(exp)
+      case Expression.Untag(tag, exp, tpe, loc) => visitExp(exp)
+      case Expression.Index(base, offset, tpe, loc) => visitExp(base)
+      case Expression.Tuple(elms, tpe, loc) => elms.foreach(e => visitExp(e))
+      case Expression.Existential(fparam, exp, loc) => visitExp(exp)
+      case Expression.Universal(fparam, exp, loc) => visitExp(exp)
+      case _ =>
+    }
+
+    /**
+      * Adds the function `sym` to the map of reachable functions.
+      */
+    def newRef(sym: Symbol.DefnSym): Unit = {
+      reachableFunctions.get(sym) match {
+        // If `sym` has not already been determined reachable, look up its definition in `root`.
+        case None =>
+          root.definitions.get(sym) match {
+            case Some(defn) =>
+              reachableFunctions.put(sym, defn)
+              queue.enqueue(defn)
+            // If `sym` is not defined in `root`, leave this for error checking later.
+            case _ =>
+          }
+        // If `sym` has already been determined reachable, do nothing.
+        case _ =>
+      }
+    }
+
+    /*
+     * We can now use these helper functions to perform tree shaking.
+     */
+
+    // Start the timer.
+    val t = System.nanoTime()
+
+    /*
+     * Find all initially reachable function definitions.
+     */
+    for ((sym, defn) <- root.definitions) {
+      if (firstReachable(defn)) {
+        queue.enqueue(defn)
+        reachableFunctions.put(sym, defn)
+      }
+    }
+
+    /*
+     * Search for nested reachable functions.
+     */
+    while (queue.nonEmpty) {
+      // Extract a function from the queue and search for other functions.
+      visitExp(queue.dequeue().exp)
+    }
+
+    // Calculate the elapsed time.
+    val e = System.nanoTime() - t
+
+    // Reassemble the AST.
+    root.copy(
+      definitions = reachableFunctions.toMap,
+      time = root.time.copy(treeshaker = e)
+    ).toSuccess
   }
 
 }
--- main/src/ca/uwaterloo/flix/runtime/debugger/RestServer.scala
+++ main/src/ca/uwaterloo/flix/runtime/debugger/RestServer.scala
@@ -340,6 +340,7 @@ class RestServer(solver: Solver) {
       JObject(List(JField("name", JString("Verifier")), JField("time", JInt(solver.root.time.verifier / 1000000)))),
       JObject(List(JField("name", JString("LambdaLift")), JField("time", JInt(solver.root.time.lambdaLift / 1000000)))),
       JObject(List(JField("name", JString("Simplifier")), JField("time", JInt(solver.root.time.simplifier / 1000000)))),
+      JObject(List(JField("name", JString("TreeShaker")), JField("time", JInt(solver.root.time.treeshaker / 1000000)))),
       JObject(List(JField("name", JString("VarNumbering")), JField("time", JInt(solver.root.time.varNumbering / 1000000)))),
       JObject(List(JField("name", JString("CodeGen")), JField("time", JInt(solver.root.time.codeGen / 1000000))))
     ))
