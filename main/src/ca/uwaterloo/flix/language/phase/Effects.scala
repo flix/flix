b7368d5a98a70cdcdd2dae57ff82a6115cb73b7c
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2017-05-19 09:56:59 -0400

    Added Effect to TypedAst.

---
+++ main/src/ca/uwaterloo/flix/language/phase/Effects.scala
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2017 Magnus Madsen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ca.uwaterloo.flix.language.phase
+
+import ca.uwaterloo.flix.api.Flix
+import ca.uwaterloo.flix.language.CompilationError
+import ca.uwaterloo.flix.language.ast.TypedAst
+import ca.uwaterloo.flix.util.Validation
+import ca.uwaterloo.flix.util.Validation._
+
+object Effects extends Phase[TypedAst.Root, TypedAst.Root] {
+
+  /**
+    * Performs effect inference on the given AST `root`.
+    */
+  def run(root: TypedAst.Root)(implicit flix: Flix): Validation[TypedAst.Root, CompilationError] = {
+
+    // TODO
+
+    root.toSuccess
+  }
+
+}
