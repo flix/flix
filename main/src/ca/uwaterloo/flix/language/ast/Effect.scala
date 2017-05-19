5e04a2f464b5b31a6681a249a586c87c75fcf8c3
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2017-05-19 10:37:37 -0400

    Added some more infrastructure for effects.

--- main/src/ca/uwaterloo/flix/language/ast/Effect.scala
+++ main/src/ca/uwaterloo/flix/language/ast/Effect.scala
@@ -19,7 +19,11 @@ package ca.uwaterloo.flix.language.ast
 /**
   * Representation of effects.
   */
-trait Effect
+trait Effect {
+
+  def latent: Effect = ???
+
+}
 
 object Effect {
 
@@ -27,5 +31,40 @@ object Effect {
     * A pure (empty) effect.
     */
   case object Pure extends Effect
+
+  /**
+    * An effect that represents that the expression may throw an exception.
+    */
+  case object Exception extends Effect
+
+  /**
+    * A set of effects.
+    */
+  case class EffectSet(effs: Set[Effect]) extends Effect
+
+  /**
+    * Combines the two given effects `eff1` and `eff2` into a single effect.
+    */
+  def combine(eff1: Effect, eff2: Effect): Effect = (eff1, eff2) match {
+    case (Pure, Pure) => Pure
+    case (Exception, Exception) => Exception
+
+    case (EffectSet(es), Pure) => EffectSet(es)
+    case (EffectSet(es), Exception) => EffectSet(es + Exception)
+
+    case (Pure, EffectSet(es)) => EffectSet(es)
+    case (Exception, EffectSet(es)) => EffectSet(es)
+
+    case (EffectSet(es1), EffectSet(es2)) => EffectSet(es1 ++ es2)
+  }
+
+  // TODO: DOC
+  def combine(eff1: Effect, eff2: Effect, es: Effect*): Effect =
+    es.foldLeft(combine(eff1, eff2))(combine)
+
+  // TODO: DOC
+  def combine(eff: Effect, effs: List[Effect]): Effect =
+    effs.foldLeft(eff)(combine)
+
 
 }
--- main/src/ca/uwaterloo/flix/language/phase/Effects.scala
+++ main/src/ca/uwaterloo/flix/language/phase/Effects.scala
@@ -18,7 +18,8 @@ package ca.uwaterloo.flix.language.phase
 
 import ca.uwaterloo.flix.api.Flix
 import ca.uwaterloo.flix.language.CompilationError
-import ca.uwaterloo.flix.language.ast.TypedAst
+import ca.uwaterloo.flix.language.ast._
+import ca.uwaterloo.flix.language.ast.TypedAst.Expression
 import ca.uwaterloo.flix.util.Validation
 import ca.uwaterloo.flix.util.Validation._
 
@@ -33,5 +34,43 @@ object Effects extends Phase[TypedAst.Root, TypedAst.Root] {
 
     root.toSuccess
   }
+
+  object Expressions {
+
+    /**
+      * Infers the effects of the given expression `exp0`.
+      */
+    def infer(exp0: Expression, root: TypedAst.Root): Expression = {
+      /**
+        * Local visitor.
+        */
+      def visit(e0: Expression): Expression = exp0 match {
+
+        // TODO: Add all other cases.
+
+        case Expression.Apply(lambda, args, tpe, _, loc) =>
+          val e = visit(lambda)
+          val es = args.map(visit)
+
+          // The effect is the effect of the individual arguments and the latent effect of the lambda.
+          val eff = Effect.combine(e.eff.latent, es.map(_.eff))
+          Expression.Apply(e, es, tpe, eff, loc)
+
+        case Expression.IfThenElse(exp1, exp2, exp3, tpe, _, loc) =>
+          val e1 = visit(exp1)
+          val e2 = visit(exp2)
+          val e3 = visit(exp3)
+          val eff = Effect.combine(e1.eff, e2.eff, e3.eff)
+          Expression.IfThenElse(e1, e2, e3, tpe, eff, loc)
+
+        case _ => ??? // TODO: Remove once all cases have been added.
+
+      }
+
+      visit(exp0)
+    }
+
+  }
+
 
 }
