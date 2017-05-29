18acebbb5c5b00aa3c5775c5a47002cf307aefe9
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2017-05-29 10:34:08 -0400

    Fixes #445

--- main/src/ca/uwaterloo/flix/runtime/Solver.scala
+++ main/src/ca/uwaterloo/flix/runtime/Solver.scala
@@ -387,9 +387,9 @@ class Solver(val root: ExecutableAst.Root, options: Options) {
           case ExecutableAst.Term.Body.Var(sym, _, _) =>
             // A variable is replaced by its value from the environment (or null if unbound).
             env(sym.getStackOffset)
-          case ExecutableAst.Term.Body.Lit(v, _, _) =>
-            // A literal has already been evaluated to a value.
-            v
+          case ExecutableAst.Term.Body.Lit(litSym, _, _) =>
+            // Every literal is lifted to a function definition and its value is obtained by invoking it.
+            Linker.link(litSym, root).invoke(Array.emptyObjectArray)
           case ExecutableAst.Term.Body.Wild(_, _) =>
             // A wildcard places no restrictions on the value.
             null
@@ -499,9 +499,9 @@ class Solver(val root: ExecutableAst.Root, options: Options) {
           case Term.Body.Var(x, _, _) =>
             // A variable is replaced by its value from the environment.
             env(x.getStackOffset)
-          case Term.Body.Lit(v, _, _) =>
-            // A literal has already been evaluated to a value.
-            v
+          case Term.Body.Lit(litSym, _, _) =>
+            // Every literal is lifted to a function definition and its value is obtained by invoking it.
+            Linker.link(litSym, root).invoke(Array.emptyObjectArray)
           case Term.Body.Wild(_, _) =>
             // A wildcard should not appear as an argument to a filter function.
             throw InternalRuntimeException("Wildcard not allowed here!")
@@ -558,7 +558,9 @@ class Solver(val root: ExecutableAst.Root, options: Options) {
     */
   def evalHeadTerm(t: Term.Head, root: Root, env: Env): AnyRef = t match {
     case Term.Head.Var(sym, _, _) => env(sym.getStackOffset)
-    case Term.Head.Lit(v, _, _) => v
+    case Term.Head.Lit(litSym, _, _) =>
+      // Every literal is lifted to a function definition and its value is obtained by invoking it.
+      Linker.link(litSym, root).invoke(Array.emptyObjectArray)
     case Term.Head.App(sym, syms, _, _) =>
       val args = new Array[AnyRef](syms.length)
       var i = 0
