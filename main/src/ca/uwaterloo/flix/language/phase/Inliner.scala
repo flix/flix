c1b63f64ba3324f8670bf214cede44ff990be9d4
Ifaz Kabir <ifazk@users.noreply.github.com>
2017-06-18 04:25:13 -0400

    Finished inliner for ApplyRef

--- main/src/ca/uwaterloo/flix/language/phase/Inliner.scala
+++ main/src/ca/uwaterloo/flix/language/phase/Inliner.scala
@@ -17,12 +17,13 @@
 package ca.uwaterloo.flix.language.phase
 
 import ca.uwaterloo.flix.api.Flix
-import ca.uwaterloo.flix.language.CompilationError
-import ca.uwaterloo.flix.language.ast.{SimplifiedAst, Symbol, Type}
+import ca.uwaterloo.flix.language.{CompilationError, GenSym}
 import ca.uwaterloo.flix.language.ast.SimplifiedAst.Definition.Constant
-import ca.uwaterloo.flix.language.ast.SimplifiedAst.{Expression, LoadExpression, StoreExpression}
 import ca.uwaterloo.flix.language.ast.SimplifiedAst.Expression._
-import ca.uwaterloo.flix.util.Validation
+import ca.uwaterloo.flix.language.ast.SimplifiedAst.{Expression, FreeVar, LoadExpression, StoreExpression}
+import ca.uwaterloo.flix.language.ast.Symbol.{DefnSym, VarSym}
+import ca.uwaterloo.flix.language.ast.{SimplifiedAst, Symbol}
+import ca.uwaterloo.flix.util.{InternalCompilerException, Validation}
 import ca.uwaterloo.flix.util.Validation._
 
 import scala.annotation.tailrec
@@ -39,158 +40,339 @@ import scala.collection.immutable.{::, Nil}
 object Inliner extends Phase[SimplifiedAst.Root, SimplifiedAst.Root] {
 
   /**
-    * The maximum score of a function for it to be eligible for inlining.
-    */
-  val MaxScore = 25
-
-  /**
     * Performs inlining on the given AST `root`.
     */
   def run(root: SimplifiedAst.Root)(implicit flix: Flix): Validation[SimplifiedAst.Root, CompilationError] = {
+    implicit val genSym: GenSym = flix.genSym
 
-    val defnSizes = root.definitions.mapValues(c => (c, score(c)) )
+    val definitions = root.definitions
 
-    val newDefns =
-    root.definitions.mapValues(c => Constant(c.ann, c.sym, c.formals, inlineExpr(defnSizes, c.exp), c.isSynthetic, c.tpe, c.loc))
+    val scores = definitions.foldLeft(Map.empty[DefnSym, Int]) {
+      case (macc, (sym, constant)) =>
+        macc + (sym -> Score.score(constant))
+    }
 
-    SimplifiedAst.Root(newDefns, root.enums, root.lattices, root.tables, root.indexes, root.strata, root.properties, root.reachable, root.time).toSuccess
+    val inlinedDefinitions =
+    definitions.foldLeft(Map.empty[DefnSym, Constant]) {
+      case (macc, (sym,constant)) =>
+        val newExp = inlineExpr(definitions, scores, constant.exp)
+      macc + (sym -> constant.copy(exp = newExp))
+    }
+
+    root.copy(definitions = inlinedDefinitions).toSuccess
 
   }
 
   /**
-    * A param is considered safe to use for inlining if it is a variable or a literal
+    * Performs the variable substitutions from `varSyms` into `exp`
     */
-
-  def isSafeParam(param: Expression): Boolean =
-  param match {
-    case Unit => true
-    case True => true
-    case False => true
-    case Char(_) => true
-    case Float32(_) => true
-    case Float64(_) => true
-    case Int8(_) => true
-    case Int16(_) => true
-    case Int32(_) => true
-    case Int64(_) => true
-    case BigInt(_) => true
-    case Str(_) => true
-    case Var(_, _, _) => true
-    case _ => false
-  }
-
-  /**
-    * Performs the substitutions from `sub` into `exp`
-    */
-  def doSubst(sub: Map[(Symbol.VarSym, Type), Expression], exp: Expression): Expression =
+  def substituteWith(varSyms: Map[VarSym, VarSym], exp: Expression): Expression =
   {
     def f(e: Expression): Option[Expression] =
     e match {
-      case Var(sym, tpe, _) =>
-        sub.get(sym, tpe)
-      case _ => None
+      /* Substitute this expression */
+      case v: Var =>
+        varSyms.get(v.sym) match {
+          case None => None
+          case Some(sym) => Some(v.copy(sym = sym))
+        }
+      case MkClosureRef(ref, freeVars, tpe, loc) =>
+        Some(MkClosureRef(ref, freeVars.map(fv => varSyms.get(fv.sym) match {
+          case None => fv
+          case Some(sym) => FreeVar(sym, fv.tpe)
+        }), tpe, loc))
+      /* Substitute inside expression */
+      case _: LoadExpression => None
+      case _: StoreExpression => None
+      case Unit => None
+      case True => None
+      case False => None
+      case Char(_) => None
+      case Float32(_) => None
+      case Float64(_) => None
+      case Int8(_) => None
+      case Int16(_) => None
+      case Int32(_) => None
+      case Int64(_) => None
+      case BigInt(_) => None
+      case Str(_) => None
+      case Ref(sym, _, _) => None
+      case Lambda(_, _, _, _) => None
+      case Hook(_, _, _) => None
+      case MkClosure(_, _, _, _) =>
+        throw InternalCompilerException(s"Unexpected expression $e after lambda lifting")
+      case ApplyRef(_, _, _, _) => None
+      case ApplyTail(_, _, _, _, _) => None
+      case ApplyHook(_, _, _, _) => None
+      case Apply(_, _, _, _) => None
+      case Unary(_, _, _, _) => None
+      case Binary(_, _, _, _, _) => None
+      case IfThenElse(_, _, _, _, _) => None
+      case Let(_, _, _, _, _) => None
+      case Is(_, _, _) => None
+      case Tag(_, _, _, _, _) => None
+      case Untag(_, _, _, _) => None
+      case Index(_, _, _, _) => None
+      case Tuple(_, _, _) => None
+      case Existential(_, _, _) => None
+      case Universal(_, _, _) => None
+      case NativeConstructor(_, _, _, _) => None
+      case NativeField(_, _, _) => None
+      case NativeMethod(_, _, _, _) => None
+      case UserError(_, _) => None
+      case MatchError(_, _) => None
+      case SwitchError(_, _) => None
     }
-    traverseExpr(f, exp)
+    AstVisitor.visitExpressionWith(f, exp)
   }
 
   /**
-    * Either performs some high-level inlining using `defnSizes`
-    * or returns None to ask traversal to inline deeper
+    * Either performs some high-level inlining using `scores`
+    * or returns None to ask traversal to inline inside `exp`
     */
-  def inlineOpt(defnSizes:  Map[Symbol.DefnSym, (Constant, Int)], exp: SimplifiedAst): Option[Expression] =
+  def inlineVisitor(definitions: Map[DefnSym, Constant], scores: Map[DefnSym, Int], exp: Expression)(implicit genSym: GenSym): Option[Expression] =
   {
+    /**
+      * Returns args and the symbols they should be bound to in required order for substitution and let binding
+      * @param args List[Expression] of arguments
+      * @return (revArgs, argSymbols) where argSymbols is arguments with all arguments replaced with appropriate symbols
+      *         and revArgs are non-var arguments in reverse order
+      */
+    def argsToSym(args: List[Expression]): (List[(Expression,VarSym)], List[VarSym]) =
+    {
+      /**
+        * @param letAcc Arguments and symbols they need to be bound to in reverse order
+        * @param symAcc Symbols to use for inlining in reverse order
+        * @param args Arguments to replace with variables if not variables
+        */
+      @tailrec def reverseArgVars(letAcc: List[(Expression,VarSym)], symAcc: List[VarSym], args: List[Expression]):
+      (List[(Expression,VarSym)], List[VarSym]) =
+      {
+        args match {
+          case ::(head, tl) =>
+            head match {
+              case v: Var =>
+                reverseArgVars(letAcc, v.sym::symAcc, tl)
+              case _ =>
+                val s = Symbol.freshVarSym("inlnrL")
+                reverseArgVars((head, s)::letAcc, s::symAcc, tl)
+            }
+          case Nil =>
+            (letAcc, symAcc.reverse)
+        }
+      }
+
+      reverseArgVars(Nil, Nil, args)
+    }
+
+    @tailrec def letBindAndInlineArgs(revArgs: List[(Expression,VarSym)], expr: Expression): Expression =
+    {
+      revArgs match {
+        case ::(head, tl) =>
+          val letTerm = inlineExpr(definitions,scores,head._1)
+          val letSym = head._2
+          letBindAndInlineArgs(tl, Let(letSym,letTerm,expr,expr.tpe,letTerm.loc))
+        case Nil =>
+          expr
+      }
+    }
+
     exp match {
-      case ApplyRef(sym, args, tpe, loc) =>
-        if (args.forall(isSafeParam)) {
-          defnSizes.get(sym) match {
-            case None => None
-            case Some((Constant(_, _, formals, exp1, _, _, _), size)) =>
-              if (size <= MaxScore){
-                def formalMap = formals.map(f => (f.sym,f.tpe)).zip(args).toMap
-                Some(doSubst(formalMap,exp1))
-              }
-              else {
-                None
-              }
-          }
-        } else {
-          None
+      /* Inline application */
+      case ApplyRef(sym, args, _, _) =>
+        definitions(sym) match {
+          case Constant(_, _, formals, exp1, _, _, _) =>
+            if (scores(sym) <= Score.MaxScore){
+              val freshExp1 = FreshenExpr.freshenExpr(exp1)
+              val (revArgs, argSymbols) = argsToSym(args)
+              val formalMap = formals.map(f => f.sym).zip(argSymbols).toMap
+              val substituted = substituteWith(formalMap,freshExp1)
+              val letBoundExpr = letBindAndInlineArgs(revArgs, substituted)
+              Some(letBoundExpr)
+            }
+            else {
+              None
+            }
         }
-      case Let(sym, exp1, exp2, tpe, loc) =>
-        if (isSafeParam(exp1)){
-          val varMap = (((sym,exp1.tpe), exp1)::Nil).toMap
-          val cleanExp = doSubst(varMap,exp2)
-          // this is currently n^2 for deeply nested bindings, but type inference is exp(n)
-          Some(inlineExpr(defnSizes, cleanExp))
-        } else {
-          None
-        }
-      case _ => None
+      /* Inline inside expression */
+      case MkClosureRef(_, _, _, _) => None
+      case _: LoadExpression => None
+      case _: StoreExpression => None
+      case Unit => None
+      case True => None
+      case False => None
+      case Char(_) => None
+      case Float32(_) => None
+      case Float64(_) => None
+      case Int8(_) => None
+      case Int16(_) => None
+      case Int32(_) => None
+      case Int64(_) => None
+      case BigInt(_) => None
+      case Str(_) => None
+      case Var(_, _, _) => None
+      case Ref(_, _, _) => None
+      case Lambda(_, _, _, _) => None
+      case Hook(_, _, _) => None
+      case ApplyTail(_, _, _, _, _) => None
+      case ApplyHook(_, _, _, _) => None
+      case Apply(_, _, _, _) => None
+      case Unary(_, _, _, _) => None
+      case Binary(_, _, _, _, _) => None
+      case IfThenElse(_, _, _, _, _) => None
+      case Let(_, _, _, _, _) => None
+      case Is(_, _, _) => None
+      case Tag(_, _, _, _, _) => None
+      case Untag(_, _, _, _) => None
+      case Index(_, _, _, _) => None
+      case Tuple(_, _, _) => None
+      case Existential(_, _, _) => None
+      case Universal(_, _, _) => None
+      case NativeConstructor(_, _, _, _) => None
+      case NativeField(_, _, _) => None
+      case NativeMethod(_, _, _, _) => None
+      case UserError(_, _) => None
+      case MatchError(_, _) => None
+      case SwitchError(_, _) => None
+      /* Error */
+      case MkClosure(_, _, _, _) =>
+        throw InternalCompilerException(s"Unexpected expression $exp after lambda lifting")
     }
   }
 
   /**
     * Traverses through `exp` and performs inlining using
-    * definitions and sizes from `defnSizes`
+    * definitions and sizes from `scores`
     */
-  def inlineExpr(defnSizes: Map[Symbol.DefnSym, (Constant, Int)], exp: Expression): Expression =
+  def inlineExpr(definitions: Map[DefnSym, Constant], scores: Map[DefnSym, Int], exp: Expression)(implicit genSym: GenSym): Expression =
   {
-    traverseExpr(inlineOpt(defnSizes,_), exp)
+    def f = inlineVisitor(definitions,scores,_: Expression)
+    AstVisitor.visitExpressionWith(f, exp)
   }
 
+}
+
+object FreshenExpr {
+
   /**
-    * Traversal function for SimplifiedAst
+    * Freshen all let binds in an expression
     */
-  def traverseExpr(f: Expression => Option[Expression], exp: Expression): Expression =
+  def freshenExpr(exp: Expression)(implicit genSym: GenSym): Expression =
   {
-    def trav(exp1: Expression): Expression = traverseExpr(f,exp1)
+    val (m,newLets) = freshSymbols(exp)
+    Inliner.substituteWith(m,newLets)
+  }
+
+  def freshSymbols(exp:Expression)(implicit genSym: GenSym): (Map[VarSym, VarSym], Expression) =
+  {
+    var m = Map.empty[VarSym, VarSym]
+
+    def freshExprVisitor(exp3:Expression): Option[Expression] =
+    {
+      exp3 match {
+        case Let(sym, exp1, exp2, tpe, loc) =>
+          val newSym = Symbol.freshVarSym("inlnrFresh")// Symbol.freshVarSym(sym)
+          m += (sym -> newSym)
+          val freshExp1 = AstVisitor.visitExpressionWith(freshExprVisitor, exp1)
+          val freshExp2 = AstVisitor.visitExpressionWith(freshExprVisitor, exp2)
+          Some(Let(newSym,freshExp1,freshExp2,tpe,loc))
+        case _ => None
+      }
+    }
+
+    val newLets = AstVisitor.visitExpressionWith(freshExprVisitor, exp)
+    (m,newLets)
+  }
+
+}
+
+object AstVisitor {
+
+  /**
+    * Expression Visitor function for SimplifiedAST
+    *
+    * @param f function that takes in a Expression and returns a Option[Expression]
+    * @param exp Expression to visit
+    * @return if f(exp) is Some(e) then e, else recursively visit expressions in exp
+    */
+  def visitExpressionWith(f: Expression => Option[Expression], exp: Expression): Expression =
+  {
+    def visit(exp1: Expression): Expression = visitExpressionWith(f,exp1)
     f(exp) match {
       case None =>
         exp match {
+          case _: StoreExpression => exp
+          case _: LoadExpression => exp
+          case Unit => exp
+          case True => exp
+          case False => exp
+          case Char(_) => exp
+          case Float32(_) => exp
+          case Float64(_) => exp
+          case Int8(_) => exp
+          case Int16(_) => exp
+          case Int32(_) => exp
+          case Int64(_) => exp
+          case BigInt(_) => exp
+          case Str(lit) => exp
+          case Var(_, _, _) => exp
+          case Ref(_, _, _) => exp
+          case Hook(_,_,_) => exp
+          case MkClosureRef(_, _, _, _) => exp
+          case NativeField(_, _, _) => exp
+          case UserError(_, _) => exp
+          case MatchError(_, _) => exp
+          case SwitchError(_, _) => exp
           case Lambda(args, body, tpe, loc) =>
-            Lambda(args, trav(body), tpe, loc)
-          case MkClosure(lambda, freeVars, tpe, loc) =>
-            MkClosure(Lambda(lambda.args, trav(lambda.body), lambda.tpe, lambda.loc), freeVars, tpe, loc)
+            Lambda(args, visit(body), tpe, loc)
           case ApplyRef(sym, args, tpe, loc) =>
-            ApplyRef(sym, args.map(trav), tpe, loc)
+            ApplyRef(sym, args.map(visit), tpe, loc)
           case ApplyTail(sym, formals, actuals, tpe, loc) =>
-            ApplyTail(sym, formals, actuals.map(trav), tpe, loc)
+            ApplyTail(sym, formals, actuals.map(visit), tpe, loc)
           case ApplyHook(hook, args, tpe, loc) =>
-            ApplyHook(hook, args.map(trav), tpe, loc)
+            ApplyHook(hook, args.map(visit), tpe, loc)
           case Apply(exp1, args, tpe, loc) =>
-            Apply(exp1, args.map(trav), tpe, loc)
+            Apply(visit(exp1), args.map(visit), tpe, loc)
           case Unary(op, exp1, tpe, loc) =>
-            Unary(op, trav(exp1), tpe, loc)
+            Unary(op, visit(exp1), tpe, loc)
           case Binary(op, exp1, exp2, tpe, loc) =>
-            Binary(op, trav(exp1), trav(exp2), tpe, loc)
+            Binary(op, visit(exp1), visit(exp2), tpe, loc)
           case IfThenElse(exp1, exp2, exp3, tpe, loc) =>
-            IfThenElse(trav(exp1), trav(exp2), trav(exp3), tpe, loc)
+            IfThenElse(visit(exp1), visit(exp2), visit(exp3), tpe, loc)
           case Let(sym, exp1, exp2, tpe, loc) =>
-            Let(sym, trav(exp1), trav(exp2), tpe, loc)
+            Let(sym, visit(exp1), visit(exp2), tpe, loc)
           case Is(exp1, tag, loc) =>
-            Is(trav(exp1), tag, loc)
+            Is(visit(exp1), tag, loc)
           case Tag(sym, tag, exp1, tpe, loc) =>
-            Tag(sym, tag, trav(exp1), tpe, loc)
+            Tag(sym, tag, visit(exp1), tpe, loc)
           case Untag(tag, exp1, tpe, loc) =>
-            Untag(tag, trav(exp1), tpe, loc)
+            Untag(tag, visit(exp1), tpe, loc)
           case Index(base, offset, tpe, loc) =>
-            Index(trav(base), offset, tpe, loc)
+            Index(visit(base), offset, tpe, loc)
           case Tuple(elms, tpe, loc) =>
-            Tuple(elms.map(trav), tpe, loc)
+            Tuple(elms.map(visit), tpe, loc)
           case Existential(fparam, exp1, loc) =>
-            Existential(fparam, trav(exp1), loc)
+            Existential(fparam, visit(exp1), loc)
           case Universal(fparam, exp1, loc) =>
-            Universal(fparam, trav(exp1), loc)
+            Universal(fparam, visit(exp1), loc)
           case NativeConstructor(constructor, args, tpe, loc) =>
-            NativeConstructor(constructor, args.map(trav), tpe, loc)
+            NativeConstructor(constructor, args.map(visit), tpe, loc)
           case NativeMethod(method, args, tpe, loc) =>
-            NativeMethod(method, args.map(trav), tpe, loc)
-          case _ => exp
+            NativeMethod(method, args.map(visit), tpe, loc)
+          case MkClosure(_, _, _, _) =>
+            throw InternalCompilerException(s"Unexpected expression $exp after lambda lifting")
         }
       case Some(exp1) => exp1
     }
   }
 
+}
+
+object Score {
+  /**
+    * The maximum score of a function for it to be eligible for inlining.
+    */
+  val MaxScore = 25
 
   /**
     * Returns the score of the given function definition `defn`.
@@ -202,18 +384,18 @@ object Inliner extends Phase[SimplifiedAst.Root, SimplifiedAst.Root] {
     * - A UserError, MatchError, or SwitchError is worth zero points.
     * - An if-then-else statement is worth the value of the condition plus two times the sum of the consequent and alternative.
     */
-  def score(defn: Constant): Int = exprScore(defn.exp)
+  def score(definitions: Constant): Int = exprScore(definitions.exp)
 
-/**
-  * Returns the score of the given expression `exp`.
-  */
+  /**
+    * Returns the score of the given expression `exp`.
+    */
   def exprScore(exp: Expression): Int = exprListScore(0,List(exp))
 
   /**
     * Returns the total score of the list of expressions `exps`.
     */
-  @tailrec def exprListScore(acc: Int, exps: List[Expression]): Int =
-  exps match {
+  @tailrec def exprListScore(acc: Int, expressions: List[Expression]): Int =
+  expressions match {
     case ::(head, tl) =>
       head match {
         case _: LoadExpression => exprListScore(acc + 1, tl)
@@ -237,7 +419,9 @@ object Inliner extends Phase[SimplifiedAst.Root, SimplifiedAst.Root] {
         case MkClosure(lambda, freeVars, _, _) => exprListScore(acc + 1 + freeVars.length, lambda::tl)
         case MkClosureRef(ref, freeVars, _, _) => exprListScore(acc + 1 + freeVars.length, ref::tl)
         case ApplyRef(_, args, _, _) => exprListScore(acc + 2, args:::tl)
-        case ApplyTail(_, formals, actuals, _, _) => exprListScore(acc + 2 + formals.length, actuals:::tl)
+        case ApplyTail(_, formals, actuals, _, _) =>
+          // not be inlined
+          exprListScore(MaxScore + acc + 2 + formals.length, actuals:::tl)
         case ApplyHook(_, args, _, _) => exprListScore(acc + 2, args:::tl)
         case Apply(exp, args, _, _) => exprListScore(acc + 1, args:::exp::tl)
         case Unary(_, exp, _, _) => exprListScore(acc + 1, exp::tl)
