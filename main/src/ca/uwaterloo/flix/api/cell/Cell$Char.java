c0d641bc4ac30979e13456d6794026736f0877a6
Ramin Zarifi <raminzarifi@gmail.com>
2017-08-21 18:29:26 -0400

    Implementing Referencing, Dereferencing and reference assignment in backend.

---
+++ main/src/ca/uwaterloo/flix/api/cell/Cell$Char.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2017 Ramin Zarifi
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ca.uwaterloo.flix.api.cell;
+
+/**
+ * Representation of a mutable Char cell.
+ */
+public final class Cell$Char {
+
+    /**
+     * The internal value.
+     */
+    private char value;
+
+    /**
+     * Constructs a new cell with the given value.
+     */
+    public Cell$Char(char value) {
+        this.value = value;
+    }
+
+    /**
+     * Returns the value of the cell.
+     */
+    public char getValue() {
+        return this.value;
+    }
+
+    /**
+     * Sets the value of the cell.
+     */
+    public void setValue(char value) {
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        throw new UnsupportedOperationException("Cell$Char does not support 'equals'.");
+    }
+
+    @Override
+    public int hashCode() {
+        throw new UnsupportedOperationException("Cell$Char does not support 'hashCode'.");
+    }
+
+    @Override
+    public String toString() {
+        throw new UnsupportedOperationException("Cell$Char does not support 'toString'.");
+    }
+
+}
---
+++ main/src/ca/uwaterloo/flix/api/cell/Cell$Float32.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2017 Ramin Zarifi
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ca.uwaterloo.flix.api.cell;
+
+/**
+ * Representation of a mutable Float32 cell.
+ */
+public final class Cell$Float32 {
+
+    /**
+     * The internal value.
+     */
+    private float value;
+
+    /**
+     * Constructs a new cell with the given value.
+     */
+    public Cell$Float32(float value) {
+        this.value = value;
+    }
+
+    /**
+     * Returns the value of the cell.
+     */
+    public float getValue() {
+        return this.value;
+    }
+
+    /**
+     * Sets the value of the cell.
+     */
+    public void setValue(float value) {
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        throw new UnsupportedOperationException("Cell$Float32 does not support 'equals'.");
+    }
+
+    @Override
+    public int hashCode() {
+        throw new UnsupportedOperationException("Cell$Float32 does not support 'hashCode'.");
+    }
+
+    @Override
+    public String toString() {
+        throw new UnsupportedOperationException("Cell$Float32 does not support 'toString'.");
+    }
+
+}
