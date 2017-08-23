5fc84071f0cc65647e3a5fe7a237b642455bb800
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2017-08-23 09:23:33 +0200

    Implemented getRelation and getLattice.

--- main/src/ca/uwaterloo/flix/runtime/Model.scala
+++ main/src/ca/uwaterloo/flix/runtime/Model.scala
@@ -102,20 +102,54 @@ class Model(root: Root,
     }
   }
 
-  def getRelations: Map[String, (List[String], Iterable[List[String]])] = relations map {
-    case (sym, rows) =>
-      val attributes = root.tables(sym) match {
-        case Table.Relation(_, attr, _) => attr.toList
-        case Table.Lattice(_, keys, value, _) => keys.toList ::: value :: Nil
+  /**
+    * Returns a map from fully-qualified relation names to a pair of an attribute list and a set of rows for that table.
+    */
+  def getRelations: Map[String, (List[String], Iterable[List[String]])] =
+    relations.foldLeft(Map.empty[String, (List[String], Iterable[List[String]])]) {
+      case (macc, (sym, rows)) =>
+        root.tables(sym) match {
+          case Table.Relation(_, attr, _) =>
+            // Compute the attributes names.
+            val attributes: List[String] = attr.map(_.name).toList
+
+            // Compute the rows of the table.
+            val rows: Iterable[List[String]] = relations(sym).map {
+              case row => (row zip attr) map {
+                case (obj, Attribute(_, tpe)) => toString(obj, tpe)
+              }
+            }
+
+            macc + (sym.toString -> (attributes, rows))
+          case Table.Lattice(_, _, _, _) => macc // Nop
+        }
+    }
+
+  /**
+    * Returns a map from fully-qualified lattices names to a pair of an attribute list and a set of rows for that table.
+    */
+  def getLattices: Map[String, (List[String], Iterable[List[String]])] = relations.foldLeft(Map.empty[String, (List[String], Iterable[List[String]])]) {
+    case (macc, (sym, rows)) =>
+      root.tables(sym) match {
+        case Table.Relation(_, attr, _) => macc // top
+
+        case Table.Lattice(_, keys, value, _) =>
+          // Compute the attributes of the table.
+          val attr = keys.toList ::: value :: Nil
+
+          // Compute the attribute names.
+          val attributes: List[String] = attr.map(_.name)
+
+          // Compute the rows of the table.
+          val rows: Iterable[List[String]] = relations(sym).map {
+            case row => (row zip attr) map {
+              case (obj, Attribute(_, tpe)) => toString(obj, tpe)
+            }
+          }
+
+          macc + (sym.toString -> (attributes, rows))
       }
-
-      val rows = ???
-
-      (attributes.map(_.name), rows)
-      ???
   }
-
-  def getLattices: Map[String, (List[String], Iterable[List[String]])] = ???
 
   /**
     * Returns the result type of the given lambda type.
