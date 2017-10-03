90e822e488fae8de8f7098e139f20f3146d6527e
Ramin Zarifi <raminzarifi@gmail.com>
2017-10-03 00:32:02 -0400

    Skipping JvmBackend phase

--- main/src/ca/uwaterloo/flix/language/phase/jvm/JvmBackend.scala
+++ main/src/ca/uwaterloo/flix/language/phase/jvm/JvmBackend.scala
@@ -36,6 +36,7 @@ object JvmBackend extends Phase[Root, Root] {
     * Emits JVM bytecode for the given AST `root`.
     */
   def run(root: Root)(implicit flix: Flix): Validation[Root, CompilationError] = {
+    return root.toSuccess
     //
     // Compute the set of types in the program.
     //
