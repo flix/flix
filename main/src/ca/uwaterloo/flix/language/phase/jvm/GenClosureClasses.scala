2670bb54b9889de9d47a502c03786292221f922c
Magnus Madsen <mmadsen@uwaterloo.ca>
2017-10-16 10:51:21 +0200

    Added GenClosureClasses.

---
+++ main/src/ca/uwaterloo/flix/language/phase/jvm/GenClosureClasses.scala
@@ -0,0 +1,59 @@
+package ca.uwaterloo.flix.language.phase.jvm
+
+import ca.uwaterloo.flix.api.Flix
+import ca.uwaterloo.flix.language.ast.ExecutableAst.{Def, FreeVar, Root}
+import ca.uwaterloo.flix.language.ast.Symbol
+
+/**
+  * Generates byte code for the closure classes.
+  */
+object GenClosureClasses {
+
+  /**
+    * Returns the set of closures classes for the given set of definitions `defs`.
+    */
+  def gen(defs: Map[Symbol.DefnSym, Def])(implicit root: Root, flix: Flix): Map[JvmName, JvmClass] = {
+    //
+    // Generate a closure class for each closure and collect the results in a map.
+    //
+    // TODO: Do we need a mechanism to find each closure expression?
+    defs.foldLeft(Map.empty[JvmName, JvmClass]) {
+      case (macc, _) => macc // TODO
+    }
+  }
+
+  /**
+    * Returns the byte code for the closure with the given symbol `sym` and free variables `freeVars`.
+    *
+    * For example, given the symbol `mkAdder` with type (Int, Int) -> Int and the free variable `x`, we create:
+    *
+    * Clo$mkAdder extends Fn1$Int$Int {
+    *   private int x;
+    *   private int arg0;
+    *   private int res;
+    *
+    *   public Clo$mkAdder(int x) {
+    *     this.x = x;
+    *   }
+    *
+    *   public setArg0(int arg0) {
+    *     this.arg0 = arg0;
+    *   }
+    *
+    *   public int getResult() {
+    *     return this.res;
+    *   }
+    *
+    *   public void apply(Context ctx) {
+    *     this.res = this.x + this.arg0;
+    *   }
+    * }
+    *
+    *
+    */
+  // TODO: Probably needs more arguments.
+  private def genByteCode(sym: Symbol.DefnSym, freeVars: List[FreeVar])(implicit root: Root, flix: Flix): Array[Byte] = {
+    List(0xCA.toByte, 0xFE.toByte, 0xBA.toByte, 0xBE.toByte).toArray
+  }
+
+}
