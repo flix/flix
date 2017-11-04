4aacd23b4435c9a5cb7124528098c73fbb894065
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2017-11-04 15:54:53 +0100

    Added Apples-to-Oranges test cases.

--- main/src/ca/uwaterloo/flix/util/benchmark/ApplesToOranges.scala
+++ main/src/ca/uwaterloo/flix/util/benchmark/ApplesToOranges.scala
@@ -64,92 +64,103 @@ object ApplesToOranges {
   def benchmark09: Boolean = (0 until 100 * 1000).toList.foldLeft(0) { case (x, y) => x + y } == 704982704
 
   //
-  // def benchmark10(): Bool = List.foldRight((x, y) -> x + y, 0, List.range(0, 100 * 1000)) `assertEq!` 704982704
+  //  def benchmark10(): Bool = List.foldRight((x, y) -> x + y, 0, List.range(0, 100 * 1000)) `assertEq!` 704982704
   //
   def benchmark10: Boolean = (0 until 100 * 1000).toList.foldRight(0) { case (x, y) => x + y } == 704982704
 
-  //  ///
-  //  /// create two lists of integers, zip them, and compute the length of the result.
-  //  ///
-  //  @benchmark
+  //
   //  def benchmark11(): Bool = List.length(List.zip(List.range(0, 100 * 1000), List.range(0, 100 * 1000))) `assertEq!` 100000
   //
-  //  ///
-  //  /// create a list of pairs, unzip it, and compute the length of the result.
-  //  ///
-  //  @benchmark
-  //  def benchmark12(): Bool =
-  //    let (xs, ys) = List.unzip(List.map(x -> (x, 2 * x), List.range(0, 100 * 1000)));
-  //  (List.length(xs) + List.length(ys)) `assertEq!` 200000
+  def benchmark11: Boolean = (0 until 100 * 1000).toList.zip((0 until 100 * 1000).toList).length == 100000
+
   //
-  //  ///
-  //  /// create two lists of integers, check that each integer of the first list exists in the latter list.
-  //  ///
-  //  @benchmark
-  //  def benchmark13(): Bool =
-  //    let xs = List.range(0, 1000);
-  //  let ys = List.range(0, 1000);
-  //  let zs = List.map(y -> List.memberOf(y, xs), ys);
-  //  List.length(zs) `assertEq!` 1000
+  //  def benchmark12(): Bool = let (xs, ys) = List.unzip(List.map(x -> (x, 2 * x), List.range(0, 100 * 1000))); (List.length(xs) + List.length(ys)) `assertEq!` 200000
   //
-  //  ///
-  //  /// create two lists of integers, check if the cube of each integer in the first list exists in the latter list.
-  //  ///
-  //  @benchmark
-  //  def benchmark14(): Bool =
-  //    let xs = List.range(0, 1000);
-  //  let ys = List.range(0, 1000);
-  //  let zs = List.map(y -> List.exists(x -> x * x == y, xs), ys);
-  //  List.length(zs) `assertEq!` 1000
+  def benchmark12: Boolean = {
+    val (xs, ys) = (0 until 100 * 1000).toList.map(x => (x, 2 * x)).unzip
+    xs.length + ys.length == 200000
+  }
+
   //
-  //  ///
-  //  /// intersperses an integer into a list of integers.
-  //  ///
-  //  @benchmark
+  //  def benchmark13(): Bool = let xs = List.range(0, 1000); let ys = List.range(0, 1000); let zs = List.map(y -> List.memberOf(y, xs), ys); List.length(zs) `assertEq!` 1000
+  //
+  def benchmark13: Boolean = {
+    val xs = (0 until 1000).toList
+    val ys = (0 until 1000).toList
+    val zs = xs.map(y => ys.contains(y))
+    zs.length == 1000
+  }
+
+  //
+  //  def benchmark14(): Bool = let xs = List.range(0, 1000); let ys = List.range(0, 1000); let zs = List.map(y -> List.exists(x -> x * x == y, xs), ys); List.length(zs) `assertEq!` 1000
+  //
+  def benchmark14: Boolean = {
+    val xs = (0 until 1000).toList
+    val ys = (0 until 1000).toList
+    val zs = ys.map(y => xs.exists(x => x * x == y))
+    zs.length == 1000
+  }
+
+  //
   //  def benchmark15(): Bool = List.length(List.intersperse(42, List.range(0, 100 * 1000))) `assertEq!` 199999
   //
-  //  ///
-  //  /// create two lists of integers, zip them, unzip them, and compute the length of the result.
-  //  ///
-  //  @benchmark
-  //  def benchmark16(): Bool =
-  //    let (xs, ys) = List.unzip(List.zip(List.range(0, 100 * 1000), List.range(0, 100 * 1000)));
-  //  (List.length(xs) + List.length(ys)) `assertEq!` 200000
+  // TODO: Omitted. No intersperse.
+  def benchmark15: Boolean = true
+
   //
-  //  ///
-  //  /// creates a list and searches for an item from the left.
-  //  ///
-  //  @benchmark
+  //  def benchmark16(): Bool = let (xs, ys) = List.unzip(List.zip(List.range(0, 100 * 1000), List.range(0, 100 * 1000))); (List.length(xs) + List.length(ys)) `assertEq!` 200000
+  //
+  def benchmark16: Boolean = {
+    val (xs, ys) = (0 until 100 * 1000).toList.zip((0 until 100 * 1000).toList).unzip
+    xs.length + ys.length == 200000
+  }
+
+  //
   //  def benchmark17(): Bool = List.findLeft(x -> x == 50 * 1000, List.range(0, 100 * 1000)) `assertEq!` Some(50000)
   //
-  //  ///
-  //  /// creates a list and searches for an item from the right.
-  //  ///
-  //  @benchmark
+  def benchmark17: Boolean = (0 until 100 * 1000).toList.find(x => x == 50 * 1000) == Some(50000)
+
+  //
   //  def benchmark18(): Bool = List.findRight(x -> x == 50 * 1000, List.range(0, 100 * 1000)) `assertEq!` Some(50000)
   //
-  //  ///
-  //  /// creates a list of integers (in celsius) and sums them (as integers).
-  //  ///
-  //  @benchmark
+  // TODO: Omitted. No findRight.
+  def benchmark18: Boolean = true
+
+  //
   //  def benchmark19(): Bool =
   //    let f = (x, y) -> match y with {
   //    case Celsius(z) => x + z
   //  };
   //  List.foldLeft(f, 0, List.map(Celsius, List.range(0, 100 * 1000))) `assertEq!` 704982704
   //
-  //  ///
-  //  /// creates a list of integers (in celsius) and sums them (as celsius).
-  //  ///
-  //  @benchmark
+  def benchmark19: Boolean = {
+    val f: (Int, Celsius) => Int = {
+      case (x: Int, y: Celsius) => y match {
+        case Celsius(z) => x + z
+      }
+    }
+    (0 until 100 * 1000).toList.map(Celsius).foldLeft(0)(f) == 704982704
+  }
+
+  //
   //  def benchmark20(): Bool =
   //    let f = (x, y) ->
   //      let Celsius(a) = x;
-  //  let Celsius(b) = y;
-  //  Celsius(a + b);
+  //      let Celsius(b) = y;
+  //        Celsius(a + b);
   //  List.foldLeft(f, Celsius(0), List.map(Celsius, List.range(0, 100 * 1000)))  `assertEq!` Celsius(704982704)
   //
-  //  // A type safe wrapper of integers.
-  //  type Celsius = Celsius(Int)
+  def benchmark20: Boolean = {
+    val f: (Celsius, Celsius) => Celsius = {
+      case (x: Celsius, y: Celsius) => {
+        val Celsius(a) = x
+        val Celsius(b) = y
+        Celsius(a + b)
+      }
+    }
+    (0 until 100 * 1000).toList.map(Celsius).foldLeft(Celsius(0))(f) == Celsius(704982704)
+  }
+
+  final case class Celsius(t: Int) extends AnyVal
 
 }
