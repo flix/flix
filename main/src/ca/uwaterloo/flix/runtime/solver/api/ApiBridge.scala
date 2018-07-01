1405787959d81e7d68ff791afff582b5b84b11e0
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2018-07-01 11:26:46 +0200

    Refactored equality of variable symbols.

--- main/src/ca/uwaterloo/flix/runtime/solver/api/ApiBridge.scala
+++ main/src/ca/uwaterloo/flix/runtime/solver/api/ApiBridge.scala
@@ -8,13 +8,31 @@ import ca.uwaterloo.flix.runtime.solver.api.predicate._
 import ca.uwaterloo.flix.runtime.solver.api.symbol.{TableSym, VarSym}
 import ca.uwaterloo.flix.runtime.solver.api.term._
 
+import scala.collection.mutable
+
 object ApiBridge {
 
   // TODO: Transitory class.
   // TODO: Accept arguments only as proxy objects?
 
+  // Class used to ensure that the symbols share the same object by identity.
+  class SymbolCache {
+
+    private val varSyms = mutable.Map.empty[Symbol.VarSym, VarSym]
+
+    def getVarSym(sym: Symbol.VarSym): VarSym = varSyms.get(sym) match {
+      case None =>
+        val newSym = new VarSym(sym.getStackOffset)
+        varSyms += (sym -> newSym)
+        newSym
+      case Some(res) => res
+    }
+
+  }
+
   def translate(root: ExecutableAst.Root)(implicit flix: Flix): ConstraintSystem = {
     implicit val _ = root
+    implicit val cache = new SymbolCache
 
     val strata = root.strata.map(visitStratum)
     val tables = visitTables(root.tables)
@@ -22,11 +40,11 @@ object ApiBridge {
     ConstraintSystem(strata, tables, latOps)
   }
 
-  private def visitStratum(stratum: ExecutableAst.Stratum)(implicit root: ExecutableAst.Root, flix: Flix): Stratum = {
+  private def visitStratum(stratum: ExecutableAst.Stratum)(implicit root: ExecutableAst.Root, cache: SymbolCache, flix: Flix): Stratum = {
     Stratum(stratum.constraints.map(visitConstraint))
   }
 
-  private def visitTables(tables: Map[Symbol.TableSym, ExecutableAst.Table])(implicit root: ExecutableAst.Root, flix: Flix): Map[TableSym, Table] = {
+  private def visitTables(tables: Map[Symbol.TableSym, ExecutableAst.Table])(implicit root: ExecutableAst.Root, cache: SymbolCache, flix: Flix): Map[TableSym, Table] = {
     tables.foldLeft(Map.empty[TableSym, Table]) {
       case (macc, (sym, ExecutableAst.Table.Relation(_, attributes, _))) =>
         macc + (visitTableSym(sym) -> Table.Relation(visitTableSym(sym), attributes.map(visitAttribute)))
@@ -36,25 +54,25 @@ object ApiBridge {
     }
   }
 
-  private def visitConstraint(c0: ExecutableAst.Constraint)(implicit root: ExecutableAst.Root, flix: Flix): Constraint = {
+  private def visitConstraint(c0: ExecutableAst.Constraint)(implicit root: ExecutableAst.Root, cache: SymbolCache, flix: Flix): Constraint = {
     val cparams = c0.cparams.map(visitConstraintParam)
     val head = visitHeadPredicate(c0.head)
     val body = c0.body.map(visitBodyPredicate)
     Constraint(cparams, head, body)
   }
 
-  private def visitConstraintParam(c0: ExecutableAst.ConstraintParam)(implicit root: ExecutableAst.Root, flix: Flix): VarSym = c0 match {
+  private def visitConstraintParam(c0: ExecutableAst.ConstraintParam)(implicit root: ExecutableAst.Root, cache: SymbolCache, flix: Flix): VarSym = c0 match {
     case ExecutableAst.ConstraintParam.HeadParam(sym, _, _) => visitVarSym(sym)
     case ExecutableAst.ConstraintParam.RuleParam(sym, _, _) => visitVarSym(sym)
   }
 
-  private def visitHeadPredicate(h0: ExecutableAst.Predicate.Head)(implicit root: ExecutableAst.Root, flix: Flix): Predicate = h0 match {
+  private def visitHeadPredicate(h0: ExecutableAst.Predicate.Head)(implicit root: ExecutableAst.Root, cache: SymbolCache, flix: Flix): Predicate = h0 match {
     case ExecutableAst.Predicate.Head.True(_) => new TruePredicate()
     case ExecutableAst.Predicate.Head.False(_) => new FalsePredicate()
     case ExecutableAst.Predicate.Head.Atom(sym, terms, _) => new AtomPredicate(visitTableSym(sym), positive = true, terms.map(visitHeadTerm).toArray, null)
   }
 
-  private def visitBodyPredicate(b0: ExecutableAst.Predicate.Body)(implicit root: ExecutableAst.Root, flix: Flix): Predicate = b0 match {
+  private def visitBodyPredicate(b0: ExecutableAst.Predicate.Body)(implicit root: ExecutableAst.Root, cache: SymbolCache, flix: Flix): Predicate = b0 match {
     case ExecutableAst.Predicate.Body.Atom(sym, polarity, terms, index2sym, loc) =>
       val s = visitTableSym(sym)
       val p = polarity match {
@@ -76,9 +94,9 @@ object ApiBridge {
     case ExecutableAst.Predicate.Body.Loop(_, _, _) => throw new UnsupportedOperationException("Loop currently not supported")
   }
 
-  private def visitTableSym(sym: Symbol.TableSym)(implicit root: ExecutableAst.Root, flix: Flix): TableSym = new TableSym(sym.toString)
+  private def visitTableSym(sym: Symbol.TableSym)(implicit root: ExecutableAst.Root, cache: SymbolCache, flix: Flix): TableSym = new TableSym(sym.toString)
 
-  private def visitHeadTerm(t0: ExecutableAst.Term.Head)(implicit root: ExecutableAst.Root, flix: Flix): Term = t0 match {
+  private def visitHeadTerm(t0: ExecutableAst.Term.Head)(implicit root: ExecutableAst.Root, cache: SymbolCache, flix: Flix): Term = t0 match {
     case ExecutableAst.Term.Head.Var(sym, _, _) => new VarTerm(visitVarSym(sym))
     case ExecutableAst.Term.Head.Lit(lit, _, _) => new LitTerm(() => lit)
     case ExecutableAst.Term.Head.Cst(sym, _, _) => new LitTerm(() => Linker.link(sym, root).invoke(Array.emptyObjectArray))
@@ -88,7 +106,7 @@ object ApiBridge {
       new AppTerm(f, as.toArray)
   }
 
-  def visitBodyTerm(t0: ExecutableAst.Term.Body)(implicit root: ExecutableAst.Root, flix: Flix): Term = t0 match {
+  def visitBodyTerm(t0: ExecutableAst.Term.Body)(implicit root: ExecutableAst.Root, cache: SymbolCache, flix: Flix): Term = t0 match {
     case ExecutableAst.Term.Body.Wild(_, _) => new WildTerm()
     case ExecutableAst.Term.Body.Var(sym, _, _) => new VarTerm(visitVarSym(sym))
     case ExecutableAst.Term.Body.Lit(lit, _, _) => new LitTerm(() => lit)
@@ -96,12 +114,13 @@ object ApiBridge {
     case ExecutableAst.Term.Body.Pat(_, _, _) => throw new UnsupportedOperationException("Loop currently not supported")
   }
 
-  private def visitVarSym(sym: Symbol.VarSym)(implicit root: ExecutableAst.Root, flix: Flix): VarSym = new VarSym(sym.id, sym.getStackOffset)
+  private def visitVarSym(sym: Symbol.VarSym)(implicit root: ExecutableAst.Root, cache: SymbolCache, flix: Flix): VarSym =
+    cache.getVarSym(sym)
 
-  private def visitAttribute(a: ExecutableAst.Attribute)(implicit root: ExecutableAst.Root, flix: Flix): Attribute =
+  private def visitAttribute(a: ExecutableAst.Attribute)(implicit root: ExecutableAst.Root, cache: SymbolCache, flix: Flix): Attribute =
     Attribute(a.name)
 
-  private def visitLatOps(tables: Map[Symbol.TableSym, ExecutableAst.Table])(implicit root: ExecutableAst.Root, flix: Flix): Map[TableSym, LatticeOps] = {
+  private def visitLatOps(tables: Map[Symbol.TableSym, ExecutableAst.Table])(implicit root: ExecutableAst.Root, cache: SymbolCache, flix: Flix): Map[TableSym, LatticeOps] = {
     tables.foldLeft(Map.empty[TableSym, LatticeOps]) {
       case (macc, (sym, ExecutableAst.Table.Relation(_, attributes, _))) =>
         // relation
--- main/src/ca/uwaterloo/flix/runtime/solver/api/symbol/VarSym.scala
+++ main/src/ca/uwaterloo/flix/runtime/solver/api/symbol/VarSym.scala
@@ -1,22 +1,10 @@
 package ca.uwaterloo.flix.runtime.solver.api.symbol
 
-class VarSym(val id: Int, stackoffset: Int) {
-
-  // TODO: Update equality.
-
+/**
+  * Represents a variable symbol.
+  *
+  * Note: Two variable symbols are considered the same variable if they are the same object.
+  */
+class VarSym(stackoffset: Int) {
   def getStackOffset: Int = stackoffset
-
-  def canEqual(other: Any): Boolean = other.isInstanceOf[VarSym]
-
-  override def equals(other: Any): Boolean = other match {
-    case that: VarSym =>
-      (that canEqual this) &&
-        id == that.id
-    case _ => false
-  }
-
-  override def hashCode(): Int = {
-    val state = Seq(id)
-    state.map(_.hashCode()).foldLeft(0)((a, b) => 31 * a + b)
-  }
 }
