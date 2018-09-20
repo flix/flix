d91455edaffba20e523ab8f1716fca9b02832cee
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2018-09-21 00:35:12 +0200

    Added skeleton test.

--- main/test/ca/uwaterloo/flix/language/feature/FeatureSuite.scala
+++ main/test/ca/uwaterloo/flix/language/feature/FeatureSuite.scala
@@ -52,6 +52,7 @@ class FeatureSuite extends Suites(
   new FlixTest("Test.Expression.Record.Literal", "main/test/ca/uwaterloo/flix/language/feature/Test.Expression.Record.Literal.flix")(compiled = false), // TODO
   new FlixTest("Test.Expression.Record.Polymorphism", "main/test/ca/uwaterloo/flix/language/feature/Test.Expression.Record.Polymorphism.flix")(compiled = false), // TODO
   new FlixTest("Test.Expression.Record.Restrict", "main/test/ca/uwaterloo/flix/language/feature/Test.Expression.Record.Restrict.flix")(compiled = false), // TODO
+  new FlixTest("Test.Expression.Record.Update", "main/test/ca/uwaterloo/flix/language/feature/Test.Expression.Record.Update.flix")(compiled = false), // TODO
   new FlixTest("Test.Expression.Record.Select", "main/test/ca/uwaterloo/flix/language/feature/Test.Expression.Record.Select.flix")(compiled = false), // TODO
 
   new FlixTest("Test.Expression.Float", "main/test/ca/uwaterloo/flix/language/feature/Test.Expression.Float.flix"),
