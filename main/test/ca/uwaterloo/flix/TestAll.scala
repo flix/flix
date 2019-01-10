8058af6fa781bb48fde6f3aa6d39ed948a91649f
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2019-01-10 23:17:29 +0100

    Fixed test cases.

--- main/test/ca/uwaterloo/flix/TestAll.scala
+++ main/test/ca/uwaterloo/flix/TestAll.scala
@@ -16,16 +16,16 @@
 
 package ca.uwaterloo.flix
 
-import ca.uwaterloo.flix.api.ApiSuite
 import ca.uwaterloo.flix.language.LanguageSuite
 import ca.uwaterloo.flix.library.LibrarySuite
+import ca.uwaterloo.flix.tools.ToolsSuite
 import ca.uwaterloo.flix.util.UtilSuite
 import org.scalatest.{ParallelTestExecution, Suites}
 
 class TestAll extends Suites(
-  new ApiSuite,
   new LanguageSuite,
   new LibrarySuite,
+  new ToolsSuite,
   new UtilSuite,
   new TestMain,
   new TestExamples,
