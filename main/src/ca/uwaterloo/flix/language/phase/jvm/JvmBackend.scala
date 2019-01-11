e04f4830ee1eab6c1910f3605b2bc7e764c149e2
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2019-01-11 12:47:02 +0100

    Refactoring.

--- main/src/ca/uwaterloo/flix/language/phase/jvm/JvmBackend.scala
+++ main/src/ca/uwaterloo/flix/language/phase/jvm/JvmBackend.scala
@@ -183,17 +183,20 @@ object JvmBackend extends Phase[Root, CompilationResult] {
     //
     // Construct a map from symbols to actual JVM code.
     //
-    val defs = root.defs.foldLeft(Map.empty[Symbol.DefnSym, () => ProxyObject]) {
-      case (macc, (sym, defn)) =>
-        // Invokes the function with a single argument (which is supposed to be the Unit value, but we pass null instead).
-        val args: Array[AnyRef] = Array(null)
-        macc + (sym -> (() => link(sym, root).apply(args)))
-    }
+    val defs = getCompiledDefs(root)
 
     //
     // Return the compilation result.
     //
     new CompilationResult(root, defs).toSuccess
+  }
+
+  // TODO: DOC
+  private def getCompiledDefs(root: Root)(implicit flix: Flix): Map[Symbol.DefnSym, () => ProxyObject] = root.defs.foldLeft(Map.empty[Symbol.DefnSym, () => ProxyObject]) {
+    case (macc, (sym, defn)) =>
+      // Invokes the function with a single argument (which is supposed to be the Unit value, but we pass null instead).
+      val args: Array[AnyRef] = Array(null)
+      macc + (sym -> (() => link(sym, root).apply(args)))
   }
 
   /**
