59d677f0c98d41f427c5bf76b96d6a3bafc8512d
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2019-01-22 18:26:45 +0100

    Replace RpcServer with WebSockets (#766)
    * WIP SocketServer
    
    * Refactored SocketServer. Removed obsolete RpcServer.
    
    * Refactored SocketServer.
    
    * Replace .lines by replace.
    
    * Minor fixes.

--- main/src/ca/uwaterloo/flix/Main.scala
+++ main/src/ca/uwaterloo/flix/Main.scala
@@ -21,7 +21,7 @@ import java.nio.file.Paths
 
 import ca.uwaterloo.flix.api.{Flix, Version}
 import ca.uwaterloo.flix.runtime.shell.Shell
-import ca.uwaterloo.flix.tools.{Benchmarker, Packager, Tester}
+import ca.uwaterloo.flix.tools.{Benchmarker, Packager, SocketServer, Tester}
 import ca.uwaterloo.flix.util._
 import ca.uwaterloo.flix.util.vt._
 import flix.runtime.FlixError
@@ -45,9 +45,9 @@ object Main {
 
     // check if the --listen flag was passed.
     if (cmdOpts.listen.nonEmpty) {
-      val rpcServer = new RpcServer(cmdOpts.listen.get)
-      rpcServer.start()
-      rpcServer.await()
+      val socketServer = new SocketServer(cmdOpts.listen.get)
+      socketServer.run()
+      System.exit(0)
     }
 
     // the default color context.
