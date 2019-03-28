d215cc48a21c19d85c19e400d6cca17de68d80ae
Quentin Stiévenart <quentin.stievenart@gmail.com>
2019-03-28 11:35:36 +0100

    Ensure unlock operations are always performed

--- main/src/ca/uwaterloo/flix/runtime/interpreter/Channel.java
+++ main/src/ca/uwaterloo/flix/runtime/interpreter/Channel.java
@@ -92,39 +92,39 @@ public final class Channel {
     while (!Thread.interrupted()) {
       // Lock all channels in sorted order
       lockAllChannels(sortedChannels);
-      // Lock the select lock after the channels
-      selectLock.lock();
-
       try {
-        // Check if any channel has an element
-        for (int index = 0; index < channels.length; index++) {
-          Channel channel = channels[index];
-          Object element = channel.tryGet();
-          if (element != null) {
-            // Element found.
-            // Return the element and the branchNumber (index of the array) of the containing channel
-            return new SelectChoice(index, element);
+        // Lock the select lock after the channels
+        selectLock.lock();
+
+        try {
+          // Check if any channel has an element
+          for (int index = 0; index < channels.length; index++) {
+            Channel channel = channels[index];
+            Object element = channel.tryGet();
+            if (element != null) {
+              // Element found.
+              // Return the element and the branchNumber (index of the array) of the containing channel
+              return new SelectChoice(index, element);
+            }
           }
+
+          // No element was found.
+
+          // If there is a default case, choose this
+          if (hasDefault) {
+            return SelectChoice.DEFAULT_CHOICE;
+          }
+
+          // Add our condition to all channels to get notified when a new element is added
+          for (Channel channel : channels) {
+            channel.addGetter(selectLock, condition);
+          }
+        } finally {
+          // Unlock all channels in sorted order, so other threads may input elements
+          unlockAllChannels(sortedChannels);
         }
 
-        // No element was found.
-
-        // If there is a default case, choose this
-        if (hasDefault) {
-          return SelectChoice.DEFAULT_CHOICE;
-        }
-
-        // Add our condition to all channels to get notified when a new element is added
-        for (Channel channel : channels) {
-          channel.addGetter(selectLock, condition);
-        }
-      } finally {
-        // Unlock all channels in sorted order, so other threads may input elements
-        unlockAllChannels(sortedChannels);
-      }
-
-      // Wait for an element to be added to any of the channels
-      try {
+        // Wait for an element to be added to any of the channels
         condition.await();
       } catch (InterruptedException e) {
         throw new RuntimeException("Thread interrupted");
