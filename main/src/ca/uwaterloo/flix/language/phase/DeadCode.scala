18c91976308682a97ba1b67b6a722f77afd38e65
Jonas Riise Hamre <jonas.riise.hamre@post.au.dk>
2019-05-02 13:28:41 +0200

    Rewrote DeadCode to work with Used in which a Validation list is included

--- main/src/ca/uwaterloo/flix/language/phase/DeadCode.scala
+++ main/src/ca/uwaterloo/flix/language/phase/DeadCode.scala
@@ -45,270 +45,164 @@ object DeadCode extends Phase[Root, Root] {
     enumKeys.map(x => (enum.sym, x))
   }
 
-  private def visitDef(defn: TypedAst.Def): (Validation[TypedAst.Def, DeadCodeError], Used) = {
-    val expVal = visitExp(defn.exp)
-    val used = expVal match {
-      case Validation.Success(value) => value
-      case _ => Used() // TODO: Think this through
-    }
-    val fparamSyms = (for (fparam <- defn.fparams if fparam.sym.text != "_unit") yield fparam.sym).toSet
-    val unusedFParamSyms = fparamSyms -- used.vars
-    val fparamSymVal = if (unusedFParamSyms.size == 0) defn.toSuccess else DeadCodeError(unusedFParamSyms.head.loc, "Formal parameter never used.").toFailure
+  private def visitDef(defn: TypedAst.Def): (Validation[TypedAst.Def, DeadCodeError], Used) = { // TODO: Check
+    val expU = visitExp(defn.exp)
+    val expVal = expU.getTraversedVal
+    val declSyms = (for (fparam <- defn.fparams if fparam.sym.text != "_unit") yield fparam.sym).toSet
+    val unusedSyms = declSyms -- expU.vars
+    val fparamSymVal = if (unusedSyms.size == 0) defn.toSuccess else DeadCodeError(unusedSyms.head.loc, "Formal parameter never used.").toFailure
     (mapN(expVal, fparamSymVal) {
       case _ => defn
-    }, used.removeVars)
+    }, expU)
   }
 
   // TODO: Clean up variable names now that this functions returns things other than Validation
-  private def visitExp(e: TypedAst.Expression): (Validation[Used, DeadCodeError]) =
+  private def visitExp(e: TypedAst.Expression): Used =
     e match {
       case TypedAst.Expression.Var(sym, tpe, eff, loc) =>
-        Used(Set(sym), Set.empty, Set.empty).toSuccess
+        Used(Set(sym), Set.empty, Set.empty, List(e.toSuccess))
       case TypedAst.Expression.Def(sym, tpe, eff, loc) =>
-        Used(Set.empty, Set.empty, Set(sym)).toSuccess
+        Used(Set.empty, Set.empty, Set(sym), List(e.toSuccess))
       case TypedAst.Expression.Lambda(fparam, exp, tpe, eff, loc) =>
-        val expVal = visitExp(exp)
-        if (expVal.get.vars.contains(fparam.sym)) { // TODO: Account for case of Failure
-          expVal
+        val expU = visitExp(exp)
+        if (expU.vars.contains(fparam.sym)) { // TODO: Account for case of Failure
+          expU
         } else {
-          DeadCodeError(fparam.sym.loc, "Lambda parameter never used.").toFailure
+          Used() + DeadCodeError(fparam.sym.loc, "Lambda parameter never used.").toFailure
         }
-      case TypedAst.Expression.Apply(exp1, exp2, tpe, eff, loc) =>
-        mapN(visitExp(exp1), visitExp(exp2)) {
-          case (e1, e2) => e1 + e2
-        }
+      case TypedAst.Expression.Apply(exp1, exp2, tpe, eff, loc) => visitExp(exp1) + visitExp(exp2)
       case TypedAst.Expression.Unary(op, exp, tpe, eff, loc) => visitExp(exp)
-      case TypedAst.Expression.Binary(op, exp1, exp2, tpe, eff, loc) =>
-        mapN(visitExp(exp1), visitExp(exp2)) {
-          case (e1, e2) => e1 + e2
-        }
+      case TypedAst.Expression.Binary(op, exp1, exp2, tpe, eff, loc) => visitExp(exp1) + visitExp(exp2)
       case TypedAst.Expression.Let(sym, exp1, exp2, tpe, eff, loc) =>
-        val letVal = visitExp(exp1)
-        val expVal = visitExp(exp2)
-        expVal match {
-          case Validation.Success(value) =>
-            if (!value.vars.contains(sym)) {
-              DeadCodeError(sym.loc, "Variable never used.").toFailure // Correct?
-            } else {
-              mapN(letVal, expVal) {
-                case (l, e) => l + e
-              }
-            }
-          case _ => // TODO: Improve/fix (the below repeats the above)
-            mapN(letVal, expVal) {
-              case (l, e) => l + e
-            }
+        val letU = visitExp(exp1)
+        val expU = visitExp(exp2)
+        if(sym.text == "_temp" || expU.vars.contains(sym)) { // TODO: Consider alternatives
+          letU + expU
+        } else {
+          letU + expU + DeadCodeError(sym.loc, "Variable never used.").toFailure
         }
       case TypedAst.Expression.LetRec(sym, exp1, exp2, tpe, eff, loc) =>
-        // I just copied this from the above case
-        val letVal = visitExp(exp1)
-        val expVal = visitExp(exp2)
-        if(!expVal.get.vars.contains(sym)) { // TODO: Account for case of Failure
-          DeadCodeError(sym.loc, "Variable never used.").toFailure // Correct?
+        val used = visitExp(exp1) + visitExp(exp2)
+        if(sym.text == "_temp" || used.vars.contains(sym)) {
+          used
         } else {
-          mapN(letVal, expVal) {
-            case (l, ex) => (l + ex) - sym
-          }
+          used + DeadCodeError(sym.loc, "Variable never used.").toFailure
         }
-      case TypedAst.Expression.IfThenElse(exp1, exp2, exp3, tpe, eff, loc) =>
-        val condVal = visitExp(exp1)
-        val thenVal = visitExp(exp2)
-        val elseVal = visitExp(exp3)
-        mapN(condVal, thenVal, elseVal) {
-          case (c, t, ex) => c + t + ex
-        }
+      case TypedAst.Expression.IfThenElse(exp1, exp2, exp3, tpe, eff, loc) => visitExp(exp1) + visitExp(exp2) + visitExp(exp3)
       case TypedAst.Expression.Match(exp, rules, tpe, eff, loc) =>
-        val matchVal = visitExp(exp)
-        val rulesVal = traverse(rules) {
-          case TypedAst.MatchRule(pat, guard, exp) =>
-            val guardVal = visitExp(guard)
-            val expVal = visitExp(exp)
-            val patRes = patternVar(pat)
-            val unusedSyms = patRes -- expVal.get.vars // TODO: Account for case of Failure
-            if(unusedSyms.size == 0) {
-              mapN(guardVal, expVal) {
-                case (g, ex) => g + ex
-              }
-            } else {
-              DeadCodeError(unusedSyms.head.loc, "Matching variable(s) not used in expression.").toFailure
-            }
-        }
-        mapN(matchVal, rulesVal) {
-          case (m, r) => m + (r.foldLeft(Used()) (_ + _))
-        }
+        val matchU = visitExp(exp)
+        val rulesU = (for (r <- rules) yield {
+          val guardU = visitExp(r.guard)
+          val expU = visitExp(r.exp)
+          val patRes = patternVar(r.pat)
+          val unusedSyms = patRes -- expU.vars
+          if(unusedSyms.size == 0) {
+            guardU + expU
+          } else {
+            guardU + expU + DeadCodeError(unusedSyms.head.loc, "Matching variable(s) not used in expression.").toFailure
+          }
+        }).foldLeft(Used()) (_ + _)
+        matchU + rulesU
       case TypedAst.Expression.Switch(rules, tpe, eff, loc) =>
-        val rulesVal = traverse(rules) {
-          case (elm1, elm2) =>
-            mapN(visitExp(elm1), visitExp(elm2)) {
-              case (e1, e2) => e1 + e2
-            }
-        }
-        mapN(rulesVal) {
-          case ru => ru.foldLeft(Used()) (_ + _)
-        }
+        (for (r <- rules) yield {
+          visitExp(r._1) + visitExp(r._2)
+        }).foldLeft(Used()) (_ + _) // TODO: Improve
       case TypedAst.Expression.Tag(sym, tag, exp, tpe, eff, loc) =>
-        val expVal = visitExp(exp)
-        val newTagVal = Used(Set.empty, Set((sym, tag)), Set.empty).toSuccess
-        mapN(expVal, newTagVal) {
-          case (ex, n) => ex + n
-        }
+        visitExp(exp) + (sym, tag)
       case TypedAst.Expression.Tuple(elms, tpe, eff, loc) =>
-        val elmsVal = traverse(elms) { case elm => visitExp(elm) }
-        mapN(elmsVal) {
-          case ex => ex.foldLeft(Used()) (_ + _)
-        }
+        elms.foldLeft(Used()) ((u, e) => u + visitExp(e))
       case TypedAst.Expression.RecordSelect(exp, label, tpe, eff, loc) => visitExp(exp)
       case TypedAst.Expression.RecordExtend(label, value, rest, tpe, eff, loc) =>
-        mapN(visitExp(value), visitExp(rest)) {
-          case (v, r) => v + r
-        }
+        visitExp(value) + visitExp(rest)
       case TypedAst.Expression.RecordRestrict(label, rest, tpe, eff, loc) => visitExp(rest)
       case TypedAst.Expression.ArrayLit(elms, tpe, eff, loc) =>
-        val elmsVal = traverse(elms) { case elm => visitExp(elm) }
-        mapN(elmsVal) {
-          case ex => ex.foldLeft(Used()) (_ + _)
-        }
+        elms.foldLeft(Used()) ((u, e) => u + visitExp(e))
       case TypedAst.Expression.ArrayNew(elm, len, tpe, eff, loc) =>
-        mapN(visitExp(elm), visitExp(len)) {
-          case (el, l) => el + l
-        }
+        visitExp(elm) + visitExp(len)
       case TypedAst.Expression.ArrayLoad(base, index, tpe, eff, loc) =>
-        mapN(visitExp(base), visitExp(index)) {
-          case (b, i) => b + i
-        }
+        visitExp(base) + visitExp(index)
       case TypedAst.Expression.ArrayLength(base, tpe, eff, loc) => visitExp(base)
       case TypedAst.Expression.ArrayStore(base, index, elm, tpe, eff, loc) =>
-        val baseVal = visitExp(base)
-        val indexVal = visitExp(index)
-        val elmVal = visitExp(elm)
-        mapN(visitExp(base), visitExp(index), visitExp(elm)) {
-          case (b, i, el) => b + i + el
-        }
+        visitExp(base) + visitExp(index) + visitExp(elm)
       case TypedAst.Expression.ArraySlice(base, beginIndex, endIndex, tpe, eff, loc) =>
-        mapN(visitExp(base), visitExp(beginIndex), visitExp(endIndex)) {
-          case (ba, be, en) => ba + be + en
-        }
+        visitExp(base) + visitExp(beginIndex) + visitExp(endIndex)
       case TypedAst.Expression.VectorLit(elms, tpe, eff, loc) =>
-        val elmsVal = traverse(elms) { case elm => visitExp(elm) }
-        mapN(elmsVal) {
-          case ex => ex.foldLeft(Used()) (_ + _)
-        }
+        elms.foldLeft(Used()) ((u, e) => u + visitExp(e))
       case TypedAst.Expression.VectorNew(elm, len, tpe, eff, loc) => visitExp(elm)
       case TypedAst.Expression.VectorLoad(base, index, tpe, eff, loc) => visitExp(base)
       case TypedAst.Expression.VectorStore(base, index, elm, tpe, eff, loc) =>
-        mapN(visitExp(base), visitExp(elm)) {
-          case (b, el) => b + el
-        }
+        visitExp(base) + visitExp(elm)
       case TypedAst.Expression.VectorLength(base, tpe, eff, loc) => visitExp(base)
       case TypedAst.Expression.VectorSlice(base, startIndex, endIndex, tpe, eff, loc) =>
-        mapN(visitExp(base), visitExp(endIndex)) {
-          case (b, en) => b + en
-        }
+        visitExp(base) + visitExp(endIndex)
       case TypedAst.Expression.Ref(exp, tpe, eff, loc) => visitExp(exp)
       case TypedAst.Expression.Deref(exp, tpe, eff, loc) => visitExp(exp)
       case TypedAst.Expression.Assign(exp1, exp2, tpe, eff, loc) =>
-        mapN(visitExp(exp1), visitExp(exp2)) {
-          case (e1, e2) => e1 + e2
-        }
+        visitExp(exp1) + visitExp(exp2)
       case TypedAst.Expression.HandleWith(exp, bindings, tpe, eff, loc) =>
-        val bindingsVal = traverse(bindings) {
-          case TypedAst.HandlerBinding(sym, exp) => visitExp(exp)
-        }
-        mapN(visitExp(exp), bindingsVal) {
-          case (ex, bi) => ex + bi.foldLeft(Used()) (_ + _)
-        }
+        val bindingsU = bindings.foldLeft(Used()) ((u, b) => u + visitExp(b.exp))
+        visitExp(exp) + bindingsU
       case TypedAst.Expression.Existential(fparam, exp, eff, loc) =>
-        mapN(visitExp(exp)) {
-          case ex => ex + Used(Set(fparam.sym), Set.empty, Set.empty)
-        }
+        visitExp(exp) + fparam.sym
       case TypedAst.Expression.Universal(fparam, exp, eff, loc) =>
-        mapN(visitExp(exp)) {
-          case ex => ex + Used(Set(fparam.sym), Set.empty, Set.empty)
-        }
+        visitExp(exp) + fparam.sym
       case TypedAst.Expression.Ascribe(exp, tpe, eff, loc) => visitExp(exp)
       case TypedAst.Expression.Cast(exp, tpe, eff, loc) => visitExp(exp)
       case TypedAst.Expression.NativeConstructor(constructor, args, tpe, eff, loc) =>
-        val argsVal = traverse(args) { case arg => visitExp(arg) }
-        mapN(argsVal) {
-          case ar => ar.foldLeft(Used()) (_ + _)
-        }
+        args.foldLeft(Used()) ((u, e) => u + visitExp(e))
       case TypedAst.Expression.TryCatch(exp, rules, tpe, eff, loc) =>
-        val rulesVal = traverse(rules) {
-          case TypedAst.CatchRule(sym, clazz, exp) => mapN(Used(Set(sym), Set.empty, Set.empty).toSuccess, visitExp(exp)) {
-            case (sy, ex) => sy + ex
-          }
-        }
-        mapN(visitExp(exp), rulesVal) {
-          case (ex, ru) => ex + ru.foldLeft(Used()) (_ + _)
-        }
+        val rulesU = rules.foldLeft(Used()) ((u, c) => u + visitExp(c.exp) + c.sym)
+        visitExp(exp) + rulesU
       case TypedAst.Expression.NewChannel(exp, tpe, eff, loc) => visitExp(exp)
       case TypedAst.Expression.GetChannel(exp, tpe, eff, loc) => visitExp(exp)
       case TypedAst.Expression.PutChannel(exp1, exp2, tpe, eff, loc) =>
-        mapN(visitExp(exp1), visitExp(exp2)) {
-          case (e1, e2) => e1 + e2
-        }
+        visitExp(exp1) + visitExp(exp2)
       case TypedAst.Expression.SelectChannel(rules, default, tpe, eff, loc) =>
-        val defVal = default match {
+        val defU = default match {
           case Some(ex) => visitExp(ex)
-          case None => Used().toSuccess
+          case None => Used()
         }
-        val rulesVal = traverse(rules) {
-          case TypedAst.SelectChannelRule(sym, chan, exp) =>
-            mapN(Used(Set(sym), Set.empty, Set.empty).toSuccess, visitExp(chan), visitExp(exp)) {
-              case (sy, ch, ex) => sy + ch + ex
-            }
-        }
-        mapN(defVal, rulesVal) {
-          case (de, ru) => de + ru.foldLeft(Used()) (_ + _)
-        }
+        val rulesU = (for (r <- rules) yield {
+          val chanU = visitExp(r.chan)
+          val expU = visitExp(r.exp)
+          if (expU.vars.contains(r.sym)) {
+            chanU + expU
+          } else {
+            chanU + expU + DeadCodeError(r.sym.loc, "Select channel variable never used.").toFailure
+          }
+        }).foldLeft(Used()) (_ + _)
+        defU + rulesU
       case TypedAst.Expression.Spawn(exp, tpe, eff, loc) => visitExp(exp)
       case TypedAst.Expression.Sleep(exp, tpe, eff, loc) => visitExp(exp)
       case TypedAst.Expression.FixpointConstraint(c, tpe, eff, loc) =>
-        val cparamsVal = traverse(c.cparams) {
-          case TypedAst.ConstraintParam.HeadParam(sym, tpe, loc) => Used(Set(sym), Set.empty, Set.empty).toSuccess
-          case TypedAst.ConstraintParam.RuleParam(sym, tpe, loc) => Used(Set(sym), Set.empty, Set.empty).toSuccess
-        }
-        val headVal = c.head match {
+        val cparamsSyms = (for (cp <- c.cparams) yield {
+          cp match {
+            case TypedAst.ConstraintParam.HeadParam(sym, tpe, loc) => sym
+            case TypedAst.ConstraintParam.RuleParam(sym, tpe, loc) => sym
+          }
+        }).toSet
+        val headU = c.head match {
           case TypedAst.Predicate.Head.Atom(pred, terms, tpe, loc) =>
-            val predVal = visitExp(pred.exp)
-            val termsVal = traverse(terms) {
-              case ex => visitExp(ex)
-            }
-            mapN(predVal, termsVal) {
-              case (pr, te) => pr + te.foldLeft(Used()) (_ + _)
-            }
+            visitExp(pred.exp) + terms.foldLeft(Used()) ((u, e) => u + visitExp(e))
         }
-        val bodyVal = traverse(c.body) {
-          case TypedAst.Predicate.Body.Atom(pred, polarity, terms, tpe, loc) => visitExp(pred.exp)
-          case TypedAst.Predicate.Body.Filter(sym, terms, loc) =>
-            val termsVal = traverse(terms) {
-              case ex => visitExp(ex)
-            }
-            mapN(termsVal) {
-              case trm => (trm.foldLeft(Used()) (_ + _)) + sym
-            }
-          case TypedAst.Predicate.Body.Functional(sym, term, loc) =>
-            mapN(Used(Set(sym), Set.empty, Set.empty).toSuccess, visitExp(term)) {
-              case (sy, te) => sy + te
-            }
+        val bodyU = for (b <- c.body) yield {
+          b match {
+            case TypedAst.Predicate.Body.Atom(pred, polarity, terms, tpe, loc) => visitExp(pred.exp)
+            case TypedAst.Predicate.Body.Filter(sym, terms, loc) =>
+              terms.foldLeft(Used()) ((u, e) => u + visitExp(e)) + sym
+            case TypedAst.Predicate.Body.Functional(sym, term, loc) =>
+              visitExp(term) + sym
+          }
         }
-        mapN(cparamsVal, headVal, bodyVal) {
-          (cp, he, bo) => cp.foldLeft(Used()) (_ + _) + he + bo.foldLeft(Used()) (_ + _)
-        }
+        headU + bodyU.foldLeft(Used()) (_ + _) + cparamsSyms
       case TypedAst.Expression.FixpointCompose(exp1, exp2, tpe, eff, loc) =>
-        mapN(visitExp(exp1), visitExp(exp2)) {
-          case (e1, e2) => e1 + e2
-        }
+        visitExp(exp1) + visitExp(exp2)
       case TypedAst.Expression.FixpointSolve(exp, tpe, eff, loc) => visitExp(exp)
       case TypedAst.Expression.FixpointProject(pred, exp, tpe, eff, loc) =>
-        mapN(visitExp(pred.exp), visitExp(exp)) {
-          case (p, ex) => p + ex
-        }
+        visitExp(pred.exp) + visitExp(exp)
       case TypedAst.Expression.FixpointEntails(exp1, exp2, tpe, eff, loc) =>
-        mapN(visitExp(exp1), visitExp(exp2)) {
-          case (e1, e2) => e1 + e2
-        }
-      case _ => Used().toSuccess
+        visitExp(exp1) + visitExp(exp2)
+      case _ => Used()
     }
 
   private def patternVar(pat: TypedAst.Pattern): Set[Symbol.VarSym] =
@@ -320,12 +214,18 @@ object DeadCode extends Phase[Root, Root] {
     }
 }
 
-case class Used(vars: Set[Symbol.VarSym] = Set.empty, tags: Set[(Symbol.EnumSym, String)] = Set.empty, defs: Set[Symbol.DefnSym] = Set.empty) {
-  def +(that: Used): Used = Used(vars ++ that.vars, tags ++ that.tags, defs ++ that.defs)
-  def +(that: Symbol.VarSym) = Used(vars + that, tags, defs)
-  def +(that: Symbol.DefnSym) = Used(vars, tags, defs + that)
-  def -(that: Used): Used = Used(vars -- that.vars, tags -- that.tags, defs -- that.defs)
-  def -(sym: Symbol.VarSym): Used = Used(vars - sym, tags, defs)
-  def removeVars: Used = Used(Set.empty, tags, defs)
+case class Used(vars: Set[Symbol.VarSym] = Set.empty, tags: Set[(Symbol.EnumSym, String)] = Set.empty, defs: Set[Symbol.DefnSym] = Set.empty, vals: List[Validation[TypedAst.Expression, DeadCodeError]] = List.empty) {
+  def +(that: Used): Used = Used(vars ++ that.vars, tags ++ that.tags, defs ++ that.defs, vals ::: that.vals)
+  def +(that: Symbol.VarSym) = Used(vars + that, tags, defs, vals)
+  def +(that: Set[Symbol.VarSym]) = Used(vars ++ that, tags, defs, vals)
+  def +(that: (Symbol.EnumSym, String)) = Used(vars, tags + that, defs, vals)
+  def +(that: Symbol.DefnSym) = Used(vars, tags, defs + that, vals)
+  def +(that: Validation[TypedAst.Expression, DeadCodeError]) = Used(vars, tags, defs, vals ::: List(that))
+  def -(that: Used): Used = Used(vars -- that.vars, tags -- that.tags, defs -- that.defs, vals)
+  def -(sym: Symbol.VarSym): Used = Used(vars - sym, tags, defs, vals)
+  def removeVars: Used = Used(Set.empty, tags, defs, vals)
+  def getTraversedVal: Validation[List[TypedAst.Expression], DeadCodeError] = traverse(this.vals) {
+    case exp => exp
+  }
 }
 
