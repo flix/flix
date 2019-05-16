3fae4e3db6f3cf521f19246e285f4d3f18a71fb1
Miguel Fialho <miguel.a.fialho@gmail.com>
2019-05-16 12:23:37 +0200

    Added comments to the closure class

--- main/src/ca/uwaterloo/flix/language/phase/njvm/classes/Closure.scala
+++ main/src/ca/uwaterloo/flix/language/phase/njvm/classes/Closure.scala
@@ -28,20 +28,30 @@ import ca.uwaterloo.flix.util.InternalCompilerException
 
 import scala.reflect.runtime.universe._
 
-class Closure(closure : ClosureInfo)(implicit root: Root, flix : Flix) extends MnemonicsClass {
+/**
+  * Class that generates the closure classes
+  * It receives all the necessary informantion through the parameter closure
+  *
+  * @param closure contains all the information about the closure
+  */
+class Closure(closure: ClosureInfo)(implicit root: Root, flix: Flix) extends MnemonicsClass {
 
+  //Extract the arguments and the result type from the closure
   private val MonoType.Arrow(targs, tresult) = closure.tpe
-  private val elms = targs.map(getErasedJvmType)
-  private val returnType = getErasedJvmType(tresult)
-  private val cloFieldsTypes = closure.freeVars.map(freeVar => getErasedJvmType(freeVar.tpe))
-  //Setup
+  //Erase the arguments, result and the free vars types of the closure
+  private val argsTpe = targs.map(getErasedJvmType)
+  private val retTpe = getErasedJvmType(tresult)
+  private val cloFreevarsTpes = closure.freeVars.map(freeVar => getErasedJvmType(freeVar.tpe))
+  //Get the class reference by providing the closure information
   private val ct: Reference = getClosureClassType(closure)
-  private val cg: ClassGenerator = new ClassGenerator(ct, List(getFunctionInterfaceType(elms, returnType), NJvmType.Spawnable))
+  //Instantiate the class generator. Closures implment the spawnable interface and the respective function interface
+  private val cg: ClassGenerator = new ClassGenerator(ct, List(getFunctionInterfaceType(argsTpe, retTpe), NJvmType.Spawnable))
 
-  private val creationContext : Field[Ref[MObject]] = cg.mkField("creationContext")
+  //Generate the creation context field, the field is of type Object.
+  private val creationContext: Field[Ref[MObject]] = cg.mkField("creationContext")
 
-  //Fields
-  for ((arg, ind) <- cloFieldsTypes.zipWithIndex) {
+  //For each of the free vars generate a field. If it is a prim type generate a prim field as it can be boxed
+  for ((arg, ind) <- cloFreevarsTpes.zipWithIndex) {
     arg match {
       case PrimBool => cg.mkPrimField[MBool]("clo" + ind)
       case PrimChar => cg.mkPrimField[MChar]("clo" + ind)
@@ -56,26 +66,40 @@ class Closure(closure : ClosureInfo)(implicit root: Root, flix : Flix) extends M
     }
   }
 
-  private def getCloField[T1 <: MnemonicsTypes : TypeTag](ind : Int) : Field[T1] =
-    new Field[T1]("clo" + ind)
+  /**
+    * Method which generates the capability to LOAD/STORE a free var field
+    *
+    * @param offset of the field we want to obtain
+    */
+  private def getCloField[T1 <: MnemonicsTypes : TypeTag](offset: Int): Field[T1] =
+    new Field[T1]("clo" + offset)
 
-  private def getCloPrimField[T1 <: MnemonicsPrimTypes : TypeTag](ind : Int) : PrimField[T1] =
-    new PrimField[T1]("clo" + ind)
+  /**
+    * Method which generates the capability to LOAD/STORE a free var field,
+    * since it is a prim field it also allows boxing
+    *
+    * @param offset of the field we want to obtain
+    */
+  private def getCloPrimField[T1 <: MnemonicsPrimTypes : TypeTag](offset: Int): PrimField[T1] =
+    new PrimField[T1]("clo" + offset)
 
-  for ((arg, ind) <- elms.zipWithIndex) {
+  //For each of the closure arguments generate the field and a set argument method.
+  for ((arg, offset) <- argsTpe.zipWithIndex) {
+    //Pattern match on each of the possible arg types and generate the respective field and setArg method
+    //We need to pattern match as we want to the field and the method signature to match the types.
     arg match {
       case PrimBool =>
-        val field = cg.mkPrimField[MBool]("arg" + ind)
-        cg.mkVoidMethod2[Ref[Closure], MBool]("setArg" + ind,
+        val field = cg.mkPrimField[MBool]("arg" + offset)
+        cg.mkVoidMethod2[Ref[Closure], MBool]("setArg" + offset,
           sig =>
             sig.getArg1.LOAD[StackNil] |>>
-             sig.getArg2.LOAD |>>
-             field.PUT_FIELD |>>
-             RETURN_VOID
+              sig.getArg2.LOAD |>>
+              field.PUT_FIELD |>>
+              RETURN_VOID
         )
       case PrimChar =>
-        val field = cg.mkPrimField[MChar]("arg" + ind)
-        cg.mkVoidMethod2[Ref[Closure], MChar]("setArg" + ind,
+        val field = cg.mkPrimField[MChar]("arg" + offset)
+        cg.mkVoidMethod2[Ref[Closure], MChar]("setArg" + offset,
           sig =>
             sig.getArg1.LOAD[StackNil] |>>
               sig.getArg2.LOAD |>>
@@ -83,8 +107,8 @@ class Closure(closure : ClosureInfo)(implicit root: Root, flix : Flix) extends M
               RETURN_VOID
         )
       case PrimByte =>
-        val field = cg.mkPrimField[MByte]("arg" + ind)
-        cg.mkVoidMethod2[Ref[Closure], MByte]("setArg" + ind,
+        val field = cg.mkPrimField[MByte]("arg" + offset)
+        cg.mkVoidMethod2[Ref[Closure], MByte]("setArg" + offset,
           sig =>
             sig.getArg1.LOAD[StackNil] |>>
               sig.getArg2.LOAD |>>
@@ -92,8 +116,8 @@ class Closure(closure : ClosureInfo)(implicit root: Root, flix : Flix) extends M
               RETURN_VOID
         )
       case PrimShort =>
-        val field = cg.mkPrimField[MShort]("arg" + ind)
-        cg.mkVoidMethod2[Ref[Closure], MShort]("setArg" + ind,
+        val field = cg.mkPrimField[MShort]("arg" + offset)
+        cg.mkVoidMethod2[Ref[Closure], MShort]("setArg" + offset,
           sig =>
             sig.getArg1.LOAD[StackNil] |>>
               sig.getArg2.LOAD |>>
@@ -101,8 +125,8 @@ class Closure(closure : ClosureInfo)(implicit root: Root, flix : Flix) extends M
               RETURN_VOID
         )
       case PrimInt =>
-        val field = cg.mkPrimField[MInt]("arg" + ind)
-        cg.mkVoidMethod2[Ref[Closure], MInt]("setArg" + ind,
+        val field = cg.mkPrimField[MInt]("arg" + offset)
+        cg.mkVoidMethod2[Ref[Closure], MInt]("setArg" + offset,
           sig =>
             sig.getArg1.LOAD[StackNil] |>>
               sig.getArg2.LOAD |>>
@@ -110,8 +134,8 @@ class Closure(closure : ClosureInfo)(implicit root: Root, flix : Flix) extends M
               RETURN_VOID
         )
       case PrimLong =>
-        val field = cg.mkPrimField[MLong]("arg" + ind)
-        cg.mkVoidMethod2[Ref[Closure], MLong]("setArg" + ind,
+        val field = cg.mkPrimField[MLong]("arg" + offset)
+        cg.mkVoidMethod2[Ref[Closure], MLong]("setArg" + offset,
           sig =>
             sig.getArg1.LOAD[StackNil] |>>
               sig.getArg2.LOAD |>>
@@ -119,8 +143,8 @@ class Closure(closure : ClosureInfo)(implicit root: Root, flix : Flix) extends M
               RETURN_VOID
         )
       case PrimFloat =>
-        val field = cg.mkPrimField[MFloat]("arg" + ind)
-        cg.mkVoidMethod2[Ref[Closure], MFloat]("setArg" + ind,
+        val field = cg.mkPrimField[MFloat]("arg" + offset)
+        cg.mkVoidMethod2[Ref[Closure], MFloat]("setArg" + offset,
           sig =>
             sig.getArg1.LOAD[StackNil] |>>
               sig.getArg2.LOAD |>>
@@ -128,8 +152,8 @@ class Closure(closure : ClosureInfo)(implicit root: Root, flix : Flix) extends M
               RETURN_VOID
         )
       case PrimDouble =>
-        val field = cg.mkPrimField[MDouble]("arg" + ind)
-        cg.mkVoidMethod2[Ref[Closure], MDouble]("setArg" + ind,
+        val field = cg.mkPrimField[MDouble]("arg" + offset)
+        cg.mkVoidMethod2[Ref[Closure], MDouble]("setArg" + offset,
           sig =>
             sig.getArg1.LOAD[StackNil] |>>
               sig.getArg2.LOAD |>>
@@ -137,8 +161,8 @@ class Closure(closure : ClosureInfo)(implicit root: Root, flix : Flix) extends M
               RETURN_VOID
         )
       case Reference(_) =>
-        val field = cg.mkField[Ref[MObject]]("arg" + ind)
-        cg.mkVoidMethod2[Ref[Closure], Ref[MObject]]("setArg" + ind,
+        val field = cg.mkField[Ref[MObject]]("arg" + offset)
+        cg.mkVoidMethod2[Ref[Closure], Ref[MObject]]("setArg" + offset,
           sig =>
             sig.getArg1.LOAD[StackNil] |>>
               sig.getArg2.LOAD |>>
@@ -149,16 +173,36 @@ class Closure(closure : ClosureInfo)(implicit root: Root, flix : Flix) extends M
     }
   }
 
-  private def getArgField[T1 <: MnemonicsTypes : TypeTag](ind : Int) : Field[T1] =
-    new Field[T1]("arg" + ind)
+  /**
+    * Method which generates the capability to LOAD/STORE an argument field
+    *
+    * @param offset of the field we want to obtain
+    */
+  private def getArgField[T1 <: MnemonicsTypes : TypeTag](offset: Int): Field[T1] =
+    new Field[T1]("arg" + offset)
 
-  private def getArgPrimField[T1 <: MnemonicsPrimTypes : TypeTag](ind : Int) : PrimField[T1] =
-    new PrimField[T1]("arg" + ind)
+  /**
+    * Method which generates the capability to LOAD/STORE an argument field,
+    * since it is a prim field it also allows boxing
+    *
+    * @param offset of the field we want to obtain
+    */
+  private def getArgPrimField[T1 <: MnemonicsPrimTypes : TypeTag](offset: Int): PrimField[T1] =
+    new PrimField[T1]("arg" + offset)
 
-  def setArgMethod[T1 <: MnemonicsTypes : TypeTag](ind : Int) : VoidMethod2[Ref[Closure], T1] =
-    new VoidMethod2(JvmModifier.InvokeVirtual, ct,"setArg" + ind)
+  /**
+    * Method which generates the capability to invoke a setArg method,
+    *
+    * @param offset of the setArg method we want to invoke
+    */
+  def setArgMethod[T1 <: MnemonicsTypes : TypeTag](offset: Int): VoidMethod2[Ref[Closure], T1] =
+    new VoidMethod2(JvmModifier.InvokeVirtual, ct, "setArg" + offset)
 
-  returnType match {
+  //Generate the result field and the getResult method.
+  //Only one method and field are generated
+  retTpe match {
+    //Pattern match on each of the possible return type and generate the respective field and getResult method
+    //We need to pattern match as we want the field and the method signature to match the type.
     case PrimBool =>
       val field = cg.mkPrimField[MBool]("result")
       cg.mkMethod1[Ref[Closure], MBool]("getResult",
@@ -231,70 +275,113 @@ class Closure(closure : ClosureInfo)(implicit root: Root, flix : Flix) extends M
             field.GET_FIELD |>>
             RETURN
       )
-    case _ => throw InternalCompilerException(s"Unexpected type $returnType")
+    case _ => throw InternalCompilerException(s"Unexpected type $retTpe")
   }
 
+  /**
+    * Method which generates the capability to LOAD/STORE the result field
+    */
   private def getResultField[T1 <: MnemonicsTypes : TypeTag]: Field[T1] =
     new Field[T1]("result")
 
-  private def getResultPrimField[T1 <: MnemonicsPrimTypes : TypeTag] : PrimField[T1] =
+  /**
+    * Method which generates the capability to LOAD/STORE the result field,
+    * since it is a prim field it also allow boxing
+    */
+  private def getResultPrimField[T1 <: MnemonicsPrimTypes : TypeTag]: PrimField[T1] =
     new PrimField[T1]("result")
 
-  private def getResultMethod[T1 <: MnemonicsPrimTypes : TypeTag] : Method1[Ref[Closure], T1] =
-    new Method1(JvmModifier.InvokeVirtual,ct, "getResult")
+  /**
+    * Method which generates the capability to invoke a getResult method,
+    */
+  private def getResultMethod[T1 <: MnemonicsPrimTypes : TypeTag]: Method1[Ref[Closure], T1] =
+    new Method1(JvmModifier.InvokeVirtual, ct, "getResult")
 
-  val defaultConstructor : UncheckedVoidMethod = {
-    val setCloFields =
-      (sig : UncheckedFunSig)  =>{
-        var ins = NO_OP[StackNil]
-        var ind: Int = 2
-        for (arg <- cloFieldsTypes) {
-          ins = ins |>>
-            sig.getArg[Ref[Closure]](0).LOAD |>>
-            (arg match {
-              case PrimBool => sig.getArg[MBool](ind).LOAD[StackNil ** Ref[Closure]] |>>
-                getCloPrimField(ind).PUT_FIELD
-              case PrimChar =>  sig.getArg[MChar](ind).LOAD[StackNil ** Ref[Closure]] |>>
-                getCloPrimField(ind).PUT_FIELD
-              case PrimByte =>   sig.getArg[MByte](ind).LOAD[StackNil ** Ref[Closure]] |>>
-                getCloPrimField(ind).PUT_FIELD
-              case PrimShort => sig.getArg[MShort](ind).LOAD[StackNil ** Ref[Closure]] |>>
-                getCloPrimField(ind).PUT_FIELD
-              case PrimInt =>   sig.getArg[MInt](ind).LOAD[StackNil ** Ref[Closure]] |>>
-                getCloPrimField(ind).PUT_FIELD
-              case PrimLong =>   sig.getArg[MLong](ind).LOAD[StackNil ** Ref[Closure]] |>>
-                getCloPrimField(ind).PUT_FIELD
-              case PrimFloat =>  sig.getArg[MFloat](ind).LOAD[StackNil ** Ref[Closure]] |>>
-                getCloPrimField(ind).PUT_FIELD
-              case PrimDouble =>   sig.getArg[MDouble](ind).LOAD[StackNil ** Ref[Closure]] |>>
-                getCloPrimField(ind).PUT_FIELD
-              case Reference(_) =>   sig.getArg[Ref[MObject]](ind).LOAD[StackNil ** Ref[Closure]] |>>
-                getCloField(ind).PUT_FIELD
+  //Generate the class construtor
+  //This method is unchecked as we can't know at the compile time the number of
+  //arguments it takes. As it depends on the number of free variables the closure has.
+  val defaultConstructor: UncheckedVoidMethod = {
+    val setCloFreevars =
+      (sig: UncheckedFunSig) => {
+        //We need initialize each of the free vars in the closure
+        //Therefore we need to accumulate the instructions
+        //To do this we use a foldleft.
+        //Note we also acumulate on the offset this is because long and double take double the space
+        //in the stack
+        val (setCloFields, _) = cloFreevarsTpes.zipWithIndex.foldLeft((NO_OP[StackNil], 2)) {
+          case ((ins, offset), (arg, ind)) => {
+            arg match {
+              //We pattern on the arg type match as we want to match the free var type
+              case PrimBool =>
+                (ins |>>
+                  sig.getArg[Ref[Closure]](0).LOAD |>>
+                  sig.getArg[MBool](offset).LOAD[StackNil ** Ref[Closure]] |>>
+                  getCloPrimField(ind).PUT_FIELD, offset + 1)
+              case PrimChar =>
+                (ins |>>
+                  sig.getArg[Ref[Closure]](0).LOAD |>>
+                  sig.getArg[MChar](offset).LOAD[StackNil ** Ref[Closure]] |>>
+                  getCloPrimField(ind).PUT_FIELD, offset + 1)
+              case PrimByte =>
+                (ins |>>
+                  sig.getArg[Ref[Closure]](0).LOAD |>>
+                  sig.getArg[MByte](offset).LOAD[StackNil ** Ref[Closure]] |>>
+                  getCloPrimField(ind).PUT_FIELD, offset + 1)
+              case PrimShort =>
+                (ins |>>
+                  sig.getArg[Ref[Closure]](0).LOAD |>>
+                  sig.getArg[MShort](offset).LOAD[StackNil ** Ref[Closure]] |>>
+                  getCloPrimField(ind).PUT_FIELD, offset + 1)
+              case PrimInt =>
+                (ins |>>
+                  sig.getArg[Ref[Closure]](0).LOAD |>>
+                  sig.getArg[MInt](offset).LOAD[StackNil ** Ref[Closure]] |>>
+                  getCloPrimField(ind).PUT_FIELD, offset + 1)
+              case PrimLong =>
+                (ins |>>
+                  sig.getArg[Ref[Closure]](0).LOAD |>>
+                  sig.getArg[MLong](offset).LOAD[StackNil ** Ref[Closure]] |>>
+                  getCloPrimField(ind).PUT_FIELD, offset + 2)
+              case PrimFloat =>
+                (ins |>>
+                  sig.getArg[Ref[Closure]](0).LOAD |>>
+                  sig.getArg[MFloat](offset).LOAD[StackNil ** Ref[Closure]] |>>
+                  getCloPrimField(ind).PUT_FIELD, offset + 1)
+              case PrimDouble =>
+                (ins |>>
+                  sig.getArg[Ref[Closure]](0).LOAD |>>
+                  sig.getArg[MDouble](offset).LOAD[StackNil ** Ref[Closure]] |>>
+                  getCloPrimField(ind).PUT_FIELD, offset + 2)
+              case Reference(_) =>
+                (ins |>>
+                  sig.getArg[Ref[Closure]](0).LOAD |>>
+                  sig.getArg[Ref[MObject]](offset).LOAD[StackNil ** Ref[Closure]] |>>
+                  getCloField(ind).PUT_FIELD, offset + 1)
               case _ => throw InternalCompilerException(s"Unexpected type $arg")
-            })
-          arg match {
-            case PrimLong | PrimDouble => ind += 2
-            case _ => ind += 1
+            }
           }
         }
-        ins
+        setCloFields
       }
 
-    cg.mkUncheckedConstructor(ct +: cloFieldsTypes,
+    //Create an unchecked construstor, specify the argument list. The first agurment is the class type
+    //As this is in scope, however this is not present in the final signature/
+    cg.mkUncheckedConstructor(ct +: NJvmType.Object +: cloFreevarsTpes,
       sig =>
-        sig.getArg(0).asInstanceOf[Local[Ref[Closure]]].LOAD[StackNil] |>>
+        sig.getArg[Ref[Closure]](0).LOAD[StackNil] |>>
           cg.SUPER |>>
-          sig.getArg(0).asInstanceOf[Local[Ref[Closure]]].LOAD |>>
-          sig.getArg(1).asInstanceOf[Local[Ref[MObject]]].LOAD |>>
+          sig.getArg[Ref[Closure]](0).LOAD |>>
+          sig.getArg[Ref[MObject]](1).LOAD |>>
           creationContext.PUT_FIELD |>>
-          setCloFields(sig) |>>
+          setCloFreevars(sig) |>>
           RETURN_VOID
     )
   }
   /**
     * Variable which generates the JvmClass (contains the class bytecode)
     */
-  private val jvmClass: JvmClass = JvmClass(ct.name, cg.compile())
+  private val jvmClass: JvmClass =
+    JvmClass(ct.name, cg.compile())
 
   def getJvmClass: JvmClass = jvmClass
 
