b6f3fe71c76772ce31c52f0f2bb9c400c2ed08b2
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2019-10-31 09:13:48 +0100

    Fix #903 (#904)
    Fixed #903

--- main/src/ca/uwaterloo/flix/language/ast/SimplifiedAst.scala
+++ main/src/ca/uwaterloo/flix/language/ast/SimplifiedAst.scala
@@ -304,7 +304,7 @@ object SimplifiedAst {
 
       case class Lit(lit: SimplifiedAst.Expression, tpe: Type, loc: SourceLocation) extends SimplifiedAst.Term.Head
 
-      case class App(sym: Symbol.DefnSym, args: List[Symbol.VarSym], tpe: Type, loc: SourceLocation) extends SimplifiedAst.Term.Head
+      case class App(exp: SimplifiedAst.Expression, args: List[Symbol.VarSym], tpe: Type, loc: SourceLocation) extends SimplifiedAst.Term.Head
 
     }
 
