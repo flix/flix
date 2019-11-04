7c35ee19f263788bf4aa5a24b9f91eceb29359db
frederikarpfrandsen <fredearp@gmail.com>
2019-11-04 21:50:07 +0100

    re-adding array case

--- main/src/ca/uwaterloo/flix/language/phase/Namer.scala
+++ main/src/ca/uwaterloo/flix/language/phase/Namer.scala
@@ -1041,6 +1041,7 @@ object Namer extends Phase[WeededAst.Program, NamedAst.Root] {
       case WeededAst.Pattern.Str(lit, loc) => NamedAst.Pattern.Str(lit, loc)
       case WeededAst.Pattern.Tag(enum, tag, pat, loc) => NamedAst.Pattern.Tag(enum, tag, visit(pat), Type.freshTypeVar(), loc)
       case WeededAst.Pattern.Tuple(elms, loc) => NamedAst.Pattern.Tuple(elms map visit, Type.freshTypeVar(), loc)
+      case WeededAst.Pattern.Array(elms, loc) => NamedAst.Pattern.Array(elms map visit, Type.freshTypeVar(), loc)
       case WeededAst.Pattern.ArrayTailSpread(elms, ident, loc) => ident match {
         case None => NamedAst.Pattern.ArrayTailSpread(elms map visit, None, Type.freshTypeVar(),loc)
         case Some(value) =>
