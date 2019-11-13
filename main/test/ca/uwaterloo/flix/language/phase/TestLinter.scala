8096ff17ad1c00628c1f8b09363f1cb9ab3dad32
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2019-11-13 17:08:31 +0100

    Remove useless tests.

--- main/test/ca/uwaterloo/flix/language/phase/TestLinter.scala
+++ main/test/ca/uwaterloo/flix/language/phase/TestLinter.scala
@@ -1,23 +1,12 @@
 package ca.uwaterloo.flix.language.phase
 
 import ca.uwaterloo.flix.TestUtils
-import ca.uwaterloo.flix.language.errors.LinterError
 import ca.uwaterloo.flix.util.Options
 import org.scalatest.FunSuite
 
 class TestLinter extends FunSuite with TestUtils {
 
   val DefaultOptions: Options = Options.DefaultTest.copy(core = true)
-
-  ignore("TrivialExpression.LeftAdditionByZero") {
-    val input =
-      """
-        |pub def f(): Int = 0 + 123
-        |
-      """.stripMargin
-    val result = compile(input, DefaultOptions)
-    expectError[LinterError.TrivialExpression](result)
-  }
 
   // TODO: Add tests
 
