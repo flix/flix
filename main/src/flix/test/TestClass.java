6bb327c53591608c603405decf15516652c2a60e
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2020-01-22 19:44:27 +0100

    WIP

--- main/src/flix/test/TestClass.java
+++ main/src/flix/test/TestClass.java
@@ -4,8 +4,63 @@ public class TestClass {
 
     // TODO: All types
 
+
     public static boolean BOOLEAN_FIELD;
+    public static char CHAR_FIELD;
+    public static float FLOAT32_FIELD;
+    public static double FLOAT64_FIELD;
     public static String STRING_FIELD;
+
+
+//    /**
+//     * A type constructor that represent the type of 64-bit floating point numbers.
+//     */
+//  case object Float64 extends TypeConstructor {
+//        def kind: Kind = Kind.Star
+//    }
+//
+//    /**
+//     * A type constructor that represent the type of 8-bit integers.
+//     */
+//  case object Int8 extends TypeConstructor {
+//        def kind: Kind = Kind.Star
+//    }
+//
+//    /**
+//     * A type constructor that represent the type of 16-bit integers.
+//     */
+//  case object Int16 extends TypeConstructor {
+//        def kind: Kind = Kind.Star
+//    }
+//
+//    /**
+//     * A type constructor that represent the type of 32-bit integers.
+//     */
+//  case object Int32 extends TypeConstructor {
+//        def kind: Kind = Kind.Star
+//    }
+//
+//    /**
+//     * A type constructor that represent the type of 64-bit integers.
+//     */
+//  case object Int64 extends TypeConstructor {
+//        def kind: Kind = Kind.Star
+//    }
+//
+//    /**
+//     * A type constructor that represent the type of arbitrary-precision integers.
+//     */
+//  case object BigInt extends TypeConstructor {
+//        def kind: Kind = Kind.Star
+//    }
+//
+//    /**
+//     * A type constructor that represent the type of strings.
+//     */
+//  case object Str extends TypeConstructor {
+//        def kind: Kind = Kind.Star
+//    }
+
 
     public boolean booleanField;
     public String stringField;
