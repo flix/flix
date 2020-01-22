8a3deb42302fd8a1ccb6c07f51823c8e2d4f354d
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2020-01-22 21:44:25 +0100

    WIP

--- main/src/flix/test/TestClass.java
+++ main/src/flix/test/TestClass.java
@@ -1,59 +1,23 @@
 package flix.test;
 
+import java.math.BigInteger;
+
 public class TestClass {
 
-    // TODO: All types
+    //
+    // Static Fields.
+    //
+    public static boolean    BOOL_FIELD = true;
+    public static char       CHAR_FIELD = 'A';
+    public static float      FLOAT32_FIELD = 123.456f;
+    public static double     FLOAT64_FIELD = 123.456;
+    public static byte       INT8_FIELD = 127;
+    public static short      INT16_FIELD = 127;
+    public static int        INT32_FIELD = 127;
+    public static long       INT64_FIELD = 127;
+    public static BigInteger BIGINT_FIELD = BigInteger.valueOf(123L);
+    public static String     STRING_FIELD = "Hello World";
 
-
-    public static boolean   BOOL_FIELD = true;
-    public static char      CHAR_FIELD = 'A';
-    public static float     FLOAT32_FIELD = 123.456f;
-    public static double    FLOAT64_FIELD = 123.456;
-    public static byte      INT8_FIELD = 127;
-    public static String    STRING_FIELD = "Hello World";
-
-
-//    /**
-//     * A type constructor that represent the type of 8-bit integers.
-//     */
-//  case object Int8 extends TypeConstructor {
-//        def kind: Kind = Kind.Star
-//    }
-//
-//    /**
-//     * A type constructor that represent the type of 16-bit integers.
-//     */
-//  case object Int16 extends TypeConstructor {
-//        def kind: Kind = Kind.Star
-//    }
-//
-//    /**
-//     * A type constructor that represent the type of 32-bit integers.
-//     */
-//  case object Int32 extends TypeConstructor {
-//        def kind: Kind = Kind.Star
-//    }
-//
-//    /**
-//     * A type constructor that represent the type of 64-bit integers.
-//     */
-//  case object Int64 extends TypeConstructor {
-//        def kind: Kind = Kind.Star
-//    }
-//
-//    /**
-//     * A type constructor that represent the type of arbitrary-precision integers.
-//     */
-//  case object BigInt extends TypeConstructor {
-//        def kind: Kind = Kind.Star
-//    }
-//
-//    /**
-//     * A type constructor that represent the type of strings.
-//     */
-//  case object Str extends TypeConstructor {
-//        def kind: Kind = Kind.Star
-//    }
 
 
     public boolean booleanField;
