d54498f8931da15ef88fa438247659180621075f
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2020-01-23 12:20:32 +0100

    WIP

--- main/src/flix/test/TestClass.java
+++ main/src/flix/test/TestClass.java
@@ -1,31 +1,31 @@
 package flix.test;
 
-import java.math.BigInteger;
-
 public class TestClass {
 
     //
     // Static Fields.
     //
-    public static boolean    BOOL_FIELD = true;
-    public static char       CHAR_FIELD = 'A';
-    public static float      FLOAT32_FIELD = 123.456f;
-    public static double     FLOAT64_FIELD = 123.456;
-    public static byte       INT8_FIELD = 127;
-    public static short      INT16_FIELD = 127;
-    public static int        INT32_FIELD = 127;
-    public static long       INT64_FIELD = 127;
-    public static String     STRING_FIELD = "Hello World";
+    public static boolean   BOOL_FIELD = true;
+    public static char      CHAR_FIELD = 'A';
+    public static float     FLOAT32_FIELD = 123.456f;
+    public static double    FLOAT64_FIELD = 123.456;
+    public static byte      INT8_FIELD = 127;
+    public static short     INT16_FIELD = 127;
+    public static int       INT32_FIELD = 127;
+    public static long      INT64_FIELD = 127;
+    public static String    STRING_FIELD = "Hello World";
 
     //
     // Instance Fields.
     //
-
-    public boolean      boolField;
-    public char         charField;
-    public float        float32Field;
-    public double       float64Field;
-    public byte         int8Field;
-    public String stringField;
+    public boolean          boolField       = true;
+    public char             charField       = 'A';
+    public float            float32Field    = 123.456f;
+    public double           float64Field    = 123.456;
+    public byte             int8Field       = 127;
+    public short            int16Field      = 127;
+    public int              int32Field      = 127;
+    public long             int64Field      = 127;
+    public String           stringField     = "Hello World";
 
 }
