64bf0d647ad813fe24aa0c1a8b0a8ed7b824d6b3
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2020-02-11 09:12:15 +0100

    WIP

--- main/test/ca/uwaterloo/flix/language/feature/FeatureSuite.scala
+++ main/test/ca/uwaterloo/flix/language/feature/FeatureSuite.scala
@@ -25,7 +25,6 @@ class FeatureSuite extends Suites(
   new FlixTest("Test.Expression.PutChannel", "main/test/ca/uwaterloo/flix/language/feature/Test.Expression.PutChannel.flix"),
   new FlixTest("Test.Expression.GetChannel", "main/test/ca/uwaterloo/flix/language/feature/Test.Expression.GetChannel.flix"),
   new FlixTest("Test.Expression.SelectChannel", "main/test/ca/uwaterloo/flix/language/feature/Test.Expression.SelectChannel.flix"),
-  new FlixTest("Test.Expression.Spawn", "main/test/ca/uwaterloo/flix/language/feature/Test.Expression.Spawn.flix"),
   new FlixTest("Test.Expression.Stm", "main/test/ca/uwaterloo/flix/language/feature/Test.Expression.Stm.flix"),
 
   new FlixTest("TestCurrying", "main/test/ca/uwaterloo/flix/language/feature/TestCurrying.flix"),
