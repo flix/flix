e11df09ab449a895f5574fc2dbf63ac0d3a086b9
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2020-02-11 10:47:20 +0100

    WIP

--- main/test/ca/uwaterloo/flix/language/feature/FeatureSuite.scala
+++ main/test/ca/uwaterloo/flix/language/feature/FeatureSuite.scala
@@ -22,7 +22,6 @@ import org.scalatest.{ParallelTestExecution, Suites}
 // TODO: Sort
 class FeatureSuite extends Suites(
   new FlixTest("Test.Expression.PutChannel", "main/test/ca/uwaterloo/flix/language/feature/Test.Expression.PutChannel.flix"),
-  new FlixTest("Test.Expression.GetChannel", "main/test/ca/uwaterloo/flix/language/feature/Test.Expression.GetChannel.flix"),
   new FlixTest("Test.Expression.SelectChannel", "main/test/ca/uwaterloo/flix/language/feature/Test.Expression.SelectChannel.flix"),
   new FlixTest("Test.Expression.Stm", "main/test/ca/uwaterloo/flix/language/feature/Test.Expression.Stm.flix"),
 
