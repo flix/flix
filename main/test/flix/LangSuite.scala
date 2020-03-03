90d3a49c0b64925cdc4d02376f355a19e97c420b
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2020-03-03 09:24:31 +0100

    Added test cases.

--- main/test/flix/LangSuite.scala
+++ main/test/flix/LangSuite.scala
@@ -39,6 +39,11 @@ class LangSuite extends Suites(
   //
   new FlixTest("Test.Exp.Cast", "main/test/flix/Test.Exp.Cast.flix"),
 
+  //
+  // Compare.
+  //
+  new FlixTest("Test.Exp.Compare.Poly", "main/test/flix/Test.Exp.Compare.Poly.flix"),
+
   ///
   /// Concurrency.
   ///
