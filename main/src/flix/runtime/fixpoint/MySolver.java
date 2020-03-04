96aababe18ac1d23285d806fa6f62b4858841a82
racerand <andreas_heglingegaard@hotmail.com>
2020-03-04 13:08:45 +0100

    I  have written the eval function and trying to work on an evalRule function which has a constraint and a map from PredSym to TableName, to make sure that the correct kind of table is used

--- main/src/flix/runtime/fixpoint/MySolver.java
+++ main/src/flix/runtime/fixpoint/MySolver.java
@@ -3,6 +3,7 @@ package flix.runtime.fixpoint;
 import flix.runtime.ProxyObject;
 import flix.runtime.fixpoint.predicate.AtomPredicate;
 import flix.runtime.fixpoint.predicate.Predicate;
+import flix.runtime.fixpoint.ram.LocalVariable;
 import flix.runtime.fixpoint.ram.RamTerm;
 import flix.runtime.fixpoint.ram.TableClassifier;
 import flix.runtime.fixpoint.ram.TableName;
@@ -13,50 +14,94 @@ import flix.runtime.fixpoint.symbol.PredSym;
 import flix.runtime.fixpoint.symbol.RelSym;
 import flix.runtime.fixpoint.term.LitTerm;
 import flix.runtime.fixpoint.term.Term;
-import javafx.util.Pair;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Stream;
 
 public class MySolver {
     public static void solve(ConstraintSystem cs, Stratification stf, Options o) {
-        Stmt[] factProjections = generateFactProjectionStmts(cs);
+        ArrayList<RelSym> relHasFact = new ArrayList<>();
+        Stmt[] factProjections = generateFactProjectionStmts(cs, relHasFact);
 
         Map<RelSym, ArrayList<Constraint>> derived = findRulesForDerived(cs);
         RelSym[] relSyms = cs.getRelationSymbols();
-        ArrayList<RelSym> notDerived = new ArrayList<>();
-        for (RelSym relSym : relSyms) {
-            // We check if the RelSym is derived by a rule, if not, we remember that
-            if (!derived.containsKey(relSym)){
-                notDerived.add(relSym);
+        // For representing all the RelSym that have rules but also initial facts
+        ArrayList<RelSym> derivedButHasFacts = new ArrayList<>();
+        // For representing the RelSym for tables that only have facts
+        ArrayList<RelSym> factRelSyms = new ArrayList<>();
+        for (RelSym relSym : relHasFact) {
+            // We check if the RelSym that has a fact is also derived, and remember bot if and not
+            if (derived.containsKey(relSym)) {
+                derivedButHasFacts.add(relSym);
+            } else {
+                factRelSyms.add(relSym);
             }
         }
-        for (RelSym relSym : derived.keySet()){
+        for (RelSym relSym : derived.keySet()) {
             Stmt[] ramRule = eval(relSym, derived);
         }
         SeqStmt seqStmt = new SeqStmt(factProjections);
         seqStmt.prettyPrint(System.out, 0);
     }
-/*
-    Vi har brug for at vi kan have en tuble som type datatype som hvor vi kan spørge om  eksistens i en tabel og indsætte i en tabel
-    Hvad nu hvis der optræder en literal (konstant) i Head eller body af en regel
- */
+
+    /*
+        Vi har brug for at vi kan have en tuble som type datatype som hvor vi kan spørge om  eksistens i en tabel og indsætte i en tabel
+        Hvad nu hvis der optræder en literal (konstant) i Head eller body af en regel
+     */
     private static Stmt[] eval(RelSym relSym, Map<RelSym, ArrayList<Constraint>> derived) {
-        for(Constraint c : derived.get(relSym)){
-            Predicate head = c.getHeadPredicate();
-            assert head instanceof AtomPredicate;
-            Term[] headTerms = ((AtomPredicate) head).getTerms();
+        // Generate facts for each rule for the fact
+        Stmt[][] acc = new Stmt[derived.get(relSym).size()][];
+        ArrayList<Constraint> get = derived.get(relSym);
+        for (int i = 0; i < get.size(); i++) {
+            Constraint c = get.get(i);
+            Map<PredSym, TableName> relTableMap = new HashMap<>();
+            for (Predicate pred : c.getBodyPredicates()) {
+                if (pred instanceof AtomPredicate) {
+                    PredSym sym = ((AtomPredicate) pred).getSym();
+                    // For all relation symbols we make a map to the TableName
+                    if (sym instanceof RelSym) {
+                        // Since we want to use all the facts, generated, we don't care if the facts are derived or not
+                        relTableMap.put(sym, new TableName(TableClassifier.RESULT, sym));
+                    } else {
+                        throw new UnsupportedOperationException("We do not support LatSym yet");
+                    }
+                } else {
+                    throw new UnsupportedOperationException("We only support AtomPredicates for now");
+                }
+            }
+            // Evaluate each rule individually
+            acc[i] = evalRule(c, relTableMap);
+        }
+        return Stream.of(acc).flatMap(Stream::of).toArray(Stmt[]::new);
+    }
 
-            ArrayList<Pair<Term, Term>> pairs = new ArrayList<>();
-
+    private static Stmt[] evalRule(Constraint c, Map<PredSym, TableName> relTableMap) {
+        Predicate head = c.getHeadPredicate();
+        assert head instanceof AtomPredicate;
+        PredSym headSym = ((AtomPredicate) head).getSym();
+        Term[] headTerms = ((AtomPredicate) head).getTerms();
+        // Map from AtomPredicate to the localvar used to get values to that recursion
+        Map<AtomPredicate, LocalVariable> atomToLocal = new HashMap<>();
+        for (Predicate bodyPred : c.getBodyPredicates()) {
+            if (bodyPred instanceof AtomPredicate) {
+                LocalVariable localVar = genNewLocalVariable(((AtomPredicate) bodyPred).getSym().getName());
+                atomToLocal.put((AtomPredicate) bodyPred, localVar);
+            } else {
+                throw new UnsupportedOperationException("We only support AtomPredicates");
+            }
         }
         return new Stmt[0];
     }
 
+    private static LocalVariable genNewLocalVariable(String name) {
+        throw new UnsupportedOperationException("Make actual new variables");
+    }
+
     private static Map<RelSym, ArrayList<Constraint>> findRulesForDerived(ConstraintSystem cs) {
         Map<RelSym, ArrayList<Constraint>> result = new HashMap<>();
-        for (Constraint c : cs.getRules()){
+        for (Constraint c : cs.getRules()) {
             Predicate hPred = c.getHeadPredicate();
             assert hPred instanceof AtomPredicate;
 
@@ -80,7 +125,7 @@ public class MySolver {
      * @param cs Is the constraint system
      * @return an array of ProjectStmt representing the projection of the facts
      */
-    private static Stmt[] generateFactProjectionStmts(ConstraintSystem cs) {
+    private static Stmt[] generateFactProjectionStmts(ConstraintSystem cs, ArrayList<RelSym> predHasFacts) {
         Stmt[] stmts = new Stmt[cs.getFacts().length];
         for (int factI = 0; factI < cs.getFacts().length; factI++) {
             Constraint c = cs.getFacts()[factI];
@@ -90,6 +135,9 @@ public class MySolver {
             assert pred instanceof AtomPredicate;
 
             AtomPredicate atom = (AtomPredicate) pred;
+            PredSym predSym = atom.getSym();
+            assert predSym instanceof RelSym;
+            predHasFacts.add((RelSym) predSym);
             RamTerm[] ramTerms = new RamTerm[atom.getTerms().length];
             for (int termI = 0; termI < atom.getTerms().length; termI++) {
                 Term term = atom.getTerms()[termI];
