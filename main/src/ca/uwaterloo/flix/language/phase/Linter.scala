98c310d9efe89c35e8114196b44c351e00ad0fb4
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2020-03-07 15:18:38 +0100

    WIP

--- main/src/ca/uwaterloo/flix/language/phase/Linter.scala
+++ main/src/ca/uwaterloo/flix/language/phase/Linter.scala
@@ -17,16 +17,38 @@ package ca.uwaterloo.flix.language.phase
 
 import ca.uwaterloo.flix.api.Flix
 import ca.uwaterloo.flix.language.ast.TypedAst._
-import ca.uwaterloo.flix.language.ast.{BinaryOperator, Symbol, TypedAst}
+import ca.uwaterloo.flix.language.ast.{BinaryOperator, SourceLocation, Symbol, TypedAst}
 import ca.uwaterloo.flix.language.debug.FormatExpression
 import ca.uwaterloo.flix.language.errors.LinterError
-import ca.uwaterloo.flix.util.Validation
+import ca.uwaterloo.flix.util.{ParOps, Result, Validation}
 import ca.uwaterloo.flix.util.Validation._
 
 object Linter extends Phase[TypedAst.Root, TypedAst.Root] {
 
-  def run(root: TypedAst.Root)(implicit flix: Flix): Validation[TypedAst.Root, LinterError] = flix.phase("Linter") {
-    // TODO: Implement the linter.
-    root.toSuccess
+  def run(root: Root)(implicit flix: Flix): Validation[Root, LinterError] = flix.phase("Linter") {
+    // Finds all lints the in the ast.
+    val lints = lintsOf(root)
+
+    // Every definition in the program.
+    val defs = root.defs.values.toList
+
+    // Visit every definition in parallel.
+    val results = ParOps.parMap(visitDef(_, lints), defs)
+
+    // Collect all the results.
+    results.flatten match {
+      case Nil => root.toSuccess
+      case xs => Failure(LazyList.from(xs))
+    }
   }
+
+  private def visitDef(defn: Def, lints: List[Lint]): List[LinterError] =
+    LinterError.TrivialExpression(SourceLocation.Unknown) :: Nil
+
+  private def lintsOf(root: Root): List[Lint] = root.defs.foldLeft(Nil: List[Lint]) {
+    case (acc, (sym, defn)) => if (defn.ann.isLint) Lint(defn.exp) :: acc else acc
+  }
+
+  case class Lint(exp: Expression)
+
 }
