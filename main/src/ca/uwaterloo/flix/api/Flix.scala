8e54a9980b6d568ba2ee4196fbb8eb9483a1ec9c
Esben Bjerre <hej@esbenbjerre.dk>
2020-03-28 11:53:31 +0100

    Use namespace to rename RBTree -> RedBlackTree

--- main/src/ca/uwaterloo/flix/api/Flix.scala
+++ main/src/ca/uwaterloo/flix/api/Flix.scala
@@ -104,7 +104,7 @@ class Flix {
 
     "StringBuilder.flix" -> LocalResource.get("/library/StringBuilder.flix"),
 
-    "RBTree.flix" -> LocalResource.get("/library/RBTree.flix"),
+    "RedBlackTree.flix" -> LocalResource.get("/library/RedBlackTree.flix"),
 
     //"CodePatterns.flix" -> LocalResource.get("/library/CodePatterns.flix"),
     // "Vec.flix" -> LocalResource.get("/library/Vec.flix"),
