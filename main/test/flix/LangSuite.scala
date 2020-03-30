52c5d05c769db475b19c53227f84c002c94c5986
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2020-03-30 11:47:29 +0200

    WIP

--- main/test/flix/LangSuite.scala
+++ main/test/flix/LangSuite.scala
@@ -98,6 +98,7 @@ class LangSuite extends Suites(
   new FlixTest("Test.Term.Lit.Option", "main/test/flix/Test.Term.Lit.List.flix"),
   new FlixTest("Test.Term.Lit.Option", "main/test/flix/Test.Term.Lit.Option.flix"),
   new FlixTest("Test.Term.Lit.Result", "main/test/flix/Test.Term.Lit.Result.flix"),
+  new FlixTest("Test.Term.Lit.Set", "main/test/flix/Test.Term.Lit.Set.flix")(Options.TestWithLibrary),
 
   //
   // Use.
