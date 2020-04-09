c06b468a111a7e7bcdf1e1d11b3f68d309314a63
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2020-04-09 21:42:54 +0200

    WIP

--- main/src/ca/uwaterloo/flix/tools/lsp/Index.scala
+++ main/src/ca/uwaterloo/flix/tools/lsp/Index.scala
@@ -6,7 +6,14 @@ object Index {
   def of(e: Expression): Index = ???
 }
 
-trait Index {
+class Index(m: Map[Int, List[(Int, Expression)]]) {
+
+  def query(line: Int, column: Int): Option[Expression] = {
+    val candidates = m.getOrElse(line, Nil).filter(p => p._1 >= column)
+    // TODO: Compute some notion of span. candidates.sortBy()
+    None
+  }
+
   def ++(that: Index): Index = ???
 
   def +(e: Expression): Index = ???
