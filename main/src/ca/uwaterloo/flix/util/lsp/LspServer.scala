7db53aa155e523b9bdf1b55f5f67cf30d3349bc0
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2020-04-09 20:58:41 +0200

    WIP

--- main/src/ca/uwaterloo/flix/util/lsp/LspServer.scala
+++ main/src/ca/uwaterloo/flix/util/lsp/LspServer.scala
@@ -1,7 +1,31 @@
 package ca.uwaterloo.flix.util.lsp
 
+import ca.uwaterloo.flix.language.ast.TypedAst.{Expression, Root}
+import ca.uwaterloo.flix.language.ast.{SourceLocation, Symbol, Type}
+
+import scala.collection.mutable
+
 class LspServer {
 
+  case class Location()
 
+  def toLoc(loc: SourceLocation): Location = ???
+
+  class Index(root: Root) {
+
+    private val m = mutable.Map.empty[Location, Expression]
+
+    def visitExp(exp0: Expression): Unit = exp0 match {
+      case Expression.Unit(loc) => m += toLoc(loc) -> exp0
+    }
+
+    def getSym(loc: Location): Option[Symbol.DefnSym] = ???
+
+    def getTyp(loc: Location): Option[Type] = ???
+
+    def getExp(loc: Location): Option[Expression] = ???
+
+
+  }
 
 }
