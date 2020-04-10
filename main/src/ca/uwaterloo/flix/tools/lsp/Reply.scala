acca1e397d1e498b22e5dc3dce4fd4b0e7f92d38
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2020-04-10 15:59:09 +0200

    WIP

--- main/src/ca/uwaterloo/flix/tools/lsp/Reply.scala
+++ main/src/ca/uwaterloo/flix/tools/lsp/Reply.scala
@@ -1,19 +1,27 @@
 package ca.uwaterloo.flix.tools.lsp
 
-import org.json4s.JValue
 import org.json4s.JsonAST.{JField, JObject, JString}
 
+/**
+  * A common super-type for language server replies.
+  */
 trait Reply {
+  /**
+    * Returns `this` reply as a JSON object.
+    */
   def toJSON: JObject
 }
 
 object Reply {
 
+  /**
+    * A reply that represents that the specified entity was not found.
+    */
   case class NotFound() extends Reply {
     def toJSON: JObject = {
       JObject(
-        JField("status", JString("success")),
-        JField("result", JString("unknown"))
+        JField("status", JString("failure")),
+        JField("result", JString("Not Found"))
       )
     }
   }
