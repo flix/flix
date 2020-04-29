fa47b7b33fb05ef5c0f6646320427278d4486c59
Rasmus Larsen <doctorherbstein@gmail.com>
2020-04-29 14:05:09 +0200

    Fix typo in LibrarySuite

--- main/test/ca/uwaterloo/flix/library/LibrarySuite.scala
+++ main/test/ca/uwaterloo/flix/library/LibrarySuite.scala
@@ -47,5 +47,5 @@ class LibrarySuite extends Suites(
   new FlixTest("Core/Io/TestInputStream", "main/test/ca/uwaterloo/flix/library/Core/Io/TestInputStream.flix")(Options.TestWithLibrary),
   new FlixTest("Core/Io/TestOutputStream", "main/test/ca/uwaterloo/flix/library/Core/Io/TestOutputStream.flix")(Options.TestWithLibrary),
 
-  new FlixTest("Pkger/SemVer", "main/test/ca/uwaterloo/flix/library/Pkger/SemVer.flix")(Options.TestWithLibrary)
+  new FlixTest("Pkger/TestSemVer", "main/test/ca/uwaterloo/flix/library/Pkger/TestSemVer.flix")(Options.TestWithLibrary)
 )
