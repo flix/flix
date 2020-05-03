99160f350b6dee4f1f848912df592468d7c64095
Matt Lutze <mcdlutze@gmail.com>
2020-05-02 21:51:13 -0700

    add more type formatting

--- main/src/ca/uwaterloo/flix/language/debug/FormatType2.scala
+++ main/src/ca/uwaterloo/flix/language/debug/FormatType2.scala
@@ -2,59 +2,180 @@ package ca.uwaterloo.flix.language.debug
 
 import ca.uwaterloo.flix.language.ast.Kind.Effect
 import ca.uwaterloo.flix.language.ast.{Type, TypeConstructor}
+import ca.uwaterloo.flix.util.InternalCompilerException
 
 object FormatType2 { // MATT rename and replace FormatType
 
   // MATT this may be a very silly idea
-  sealed trait Readability
-  object Readability {
-    case object Internal extends Readability
-    case object External extends Readability
+  sealed trait Audience
+
+  object Audience {
+
+    case object Internal extends Audience
+
+    case object External extends Audience
+
   }
 
-  def format(tpe: Type)(implicit readability: Readability): String = tpe match {
-    case tvar@Type.Var(id, kind, _) => readability match {
-      case Readability.Internal => kind match {
-        case Effect => s"''$id"
-        case _ => s"'$id" // MATT probably want the text here too
-      }
-      case Readability.External => tvar.getText.getOrElse("<internal variable>") // MATT need a better representation?
-    }
+  def format(tpe: Type)(implicit audience: Audience): String = {
 
-    case Type.Cst(TypeConstructor.Unit) => "Unit"
-    case Type.Cst(TypeConstructor.Bool) => "Bool"
-    case Type.Cst(TypeConstructor.Char) => "Char"
-    case Type.Cst(TypeConstructor.Float32) => "Float32"
-    case Type.Cst(TypeConstructor.Float64) => "Float64"
-    case Type.Cst(TypeConstructor.Int8) => "Int8"
-    case Type.Cst(TypeConstructor.Int16) => "Int16"
-    case Type.Cst(TypeConstructor.Int32) => "Int32"
-    case Type.Cst(TypeConstructor.Int64) => "Int64"
-    case Type.Cst(TypeConstructor.BigInt) => "BigInt"
-    case Type.Cst(TypeConstructor.Str) => "Str"
-    case Type.Cst(TypeConstructor.RecordEmpty) => "{ }"
-    case Type.Cst(TypeConstructor.SchemaEmpty) => "#{ }"
-    case Type.Cst(TypeConstructor.Pure) => "Pure"
-    case Type.Cst(TypeConstructor.Impure) => "Impure"
+    val renameMap = alphaRenameVars(tpe)
 
-    case record@Type.Apply(Type.Apply(Type.Cst(TypeConstructor.RecordExtend(_)), _), _) => flattenRecord(record) match {
+    def formatWellFormedRecord(record: Type): String = flattenRecord(record) match {
       case FlatThing(fields, Type.Cst(TypeConstructor.RecordEmpty)) =>
-        fields.map { case (label, tpe) => s"$label: ${format(tpe)}" }.mkString("{ ", ", ", " }")
+        fields.map { case (label, tpe) => formatRecordField(label, tpe) }.mkString("{ ", ", ", " }")
       case FlatThing(fields, rest) =>
-        val fieldString = fields.map { case (label, tpe) => s"$label: ${format(tpe)}" }.mkString(", ")
-        s"{ $fieldString | ${format(rest)} }"
+        val fieldString = fields.map { case (label, tpe) => formatRecordField(label, tpe) }.mkString(", ")
+        s"{ $fieldString | ${visit(rest)} }"
     }
 
-      // MATT this one is wrong: needs to be like #{ A(Int32, Str), B(Str) }
-    case schema@Type.Apply(Type.Apply(Type.Cst(TypeConstructor.SchemaExtend(_)), _), _) => flattenSchema(schema) match {
+    def formatWellFormedSchema(schema: Type): String = flattenSchema(schema) match {
       case FlatThing(fields, Type.Cst(TypeConstructor.SchemaEmpty)) =>
-        fields.map { case (label, tpe) => s"$label: ${format(tpe)}" }.mkString("#{ ", ", ", " }")
+        fields.map { case (label, tpe) => formatSchemaField(label, tpe) }.mkString("#{ ", ", ", " }")
       case FlatThing(fields, rest) =>
-        val fieldString = fields.map { case (label, tpe) => s"$label: ${format(tpe)}" }.mkString(", ")
-        s"#{ $fieldString | ${format(rest)} }"
+        val fieldString = fields.map { case (label, tpe) => formatSchemaField(label, tpe) }.mkString(", ")
+        s"#{ $fieldString | ${visit(rest)} }"
     }
 
-    case _ => ???
+    def formatRecordField(label: String, tpe: Type): String = {
+      s"$label: ${visit(tpe)}"
+    }
+
+    def formatSchemaField(name: String, tpe: Type): String = {
+      tpe.typeConstructor match {
+        case Type.Cst(TypeConstructor.Unit) => s"$name()"
+        case Type.Cst(TypeConstructor.Tuple(_)) => s"$name${visit(tpe)}"
+        case _ => s"$name(${visit(tpe)})"
+      }
+    }
+
+    def formatApply(name: String, args: List[Type]): String = {
+      if (args.isEmpty)
+        name
+      else
+        name + "[" + args.map(visit).mkString(", ") + "]"
+    }
+
+
+    def visit(tpe: Type)(implicit audience: Audience): String = {
+      val base = tpe.typeConstructor
+      val args = tpe.typeArguments
+
+      base match {
+        case tvar@Type.Var(id, kind, _) => audience match {
+          case Audience.Internal => kind match {
+            case Effect => s"''$id"
+            case _ => s"'$id" // MATT probably want the text here too
+          }
+          case Audience.External => tvar.getText.getOrElse(renameMap(tvar.id))
+        }
+
+        case Type.Cst(TypeConstructor.Unit) => formatApply("Unit", args)
+        case Type.Cst(TypeConstructor.Bool) => formatApply("Bool", args)
+        case Type.Cst(TypeConstructor.Char) => formatApply("Char", args)
+        case Type.Cst(TypeConstructor.Float32) => formatApply("Float32", args)
+        case Type.Cst(TypeConstructor.Float64) => formatApply("Float64", args)
+        case Type.Cst(TypeConstructor.Int8) => formatApply("Int8", args)
+        case Type.Cst(TypeConstructor.Int16) => formatApply("Int16", args)
+        case Type.Cst(TypeConstructor.Int32) => formatApply("Int32", args)
+        case Type.Cst(TypeConstructor.Int64) => formatApply("Int64", args)
+        case Type.Cst(TypeConstructor.BigInt) => formatApply("BigInt", args)
+        case Type.Cst(TypeConstructor.Str) => formatApply("Str", args)
+        case Type.Cst(TypeConstructor.RecordEmpty) => formatApply("{ }", args)
+        case Type.Cst(TypeConstructor.SchemaEmpty) => formatApply("#{ }", args)
+        case Type.Cst(TypeConstructor.Pure) => formatApply("Pure", args)
+        case Type.Cst(TypeConstructor.Impure) => formatApply("Impure", args)
+
+        case Type.Cst(TypeConstructor.Array) => formatApply("Array", args)
+        case Type.Cst(TypeConstructor.Channel) => formatApply("Channel", args)
+        case Type.Cst(TypeConstructor.Enum(sym, _)) => formatApply(sym.toString, args)
+
+        case Type.Zero => formatApply("Zero", args)
+        case Type.Succ(n, tpe) => s"$n $tpe"
+
+
+        // MATT still to do: vector, relation, lattice
+
+        case Type.Cst(TypeConstructor.RecordExtend(_)) =>
+          if (args.lengthIs == 2) {
+            formatWellFormedRecord(tpe)
+          } else {
+            formatApply("Record", args)
+          }
+
+        case Type.Cst(TypeConstructor.SchemaExtend(_)) =>
+          if (args.lengthIs == 2) {
+            formatWellFormedSchema(tpe)
+          } else {
+            formatApply("Schema", args)
+          }
+
+        case Type.Cst(TypeConstructor.Tuple(_)) => args.map(visit).mkString("(", ", ", ")") // MATT different format if length != applies?
+
+
+        case Type.Cst(TypeConstructor.Not) => args match {
+          case (t1: Type.Var) :: Nil => s"¬${visit(t1)}"
+          case t1 :: Nil => s"¬(${visit(t1)})"
+          case _ => formatApply("Not", args)
+        }
+
+        case Type.Cst(TypeConstructor.And) => args match {
+          case (t1: Type.Var) :: (t2: Type.Var) :: Nil => s"${visit(t1)} ∧ ${visit(t2)}"
+          case (t1: Type.Var) :: t2 :: Nil => s"${visit(t1)} ∧ (${visit(t2)})"
+          case t1 :: (t2: Type.Var) :: Nil => s"(${visit(t1)}) ∧ ${visit(t2)}"
+          case t1 :: t2 :: Nil => s"(${visit(t1)}) ∧ (${visit(t2)})"
+          case _ => formatApply("And", args)
+        }
+
+        case Type.Cst(TypeConstructor.Or) => args match {
+          case (t1: Type.Var) :: (t2: Type.Var) :: Nil => s"${visit(t1)} ∨ ${visit(t2)}"
+          case (t1: Type.Var) :: t2 :: Nil => s"${visit(t1)} ∨ (${visit(t2)})"
+          case t1 :: (t2: Type.Var) :: Nil => s"(${visit(t1)}) ∨ ${visit(t2)}"
+          case t1 :: t2 :: Nil => s"(${visit(t1)}) ∨ (${visit(t2)})"
+          case _ => formatApply("Or", args)
+        }
+
+        case Type.Arrow(l, eff) =>
+          if (args.lengthIs < 2) {
+            formatApply("Arrow", args)
+          } else {
+            // Retrieve the arguments and result types.
+            val argumentTypes = args.init
+            val resultType = args.last
+
+            // Format the arguments.
+            val argPart = if (argumentTypes.length == 1) {
+              visit(argumentTypes.head)
+            } else {
+              argumentTypes.map(visit).mkString("(", ", ", ")")
+            }
+            // Format the arrow.
+            val arrowPart = eff match {
+              case Type.Cst(TypeConstructor.Impure) => " ~> "
+              case _ => " -> "
+            }
+            // Format the effect.
+            val effPart = eff match {
+              case Type.Cst(TypeConstructor.Pure) => ""
+              case Type.Cst(TypeConstructor.Impure) => ""
+              case _ => " & (" + visit(eff) + ")"
+            }
+            // Format the result type.
+            val resultPart = visit(resultType)
+
+            // Put everything together.
+            argPart + arrowPart + resultPart + effPart
+
+          }
+        case Type.Lambda(tvar, tpe) => audience match {
+          case Audience.Internal => s"${tvar.id} => ${visit(tpe)}"
+          case Audience.External => s"${renameMap.get(tvar.id)} => ${visit(tpe)}"
+        }
+        case _ => throw InternalCompilerException(s"Unexpected type: '$tpe''") // MATT this will infinitely loop if toString calls us
+      }
+    }
+
+    visit(tpe)
 
   }
 
@@ -65,16 +186,30 @@ object FormatType2 { // MATT rename and replace FormatType
     }
   }
 
-  private def flattenRecord(record: Type): FlatThing = record match {
+  def flattenRecord(record: Type): FlatThing = record match {
     case Type.Apply(Type.Apply(Type.Cst(TypeConstructor.RecordExtend(label)), tpe), rest) =>
       (label, tpe) :: flattenRecord(rest)
     case _ => FlatThing(Nil, record)
   }
 
 
-  private def flattenSchema(schema: Type): FlatThing = schema match {
+  def flattenSchema(schema: Type): FlatThing = schema match {
     case Type.Apply(Type.Apply(Type.Cst(TypeConstructor.SchemaExtend(label)), tpe), rest) =>
       (label, tpe) :: flattenSchema(rest)
     case _ => FlatThing(Nil, schema)
+  }
+
+
+  def getVarName(index: Int): String = {
+    if (index / 26 <= 0)
+      (index + 'a').toChar.toString
+    else
+      (index + 'a').toChar.toString + (index / 26).toString
+  }
+
+  def alphaRenameVars(tpe0: Type): Map[Int, String] = {
+    tpe0.typeVars.toList.sortBy(_.id).zipWithIndex.map {
+      case (tvar, index) => tvar.id -> getVarName(index)
+    }.toMap
   }
 }
--- main/test/ca/uwaterloo/flix/language/debug/TestFormatType.scala
+++ main/test/ca/uwaterloo/flix/language/debug/TestFormatType.scala
@@ -1,15 +1,17 @@
 package ca.uwaterloo.flix.language.debug
 
 import ca.uwaterloo.flix.TestUtils
-import ca.uwaterloo.flix.language.ast.{Kind, Rigidity, Type}
+import ca.uwaterloo.flix.language.ast.{Kind, Rigidity, SourceLocation, Symbol, Type, TypeConstructor}
 import org.scalatest.FunSuite
+
+import scala.util.Random
 
 class TestFormatType extends FunSuite with TestUtils {
   test("FormatType.WellFormedType.External.01") {
     val tpe = Type.mkRecordExtend("x", Type.Int32, Type.mkRecordExtend("y", Type.Str, Type.RecordEmpty))
 
     val expected = "{ x: Int32, y: Str }"
-    val actual = FormatType2.format(tpe)(FormatType2.Readability.External)
+    val actual = FormatType2.format(tpe)(FormatType2.Audience.External)
 
     assert(actual == expected)
   }
@@ -20,8 +22,135 @@ class TestFormatType extends FunSuite with TestUtils {
     val tpe = Type.mkRecordExtend("x", Type.Int32, rest)
 
     val expected = "{ x: Int32 | theRest }"
-    val actual = FormatType2.format(tpe)(FormatType2.Readability.External)
+    val actual = FormatType2.format(tpe)(FormatType2.Audience.External)
 
     assert(actual == expected)
+  }
+
+  test("FormatWellFormedType.External.03") {
+    val paramType = Type.Var(0, Kind.Star, Rigidity.Rigid)
+    paramType.setText("t1")
+    val tpe = Type.mkArrow(paramType, Type.Pure, paramType)
+
+    val expected = "t1 -> t1"
+    val actual = FormatType2.format(tpe)(FormatType2.Audience.External)
+
+    assert(actual == expected)
+  }
+
+  test("FormatWellFormedType.External.04") {
+    val paramType = Type.Var(0, Kind.Star, Rigidity.Rigid)
+    val returnType = Type.Var(1, Kind.Star, Rigidity.Rigid)
+    val effectType = Type.Var(2, Kind.Effect, Rigidity.Rigid)
+    val tpe = Type.mkArrow(paramType, effectType, returnType)
+
+    val expected = "a -> b & (c)"
+    val actual = FormatType2.format(tpe)(FormatType2.Audience.External)
+
+    assert(actual == expected)
+  }
+
+  private def randomType(complexity: Int): Type = {
+
+    def randomChoice[T](list: List[T]): T = {
+      list(Random.nextInt(list.length))
+    }
+
+    def randomKind(complexity: Int): Kind = {
+      if (complexity <= 0) {
+        Kind.Star
+      } else {
+        val generators = List(
+          () => Kind.Star,
+          () => Kind.Record,
+          () => Kind.Schema,
+          () => Kind.Nat,
+          () => Kind.Effect,
+          () => Kind.Arrow(List(randomKind(complexity - 1)), randomKind(complexity - 1))
+        )
+        randomChoice(generators).apply()
+      }
+    }
+
+    def randomRigidity(): Rigidity = {
+      randomChoice(List(Rigidity.Rigid, Rigidity.Flexible))
+    }
+
+    def randomLower(): String = {
+      randomChoice("abcdefghijklmnopqrstuvwxyz".toList.map(_.toString))
+    }
+
+    def randomUpper(): String = {
+      randomChoice("ABCDEFGHIJKLMNOPQRSTUVWXYZ".toList.map(_.toString))
+    }
+
+    def randomEnumSym(): Symbol.EnumSym = {
+      new Symbol.EnumSym(List(), randomUpper(), SourceLocation.Unknown)
+    }
+
+    def randomTypeConstructor(complexity: Int): TypeConstructor = {
+      if (complexity <= 0) {
+        TypeConstructor.Unit
+      } else {
+        val generators = List(
+          () => TypeConstructor.Unit,
+          () => TypeConstructor.Bool,
+          () => TypeConstructor.Char,
+          () => TypeConstructor.Float32,
+          () => TypeConstructor.Float64,
+          () => TypeConstructor.Int8,
+          () => TypeConstructor.Int16,
+          () => TypeConstructor.Int32,
+          () => TypeConstructor.Int64,
+          () => TypeConstructor.BigInt,
+          () => TypeConstructor.Str,
+          () => TypeConstructor.RecordEmpty,
+          () => TypeConstructor.RecordExtend(randomLower()),
+          () => TypeConstructor.SchemaEmpty,
+          () => TypeConstructor.SchemaExtend(randomLower()),
+          () => TypeConstructor.Array,
+          () => TypeConstructor.Channel,
+          () => TypeConstructor.Enum(randomEnumSym(), randomKind(complexity - 1)),
+          () => TypeConstructor.Native(classOf[Object]),
+          () => TypeConstructor.Ref,
+          () => TypeConstructor.Tuple(Random.nextInt()),
+          () => TypeConstructor.Vector,
+          () => TypeConstructor.Relation,
+          () => TypeConstructor.Lattice,
+          () => TypeConstructor.Pure,
+          () => TypeConstructor.Impure,
+          () => TypeConstructor.Not,
+          () => TypeConstructor.And,
+          () => TypeConstructor.Or
+        )
+        randomChoice(generators).apply()
+      }
+    }
+
+    def randomTypeVar(complexity: Int): Type.Var = {
+      Type.Var(Random.nextInt(), randomKind(complexity - 1), randomRigidity())
+    }
+
+    if (complexity <= 0) {
+      Type.Var(Random.nextInt(), randomKind(0), randomRigidity())
+    } else {
+      val generators = List(
+        () => Type.Var(Random.nextInt(), randomKind(complexity - 1), randomRigidity()),
+        () => Type.Cst(randomTypeConstructor(complexity - 1)),
+        () => Type.Arrow(Random.nextInt(), randomType(complexity - 1)),
+        () => Type.Succ(Random.nextInt(), randomType(complexity - 1)),
+        () => Type.Lambda(randomTypeVar(complexity - 1), randomType(complexity - 1))
+      )
+      randomChoice(generators).apply()
+    }
+
+  }
+
+  // MATT probably don't want to keep this, just for development/checking for holes
+  test("FormatRandomType.01") {
+    for (_ <- 0 to 10) {
+      val tpe = randomType(100)
+      System.err.println(FormatType2.format(tpe)(FormatType2.Audience.External))
+    }
   }
 }
