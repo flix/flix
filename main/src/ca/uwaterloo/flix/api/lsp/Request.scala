b752f5b849007752d64a81a9404e1866e9d71c9a
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2020-09-08 11:43:42 +0200

    WIP

--- main/src/ca/uwaterloo/flix/api/lsp/Request.scala
+++ main/src/ca/uwaterloo/flix/api/lsp/Request.scala
@@ -20,129 +20,135 @@ import java.nio.file.{Path, Paths}
 import ca.uwaterloo.flix.util.Result
 import ca.uwaterloo.flix.util.Result.{Err, Ok}
 import org.json4s
-import org.json4s.JsonAST.{JString, JValue}
+import org.json4s.JsonAST.{JInt, JString, JValue}
 
 /**
   * A common super-type for language server requests.
   */
-sealed trait Request
+sealed trait Request {
+  /**
+    * A unique number that identifies this specific request.
+    */
+  def requestId: Int
+}
 
 object Request {
 
   /**
     * A request to add (or update) the given uri with the given source code.
     */
-  case class AddUri(uri: String, src: String) extends Request
+  case class AddUri(requestId: Int, uri: String, src: String) extends Request
 
   /**
     * A request to remove the given uri.
     */
-  case class RemUri(uri: String) extends Request
-
-  /**
-    * A request to return the compiler version.
-    */
-  case object Version extends Request
+  case class RemUri(requestId: Int, uri: String) extends Request
 
   /**
     * A request to shutdown the language server.
     */
-  case object Shutdown extends Request
+  case object Shutdown extends Request {
+    def requestId: Int = -1
+  }
 
   /**
     * A request to run all benchmarks using the added URIs.
     */
-  case object RunBenchmarks extends Request
+  case class RunBenchmarks(requestId: Int) extends Request
 
   /**
     * A request to run main using the added URIs.
     */
-  case object RunMain extends Request
+  case class RunMain(requestId: Int) extends Request
 
   /**
     * A request to run all tests using the added URIs.
     */
-  case object RunTests extends Request
+  case class RunTests(requestId: Int) extends Request
 
   /**
     * A request to compile and check all source files.
     */
-  case object Check extends Request
+  case class Check(requestId: Int) extends Request
 
   /**
     * A request to get the type and effect of an expression.
     */
-  case class Context(uri: String, pos: Position) extends Request
+  case class Context(requestId: Int, uri: String, pos: Position) extends Request
 
   /**
     * A code lens request.
     */
-  case class Codelens(uri: String) extends Request
+  case class Codelens(requestId: Int, uri: String) extends Request
 
   /**
     * A request for code completion.
     */
-  case class Complete(uri: String, pos: Position) extends Request
+  case class Complete(requestId: Int, uri: String, pos: Position) extends Request
 
   /**
     * A request to go to a declaration.
     */
-  case class Goto(uri: String, pos: Position) extends Request
+  case class Goto(requestId: Int, uri: String, pos: Position) extends Request
 
   /**
     * A folding range request.
     */
-  case class FoldingRange(uri: String) extends Request
+  case class FoldingRange(requestId: Int, uri: String) extends Request
 
   /**
     * A request for all symbols.
     */
-  case class Symbols(uri: String) extends Request
+  case class Symbols(requestId: Int, uri: String) extends Request
 
   /**
     * A request to find all uses of an entity.
     */
-  case class Uses(uri: String, pos: Position) extends Request
+  case class Uses(requestId: Int, uri: String, pos: Position) extends Request
 
   /**
     * A request to run all benchmarks in the project.
     */
-  case class PackageBenchmark(projectRoot: Path) extends Request
+  case class PackageBenchmark(requestId: Int, projectRoot: Path) extends Request
 
   /**
     * A request to build the project.
     */
-  case class PackageBuild(projectRoot: Path) extends Request
+  case class PackageBuild(requestId: Int, projectRoot: Path) extends Request
 
   /**
     * A request to build the project documentation.
     */
-  case class PackageBuildDoc(projectRoot: Path) extends Request
+  case class PackageBuildDoc(requestId: Int, projectRoot: Path) extends Request
 
   /**
     * A request to build the JAR from the project.
     */
-  case class PackageBuildJar(projectRoot: Path) extends Request
+  case class PackageBuildJar(requestId: Int, projectRoot: Path) extends Request
 
   /**
     * A request to build a Flix package from the project.
     */
-  case class PackageBuildPkg(projectRoot: Path) extends Request
+  case class PackageBuildPkg(requestId: Int, projectRoot: Path) extends Request
 
   /**
     * A request to init a new project.
     */
-  case class PackageInit(projectRoot: Path) extends Request
+  case class PackageInit(requestId: Int, projectRoot: Path) extends Request
 
   /**
     * A request to run all tests in the project.
     */
-  case class PackageTest(projectRoot: Path) extends Request
+  case class PackageTest(requestId: Int, projectRoot: Path) extends Request
 
   /**
     * Tries to parse the given `json` value as a [[AddUri]] request.
     */
   def parseAddUri(json: json4s.JValue): Result[Request, String] = {
+    val idRes: Result[Int, String] = json \\ "uri" match {
+      case JInt(i) => Ok(i.toInt)
+      case s => Err(s"Unexpected id: '$s'.")
+    }
     val uriRes: Result[String, String] = json \\ "uri" match {
       case JString(s) => Ok(s)
       case s => Err(s"Unexpected uri: '$s'.")
@@ -152,208 +158,336 @@ object Request {
       case s => Err(s"Unexpected src: '$s'.")
     }
     for {
+      id <- idRes
       uri <- uriRes
       src <- srcRes
-    } yield Request.AddUri(uri, src)
+    } yield Request.AddUri(id, uri, src)
   }
 
   /**
     * Tries to parse the given `json` value as a [[RemUri]] request.
     */
   def parseRemUri(json: json4s.JValue): Result[Request, String] = {
+    val idRes: Result[Int, String] = json \\ "uri" match {
+      case JInt(i) => Ok(i.toInt)
+      case s => Err(s"Unexpected id: '$s'.")
+    }
     val uriRes: Result[String, String] = json \\ "uri" match {
       case JString(s) => Ok(s)
       case s => Err(s"Unexpected uri: '$s'.")
     }
     for {
+      id <- idRes
       uri <- uriRes
-    } yield Request.RemUri(uri)
+    } yield Request.RemUri(id, uri)
   }
 
   /**
     * Tries to parse the given `json` value as a [[Complete]] request.
     */
   def parseComplete(json: json4s.JValue): Result[Request, String] = {
+    val idRes: Result[Int, String] = json \\ "uri" match {
+      case JInt(i) => Ok(i.toInt)
+      case s => Err(s"Unexpected id: '$s'.")
+    }
     val uriRes: Result[String, String] = json \\ "uri" match {
       case JString(s) => Ok(s)
       case s => Err(s"Unexpected uri: '$s'.")
     }
     for {
+      id <- idRes
       uri <- uriRes
       pos <- Position.parse(json \\ "position")
-    } yield Request.Complete(uri, pos)
+    } yield Request.Complete(id, uri, pos)
   }
 
   /**
     * Tries to parse the given `json` value as a [[Context]] request.
     */
   def parseContext(json: JValue): Result[Request, String] = {
+    val idRes: Result[Int, String] = json \\ "uri" match {
+      case JInt(i) => Ok(i.toInt)
+      case s => Err(s"Unexpected id: '$s'.")
+    }
     val uriRes: Result[String, String] = json \\ "uri" match {
       case JString(s) => Ok(s)
       case s => Err(s"Unexpected uri: '$s'.")
     }
     for {
+      id <- idRes
       uri <- uriRes
       pos <- Position.parse(json \\ "position")
-    } yield Request.Context(uri, pos)
+    } yield Request.Context(id, uri, pos)
+  }
+
+  /**
+    * Tries to parse the given `json` value as a [[Check]] request.
+    */
+  def parseCheck(json: json4s.JValue): Result[Request, String] = {
+    val idRes: Result[Int, String] = json \\ "uri" match {
+      case JInt(i) => Ok(i.toInt)
+      case s => Err(s"Unexpected id: '$s'.")
+    }
+    for {
+      id <- idRes
+    } yield Request.Check(id)
   }
 
   /**
     * Tries to parse the given `json` value as a [[Codelens]] request.
     */
   def parseCodelens(json: json4s.JValue): Result[Request, String] = {
+    val idRes: Result[Int, String] = json \\ "uri" match {
+      case JInt(i) => Ok(i.toInt)
+      case s => Err(s"Unexpected id: '$s'.")
+    }
     val uriRes: Result[String, String] = json \\ "uri" match {
       case JString(s) => Ok(s)
       case s => Err(s"Unexpected uri: '$s'.")
     }
     for {
+      id <- idRes
       uri <- uriRes
-    } yield Request.Codelens(uri)
+    } yield Request.Codelens(id, uri)
   }
 
   /**
     * Tries to parse the given `json` value as a [[FoldingRange]] request.
     */
   def parseFoldingRange(json: json4s.JValue): Result[Request, String] = {
+    val idRes: Result[Int, String] = json \\ "uri" match {
+      case JInt(i) => Ok(i.toInt)
+      case s => Err(s"Unexpected id: '$s'.")
+    }
     val uriRes: Result[String, String] = json \\ "uri" match {
       case JString(s) => Ok(s)
       case s => Err(s"Unexpected uri: '$s'.")
     }
     for {
+      id <- idRes
       uri <- uriRes
-    } yield Request.FoldingRange(uri)
+    } yield Request.FoldingRange(id, uri)
   }
 
   /**
     * Tries to parse the given `json` value as a [[Goto]] request.
     */
   def parseGoto(json: json4s.JValue): Result[Request, String] = {
+    val idRes: Result[Int, String] = json \\ "uri" match {
+      case JInt(i) => Ok(i.toInt)
+      case s => Err(s"Unexpected id: '$s'.")
+    }
     val uriRes: Result[String, String] = json \\ "uri" match {
       case JString(s) => Ok(s)
       case s => Err(s"Unexpected uri: '$s'.")
     }
     for {
+      id <- idRes
       uri <- uriRes
       pos <- Position.parse(json \\ "position")
-    } yield Request.Goto(uri, pos)
+    } yield Request.Goto(id, uri, pos)
   }
 
   /**
     * Tries to parse the given `json` value as a [[Symbols]] request.
     */
   def parseSymbols(json: json4s.JValue): Result[Request, String] = {
+    val idRes: Result[Int, String] = json \\ "uri" match {
+      case JInt(i) => Ok(i.toInt)
+      case s => Err(s"Unexpected id: '$s'.")
+    }
     val uriRes: Result[String, String] = json \\ "uri" match {
       case JString(s) => Ok(s)
       case s => Err(s"Unexpected uri: '$s'.")
     }
     for {
+      id <- idRes
       uri <- uriRes
-    } yield Request.Symbols(uri)
+    } yield Request.Symbols(id, uri)
   }
 
   /**
     * Tries to parse the given `json` value as a [[Uses]] request.
     */
   def parseUses(json: json4s.JValue): Result[Request, String] = {
+    val idRes: Result[Int, String] = json \\ "uri" match {
+      case JInt(i) => Ok(i.toInt)
+      case s => Err(s"Unexpected id: '$s'.")
+    }
     val uriRes: Result[String, String] = json \\ "uri" match {
       case JString(s) => Ok(s)
       case s => Err(s"Unexpected uri: '$s'.")
     }
     for {
+      id <- idRes
       uri <- uriRes
       pos <- Position.parse(json \\ "position")
-    } yield Request.Uses(uri, pos)
+    } yield Request.Uses(id, uri, pos)
+  }
+
+  /**
+    * Tries to parse the given `json` value as a [[RunBenchmarks]] request.
+    */
+  def parseRunBenchmarks(json: json4s.JValue): Result[Request, String] = {
+    val idRes: Result[Int, String] = json \\ "uri" match {
+      case JInt(i) => Ok(i.toInt)
+      case s => Err(s"Unexpected id: '$s'.")
+    }
+    for {
+      id <- idRes
+    } yield Request.RunBenchmarks(id)
+  }
+
+  /**
+    * Tries to parse the given `json` value as a [[RunMain]] request.
+    */
+  def parseRunMain(json: json4s.JValue): Result[Request, String] = {
+    val idRes: Result[Int, String] = json \\ "uri" match {
+      case JInt(i) => Ok(i.toInt)
+      case s => Err(s"Unexpected id: '$s'.")
+    }
+    for {
+      id <- idRes
+    } yield Request.RunMain(id)
+  }
+
+  /**
+    * Tries to parse the given `json` value as a [[RunTests]] request.
+    */
+  def parseRunTests(json: json4s.JValue): Result[Request, String] = {
+    val idRes: Result[Int, String] = json \\ "uri" match {
+      case JInt(i) => Ok(i.toInt)
+      case s => Err(s"Unexpected id: '$s'.")
+    }
+    for {
+      id <- idRes
+    } yield Request.RunTests(id)
   }
 
   /**
     * Tries to parse the given `json` value as a [[PackageBenchmark]] request.
     */
   def parsePackageBenchmark(json: JValue): Result[Request, String] = {
+    val idRes: Result[Int, String] = json \\ "uri" match {
+      case JInt(i) => Ok(i.toInt)
+      case s => Err(s"Unexpected id: '$s'.")
+    }
     val projectRootUri: Result[String, String] = json \\ "projectRoot" match {
       case JString(s) => Ok(s)
       case s => Err(s"Unexpected uri: '$s'.")
     }
     for {
+      id <- idRes
       projectRoot <- projectRootUri
-    } yield Request.PackageBenchmark(Paths.get(projectRoot))
+    } yield Request.PackageBenchmark(id, Paths.get(projectRoot))
   }
 
   /**
     * Tries to parse the given `json` value as a [[PackageBuild]] request.
     */
   def parsePackageBuild(json: JValue): Result[Request, String] = {
+    val idRes: Result[Int, String] = json \\ "uri" match {
+      case JInt(i) => Ok(i.toInt)
+      case s => Err(s"Unexpected id: '$s'.")
+    }
     val projectRootUri: Result[String, String] = json \\ "projectRoot" match {
       case JString(s) => Ok(s)
       case s => Err(s"Unexpected uri: '$s'.")
     }
     for {
+      id <- idRes
       projectRoot <- projectRootUri
-    } yield Request.PackageBuild(Paths.get(projectRoot))
+    } yield Request.PackageBuild(id, Paths.get(projectRoot))
   }
 
   /**
     * Tries to parse the given `json` value as a [[PackageBuildDoc]] request.
     */
   def parsePackageBuildDoc(json: JValue): Result[Request, String] = {
+    val idRes: Result[Int, String] = json \\ "uri" match {
+      case JInt(i) => Ok(i.toInt)
+      case s => Err(s"Unexpected id: '$s'.")
+    }
     val projectRootUri: Result[String, String] = json \\ "projectRoot" match {
       case JString(s) => Ok(s)
       case s => Err(s"Unexpected uri: '$s'.")
     }
     for {
+      id <- idRes
       projectRoot <- projectRootUri
-    } yield Request.PackageBuildDoc(Paths.get(projectRoot))
+    } yield Request.PackageBuildDoc(id, Paths.get(projectRoot))
   }
 
   /**
     * Tries to parse the given `json` value as a [[PackageBuildJar]] request.
     */
   def parsePackageBuildJar(json: JValue): Result[Request, String] = {
+    val idRes: Result[Int, String] = json \\ "uri" match {
+      case JInt(i) => Ok(i.toInt)
+      case s => Err(s"Unexpected id: '$s'.")
+    }
     val projectRootUri: Result[String, String] = json \\ "projectRoot" match {
       case JString(s) => Ok(s)
       case s => Err(s"Unexpected uri: '$s'.")
     }
     for {
+      id <- idRes
       projectRoot <- projectRootUri
-    } yield Request.PackageBuildJar(Paths.get(projectRoot))
+    } yield Request.PackageBuildJar(id, Paths.get(projectRoot))
   }
 
   /**
     * Tries to parse the given `json` value as a [[PackageBuildPkg]] request.
     */
   def parsePackageBuildPkg(json: JValue): Result[Request, String] = {
+    val idRes: Result[Int, String] = json \\ "uri" match {
+      case JInt(i) => Ok(i.toInt)
+      case s => Err(s"Unexpected id: '$s'.")
+    }
     val projectRootUri: Result[String, String] = json \\ "projectRoot" match {
       case JString(s) => Ok(s)
       case s => Err(s"Unexpected uri: '$s'.")
     }
     for {
+      id <- idRes
       projectRoot <- projectRootUri
-    } yield Request.PackageBuildPkg(Paths.get(projectRoot))
+    } yield Request.PackageBuildPkg(id, Paths.get(projectRoot))
   }
 
   /**
     * Tries to parse the given `json` value as a [[PackageInit]] request.
     */
   def parsePackageInit(json: JValue): Result[Request, String] = {
+    val idRes: Result[Int, String] = json \\ "uri" match {
+      case JInt(i) => Ok(i.toInt)
+      case s => Err(s"Unexpected id: '$s'.")
+    }
     val projectRootUri: Result[String, String] = json \\ "projectRoot" match {
       case JString(s) => Ok(s)
       case s => Err(s"Unexpected uri: '$s'.")
     }
     for {
+      id <- idRes
       projectRoot <- projectRootUri
-    } yield Request.PackageInit(Paths.get(projectRoot))
+    } yield Request.PackageInit(id, Paths.get(projectRoot))
   }
 
   /**
     * Tries to parse the given `json` value as a [[PackageTest]] request.
     */
   def parsePackageTest(json: JValue): Result[Request, String] = {
+    val idRes: Result[Int, String] = json \\ "uri" match {
+      case JInt(i) => Ok(i.toInt)
+      case s => Err(s"Unexpected id: '$s'.")
+    }
     val projectRootUri: Result[String, String] = json \\ "projectRoot" match {
       case JString(s) => Ok(s)
       case s => Err(s"Unexpected uri: '$s'.")
     }
     for {
+      id <- idRes
       projectRoot <- projectRootUri
-    } yield Request.PackageTest(Paths.get(projectRoot))
+    } yield Request.PackageTest(id, Paths.get(projectRoot))
   }
 
 }
