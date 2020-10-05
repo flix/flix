f20a96235fc520e1daef10c031674cecd91efa16
Matthew Lutze <mcdlutze@gmail.com>
2020-10-05 21:22:05 +0200

    implement context reduction

--- main/src/ca/uwaterloo/flix/language/phase/unification/ContextReduction.scala
+++ main/src/ca/uwaterloo/flix/language/phase/unification/ContextReduction.scala
@@ -1,52 +1,83 @@
 package ca.uwaterloo.flix.language.phase.unification
 
+import ca.uwaterloo.flix.api.Flix
 import ca.uwaterloo.flix.language.ast.{Symbol, Type, TypedAst}
+import ca.uwaterloo.flix.util.Result
+import ca.uwaterloo.flix.util.collection.MultiMap
 
 import scala.annotation.tailrec
 
+// MATT maybe change name to ClassUnification or something like that
 object ContextReduction {
-  private def toHeadNormalForm(classEnv: Map[Symbol.ClassSym, TypedAst.Class], tconstr: TypedAst.TypeConstraint): Option[List[TypedAst.TypeConstraint]] = {
+
+  // MATT docs
+  // MATT THIH says that toncstrs0 should always be in HNF so checking for byInst is a waste.
+  def entails(instances: MultiMap[Symbol.ClassSym, TypedAst.Instance], tconstrs0: List[TypedAst.TypeConstraint], tconstr: TypedAst.TypeConstraint)(implicit flix: Flix): Boolean = {
+    // tconstrs0 is unused for now; it will be used when superclasses are implemented
+    byInst(instances, tconstr) match {
+      case Some(tconstrs) => tconstrs.forall(entails(instances, tconstrs0, _))
+      case None => false
+    }
+  }
+
+  // MATT docs
+  def simplify(instances: MultiMap[Symbol.ClassSym, TypedAst.Instance], tconstrs0: List[TypedAst.TypeConstraint])(implicit flix: Flix): List[TypedAst.TypeConstraint] = {
+
+    // MATT docs
+    @tailrec
+    def loop(acc: List[TypedAst.TypeConstraint], tconstrs0: List[TypedAst.TypeConstraint]): List[TypedAst.TypeConstraint] = tconstrs0 match {
+      case Nil => acc
+      case head :: tail if entails(instances, acc ++ tail, head) => loop(acc, tail)
+      case head :: tail => loop(head :: acc, tail)
+    }
+    loop(tconstrs0, Nil)
+  }
+
+  // MATT docs
+  def reduce(instances: MultiMap[Symbol.ClassSym, TypedAst.Instance], tconstrs0: List[TypedAst.TypeConstraint])(implicit flix: Flix): Option[List[TypedAst.TypeConstraint]] = {
+    def sequence[T](list: List[Option[T]]): Option[List[T]] = { // MATT change to validations
+      Option.unless(list contains None)(list.map(_.get))
+    }
+    for {
+      tconstrs <- sequence(tconstrs0.map(toHeadNormalForm(instances, _)))
+    } yield simplify(instances, tconstrs.flatten)
+  }
+
+  // MATT docs
+  private def toHeadNormalForm(instances: MultiMap[Symbol.ClassSym, TypedAst.Instance], tconstr: TypedAst.TypeConstraint)(implicit flix: Flix): Option[List[TypedAst.TypeConstraint]] = {
     if (isHeadNormalForm(tconstr.arg)) {
       Some(List(tconstr))
     } else {
-      ???
-      /*
-      byInst(classEnv, tconstr) match {
-        case None => error
-        case Some(preds) => preds.map(toHeadNormalForm)
-      }
-       */
+      byInst(instances, tconstr)
     }
   }
 
-  private def byInst(classEnv: Map[Symbol.ClassSym, TypedAst.Class], tconstr: TypedAst.TypeConstraint): Option[List[TypedAst.TypeConstraint]] = {
-    val instances = tconstr match {
-      case TypedAst.TypeConstraint(sym, _) => insts(classEnv, sym)
+  // MATT docs
+  private def byInst(instances0: MultiMap[Symbol.ClassSym, TypedAst.Instance], tconstr: TypedAst.TypeConstraint)(implicit flix: Flix): Option[List[TypedAst.TypeConstraint]] = {
+    val matchingInstances = instances0(tconstr.sym)
+
+    // MATT docs
+    def tryInst(inst: TypedAst.Instance): Option[List[TypedAst.TypeConstraint]] = {
+      for {
+        subst <- toOption(Unification.unifyTypes(tconstr.arg, inst.tpe))
+      } yield inst.tconstrs.map(subst(_))
     }
 
-    // for each instance matching this class
-    // get substitution from instance.tpe to tconstr.tpe
-    // if it works, apply it to the requirement predicates
-    // return mapped requirement predicates
-    // (if no instance works, error)
-    def tryInst(inst: TypedAst.Instance)
+    matchingInstances.flatMap(tryInst).headOption // MATT maybe assert that there is only one
   }
 
-  // THIH class(list of superclasses, list of instances)
-  // THIH instance(qual of pred) ~= list of preds (like Show[a]) => pred (like Show[List[a]])
-  // this can be implemented for flix as:
-    // instance(requirements: list of preds, class: the class this implements, type: the type this implements)
-    // or we can promote typeconstraint to be used outside typedast (kinda doing that already)
 
-  private def insts(classEnv: Map[Symbol.ClassSym, TypedAst.Class], sym: Symbol.ClassSym): List[TypedAst.TypeConstraint] = {
-    ??? // lookup all instances of the class matching this constraint; should use instance env instead
-    // can just do simple Map access if we have Symbol.ClassSym -> List[Instance] map
+  /**
+    * Returns `this` result as an [[Option]].
+    */
+  def toOption[T](result: Result[T, _]): Option[T] = result match {
+    case Result.Ok(t) => Some(t)
+    case Result.Err(_) => None
   }
 
-  @tailrec
-  private def isHeadNormalForm(tpe: Type): Boolean = tpe match {
-    case _: Type.Var => true
-    case _: Type.Cst => false
-    case Type.Apply(t, _) => isHeadNormalForm(t)
+
+  // MATT docs
+  private def isHeadNormalForm(tpe: Type): Boolean = {
+    tpe.typeConstructor.isEmpty
   }
 }
