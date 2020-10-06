6db62b6a4223b25cc5a79bf6892c80ac0c8dc115
Matthew Lutze <mcdlutze@gmail.com>
2020-10-06 15:43:39 +0200

    add documentation to ContextReduction

--- main/src/ca/uwaterloo/flix/language/phase/unification/ContextReduction.scala
+++ main/src/ca/uwaterloo/flix/language/phase/unification/ContextReduction.scala
@@ -2,7 +2,7 @@ package ca.uwaterloo.flix.language.phase.unification
 
 import ca.uwaterloo.flix.api.Flix
 import ca.uwaterloo.flix.language.ast.{Symbol, Type, TypedAst}
-import ca.uwaterloo.flix.util.Result
+import ca.uwaterloo.flix.util.{InternalCompilerException, Result}
 import ca.uwaterloo.flix.util.Result.ToOk
 import ca.uwaterloo.flix.util.collection.MultiMap
 
@@ -12,38 +12,46 @@ import scala.annotation.tailrec
 // MATT maybe change name to ClassUnification or something like that
 object ContextReduction {
 
-  // MATT docs
+  /**
+    * Returns `true` iff type constraints `tconstrs0` entail type constraint `tconstr`, under class environment `instances`.
+    * That is, `tconstr` is true if all of `tconstrs0` are true.
+    */
   // MATT THIH says that toncstrs0 should always be in HNF so checking for byInst is a waste.
-  def entails(instances: MultiMap[Symbol.ClassSym, TypedAst.Instance], tconstrs0: List[TypedAst.TypeConstraint], tconstr: TypedAst.TypeConstraint)(implicit flix: Flix): Boolean = {
+  def entail(instances: MultiMap[Symbol.ClassSym, TypedAst.Instance], tconstrs0: List[TypedAst.TypeConstraint], tconstr: TypedAst.TypeConstraint)(implicit flix: Flix): Boolean = {
     // tconstrs0 is unused for now; it will be used when superclasses are implemented
     byInst(instances, tconstr) match {
-      case Result.Ok(tconstrs) => tconstrs.forall(entails(instances, tconstrs0, _))
+      case Result.Ok(tconstrs) => tconstrs.forall(entail(instances, tconstrs0, _))
       case Result.Err(_) => false
     }
   }
 
-  // MATT docs
-  def simplify(instances: MultiMap[Symbol.ClassSym, TypedAst.Instance], tconstrs0: List[TypedAst.TypeConstraint])(implicit flix: Flix): List[TypedAst.TypeConstraint] = {
+  /**
+    * Removes the type constraints which are entailed by the others in the list.
+    */
+  private def simplify(instances: MultiMap[Symbol.ClassSym, TypedAst.Instance], tconstrs0: List[TypedAst.TypeConstraint])(implicit flix: Flix): List[TypedAst.TypeConstraint] = {
 
-    // MATT docs
     @tailrec
     def loop(acc: List[TypedAst.TypeConstraint], tconstrs0: List[TypedAst.TypeConstraint]): List[TypedAst.TypeConstraint] = tconstrs0 match {
       case Nil => acc
-      case head :: tail if entails(instances, acc ++ tail, head) => loop(acc, tail)
+      case head :: tail if entail(instances, acc ++ tail, head) => loop(acc, tail)
       case head :: tail => loop(head :: acc, tail)
     }
 
     loop(tconstrs0, Nil)
   }
 
-  // MATT docs
-  def reduce(instances: MultiMap[Symbol.ClassSym, TypedAst.Instance], tconstrs0: List[TypedAst.TypeConstraint])(implicit flix: Flix): Result[List[TypedAst.TypeConstraint], UnificationError] = {
+  /**
+    * Normalizes a list of type constraints, converting to head-normal form and removing semantic duplicates.
+    */
+  private def reduce(instances: MultiMap[Symbol.ClassSym, TypedAst.Instance], tconstrs0: List[TypedAst.TypeConstraint])(implicit flix: Flix): Result[List[TypedAst.TypeConstraint], UnificationError] = {
     for {
       tconstrs <- Result.sequence(tconstrs0.map(toHeadNormalForm(instances, _)))
     } yield simplify(instances, tconstrs.flatten)
   }
 
-  // MATT docs
+  /**
+    * Converts the type constraint to head-normal form, i.e. `a[X1, Xn]`, where `a` is a variable and `n >= 0`.
+    */
   private def toHeadNormalForm(instances: MultiMap[Symbol.ClassSym, TypedAst.Instance], tconstr: TypedAst.TypeConstraint)(implicit flix: Flix): Result[List[TypedAst.TypeConstraint], UnificationError] = {
     if (isHeadNormalForm(tconstr.arg)) {
       List(tconstr).toOk
@@ -52,11 +60,12 @@ object ContextReduction {
     }
   }
 
-  // MATT docs
+  /**
+    * Returns the list of constraints that hold if the given constraint `tconstr` hold.
+    */
   private def byInst(instances0: MultiMap[Symbol.ClassSym, TypedAst.Instance], tconstr: TypedAst.TypeConstraint)(implicit flix: Flix): Result[List[TypedAst.TypeConstraint], UnificationError] = {
     val matchingInstances = instances0(tconstr.sym).toList
 
-    // MATT docs
     def tryInst(inst: TypedAst.Instance): Result[List[TypedAst.TypeConstraint], UnificationError] = {
       for {
         subst <- Unification.unifyTypes(tconstr.arg, inst.tpe)
@@ -66,15 +75,17 @@ object ContextReduction {
     for {
       tconstrs0 <- Result.sequence(matchingInstances.map(tryInst))
     } yield {
-      tconstrs0.headOption match {
-        case Some(tconstrs) => tconstrs.toOk
-        case None => ??? // MATT UnificationError.NoMatchingInstance
-        // MATT maybe assert that there is only one
+      tconstrs0 match {
+        case tconstrs :: Nil => tconstrs.toOk
+        case Nil => ??? // MATT UnificationError.NoMatchingInstance
+        case _ => throw InternalCompilerException("Multiple matching instances.")
       }
     }
   }
 
-  // MATT docs
+  /**
+    * Returns true iff this type is in head-normal form.
+    */
   private def isHeadNormalForm(tpe: Type): Boolean = {
     tpe.typeConstructor.isEmpty
   }
