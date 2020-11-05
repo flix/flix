60f5f52bbb40a4df34652c624795de54fc928c78
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2020-11-05 12:16:19 +0100

    WIP

--- main/src/ca/uwaterloo/flix/api/lsp/provider/HoverProvider.scala
+++ main/src/ca/uwaterloo/flix/api/lsp/provider/HoverProvider.scala
@@ -96,11 +96,10 @@ object HoverProvider {
     ("status" -> "success") ~ ("result" -> result)
   }
 
-
   private def formatTypAndEff(tpe0: Type, eff0: Type): String = {
     val t = FormatType.formatType(tpe0)
     eff0 match {
-      case Type.Cst(TypeConstructor.True, _) => t
+      case Type.Cst(TypeConstructor.True, _) => s"$t & Pure"
       case Type.Cst(TypeConstructor.False, _) => s"$t & Impure"
       case eff => s"$t & ${FormatType.formatType(eff)}"
     }
