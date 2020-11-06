f8b37aeea4c0e005079d2822965e31bface47fc2
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2020-11-06 11:11:41 +0100

    WIP

--- main/test/ca/uwaterloo/flix/language/feature/FeatureSuite.scala
+++ main/test/ca/uwaterloo/flix/language/feature/FeatureSuite.scala
@@ -21,8 +21,6 @@ import org.scalatest.Suites
 
 class FeatureSuite extends Suites(
 
-  new FlixTest("Test.Expression.Tuple", "main/test/ca/uwaterloo/flix/language/feature/Test.Expression.Tuple.flix")(Options.TestWithLibrary),
-
   //
   // Pattern Match.
   //
