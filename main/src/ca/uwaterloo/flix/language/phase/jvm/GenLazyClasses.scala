a943c3546e7ad0f18890804fcf31eeb3d30fc307
JonathanStarup <32037926+JonathanStarup@users.noreply.github.com>
2020-11-17 19:53:30 +0100

    Added locks, avoided unnecessary genByteCode calls, and added comments.

--- main/src/ca/uwaterloo/flix/language/phase/jvm/GenLazyClasses.scala
+++ main/src/ca/uwaterloo/flix/language/phase/jvm/GenLazyClasses.scala
@@ -37,8 +37,12 @@ object GenLazyClasses {
         // Construct lazy class.
         val fullType = JvmOps.getLazyClassType(tpe)
         val jvmName = fullType.name
-        val bytecode = genByteCode(fullType, valueType)
-        macc + (jvmName -> JvmClass(jvmName, bytecode))
+        if (!macc.contains(jvmName)) {
+          val bytecode = genByteCode(fullType, valueType)
+          macc + (jvmName -> JvmClass(jvmName, bytecode))
+        } else {
+          macc
+        }
       case (macc, tpe) =>
         // Case 2: The type constructor is a non-tuple.
         // Nothing to be done. Return the map.
@@ -47,7 +51,17 @@ object GenLazyClasses {
   }
 
   /**
-   * TODO: make documentation
+   * This method creates the class for each lazy value.
+   * The specific lazy class has an associated value type (tpe) which
+   * is either a jvm primitive or object.
+   *
+   * The lazy class has three fields - initialized: bool, expression: () -> tpe,
+   * and value: tpe. These are all private. force(context) is the only public
+   * method, which retuns a value of type tpe given a context to call the
+   * expression closure in.
+   *
+   * force will only evaluate the expression the first time, based on the flag initialized.
+   * After that point it will store the result in value and just return that.
    */
   private def genByteCode(classType: JvmType.Reference, valueType: MonoType)(implicit root: Root, flix: Flix): Array[Byte] = {
     // class writer
@@ -58,9 +72,6 @@ object GenLazyClasses {
 
     // Initialize the visitor to create a class.
     visitor.visit(AsmOps.JavaVersion, ACC_PUBLIC + ACC_FINAL, classType.name.toInternalName, null, superClass, null)
-
-    // Source of the class TODO: what is the point of this?
-    visitor.visitSource(classType.name.toInternalName, null)
 
     AsmOps.compileField(visitor, "initialized", JvmType.PrimBool, isStatic = false, isPrivate = true)
     AsmOps.compileField(visitor, "expression", JvmType.Object, isStatic = false, isPrivate = true)
@@ -87,11 +98,12 @@ object GenLazyClasses {
   }
 
   /**
-   * Emits code to call a closure (not in tail position). fType is the type of the called closure. argsType is the type of its arguments, and resultType is the type of its result.
-   * TODO: avoid all this dublication
-   * OBS: This is copied since the last cast of the original method is not known at the class but at the force expression instead
+   * !OBS!: This code was copied from AsmOps to omit the last cast in the function.
+   *
+   * Emits code to call a closure (not in tail position). fType is the type of the called
+   * closure. argsType is the type of its arguments, and resultType is the type of its result.
    */
-  def compileClosureApplication(visitor: MethodVisitor, valueType: MonoType)(implicit root: Root, flix: Flix) = {
+  private def compileClosureApplication(visitor: MethodVisitor, valueType: MonoType)(implicit root: Root, flix: Flix) = {
     val fType = MonoType.Arrow(List(MonoType.Unit), valueType)
     // Type of the continuation interface
     val cont = JvmOps.getContinuationInterfaceType(fType)
@@ -125,52 +137,92 @@ object GenLazyClasses {
     visitor.visitMethodInsn(INVOKEINTERFACE, cont.name.toInternalName, "getResult", AsmOps.getMethodDescriptor(Nil, JvmOps.getErasedJvmType(valueType)), true)
   }
 
-    /**
-   * TODO: make documentation
+  /**
+   * The force method takes a context as argument to call the expression closure in.
+   * The result of the expression given in the constructor is then returned.
+   * This is only actually evaluated the first time, and saved to return directly
+   * afterwards.
+   *
+   * If lazy has associated type of Obj, the returned object needs to be casted
+   * to whatever expected type.
    */
   private def compileForceMethod(visitor: ClassWriter, classType: JvmType.Reference, valueType: MonoType)(implicit root: Root, flix: Flix): Unit = {
+    /*
+    force(context) :=
+
+    lock(this)
+    if (!this.initialized) {
+      this.value = this.expression()
+      this.initialized = true
+    }
+    tpe result = this.value
+    unlock(this)
+    return result
+     */
+
     val erasedValueType = JvmOps.getErasedJvmType(valueType)
     val erasedValueTypeDescriptor = erasedValueType.toDescriptor
     val internalClassType = classType.name.toInternalName
 
-    // header of the method
+    // Header of the method.
     val returnDescription = AsmOps.getMethodDescriptor(List(JvmType.Context), erasedValueType)
     val method = visitor.visitMethod(ACC_PUBLIC + ACC_FINAL, "force", returnDescription, null, null)
     method.visitCode()
 
-    /*
-    if (!initialized) {
-      this.value = expression()
-      this.initialized = true
-    }
-    return this.value
-     */
-
+    // [this] This is pushed to lock the object.
     method.visitVarInsn(ALOAD, 0)
+    // [] The object is now locked.
+    method.visitInsn(MONITORENTER)
+
+    // [this] this is pushed to retrieve initialized to check if the Lazy object has already been evaluated.
+    method.visitVarInsn(ALOAD, 0)
+    // [this.initialized] the condition can now be checked.
     method.visitFieldInsn(GETFIELD, internalClassType, "initialized", JvmType.PrimBool.toDescriptor)
+
+    // [] If expression has been evaluated, skip to returning this.value.
     val skip = new Label
     method.visitJumpInsn(IFNE, skip)
 
+    // [context] Load the context to give as an argument to the expression closure.
     method.visitVarInsn(ALOAD, 1)
+    // [context, this] push this to get the expression.
     method.visitVarInsn(ALOAD, 0)
+    // [context, expression] now ready to call the expression closure.
     method.visitFieldInsn(GETFIELD, internalClassType, "expression", JvmType.Object.toDescriptor)
+    // [value] The result of expression remains on the stack.
     compileClosureApplication(method, valueType)
+    // [value, this] this is pushed to assign this.value = value.
     method.visitVarInsn(ALOAD, 0)
+    // [this, value] the two stack elements are swapped, technique depending on values type category.
     if (AsmOps.getStackSize(erasedValueType) == 1) {
       method.visitInsn(SWAP)
     } else {
       method.visitInsn(DUP_X2)
       method.visitInsn(POP)
     }
+    // [] this.value now stores the result from expression.
     method.visitFieldInsn(PUTFIELD, internalClassType, "value", erasedValueTypeDescriptor)
 
+    // [this] this is pushed to update initialized such that evaluation is skipped the next call.
     method.visitVarInsn(ALOAD, 0)
+    // [this, 1] true is pushed to assign this.initialized = true.
     method.visitInsn(ICONST_1)
+    // [] initialized is now true.
     method.visitFieldInsn(PUTFIELD, internalClassType, "initialized", JvmType.PrimBool.toDescriptor)
 
+    // This is the return point if initialized was true at the start of force.
     method.visitLabel(skip)
+    // [this] this is pushed to retrieve this.value.
     method.visitVarInsn(ALOAD, 0)
+    // [this.value] The return value is now on the stack.
     method.visitFieldInsn(GETFIELD, internalClassType, "value", erasedValueTypeDescriptor)
+
+    // [this.value, this] this is pushed to unlock the object.
+    method.visitVarInsn(ALOAD, 0)
+    // [this.value] we are now ready to return.
+    method.visitInsn(MONITOREXIT)
+
+    // [] Return the value of appropriate type.
     method.visitInsn(AsmOps.getReturnInstruction(erasedValueType))
 
     // Parameters of visit max are thrown away because visitor will calculate the frame and variable stack size
@@ -179,29 +231,40 @@ object GenLazyClasses {
   }
 
   /**
-   * TODO: make documentation
+   * The constructor takes a expression object, which should be a function that takes
+   * no argument and returns something of type tpe, related to the type of the lazy class.
    */
   def compileLazyConstructor(visitor: ClassWriter, classType: JvmType.Reference)(implicit root: Root, flix: Flix): Unit = {
+    /*
+    Lazy$tpe(expression) :=
 
+    this.initialized = false
+    this.expression = expression.
+     */
     val constructor = visitor.visitMethod(ACC_PUBLIC, "<init>", AsmOps.getMethodDescriptor(List(JvmType.Object), JvmType.Void), null, null)
 
     constructor.visitCode()
+    // [this] push this to call the object constructor.
     constructor.visitVarInsn(ALOAD, 0)
 
-    // Call the super (java.lang.Object) constructor
+    // [] Call the super (java.lang.Object) constructor
     constructor.visitMethodInsn(INVOKESPECIAL, JvmName.Object.toInternalName, "<init>", AsmOps.getMethodDescriptor(Nil, JvmType.Void), false)
 
-    // this.uninitialized = false
+    // [this] this is pushed to assign initialized to false.
     constructor.visitVarInsn(ALOAD, 0)
+    // [this, false] now ready to put field.
     constructor.visitInsn(ICONST_0)
+    // [] initialized = false.
     constructor.visitFieldInsn(PUTFIELD, classType.name.toInternalName, "initialized", JvmType.PrimBool.toDescriptor)
 
-    // this.expression = expression
+    // [this] save the argument to expression.
     constructor.visitVarInsn(ALOAD, 0)
+    // [this, expression] now ready to put field.
     constructor.visitVarInsn(ALOAD, 1)
+    // [] expression has the value of the argument.
     constructor.visitFieldInsn(PUTFIELD, classType.name.toInternalName, "expression", JvmType.Object.toDescriptor)
 
-    // Return
+    // [] Return nothing.
     constructor.visitInsn(RETURN)
 
     // Parameters of visit max are thrown away because visitor will calculate the frame and variable stack size
