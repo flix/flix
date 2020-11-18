a5b6bc804a1a8f2f706127856ce751cccf9a0fa9
JonathanStarup <32037926+JonathanStarup@users.noreply.github.com>
2020-11-18 11:10:12 +0100

    corrected copyright note

--- main/src/ca/uwaterloo/flix/language/phase/jvm/GenLazyClasses.scala
+++ main/src/ca/uwaterloo/flix/language/phase/jvm/GenLazyClasses.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Magnus Madsen
+ * Copyright 2020 Jonathan Lindegaard Starup
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
