615e93186cf01f9c8bf967d703e02df3188757cb
Esben Bjerre <hej@esbenbjerre.dk>
2020-12-10 19:31:02 +0100

    Add LazyList (#1442)

--- main/test/ca/uwaterloo/flix/library/LibrarySuite.scala
+++ main/test/ca/uwaterloo/flix/library/LibrarySuite.scala
@@ -49,6 +49,7 @@ class LibrarySuite extends Suites(
   new FlixTest("TestFromString", "main/test/ca/uwaterloo/flix/library/TestFromString.flix")(Options.TestWithLibrary),
   new FlixTest("TestFunctor", "main/test/ca/uwaterloo/flix/library/TestFunctor.flix")(Options.TestWithLibrary),
   new FlixTest("TestHash", "main/test/ca/uwaterloo/flix/library/TestHash.flix")(Options.TestWithLibrary),
+  new FlixTest("TestLazyList", "main/test/ca/uwaterloo/flix/library/TestLazyList.flix")(Options.TestWithLibrary),
 
   new FlixTest("Core/Io/TestFile", "main/test/ca/uwaterloo/flix/library/Core/Io/TestFile.flix")(Options.TestWithLibrary),
   new FlixTest("Core/Io/TestInputStream", "main/test/ca/uwaterloo/flix/library/Core/Io/TestInputStream.flix")(Options.TestWithLibrary),
