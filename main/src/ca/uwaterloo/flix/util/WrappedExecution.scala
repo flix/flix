28efe41ea415060be13e58dfb693a2f12b548562
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2021-01-08 14:11:05 +0100

    WIP

--- main/src/ca/uwaterloo/flix/util/WrappedExecution.scala
+++ main/src/ca/uwaterloo/flix/util/WrappedExecution.scala
@@ -15,8 +15,40 @@
  */
 package ca.uwaterloo.flix.util
 
+import java.io.{ByteArrayOutputStream, PrintStream}
+import java.nio.charset.StandardCharsets
+
+// TODO: Name
 object WrappedExecution {
 
-  def execute(): Unit = ??? // TODO: Set out and err. and return them.
+  private val UTF8: String = StandardCharsets.UTF_8.name()
+
+  // TODO: DOC
+  def execute[T](f: () => T): (T, String, String) = {
+    // Capture the original out and err streams.
+    val originalOut = System.out
+    val originalErr = System.err
+
+    // Construct new buffers for out and err.
+    val newOut = new ByteArrayOutputStream()
+    val newErr = new ByteArrayOutputStream()
+
+    // Set them.
+    System.setOut(new PrintStream(newOut, true, UTF8))
+    System.setErr(new PrintStream(newErr, true, UTF8))
+
+    // Execute the function f.
+    val result = f()
+
+    // Restore the original out and err streams.
+    System.setOut(originalOut)
+    System.setErr(originalErr)
+
+    // Return a triple of the result and the two captured streams as strings.
+    val capturedOut = newOut.toString(UTF8)
+    val capturedErr = newErr.toString(UTF8)
+
+    (result, capturedOut, capturedErr)
+  }
 
 }
