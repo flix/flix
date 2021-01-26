4a9aa3afce720bf0987ef636200357ac74689368
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2021-01-26 18:24:47 +0100

    Fixes #1506

--- main/src/ca/uwaterloo/flix/language/phase/Redundancy.scala
+++ main/src/ca/uwaterloo/flix/language/phase/Redundancy.scala
@@ -147,9 +147,9 @@ object Redundancy extends Phase[TypedAst.Root, TypedAst.Root] {
           case Some(usedTags) =>
             // Case 2: Enum is used and here are its used tags.
             // Check if there is any unused tag.
-            decl.cases.values.find(caze => !usedTags.contains(caze.tag)) match {
-              case None => acc
-              case Some(caze) => acc + UnusedEnumTag(sym, caze.tag)
+            decl.cases.foldLeft(acc) {
+              case (innerAcc, (tag, caze)) if deadTag(tag, caze, usedTags) => acc + UnusedEnumTag(sym, caze.tag)
+              case (innerAcc, _) => innerAcc
             }
         }
     }
@@ -679,6 +679,13 @@ object Redundancy extends Phase[TypedAst.Root, TypedAst.Root] {
       !decl.sym.name.startsWith("_") &&
       !used.defSyms.contains(decl.sym) &&
       !root.reachable.contains(decl.sym)
+
+  /**
+    * Returns `true` if the given tag `decl` is unused according to `used`.
+    */
+  private def deadTag(tag: Name.Tag, caze: Case, usedTags: Set[Name.Tag]): Boolean =
+    !tag.name.startsWith("_") &&
+      !usedTags.contains(tag)
 
   /**
     * Returns `true` if the type variable `tvar` is unused according to the argument `used`.
