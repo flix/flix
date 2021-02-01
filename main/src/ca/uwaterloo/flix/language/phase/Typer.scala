730c727a6a7e11f29d898f5b71f664f13b8da9cc
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2021-02-01 16:49:49 +0100

    WIP

--- main/src/ca/uwaterloo/flix/language/phase/Typer.scala
+++ main/src/ca/uwaterloo/flix/language/phase/Typer.scala
@@ -707,15 +707,12 @@ object Typer extends Phase[ResolvedAst.Root, TypedAst.Root] {
           case ResolvedAst.ChoiceRule(pat, _) => pat
         }
 
-        println()
-        println("--")
+        println("----------------")
         println()
         println(ChoiceMatch.toPrettyString(matrix))
         val saturated = ChoiceMatch.saturate(matrix)
-        println()
+        println(" ~")
         println(ChoiceMatch.toPrettyString(saturated))
-        println()
-        println("--")
         println()
 
         /**
