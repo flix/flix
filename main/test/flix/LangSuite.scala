03e26e1004fa5475a4e32082bde85cae9b39a1a0
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2021-03-18 10:25:36 +0100

    WIP

--- main/test/flix/LangSuite.scala
+++ main/test/flix/LangSuite.scala
@@ -314,7 +314,7 @@ class LangSuite extends Suites(
   //
   // Type Alias.
   //
-  new FlixTest("Test.TypeAlias.Rel", "main/test/flix/Test.TypeAlias.Rel.flix"),
+  new FlixTest("Test.TypeAlias.Rel", "main/test/flix/Test.TypeAlias.Rel.flix")(Options.TestWithLibrary),
 
   //
   // Unused.
