484ff37c3eccbd7db40235b602e3fb9dcbc126fa
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2021-03-30 15:59:39 +0200

    Add CapturedVar tests.

--- main/test/flix/LangSuite.scala
+++ main/test/flix/LangSuite.scala
@@ -306,9 +306,9 @@ class LangSuite extends Suites(
   new FlixTest("Test.Term.Lit.Option", "main/test/flix/Test.Term.Lit.Option.flix")(Options.TestWithLibrary),
   new FlixTest("Test.Term.Lit.Result", "main/test/flix/Test.Term.Lit.Result.flix")(Options.TestWithLibrary),
   new FlixTest("Test.Term.Lit.Set", "main/test/flix/Test.Term.Lit.Set.flix")(Options.TestWithLibrary),
-  new Ignore("Test.Term.Var.CapturedVar", "main/test/flix/Test.Term.Var.CapturedVar.flix")(Options.TestWithLibrary), // TODO
+  new FlixTest("Test.Term.Var.CapturedVar", "main/test/flix/Test.Term.Var.CapturedVar.flix")(Options.TestWithLibrary),
   new FlixTest("Test.Term.Var.QuantVar", "main/test/flix/Test.Term.Var.QuantVar.flix")(Options.TestWithLibrary),
-  new FlixTest("Test.Term.Var.WildVar", "main/test/flix/Test.Term.Var.WildVar.flix")(Options.TestWithLibrary), // TODO
+  new FlixTest("Test.Term.Var.WildVar", "main/test/flix/Test.Term.Var.WildVar.flix")(Options.TestWithLibrary),
 
   //
   // Type Alias.
