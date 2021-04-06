78ff1c7bbdd4015431d89871e06fa1ab36ebbfd8
Matthew Lutze <mcdlutze@gmail.com>
2021-04-06 12:04:21 +0200

    use FQN in FormatTypeConstraint

--- main/src/ca/uwaterloo/flix/language/debug/FormatTypeConstraint.scala
+++ main/src/ca/uwaterloo/flix/language/debug/FormatTypeConstraint.scala
@@ -26,6 +26,6 @@ object FormatTypeConstraint {
   def formatTypeConstraint(tconstr: Ast.TypeConstraint)(implicit audience: Audience): String = tconstr match {
     case Ast.TypeConstraint(sym, arg, _) =>
       val typeString = FormatType.formatType(arg)
-      s"${sym.name}[${typeString}]"
+      s"${sym}[${typeString}]"
   }
 }
