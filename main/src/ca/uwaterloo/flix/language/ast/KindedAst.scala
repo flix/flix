b0ebdb88534590904139cedc1464ed5da15a4448
Matthew Lutze <mcdlutze@gmail.com>
2021-04-09 11:30:58 +0200

    update copyright date

--- main/src/ca/uwaterloo/flix/language/ast/KindedAst.scala
+++ main/src/ca/uwaterloo/flix/language/ast/KindedAst.scala
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2017 Magnus Madsen
+ *  Copyright 2021 Matthew Lutze
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
