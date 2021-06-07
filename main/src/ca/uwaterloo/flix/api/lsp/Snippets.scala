e0cf66f4216cae8fbb64138e939711fd3473b01c
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2021-06-07 14:41:24 +0200

    WIP

--- main/src/ca/uwaterloo/flix/api/lsp/Snippets.scala
+++ main/src/ca/uwaterloo/flix/api/lsp/Snippets.scala
@@ -22,15 +22,16 @@ object Snippets {
   def lookupSnippets(root: TypedAst.Root): List[CompletionItem] = {
     // TODO: Experiment with lookup.
     val result1 = if (root == null) Nil else {
-      val listDefs = root.defs.filter(_._1.namespace == List("List"))
+      // TODO: Cleanup
+      val listDefs = root.defs.filter(kv => kv._1.namespace == List("List") && kv._2.spec.mod.isPublic)
       listDefs.map {
         case (_, defn) =>
           val label = defn.sym.name
-          val insertText = defn.sym.name
-          val detail = Some(defn.spec.doc.text)
-          CompletionItem(label, insertText, detail, InsertTextFormat.PlainText)
+          val insertText = defInsertText(defn)
+          val detail = Some(defn.spec.doc.text.stripLeading())
+          CompletionItem(label, insertText, detail, InsertTextFormat.Snippet)
       }
-    }.take(10).toList
+    }.toList
 
     // TODO: Experiment with snippets
     val result2 = List(
@@ -41,6 +42,15 @@ object Snippets {
       CompletionItem("query", "query ${1:db} select ${2:cols} from ${3:preds} ${4:where ${5:cond}}", None, InsertTextFormat.Snippet),
     )
     result2 ::: result1
+  }
+
+  // TODO: DOC
+  private def defInsertText(defn: TypedAst.Def): String = {
+    val prefix = defn.sym.text
+    val args = defn.spec.fparams.zipWithIndex.map {
+      case (fparam, idx) => "$" + s"{${idx + 1}:${fparam.sym.text}}"
+    }
+    s"${prefix}(${args.mkString(", ")})"
   }
 
 }
