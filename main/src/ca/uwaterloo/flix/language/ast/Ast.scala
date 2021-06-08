6268a0771fa7f5ca6d375ee1020706a984d82402
Matthew Lutze <mcdlutze@gmail.com>
2021-06-08 12:21:38 +0200

    add returnType metadata to TypedAst.Spec

--- main/src/ca/uwaterloo/flix/language/ast/Ast.scala
+++ main/src/ca/uwaterloo/flix/language/ast/Ast.scala
@@ -446,4 +446,9 @@ object Ast {
     */
   case class ClassContext(superClasses: List[Symbol.ClassSym], instances: List[Ast.Instance])
 
+  /**
+    * Marks a value as metadata: the compiler must not rely on this information.
+    */
+  case class Meta[T](value: T)
+
 }
