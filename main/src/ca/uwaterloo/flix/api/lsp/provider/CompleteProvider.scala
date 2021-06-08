34aa6540a6790d8e3abfeead6537156b7041a816
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2021-06-08 08:37:16 +0200

    WIP

--- main/src/ca/uwaterloo/flix/api/lsp/provider/CompleteProvider.scala
+++ main/src/ca/uwaterloo/flix/api/lsp/provider/CompleteProvider.scala
@@ -9,25 +9,20 @@ object CompleteProvider {
     * Returns a list of auto-complete suggestions.
     */
   def autoComplete(uri: String, pos: Position, prefix: String, root: TypedAst.Root): List[CompletionItem] = {
-    val result2 = List(
-      CompletionItem("foo", "fooooooo", Some("This is a foo suggestion."), CompletionItemKind.Module, InsertTextFormat.PlainText, Nil),
-    )
-
-    result2 ::: getSnippetCompletionItems() ::: getKeywordCompletionItems() ::: getOtherCompletionItems(root)
+    getKeywordCompletionItems() ::: getSnippetCompletionItems() ::: getSuggestions(root)
   }
-
 
   /**
     * Returns a list of keyword completion items.
     */
   private def getKeywordCompletionItems(): List[CompletionItem] = List(
-    // TODO: Add more.
+    // TODO: Manoj: Add more.
     // NB: Please keep the list alphabetically sorted.
 
-    // Keywords
+    // Keywords:
     CompletionItem("namespace", "namespace", None, CompletionItemKind.Keyword, InsertTextFormat.PlainText, List("{", "}")),
 
-    // Keyword-like names (e.g. built-in functions).
+    // Keyword-like (e.g. println):
     CompletionItem("println", "println", None, CompletionItemKind.Keyword, InsertTextFormat.PlainText, List("(", ")")),
   )
 
@@ -35,8 +30,12 @@ object CompleteProvider {
     * Returns a list of snippet completion items.
     */
   private def getSnippetCompletionItems(): List[CompletionItem] = List(
-    // TODO: Add more.
+    // TODO: Manoj: Add more.
     // NB: Please keep the list alphabetically sorted.
+
+    // Declaration-based:
+
+    // Expressed-based:
     CompletionItem("match", "match ${1:exp} {\n case ${2:pat} => ${3:exp}\n}", None, CompletionItemKind.Snippet, InsertTextFormat.Snippet, Nil),
     CompletionItem("query", "query ${1:db} select ${2:cols} from ${3:preds} ${4:where ${5:cond}}", None, CompletionItemKind.Snippet, InsertTextFormat.Snippet, Nil),
   )
@@ -44,7 +43,7 @@ object CompleteProvider {
   /**
     * Returns a list of other completion items.
     */
-  private def getOtherCompletionItems(root: TypedAst.Root): List[CompletionItem] = {
+  private def getSuggestions(root: TypedAst.Root): List[CompletionItem] = {
     // TODO: Magnus
 
     val result1 = if (root == null) Nil else {
