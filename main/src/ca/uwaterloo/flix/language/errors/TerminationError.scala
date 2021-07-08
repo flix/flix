5538e7e4a08f4d5dd1a7795ea5be93375952c062
Matthew Lutze <mcdlutze@gmail.com>
2021-07-08 18:39:28 +0200

    add some docs to TerminationError

--- main/src/ca/uwaterloo/flix/language/errors/TerminationError.scala
+++ main/src/ca/uwaterloo/flix/language/errors/TerminationError.scala
@@ -1,11 +1,27 @@
+/*
+ * Copyright 2021 Matthew Lutze
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package ca.uwaterloo.flix.language.errors
 
 import ca.uwaterloo.flix.language.CompilationError
 import ca.uwaterloo.flix.language.ast.{SourceLocation, Symbol}
 import ca.uwaterloo.flix.util.vt.VirtualTerminal
 
-// MATT license
-// MATT docs
+/**
+  * A common super-type for termination errors.
+  */
 sealed trait TerminationError extends CompilationError {
   override def kind: String = "Termination Error"
 }
