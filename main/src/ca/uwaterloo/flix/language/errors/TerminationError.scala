e830e1deddcdd6d163a233a1938e533ebc8b05ba
Matthew Lutze <mcdlutze@gmail.com>
2021-07-08 15:07:39 +0200

    implement basic termination check

---
+++ main/src/ca/uwaterloo/flix/language/errors/TerminationError.scala
@@ -0,0 +1,19 @@
+package ca.uwaterloo.flix.language.errors
+
+import ca.uwaterloo.flix.language.CompilationError
+import ca.uwaterloo.flix.language.ast.{SourceLocation, Symbol}
+import ca.uwaterloo.flix.util.vt.VirtualTerminal
+
+// MATT license
+// MATT docs
+sealed trait TerminationError extends CompilationError {
+  override def kind: String = "Termination Error"
+}
+
+object TerminationError {
+  case class UnconditionalDefRecursion(defn: Symbol.DefnSym, loc: SourceLocation) extends TerminationError {
+    override def summary: String = "" // MATT
+
+    override def message: VirtualTerminal = new VirtualTerminal() // MATT
+  }
+}
