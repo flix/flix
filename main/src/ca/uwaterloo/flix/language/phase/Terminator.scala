f521c8ae0e0bd0e5d7c3e5f3caa2dd29d9c21130
Matthew Lutze <mcdlutze@gmail.com>
2021-07-08 15:21:36 +0200

    add licenses and add terminator test to PhaseSuite

--- main/src/ca/uwaterloo/flix/language/phase/Terminator.scala
+++ main/src/ca/uwaterloo/flix/language/phase/Terminator.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2021 Matthew Lutze
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package ca.uwaterloo.flix.language.phase
 
 import ca.uwaterloo.flix.api.Flix
@@ -7,7 +23,6 @@ import ca.uwaterloo.flix.language.errors.TerminationError
 import ca.uwaterloo.flix.util.Validation
 import ca.uwaterloo.flix.util.Validation.{ToFailure, ToSuccess}
 
-// MATT license
 // MATT docs
 object Terminator extends Phase[Root, Root] {
 
