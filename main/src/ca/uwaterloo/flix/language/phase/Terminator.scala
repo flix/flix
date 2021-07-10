959372899d361372c48fccc9b28202683dd800ff
Matthew Lutze <mcdlutze@gmail.com>
2021-07-10 11:13:09 +0200

    remove sig checks from terminator

--- main/src/ca/uwaterloo/flix/language/phase/Terminator.scala
+++ main/src/ca/uwaterloo/flix/language/phase/Terminator.scala
@@ -30,10 +30,7 @@ object Terminator extends Phase[Root, Root] {
   override def run(root: Root)(implicit flix: Flix): Validation[Root, TerminationError] = flix.phase("Terminator") {
     val defVal = Validation.traverseX(root.defs.values)(checkDef)
 
-    // val sigVal = Validation.traverseX(root.sigs.values)(checkSig)
-    // Sig check is currently too strict
-
-    Validation.sequenceX(defVal :: Nil) map {
+    defVal map {
       _ => root
     }
   }
@@ -42,126 +39,95 @@ object Terminator extends Phase[Root, Root] {
     * Returns an error if the `defn` recurses unconditionally.
     */
   def checkDef(defn: Def): Validation[Unit, TerminationError] = {
-    if (unconditionallyRecurses(defn.impl.exp, isRecursiveDefApp(defn, _))) {
-      TerminationError.UnconditionalDefRecursion(defn.sym).toFailure
+    if (unconditionallyRecurses(defn.impl.exp, defn)) {
+      TerminationError.UnconditionalRecursion(defn.sym).toFailure
     } else {
       ().toSuccess
     }
   }
 
   /**
-    * Returns an error if the `sig` recurses unconditionally.
-    */
-  def checkSig(sig: Sig): Validation[Unit, TerminationError] = {
-    sig.impl match {
-      case Some(impl) =>
-        if (unconditionallyRecurses(impl.exp, isRecursiveSigApp(sig, _))) {
-          TerminationError.UnconditionalSigRecursion(sig.sym).toFailure
-        } else {
-          ().toSuccess
-        }
-      case None => ().toSuccess
-    }
-  }
-
-  /**
-    * Returns true if `app` is a recursive application of `defn`.
-    */
-  def isRecursiveDefApp(defn: Def, app: Expression.Apply): Boolean = app match {
-    case Expression.Apply(Expression.Def(sym, _, _), exps, _, _, _) =>
-      defn.sym == sym && exps.length >= defn.spec.fparams.length
-    case _ => false
-  }
-
-  /**
-    * Returns true if `app` is a recursive application of `sig`.
-    */
-  def isRecursiveSigApp(sig: Sig, app: Expression.Apply): Boolean = app match {
-    case Expression.Apply(Expression.Sig(sym, _, _), exps, _, _, _) =>
-      sig.sym == sym && exps.length >= sig.spec.fparams.length
-    case _ => false
-  }
-
-  /**
     * Returns true if `exp00` unconditionally recurses, according to `recursiveAppCheck`.
     */
-  def unconditionallyRecurses(exp00: Expression, recursiveAppCheck: Expression.Apply => Boolean): Boolean = {
+  def unconditionallyRecurses(exp00: Expression, defn: Def): Boolean = {
     def visit(exp0: Expression): Boolean = exp0 match {
-      case Expression.Unit(loc) => false
-      case Expression.Null(tpe, loc) => false
-      case Expression.True(loc) => false
-      case Expression.False(loc) => false
-      case Expression.Char(lit, loc) => false
-      case Expression.Float32(lit, loc) => false
-      case Expression.Float64(lit, loc) => false
-      case Expression.Int8(lit, loc) => false
-      case Expression.Int16(lit, loc) => false
-      case Expression.Int32(lit, loc) => false
-      case Expression.Int64(lit, loc) => false
-      case Expression.BigInt(lit, loc) => false
-      case Expression.Str(lit, loc) => false
-      case Expression.Default(tpe, loc) => false
-      case Expression.Wild(tpe, loc) => false
-      case Expression.Var(sym, tpe, loc) => false
-      case Expression.Def(sym, tpe, loc) => false
-      case Expression.Sig(sym, tpe, loc) => false
-      case Expression.Hole(sym, tpe, eff, loc) => false
-      case Expression.Lambda(fparam, exp, tpe, loc) => false
+      case Expression.Unit(_) => false
+      case Expression.Null(_, _) => false
+      case Expression.True(_) => false
+      case Expression.False(_) => false
+      case Expression.Char(_, _) => false
+      case Expression.Float32(_, _) => false
+      case Expression.Float64(_, _) => false
+      case Expression.Int8(_, _) => false
+      case Expression.Int16(_, _) => false
+      case Expression.Int32(_, _) => false
+      case Expression.Int64(_, _) => false
+      case Expression.BigInt(_, _) => false
+      case Expression.Str(_, _) => false
+      case Expression.Default(_, _) => false
+      case Expression.Wild(_, _) => false
+      case Expression.Var(_, _, _) => false
+      case Expression.Def(_, _, _) => false
+      case Expression.Sig(_, _, _) => false
+      case Expression.Hole(_, _, _, _) => false
+      case Expression.Lambda(_, _, _, _) => false
 
       // If the Apply is a recursive call, then return true
-      case app: Expression.Apply if recursiveAppCheck(app) => true
+      case Expression.Apply(Expression.Def(sym, _, _), exps, _, _, _) if (defn.sym == sym && exps.length >= defn.spec.fparams.length) => true
       // Otherwise continue as normal
-      case Expression.Apply(exp, exps, tpe, eff, loc) => visit(exp) || exps.exists(visit)
+      case Expression.Apply(exp, exps, _, _, _) => visit(exp) || exps.exists(visit)
 
-      case Expression.Unary(sop, exp, tpe, eff, loc) => visit(exp)
-      case Expression.Binary(sop, exp1, exp2, tpe, eff, loc) => visit(exp1) || visit(exp2)
-      case Expression.Let(sym, exp1, exp2, tpe, eff, loc) => visit(exp1) || visit(exp2)
-      case Expression.LetRegion(sym, exp, tpe, eff, loc) => visit(exp)
-      case Expression.IfThenElse(exp1, exp2, exp3, tpe, eff, loc) => visit(exp1) || (visit(exp2) && visit(exp3))
-      case Expression.Stm(exp1, exp2, tpe, eff, loc) => visit(exp1) || visit(exp2)
-      case Expression.Match(exp, rules, tpe, eff, loc) => visit(exp) || rules.forall { case MatchRule(_, guard, exp1) => visit(guard) || visit(exp1) } // MATT docs
-      case Expression.Choose(exps, rules, tpe, eff, loc) => exps.exists(visit) || rules.forall { rule => visit(rule.exp) }
-      case Expression.Tag(sym, tag, exp, tpe, eff, loc) => visit(exp)
-      case Expression.Tuple(elms, tpe, eff, loc) => elms.exists(visit)
-      case Expression.RecordEmpty(tpe, loc) => false
-      case Expression.RecordSelect(exp, field, tpe, eff, loc) => visit(exp)
-      case Expression.RecordExtend(field, value, rest, tpe, eff, loc) => visit(value) || visit(rest)
-      case Expression.RecordRestrict(field, rest, tpe, eff, loc) => visit(rest)
-      case Expression.ArrayLit(elms, tpe, eff, loc) => elms.exists(visit)
-      case Expression.ArrayNew(elm, len, tpe, eff, loc) => visit(elm) || visit(len)
-      case Expression.ArrayLoad(base, index, tpe, eff, loc) => visit(base) || visit(index)
-      case Expression.ArrayLength(base, eff, loc) => visit(base)
-      case Expression.ArrayStore(base, index, elm, loc) => visit(base) || visit(index) || visit(elm)
-      case Expression.ArraySlice(base, beginIndex, endIndex, tpe, loc) => visit(base) || visit(beginIndex) || visit(endIndex)
-      case Expression.Ref(exp, tpe, eff, loc) => visit(exp)
-      case Expression.Deref(exp, tpe, eff, loc) => visit(exp)
-      case Expression.Assign(exp1, exp2, tpe, eff, loc) => visit(exp1) || visit(exp2)
-      case Expression.Existential(fparam, exp, loc) => visit(exp)
-      case Expression.Universal(fparam, exp, loc) => visit(exp)
-      case Expression.Ascribe(exp, tpe, eff, loc) => visit(exp)
-      case Expression.Cast(exp, tpe, eff, loc) => visit(exp)
-      case Expression.TryCatch(exp, rules, tpe, eff, loc) => visit(exp) || rules.forall { rule => visit(rule.exp) }
-      case Expression.InvokeConstructor(constructor, args, tpe, eff, loc) => args.exists(visit)
-      case Expression.InvokeMethod(method, exp, args, tpe, eff, loc) => visit(exp) || args.exists(visit)
-      case Expression.InvokeStaticMethod(method, args, tpe, eff, loc) => args.exists(visit)
-      case Expression.GetField(field, exp, tpe, eff, loc) => visit(exp)
-      case Expression.PutField(field, exp1, exp2, tpe, eff, loc) => visit(exp1) || visit(exp2)
-      case Expression.GetStaticField(field, tpe, eff, loc) => false
-      case Expression.PutStaticField(field, exp, tpe, eff, loc) => visit(exp)
-      case Expression.NewChannel(exp, tpe, eff, loc) => visit(exp)
-      case Expression.GetChannel(exp, tpe, eff, loc) => visit(exp)
-      case Expression.PutChannel(exp1, exp2, tpe, eff, loc) => visit(exp1) || visit(exp2)
-      case Expression.SelectChannel(rules, default, tpe, eff, loc) => rules.forall { rule => visit(rule.exp) || visit(rule.chan) } && default.forall(visit)
-      case Expression.Spawn(exp, tpe, eff, loc) => visit(exp)
-      case Expression.Lazy(exp, tpe, loc) => false
-      case Expression.Force(exp, tpe, eff, loc) => visit(exp)
-      case Expression.FixpointConstraintSet(cs, stf, tpe, loc) => false
-      case Expression.FixpointMerge(exp1, exp2, stf, tpe, eff, loc) => visit(exp1) || visit(exp2)
-      case Expression.FixpointSolve(exp, stf, tpe, eff, loc) => visit(exp)
-      case Expression.FixpointFilter(pred, exp, tpe, eff, loc) => visit(exp)
-      case Expression.FixpointProjectIn(exp, pred, tpe, eff, loc) => visit(exp)
-      case Expression.FixpointProjectOut(pred, exp, tpe, eff, loc) => visit(exp)
-      case Expression.MatchEff(exp1, exp2, exp3, tpe, eff, loc) => visit(exp1) || (visit(exp2) || visit(exp3))
+      case Expression.Unary(_, exp, _, _, _) => visit(exp)
+      case Expression.Binary(_, exp1, exp2, _, _, _) => visit(exp1) || visit(exp2)
+      case Expression.Let(_, exp1, exp2, _, _, _) => visit(exp1) || visit(exp2)
+      case Expression.LetRegion(_, exp, _, _, _) => visit(exp)
+      case Expression.IfThenElse(exp1, exp2, exp3, _, _, _) => visit(exp1) || (visit(exp2) && visit(exp3))
+      case Expression.Stm(exp1, exp2, _, _, _) => visit(exp1) || visit(exp2)
+      case Expression.Match(exp, rules, _, _, _) => visit(exp) || rules.forall { match0 => visit(match0.guard) || visit(match0.exp) }
+      case Expression.Choose(exps, rules, _, _, _) => exps.exists(visit) || rules.forall { rule => visit(rule.exp) }
+      case Expression.Tag(_, _, exp, _, _, _) => visit(exp)
+      case Expression.Tuple(elms, _, _, _) => elms.exists(visit)
+      case Expression.RecordEmpty(_, _) => false
+      case Expression.RecordSelect(exp, _, _, _, _) => visit(exp)
+      case Expression.RecordExtend(_, value, rest, _, _, _) => visit(value) || visit(rest)
+      case Expression.RecordRestrict(_, rest, _, _, _) => visit(rest)
+      case Expression.ArrayLit(elms, _, _, _) => elms.exists(visit)
+      case Expression.ArrayNew(elm, len, _, _, _) => visit(elm) || visit(len)
+      case Expression.ArrayLoad(base, index, _, _, _) => visit(base) || visit(index)
+      case Expression.ArrayLength(base, _, _) => visit(base)
+      case Expression.ArrayStore(base, index, elm, _) => visit(base) || visit(index) || visit(elm)
+      case Expression.ArraySlice(base, beginIndex, endIndex, _, _) => visit(base) || visit(beginIndex) || visit(endIndex)
+      case Expression.Ref(exp, _, _, _) => visit(exp)
+      case Expression.Deref(exp, _, _, _) => visit(exp)
+      case Expression.Assign(exp1, exp2, _, _, _) => visit(exp1) || visit(exp2)
+      case Expression.Existential(_, exp, _) => visit(exp)
+      case Expression.Universal(_, exp, _) => visit(exp)
+      case Expression.Ascribe(exp, _, _, _) => visit(exp)
+      case Expression.Cast(exp, _, _, _) => visit(exp)
+      case Expression.TryCatch(exp, rules, _, _, _) => visit(exp) || rules.forall { rule => visit(rule.exp) }
+      case Expression.InvokeConstructor(_, args, _, _, _) => args.exists(visit)
+      case Expression.InvokeMethod(_, exp, args, _, _, _) => visit(exp) || args.exists(visit)
+      case Expression.InvokeStaticMethod(_, args, _, _, _) => args.exists(visit)
+      case Expression.GetField(_, exp, _, _, _) => visit(exp)
+      case Expression.PutField(_, exp1, exp2, _, _, _) => visit(exp1) || visit(exp2)
+      case Expression.GetStaticField(_, _, _, _) => false
+      case Expression.PutStaticField(_, exp, _, _, _) => visit(exp)
+      case Expression.NewChannel(exp, _, _, _) => visit(exp)
+      case Expression.GetChannel(exp, _, _, _) => visit(exp)
+      case Expression.PutChannel(exp1, exp2, _, _, _) => visit(exp1) || visit(exp2)
+      case Expression.SelectChannel(rules, default, _, _, _) => rules.forall { rule => visit(rule.exp) || visit(rule.chan) } && default.forall(visit)
+      case Expression.Spawn(exp, _, _, _) => visit(exp)
+
+      case Expression.Lazy(_, _, _) => false // Lazily evaluated expressions terminate
+
+      case Expression.Force(exp, _, _, _) => visit(exp)
+      case Expression.FixpointConstraintSet(_, _, _, _) => false
+      case Expression.FixpointMerge(exp1, exp2, _, _, _, _) => visit(exp1) || visit(exp2)
+      case Expression.FixpointSolve(exp, _, _, _, _) => visit(exp)
+      case Expression.FixpointFilter(_, exp, _, _, _) => visit(exp)
+      case Expression.FixpointProjectIn(exp, _, _, _, _) => visit(exp)
+      case Expression.FixpointProjectOut(_, exp, _, _, _) => visit(exp)
+      case Expression.MatchEff(exp1, exp2, exp3, _, _, _) => visit(exp1) || (visit(exp2) || visit(exp3))
     }
 
     visit(exp00)
