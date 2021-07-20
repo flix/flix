0872b4bd97f8e9994d7c42fc570fbdc80bc14d50
Matthew Lutze <mcdlutze@gmail.com>
2021-07-20 10:56:30 +0200

    update compare_branches.py to use json output

--- .github/workflows/compare_branches.py
+++ .github/workflows/compare_branches.py
@@ -3,60 +3,67 @@
 # args: <master throughput> <master phases> <feature throughput> <feature phases>
 
 import sys
+import json
 
 PERCENT_FORMAT = "{:+.1f}%"
 ABSOLUTE_FORMAT = "{:+d}"
+NA = "-"
+
 
 # reads the throughput from the file
 def parse_throughput(throughput_file):
     with open(throughput_file) as f:
-        line = f.readline()
-        _, throughput_string = [field.strip() for field in line.split(",")]
-        return int(throughput_string)
-        
+        data = json.load(f)
+        return data["throughput"]
+
+
 # reads the phases file and creates a dictionary phase -> phase time
 def parse_phases(phases_file):
     with open(phases_file) as f:
         phases = {}
-        for line in f:
-            phase, _, time_string = [field.strip() for field in line.split(",")]
-            time = int(time_string)
-            phases[phase] = time
+        data = json.load(f)
+        phase_list = data["phases"]
+        for phase in phase_list:
+            name = phase["phase"]
+            time = phase["time"]
+            phases[name] = time
         return phases
 
-# creates a dictionary phase -> (master time, feature time, absolute increase, percent increase)
-# all values are strings
-def build_phase_comparison(master_phases, feature_phases):
+
+def build_comparison(master, feature):
     table = {}
-    for phase in master_phases:
-        master_time = master_phases[phase]
-        feature_time = feature_phases[phase]
-        absolute_increase = feature_time - master_time
-        percent_increase = absolute_increase / master_time * 100
-        table[phase] = (str(master_time), str(feature_time), ABSOLUTE_FORMAT.format(absolute_increase), PERCENT_FORMAT.format(percent_increase))
+    stats = set(master.keys()).union(set(feature.keys()))
+    for stat in stats:
+        master_stat = master.get(stat)
+        feature_stat = feature.get(stat)
+        master_str = NA if master_stat is None else str(master_stat)
+        feature_str = NA if feature_stat is None else str(feature_stat)
+        if master_stat is None or feature_stat is None:
+            abs_incr_str = NA
+            perc_incr_str = NA
+        else:
+            abs_incr = feature_stat - master_stat
+            perc_incr = abs_incr / master_stat * 100
+            abs_incr_str = ABSOLUTE_FORMAT.format(abs_incr)
+            perc_incr_str = PERCENT_FORMAT.format(perc_incr)
+        table[stat] = (master_str, feature_str, abs_incr_str, perc_incr_str)
     return table
 
-# creats a tuple (master throughput, feature throughput, absolute increase, percent increase)
-def build_throughput_comparison(master_throughput, feature_throughput):
-    absolute_increase = feature_throughput - master_throughput
-    percent_increase = absolute_increase / master_throughput * 100
-    return (str(master_throughput), str(feature_throughput), ABSOLUTE_FORMAT.format(absolute_increase), PERCENT_FORMAT.format(percent_increase))
 
 def print_table_line(fields):
     print(" | ".join(fields))
+
 
 def print_table_header(fields):
     print_table_line(fields)
     print_table_line("---" for f in fields)
 
-def print_phase_table(table):
-    print_table_header(("phase", "master time", "feature time", "absolute difference", "percent difference"))
-    for phase in table:
-        print_table_line((phase,) + table[phase])
 
-def print_throughput_table(table):
-    print_table_header(("master throughput", "feature throughput", "absolute difference", "percent difference"))
-    print_table_line(table)
+def print_table(table, unit, category):
+    print_table_header((category, f"master {unit}", f"feature {unit}", "absolute difference", "percent difference"))
+    for stat in table:
+        print_table_line((stat,) + table[stat])
+
 
 def main():
     _, master_throughput_file, master_phases_file, feature_throughput_file, feature_phases_file = sys.argv
@@ -65,12 +72,13 @@ def main():
     master_phases = parse_phases(master_phases_file)
     feature_phases = parse_phases(feature_phases_file)
     
-    throughput_comparison = build_throughput_comparison(master_throughput, feature_throughput)
-    phase_comparison = build_phase_comparison(master_phases, feature_phases)
+    throughput_comparison = build_comparison(master_throughput, feature_throughput)
+    phase_comparison = build_comparison(master_phases, feature_phases)
 
-    print_throughput_table(throughput_comparison)
+    print_table(throughput_comparison, "throughput", "stat")
     print()
-    print_phase_table(phase_comparison)
+    print_table(phase_comparison, "time", "phase")
+
 
 if __name__ == "__main__":
     main()
