23a62d8088e3c4d4cc871c84b3fa09dde415db72
Matthew Lutze <mlutze@users.noreply.github.com>
2021-07-30 12:14:40 +0200

    remove KindSubst artifacts

--- main/src/ca/uwaterloo/flix/language/phase/unification/KindUnificationError.scala
+++
@@ -1,9 +0,0 @@
-package ca.uwaterloo.flix.language.phase.unification
-
-import ca.uwaterloo.flix.language.ast.Kind
-
-sealed trait KindUnificationError
-
-object KindUnificationError {
-  case class MismatchedKinds(k1: Kind, k2: Kind) extends KindUnificationError
-}
