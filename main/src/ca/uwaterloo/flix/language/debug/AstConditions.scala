834adbd76bbed1a5a67800abd59c8298c2be6fcb
Matthew Lutze <mlutze@users.noreply.github.com>
2021-08-14 12:36:53 +0200

    add license

--- main/src/ca/uwaterloo/flix/language/debug/AstConditions.scala
+++ main/src/ca/uwaterloo/flix/language/debug/AstConditions.scala
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 Matthew Lutze
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package ca.uwaterloo.flix.language.debug
 
 import ca.uwaterloo.flix.language.ast.Ast.{EliminatedBy, IntroducedBy}
@@ -6,6 +21,9 @@ import ca.uwaterloo.flix.util.InternalCompilerException
 
 import scala.reflect.runtime.universe
 
+/**
+  * Verifies that annotations describing conditions on the AST are fulfilled.
+  */
 object AstConditions {
 
   /**
