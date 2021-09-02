47d94355a2630d2e94d55e9a67cf5bcb6d90ee40
JonathanStarup <32037926+JonathanStarup@users.noreply.github.com>
2021-09-02 13:58:10 +0200

    changed spawn type to tpe (Unit)

--- main/src/ca/uwaterloo/flix/language/phase/Simplifier.scala
+++ main/src/ca/uwaterloo/flix/language/phase/Simplifier.scala
@@ -272,7 +272,7 @@ object Simplifier extends Phase[TypedAst.Root, SimplifiedAst.Root] {
         val e = visitExp(exp)
         val lambdaTyp = Type.mkArrowWithEffect(Type.Unit, eff, e.tpe, loc)
         val lambdaExp = SimplifiedAst.Expression.Lambda(List(), e, lambdaTyp, loc)
-        SimplifiedAst.Expression.Spawn(lambdaExp, lambdaTyp, loc)
+        SimplifiedAst.Expression.Spawn(lambdaExp, tpe, loc)
 
       case TypedAst.Expression.Lazy(exp, tpe, loc) =>
         // Wrap the expression in a closure: () -> tpe & Pure
