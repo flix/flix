933568f83f2b36d4644d00c36ffa10eceda5a077
Nicola Dardanis <nicdard@gmail.com>
2021-09-04 17:41:57 +0200

    Bootstrap DocumentSymbol support in LSP (#2051)

---
+++ main/src/ca/uwaterloo/flix/api/lsp/SymbolTag.scala
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2021 Nicola Dardanis
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ca.uwaterloo.flix.api.lsp
+
+import org.json4s.JsonAST.{JInt, JValue}
+
+/**
+  * Represents a `SymbolTag` in LSP.
+  */
+trait SymbolTag {
+  def toJSON: JValue = this match {
+    case SymbolTag.Deprecated => JInt(1)
+  }
+}
+
+object SymbolTag {
+  case object Deprecated extends SymbolTag
+}
