9d24a80942fc409db0509fd77bfcbd969d918926
JonathanStarup <32037926+JonathanStarup@users.noreply.github.com>
2021-09-08 13:33:04 +0200

    undid changes to safetyError

--- main/src/ca/uwaterloo/flix/language/errors/SafetyError.scala
+++ main/src/ca/uwaterloo/flix/language/errors/SafetyError.scala
@@ -6,21 +6,18 @@ import ca.uwaterloo.flix.util.vt.VirtualString._
 import ca.uwaterloo.flix.util.vt.VirtualTerminal
 
 /**
- * A common super-type for safety errors.
- */
-sealed trait SafetyError extends CompilationError {
-  def kind: String = "Safety Error"
-}
+  * A common super-type for safety errors.
+  */
+sealed trait SafetyError extends CompilationError
 
 object SafetyError {
 
   /**
-   * An error raised to indicate an illegal use of a non-positively bound variable in a negative atom.
-   */
+    * An error raised to indicate an illegal use of a non-positively bound variable in a negative atom.
+    */
   case class IllegalNonPositivelyBoundVariable(sym: Symbol.VarSym, loc: SourceLocation) extends SafetyError {
-
+    def kind: String = "Safety Error"
     def summary: String = s"Illegal non-positively bound variable '$sym'."
-
     def message: VirtualTerminal = {
       val vt = new VirtualTerminal
       vt << Line(kind, source.format) << NewLine
