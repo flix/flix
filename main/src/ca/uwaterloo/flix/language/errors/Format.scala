69aed8cee7c946f0b1b1fbfc0ea2a2ff505a30f4
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2021-10-22 11:14:03 +0200

    Add `Format`

---
+++ main/src/ca/uwaterloo/flix/language/errors/Format.scala
@@ -0,0 +1,33 @@
+package ca.uwaterloo.flix.language.errors
+
+import ca.uwaterloo.flix.language.ast.SourceLocation
+
+object Format {
+
+  def line(left: String, right: String): String = ""
+
+  def code(loc: SourceLocation, text: String): String = ""
+
+  def text(s: String): String = ""
+
+  def black(s: String): String = ""
+
+  def blue(s: String): String = ""
+
+  def cyan(s: String): String = ""
+
+  def green(s: String): String = ""
+
+  def magenta(s: String): String = ""
+
+  def red(s: String): String = ""
+
+  def yellow(s: String): String = ""
+
+  def white(s: String): String = ""
+
+  def bold(s: String): String = ""
+
+  def underline(s: String): String = ""
+
+}
