815c32aa03ef33175a88641a658984d5f38769bd
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2021-10-22 12:52:37 +0200

    WIP

--- main/src/ca/uwaterloo/flix/api/doc/Api.d.ts
+++ main/src/ca/uwaterloo/flix/api/doc/Api.d.ts
@@ -1,8 +1,34 @@
 interface Api {
-    classes: [Class];
+    classes: Classes
+    enums: [Enum]
+}
+
+type Classes = {
+    [key: string]: Class
 }
 
 interface Class {
-    name: String,
+    sym: ClassSym
     doc: [String]
+    mod: [Modifier]
+    tparam: TypeParam
+    //  superClasses: List[Ast.TypeConstraint], signatures: List[TypedAst.Sig], laws: List[TypedAst.Def], loc: SourceLocation
 }
+
+interface Enum {
+
+}
+
+interface ClassSym {
+    namespace: [String]
+    name: String
+}
+
+interface TypeParam {
+    name: String,
+    kind: Kind
+}
+
+type Kind = "Bool" | "Star"
+
+type Modifier = "public"
