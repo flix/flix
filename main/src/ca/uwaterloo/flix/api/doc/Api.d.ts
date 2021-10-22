f9a640b83a7cf4aa70ca2eb120829998f0f3eb0e
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2021-10-22 12:58:06 +0200

    WIP

--- main/src/ca/uwaterloo/flix/api/doc/Api.d.ts
+++ main/src/ca/uwaterloo/flix/api/doc/Api.d.ts
@@ -1,11 +1,13 @@
 interface Api {
-    classes: Classes
+    classes: ClassesByNS
     enums: [Enum]
 }
 
-type Classes = {
-    [key: string]: Class
+type ClassesByNS = {
+    [key: string]: Class // TODO: What should key be?
 }
+
+
 
 interface Class {
     sym: ClassSym
@@ -15,6 +17,16 @@ interface Class {
     //  superClasses: List[Ast.TypeConstraint], signatures: List[TypedAst.Sig], laws: List[TypedAst.Def], loc: SourceLocation
 }
 
+//   case class Instance(doc: Ast.Doc, mod: Ast.Modifiers, sym: Symbol.ClassSym, tpe: Type, tconstrs: List[Ast.TypeConstraint], defs: List[TypedAst.Def], ns: Name.NName, loc: SourceLocation)
+
+//   case class Sig(sym: Symbol.SigSym, spec: TypedAst.Spec, impl: Option[TypedAst.Impl])
+
+//   case class Def(sym: Symbol.DefnSym, spec: TypedAst.Spec, impl: TypedAst.Impl)
+
+//   case class Enum(doc: Ast.Doc, mod: Ast.Modifiers, sym: Symbol.EnumSym, tparams: List[TypedAst.TypeParam], cases: Map[Name.Tag, TypedAst.Case], tpeDeprecated: Type, sc: Scheme, loc: SourceLocation)
+
+//   case class TypeAlias(doc: Ast.Doc, mod: Ast.Modifiers, sym: Symbol.TypeAliasSym, tparams: List[TypedAst.TypeParam], tpe: Type, loc: SourceLocation)
+
 interface Enum {
 
 }
@@ -23,6 +35,14 @@ interface ClassSym {
     namespace: [String]
     name: String
 }
+
+//   final class DefnSym(val id: Option[Int], val namespace: List[String], val text: String, val loc: SourceLocation) {
+
+//   final class EnumSym(val namespace: List[String], val name: String, val loc: SourceLocation) {
+
+//   final class SigSym(val clazz: Symbol.ClassSym, val name: String, val loc: SourceLocation) {
+
+//   final class TypeAliasSym(val namespace: List[String], val name: String, val loc: SourceLocation) {
 
 interface TypeParam {
     name: String,
