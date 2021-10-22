4e1c74fbed119949b3fb8526731b745d399b7739
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2021-10-22 14:30:43 +0200

    WIP

--- main/src/ca/uwaterloo/flix/api/doc/Api.d.ts
+++ main/src/ca/uwaterloo/flix/api/doc/Api.d.ts
@@ -1,8 +1,6 @@
-import {ClassSym} from "./ClassSym";
+import {Class} from "./Class";
 import {Def} from "./Def";
 import {Enum} from "./Enum";
-import {Modifier} from "./Modifier";
-import {TypeParam} from "./TypeParam";
 import {TypeAlias} from "./TypeAlias";
 
 export interface Api {
@@ -11,7 +9,7 @@ export interface Api {
     // TODO: signs
     defs: DefsByNS
     enums: EnumsByNs
-    typealiases: TypeAliasesByNs
+    typeAliases: TypeAliasesByNs
 }
 
 type ClassesByNS = {
@@ -29,19 +27,3 @@ type EnumsByNs = {
 type TypeAliasesByNs = {
     [key: string]: [TypeAlias]
 }
-
-interface Class {
-    sym: ClassSym
-    doc: [String]
-    mod: [Modifier]
-    tparam: TypeParam
-    //  superClasses: List[Ast.TypeConstraint], signatures: List[TypedAst.Sig], laws: List[TypedAst.Def], loc: SourceLocation
-}
-
-//   case class Instance(doc: Ast.Doc, mod: Ast.Modifiers, sym: Symbol.ClassSym, tpe: Type, tconstrs: List[Ast.TypeConstraint], defs: List[TypedAst.Def], ns: Name.NName, loc: SourceLocation)
-
-//   case class Sig(sym: Symbol.SigSym, spec: TypedAst.Spec, impl: Option[TypedAst.Impl])
-//  case class Spec(doc: Ast.Doc, ann: List[TypedAst.Annotation], mod: Ast.Modifiers, tparams: List[TypedAst.TypeParam], fparams: List[TypedAst.FormalParam], declaredScheme: Scheme, retTpe: Type, eff: Type, loc: SourceLocation)
-
-
-
