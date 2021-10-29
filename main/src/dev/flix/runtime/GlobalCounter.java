f7ebc969f5ed9772e82445fda0d6dbeb44d25cb9
JonathanStarup <32037926+JonathanStarup@users.noreply.github.com>
2021-10-29 15:26:36 +0200

    comment_file_link

--- main/src/dev/flix/runtime/GlobalCounter.java
+++ main/src/dev/flix/runtime/GlobalCounter.java
@@ -2,6 +2,11 @@ package dev.flix.runtime;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+/**
+ * A copy of the class generated in `main/src/ca/uwaterloo/flix/language/phase/jvm/GenGlobalCounterClass.scala`
+ * that allows the compiler to load the reference in `main/src/library/ChannelImpl.flix` and check for types
+ * (the code here is never run).
+ */
 public final class GlobalCounter {
     private static final AtomicLong counter = new AtomicLong();
 
