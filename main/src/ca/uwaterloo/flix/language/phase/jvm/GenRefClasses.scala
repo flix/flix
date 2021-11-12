c9be1d06c1ccfcccf48610534b6b8d7e517ea6e9
JonathanStarup <32037926+JonathanStarup@users.noreply.github.com>
2021-11-12 12:58:15 +0100

    fix

--- main/src/ca/uwaterloo/flix/language/phase/jvm/GenRefClasses.scala
+++ main/src/ca/uwaterloo/flix/language/phase/jvm/GenRefClasses.scala
@@ -3,6 +3,7 @@ package ca.uwaterloo.flix.language.phase.jvm
 import ca.uwaterloo.flix.api.Flix
 import ca.uwaterloo.flix.language.ast.ErasedAst.Root
 import ca.uwaterloo.flix.language.ast.MonoType
+import ca.uwaterloo.flix.language.phase.jvm.JvmOps.{getErasedJvmType, getJvmType}
 import org.objectweb.asm.ClassWriter
 import org.objectweb.asm.Opcodes._
 
@@ -23,7 +24,7 @@ object GenRefClasses {
     // Generating each cell class
     types.map { tpe =>
       val classType = JvmOps.getRefClassType(MonoType.Ref(tpe))
-      classType.name -> JvmClass(classType.name, genRefClass(classType, classType))
+      classType.name -> JvmClass(classType.name, genRefClass(classType, getErasedJvmType(tpe)))
     }.toMap
   }
 
