b1d071a640d25a612c1107ab7bf6823153a0274f
JonathanStarup <32037926+JonathanStarup@users.noreply.github.com>
2021-11-26 11:36:59 +0100

    copyright

--- main/src/ca/uwaterloo/flix/language/phase/jvm/GenExpression.scala
+++ main/src/ca/uwaterloo/flix/language/phase/jvm/GenExpression.scala
@@ -1,5 +1,6 @@
 /*
  * Copyright 2017 Ramin Zarifi
+ * Copyright 2021 Jonathan Lindegaard Starup
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
