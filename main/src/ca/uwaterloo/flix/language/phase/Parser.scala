8b05fa5a78f47ac18efd013e0e5037887e89cd32
JonathanStarup <32037926+JonathanStarup@users.noreply.github.com>
2021-12-09 17:48:44 +0100

    done

--- main/src/ca/uwaterloo/flix/language/phase/Parser.scala
+++ main/src/ca/uwaterloo/flix/language/phase/Parser.scala
@@ -361,7 +361,7 @@ class Parser(val source: Source) extends org.parboiled2.Parser {
 
     // Note that outside of patterns, Strings are parsed as [[Interpolation]]s
     def Str: Rule1[ParsedAst.Literal.Str] = rule {
-      SP ~ "\"" ~ zeroOrMore(!"\"" ~ Chars.CharCode) ~ "\"" ~ SP ~> ParsedAst.Literal.Str
+      SP ~ "\"" ~ zeroOrMore(!("\"" | atomic("${")) ~ Chars.CharCode) ~ "\"" ~ SP ~> ParsedAst.Literal.Str
     }
 
     def Float: Rule1[ParsedAst.Literal] = rule {
