e04d2cc8c4fec18f0fd27e8de4b627a24eedaada
Jonathan Lindegaard Starup <32037926+JonathanStarup@users.noreply.github.com>
2021-12-29 17:46:49 +0100

    refactor: add method objects to backend types (#2925)

--- main/src/ca/uwaterloo/flix/language/phase/jvm/GenMatchErrorClass.scala
+++ main/src/ca/uwaterloo/flix/language/phase/jvm/GenMatchErrorClass.scala
@@ -42,10 +42,11 @@ object GenMatchErrorClass {
 
     cm.mkConstructor(genConstructor(), mkDescriptor(JvmName.ReifiedSourceLocation.toTpe)(VoidableType.Void), IsPublic)
 
+    locationField.mkField(cm, IsPublic, IsFinal)
+
     // TODO: Are these ever used?
     cm.mkMethod(genEqualsMethod(), "equals", mkDescriptor(JvmName.Object.toTpe)(BackendType.Bool), IsPublic, NotFinal)
     cm.mkMethod(genHashCodeMethod(), "hashCode", mkDescriptor()(BackendType.Int32), IsPublic, NotFinal)
-    locationField.mkField(cm, IsPublic, IsFinal)
 
     cm.closeClassMaker()
   }
