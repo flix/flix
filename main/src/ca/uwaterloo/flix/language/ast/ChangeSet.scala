4e695817797352c6a1987df28bd4db0f73f925bb
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2021-12-31 16:37:14 +0100

    feat: add incremental compilation to `Typer`

--- main/src/ca/uwaterloo/flix/language/ast/ChangeSet.scala
+++ main/src/ca/uwaterloo/flix/language/ast/ChangeSet.scala
@@ -28,28 +28,32 @@ sealed trait ChangeSet {
   /**
     * Returns two maps: `stale` and `fresh` according to the given `newMap` and `oldMap`.
     *
-    * A fresh element is one that can be reused. A stale element is one that must be re-compiled.
-    * A element that is neither fresh nor stale can be deleted.
+    * A fresh key is one that can be reused. A stale key is one that must be re-compiled.
+    * A key that is neither fresh nor stale can be deleted.
     *
     * An entry is stale if it is (a) stale w.r.t. the current change set and `oldMap`.
     * An entry is fresh if it is (a) not stale and (b) occurs in `newMap`.
     *
     * Note that the union of stale and fresh does not have to equal `newMap` or `oldMap`.
-    * This happens if an element is deleted. Then it does not occur `newMap` but it occurs in `oldMap`.
+    * This happens if a key is deleted. Then it does not occur `newMap` but it occurs in `oldMap`.
     * However, it is neither fresh nor stale. It should simply be forgotten.
     */
-  def partition[V1, V2](newMap: Map[Symbol.DefnSym, V1], oldMap: Map[Symbol.DefnSym, V2]): (Map[Symbol.DefnSym, V1], Map[Symbol.DefnSym, V2]) = {
+  def partition[K <: Locatable, V1, V2](newMap: Map[K, V1], oldMap: Map[K, V2]): (Map[K, V1], Map[K, V2]) = {
     val stale = newMap.filter(kv => isStale(kv._1, oldMap))
     val fresh = (oldMap -- stale.keySet).filter(kv => newMap.contains(kv._1))
     (stale, fresh)
   }
 
   /**
-    * Returns `true` if the given symbol `sym` is stale w.r.t. the current change set and `oldMap`.
+    * Returns `true` if the given `key` is stale w.r.t. the current change set and `oldMap`.
+    *
+    * A key is stale if either (a) everything has changed, (b) it is new (i.e. not in `oldMap`),
+    * (c) it is in the change set, or (d) if its source location is unknown.
     */
-  private def isStale[V](sym: Symbol.DefnSym, oldMap: Map[Symbol.DefnSym, V]): Boolean = this match {
+  private def isStale[K <: Locatable, V](key: K, oldMap: Map[K, V]): Boolean = this match {
     case ChangeSet.Everything => true
-    case ChangeSet.Changes(s) => !oldMap.contains(sym) || s.contains(sym.loc.source)
+    case ChangeSet.Changes(s) =>
+      !oldMap.contains(key) || s.contains(key.loc.source) || key.loc == SourceLocation.Unknown
   }
 
 }
