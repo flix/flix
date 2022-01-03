ee43d32a8b5ffd73b264cd4b3a4a8fe25bb52617
ninaandrup <nina@andruppedersen.dk>
2022-01-03 16:57:08 +0100

    Relocate

--- main/src/ca/uwaterloo/flix/api/Flix.scala
+++ main/src/ca/uwaterloo/flix/api/Flix.scala
@@ -126,7 +126,7 @@ class Flix {
     "Chain.flix" -> LocalResource.get("/src/library/Chain.flix"),
     "Char.flix" -> LocalResource.get("/src/library/Char.flix"),
     "Choice.flix" -> LocalResource.get("/src/library/Choice.flix"),
-    "Condition.flix" -> LocalResource.get("/src/library/Condition.flix"),
+    "Condition.flix" -> LocalResource.get("/src/library/Concurrent/Condition.flix"),
     "Console.flix" -> LocalResource.get("/src/library/Console.flix"),
     "DelayMap.flix" -> LocalResource.get("/src/library/DelayMap.flix"),
     "DemandList.flix" -> LocalResource.get("/src/library/DemandList.flix"),
@@ -144,7 +144,7 @@ class Flix {
     "Object.flix" -> LocalResource.get("/src/library/Object.flix"),
     "Option.flix" -> LocalResource.get("/src/library/Option.flix"),
     "Random.flix" -> LocalResource.get("/src/library/Random.flix"),
-    "ReentrantLock.flix" -> LocalResource.get("/src/library/ReentrantLock.flix"),
+    "ReentrantLock.flix" -> LocalResource.get("/src/library/Concurrent/ReentrantLock.flix"),
     "Result.flix" -> LocalResource.get("/src/library/Result.flix"),
     "Set.flix" -> LocalResource.get("/src/library/Set.flix"),
     "String.flix" -> LocalResource.get("/src/library/String.flix"),
@@ -171,9 +171,9 @@ class Flix {
 
     "Validation.flix" -> LocalResource.get("/src/library/Validation.flix"),
 
-    "Channel.flix" -> LocalResource.get("/src/library/Channel.flix"),
-    "Ticker.flix" -> LocalResource.get("/src/library/Ticker.flix"),
-    "Timer.flix" -> LocalResource.get("/src/library/Timer.flix"),
+    "Channel.flix" -> LocalResource.get("/src/library/Concurrent/Channel.flix"),
+    "Ticker.flix" -> LocalResource.get("/src/library/Concurrent/Channel/Ticker.flix"),
+    "Timer.flix" -> LocalResource.get("/src/library/Concurrent/Channel/Timer.flix"),
     "Duration.flix" -> LocalResource.get("/src/library/Duration.flix"),
     "Instant.flix" -> LocalResource.get("/src/library/Instant.flix"),
 
