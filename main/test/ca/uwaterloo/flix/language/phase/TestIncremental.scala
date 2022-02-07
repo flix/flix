beafb758aebdecacc638e05e19eccc2d7e43c247
Matthew Lutze <mlutze@users.noreply.github.com>
2022-02-07 09:25:21 +0100

    refactor TestIncremental

--- main/test/ca/uwaterloo/flix/language/phase/TestIncremental.scala
+++ main/test/ca/uwaterloo/flix/language/phase/TestIncremental.scala
@@ -17,78 +17,75 @@ package ca.uwaterloo.flix.language.phase
 
 import ca.uwaterloo.flix.api.Flix
 import ca.uwaterloo.flix.util.Options
-import org.scalatest.{BeforeAndAfterAll, FunSuite}
+import org.scalatest.{BeforeAndAfter, FunSuite}
 
-class TestIncremental extends FunSuite with BeforeAndAfterAll {
-
-  //
-  // Note: This class deliberately uses the *same* Flix instance.
-  //
-  // This is because we want to test incremental compilation by compiling in the same instance.
-  //
+class TestIncremental extends FunSuite with BeforeAndAfter {
 
   private val FileA = "FileA.flix"
   private val FileB = "FileB.flix"
   private val FileC = "FileC.flix"
-  private val Flix = new Flix()
   private val Opts = Options.Default.copy(incremental = true)
 
-  override protected def beforeAll(): Unit = {
-    Flix.setOptions(Opts)
-    Flix.addSourceCode(FileA,
+  private var flix: Flix = _
+
+  // A new Flix instance is created and initialized with some source code for each test.
+  before {
+    flix = new Flix()
+    flix.setOptions(Opts)
+    flix.addSourceCode(FileA,
       s"""
          |pub def f(x: Bool): Bool = not x
          |
          |""".stripMargin)
-    Flix.addSourceCode(FileB,
+    flix.addSourceCode(FileB,
       s"""
          |def main(_args: Array[String]): Int32 & Impure =
          |    println(f(true));
          |    0
          |""".stripMargin)
-    Flix.addSourceCode(FileC,
+    flix.addSourceCode(FileC,
       s"""
          |pub lawless class C[a] {
          |    pub def cf(x: Bool, y: a, z: a): a = if (f(x) == x) y else z
          |}
          |""".stripMargin)
-    Flix.compile().get
+    flix.compile().get
   }
 
   test("Incremental.01") {
-    Flix.addSourceCode(FileA,
+    flix.addSourceCode(FileA,
       s"""
          |pub def f(x: Int32): Int32 = x + 1i32
          |
          |""".stripMargin)
-    Flix.addSourceCode(FileB,
+    flix.addSourceCode(FileB,
       s"""
          |def main(_args: Array[String]): Int32 & Impure =
          |    println(f(123));
          |    0
          |""".stripMargin)
-    Flix.addSourceCode(FileC,
+    flix.addSourceCode(FileC,
       s"""
          |pub lawless class C[a] {
          |    pub def cf(x: Int32, y: a, z: a): a = if (f(x) == x) y else z
          |    pub def cg(x: a): a
          |}
          |""".stripMargin)
-    Flix.compile().get
+    flix.compile().get
   }
 
   test("Incremental.02") {
-    Flix.addSourceCode(FileA,
+    flix.addSourceCode(FileA,
       s"""
          |pub def f(x: String): String = String.toUpperCase(x)
          |""".stripMargin)
-    Flix.addSourceCode(FileB,
+    flix.addSourceCode(FileB,
       s"""
          |def main(_args: Array[String]): Int32 & Impure =
          |    println(f("Hello World"));
          |    0
          |""".stripMargin)
-    Flix.addSourceCode(FileC,
+    flix.addSourceCode(FileC,
       s"""
          |pub lawless class C[a] {
          |    pub def cf(x: String, y: a, z: a): a = if (f(x) == x) y else z
@@ -96,7 +93,7 @@ class TestIncremental extends FunSuite with BeforeAndAfterAll {
          |    pub def ch(x: a): a
          |}
          |""".stripMargin)
-    Flix.compile().get
+    flix.compile().get
   }
 
 }
