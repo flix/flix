7d60b61b3c72c5dd105767b54975ab25abcff584
Matthew Lutze <mlutze@users.noreply.github.com>
2022-03-13 20:53:09 +0100

    WIP add precedence function

--- main/src/ca/uwaterloo/flix/language/debug/FormatSimpleType.scala
+++ main/src/ca/uwaterloo/flix/language/debug/FormatSimpleType.scala
@@ -57,6 +57,65 @@ object FormatSimpleType {
       }
     }
 
+    val Delimited = 0
+
+    def precedence(tpe0: SimpleType): Int = tpe0 match {
+      case SimpleType.Unit => Delimited
+      case SimpleType.Null => Delimited
+      case SimpleType.Bool => Delimited
+      case SimpleType.Char => Delimited
+      case SimpleType.Float32 => Delimited
+      case SimpleType.Float64 => Delimited
+      case SimpleType.Int8 => Delimited
+      case SimpleType.Int16 => Delimited
+      case SimpleType.Int32 => Delimited
+      case SimpleType.Int64 => Delimited
+      case SimpleType.BigInt => Delimited
+      case SimpleType.Str => Delimited
+      case SimpleType.Array => Delimited
+      case SimpleType.ScopedRef => Delimited
+      case SimpleType.Channel => Delimited
+      case SimpleType.Lazy => Delimited
+      case SimpleType.True => Delimited
+      case SimpleType.False => Delimited
+      case SimpleType.Region => Delimited
+      case SimpleType.Record(fields, rest) => Delimited
+      case SimpleType.RecordRow(fields, rest) => Delimited
+      case SimpleType.RecordRowEmpty => Delimited
+      case SimpleType.RecordEmpty => Delimited
+      case SimpleType.RecordConstructor => Delimited
+      case SimpleType.RecordRowConstructor(field) => Delimited
+      case SimpleType.RecordRowHead(name, tpe) => Delimited
+      case SimpleType.Schema(fields, rest) => Delimited
+      case SimpleType.SchemaRow(fields, rest) => Delimited
+      case SimpleType.SchemaRowEmpty => Delimited
+      case SimpleType.SchemaEmpty => Delimited
+      case SimpleType.SchemaConstructor => Delimited
+      case SimpleType.SchemaRowConstructor(field) => Delimited
+      case SimpleType.SchemaRowHead(name, tpe) => Delimited
+      case SimpleType.Not(tpe) => ???
+      case SimpleType.And(tpes) => ???
+      case SimpleType.Or(tpes) => ???
+      case SimpleType.RelationConstructor => Delimited
+      case SimpleType.Relation(tpes) => Delimited
+      case SimpleType.LatticeConstructor => Delimited
+      case SimpleType.Lattice(tpes) => Delimited
+      case SimpleType.ArrowConstructor(arity) => ???
+      case SimpleType.PartialPureArrow(arity, tpes) => ???
+      case SimpleType.PartialImpureArrow(arity, tpes) => ???
+      case SimpleType.PartialPolyArrow(arity, tpes, eff) => ???
+      case SimpleType.PureArrow(args, ret) => ???
+      case SimpleType.ImpureArrow(args, ret) => ???
+      case SimpleType.PolyArrow(args, ret, eff) => ???
+      case SimpleType.TagConstructor(name) => ???
+      case SimpleType.PartialTag(name, args) => ???
+      case SimpleType.Tag(name, args, ret) => ???
+      case SimpleType.Name(name) => Delimited
+      case SimpleType.Apply(tpe, tpes) => Delimited
+      case SimpleType.Var(id) => Delimited
+      case SimpleType.Tuple(length, fields) => Delimited
+    }
+
     def visit(tpe0: SimpleType): String = tpe0 match {
       case SimpleType.Unit => "Unit"
       case SimpleType.Null => "Null"
