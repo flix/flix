8568720d482d2677c54437651a44750da8cf1b23
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2022-03-18 14:52:04 +0100

    feat: add `region` expression (#3352)

--- main/src/ca/uwaterloo/flix/language/ast/TypedAst.scala
+++ main/src/ca/uwaterloo/flix/language/ast/TypedAst.scala
@@ -220,9 +220,7 @@ object TypedAst {
       def eff: Type = Type.Impure
     }
 
-    case class Ref(exp: TypedAst.Expression, tpe: Type, eff: Type, loc: SourceLocation) extends TypedAst.Expression
-
-    case class RefWithRegion(exp1: TypedAst.Expression, exp2: TypedAst.Expression, tpe: Type, eff: Type, loc: SourceLocation) extends TypedAst.Expression
+    case class Ref(exp1: TypedAst.Expression, exp2: TypedAst.Expression, tpe: Type, eff: Type, loc: SourceLocation) extends TypedAst.Expression
 
     case class Deref(exp: TypedAst.Expression, tpe: Type, eff: Type, loc: SourceLocation) extends TypedAst.Expression
 
