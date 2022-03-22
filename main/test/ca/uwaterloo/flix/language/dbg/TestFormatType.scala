a14cd6f8f407b55e8ce33c060abe15e252eda360
Matthew Lutze <mlutze@users.noreply.github.com>
2022-03-22 15:10:14 +0100

    fix another thing

--- main/test/ca/uwaterloo/flix/language/dbg/TestFormatType.scala
+++ main/test/ca/uwaterloo/flix/language/dbg/TestFormatType.scala
@@ -18,7 +18,7 @@ package ca.uwaterloo.flix.language.dbg
 
 import ca.uwaterloo.flix.TestUtils
 import ca.uwaterloo.flix.language.ast.{Kind, Name, Rigidity, SourceLocation, SourcePosition, Symbol, Type, TypeConstructor}
-import ca.uwaterloo.flix.language.fmt.FormatType
+import ca.uwaterloo.flix.language.fmt.{Audience, FormatType}
 import org.scalatest.FunSuite
 
 class TestFormatType extends FunSuite with TestUtils {
