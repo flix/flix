0812a23090ed19fe7212f12ab02e14d193c8c91f
JonathanStarup <32037926+JonathanStarup@users.noreply.github.com>
2022-04-12 12:01:30 +0200

    change-mock-class

--- main/src/dev/flix/runtime/Global.java
+++ main/src/dev/flix/runtime/Global.java
@@ -1,16 +1,12 @@
 package dev.flix.runtime;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 /**
- * OBS: A copy of the class generated in
- * `main/src/ca/uwaterloo/flix/language/phase/jvm/GenGlobalClass.scala` that
- * allows the compiler to load the imports in i.e. channel and check for types
- * (the code here is never run).
+ * OBS: A interface-like copy of the class generated in
+ * `main/src/ca/uwaterloo/flix/language/phase/jvm/GenGlobalClass.scala` with
+ * exception bodies that allows the compiler to load the imports in i.e. channel
+ * and check for types (the code here is never run).
  */
 public final class Global {
-    private static final AtomicLong tCounter = null;
-    private static String[] tArgs = null;
 
     public static final long newId() {
         throw new RuntimeException("Global.newId should not be called on the mock class");
@@ -23,4 +19,5 @@ public final class Global {
     public static final void setArgs(String[] var0) {
         throw new RuntimeException("Global.setArgs should not be called on the mock class");
     }
+
 }
