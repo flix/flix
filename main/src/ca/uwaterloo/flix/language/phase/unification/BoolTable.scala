762fa03836d026be5fc922e8b172debdcfd7735e
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2022-04-20 11:34:19 +0200

    feat: add simplification via minimization.

--- main/src/ca/uwaterloo/flix/language/phase/unification/BoolTable.scala
+++ main/src/ca/uwaterloo/flix/language/phase/unification/BoolTable.scala
@@ -1,6 +1,6 @@
 package ca.uwaterloo.flix.language.phase.unification
 
-import ca.uwaterloo.flix.language.ast.{Symbol, Type, TypeConstructor}
+import ca.uwaterloo.flix.language.ast.{SourceLocation, Symbol, Type, TypeConstructor}
 import ca.uwaterloo.flix.util.InternalCompilerException
 
 object BoolTable {
@@ -8,27 +8,45 @@ object BoolTable {
   sealed trait Term
 
   object Term {
-    case class Var(sym: Symbol.TypeVarSym) extends Term
+    case object True extends Term
+
+    case object False extends Term
+
+    case class Var(sym: Symbol.KindedTypeVarSym) extends Term
 
     case class Neg(t: Term) extends Term
 
-    case class Conj(x: Term, y: Term) extends Term
+    case class Conj(t1: Term, t2: Term) extends Term
 
-    case class Disj(x: Term, y: Term) extends Term
+    case class Disj(t1: Term, t2: Term) extends Term
   }
 
   def minimize(tpe: Type): Type = {
     val t = fromType(tpe)
 
-    tpe
+    val freeVars = tpe.typeVars.toList
+
+
+    toType(t)
   }
 
   def fromType(tpe: Type): Term = tpe match {
     case Type.KindedVar(sym, _) => Term.Var(sym)
+    case Type.True => Term.True
+    case Type.False => Term.False
     case Type.Apply(Type.Cst(TypeConstructor.Not, _), x, _) => Term.Neg(fromType(x))
     case Type.Apply(Type.Apply(Type.Cst(TypeConstructor.And, _), x, _), y, _) => Term.Conj(fromType(x), fromType(y))
     case Type.Apply(Type.Apply(Type.Cst(TypeConstructor.Or, _), x, _), y, _) => Term.Disj(fromType(x), fromType(y))
     case _ => throw InternalCompilerException(s"Unexpected type: '$tpe'.")
+  }
+
+  def toType(t0: Term): Type = t0 match {
+    case Term.True => Type.True
+    case Term.False => Type.False
+    case Term.Var(sym) => Type.KindedVar(sym, SourceLocation.Unknown)
+    case Term.Neg(t) => Type.mkNot(toType(t), SourceLocation.Unknown)
+    case Term.Conj(t1, t2) => Type.mkAnd(toType(t1), toType(t2), SourceLocation.Unknown)
+    case Term.Disj(t1, t2) => Type.mkOr(toType(t1), toType(t2), SourceLocation.Unknown)
   }
 
 }
--- main/src/ca/uwaterloo/flix/language/phase/unification/BoolUnification.scala
+++ main/src/ca/uwaterloo/flix/language/phase/unification/BoolUnification.scala
@@ -17,7 +17,7 @@ package ca.uwaterloo.flix.language.phase.unification
 
 import ca.uwaterloo.flix.api.Flix
 import ca.uwaterloo.flix.language.ast.Scheme.InstantiateMode
-import ca.uwaterloo.flix.language.ast.Type.eraseAliases
+import ca.uwaterloo.flix.language.ast.Type.{Bool, eraseAliases}
 import ca.uwaterloo.flix.language.ast._
 import ca.uwaterloo.flix.util.Result.{Err, Ok}
 import ca.uwaterloo.flix.util.{InternalCompilerException, Result}
@@ -111,9 +111,8 @@ object BoolUnification {
       val t1 = Substitution.singleton(x.sym, Type.True)(f)
       val se = successiveVariableElimination(mkAnd(t0, t1), xs)
 
-      // Investigate impact on Monomorph semantics:
-      val st = Substitution.singleton(x.sym, mkOr(se(t0), mkAnd(x, mkNot(se(t1)))))
-      //val st = Substitution.singleton(x, mkOr(se(t0), mkAnd(Type.freshVar(Kind.Bool, f.loc), mkNot(se(t1)))))
+      val f1 = BoolTable.minimize(mkOr(se(t0), mkAnd(x, mkNot(se(t1)))))
+      val st = Substitution.singleton(x.sym, f1)
       st ++ se
   }
 
