9a42e7c2ecf2c06237fea6068f996dd39f09bb7b
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2022-04-21 19:24:10 +0200

    feat: add Boolean minimization via tabling.

--- main/src/ca/uwaterloo/flix/language/phase/unification/BoolTable.scala
+++ main/src/ca/uwaterloo/flix/language/phase/unification/BoolTable.scala
@@ -17,7 +17,6 @@ package ca.uwaterloo.flix.language.phase.unification
 
 import ca.uwaterloo.flix.api.Flix
 import ca.uwaterloo.flix.language.ast.{Kind, SourceLocation, Symbol, Type, TypeConstructor}
-import ca.uwaterloo.flix.language.fmt.{Audience, FormatType}
 import ca.uwaterloo.flix.util.InternalCompilerException
 import ca.uwaterloo.flix.util.collection.Bimap
 
