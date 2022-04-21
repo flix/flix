f33346c1a4733d2f9b930ec3f9d9f981d027092a
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2022-04-21 09:20:17 +0200

    feat: add Boolean minimization via tabling.

--- main/test/ca/uwaterloo/flix/language/phase/unification/TestBoolTable.scala
+++ main/test/ca/uwaterloo/flix/language/phase/unification/TestBoolTable.scala
@@ -6,28 +6,68 @@ import BoolTable.Formula._
 
 class TestBoolTable extends FunSuite {
 
-  test("Minimize.01") {
+  test("Minimize.True.01") {
     assertResult(expected = True)(actual = minimize(True))
   }
 
-  test("Minimize.02") {
-    assertResult(expected = False)(actual = minimize(False))
+  test("Minimize.True.02") {
+    assertResult(expected = True)(actual = minimize(Neg(False)))
   }
 
-  test("Minimize.03") {
+  test("Minimize.True.03") {
     assertResult(expected = True)(actual = minimize(Conj(True, True)))
   }
 
-  test("Minimize.04") {
-    assertResult(expected = False)(actual = minimize(Conj(True, False)))
+  test("Minimize.True.04") {
+    assertResult(expected = True)(actual = minimize(Disj(True, True)))
   }
 
-  test("Minimize.05") {
+  test("Minimize.True.05") {
+    assertResult(expected = True)(actual = minimize(Disj(True, False)))
+  }
+
+  test("Minimize.True.06") {
+    assertResult(expected = True)(actual = minimize(Disj(False, True)))
+  }
+
+  // TODO: Add more tests that follow the same structure, but for false.
+
+  test("Minimize.Var1.01") {
     assertResult(expected = Var(0))(actual = minimize(Var(0)))
   }
 
-  test("Minimize.06") {
+  test("Minimize.Var1.02") {
+    assertResult(expected = Var(0))(actual = minimize(Neg(Neg(Var(0)))))
+  }
+
+  test("Minimize.Var1.03") {
     assertResult(expected = Var(0))(actual = minimize(Conj(Var(0), Var(0))))
+  }
+
+  test("Minimize.Var1.04") {
+    assertResult(expected = Var(0))(actual = minimize(Conj(Var(0), True)))
+  }
+
+  test("Minimize.Var1.05") {
+    assertResult(expected = Var(0))(actual = minimize(Conj(True, Var(0))))
+  }
+
+  // TODO: Add more Minimize.Var1 tests, but with Disj.
+
+  test("Minimize.Var1AndVar2.01") {
+    assertResult(expected = Conj(Var(0), Var(1)))(actual = minimize(Conj(Var(0), Var(1))))
+  }
+
+  test("Minimize.Var1AndVar2.02") {
+    assertResult(expected = Conj(Var(0), Var(1)))(actual = minimize(Conj(Var(1), Var(0))))
+  }
+
+  test("Minimize.Var1AndVar2.03") {
+    assertResult(expected = Conj(Var(0), Var(1)))(actual = minimize(Conj(Var(0), Conj(Var(1), Var(1)))))
+  }
+
+  test("Minimize.Var1AndVar2.04") {
+    assertResult(expected = Conj(Var(0), Var(1)))(actual = minimize(Conj(Conj(Var(1), Var(1)), Var(0))))
   }
 
 }
