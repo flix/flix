370f26ad923633e2f008ec342d5ba67a68a5f855
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2022-04-24 18:10:03 +0200

    feat: add `static` region.

--- main/src/ca/uwaterloo/flix/language/ast/ParsedAst.scala
+++ main/src/ca/uwaterloo/flix/language/ast/ParsedAst.scala
@@ -659,6 +659,14 @@ object ParsedAst {
     case class LetImport(sp1: SourcePosition, op: ParsedAst.JvmOp, exp: ParsedAst.Expression, sp2: SourcePosition) extends ParsedAst.Expression
 
     /**
+      * Static Region Expression.
+      *
+      * @param sp1 the position of the first character in the expression.
+      * @param sp2 the position of the last character in the expression.
+      */
+    case class Static(sp1: SourcePosition, sp2: SourcePosition) extends ParsedAst.Expression
+
+    /**
       * Scope Expression.
       *
       * @param sp1   the position of the first character in the expression.
@@ -1416,6 +1424,14 @@ object ParsedAst {
       * @param sp2     the position of the last character in the type.
       */
     case class Apply(base: ParsedAst.Type, tparams: Seq[ParsedAst.Type], sp2: SourcePosition) extends ParsedAst.Type
+
+    /**
+      * The Static Region type constructor.
+      *
+      * @param sp1 the position of the first character in the type.
+      * @param sp2 the position of the last character in the type.
+      */
+    case class StaticRegion(sp1: SourcePosition, sp2: SourcePosition) extends ParsedAst.Type
 
     /**
       * The True type constructor.
