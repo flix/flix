211dfa92bf456efd98d20ce64a2f05967e8e66a2
Matthew Lutze <mlutze@users.noreply.github.com>
2022-05-03 09:21:01 +0200

    add some checks to effect declarations

--- main/src/ca/uwaterloo/flix/language/ast/WeededAst.scala
+++ main/src/ca/uwaterloo/flix/language/ast/WeededAst.scala
@@ -47,7 +47,7 @@ object WeededAst {
 
     case class Effect(doc: Ast.Doc, mod: Ast.Modifiers, ident: Name.Ident, ops: List[WeededAst.Declaration.Op], loc: SourceLocation) extends WeededAst.Declaration
 
-    case class Op(doc: Ast.Doc, ann: List[WeededAst.Annotation], mod: Ast.Modifiers, ident: Name.Ident, tparams: WeededAst.KindedTypeParams, fparams: List[WeededAst.FormalParam], tpe: WeededAst.Type, retTpe: WeededAst.Type, eff: WeededAst.Type, tconstrs: List[WeededAst.TypeConstraint], loc: SourceLocation) extends WeededAst.Declaration
+    case class Op(doc: Ast.Doc, ann: List[WeededAst.Annotation], mod: Ast.Modifiers, ident: Name.Ident, fparams: List[WeededAst.FormalParam], tpe: WeededAst.Type, retTpe: WeededAst.Type, eff: WeededAst.Type, tconstrs: List[WeededAst.TypeConstraint], loc: SourceLocation) extends WeededAst.Declaration
 
   }
 
