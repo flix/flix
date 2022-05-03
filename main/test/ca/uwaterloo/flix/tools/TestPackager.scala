95158d721339d91a32691efd652829e30a983f22
Kengo TODA <skypencil@gmail.com>
2022-05-03 12:52:57 +0800

    test: verify that packages are always the same

--- main/test/ca/uwaterloo/flix/tools/TestPackager.scala
+++ main/test/ca/uwaterloo/flix/tools/TestPackager.scala
@@ -3,11 +3,13 @@ package ca.uwaterloo.flix.tools
 import ca.uwaterloo.flix.util.Options
 import org.scalatest.FunSuite
 
-import java.nio.file.Files
+import java.nio.file.{Files, Path}
+import java.security.{DigestInputStream, MessageDigest}
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.zip.ZipFile
 import scala.jdk.CollectionConverters.EnumerationHasAsScala
+import scala.util.Using
 
 class TestPackager extends FunSuite {
 
@@ -59,6 +61,33 @@ class TestPackager extends FunSuite {
     }
   }
 
+  def calcHash(p: Path): String = {
+    val buffer = new Array[Byte](8192)
+    val sha = MessageDigest.getInstance("SHA-256")
+    Using(new DigestInputStream(Files.newInputStream(p), sha)) { input =>
+      while (input.read(buffer) != -1) { }
+      sha.digest.map("%02x".format(_)).mkString
+    }.get
+  }
+
+  test("build-jar always generates package that is byte-for-byte exactly the same") {
+    val p = Files.createTempDirectory(ProjectPrefix)
+    Packager.init(p, DefaultOptions)
+    val packageName = p.getFileName.toString
+    val packagePath = p.resolve(packageName + ".jar")
+
+    Packager.buildJar(p, DefaultOptions)
+    def hash1 = calcHash(packagePath)
+
+    Files.delete(packagePath)
+    Packager.buildJar(p, DefaultOptions)
+    def hash2 = calcHash(packagePath)
+
+    assert(
+      hash1.equals(hash2),
+      s"Two file hashes are not same: ${hash1} and ${hash2}")
+  }
+
   test("build-pkg") {
     val p = Files.createTempDirectory(ProjectPrefix)
     Packager.init(p, DefaultOptions)
@@ -83,6 +112,24 @@ class TestPackager extends FunSuite {
       val formatted = format.format(time)
       assert(formatted.equals("2014-06-27 00:00:00"))
     }
+  }
+
+  test("build-pkg always generates package that is byte-for-byte exactly the same") {
+    val p = Files.createTempDirectory(ProjectPrefix)
+    Packager.init(p, DefaultOptions)
+    val packageName = p.getFileName.toString
+    val packagePath = p.resolve(packageName + ".fpkg")
+
+    Packager.buildPkg(p, DefaultOptions)
+    def hash1 = calcHash(packagePath)
+
+    Files.delete(packagePath)
+    Packager.buildPkg(p, DefaultOptions)
+    def hash2 = calcHash(packagePath)
+
+    assert(
+      hash1.equals(hash2),
+      s"Two file hashes are not same: ${hash1} and ${hash2}")
   }
 
   test("benchmark") {
