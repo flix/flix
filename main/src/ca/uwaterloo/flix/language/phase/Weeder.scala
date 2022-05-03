835b434ffd9f5cdd97d2a24ee79d6f0ecdde9cee
Matthew Lutze <mlutze@users.noreply.github.com>
2022-05-03 09:21:01 +0200

    add some checks to effect declarations

--- main/src/ca/uwaterloo/flix/language/phase/Weeder.scala
+++ main/src/ca/uwaterloo/flix/language/phase/Weeder.scala
@@ -252,13 +252,15 @@ object Weeder {
       }
   }
 
-  // MATT docs
+  /**
+    * Performs weeding on the given effect declaration.
+    */
   private def visitEffect(d0: ParsedAst.Declaration.Effect)(implicit flix: Flix): Validation[List[WeededAst.Declaration.Effect], WeederError] = d0 match {
-    case ParsedAst.Declaration.Effect(doc0, mod0, sp1, ident, tparams, ops0, sp2) =>
+    case ParsedAst.Declaration.Effect(doc0, mod0, sp1, ident, tparams0, ops0, sp2) =>
       val doc = visitDoc(doc0)
       val modVal = visitModifiers(mod0, legalModifiers = Set.empty) // MATT public and stuff?
       val identVal = visitName(ident)
-      val tparamsVal = ??? // assert no tparams
+      val tparamsVal = requireNoTypeParams(tparams0)
       val opsVal = traverse(ops0)(visitOp)
       mapN(modVal, identVal, tparamsVal, opsVal) {
         case (mod, _, _, ops) =>
@@ -266,19 +268,25 @@ object Weeder {
       }
   }
 
-  // MATT docs
+  /**
+    * Performs weeding on the given effect operation.
+    */
   private def visitOp(d0: ParsedAst.Declaration.Op)(implicit flix: Flix): Validation[WeededAst.Declaration.Op, WeederError] = d0 match {
-    case ParsedAst.Declaration.Op(doc, ann0, mod0, sp1, ident, tparams0, fparamsOpt0, tpe0, pur0, tconstrs0, sp2) =>
+    case ParsedAst.Declaration.Op(doc0, ann0, mod0, sp1, ident, tparams0, fparamsOpt0, tpe0, pur0, tconstrs0, sp2) =>
+      val doc = visitDoc(doc0)
+      val annVal = visitAnnotations(ann0)
       val modVal = visitModifiers(mod0, legalModifiers = Set(Ast.Modifier.Public))
       val pubVal = requirePublic(mod0, ident)
-      val tparamsVal = ??? // assert no tparams
+      val tparamsVal = requireNoTypeParams(tparams0)
       val fparamsVal = visitFormalParams(fparamsOpt0, typeRequired = true)
-      val tpe = visitType(tpe0)
       val pur = visitEffectOrPurity(pur0, ident.loc)
       val tconstrsVal = traverse(tconstrs0)(visitTypeConstraint)
-      mapN(modVal, pubVal, tparamsVal, fparamsVal, tconstrsVal) {
-        case (mod, _, _, fparams, tconstrs) =>
-          ??? // MATT
+      mapN(annVal, modVal, pubVal, tparamsVal, fparamsVal, tconstrsVal) {
+        case (ann, mod, _, _, fparams, tconstrs) =>
+          val ts = fparams.map(_.tpe.get)
+          val retTpe = visitType(tpe0)
+          val tpe = WeededAst.Type.Arrow(ts, pur, retTpe, ident.loc)
+          WeededAst.Declaration.Op(doc, ann, mod, ident, fparams, tpe, retTpe, pur, tconstrs, mkSL(sp1, sp2));
       }
   }
 
