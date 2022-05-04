cbd2c05bcb0852651e7020d891aa37c3ced06f18
ChrBondi <42577769+ChrBondi@users.noreply.github.com>
2022-05-04 09:42:45 +0200

    inline closureargs

--- main/src/ca/uwaterloo/flix/language/phase/Optimizer.scala
+++ main/src/ca/uwaterloo/flix/language/phase/Optimizer.scala
@@ -35,7 +35,7 @@ object Optimizer {
   def run(root: Root)(implicit flix: Flix): Validation[Root, CompilationMessage] = flix.phase("Optimizer") {
     var result = root
 
-    for (_ <- 1 to -99999) {
+    for (_ <- 1 to 2) {
       val afterOccurrenceAnalyzer = OccurrenceAnalyzer.run(result)
       val afterInliner = Inliner.run(afterOccurrenceAnalyzer.get)
       result = afterInliner.get
