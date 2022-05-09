8af58d8ecfa385d086598c867587e4f489bd7ba2
Matthew Lutze <mlutze@users.noreply.github.com>
2022-05-09 15:29:48 +0200

    test: add more IllegalResume tests

--- main/test/ca/uwaterloo/flix/language/phase/TestWeeder.scala
+++ main/test/ca/uwaterloo/flix/language/phase/TestWeeder.scala
@@ -670,6 +670,34 @@ class TestWeeder extends FunSuite with TestUtils {
     expectError[WeederError.IllegalResume](result)
   }
 
+  test("IllegalResume.02") {
+    val input =
+      """
+        |def f(): Bool =
+        |   try {
+        |       true
+        |   } catch {
+        |       case _: ##java.lang.Exception => resume(true)
+        |   }
+        |""".stripMargin
+    val result = compile(input, Options.TestWithLibNix)
+    expectError[WeederError.IllegalResume](result)
+  }
+
+  test("IllegalResume.03") {
+    val input =
+      """
+        |def f(): Bool =
+        |   try {
+        |       resume(true)
+        |   } with Fail {
+        |       def fail() = false
+        |   }
+        |""".stripMargin
+    val result = compile(input, Options.TestWithLibNix)
+    expectError[WeederError.IllegalResume](result)
+  }
+
   test("IllegalFormalParamAscription.01") {
     val input =
       """
