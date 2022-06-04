c15c26ff3cee8512f402785cbe5073b913d52b42
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2022-06-04 12:44:25 +0200

    Fix doc

--- main/src/ca/uwaterloo/flix/language/ast/TypeConstructor.scala
+++ main/src/ca/uwaterloo/flix/language/ast/TypeConstructor.scala
@@ -225,7 +225,7 @@ object TypeConstructor {
     */
   case object Array extends TypeConstructor {
     /**
-      * The shape of a reference is `Array[t, l]`.
+      * The shape of an array is `Array[t, l]`.
       */
     def kind: Kind = Kind.Star ->: Kind.Bool ->: Kind.Star
   }
