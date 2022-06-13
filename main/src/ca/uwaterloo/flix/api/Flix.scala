b6f90d4c7e26f3a7d08604631a1c1122aba502c4
stephentetley <stephen.tetley@gmail.com>
2022-06-13 22:36:27 +0100

    Adding FunctorFilter class and List, Chain instances.

--- main/src/ca/uwaterloo/flix/api/Flix.scala
+++ main/src/ca/uwaterloo/flix/api/Flix.scala
@@ -178,6 +178,7 @@ class Flix {
     "Reducible.flix" -> LocalResource.get("/src/library/Reducible.flix"),
     "SemiGroup.flix" -> LocalResource.get("/src/library/SemiGroup.flix"),
     "Traversable.flix" -> LocalResource.get("/src/library/Traversable.flix"),
+    "FunctorFilter.flix" -> LocalResource.get("/src/library/FunctorFilter.flix"),
 
     "Validation.flix" -> LocalResource.get("/src/library/Validation.flix"),
 
