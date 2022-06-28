ddef14532e2502c15ad7a9ffc18fc5a5337d96f1
Matthew Lutze <mlutze@users.noreply.github.com>
2022-06-28 13:27:23 +0200

    add some smartness to mkUnion

--- main/src/ca/uwaterloo/flix/language/ast/Type.scala
+++ main/src/ca/uwaterloo/flix/language/ast/Type.scala
@@ -939,7 +939,11 @@ object Type {
     *
     * Must not be used before kinding.
     */
-  def mkUnion(tpe1: Type, tpe2: Type, loc: SourceLocation): Type = Type.mkApply(Type.Cst(TypeConstructor.Union, loc), List(tpe1, tpe2), loc)
+  def mkUnion(tpe1: Type, tpe2: Type, loc: SourceLocation): Type = (tpe1, tpe2) match {
+    case (Empty, t) => t
+    case (t, Empty) => t
+    case _ => mkApply(Type.Cst(TypeConstructor.Union, loc), List(tpe1, tpe2), loc)
+  }
 
   /**
     * Returns the union of all the given types.
