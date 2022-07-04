abba1ce835bdd770ca69eb46223e24f7ca7e379a
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2022-07-04 12:41:05 +0200

    Revert "REVERT ME: Run only 1 test file"
    This reverts commit 933f13b88eac4b91f1cc6391abf057863c15cc57.

--- main/test/flix/CompilerSuite.scala
+++ main/test/flix/CompilerSuite.scala
@@ -5,8 +5,6 @@ import ca.uwaterloo.flix.util.{FlixSuite, Options}
 class CompilerSuite extends FlixSuite(incremental = true) {
   implicit val options: Options = Options.TestWithLibAll
 
-  // REVERT ME
-  //mkTestDir("main/test/flix/")
-  mkTest("main/test/flix/Test.Exp.Foreach.flix")
+  mkTestDir("main/test/flix/")
 
 }
