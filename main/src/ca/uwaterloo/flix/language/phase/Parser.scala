bddb43ba580cf001e3c120b88f6039b9abbda831
Jakob Schneider <60233376+jaschdoc@users.noreply.github.com>
2022-07-05 11:31:40 +0200

    feat: add `foreach (x <- xs; y <- ys) exp` expression (#4144)

--- main/src/ca/uwaterloo/flix/language/phase/Parser.scala
+++ main/src/ca/uwaterloo/flix/language/phase/Parser.scala
@@ -502,8 +502,14 @@ class Parser(val source: Source) extends org.parboiled2.Parser {
       SP ~ keyword("discard") ~ WS ~ Expression ~ SP ~> ParsedAst.Expression.Discard
     }
 
-    def ForEach: Rule1[ParsedAst.Expression.ForEach] = rule {
-      SP ~ keyword("foreach") ~ optWS ~ "(" ~ optWS ~ Pattern ~ WS ~ keyword("<-") ~ WS ~ Expression ~ optWS ~ ")" ~ optWS ~ Expression ~ SP ~> ParsedAst.Expression.ForEach
+    def ForEach: Rule1[ParsedAst.Expression.ForEach] = {
+      def ForEachGenerator: Rule1[ParsedAst.ForEachGenerator] = rule {
+        SP ~ Pattern ~ WS ~ keyword("<-") ~ WS ~ Expression ~ SP ~> ParsedAst.ForEachGenerator
+      }
+
+      rule {
+        SP ~ keyword("foreach") ~ optWS ~ "(" ~ optWS ~ oneOrMore(ForEachGenerator).separatedBy(optWS ~ ";" ~ optWS) ~ optWS ~ ")" ~ optWS ~ Expression ~ SP ~> ParsedAst.Expression.ForEach
+      }
     }
 
     def Assign: Rule1[ParsedAst.Expression] = rule {
