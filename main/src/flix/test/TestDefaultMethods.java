e6ef0d8fb69d63c8aec56c63f2a14a18467ad1bf
Paul Butcher <paul@paulbutcher.com>
2022-07-24 12:14:18 +0100

    Test default methods

---
+++ main/src/flix/test/TestDefaultMethods.java
@@ -0,0 +1,8 @@
+package flix.test;
+
+public interface TestDefaultMethods {
+  int methodWithNoImplementation(int x);
+  default int methodWithDefaultImplementation(int x) {
+    return x + 42;
+  }
+}
