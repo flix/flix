b4f98556cbd0baa751c2543e2e45b923affc1eef
Paul Butcher <paul@paulbutcher.com>
2022-07-24 13:56:42 +0100

    Test generics

--- main/src/flix/test/TestDefaultMethods.java
+++ main/src/flix/test/TestDefaultMethods.java
@@ -2,6 +2,7 @@ package flix.test;
 
 public interface TestDefaultMethods {
   int methodWithNoImplementation(int x);
+
   default int methodWithDefaultImplementation(int x) {
     return x + 42;
   }
