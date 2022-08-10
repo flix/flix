51e4cd659c83662cd6c2dc17227be6fe667ad96a
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2022-08-10 13:10:06 +0200

    refactor: rewrite `Tester`.

--- main/src/ca/uwaterloo/flix/tools/Tester.scala
+++ main/src/ca/uwaterloo/flix/tools/Tester.scala
@@ -40,7 +40,7 @@ object Tester {
 
     // Start the TestRunner and TestReporter.
     val queue = new ConcurrentLinkedQueue[TestEvent]()
-    val reporter = new TestReporter(queue)
+    val reporter = new TestReporter(queue, tests)
     val runner = new TestRunner(queue, tests)
     reporter.start()
     runner.start()
@@ -50,9 +50,10 @@ object Tester {
     runner.join()
   }
 
-  // TODO: DOC
-  private class TestReporter(queue: ConcurrentLinkedQueue[TestEvent])(implicit flix: Flix) extends Thread {
-
+  /**
+    * A class that reports the results of test events as they come in.
+    */
+  private class TestReporter(queue: ConcurrentLinkedQueue[TestEvent], tests: Vector[TestCase])(implicit flix: Flix) extends Thread {
     override def run(): Unit = {
       // Silence JLine warnings about terminal type.
       Logger.getLogger("org.jline").setLevel(Level.OFF)
@@ -62,45 +63,51 @@ object Tester {
         .builder()
         .system(true)
         .build()
+      val writer = terminal.writer()
 
+      // Print headline.
+      writer.println(s"Testing started... Total tests: ${tests.length}.")
+
+      // Main event loop.
       var passed = 0
       var failed = 0
 
-      while (true) {
+      var finished = false
+      while (!finished) {
         queue.poll() match {
           case TestEvent.Success(sym, elapsed) =>
             passed = passed + 1
-            terminal.writer().println(s"  - ${green(sym.toString)} ${magenta(TimeOps.toMilliSeconds(elapsed) + "ms")}")
+            writer.println(s"  - ${green(sym)} ${magenta(TimeOps.toMilliSeconds(elapsed) + "ms")}")
             terminal.flush()
 
           case TestEvent.Failure(sym, elapsed) =>
             failed = failed + 1
-            terminal.writer().println(s"  - ${red(sym.toString)} ${magenta(TimeOps.toMilliSeconds(elapsed) + "ms")}")
+            writer.println(s"  - ${red(sym)} ${magenta(TimeOps.toMilliSeconds(elapsed) + "ms")}")
             terminal.flush()
 
           case TestEvent.Finished(elapsed) =>
-            terminal.writer().println()
-            terminal.writer().println(s"Finished. Passed: ${passed}, Failed: $failed. Elapsed: $elapsed ")
+            writer.println()
+            writer.println(s"Finished. Passed: $passed, Failed: $failed. Elapsed: $elapsed ms.")
             terminal.flush()
-            return
+            finished = true
+
           case _ => // nop
         }
       }
-      throw InternalCompilerException("Unreachable")
     }
 
     // TODO: Use flix.formatter
-    private def green(s: String): String = Console.GREEN + s + Console.RESET
+    private def green(s: AnyRef): String = Console.GREEN + s + Console.RESET
 
     // TODO: Use flix.formatter
-    private def magenta(s: String): String = Console.MAGENTA + s + Console.RESET
+    private def magenta(s: AnyRef): String = Console.MAGENTA + s + Console.RESET
 
     // TODO: Use flix.formatter
-    private def red(s: String): String = Console.RED + s + Console.RESET
+    private def red(s: AnyRef): String = Console.RED + s + Console.RESET
   }
 
   // TODO: DOC
-  private class TestRunner(queue: ConcurrentLinkedQueue[TestEvent], tests: List[TestCase])(implicit flix: Flix) extends Thread {
+  private class TestRunner(queue: ConcurrentLinkedQueue[TestEvent], tests: Vector[TestCase])(implicit flix: Flix) extends Thread {
 
     // TODO: DOC
     override def run(): Unit = {
@@ -139,8 +146,8 @@ object Tester {
   /**
     * Returns all test cases from the given compilation `result`.
     */
-  private def getTestCases(compilationResult: CompilationResult): List[TestCase] =
-    compilationResult.getTests.toList.sortBy(_._1.loc).map {
+  private def getTestCases(compilationResult: CompilationResult): Vector[TestCase] =
+    compilationResult.getTests.toVector.sortBy(_._1.loc).map {
       case (sym, defn) => TestCase(sym, defn)
     }
 
