3c9ffd2c3dfd03dc5aefe91bf64271f6ea6ce8e7
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2022-08-10 14:27:28 +0200

    feat: add console redirection to `Tester`

--- main/src/ca/uwaterloo/flix/util/Duration.scala
+++ main/src/ca/uwaterloo/flix/util/Duration.scala
@@ -43,7 +43,7 @@ object Duration {
 /**
   * A simple class to format a time duration.
   */
-case class Duration(val d: Long) {
+case class Duration(d: Long) {
 
   /**
     * Returns the elapsed time in nanoseconds.
