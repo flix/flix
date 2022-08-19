d5e9d0013856ecf2a74023388620a525bbb8a267
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2022-08-19 13:15:00 +0200

    Add TraverseFilter

--- main/src/ca/uwaterloo/flix/api/Flix.scala
+++ main/src/ca/uwaterloo/flix/api/Flix.scala
@@ -176,6 +176,7 @@ class Flix {
     "Reducible.flix" -> LocalResource.get("/src/library/Reducible.flix"),
     "SemiGroup.flix" -> LocalResource.get("/src/library/SemiGroup.flix"),
     "Traversable.flix" -> LocalResource.get("/src/library/Traversable.flix"),
+    "TraverseFilter.flix" -> LocalResource.get("/src/library/TraverseFilter.flix"),
     "UnorderedFoldable.flix" -> LocalResource.get("/src/library/UnorderedFoldable.flix"),
 
     "Validation.flix" -> LocalResource.get("/src/library/Validation.flix"),
