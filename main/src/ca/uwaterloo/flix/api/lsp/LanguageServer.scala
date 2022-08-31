b4293b3db11bac07ed1166a7e68563296e9e34eb
Matthew Lutze <mlutze@users.noreply.github.com>
2022-08-31 15:03:57 +0200

    fix comp error

--- main/src/ca/uwaterloo/flix/api/lsp/LanguageServer.scala
+++ main/src/ca/uwaterloo/flix/api/lsp/LanguageServer.scala
@@ -263,7 +263,7 @@ class LanguageServer(port: Int) extends WebSocketServer(new InetSocketAddress("l
         ("id" -> id) ~ ("status" -> "success") ~ ("result" -> Nil)
 
     case Request.Complete(id, uri, pos) =>
-      ("id" -> id) ~ CompletionProvider.autoComplete(uri, pos, sources.get(uri), currentErrors)(index, root)
+      ("id" -> id) ~ CompletionProvider.autoComplete(uri, pos, sources.get(uri), currentErrors)(flix, index, root)
 
     case Request.Highlight(id, uri, pos) =>
       ("id" -> id) ~ HighlightProvider.processHighlight(uri, pos)(index, root)
