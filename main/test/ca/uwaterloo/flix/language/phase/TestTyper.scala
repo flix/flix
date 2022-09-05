da22681f87a18131d2ae4f2ff1f6d34659e967d2
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2022-09-05 12:51:15 +0200

    Refactor phase suite

--- main/test/ca/uwaterloo/flix/language/phase/TestTyper.scala
+++ main/test/ca/uwaterloo/flix/language/phase/TestTyper.scala
@@ -95,7 +95,7 @@ class TestTyper extends FunSuite with TestUtils {
   test("TestLeq07") {
     val input =
       """
-        |def foo(): {x :: Int32 | r} = {x = 21}
+        |def foo(): {x = Int32 | r} = {x = 21}
       """.stripMargin
     val result = compile(input, Options.TestWithLibNix)
     expectError[TypeError.GeneralizationError](result)
@@ -104,7 +104,7 @@ class TestTyper extends FunSuite with TestUtils {
   test("TestLeq08") {
     val input =
       """
-        |def foo(): {x :: Int32, y :: Int32 | r} = {y = 42, x = 21}
+        |def foo(): {x = Int32, y = Int32 | r} = {y = 42, x = 21}
       """.stripMargin
     val result = compile(input, Options.TestWithLibNix)
     expectError[TypeError.GeneralizationError](result)
@@ -135,7 +135,7 @@ class TestTyper extends FunSuite with TestUtils {
   }
 
   test("TestMismatchedTypes.03") {
-    val input = "def foo(): {a :: Int32} = {a = 2} <+> {a = 2}"
+    val input = "def foo(): {a = Int32} = {a = 2} <+> {a = 2}"
     val result = compile(input, Options.TestWithLibNix)
     expectError[TypeError.MismatchedTypes](result)
   }
