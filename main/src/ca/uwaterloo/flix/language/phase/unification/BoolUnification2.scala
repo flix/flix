d541f8d73497ade9b8fec8a6ac9e39f6dbcf5605
Matthew Lutze <mlutze@users.noreply.github.com>
2022-09-14 09:11:05 +0200

    define BoolUnification on type class instances

---
+++ main/src/ca/uwaterloo/flix/language/phase/unification/BoolUnification2.scala
@@ -0,0 +1,205 @@
+/*
+ *  Copyright 2020 Magnus Madsen
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package ca.uwaterloo.flix.language.phase.unification
+
+import ca.uwaterloo.flix.api.Flix
+import ca.uwaterloo.flix.language.ast._
+import ca.uwaterloo.flix.language.phase.unification.BoolAlgebra.{VarOrEff, fromBoolType, toType}
+import ca.uwaterloo.flix.language.phase.unification.BoolAlgebraTable.minimizeFormula
+import ca.uwaterloo.flix.util.Result.Ok
+import ca.uwaterloo.flix.util.collection.Bimap
+import ca.uwaterloo.flix.util.{InternalCompilerException, Result}
+
+import scala.util.{Failure, Success, Try}
+
+object BoolUnification2 {
+
+  /**
+    * Returns the most general unifier of the two given Boolean formulas `tpe1` and `tpe2`.
+    */
+  def unify(tpe10: Type, tpe20: Type, renv0: RigidityEnv)(implicit flix: Flix): Result[Substitution, UnificationError] = {
+    ///
+    /// Perform aggressive matching to optimize for common cases.
+    ///
+    if (tpe10 eq tpe20) {
+      return Ok(Substitution.empty)
+    }
+
+    tpe10 match {
+      case x: Type.Var if renv0.isFlexible(x.sym) =>
+        if (tpe20 eq Type.True)
+          return Ok(Substitution.singleton(x.sym, Type.True))
+        if (tpe20 eq Type.False)
+          return Ok(Substitution.singleton(x.sym, Type.False))
+
+      case _ => // nop
+    }
+
+    tpe20 match {
+      case y: Type.Var if renv0.isFlexible(y.sym) =>
+        if (tpe10 eq Type.True)
+          return Ok(Substitution.singleton(y.sym, Type.True))
+        if (tpe10 eq Type.False)
+          return Ok(Substitution.singleton(y.sym, Type.False))
+
+      case _ => // nop
+    }
+
+    // translate the types into formulas
+    // Erase aliases to get a processable type
+    val tpe1 = Type.eraseAliases(tpe10)
+    val tpe2 = Type.eraseAliases(tpe20)
+
+    // Compute the variables in `tpe`.
+    val tvars = (tpe1.typeVars ++ tpe2.typeVars).toList.map(tvar => BoolAlgebra.VarOrEff.Var(tvar.sym))
+
+    // Construct a bi-directional map from type variables to indices.
+    // The idea is that the first variable becomes x0, the next x1, and so forth.
+    val m = tvars.zipWithIndex.foldLeft(Bimap.empty[BoolAlgebra.VarOrEff, BoolAlgebraTable.Variable]) {
+      case (macc, (sym, x)) => macc + (sym -> x)
+    }
+
+    // Convert the type `tpe` to a Boolean formula.
+    val f1 = fromBoolType(tpe1, m)
+
+    val f2 = fromBoolType(tpe2, m)
+
+    ///
+    /// Run the expensive boolean unification algorithm.
+    ///
+    val renv = renv0.s.toList.flatMap(tvar => m.getForward(VarOrEff.Var(tvar))).toSet
+    booleanUnification(f1, f2, renv) match {
+      case Success(subst0) =>
+        val map = subst0.m.toList.map {
+          case (key0, value0) =>
+            val key = m.getBackward(key0) match {
+              case Some(VarOrEff.Var(sym)) => sym
+              case _ => throw InternalCompilerException(s"unexpected missing var $key0")
+            }
+            val value = toType(value0, m, Kind.Bool, tpe1.loc)
+            (key: Symbol.TypeVarSym, value)
+        }.toMap
+        Ok(Substitution(map))
+      case Failure(BooleanUnificationException) => Result.Err(UnificationError.MismatchedBools(tpe1, tpe2))
+      case Failure(error) => throw error
+    }
+  }
+
+  /**
+    * Returns the most general unifier of the two given Boolean formulas `tpe1` and `tpe2`.
+    */
+  private def booleanUnification[F](tpe1: F, tpe2: F, renv: Set[Int])(implicit flix: Flix, alg: BoolAlgTrait[F]): Try[BoolAlgebraSubstitution2[F]] = {
+    // The boolean expression we want to show is 0.
+    val query = mkEq(tpe1, tpe2)
+
+    // Compute the variables in the query.
+    val typeVars = alg.freeVars(query).toList
+
+    // Compute the flexible variables.
+    val flexibleTypeVars = typeVars.filterNot(renv.contains)
+
+    // Determine the order in which to eliminate the variables.
+    val freeVars = computeVariableOrder(flexibleTypeVars)
+
+    // Eliminate all variables.
+    Try {
+      val subst = successiveVariableElimination(query, freeVars)
+
+      //    if (!subst.isEmpty) {
+      //      val s = subst.toString
+      //      val len = s.length
+      //      if (len > 50) {
+      //        println(s.substring(0, Math.min(len, 300)))
+      //        println()
+      //      }
+      //    }
+
+      subst
+    }
+  }
+
+  /**
+    * A heuristic used to determine the order in which to eliminate variable.
+    *
+    * Semantically the order of variables is immaterial. Changing the order may
+    * yield different unifiers, but they are all equivalent. However, changing
+    * the can lead to significant speed-ups / slow-downs.
+    *
+    * We make the following observation:
+    *
+    * We want to have synthetic variables (i.e. fresh variables introduced during
+    * type inference) expressed in terms of real variables (i.e. variables that
+    * actually occur in the source code). We can ensure this by eliminating the
+    * synthetic variables first.
+    */
+  private def computeVariableOrder(l: List[Int]): List[Int] = {
+    l // TODO hack
+  }
+
+  /**
+    * Performs success variable elimination on the given boolean expression `f`.
+    *
+    * `flexvs` is the list of remaining flexible variables in the expression.
+    */
+  private def successiveVariableElimination[F](f: F, flexvs: List[Int])(implicit flix: Flix, alg: BoolAlgTrait[F]): BoolAlgebraSubstitution2[F] = flexvs match {
+    case Nil =>
+      // Determine if f is unsatisfiable when all (rigid) variables are made flexible.
+      if (!satisfiable(f))
+        BoolAlgebraSubstitution2.empty
+      else
+        throw BooleanUnificationException
+
+    case x :: xs =>
+      val t0 = BoolAlgebraSubstitution2.singleton(x, alg.mkFalse)(f)
+      val t1 = BoolAlgebraSubstitution2.singleton(x, alg.mkTrue)(f)
+      val se = successiveVariableElimination(alg.mkAnd(t0, t1), xs)
+
+      val f1 = alg.minimize(alg.mkOr(se(t0), alg.mkAnd(alg.mkVar(x), alg.mkNot(se(t1)))))
+      val st = BoolAlgebraSubstitution2.singleton(x, f1)
+      st ++ se
+  }
+
+  /**
+    * An exception thrown to indicate that boolean unification failed.
+    */
+  private case object BooleanUnificationException extends RuntimeException
+
+  /**
+    * Returns `true` if the given boolean formula `f` is satisfiable
+    * when ALL variables in the formula are flexible.
+    */
+  private def satisfiable[F](f: F)(implicit flix: Flix, alg: BoolAlgTrait[F]): Boolean = {
+    if (f == alg.mkTrue) {
+      true
+    } else if (f == alg.mkFalse) {
+      false
+    } else {
+      val q = mkEq(f, alg.mkTrue)
+      try {
+        successiveVariableElimination(q, alg.freeVars(q).toList)
+        true
+      } catch {
+        case BooleanUnificationException => false
+      }
+    }
+  }
+
+
+  /**
+    * To unify two Boolean formulas p and q it suffices to unify t = (p ∧ ¬q) ∨ (¬p ∧ q) and check t = 0.
+    */
+  private def mkEq[F](p: F, q: F)(implicit alg: BoolAlgTrait[F]): F = alg.mkOr(alg.mkAnd(p, alg.mkNot(q)), alg.mkAnd(alg.mkNot(p), q))
+}
