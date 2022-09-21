a7d3ba7da0ea4f54a2a64d426622e1de300a930e
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2022-09-21 18:42:31 +0200

    experiment: measure overhead of effect monomorphization

--- experiments/run.py
+++ experiments/run.py
@@ -5,13 +5,16 @@ ITERATIONS = 1
 
 for pkg in glob.glob("*.fpkg"):
     print(pkg)
-    run1 = subprocess.run(["java", "-jar", "flix.jar", "--Xno-optimizer", pkg], capture_output=True, text=True)
+
+    # Baseline
+    run1 = subprocess.run(["java", "-jar", "flix.jar", "--Xno-optimizer", "--Xno-reifyeff", pkg], capture_output=True, text=True)
     lines1, time1, totalBytes1, totalClasses1 = run1.stdout.split(",")
     
-    run2 = subprocess.run(["java", "-jar", "flix.jar", "--Xno-optimizer", "--Xno-reifyeff", pkg], capture_output=True, text=True)
+    # Extended
+    run2 = subprocess.run(["java", "-jar", "flix.jar", "--Xno-optimizer", pkg], capture_output=True, text=True)
     lines2, time2, totalBytes2, totalClasses2 = run2.stdout.split(",")
 
-    classRatio = round((100 * int(totalClasses1)) / int(totalClasses2))
+    classRatio = round((100 * int(totalClasses2)) / int(totalClasses1))
 
     print(f'Lines: {int(lines1)}, Classes: {int(totalClasses1)} / {int(totalClasses2)} ({classRatio}%)')
     print()
