6215d7b73bf358ef6cd7311eef3bd59686e0c7a3
JonathanStarup <32037926+JonathanStarup@users.noreply.github.com>
2022-10-11 12:07:31 +0200

    fix

--- main/src/ca/uwaterloo/flix/language/phase/jvm/GenTagClasses.scala
+++ main/src/ca/uwaterloo/flix/language/phase/jvm/GenTagClasses.scala
@@ -218,31 +218,39 @@ object GenTagClasses {
   }
 
   def compileToStringMethod(visitor: ClassWriter, classType: JvmType.Reference, tag: TagInfo)(implicit root: Root, flix: Flix): Unit = {
-    val printParanthesis = tag.tagType match {
-      case MonoType.Tuple(_) => false
-      case _ => true
-    }
     val method = visitor.visitMethod(ACC_PUBLIC + ACC_FINAL, "toString", AsmOps.getMethodDescriptor(Nil, JvmType.String), null, null)
-    method.visitLdcInsn("") // for last join call
+    tag.tagType match {
+      case MonoType.Unit => // "$Tag"
+        method.visitLdcInsn(tag.tag)
+        method.visitInsn(ARETURN)
 
-    method.visitInsn(ICONST_3)
-    method.visitTypeInsn(ANEWARRAY, "java/lang/String")
-    method.visitInsn(DUP)
-    method.visitInsn(ICONST_0)
-    method.visitLdcInsn(tag.tag + (if (printParanthesis) "(" else ""))
-    method.visitInsn(AASTORE)
-    method.visitInsn(DUP)
-    method.visitInsn(ICONST_1)
-    method.visitVarInsn(ALOAD, 0)
-    method.visitMethodInsn(INVOKEVIRTUAL, classType.name.toInternalName, "getBoxedTagValue", AsmOps.getMethodDescriptor(Nil, JvmType.Object), false)
-    method.visitMethodInsn(INVOKEVIRTUAL, JvmType.Object.name.toInternalName, "toString", AsmOps.getMethodDescriptor(Nil, JvmType.String), false)
-    method.visitInsn(AASTORE)
-    method.visitInsn(DUP)
-    method.visitInsn(ICONST_2)
-    method.visitLdcInsn(if (printParanthesis) ")" else "")
-    method.visitInsn(AASTORE)
-    method.visitMethodInsn(INVOKESTATIC, JvmType.String.name.toInternalName, "join", "(Ljava/lang/CharSequence;[Ljava/lang/CharSequence;)Ljava/lang/String;", false)
-    method.visitInsn(ARETURN)
+      case _ => // "$Tag($value)" or "$Tag$value" if value already prints "(...)"
+        val printParanthesis = tag.tagType match {
+          case MonoType.Tuple(_) => false
+          case MonoType.Unit => false
+          case _ => true
+        }
+        method.visitLdcInsn("") // for last join call
+
+        method.visitInsn(ICONST_3)
+        method.visitTypeInsn(ANEWARRAY, JvmType.String.name.toInternalName)
+        method.visitInsn(DUP)
+        method.visitInsn(ICONST_0)
+        method.visitLdcInsn(tag.tag + (if (printParanthesis) "(" else ""))
+        method.visitInsn(AASTORE)
+        method.visitInsn(DUP)
+        method.visitInsn(ICONST_1)
+        method.visitVarInsn(ALOAD, 0)
+        method.visitMethodInsn(INVOKEVIRTUAL, classType.name.toInternalName, "getBoxedTagValue", AsmOps.getMethodDescriptor(Nil, JvmType.Object), false)
+        method.visitMethodInsn(INVOKEVIRTUAL, JvmType.Object.name.toInternalName, "toString", AsmOps.getMethodDescriptor(Nil, JvmType.String), false)
+        method.visitInsn(AASTORE)
+        method.visitInsn(DUP)
+        method.visitInsn(ICONST_2)
+        method.visitLdcInsn(if (printParanthesis) ")" else "")
+        method.visitInsn(AASTORE)
+        method.visitMethodInsn(INVOKESTATIC, JvmType.String.name.toInternalName, "join", "(Ljava/lang/CharSequence;[Ljava/lang/CharSequence;)Ljava/lang/String;", false)
+        method.visitInsn(ARETURN)
+    }
     method.visitMaxs(1, 1)
     method.visitEnd()
   }
