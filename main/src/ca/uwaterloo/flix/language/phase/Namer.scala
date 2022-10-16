09c234d80c073cf36053b182b37efecbd4a17b91
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2022-10-16 15:50:29 +0200

    Update Namer

--- main/src/ca/uwaterloo/flix/language/phase/Namer.scala
+++ main/src/ca/uwaterloo/flix/language/phase/Namer.scala
@@ -55,9 +55,9 @@ object Namer {
 
     // collect all the declarations.
     val declarations = mapN(traverse(program.units.values) {
-      case root => 
+      case root =>
         flatMapN(mergeUseEnvs(root.uses, Name.RootNS, UseEnv.empty, ImportEnv.empty, prog0)) {
-          case uenv0 => 
+          case uenv0 =>
             mapN(mergeImportEnvs(root.imports, ImportEnv.empty, uenv0, Name.RootNS, prog0)) {
               case ienv0 => root.decls.map(d => (uenv0, ienv0, d))
             }
@@ -1051,7 +1051,7 @@ object Namer {
       }
 
     case WeededAst.Expression.ParYield(exp, loc) =>
-      mapN(visitExp(exp, env0, uenv0, ienv0, tenv0)) {
+      mapN(visitExp(exp, env0, uenv0, ienv0, tenv0, ns0, prog0)) {
         case e => NamedAst.Expression.ParYield(e, loc)
       }
 
