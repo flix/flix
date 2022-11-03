d3ca5536395627352e56c144cc24294ecb2a08e4
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2022-11-03 21:08:40 +0100

    Revert CompilerSuite

--- main/test/flix/CompilerSuite.scala
+++ main/test/flix/CompilerSuite.scala
@@ -5,7 +5,6 @@ import ca.uwaterloo.flix.util.{FlixSuite, Options}
 class CompilerSuite extends FlixSuite(incremental = true) {
   implicit val options: Options = Options.TestWithLibAll
 
-  mkTest("main/test/flix/Test.Exp.Par.flix")
-  mkTest("main/test/flix/Test.Exp.ParYield.flix")
+  mkTestDir("main/test/flix/")
 
 }
