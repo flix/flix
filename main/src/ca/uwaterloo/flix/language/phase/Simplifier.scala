369dc96e2ba5b49c4ae1796efbb2c774a3392374
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2022-11-04 20:31:20 +0100

    Add InternalCompilerException

--- main/src/ca/uwaterloo/flix/language/phase/Simplifier.scala
+++ main/src/ca/uwaterloo/flix/language/phase/Simplifier.scala
@@ -337,7 +337,8 @@ object Simplifier {
       case TypedAst.Expression.Par(_, _) =>
         throw InternalCompilerException(s"Unexpected expression: $exp0.")
 
-      case TypedAst.Expression.ParYield(frags, exp, tpe, pur, eff, loc) => ???
+      case TypedAst.Expression.ParYield(_, _, _, _, _, _) =>
+        throw InternalCompilerException(s"Unexpected expression: $exp0.")
 
       case TypedAst.Expression.Mask(_, _, _, _, _) =>
         throw InternalCompilerException(s"Unexpected expression: $exp0.")
