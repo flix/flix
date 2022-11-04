3ca59f23de3b5263892d6fcb672b175c4331e64f
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2022-11-04 20:33:04 +0100

    Add CodeHinter phase

--- main/src/ca/uwaterloo/flix/language/phase/extra/CodeHinter.scala
+++ main/src/ca/uwaterloo/flix/language/phase/extra/CodeHinter.scala
@@ -295,7 +295,10 @@ object CodeHinter {
     case Expression.Par(exp, _) =>
       visitExp(exp)
 
-    case Expression.ParYield(frags, exp, tpe, pur, eff, loc) => ???
+    case Expression.ParYield(frags, exp, _, _, _, _) =>
+      frags.flatMap {
+        case ParYieldFragment(_, e, _) => visitExp(e)
+      } ++ visitExp(exp)
 
     case Expression.Lazy(exp, _, _) =>
       visitExp(exp)
