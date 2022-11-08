c5711a1fd039a08d7346c6f663ce32d44449466a
Anna Blume <65231594+AnnaBlume99@users.noreply.github.com>
2022-11-08 14:37:09 +0100

    Clean up and delete old code

--- main/src/ca/uwaterloo/flix/language/phase/unification/BddFormula.scala
+++ main/src/ca/uwaterloo/flix/language/phase/unification/BddFormula.scala
@@ -20,19 +20,14 @@ import ca.uwaterloo.flix.util.InternalCompilerException
 import ca.uwaterloo.flix.util.collection.Bimap
 
 import scala.collection.immutable.SortedSet
-import com.juliasoft.beedeedee.bdd.BDD
-import com.juliasoft.beedeedee.factories.Factory
-
-import java.util.concurrent.locks.ReentrantLock
 import scala.collection.mutable
 import scala.jdk.CollectionConverters._
-import scala.sys.exit
-
+import com.juliasoft.beedeedee.bdd.BDD
+import com.juliasoft.beedeedee.factories.Factory
 
 object BddFormula {
 
   val factory: Factory = Factory.mk(1000 * 1000, 100000)
-  val lock = new ReentrantLock()
 
   class BddFormula(val dd: BDD) {
     def getDD(): BDD = dd
@@ -91,6 +86,9 @@ object BddFormula {
       new BddFormula(f1.getDD().and(f2.getDD()))
     }
 
+    /**
+      * Returns a representation of the formula `f1 xor f2`.
+      */
     override def mkXor(f1: BddFormula, f2: BddFormula): BddFormula = {
       new BddFormula(f1.getDD().xor(f2.getDD()))
     }
@@ -102,6 +100,9 @@ object BddFormula {
       freeVarsAux(f.getDD())
     }
 
+    /**
+      * Helper function for freeVars (works on internal BDD)
+      */
     def freeVarsAux(dd: BDD): SortedSet[Int] = {
       var res: SortedSet[Int] = SortedSet.empty
       if (dd.isOne || dd.isZero) {
@@ -119,13 +120,21 @@ object BddFormula {
     /**
       * Applies the function `fn` to every variable in `f`.
       */
-      //TODO: Check correctness
     override def map(f: BddFormula)(fn: Int => BddFormula): BddFormula = {
+      //if f is true or false, map has no effect
       if(f.getDD().isOne || f.getDD().isZero) {
         f
       } else {
-        val varSetF = freeVars(f)
+        /*to avoid problems with overlapping variables in f and the results of fn
+          each variable x in f or a result of fn on a variable from f, must get a
+          fresh name x'. Then for each variable i in f, substitute all variables
+          in fn(i) with their primed versions. Then substitute i for the primed
+          version of fn(i). At the end go through all primed variables in f and
+          replace them with their unprimed versions.
+        */
 
+        //collect all the variables in f and the substitutions for variables in f
+        val varSetF = freeVars(f)
         var varSetFull = varSetF
         for(var_i <- varSetF) {
           val subst_i = fn(var_i)
@@ -133,7 +142,8 @@ object BddFormula {
           varSetFull = varSetFull ++ varSet_i
         }
 
-        //make x -> x' map
+        //make x -> x' map and x' -> x map
+        //cannot use a BiMap, since backward map must be mutable
         val maxVar = varSetFull.max
         val noVars = varSetFull.size
         val newVarNames = (maxVar+1 to maxVar+noVars).toList
@@ -145,8 +155,10 @@ object BddFormula {
         }
         var res = f.getDD()
 
-        //for each i in varSet create BddFormula' with primed variables
-        //and compose f with BddFormula'
+        //for each i in the variable set for f, get fn(i), create fn(i)'
+        //with primed variables and compose f with fn(i)'
+        //if fn(i) is true or false, just restrict f
+        //if fn(i) = i, do not do anything
         for (var_i <- varSetF) {
           val subst = fn(var_i)
           var substDD = subst.getDD()
@@ -160,20 +172,30 @@ object BddFormula {
           }
         }
 
+        //replace all primed xs in the result with their unprimed versions
+        //to avoid problems with the replace operation, handle all variables
+        //that occur both in primed and unprimed versions separately
         val varSetRes = freeVarsAux(res)
         for(var_k <- varSetRes) {
+          //check whether k is an unprimed variable
           if(varMapForward contains var_k) {
             val k_prime = varMapForward.get(var_k) match  {
               case Some(k) => k
               case None => ??? //cannot happen
             }
+
+            //removed the primed version of k to avoid problems
             varMapBackward.remove(k_prime)
+
+            //replace any primed occurrences of k with the unprimed version
             if(varSetRes.contains(k_prime)) {
               res = res.compose(factory.makeVar(var_k), k_prime)
             }
           }
         }
 
+        //now the backward map only contains the primed variables that
+        //do not occur as unprimed so the replacement is safe
         res = res.replace(varMapBackward.asJava.asInstanceOf[java.util.Map[Integer,Integer]])
 
         new BddFormula(res)
@@ -206,28 +228,38 @@ object BddFormula {
       * Converts the given formula f into a type.
       */
     override def toType(f: BddFormula, env: Bimap[Symbol.KindedTypeVarSym, Int]): Type = {
-      //printBdd(f.getDD(), "toType")
       createTypeFromBDDAux(f.getDD(), Type.True, env)
     }
 
-    //TODO: Optimize
+    /**
+      * Helper function for toType that collects true paths in the BDD and OR them together
+      * TODO: Optimize using 2-level minimization
+      */
     private def createTypeFromBDDAux(dd: BDD, tpe: Type, env: Bimap[Symbol.KindedTypeVarSym, Int]): Type = {
+      //at the bottom of a true path - return the collected path
       if(dd.isOne()) return tpe
+      //at the bottom of a false path - throw away this path
       if(dd.isZero()) return Type.False
 
+      //not at the bottom yet - get the current variable and
+      //find the corresponding type var in the environment
       val currentVar = dd.`var`()
       val typeVar = env.getBackward(currentVar) match {
         case Some(sym) => Type.Var(sym, SourceLocation.Unknown)
-        case None => /*printBdd(dd, "Wrong dd"); println(env);*/ throw InternalCompilerException(s"unexpected unknown ID: $currentVar")
+        case None => throw InternalCompilerException(s"unexpected unknown ID: $currentVar")
       }
 
+      //expand the path so far into low and high paths
+      //(make and AND with the type var in positive or negative form)
+      //and call recursively on the low and high branches
       val lowType = Type.mkApply(Type.And, List(tpe, Type.Apply(Type.Not, typeVar, SourceLocation.Unknown)), SourceLocation.Unknown)
       val lowRes = createTypeFromBDDAux(dd.low(), lowType, env)
       val highType = Type.mkApply(Type.And, List(tpe, typeVar), SourceLocation.Unknown)
       val highRes = createTypeFromBDDAux(dd.high(), highType, env)
 
+      //if the result from either branch is false, just use the other
+      //if both are not false, make an OR of them
       (lowRes, highRes) match {
-        case (Type.False, Type.False) => Type.False
         case (Type.False, _) => highRes
         case (_, Type.False) => lowRes
         case (_, _) => Type.mkApply(Type.Or, List(lowRes, highRes), SourceLocation.Unknown)
@@ -235,36 +267,10 @@ object BddFormula {
     }
 
     /**
-      * Optional operation. Returns `None` if not implemented.
-      *
       * Returns `Some(true)` if `f` is satisfiable (i.e. has a satisfying assignment).
       * Returns `Some(false)` otherwise.
       */
     override def satisfiable(f: BddFormula): Option[Boolean] = Some(!isFalse(f))
 
-    def printBdd(bdd : BDD, title : String) {
-      lock.lock()
-      println(title)
-      __printBdd(" ", bdd, false);
-      lock.unlock()
-    }
-
-    //HELPER workhorse function for printing the bdd
-    def __printBdd (prefix : String, bdd : BDD, isLeft : Boolean) {
-      print(prefix)
-      print(if (isLeft) "├─T─" else "└─F─")
-
-      if (bdd.isOne || bdd.isZero) {
-        println(bdd.isOne())
-        return
-      } else {
-        println("x" + bdd.`var`())
-      }
-
-      val newString : String = if (isLeft) "│   " else "    "
-      // enter the next tree level - left and right branch
-      __printBdd(prefix + newString, bdd.high(), true)
-      __printBdd(prefix + newString, bdd.low(), false)
-    }
   }
 }
