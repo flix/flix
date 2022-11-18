a3f6a25cb227394666fe4abfbbfacce30729d5d9
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2022-11-18 23:35:41 +0100

    feat: add `UnificationCache`

--- main/src/ca/uwaterloo/flix/language/phase/unification/BoolFormula.scala
+++ main/src/ca/uwaterloo/flix/language/phase/unification/BoolFormula.scala
@@ -248,10 +248,6 @@ object BoolFormula {
       case (_, BoolFormula.False) =>
         BoolFormula.False
 
-      // ¬x ∧ (x ∨ y) => ¬x ∧ y
-      case (BoolFormula.Not(x1), BoolFormula.Or(x2, y)) if x1 == x2 =>
-        mkAnd(mkNot(x1), y)
-
       // x ∧ ¬x => F
       case (x1, BoolFormula.Not(x2)) if x1 == x2 =>
         BoolFormula.False
@@ -259,14 +255,6 @@ object BoolFormula {
       // ¬x ∧ x => F
       case (BoolFormula.Not(x1), x2) if x1 == x2 =>
         BoolFormula.False
-
-      // x ∧ (x ∧ y) => (x ∧ y)
-      case (x1, BoolFormula.And(x2, y)) if x1 == x2 =>
-        mkAnd(x1, y)
-
-      // x ∧ (y ∧ x) => (x ∧ y)
-      case (x1, BoolFormula.And(y, x2)) if x1 == x2 =>
-        mkAnd(x1, y)
 
       // (x ∧ y) ∧ x) => (x ∧ y)
       case (BoolFormula.And(x1, y), x2) if x1 == x2 =>
@@ -276,40 +264,9 @@ object BoolFormula {
       case (BoolFormula.And(x, y1), y2) if y1 == y2 =>
         mkAnd(x, y1)
 
-      // x ∧ (x ∨ y) => x
-      case (x1, BoolFormula.Or(x2, _)) if x1 == x2 =>
-        x1
-
-      // (x ∨ y) ∧ x => x
-      case (BoolFormula.Or(x1, _), x2) if x1 == x2 =>
-        x1
-
-      // x ∧ (y ∧ ¬x) => F
-      case (x1, BoolFormula.And(_, BoolFormula.Not(x2))) if x1 == x2 =>
-        BoolFormula.False
-
-      // (¬x ∧ y) ∧ x => F
-      case (BoolFormula.And(BoolFormula.Not(x1), _), x2) if x1 == x2 =>
-        BoolFormula.False
-
-      // x ∧ ¬(x ∨ y) => F
-      case (x1, BoolFormula.Not(BoolFormula.Or(x2, _))) if x1 == x2 =>
-        BoolFormula.False
-
-      // ¬(x ∨ y) ∧ x => F
-      case (BoolFormula.Not(BoolFormula.Or(x1, _)), x2) if x1 == x2 =>
-        BoolFormula.False
-
-      // x ∧ (¬x ∧ y) => F
-      case (x1, BoolFormula.And(BoolFormula.Not(x2), _)) if x1 == x2 =>
-        BoolFormula.False
-
-      // (¬x ∧ y) ∧ x => F
-      case (BoolFormula.And(BoolFormula.Not(x1), _), x2) if x1 == x2 =>
-        BoolFormula.False
-
       // x ∧ x => x
-      case _ if alg1 == alg2 => alg1
+      case _ if alg1 == alg2 =>
+        alg1
 
       case _ =>
         //      val s = s"And($eff1, $eff2)"
@@ -321,7 +278,6 @@ object BoolFormula {
         BoolFormula.And(alg1, alg2)
     }
 
-    @tailrec
     override def mkOr(tpe1: BoolFormula, tpe2: BoolFormula): BoolFormula = (tpe1, tpe2) match {
       // T ∨ x => T
       case (BoolFormula.True, _) =>
@@ -339,14 +295,6 @@ object BoolFormula {
       case (_, BoolFormula.False) =>
         tpe1
 
-      // x ∨ (y ∨ x) => x ∨ y
-      case (x1, BoolFormula.Or(y, x2)) if x1 == x2 =>
-        mkOr(x1, y)
-
-      // (x ∨ y) ∨ x => x ∨ y
-      case (BoolFormula.Or(x1, y), x2) if x1 == x2 =>
-        mkOr(x1, y)
-
       // ¬x ∨ x => T
       case (BoolFormula.Not(x), y) if x == y =>
         BoolFormula.True
@@ -354,24 +302,6 @@ object BoolFormula {
       // x ∨ ¬x => T
       case (x, BoolFormula.Not(y)) if x == y =>
         BoolFormula.True
-
-      // (¬x ∨ y) ∨ x) => T
-      case (BoolFormula.Or(BoolFormula.Not(x), _), y) if x == y =>
-        BoolFormula.True
-
-      // x ∨ (¬x ∨ y) => T
-      case (x, BoolFormula.Or(BoolFormula.Not(y), _)) if x == y =>
-        BoolFormula.True
-
-      // x ∨ (y ∧ x) => x
-      case (x1, BoolFormula.And(_, x2)) if x1 == x2 => x1
-
-      // (y ∧ x) ∨ x => x
-      case (BoolFormula.And(_, x1), x2) if x1 == x2 => x1
-
-      // x ∨ x => x
-      case _ if tpe1 == tpe2 =>
-        tpe1
 
       case _ =>
 
@@ -393,14 +323,6 @@ object BoolFormula {
 
       case BoolFormula.Not(x) =>
         x
-
-      // ¬(¬x ∨ y) => x ∧ ¬y
-      case BoolFormula.Or(BoolFormula.Not(x), y) =>
-        mkAnd(x, mkNot(y))
-
-      // ¬(x ∨ ¬y) => ¬x ∧ y
-      case BoolFormula.Or(x, BoolFormula.Not(y)) =>
-        mkAnd(mkNot(x), y)
 
       case _ => BoolFormula.Not(f0)
     }
