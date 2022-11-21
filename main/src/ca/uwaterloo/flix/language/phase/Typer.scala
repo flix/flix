4377588dfe7bd58c93c4ecc79e383db19cd451e3
Matthew Lutze <mlutze@users.noreply.github.com>
2022-11-21 14:54:29 +0100

    refactor: bring use expressions through Typer

--- main/src/ca/uwaterloo/flix/language/phase/Typer.scala
+++ main/src/ca/uwaterloo/flix/language/phase/Typer.scala
@@ -63,7 +63,7 @@ object Typer {
   private def collectModules(root: KindedAst.Root): Map[Symbol.ModuleSym, List[Symbol]] = root match {
     case KindedAst.Root(classes, _, defs, enums, effects, typeAliases, _, _, _) =>
       val sigs = classes.values.flatMap { clazz => clazz.sigs.values.map(_.sym) }
-      val ops = effects.values.flatMap{ eff => eff.ops.map(_.sym) }
+      val ops = effects.values.flatMap { eff => eff.ops.map(_.sym) }
 
       val syms = classes.keys ++ defs.keys ++ enums.keys ++ effects.keys ++ typeAliases.keys ++ sigs ++ ops
 
@@ -1888,8 +1888,9 @@ object Typer {
       case KindedAst.Expression.Hole(sym, tpe, loc) =>
         TypedAst.Expression.Hole(sym, subst0(tpe), loc)
 
-      // TODO don't erase
-      case KindedAst.Expression.Use(sym, exp, loc) => visitExp(exp, subst0)
+      case KindedAst.Expression.Use(sym, exp, loc) =>
+        val e = visitExp(exp, subst0)
+        TypedAst.Expression.Use(sym, e, loc)
 
       // change null to Unit type
       case KindedAst.Expression.Cst(Ast.Constant.Null, loc) => TypedAst.Expression.Cst(Ast.Constant.Null, Type.Unit, loc)
