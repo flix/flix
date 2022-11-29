3c13b2d6bd08ec54b98f677039ca9174180c49ea
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2022-11-29 21:16:14 +0100

    Refactor tests

--- main/test/ca/uwaterloo/flix/language/phase/TestSafety.scala
+++ main/test/ca/uwaterloo/flix/language/phase/TestSafety.scala
@@ -441,17 +441,6 @@ class TestSafety extends FunSuite with TestUtils {
     expectError[SafetyError.UnsafeUpcast](result)
   }
 
-  test("TestUpcast.12") {
-    val input =
-      """
-        |def f(g: a -> b): (a -> b \ ef) =
-        |    upcast g: a -> b
-        |""".stripMargin
-
-    val result = compile(input, Options.TestWithLibNix)
-    expectError[SafetyError.UnsafeUpcast](result)
-  }
-
   test("TestNonDefaultConstructor.01") {
     val input =
       """
