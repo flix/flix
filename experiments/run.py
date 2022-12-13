544c46a6a1239f661c47cdcc1acf63bd0a6dd5df
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2022-12-13 22:05:20 +0100

    experiments: Boolean unification

--- experiments/run.py
+++ experiments/run.py
@@ -2,11 +2,10 @@ import glob
 import subprocess
 import json 
 
-for i in [0, 1, 2, 3, 4, 5, 6, 7, 8]:
+for i in [0, 1, 2, 3, 4, 5, 6, 1000]:
     run = subprocess.run(["java", "-jar", "flix.jar", "--json", "--Xbenchmark-throughput", "--Xbdd-threshold", str(i)], capture_output=True, text=True)
     data = json.loads(run.stdout)
     throughput = data["throughput"]["avg"]
     print(f'{int(i):,}', end='')
     print(f' & {int(throughput):,}', end='')
     print(" \\\\")
-
