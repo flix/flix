b51c203fd9c1fbfe12b21126ffb89b163e095181
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2022-12-14 22:48:30 +0100

    experiments: boolean unif

--- experiments/run.py
+++ experiments/run.py
@@ -5,7 +5,14 @@ import json
 for i in [0, 1, 2, 3, 4, 5, 6, 1000]:
     run = subprocess.run(["java", "-jar", "flix.jar", "--json", "--Xbenchmark-throughput", "--Xbdd-threshold", str(i)], capture_output=True, text=True)
     data = json.loads(run.stdout)
-    throughput = data["throughput"]["avg"]
+    minimum = data["throughput"]["min"]
+    maximum = data["throughput"]["max"]
+    average = data["throughput"]["avg"]
+    median = data["throughput"]["median"]
+    
     print(f'{int(i):,}', end='')
-    print(f' & {int(throughput):,}', end='')
+    print(f' & {int(minimum):,}', end='')
+    print(f' & {int(maximum):,}', end='')
+    print(f' & {int(average):,}', end='')
+    print(f' & {int(median):,}', end='')
     print(" \\\\")
