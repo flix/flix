03cb83c10cf8f142e7f33f17d64c2d96505b916d
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2022-12-23 18:55:27 +0100

    Remove option from ArrayNew & swap AST params to match flix syntax

--- main/src/ca/uwaterloo/flix/language/phase/Weeder.scala
+++ main/src/ca/uwaterloo/flix/language/phase/Weeder.scala
@@ -657,7 +657,7 @@ object Weeder {
           case ("CHANNEL_PUT", e1 :: e2 :: Nil) => WeededAst.Expression.PutChannel(e1, e2, loc).toSuccess
           case ("CHANNEL_NEW", e1 :: e2 :: Nil) => WeededAst.Expression.NewChannel(e1, e2, loc).toSuccess
 
-          case ("ARRAY_REPEAT", e1 :: e2 :: e3 :: Nil) => WeededAst.Expression.ArrayNew(e2, e3, Some(e1), loc).toSuccess
+          case ("ARRAY_REPEAT", e1 :: e2 :: e3 :: Nil) => WeededAst.Expression.ArrayNew(e1, e2, e3, loc).toSuccess
           case ("ARRAY_LENGTH", e1 :: Nil) => WeededAst.Expression.ArrayLength(e1, loc).toSuccess
 
           case _ => WeederError.IllegalIntrinsic(loc).toFailure
