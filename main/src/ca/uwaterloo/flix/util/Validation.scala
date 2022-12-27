1d848fda096f4e1a8fa0a33aa3b996af30bf5f04
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2022-12-27 15:00:42 +0100

    Remove unused `curry` functions

--- main/src/ca/uwaterloo/flix/util/Validation.scala
+++ main/src/ca/uwaterloo/flix/util/Validation.scala
@@ -241,12 +241,6 @@ object Validation {
         Failure(errs1 #::: errs2)
     }
 
-  private def curry[T1, T2, T3](f: (T1, T2) => T3): T1 => T2 => T3 =
-    (t1: T1) => (t2: T2) => f(t1, t2)
-
-  private def curry[T1, T2, T3, T4](f: (T1, T2, T3) => T4): (T1, T2) => T3 => T4 =
-    (t1: T1, t2: T2) => (t3: T3) => f(t1, t2, t3)
-
   private def curry[T1, T2, T3, T4, T5](f: (T1, T2, T3, T4) => T5): (T1, T2, T3) => T4 => T5 =
     (t1: T1, t2: T2, t3: T3) => (t4: T4) => f(t1, t2, t3, t4)
 
