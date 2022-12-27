7c0c0a0801bad9bddee9dffa3f956b103d7ffde0
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2022-12-27 16:37:16 +0100

    Add doc

--- main/src/ca/uwaterloo/flix/util/Validation.scala
+++ main/src/ca/uwaterloo/flix/util/Validation.scala
@@ -252,42 +252,42 @@ object Validation {
     }
 
   /**
-    * Returns `f` where the last parameter is curried.
+    * Returns `f` with the last parameter curried.
     */
   private def curry[T1, T2, T3, T4, T5](f: (T1, T2, T3, T4) => T5): (T1, T2, T3) => T4 => T5 =
     (t1: T1, t2: T2, t3: T3) => (t4: T4) => f(t1, t2, t3, t4)
 
 
   /**
-    * Returns `f` where the last parameter is curried.
+    * Returns `f` with the last parameter curried.
     */
   private def curry[T1, T2, T3, T4, T5, T6](f: (T1, T2, T3, T4, T5) => T6): (T1, T2, T3, T4) => T5 => T6 =
     (t1: T1, t2: T2, t3: T3, t4: T4) => (t5: T5) => f(t1, t2, t3, t4, t5)
 
 
   /**
-    * Returns `f` where the last parameter is curried.
+    * Returns `f` with the last parameter curried.
     */
   private def curry[T1, T2, T3, T4, T5, T6, T7](f: (T1, T2, T3, T4, T5, T6) => T7): (T1, T2, T3, T4, T5) => T6 => T7 =
     (t1: T1, t2: T2, t3: T3, t4: T4, t5: T5) => (t6: T6) => f(t1, t2, t3, t4, t5, t6)
 
 
   /**
-    * Returns `f` where the last parameter is curried.
+    * Returns `f` with the last parameter curried.
     */
   private def curry[T1, T2, T3, T4, T5, T6, T7, T8](f: (T1, T2, T3, T4, T5, T6, T7) => T8): (T1, T2, T3, T4, T5, T6) => T7 => T8 =
     (t1: T1, t2: T2, t3: T3, t4: T4, t5: T5, t6: T6) => (t7: T7) => f(t1, t2, t3, t4, t5, t6, t7)
 
 
   /**
-    * Returns `f` where the last parameter is curried.
+    * Returns `f` with the last parameter curried.
     */
   private def curry[T1, T2, T3, T4, T5, T6, T7, T8, T9](f: (T1, T2, T3, T4, T5, T6, T7, T8) => T9): (T1, T2, T3, T4, T5, T6, T7) => T8 => T9 =
     (t1: T1, t2: T2, t3: T3, t4: T4, t5: T5, t6: T6, t7: T7) => (t8: T8) => f(t1, t2, t3, t4, t5, t6, t7, t8)
 
 
   /**
-    * Returns `f` where the last parameter is curried.
+    * Returns `f` with the last parameter curried.
     */
   private def curry[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10](f: (T1, T2, T3, T4, T5, T6, T7, T8, T9) => T10): (T1, T2, T3, T4, T5, T6, T7, T8) => T9 => T10 =
     (t1: T1, t2: T2, t3: T3, t4: T4, t5: T5, t6: T6, t7: T7, t8: T8) => (t9: T9) => f(t1, t2, t3, t4, t5, t6, t7, t8, t9)
