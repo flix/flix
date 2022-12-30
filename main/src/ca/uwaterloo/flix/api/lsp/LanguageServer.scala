3573e4723c65228e6f2697ecd25c9dd85d7b6ba3
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2022-12-30 20:14:20 +0100

    Update root AST on SoftFailure

--- main/src/ca/uwaterloo/flix/api/lsp/LanguageServer.scala
+++ main/src/ca/uwaterloo/flix/api/lsp/LanguageServer.scala
@@ -23,7 +23,7 @@ import ca.uwaterloo.flix.language.ast.TypedAst.Root
 import ca.uwaterloo.flix.language.phase.extra.CodeHinter
 import ca.uwaterloo.flix.util.Formatter.NoFormatter
 import ca.uwaterloo.flix.util.Result.{Err, Ok}
-import ca.uwaterloo.flix.util.Validation.Success
+import ca.uwaterloo.flix.util.Validation.{Failure, SoftFailure, Success}
 import ca.uwaterloo.flix.util._
 import org.java_websocket.WebSocket
 import org.java_websocket.handshake.ClientHandshake
@@ -338,15 +338,40 @@ class LanguageServer(port: Int, o: Options) extends WebSocketServer(new InetSock
             ("id" -> requestId) ~ ("status" -> "failure") ~ ("time" -> e) ~ ("result" -> results.map(_.toJSON))
           }
 
-        case failure =>
-          // Case 2: Compilation failed. Send back the error messages.
+        case SoftFailure(root, errors) =>
+          // Case 2: Compilation was almost successful. Build the reverse index.
+          this.root = Some(root)
+          this.index = Indexer.visitRoot(root)
+          this.current = true
+          this.currentErrors = errors.toList
+
+          // Compute elapsed time.
+          val e = System.nanoTime() - t
+
+          // Print query time.
+          // println(s"lsp/check: ${e / 1_000_000}ms")
+
+          // Compute Code Quality hints.
+          val codeHints = CodeHinter.run(root, sources.keySet.toSet)(flix, index)
+          if (codeHints.isEmpty) {
+            // Case 1: No code hints.
+            val results = PublishDiagnosticsParams.fromMessages(errors)
+            ("id" -> requestId) ~ ("status" -> "success") ~ ("time" -> e) ~ ("result" -> results.map(_.toJSON))
+          } else {
+            // Case 2: Code hints are available.
+            val results = PublishDiagnosticsParams.fromMessages(errors) ::: PublishDiagnosticsParams.fromCodeHints(codeHints)
+            ("id" -> requestId) ~ ("status" -> "failure") ~ ("time" -> e) ~ ("result" -> results.map(_.toJSON))
+          }
+
+        case Failure(errors) =>
+          // Case 3: Compilation failed. Send back the error messages.
 
           // Mark the AST as outdated and update the current errors.
           this.current = false
-          this.currentErrors = failure.errors.toList
+          this.currentErrors = errors.toList
 
           // Publish diagnostics.
-          val results = PublishDiagnosticsParams.fromMessages(failure.errors)
+          val results = PublishDiagnosticsParams.fromMessages(errors)
           ("id" -> requestId) ~ ("status" -> "failure") ~ ("result" -> results.map(_.toJSON))
       }
     } catch {
