cee85642de8788cfd0b0cd48f2721716e66f9c38
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2023-01-02 18:13:10 +0100

    Add Error to KindedAst

--- main/src/ca/uwaterloo/flix/language/ast/KindedAst.scala
+++ main/src/ca/uwaterloo/flix/language/ast/KindedAst.scala
@@ -16,7 +16,7 @@
 
 package ca.uwaterloo.flix.language.ast
 
-import ca.uwaterloo.flix.language.ast
+import ca.uwaterloo.flix.language.{CompilationMessage, ast}
 import ca.uwaterloo.flix.language.ast.Ast.{Denotation, Source}
 import ca.uwaterloo.flix.util.collection.MultiMap
 
@@ -200,6 +200,8 @@ object KindedAst {
     case class FixpointInject(exp: KindedAst.Expression, pred: Name.Pred, tpe: Type.Var, loc: SourceLocation) extends KindedAst.Expression
 
     case class FixpointProject(pred: Name.Pred, exp1: KindedAst.Expression, exp2: KindedAst.Expression, tpe: Type.Var, loc: SourceLocation) extends KindedAst.Expression
+
+    case class Error(m: CompilationMessage, loc: SourceLocation) extends KindedAst.Expression
 
   }
 
