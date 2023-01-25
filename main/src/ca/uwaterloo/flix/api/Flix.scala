362ecc09113eb48405ba92b0e9d0ea739278c2d0
stephentetley <stephen.tetley@gmail.com>
2023-01-25 22:29:40 +0000

    Adding Vector datatype and operations.

--- main/src/ca/uwaterloo/flix/api/Flix.scala
+++ main/src/ca/uwaterloo/flix/api/Flix.scala
@@ -249,6 +249,7 @@ class Flix {
     "Fixpoint/Tuple/Tuple.flix" -> LocalResource.get("/src/library/Fixpoint/Tuple/Tuple.flix"),
 
     "Graph.flix" -> LocalResource.get("/src/library/Graph.flix"),
+    "Vector.flix" -> LocalResource.get("/src/library/Vector.flix"),
   )
 
   /**
