a0587df24d6ab69b7b6f2c409cc5b7e3beeed2e6
Matthew Lutze <mlutze@users.noreply.github.com>
2023-01-28 00:32:23 +0100

    squish

--- main/src/ca/uwaterloo/flix/language/ast/NamedAst.scala
+++ main/src/ca/uwaterloo/flix/language/ast/NamedAst.scala
@@ -24,7 +24,6 @@ object NamedAst {
 
   case class Root(symbols: Map[Name.NName, Map[String, List[NamedAst.Declaration]]],
                   instances: Map[Name.NName, Map[String, List[NamedAst.Declaration.Instance]]],
-                  cases: Map[Name.NName, Map[String, List[NamedAst.Declaration.Case]]],
                   uses: Map[Name.NName, List[NamedAst.UseOrImport]],
                   units: Map[Ast.Source, NamedAst.CompilationUnit],
                   entryPoint: Option[Symbol.DefnSym],
