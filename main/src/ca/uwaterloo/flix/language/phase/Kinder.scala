8efe7d1bfffac691599645978b1c684da60ab682
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2023-02-16 14:56:32 +0100

    Fix type errors

--- main/src/ca/uwaterloo/flix/language/phase/Kinder.scala
+++ main/src/ca/uwaterloo/flix/language/phase/Kinder.scala
@@ -449,7 +449,7 @@ object Kinder {
       mapN(fparamVal, expVal) {
         case (fparam, exp) => KindedAst.Expression.Lambda(fparam, exp, loc)
       }.recoverOne {
-        case err: KindError.UnexpectedKind =>
+        case err: KindError =>
           val tvar = Type.freshVar(Kind.Star, loc.asSynthetic)
           val pvar = Type.freshVar(Kind.Bool, loc.asSynthetic)
           val evar = Type.freshVar(Kind.Effect, loc.asSynthetic)
@@ -536,6 +536,12 @@ object Kinder {
       val rulesVal = traverse(rules0)(visitMatchTypeRule(_, kenv0, senv, taenv, henv0, root))
       mapN(expVal, rulesVal) {
         case (exp, rules) => KindedAst.Expression.TypeMatch(exp, rules, loc)
+      }.recoverOne {
+        case err: KindError =>
+          val tvar = Type.freshVar(Kind.Star, loc.asSynthetic)
+          val pvar = Type.freshVar(Kind.Bool, loc.asSynthetic)
+          val evar = Type.freshVar(Kind.Effect, loc.asSynthetic)
+          KindedAst.Expression.Error(err, tvar, pvar, evar)
       }
 
     case ResolvedAst.Expression.RelationalChoose(star, exps0, rules0, loc) =>
@@ -1131,7 +1137,7 @@ object Kinder {
   /**
     * Performs kinding on the given type variable under the given kind environment, with `expectedKind` expected from context.
     */
-  private def visitTypeVar(tvar: UnkindedType.Var, expectedKind: Kind, kenv: KindEnv): Validation[Type.Var, KindError.UnexpectedKind] = tvar match {
+  private def visitTypeVar(tvar: UnkindedType.Var, expectedKind: Kind, kenv: KindEnv): Validation[Type.Var, KindError] = tvar match {
     case UnkindedType.Var(sym0, loc) =>
       mapN(visitTypeVarSym(sym0, expectedKind, kenv, loc)) {
         sym => Type.Var(sym, loc)
@@ -1141,7 +1147,7 @@ object Kinder {
   /**
     * Performs kinding on the given type variable symbol under the given kind environment, with `expectedKind` expected from context.
     */
-  private def visitTypeVarSym(sym: Symbol.UnkindedTypeVarSym, expectedKind: Kind, kenv: KindEnv, loc: SourceLocation): Validation[Symbol.KindedTypeVarSym, KindError.UnexpectedKind] = {
+  private def visitTypeVarSym(sym: Symbol.UnkindedTypeVarSym, expectedKind: Kind, kenv: KindEnv, loc: SourceLocation): Validation[Symbol.KindedTypeVarSym, KindError] = {
     kenv.map.get(sym) match {
       // Case 1: we don't know about this kind, just ascribe it with what the context expects
       case None => sym.withKind(expectedKind).toSuccess
@@ -1160,7 +1166,7 @@ object Kinder {
     * This is roughly analogous to the reassembly of expressions under a type environment, except that:
     * - Kind errors may be discovered here as they may not have been found during inference (or inference may not have happened at all).
     */
-  private def visitType(tpe0: UnkindedType, expectedKind: Kind, kenv: KindEnv, senv: Map[Symbol.UnkindedTypeVarSym, Symbol.UnkindedTypeVarSym], taenv: Map[Symbol.TypeAliasSym, KindedAst.TypeAlias], root: ResolvedAst.Root)(implicit flix: Flix): Validation[Type, KindError.UnexpectedKind] = tpe0 match {
+  private def visitType(tpe0: UnkindedType, expectedKind: Kind, kenv: KindEnv, senv: Map[Symbol.UnkindedTypeVarSym, Symbol.UnkindedTypeVarSym], taenv: Map[Symbol.TypeAliasSym, KindedAst.TypeAlias], root: ResolvedAst.Root)(implicit flix: Flix): Validation[Type, KindError] = tpe0 match {
     case tvar: UnkindedType.Var => visitTypeVar(tvar, expectedKind, kenv)
     case UnkindedType.Cst(cst, loc) =>
       val kind = cst.kind
