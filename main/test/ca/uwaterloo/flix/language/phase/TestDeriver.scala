b72d039d0f28ae26ec3caeee58a041fc1b040e56
Matthew Lutze <mlutze@users.noreply.github.com>
2023-03-18 11:10:28 +0100

    add negative test

---
+++ main/test/ca/uwaterloo/flix/language/phase/TestDeriver.scala
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2020 Magnus Madsen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ca.uwaterloo.flix.language.phase
+
+import ca.uwaterloo.flix.TestUtils
+import ca.uwaterloo.flix.language.errors.DerivationError
+import ca.uwaterloo.flix.util.Options
+import org.scalatest.FunSuite
+
+class TestDeriver extends FunSuite with TestUtils {
+
+  test("Test.DerivationError.IllegalFunctorShape.01") {
+    val input =
+      """
+        |enum E with Functor {
+        |    case C
+        |}
+        |""".stripMargin
+    val result = compile(input, Options.TestWithLibAll)
+    expectError[DerivationError.IllegalFunctorShape](result)
+  }
+}
