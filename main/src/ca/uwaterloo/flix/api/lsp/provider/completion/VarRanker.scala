0a6212db56e82c44acd4367a7f6c5c8cda145cde
Lukas Rønn <202007052@post.au.dk>
2023-04-17 21:53:51 +0200

    Way simpler findBest

--- main/src/ca/uwaterloo/flix/api/lsp/provider/completion/VarRanker.scala
+++ main/src/ca/uwaterloo/flix/api/lsp/provider/completion/VarRanker.scala
@@ -20,8 +20,6 @@ import ca.uwaterloo.flix.api.lsp.provider.completion.Completion.VarCompletion
 import ca.uwaterloo.flix.language.ast.{SourceLocation, Symbol}
 import ca.uwaterloo.flix.util.collection.MultiMap
 
-import scala.annotation.tailrec
-
 /**
   * Ranks the possible var completions.
   */
@@ -35,44 +33,10 @@ object VarRanker {
     * @return            Some(Completion) if a better completion is possible, else none.
     */
   def findBest(completions: Iterable[Completion], varUses: MultiMap[Symbol.VarSym, SourceLocation]): Option[VarCompletion] = {
-    /**
-      * Auxiliary function for findBestVarCompletion.
-      *
-      * @param varComp  the var completion at question.
-      * @param varComps the rest of possible var completions.
-      * @param varUses  a multimap containing all uses of a specific var.
-      * @return         Some(Completion) if a better completion is possible, else none.
-      */
-    @tailrec
-    def aux(varComp: VarCompletion, varComps: Iterable[VarCompletion], varUses: MultiMap[Symbol.VarSym, SourceLocation]): Option[VarCompletion] = {
-      // Checks how many times the var has been used
-      varUses.apply(varComp.sym).size match {
-        case 0 =>
-          // The var hasn't been used yet, and is therefore the best var completion
-          Some(varComp)
-        case _ =>
-          // The var has been used more at least once
-          // Check if we have more varCompletions
-          varComps match {
-            case Nil =>
-              // We don't have any more varCompletions, and therefore no best var completion
-              None
-            case ::(head, next) =>
-              // We have more varCompletions, and we'll check if the next varCompletion is the best varCompletion
-              aux(head, next, varUses)
-          }
-      }
-    }
-
-    // Remove all none var completions.
-    getVarCompletions(completions) match {
-      case Nil =>
-        // Case 1: We don't have any possible var completions
-        None
-      case ::(head, next) =>
-        // Case 2: We have some possible var completions
-        aux(head, next, varUses)
-    }
+    // Remove all none var completions
+    getVarCompletions(completions)
+      // Find the var comp that has 0 uses
+      .find(varComp => varUses(varComp.sym).isEmpty)
   }
 
   /**
@@ -82,9 +46,8 @@ object VarRanker {
     * @return            a List of VarCompletions.
     */
   private def getVarCompletions(completions: Iterable[Completion]): Iterable[VarCompletion] = {
-    completions.flatMap {
-      case VarCompletion(sym, tpe) => Some(VarCompletion(sym, tpe))
-      case _ => None
+    completions.collect {
+      case VarCompletion(sym, tpe) => VarCompletion(sym, tpe)
     }
   }
 }
