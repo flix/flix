914171610f4fb3ee0ad74b87493894e2932d84c6
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2023-05-20 16:23:04 +0200

    feat: add `Verifier` phase.

--- main/src/ca/uwaterloo/flix/language/phase/Verifier.scala
+++ main/src/ca/uwaterloo/flix/language/phase/Verifier.scala
@@ -16,15 +16,68 @@
 package ca.uwaterloo.flix.language.phase
 
 import ca.uwaterloo.flix.api.Flix
-import ca.uwaterloo.flix.language.ast.MonoTypedAst
+import ca.uwaterloo.flix.language.ast.Ast.Constant
+import ca.uwaterloo.flix.language.ast.{MonoType, MonoTypedAst, SourceLocation}
+import ca.uwaterloo.flix.language.ast.MonoTypedAst.{Def, Expr, Stmt}
+import ca.uwaterloo.flix.util.{InternalCompilerException, ParOps}
 
+/**
+  * We use this phase to catch miscompilation before we do bytecode generation.
+  */
 object Verifier {
-
   def run(root: MonoTypedAst.Root)(implicit flix: Flix): MonoTypedAst.Root = flix.phase("Verifier") {
-
-
-
+    ParOps.parMap(root.defs.values)(visitDef)
     root
   }
+
+  private def visitDef(def0: Def): Unit = {
+    visitStmt(def0.stmt)
+  }
+
+  private def visitExpr(expr: MonoTypedAst.Expr): MonoType = expr match {
+    case Expr.Cst(cst, tpe, loc) => cst match {
+      case Constant.Unit => expect(expected = MonoType.Unit, actual = tpe, loc)
+      case Constant.Null => ???
+      case Constant.Bool(lit) => ???
+      case Constant.Char(lit) => ???
+      case Constant.Float32(lit) => ???
+      case Constant.Float64(lit) => ???
+      case Constant.BigDecimal(lit) => ???
+      case Constant.Int8(lit) => ???
+      case Constant.Int16(lit) => ???
+      case Constant.Int32(lit) => ???
+      case Constant.Int64(lit) => ???
+      case Constant.BigInt(lit) => ???
+      case Constant.Str(lit) => ???
+      case Constant.Regex(lit) => ???
+    }
+    case Expr.Var(sym, tpe, loc) => tpe
+    case Expr.ApplyAtomic(op, exps, tpe, loc) => tpe
+    case Expr.ApplyClo(exp, args, ct, tpe, loc) => tpe
+    case Expr.ApplyDef(sym, args, ct, tpe, loc) => tpe
+    case Expr.ApplySelfTail(sym, formals, actuals, tpe, loc) => tpe
+    case Expr.IfThenElse(exp1, exp2, exp3, tpe, loc) => tpe
+    case Expr.Branch(exp, branches, tpe, loc) => tpe
+    case Expr.JumpTo(sym, tpe, loc) => tpe
+    case Expr.Let(sym, exp1, exp2, tpe, loc) => tpe
+    case Expr.LetRec(varSym, index, defSym, exp1, exp2, tpe, loc) => tpe
+    case Expr.Scope(sym, exp, tpe, loc) => tpe
+    case Expr.TryCatch(exp, rules, tpe, loc) => tpe
+    case Expr.NewObject(name, clazz, tpe, methods, loc) => tpe
+    case Expr.Spawn(exp1, exp2, tpe, loc) => tpe
+  }
+
+  private def visitStmt(stmt: MonoTypedAst.Stmt): MonoType = stmt match {
+    case Stmt.Ret(expr, tpe, loc) =>
+      visitExpr(expr)
+  }
+
+  private def expect(expected: MonoType, actual: MonoType, loc: SourceLocation): MonoType = {
+    if (expected == actual)
+      actual
+    else
+      throw InternalCompilerException(s"Expected: '$expected', but got: '$actual'.", loc)
+  }
+
 }
 
