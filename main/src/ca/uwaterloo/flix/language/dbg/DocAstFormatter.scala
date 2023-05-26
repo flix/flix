8c3c73531fad027484892cae97cf09d105509c22
Matthew Lutze <mlutze@users.noreply.github.com>
2023-05-26 15:58:02 +0200

    fix formatting

--- main/src/ca/uwaterloo/flix/language/dbg/DocAstFormatter.scala
+++ main/src/ca/uwaterloo/flix/language/dbg/DocAstFormatter.scala
@@ -189,7 +189,7 @@ object DocAstFormatter {
         val rs = semiSepOpt(rules.map {
           case (sym, params, rule) =>
             val rulef = aux(rule, paren = false, inBlock = true)
-            text("def") +: text(sym.toString) :: tuple(params.map(aux(_))) ::
+            text("def") +: text(sym.toString) :: tuple(params.map(aux(_, paren = false))) +:
               text("=") :: breakWith(" ") :: curlyOpen(rulef)
         })
         val bodyf = aux(d, paren = false, inBlock = true)
