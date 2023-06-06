8ee37f4504dce1dea9d6f1be7c902dc598dbe155
xdesou01 <103150835+xdesou01@users.noreply.github.com>
2023-06-06 07:34:59 -0400

    removed print statement used for debugging

--- main/src/ca/uwaterloo/flix/api/lsp/provider/completion/UseModuleCompleter.scala
+++ main/src/ca/uwaterloo/flix/api/lsp/provider/completion/UseModuleCompleter.scala
@@ -8,7 +8,6 @@ import ca.uwaterloo.flix.api.lsp.provider.completion.Completion.ModCompletion
 object UseModuleCompleter extends Completer {
 
   def getCompletions(ctx: CompletionContext)(implicit flix: Flix, index: Index, root: TypedAst.Root, delta: DeltaContext): Iterable[Completion] = {
-    print("in UMC \n")
     val regex = raw"\s*use\s+(.*)".r
     ctx.prefix match {
       case regex(ns) =>
