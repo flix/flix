be282ce4434628cf7873cd7ddf81d484fdc892ea
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2023-06-24 20:04:02 +0200

    feat: add a few code actions

--- main/src/ca/uwaterloo/flix/api/lsp/provider/CodeActionProvider.scala
+++ main/src/ca/uwaterloo/flix/api/lsp/provider/CodeActionProvider.scala
@@ -17,15 +17,27 @@ package ca.uwaterloo.flix.api.lsp.provider
 
 import ca.uwaterloo.flix.api.Flix
 import ca.uwaterloo.flix.api.lsp.{CodeAction, CodeActionContext, CodeActionKind, Entity, Index, Position, Range, TextEdit, WorkspaceEdit}
-import ca.uwaterloo.flix.language.ast.Symbol
+import ca.uwaterloo.flix.language.CompilationMessage
+import ca.uwaterloo.flix.language.ast.{SourceLocation, Symbol}
 import ca.uwaterloo.flix.language.ast.TypedAst.Root
+import ca.uwaterloo.flix.language.errors.{RedundancyError, ResolutionError, TypeError}
 
 object CodeActionProvider {
 
-  def getCodeActions(uri: String, range: Range, context: CodeActionContext)(implicit index: Index, root: Option[Root], flix: Flix): List[CodeAction] = {
-    println(s"uri = $uri, $range = $range, ctx = $context")
+  def getCodeActions(uri: String, range: Range, context: CodeActionContext, currentErrors: List[CompilationMessage])(implicit index: Index, root: Option[Root], flix: Flix): List[CodeAction] = {
 
+    // TODO: Maybe merge these cases?
 
+    //
+    // Case 1: There are errors. Lets try to match and see if we can find an error we can offer a quick fix for.
+    //
+    if (currentErrors.nonEmpty) {
+      return quickfixError(uri, range, currentErrors)
+    }
+
+    //
+    // Case 2: No errors. We do a lookup on the beginning of the range:
+    //
     index.query(uri, range.start) match {
       case None => Nil // No code actions.
 
@@ -38,6 +50,43 @@ object CodeActionProvider {
       }
     }
   }
+
+  // TODO: DOC
+  private def quickfixError(uri: String, range: Range, currentErrors: List[CompilationMessage]): List[CodeAction] = currentErrors.collect {
+    case RedundancyError.UnusedVarSym(sym) if onSameLine(range, sym.loc) =>
+      renameUnusedVar(sym, uri)
+
+    case ResolutionError.UndefinedType(qn, ns, loc) =>
+      // TODO: Actually check if loc and range overlap.
+      addMissingEnum(qn.ident.name, uri)
+
+
+  }
+
+  private def renameUnusedVar(sym: Symbol.VarSym, uri: String): CodeAction = CodeAction(
+    title = s"Prefix unused variable with underscore",
+    kind = CodeActionKind.QuickFix,
+    edit = Some(WorkspaceEdit(
+      Map(uri -> List(TextEdit(
+        Range(Position.fromBegin(sym.loc), Position.fromBegin(sym.loc)),
+        s"_"
+      )))
+    )),
+    command = None
+  )
+
+  // TODO: DOC
+  private def addMissingEnum(name: String, uri: String): CodeAction = CodeAction(
+    title = s"Add enum $name",
+    kind = CodeActionKind.QuickFix,
+    edit = Some(WorkspaceEdit(
+      Map(uri -> List(TextEdit(
+        Range(Position(0, 0), Position(0, 0)), // TODO: Better position.
+        s"enum $name { }"
+      )))
+    )),
+    command = None
+  )
 
   /**
     * A code action to derive the `Eq` type class.
@@ -60,7 +109,7 @@ object CodeActionProvider {
     * A code action to derive the given type class `clazz` for the given enum symbol `sym`.
     */
   private def mkDerive(sym: Symbol.EnumSym, clazz: String, uri: String): CodeAction = CodeAction(
-    title = s"Derive $clazz for $sym",
+    title = s"Derive $clazz",
     kind = CodeActionKind.Refactor,
     edit = Some(WorkspaceEdit(
       Map(uri -> List(TextEdit(
@@ -70,5 +119,8 @@ object CodeActionProvider {
     )),
     command = None
   )
+
+  // TODO: A more accurate implementation would be useful.
+  private def onSameLine(range: Range, loc: SourceLocation): Boolean = range.start.line == loc.beginLine
 
 }
