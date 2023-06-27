e122cac2c9da75c9994858cf9135eaf7118a14b0
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2023-06-27 10:46:04 +0200

    Fix typo

--- main/src/ca/uwaterloo/flix/language/phase/LateTreeShaker.scala
+++ main/src/ca/uwaterloo/flix/language/phase/LateTreeShaker.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Magnus <Madsen
+ * Copyright 2017 Magnus Madsen
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
