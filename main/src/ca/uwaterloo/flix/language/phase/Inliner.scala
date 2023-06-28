a34323ce138db8e40d49df3d6c514cb30dbdb535
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2023-06-28 13:39:18 +0200

    Refactor ArrayLoad

--- main/src/ca/uwaterloo/flix/language/phase/Inliner.scala
+++ main/src/ca/uwaterloo/flix/language/phase/Inliner.scala
@@ -260,11 +260,6 @@ object Inliner {
       val e = visitExp(exp, subst0)
       LiftedAst.Expression.Scope(sym, e, tpe, purity, loc)
 
-    case OccurrenceAst.Expression.ArrayLoad(exp1, exp2, tpe, loc) =>
-      val e1 = visitExp(exp1, subst0)
-      val e2 = visitExp(exp2, subst0)
-      LiftedAst.Expression.ApplyAtomic(AtomicOp.ArrayLoad, List(e1, e2), tpe, Purity.Impure, loc)
-
     case OccurrenceAst.Expression.ArrayStore(exp1, exp2, exp3, tpe, loc) =>
       val e1 = visitExp(exp1, subst0)
       val e2 = visitExp(exp2, subst0)
@@ -553,11 +548,6 @@ object Inliner {
     case OccurrenceAst.Expression.Scope(sym, exp, tpe, purity, loc) =>
       val e = substituteExp(exp, env0)
       LiftedAst.Expression.Scope(sym, e, tpe, purity, loc)
-
-    case OccurrenceAst.Expression.ArrayLoad(exp1, exp2, tpe, loc) =>
-      val e1 = substituteExp(exp1, env0)
-      val e2 = substituteExp(exp2, env0)
-      LiftedAst.Expression.ApplyAtomic(AtomicOp.ArrayLoad, List(e1, e2), tpe, Purity.Impure, loc)
 
     case OccurrenceAst.Expression.ArrayStore(exp1, exp2, exp3, tpe, loc) =>
       val e1 = substituteExp(exp1, env0)
