1b9ece37e894099bf6467f2ebcb5e9ee47352743
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2023-07-12 16:17:44 +0200

    experiment: handlers

--- main/src/dev/flix/runtime/ResumptionCons.java
+++ main/src/dev/flix/runtime/ResumptionCons.java
@@ -4,7 +4,7 @@ public class ResumptionCons implements Resumption {
     public final String effSym;
     public final Frames frames;
     public final Resumption tail;
-
+// TODO: Something about handlers. To be decided.
     public ResumptionCons(String effSym, Frames frames, Resumption tail) {
         this.effSym = effSym;
         this.frames = frames;
