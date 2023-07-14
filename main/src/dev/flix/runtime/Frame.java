1d0e299c2733d0625eae39b59ea7ecb28d16e135
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2023-07-14 08:29:52 +0200

    experiment: handlers

--- main/src/dev/flix/runtime/Frame.java
+++ main/src/dev/flix/runtime/Frame.java
@@ -1,5 +1,14 @@
 package dev.flix.runtime;
 
+/**
+ * A {@link Frame} is represents a function `Value -> Result`.
+ * <p>
+ * Thus a {@link Frame} is like a {@link Thunk}, but it takes an argument.
+ * <p>
+ * We use a {@link Frame} to model the resumption of a function with a resume argument.
+ * That is, to implement `resume(v)` for some value `v` where the resumption happens inside some function `f`.
+ * Then the frame represents the ability to call into `f` at the right place with the value `resumeArg`.
+ */
 public interface Frame {
-    Result apply(Value arg);
+    public Result apply(Value resumeArg);
 }
