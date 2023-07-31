d0a410a6b4db88434c3aff4c95ad8e29b41ba1f7
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2023-07-31 16:52:34 +0200

    Refactor Weeder

--- main/src/ca/uwaterloo/flix/language/phase/Weeder.scala
+++ main/src/ca/uwaterloo/flix/language/phase/Weeder.scala
@@ -2431,9 +2431,14 @@ object Weeder {
           case Some(r) if fields.isEmpty =>
             // Bad pattern { | r }
             WeederError.EmptyRecordExtensionPattern(r.loc).toFailure
-          case r@None => r.toSuccess
-          case r@Some(Pattern.Var(_, _)) => r.toSuccess
+
+          case Some(Pattern.Var(_, loc1)) =>
+            // Translate to Wild so it does not result in unused variable.
+            Some(Pattern.Wild(loc1.asSynthetic)).toSuccess
+
           case r@Some(Pattern.Wild(_)) => r.toSuccess
+          case r@None => r.toSuccess
+
           case Some(ext) =>
             // Bad pattern e.g., { fields... | (1, 2, 3) }
             WeederError.IllegalRecordExtensionPattern(ext.loc).toFailure
