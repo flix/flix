00f6e82b5c3edcc5ab3f457b89d3344f3408b89e
xdesou01 <103150835+xdesou01@users.noreply.github.com>
2023-08-03 06:40:00 -0400

    fixed

--- main/src/ca/uwaterloo/flix/api/lsp/provider/completion/UseSignatureCompleter.scala
+++ main/src/ca/uwaterloo/flix/api/lsp/provider/completion/UseSignatureCompleter.scala
@@ -20,18 +20,24 @@ import ca.uwaterloo.flix.api.Flix
 import ca.uwaterloo.flix.api.lsp.Index
 import ca.uwaterloo.flix.api.lsp.provider.completion.Completion.UseSignatureCompletion
 import ca.uwaterloo.flix.language.ast.TypedAst
-import ca.uwaterloo.flix.language.ast.Symbol.SigSym
 
 object UseSignatureCompleter extends Completer {
+  /**
+   * Returns an Iterable of UseSignatureCompletions for the completer.
+   */
   override def getCompletions(context: CompletionContext)(implicit flix: Flix, index: Index, root: TypedAst.Root, delta: DeltaContext): Iterable[UseSignatureCompletion] = {
     stripWord(context) match {
       case Some(word) =>
-        val sigs = root.sigs.keys.filter(s => matches(s, word))
-        sigs.map(sig => getUseSigCompletion(root.sigs(sig)))
+        val uri = context.uri
+        val sigs = root.sigs.values.filter(s => validMatch(s, word, uri))
+        sigs.map(sig => getUseSigCompletion(sig))
       case _ => Nil
     }
   }
 
+  /**
+   * Strips the current word of `use` to enable completion checking.
+   */
   private def stripWord(ctx: CompletionContext): Option[String] = {
     val regex = raw"\s*use\s+(.*)".r
     ctx.prefix match {
@@ -39,12 +45,20 @@ object UseSignatureCompleter extends Completer {
       case _ => None
     }
   }
-  private def matches(sym: SigSym, suffix: String): Boolean = {
-    sym.name.startsWith(suffix)
+
+  /**
+   * Returns 'true' if `parsedWord` matches a valid signature.
+   */
+  private def validMatch(sig: TypedAst.Sig, parsedWord: String, uri: String): Boolean = {
+    val isLocal = uri == sig.sym.loc.source.name
+    val isPublic = sig.spec.mod.isPublic
+    val isNamespace = parsedWord.nonEmpty && parsedWord.head.isUpper
+    val matches = if (isNamespace) sig.sym.toString.startsWith(parsedWord)
+                  else             sig.sym.name.startsWith(parsedWord)
+    matches && (isLocal || isPublic)
   }
 
-  private def getUseSigCompletion(decl: TypedAst.Sig): UseSignatureCompletion = {
-    val sym = decl.sym
-    Completion.UseSignatureCompletion(sym.toString)
+  private def getUseSigCompletion(sig: TypedAst.Sig): UseSignatureCompletion = {
+    Completion.UseSignatureCompletion(sig.sym.toString)
   }
 }
