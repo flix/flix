ac7be96fff944077ee51fbc17b10a15df70c80bd
HerlufFraDiku <herlufbaggesen13@gmail.com>
2023-08-09 22:30:51 +0200

    feat: scan more operators

--- main/src/ca/uwaterloo/flix/language/ast/TokenKind.scala
+++ main/src/ca/uwaterloo/flix/language/ast/TokenKind.scala
@@ -40,6 +40,8 @@ object TokenKind {
 
   case object ColonColon extends TokenKind
 
+  case object ColonEqual extends TokenKind
+
   case object Arrow extends TokenKind
 
   case object Plus extends TokenKind
@@ -59,6 +61,8 @@ object TokenKind {
   case object LessEqual extends TokenKind
 
   case object GreaterEqual extends TokenKind
+
+  case object Equal extends TokenKind
 
   case object EqualEqual extends TokenKind
 
@@ -194,7 +198,9 @@ object TokenKind {
 
   case object ObjectKeyword extends TokenKind
 
-  case object Name extends TokenKind
+  case object UppercaseName extends TokenKind
+
+  case object LowercaseName extends TokenKind
 
   case object Integer extends TokenKind
 
