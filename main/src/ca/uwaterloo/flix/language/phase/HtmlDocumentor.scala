faab245075b1b9694c7de09ae33ba7ef1f2ff84a
Holger Dal Mogensen <61235930+sockmaster27@users.noreply.github.com>
2023-08-17 16:18:36 +0200

    fix: write assets after generating HTML documentation (#6368)

--- main/src/ca/uwaterloo/flix/language/phase/HtmlDocumentor.scala
+++ main/src/ca/uwaterloo/flix/language/phase/HtmlDocumentor.scala
@@ -58,7 +58,6 @@ object HtmlDocumentor {
   val LibraryGitHub: String = "https://github.com/flix/flix/blob/master/main/src/library/"
 
   def run(root: TypedAst.Root)(implicit flix: Flix): Unit = {
-    writeAssets()
     val modules = splitModules(root)
     val filteredModules = filterModules(modules)
     filteredModules.foreach {
@@ -66,6 +65,7 @@ object HtmlDocumentor {
         val out = documentModule(mod)
         writeModule(mod, out)
     }
+    writeAssets()
   }
 
   /**
