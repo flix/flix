ab11c51a2dfd01107068794860dd9c708efe805e
HerlufFraDiku <herlufbaggesen13@gmail.com>
2023-08-17 21:06:44 +0200

    chore: UnexpectedSymbol -> UnexpectedChar

--- main/src/ca/uwaterloo/flix/language/phase/Lexer.scala
+++ main/src/ca/uwaterloo/flix/language/phase/Lexer.scala
@@ -274,7 +274,7 @@ object Lexer {
       case '\"' => string()
       case '\'' => char()
       case '`' => infixFunction()
-      case _ => TokenKind.Err(LexerErr.UnexpectedSymbol)
+      case _ => TokenKind.Err(LexerErr.UnexpectedChar)
     }
 
     addToken(kind)
