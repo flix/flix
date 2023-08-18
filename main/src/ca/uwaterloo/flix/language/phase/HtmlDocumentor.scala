dd06b52ccfd4c866cdf7f623d587bed3588e7f3b
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2023-08-18 17:20:24 +0200

    feat: tweak css in `HtmlDocumentor` (#6387)

--- main/src/ca/uwaterloo/flix/language/phase/HtmlDocumentor.scala
+++ main/src/ca/uwaterloo/flix/language/phase/HtmlDocumentor.scala
@@ -300,7 +300,8 @@ object HtmlDocumentor {
       |<meta name='viewport' content='width=device-width,initial-scale=1'/>
       |<link href='https://fonts.googleapis.com/css?family=Fira+Code&display=swap' rel='stylesheet'>
       |<link href='https://fonts.googleapis.com/css?family=Oswald&display=swap' rel='stylesheet'>
-      |<link href='https://fonts.googleapis.com/css?family=Roboto&display=swap' rel='stylesheet'>
+      |<link href='https://fonts.googleapis.com/css?family=Noto+Sans&display=swap' rel='stylesheet'>
+      |<link href='https://fonts.googleapis.com/css?family=Inter&display=swap' rel='stylesheet'>
       |<link href='styles.css' rel='stylesheet'>
       |<link href='favicon.png' rel='icon'>
       |<script defer src='index.js' type="text/javascript"></script>
