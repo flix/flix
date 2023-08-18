fd499a6ed44345bf276191799f08ccd82f6cf3f4
sockmaster27 <61235930+sockmaster27@users.noreply.github.com>
2023-08-18 09:12:22 +0200

    chore: use Roboto instead of system font

--- main/src/ca/uwaterloo/flix/language/phase/HtmlDocumentor.scala
+++ main/src/ca/uwaterloo/flix/language/phase/HtmlDocumentor.scala
@@ -279,6 +279,7 @@ object HtmlDocumentor {
       |<meta name='viewport' content='width=device-width,initial-scale=1'/>
       |<link href='https://fonts.googleapis.com/css?family=Fira+Code&display=swap' rel='stylesheet'>
       |<link href='https://fonts.googleapis.com/css?family=Oswald&display=swap' rel='stylesheet'>
+      |<link href='https://fonts.googleapis.com/css?family=Roboto&display=swap' rel='stylesheet'>
       |<link href='styles.css' rel='stylesheet'>
       |<link href='favicon.png' rel='icon'>
       |<title>Flix | ${esc(name)}</title>
