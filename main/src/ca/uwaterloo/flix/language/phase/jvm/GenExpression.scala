8f253091607c6a075895c5fd6781414feab8b046
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2023-08-21 14:00:10 +0200

    Update comment

--- main/src/ca/uwaterloo/flix/language/phase/jvm/GenExpression.scala
+++ main/src/ca/uwaterloo/flix/language/phase/jvm/GenExpression.scala
@@ -735,7 +735,7 @@ object GenExpression {
         val List(exp1, exp2) = exps
         // We get the inner type of the array
         val elmType = tpe.asInstanceOf[MonoType.Array].tpe
-        // We get the jvmType of elmType.
+        // We get the JVM Type of elmType.
         val jvmType = JvmOps.getJvmType(elmType)
         // Evaluating the value of the 'default element'
         compileExpr(exp1)
