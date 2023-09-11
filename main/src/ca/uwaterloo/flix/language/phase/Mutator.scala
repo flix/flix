7ce20deb4d6e25caf2b44332912c7121a2186fd1
Said <said-ibragimov-02@mail.ru>
2023-09-11 04:26:53 +0300

    test commit

--- main/src/ca/uwaterloo/flix/language/phase/Mutator.scala
+++ main/src/ca/uwaterloo/flix/language/phase/Mutator.scala
@@ -3,6 +3,7 @@ package ca.uwaterloo.flix.language.phase
 import ca.uwaterloo.flix.api.Flix
 import ca.uwaterloo.flix.language.CompilationMessage
 import ca.uwaterloo.flix.language.ast.Ast.Constant
+import ca.uwaterloo.flix.language.ast.SemanticOp.BoolOp
 import ca.uwaterloo.flix.language.ast.TypedAst._
 import ca.uwaterloo.flix.util.Validation
 import ca.uwaterloo.flix.util.Validation._
@@ -36,8 +37,8 @@ object Mutator {
   }
 
   // TODO recurse on the expr, find a random node, and mutate it.
-  private def mutateExpr(exp: Expression): Expression = exp match {
-    case Expression.Cst(cst, tpe, loc) => cst match {
+  private def mutateExpr(exp: Expr): Expr = exp match {
+    case Expr.Cst(cst, tpe, loc) => cst match {
       case Constant.Unit => exp
       case Constant.Null => ???
       case Constant.Bool(lit) => ???
@@ -54,87 +55,93 @@ object Mutator {
           case 1 => 0
           case _ => lit
         }
-        Expression.Cst(Constant.Int32(l), tpe, loc)
+        Expr.Cst(Constant.Int32(l), tpe, loc)
       case Constant.Int64(lit) => ???
       case Constant.BigInt(lit) => ???
       case Constant.Str(lit) => ???
       case Constant.Regex(lit) => ???
     }
-    case Expression.Var(sym, tpe, loc) => ???
-    case Expression.Def(sym, tpe, loc) => ???
-    case Expression.Sig(sym, tpe, loc) => ???
-    case Expression.Hole(sym, tpe, loc) => ???
-    case Expression.HoleWithExp(exp, tpe, eff, loc) => ???
-    case Expression.OpenAs(symUse, exp, tpe, loc) => ???
-    case Expression.Use(sym, alias, exp, loc) => ???
-    case Expression.Lambda(fparam, exp, tpe, loc) => ???
-    case Expression.Apply(exp, exps, tpe, eff, loc) => ???
-    case Expression.Unary(sop, exp, tpe, eff, loc) => ???
-    case Expression.Binary(sop, exp1, exp2, tpe, eff, loc) => ???
-    case Expression.Let(sym, mod, exp1, exp2, tpe, eff, loc) => ???
-    case Expression.LetRec(sym, mod, exp1, exp2, tpe, eff, loc) => ???
-    case Expression.Region(tpe, loc) => ???
-    case Expression.Scope(sym, regionVar, exp, tpe, eff, loc) => ???
-    case Expression.ScopeExit(exp1, exp2, tpe, eff, loc) => ???
-    case Expression.IfThenElse(exp1, exp2, exp3, tpe, eff, loc) => ???
-    case Expression.Stm(exp1, exp2, tpe, eff, loc) => ???
-    case Expression.Discard(exp, eff, loc) => ???
-    case Expression.Match(exp, rules, tpe, eff, loc) => ???
-    case Expression.TypeMatch(exp, rules, tpe, eff, loc) => ???
-    case Expression.RelationalChoose(exps, rules, tpe, eff, loc) => ???
-    case Expression.RestrictableChoose(star, exp, rules, tpe, eff, loc) => ???
-    case Expression.Tag(sym, exp, tpe, eff, loc) => ???
-    case Expression.RestrictableTag(sym, exp, tpe, eff, loc) => ???
-    case Expression.Tuple(elms, tpe, eff, loc) => ???
-    case Expression.RecordEmpty(tpe, loc) => ???
-    case Expression.RecordSelect(exp, field, tpe, eff, loc) => ???
-    case Expression.RecordExtend(field, value, rest, tpe, eff, loc) => ???
-    case Expression.RecordRestrict(field, rest, tpe, eff, loc) => ???
-    case Expression.ArrayLit(exps, exp, tpe, eff, loc) => ???
-    case Expression.ArrayNew(exp1, exp2, exp3, tpe, eff, loc) => ???
-    case Expression.ArrayLoad(base, index, tpe, eff, loc) => ???
-    case Expression.ArrayLength(base, eff, loc) => ???
-    case Expression.ArrayStore(base, index, elm, eff, loc) => ???
-    case Expression.VectorLit(exps, tpe, eff, loc) => ???
-    case Expression.VectorLoad(exp1, exp2, tpe, eff, loc) => ???
-    case Expression.VectorLength(exp, loc) => ???
-    case Expression.Ref(exp1, exp2, tpe, eff, loc) => ???
-    case Expression.Deref(exp, tpe, eff, loc) => ???
-    case Expression.Assign(exp1, exp2, tpe, eff, loc) => ???
-    case Expression.Ascribe(exp, tpe, eff, loc) => ???
-    case Expression.InstanceOf(exp, clazz, loc) => ???
-    case Expression.CheckedCast(cast, exp, tpe, eff, loc) => ???
-    case Expression.UncheckedCast(exp, declaredType, declaredEff, tpe, eff, loc) => ???
-    case Expression.UncheckedMaskingCast(exp, tpe, eff, loc) => ???
-    case Expression.Without(exp, effUse, tpe, eff, loc) => ???
-    case Expression.TryCatch(exp, rules, tpe, eff, loc) => ???
-    case Expression.TryWith(exp, effUse, rules, tpe, eff, loc) => ???
-    case Expression.Do(op, exps, tpe, eff, loc) => ???
-    case Expression.Resume(exp, tpe, loc) => ???
-    case Expression.InvokeConstructor(constructor, args, tpe, eff, loc) => ???
-    case Expression.InvokeMethod(method, exp, args, tpe, eff, loc) => ???
-    case Expression.InvokeStaticMethod(method, args, tpe, eff, loc) => ???
-    case Expression.GetField(field, exp, tpe, eff, loc) => ???
-    case Expression.PutField(field, exp1, exp2, tpe, eff, loc) => ???
-    case Expression.GetStaticField(field, tpe, eff, loc) => ???
-    case Expression.PutStaticField(field, exp, tpe, eff, loc) => ???
-    case Expression.NewObject(name, clazz, tpe, eff, methods, loc) => ???
-    case Expression.NewChannel(exp1, exp2, tpe, eff, loc) => ???
-    case Expression.GetChannel(exp, tpe, eff, loc) => ???
-    case Expression.PutChannel(exp1, exp2, tpe, eff, loc) => ???
-    case Expression.SelectChannel(rules, default, tpe, eff, loc) => ???
-    case Expression.Spawn(exp1, exp2, tpe, eff, loc) => ???
-    case Expression.ParYield(frags, exp, tpe, eff, loc) => ???
-    case Expression.Lazy(exp, tpe, loc) => ???
-    case Expression.Force(exp, tpe, eff, loc) => ???
-    case Expression.FixpointConstraintSet(cs, stf, tpe, loc) => ???
-    case Expression.FixpointLambda(pparams, exp, stf, tpe, eff, loc) => ???
-    case Expression.FixpointMerge(exp1, exp2, stf, tpe, eff, loc) => ???
-    case Expression.FixpointSolve(exp, stf, tpe, eff, loc) => ???
-    case Expression.FixpointFilter(pred, exp, tpe, eff, loc) => ???
-    case Expression.FixpointInject(exp, pred, tpe, eff, loc) => ???
-    case Expression.FixpointProject(pred, exp, tpe, eff, loc) => ???
-    case Expression.Error(m, tpe, eff) => ???
+    case Expr.Var(sym, tpe, loc) => ???
+    case Expr.Def(sym, tpe, loc) => ???
+    case Expr.Sig(sym, tpe, loc) => ???
+    case Expr.Hole(sym, tpe, loc) => ???
+    case Expr.HoleWithExp(exp, tpe, eff, loc) => ???
+    case Expr.OpenAs(symUse, exp, tpe, loc) => ???
+    case Expr.Use(sym, alias, exp, loc) => ???
+    case Expr.Lambda(fparam, exp, tpe, loc) => ???
+    case Expr.Apply(exp, exps, tpe, eff, loc) => ???
+    case Expr.Unary(sop, exp, tpe, eff, loc) => ???
+    case Expr.Binary(sop, exp1, exp2, tpe, eff, loc) =>
+      val op = sop match {
+        case BoolOp.Eq => BoolOp.Neq
+        case BoolOp.Neq => BoolOp.Eq
+        case _ => ???
+      }
+      Expr.Binary(op, exp1, exp2, tpe, eff, loc)
+    case Expr.Let(sym, mod, exp1, exp2, tpe, eff, loc) => ???
+    case Expr.LetRec(sym, mod, exp1, exp2, tpe, eff, loc) => ???
+    case Expr.Region(tpe, loc) => ???
+    case Expr.Scope(sym, regionVar, exp, tpe, eff, loc) => ???
+    case Expr.ScopeExit(exp1, exp2, tpe, eff, loc) => ???
+    case Expr.IfThenElse(exp1, exp2, exp3, tpe, eff, loc) => ???
+    case Expr.Stm(exp1, exp2, tpe, eff, loc) => ???
+    case Expr.Discard(exp, eff, loc) => ???
+    case Expr.Match(exp, rules, tpe, eff, loc) => ???
+    case Expr.TypeMatch(exp, rules, tpe, eff, loc) => ???
+    case Expr.RelationalChoose(exps, rules, tpe, eff, loc) => ???
+    case Expr.RestrictableChoose(star, exp, rules, tpe, eff, loc) => ???
+    case Expr.Tag(sym, exp, tpe, eff, loc) => ???
+    case Expr.RestrictableTag(sym, exp, tpe, eff, loc) => ???
+    case Expr.Tuple(elms, tpe, eff, loc) => ???
+    case Expr.RecordEmpty(tpe, loc) => ???
+    case Expr.RecordSelect(exp, field, tpe, eff, loc) => ???
+    case Expr.RecordExtend(field, value, rest, tpe, eff, loc) => ???
+    case Expr.RecordRestrict(field, rest, tpe, eff, loc) => ???
+    case Expr.ArrayLit(exps, exp, tpe, eff, loc) => ???
+    case Expr.ArrayNew(exp1, exp2, exp3, tpe, eff, loc) => ???
+    case Expr.ArrayLoad(base, index, tpe, eff, loc) => ???
+    case Expr.ArrayLength(base, eff, loc) => ???
+    case Expr.ArrayStore(base, index, elm, eff, loc) => ???
+    case Expr.VectorLit(exps, tpe, eff, loc) => ???
+    case Expr.VectorLoad(exp1, exp2, tpe, eff, loc) => ???
+    case Expr.VectorLength(exp, loc) => ???
+    case Expr.Ref(exp1, exp2, tpe, eff, loc) => ???
+    case Expr.Deref(exp, tpe, eff, loc) => ???
+    case Expr.Assign(exp1, exp2, tpe, eff, loc) => ???
+    case Expr.Ascribe(exp, tpe, eff, loc) => ???
+    case Expr.InstanceOf(exp, clazz, loc) => ???
+    case Expr.CheckedCast(cast, exp, tpe, eff, loc) => ???
+    case Expr.UncheckedCast(exp, declaredType, declaredEff, tpe, eff, loc) => ???
+    case Expr.UncheckedMaskingCast(exp, tpe, eff, loc) => ???
+    case Expr.Without(exp, effUse, tpe, eff, loc) => ???
+    case Expr.TryCatch(exp, rules, tpe, eff, loc) => ???
+    case Expr.TryWith(exp, effUse, rules, tpe, eff, loc) => ???
+    case Expr.Do(op, exps, tpe, eff, loc) => ???
+    case Expr.Resume(exp, tpe, loc) => ???
+    case Expr.InvokeConstructor(constructor, args, tpe, eff, loc) => ???
+    case Expr.InvokeMethod(method, exp, args, tpe, eff, loc) => ???
+    case Expr.InvokeStaticMethod(method, args, tpe, eff, loc) => ???
+    case Expr.GetField(field, exp, tpe, eff, loc) => ???
+    case Expr.PutField(field, exp1, exp2, tpe, eff, loc) => ???
+    case Expr.GetStaticField(field, tpe, eff, loc) => ???
+    case Expr.PutStaticField(field, exp, tpe, eff, loc) => ???
+    case Expr.NewObject(name, clazz, tpe, eff, methods, loc) => ???
+    case Expr.NewChannel(exp1, exp2, tpe, eff, loc) => ???
+    case Expr.GetChannel(exp, tpe, eff, loc) => ???
+    case Expr.PutChannel(exp1, exp2, tpe, eff, loc) => ???
+    case Expr.SelectChannel(rules, default, tpe, eff, loc) => ???
+    case Expr.Spawn(exp1, exp2, tpe, eff, loc) => ???
+    case Expr.ParYield(frags, exp, tpe, eff, loc) => ???
+    case Expr.Lazy(exp, tpe, loc) => ???
+    case Expr.Force(exp, tpe, eff, loc) => ???
+    case Expr.FixpointConstraintSet(cs, stf, tpe, loc) => ???
+    case Expr.FixpointLambda(pparams, exp, stf, tpe, eff, loc) => ???
+    case Expr.FixpointMerge(exp1, exp2, stf, tpe, eff, loc) => ???
+    case Expr.FixpointSolve(exp, stf, tpe, eff, loc) => ???
+    case Expr.FixpointFilter(pred, exp, tpe, eff, loc) => ???
+    case Expr.FixpointInject(exp, pred, tpe, eff, loc) => ???
+    case Expr.FixpointProject(pred, exp, tpe, eff, loc) => ???
+    case Expr.Error(m, tpe, eff) => ???
   }
 
   private def random[A](l: List[A]): A = ??? // TODO
