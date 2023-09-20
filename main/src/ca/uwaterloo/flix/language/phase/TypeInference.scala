fd5cf8a74ae05e3c5147d037fa327d35f2c12ca1
Matthew Lutze <mlutze@users.noreply.github.com>
2023-09-20 12:58:31 +0200

    migrate copyright stuff

--- main/src/ca/uwaterloo/flix/language/phase/TypeInference.scala
+++ main/src/ca/uwaterloo/flix/language/phase/TypeInference.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2016 Magnus Madsen
+ * Copyright 2015-2023 Magnus Madsen, Matthew Lutze
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- main/src/ca/uwaterloo/flix/language/phase/TypeReconstruction.scala
+++ main/src/ca/uwaterloo/flix/language/phase/TypeReconstruction.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 Matthew Lutze
+ * Copyright 2015-2023 Magnus Madsen, Matthew Lutze
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
