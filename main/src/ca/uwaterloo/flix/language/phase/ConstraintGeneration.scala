f0c9d88f4e477a29f623606e320241c1fa61a183
Matthew Lutze <mlutze@users.noreply.github.com>
2023-09-22 13:39:07 +0200

    change to implicit constraint building thing

--- main/src/ca/uwaterloo/flix/language/phase/ConstraintGeneration.scala
+++ main/src/ca/uwaterloo/flix/language/phase/ConstraintGeneration.scala
@@ -2,7 +2,8 @@ package ca.uwaterloo.flix.language.phase
 
 import ca.uwaterloo.flix.language.ast.KindedAst.Expr
 import ca.uwaterloo.flix.language.ast.{KindedAst, LevelEnv, RigidityEnv, SourceLocation, Symbol, Type}
-import ca.uwaterloo.flix.util.collection.Chain
+
+import scala.collection.mutable.ListBuffer
 
 object ConstraintGeneration {
 
@@ -13,25 +14,15 @@ object ConstraintGeneration {
 
     case class Class(sym: Symbol.ClassSym, tpe: Type, renv: RigidityEnv, lenv: LevelEnv, loc: SourceLocation) extends Constraint
   }
-  def unifyTypeM(tpe1: Type, tpe2: Type, renv: RigidityEnv, lenv: LevelEnv, loc: SourceLocation): Monad[Unit] = {
-    Monad((), Chain(Constraint.Equality(tpe1, tpe2, renv, lenv, loc)))
+
+  def unifyTypeM(tpe1: Type, tpe2: Type, renv: RigidityEnv, lenv: LevelEnv, loc: SourceLocation)(implicit c: Constraints): Unit = {
+    c.constrs.append(Constraint.Equality(tpe1, tpe2, renv, lenv, loc))
   }
 
-  case object Monad {
-    def point[A](value: A): Monad[A] = Monad(value, Chain.empty)
-  }
-  case class Monad[+A](value: A, constrs: Chain[Constraint]) {
-    def map[B](f: A => B): Monad[B] = Monad(f(value), constrs)
+  case class Constraints(constrs: ListBuffer[Constraint])
 
-    def flatMap[B](f: A => Monad[B]): Monad[B] = {
-      val Monad(newValue, newConstrs) = f(value)
-      Monad(newValue, constrs ++ newConstrs)
-    }
-
-    def withFilter(f: A => Boolean): Monad[A] = if (f(value)) this else Monad(value, Chain.empty)
-  }
-  def visitExp(exp0: KindedAst.Expr, renv: RigidityEnv, lenv: LevelEnv): Monad[(Type, Type)] = exp0 match {
-    case Expr.Var(sym, loc) => Monad.point((sym.tvar, Type.Pure))
+  def visitExp(exp0: KindedAst.Expr, renv: RigidityEnv, lenv: LevelEnv)(implicit c: Constraints): (Type, Type) = exp0 match {
+    case Expr.Var(sym, loc) => (sym.tvar, Type.Pure)
     case Expr.Def(sym, tpe, loc) => ???
     case Expr.Sig(sym, tpe, loc) => ???
     case Expr.Hole(sym, tpe, loc) => ???
@@ -41,32 +32,29 @@ object ConstraintGeneration {
     case Expr.Cst(cst, loc) => ???
     case Expr.Apply(exp, exps, tpe, eff, loc) => ???
     case Expr.Lambda(fparam, exp, loc) => {
-      for {
-        _ <- unifyTypeM(fparam.sym.tvar, fparam.tpe, renv, lenv, loc)
-        (tpe, eff) <- visitExp(exp, renv, lenv)
-        resTpe = Type.mkArrowWithEffect(fparam.tpe, eff, tpe, loc)
-        resEff = Type.Pure
-      } yield (resTpe, resEff)
+      unifyTypeM(fparam.sym.tvar, fparam.tpe, renv, lenv, loc)
+      val (tpe, eff) = visitExp(exp, renv, lenv)
+      val resTpe = Type.mkArrowWithEffect(fparam.tpe, eff, tpe, loc)
+      val resEff = Type.Pure
+      (resTpe, resEff)
     }
     case Expr.Unary(sop, exp, tpe, loc) => ???
     case Expr.Binary(sop, exp1, exp2, tpe, loc) => ???
     case Expr.IfThenElse(exp1, exp2, exp3, loc) =>
-      for {
-        (tpe1, eff1) <- visitExp(exp1, renv, lenv)
-        (tpe2, eff2) <- visitExp(exp2, renv, lenv)
-        (tpe3, eff3) <- visitExp(exp3, renv, lenv)
-        _ <- unifyTypeM(tpe1, Type.Bool, renv, lenv, exp1.loc) // TODO ASSOC-TYPES add `expectType`
-        _ <- unifyTypeM(tpe2, tpe3, renv, lenv, loc)
-        resTpe = tpe3
-        resEff = Type.mkUnion(eff1, eff2, eff3, loc)
-      } yield (resTpe, resEff)
+      val (tpe1, eff1) = visitExp(exp1, renv, lenv)
+      val (tpe2, eff2) = visitExp(exp2, renv, lenv)
+      val (tpe3, eff3) = visitExp(exp3, renv, lenv)
+      unifyTypeM(tpe1, Type.Bool, renv, lenv, exp1.loc)
+      unifyTypeM(tpe2, tpe3, renv, lenv, loc)
+      val resTpe = tpe3
+      val resEff = Type.mkUnion(eff1, eff2, eff3, loc)
+      (resTpe, resEff)
     case Expr.Stm(exp1, exp2, loc) =>
-      for {
-        (tpe1, eff1) <- visitExp(exp1, renv, lenv)
-        (tpe2, eff2) <- visitExp(exp2, renv, lenv)
-        resTpe = tpe2
-        resEff = Type.mkUnion(eff1, eff2, loc)
-      } yield (resTpe, resEff)
+      val (tpe1, eff1) = visitExp(exp1, renv, lenv)
+      val (tpe2, eff2) = visitExp(exp2, renv, lenv)
+      val resTpe = tpe2
+      val resEff = Type.mkUnion(eff1, eff2, loc)
+      (resTpe, resEff)
     case Expr.Discard(exp, loc) => ???
     case Expr.Let(sym, mod, exp1, exp2, loc) => ???
     case Expr.LetRec(sym, mod, exp1, exp2, loc) => ???
