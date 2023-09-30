1c47afcbb59158cdce7951acc26ea58725f05789
Joseph Tan <jdtan638@gmail.com>
2023-09-30 02:41:59 -0700

    made a MakeCompletion method

---
+++ main/src/ca/uwaterloo/flix/api/lsp/provider/completion/NewCompleter.scala
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2022 Paul Butcher, Lukas Rønn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ca.uwaterloo.flix.api.lsp.provider.completion
+
+import ca.uwaterloo.flix.api.Flix
+import ca.uwaterloo.flix.api.lsp.{Index, InsertTextFormat, TextEdit}
+import ca.uwaterloo.flix.api.lsp.provider.CompletionProvider.Priority
+import ca.uwaterloo.flix.api.lsp.provider.completion.Completion.NewCompletion
+import ca.uwaterloo.flix.language.ast.TypedAst
+
+object NewCompleter extends Completer {
+  def makeCompletion(text: String, context: CompletionContext): NewCompletion = {
+    NewCompletion(
+        name = text,
+        priority = Priority.normal("new completion"),
+        textEdit = TextEdit(context.range, text),
+        InsertTextFormat.PlainText
+    )
+  }
+  override def getCompletions(context: CompletionContext)(implicit flix: Flix, index: Index, root: TypedAst.Root, delta: DeltaContext): Iterable[NewCompletion] = {
+    val interfaceName = context.previousWord
+    makeCompletion(interfaceName, context) :: Nil
+  }
+}
