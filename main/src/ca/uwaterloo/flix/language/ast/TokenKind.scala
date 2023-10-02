addcdf7f9c8d772768b15ecf8a1d50bd0586d69a
HerlufFraDiku <herlufbaggesen13@gmail.com>
2023-10-02 09:13:24 +0200

    bug: missing symbols in tokenkind

--- main/src/ca/uwaterloo/flix/language/ast/TokenKind.scala
+++ main/src/ca/uwaterloo/flix/language/ast/TokenKind.scala
@@ -97,6 +97,8 @@ object TokenKind {
 
   case object InfixFunction extends TokenKind
 
+  case object KeywordAbsent extends TokenKind
+
   case object KeywordAlias extends TokenKind
 
   case object KeywordAnd extends TokenKind
@@ -194,6 +196,8 @@ object TokenKind {
   case object KeywordOverride extends TokenKind
 
   case object KeywordPar extends TokenKind
+
+  case object KeywordPresent extends TokenKind
 
   case object KeywordProject extends TokenKind
 
