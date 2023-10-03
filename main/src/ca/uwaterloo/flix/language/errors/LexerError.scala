118394ce75dffe3497e34aaf8caee586d64f235d
HerlufFraDiku <herlufbaggesen13@gmail.com>
2023-10-03 10:37:01 +0200

    chore: better LexerError messages

--- main/src/ca/uwaterloo/flix/language/errors/LexerError.scala
+++ main/src/ca/uwaterloo/flix/language/errors/LexerError.scala
@@ -18,7 +18,6 @@ package ca.uwaterloo.flix.language.errors
 import ca.uwaterloo.flix.language.CompilationMessage
 import ca.uwaterloo.flix.language.ast.SourceLocation
 import ca.uwaterloo.flix.util.Formatter
-import ca.uwaterloo.flix.language.phase.Lexer.{InterpolatedStringMaxNestingLevel, BlockCommentMaxNestingLevel}
 
 sealed trait LexerError extends CompilationMessage {
   val kind = "Lexer Error"
@@ -43,10 +42,7 @@ object LexerError {
          |""".stripMargin
     }
 
-    override def explain(formatter: Formatter): Option[String] = Some({
-      import formatter._
-      s"${underline("Tip:")} Ensure that block-comments are not nested more than $BlockCommentMaxNestingLevel levels deep."
-    })
+    override def explain(formatter: Formatter): Option[String] = None
   }
 
   /**
@@ -104,7 +100,7 @@ object LexerError {
     override def message(formatter: Formatter): String = {
       import formatter._
       s"""${line(kind, source.name)}
-         |>> Unterminated block-comment.
+         |>> Missing '*/' in block-comment.
          |
          |${code(loc, "Block-comment starts here.")}
          |
@@ -125,7 +121,7 @@ object LexerError {
     override def message(formatter: Formatter): String = {
       import formatter._
       s"""${line(kind, source.name)}
-         |>> Unterminated built-in.
+         |>> Missing '$$' in built-in.
          |
          |${code(loc, "Built-in starts here.")}
          |
@@ -146,7 +142,7 @@ object LexerError {
     override def message(formatter: Formatter): String = {
       import formatter._
       s"""${line(kind, source.name)}
-         |>> Unterminated char.
+         |>> Missing `'` in char.
          |
          |${code(loc, "Char starts here")}
          |
@@ -167,7 +163,7 @@ object LexerError {
     override def message(formatter: Formatter): String = {
       import formatter._
       s"""${line(kind, source.name)}
-         |>> Unterminated infix function.
+         |>> Missing '`' in infix function.
          |
          |${code(loc, "Infix function starts here.")}
          |
@@ -188,7 +184,7 @@ object LexerError {
     override def message(formatter: Formatter): String = {
       import formatter._
       s"""${line(kind, source.name)}
-         |>> Unterminated string.
+         |>> missing '"' in string.
          |
          |${code(loc, "String starts here.")}
          |
@@ -209,7 +205,7 @@ object LexerError {
     override def message(formatter: Formatter): String = {
       import formatter._
       s"""${line(kind, source.name)}
-         |>> Unterminated string interpolation.
+         |>> Missing '}' in string interpolation.
          |
          |${code(loc, "Interpolation starts here.")}
          |
@@ -230,16 +226,13 @@ object LexerError {
     override def message(formatter: Formatter): String = {
       import formatter._
       s"""${line(kind, source.name)}
-         |>> String interpolation  nested too deep.
+         |>> String interpolation nested too deep.
          |
          |${code(loc, "Interpolation starts here.")}
          |
          |""".stripMargin
     }
 
-    override def explain(formatter: Formatter): Option[String] = Some({
-      import formatter._
-      s"${underline("Tip:")} Ensure that string interpolations are not nested more than $InterpolatedStringMaxNestingLevel levels deep."
-    })
+    override def explain(formatter: Formatter): Option[String] = None
   }
 }
