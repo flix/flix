b36ac1553bcbe69745afd444d7065af8609793a9
Jonathan Lindegaard Starup <32037926+JonathanStarup@users.noreply.github.com>
2023-10-04 15:27:30 +0200

    chore: Refine effect example (#6532)

--- main/src/dev/flix/runtime/Frames.java
+++ main/src/dev/flix/runtime/Frames.java
@@ -2,17 +2,9 @@ package dev.flix.runtime;
 
 /**
  * Frames = FramesNil | FramesCons.
- *
- *
  */
 public interface Frames {
-    default Frames push(Frame t) {
-        return new FramesCons(t, this);
-    }
+    Frames push(Frame t);
 
-    // TODO: Implement other reverse function.
-    default Frames reverse() {
-        return reverseOnto(new FramesNil());
-    }
     Frames reverseOnto(Frames rest);
 }
