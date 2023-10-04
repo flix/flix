f1c7a4de02fdfeba9c79ac2bfcd18b36577c912e
JonathanStarup <32037926+JonathanStarup@users.noreply.github.com>
2023-10-04 14:55:09 +0200

    docs: refine-effect-example

--- main/src/dev/flix/runtime/example/ConsoleHandler17.java
+++ main/src/dev/flix/runtime/example/ConsoleHandler17.java
@@ -4,16 +4,17 @@ import dev.flix.runtime.Handler;
 import dev.flix.runtime.Result;
 import dev.flix.runtime.Resumption;
 import dev.flix.runtime.Unit;
+import dev.flix.runtime.Value;
 
 public class ConsoleHandler17 implements Console, Handler {
 
     public Result read(Unit opArg, Resumption k) {
-        return Resumption.rewind(k, 42);    // resume(42)
+        return Resumption.rewind(k, new Value("John"));    // resume("John")
     }
 
     public Result print(String opArg, Resumption k) {
         System.out.println("STDOUT: " + opArg);
-        return Resumption.rewind(k, 1337);  // resume(1337)
+        return Resumption.rewind(k, new Value(Unit.instance));  // resume()
     }
 
 }
