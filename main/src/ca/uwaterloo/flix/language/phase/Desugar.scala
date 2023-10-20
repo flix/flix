9fd2dd7b8259a6222a721dbee5e903ffbac82b88
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2023-10-20 21:35:14 +0200

    Add run function

--- main/src/ca/uwaterloo/flix/language/phase/Desugar.scala
+++ main/src/ca/uwaterloo/flix/language/phase/Desugar.scala
@@ -1,5 +1,16 @@
 package ca.uwaterloo.flix.language.phase
 
+import ca.uwaterloo.flix.api.Flix
+import ca.uwaterloo.flix.language.ast.{DesugaredAst, WeededAst}
+
 object Desugar {
+
+
+  /**
+    * Performs desugaring on the entire program.
+    */
+  def run(program: WeededAst.Root)(implicit flix: Flix): DesugaredAst.Root = flix.phase("Desugar") {
+    ???
+  }
 
 }
