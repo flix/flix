405b2c6fb59ac59b1fb1d300ad2065ee51caa60c
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2023-10-20 22:37:20 +0200

    Add functions

--- main/src/ca/uwaterloo/flix/language/phase/Desugar.scala
+++ main/src/ca/uwaterloo/flix/language/phase/Desugar.scala
@@ -1,7 +1,7 @@
 package ca.uwaterloo.flix.language.phase
 
 import ca.uwaterloo.flix.api.Flix
-import ca.uwaterloo.flix.language.ast.{Ast, DesugaredAst, SourceLocation, WeededAst}
+import ca.uwaterloo.flix.language.ast.{Ast, DesugaredAst, WeededAst}
 import ca.uwaterloo.flix.util.ParOps
 
 object Desugar {
@@ -22,22 +22,80 @@ object Desugar {
   }
 
   /**
-    * Performs naming on the given compilation unit `unit` under the given (partial) program `prog0`.
+    * Desugars the given compilation unit `unit`.
     */
   private def visitUnit(src: Ast.Source, unit: WeededAst.CompilationUnit)(implicit flix: Flix): (Ast.Source, DesugaredAst.CompilationUnit) = unit match {
     case WeededAst.CompilationUnit(usesAndImports0, decls0, loc) =>
       val usesAndImports = usesAndImports0.map(visitUseOrImport)
       val decls = decls0.map(visitDecl)
-      src -> DesugaredAst.CompilationUnit(usesAndImports.flatten, decls.flatten, loc)
+      src -> DesugaredAst.CompilationUnit(usesAndImports, decls, loc)
   }
 
   /**
-    * Performs weeding on the given use or import `u0`.
+    * Maps `u0` to a corresponding [[DesugaredAst.UseOrImport]].
     */
-  private def visitUseOrImport(u0: WeededAst.UseOrImport): List[DesugaredAst.UseOrImport] = ???
+  private def visitUseOrImport(u0: WeededAst.UseOrImport): DesugaredAst.UseOrImport = u0 match {
+    case WeededAst.UseOrImport.Use(qname, alias, loc) => DesugaredAst.UseOrImport.Use(qname, alias, loc)
+    case WeededAst.UseOrImport.Import(name, alias, loc) => DesugaredAst.UseOrImport.Import(name, alias, loc)
+  }
 
   /**
-    * Compiles the given parsed declaration `decl` to a list of weeded declarations.
+    * Compiles `decl` to a list of [[DesugaredAst.Declaration]].
     */
-  private def visitDecl(decl: WeededAst.Declaration)(implicit flix: Flix): List[DesugaredAst.Declaration] = ???
+  private def visitDecl(decl: WeededAst.Declaration)(implicit flix: Flix): DesugaredAst.Declaration = decl match {
+    case WeededAst.Declaration.Namespace(ident, usesAndImports, decls, loc) =>
+      val usesAndImportsVal = usesAndImports.map(visitUseOrImport)
+      val declsVal = decls.map(visitDecl)
+      DesugaredAst.Declaration.Namespace(ident, usesAndImportsVal, declsVal, loc)
+
+    case d: WeededAst.Declaration.Class => visitClass(d)
+    case d: WeededAst.Declaration.Instance => visitInstance(d)
+    case d: WeededAst.Declaration.Def => visitDef(d)
+    case d: WeededAst.Declaration.Law => visitLaw(d)
+    case d: WeededAst.Declaration.Enum => visitEnum(d)
+    case d: WeededAst.Declaration.RestrictableEnum => visitRestrictableEnum(d)
+    case d: WeededAst.Declaration.TypeAlias => visitTypeAlias(d)
+    case d: WeededAst.Declaration.Effect => visitEffect(d)
+  }
+
+  /**
+    * Desugars the given [[WeededAst.Declaration.Class]] `d`.
+    */
+  private def visitClass(d: WeededAst.Declaration.Class): DesugaredAst.Declaration.Class = ???
+
+  /**
+    * Desugars the given [[WeededAst.Declaration.Instance]] `d`.
+    */
+  private def visitInstance(d: WeededAst.Declaration.Instance): DesugaredAst.Declaration.Instance = ???
+
+  /**
+    * Desugars the given [[WeededAst.Declaration.Def]] `d`.
+    */
+  private def visitDef(d: WeededAst.Declaration.Def): DesugaredAst.Declaration.Def = ???
+
+  /**
+    * Desugars the given [[WeededAst.Declaration.Law]] `d`.
+    */
+  private def visitLaw(d: WeededAst.Declaration.Law): DesugaredAst.Declaration.Law = ???
+
+  /**
+    * Desugars the given [[WeededAst.Declaration.Enum]] `d`.
+    */
+  private def visitEnum(d: WeededAst.Declaration.Enum): DesugaredAst.Declaration.Enum = ???
+
+  /**
+    * Desugars the given [[WeededAst.Declaration.RestrictableEnum]] `d`.
+    */
+  private def visitRestrictableEnum(d: WeededAst.Declaration.RestrictableEnum): DesugaredAst.Declaration.RestrictableEnum = ???
+
+  /**
+    * Desugars the given [[WeededAst.Declaration.TypeAlias]] `d`.
+    */
+  private def visitTypeAlias(d: WeededAst.Declaration.TypeAlias): DesugaredAst.Declaration.TypeAlias = ???
+
+  /**
+    * Desugars the given [[WeededAst.Declaration.Effect]] `d`.
+    */
+  private def visitEffect(d: WeededAst.Declaration.Effect): DesugaredAst.Declaration.Effect = ???
+
 }
