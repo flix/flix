955ffe76b93aa6fd0af3238dc9e7e8d0c38c20f9
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2023-11-04 11:24:27 +0100

    Add impl

--- main/src/ca/uwaterloo/flix/language/phase/Weeder.scala
+++ main/src/ca/uwaterloo/flix/language/phase/Weeder.scala
@@ -912,6 +912,9 @@ object Weeder {
         case _ if frags.isEmpty =>
           val err = WeederError.IllegalEmptyForFragment(loc)
           WeededAst.Expr.Error(err).toSoftFailure(err)
+        case (WeededAst.ForFragment.Guard(_, loc1) :: _, _) =>
+          val err = WeederError.IllegalForFragment(loc1)
+          WeededAst.Expr.Error(err).toSoftFailure(err)
         case (fs1, e1) => WeededAst.Expr.MonadicFor(fs1, e1, loc).toSuccess
       }
 
