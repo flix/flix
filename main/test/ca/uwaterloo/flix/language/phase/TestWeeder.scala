060a531b418f8bc86f25936878733271bdb0062a
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2023-11-04 11:30:38 +0100

    Ues LibNix in more tests

--- main/test/ca/uwaterloo/flix/language/phase/TestWeeder.scala
+++ main/test/ca/uwaterloo/flix/language/phase/TestWeeder.scala
@@ -1260,11 +1260,11 @@ class TestWeeder extends AnyFunSuite with TestUtils {
       """
         |def f(): Int32 = {
         | @Test
-        | def g(i) = if (i <= 0) 0 else 1 + g(i - 1);
+        | def g(i) = if (i <= 0) 0 else g(i - 1);
         | g(10)
         |}
         |""".stripMargin
-    val result = compile(input, Options.TestWithLibMin)
+    val result = compile(input, Options.TestWithLibNix)
     expectError[WeederError.IllegalInnerFunctionAnnotation](result)
   }
 
@@ -1273,11 +1273,11 @@ class TestWeeder extends AnyFunSuite with TestUtils {
       """
         |def f(): Int32 = {
         | @benchmark @Tailrec
-        | def g(i) = if (i <= 0) 0 else 1 + g(i - 1);
+        | def g(i) = if (i <= 0) 0 else g(i - 1);
         | g(10)
         |}
         |""".stripMargin
-    val result = compile(input, Options.TestWithLibMin)
+    val result = compile(input, Options.TestWithLibNix)
     expectError[WeederError.IllegalInnerFunctionAnnotation](result)
   }
 
@@ -1286,11 +1286,11 @@ class TestWeeder extends AnyFunSuite with TestUtils {
       """
         |def f(): Int32 = {
         | @Tailrec @Tailrec
-        | def g(i) = if (i <= 0) 0 else 1 + g(i - 1);
+        | def g(i) = if (i <= 0) 0 else g(i - 1);
         | g(10)
         |}
         |""".stripMargin
-    val result = compile(input, Options.TestWithLibMin)
+    val result = compile(input, Options.TestWithLibNix)
     expectError[WeederError.DuplicateAnnotation](result)
   }
 
