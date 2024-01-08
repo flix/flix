2fb60d9e76f9c62e822fd38ab27dc4574d7f2754
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2024-01-08 20:48:19 +0100

    Fix test

--- main/test/ca/uwaterloo/flix/util/TestChain.scala
+++ main/test/ca/uwaterloo/flix/util/TestChain.scala
@@ -102,7 +102,7 @@ class TestChain extends AnyFunSuite {
 
   test("TestEq.09") {
     val c1 = Chain(1) ++ Chain(2) ++ Chain(3) ++ Chain(4) ++ Chain(5)
-    val c2 = Chain(1, 2, 3, 4, 6)
+    val c2 = Chain(1, 2, 3, 4, 5)
     assert(c1 == c2)
   }
 
