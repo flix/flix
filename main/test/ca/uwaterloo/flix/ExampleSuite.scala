b92f2681b2432206f52921d576e237f1590418fa
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2024-01-18 16:35:45 +0100

    Include domains subdir

--- main/test/ca/uwaterloo/flix/ExampleSuite.scala
+++ main/test/ca/uwaterloo/flix/ExampleSuite.scala
@@ -22,8 +22,10 @@ class ExampleSuite extends FlixSuite(incremental = true) {
   private implicit val TestOptions: Options = Options.TestWithLibAll
 
   mkTestDir("examples")
+  mkTestDir("examples/larger-examples")
   mkTestDir("examples/larger-examples/datalog")
   mkTestDir("examples/larger-examples/program-analysis")
+  mkTestDir("examples/larger-examples/program-analysis/domains")
   mkTestDir("examples/larger-examples/restrictable-variants")
 
 }
