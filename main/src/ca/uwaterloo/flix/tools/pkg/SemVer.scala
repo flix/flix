53fee02612a91e1fea27df7c4b3b647dcc8fb7a8
sockmaster27 <61235930+sockmaster27@users.noreply.github.com>
2024-02-13 13:31:32 +0100

    refactor: 'availableVersions' -> 'versions'

--- main/src/ca/uwaterloo/flix/tools/pkg/SemVer.scala
+++ main/src/ca/uwaterloo/flix/tools/pkg/SemVer.scala
@@ -44,52 +44,52 @@ case class SemVer(major: Int, minor: Int, patch: Option[Int], buildDot: Option[I
   }
 
   /**
-    * Of the given `availableVersions`, get the newest version which is a major update, if one exists.
+    * Of the given `versions`, get the newest version which is a major update, if one exists.
     *
     * Example:
     *   - this = 1.2.0
-    *   - available versions = 1.2.0, 1.2.1, 1.3.0, 2.0.0
+    *   - versions = 1.2.0, 1.2.1, 1.3.0, 2.0.0
     *   - -> 2.0.0
     */
-  def majorUpdate(availableVersions: List[SemVer]): Option[SemVer] =
-    availableVersions.filter(v =>
+  def majorUpdate(versions: List[SemVer]): Option[SemVer] =
+    versions.filter(v =>
       v.major > major
     ).maxOption
 
   /**
-    * Of the given `availableVersions`, get the newest version which is a minor update, if one exists.
+    * Of the given `versions`, get the newest version which is a minor update, if one exists.
     *
     * Example:
     *   - this = 1.2.0
-    *   - available versions = 1.2.0, 1.2.1, 1.3.0, 2.0.0
+    *   - versions = 1.2.0, 1.2.1, 1.3.0, 2.0.0
     *   - -> 1.3.0
     */
-  def minorUpdate(availableVersions: List[SemVer]): Option[SemVer] =
-    availableVersions.filter(v =>
+  def minorUpdate(versions: List[SemVer]): Option[SemVer] =
+    versions.filter(v =>
       v.major == major
         && v.minor > minor
     ).maxOption
 
   /**
-    * Of the given `availableVersions`, get the newest version which is a patch update, if one exists.
+    * Of the given `versions`, get the newest version which is a patch update, if one exists.
     *
     * Example 1:
     *   - this = 1.2.0
-    *   - available versions = 1.2.0, 1.2.1, 1.3.0, 2.0.0
+    *   - versions = 1.2.0, 1.2.1, 1.3.0, 2.0.0
     *   - -> 1.2.1
     *
     * Example 2:
     *   - this = 1.2
-    *   - available versions = 1.2, 1.2.1
+    *   - versions = 1.2, 1.2.1
     *   - -> None
     *
     * Example 3:
     *   - this = 1.2.0
-    *   - available versions = 1.2.0, 1.2
+    *   - versions = 1.2.0, 1.2
     *   - -> None
     */
-  def patchUpdate(availableVersions: List[SemVer]): Option[SemVer] =
-    availableVersions.filter(v =>
+  def patchUpdate(versions: List[SemVer]): Option[SemVer] =
+    versions.filter(v =>
       v.major == major
         && v.minor == minor
         && (v.patch zip patch).exists { case (vp, cvp) => vp > cvp }
