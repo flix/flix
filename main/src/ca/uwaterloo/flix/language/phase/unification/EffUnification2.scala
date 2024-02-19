03c76c5f781a7ecf50b6c31cf137ee81f1ae50ff
Magnus Madsen <magnusm@cs.au.dk>
2024-02-19 11:41:48 +0100

    feat: add new `unifyAll` for Boolean effect formulas.

--- main/src/ca/uwaterloo/flix/language/phase/unification/EffUnification2.scala
+++ main/src/ca/uwaterloo/flix/language/phase/unification/EffUnification2.scala
@@ -16,8 +16,11 @@
 package ca.uwaterloo.flix.language.phase.unification
 
 import ca.uwaterloo.flix.api.Flix
-import ca.uwaterloo.flix.language.ast.{RigidityEnv, Type}
-import ca.uwaterloo.flix.util.Result
+import ca.uwaterloo.flix.language.ast.{RigidityEnv, SourceLocation, Type}
+import ca.uwaterloo.flix.util.collection.Bimap
+import ca.uwaterloo.flix.util.{InternalCompilerException, Result}
+
+import scala.collection.immutable.SortedSet
 
 object EffUnification2 {
 
@@ -27,7 +30,91 @@ object EffUnification2 {
    * Note: A type in `l` must not contain any associated effects.
    */
   def unifyAll(l: List[(Type, Type)], renv0: RigidityEnv)(implicit flix: Flix): Result[Substitution, UnificationError] = {
+    // Case 1: If the list is empty we can return immediately.
+    if (l.isEmpty) {
+      return Result.Ok(Substitution.empty)
+    }
+
+    // Case 2: We translate every (Type, Type) pair to a pair of Boolean effect formulas.
+
     ??? // TODO
+  }
+
+  private case class Equation(t1: Term, t2: Term)
+
+  private sealed trait Classification
+
+  private object Classification {
+    /**
+     * An equation of the form `x = true`, `x = false`, and their mirrored versions.
+     */
+    case object Ground
+
+
+    def classify(e: Equation): Classification = ???
+  }
+
+  private sealed trait Term {
+
+    final def freeVars: SortedSet[Int] = this match {
+      case Term.True => SortedSet.empty
+      case Term.False => SortedSet.empty
+      case Term.Var(x) => SortedSet(x)
+      case Term.Not(f) => f.freeVars
+      case Term.And(ts) => ???
+      case Term.Or(ts) => ???
+    }
+
+    final def size: Int = this match {
+      case Term.True => 0
+      case Term.False => 0
+      case Term.Var(_) => 0
+      case Term.Not(t) => t.size
+      case Term.And(ts) => ???
+      case Term.Or(ts) => ???
+    }
+
+    override def toString: String = this match {
+      case Term.True => "true"
+      case Term.False => "false"
+      case Term.Var(x) => s"x$x"
+      case Term.Not(f) => f match {
+        case Term.Var(x) => s"!x$x"
+        case _ => s"!($f)"
+      }
+      case Term.And(ts) => ???
+      case Term.Or(ts) => ???
+    }
+
+  }
+
+  private object Term {
+
+    private case object True extends Term
+
+    private case object False extends Term
+
+    private case class Var(x: Int) extends Term
+
+    private case class Not(t: Term) extends Term
+
+    private case class And(ts: List[Term]) extends Term
+
+    private case class Or(ts: List[Term]) extends Term
+
+    final def mkNot(t: Term): Term = ???
+
+    final def substitute(f: Term, m: Bimap[Int, Int]): Term = f match {
+      case True => True
+      case False => False
+      case Var(x) => m.getForward(x) match {
+        case None => throw InternalCompilerException(s"Unexpected unbound variable: 'x$x'.", SourceLocation.Unknown)
+        case Some(y) => Var(y)
+      }
+      case Not(t) => mkNot(substitute(t, m))
+      case And(ts) => ???
+      case Or(ts) => ???
+    }
   }
 
 }
