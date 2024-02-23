50e8eb5a5a9f9c4ecd612a498aff5235f83665f1
Matthew Lutze <mlutze@users.noreply.github.com>
2024-02-23 13:30:39 +0100

    remove renv check

--- main/src/ca/uwaterloo/flix/language/phase/ConstraintResolution.scala
+++ main/src/ca/uwaterloo/flix/language/phase/ConstraintResolution.scala
@@ -541,7 +541,7 @@ object ConstraintResolution {
 
   def isSimpleEffectType(tpe: Type, renv: RigidityEnv): Boolean = {
     def visit(t: Type): Boolean = t match {
-      case Type.Var(sym, loc) => renv.isFlexible(sym)
+      case Type.Var(sym, loc) => true
 
       case Type.Pure => true
       case Type.Univ => true
