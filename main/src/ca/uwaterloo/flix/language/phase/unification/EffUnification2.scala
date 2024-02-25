60c65a3bf107af125f6903dfe1b96d93b0ddb2af
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2024-02-25 17:00:55 +0100

    feat: add better Bool unification

--- main/src/ca/uwaterloo/flix/language/phase/unification/EffUnification2.scala
+++ main/src/ca/uwaterloo/flix/language/phase/unification/EffUnification2.scala
@@ -28,20 +28,20 @@ object EffUnification2 {
   /**
     * Returns the most general unifier of the pairwise unification equations in `l`.
     */
-  def unifyAll(l: List[(Type, Type)], renv0: RigidityEnv)(implicit flix: Flix): Result[Substitution, UnificationError] = {
+  def unifyAll(l: List[(Type, Type)], renv: RigidityEnv)(implicit flix: Flix): Result[Substitution, UnificationError] = {
     // Compute a bi-directional from type variables to ints.
     implicit val bimap: Bimap[Type.Var, Int] = mkBidirectionalVarMap(l)
 
     // Translate all unification problems from equations on types to equations on terms.
-    val equations = l.map(mkEquation(_, renv0))
+    val equations = l.map(toEquation(_)(renv, bimap))
 
     // Compute the most-general unifier of all the term equations.
     solveAll(equations) match {
-      case Result.Ok(subst) => Result.Ok(toTypeSubst(subst))
+      case Result.Ok(subst) => Result.Ok(fromBoolSubst(subst))
 
       case Result.Err(ex) =>
-        val tpe1 = toType(ex.x, SourceLocation.Unknown) // TODO: Add more precise source locations.
-        val tpe2 = toType(ex.y, SourceLocation.Unknown) // TODO: Add more precise source locations.
+        val tpe1 = fromTerm(ex.x, SourceLocation.Unknown) // TODO: Add more precise source locations.
+        val tpe2 = fromTerm(ex.y, SourceLocation.Unknown) // TODO: Add more precise source locations.
         Result.Err(UnificationError.MismatchedEffects(tpe1, tpe2))
     }
   }
@@ -72,46 +72,60 @@ object EffUnification2 {
   /**
     * Translates the given unification equation on types `p` into a unification equation on terms.
     */
-  private def mkEquation(p: (Type, Type), renv0: RigidityEnv)(implicit bimap: Bimap[Type.Var, Int]): Equation = {
+  private def toEquation(p: (Type, Type))(implicit renv: RigidityEnv, bimap: Bimap[Type.Var, Int]): Equation = {
     val (tpe1, tpe2) = p
-    Equation.mk(fromType(tpe1, bimap, renv0), fromType(tpe2, bimap, renv0))
+    Equation.mk(toTerm(tpe1), toTerm(tpe2))
   }
 
-  private def toTypeSubst(s: FastBoolUnification.BoolSubstitution)(implicit bimap: Bimap[Type.Var, Int]): Substitution = {
+  /**
+    * Returns the given type `t` as term.
+    *
+    * The bimap is used to map type variables to term variables.
+    *
+    * The rigidity environment is used to map rigid type variables to constants and flexible type variables to term variables.
+    */
+  private def toTerm(t: Type)(implicit renv: RigidityEnv, bimap: Bimap[Type.Var, Int]): Term = Type.eraseTopAliases(t) match {
+    case Type.Pure => Term.True
+    case Type.Univ => Term.False
+
+    case t: Type.Var => bimap.getForward(t) match {
+      case None => throw InternalCompilerException(s"Unexpected unbound type variable: '$t'.", t.loc)
+      case Some(x) => renv.get(t.sym) match {
+        case Rigidity.Flexible => Term.Var(x) // A flexible variable is a real variable.
+        case Rigidity.Rigid => Term.Cst(x) // A rigid variable is a constant.
+      }
+    }
+
+    case Type.Cst(TypeConstructor.Effect(sym), _) => throw InternalCompilerException("Not yet", t.loc) // TODO: What should be done here?
+
+    case Type.Apply(Type.Cst(TypeConstructor.Complement, _), tpe1, _) => Term.mkNot(toTerm(tpe1))
+    case Type.Apply(Type.Apply(Type.Cst(TypeConstructor.Union, _), tpe1, _), tpe2, _) => Term.mkAnd(toTerm(tpe1), toTerm(tpe2))
+    case Type.Apply(Type.Apply(Type.Cst(TypeConstructor.Intersection, _), tpe1, _), tpe2, _) => Term.mkOr(toTerm(tpe1), toTerm(tpe2))
+
+    case Type.Cst(TypeConstructor.Error(_), _) => Term.True // TODO: What?
+
+    case _ => throw InternalCompilerException(s"Unexpected type: '$t'.", t.loc)
+  }
+
+  /**
+    * Returns a Boolean substitution as a regular type substitution.
+    */
+  private def fromBoolSubst(s: FastBoolUnification.BoolSubstitution)(implicit bimap: Bimap[Type.Var, Int]): Substitution = {
     Substitution(s.m.foldLeft(Map.empty[Symbol.KindedTypeVarSym, Type]) {
-      case (macc, (k, v)) => macc + (bimap.getBackward(k).get.sym -> toType(v, SourceLocation.Unknown))
+      case (macc, (k, v)) => macc + (bimap.getBackward(k).get.sym -> fromTerm(v, SourceLocation.Unknown))
     })
   }
 
-  private def toType(t0: Term, loc: SourceLocation)(implicit m: Bimap[Type.Var, Int]): Type = t0 match {
+  private def fromTerm(t: Term, loc: SourceLocation)(implicit m: Bimap[Type.Var, Int]): Type = t match {
     case Term.True => Type.Pure
     case Term.False => Type.Univ
     case Term.Cst(c) => m.getBackward(c).get
     case Term.Var(x) => m.getBackward(x).get
-    case Term.Not(t) => Type.mkComplement(toType(t, loc), loc)
+    case Term.Not(t) => Type.mkComplement(fromTerm(t, loc), loc)
     case Term.And(csts, vars, rest) =>
-      val ts = csts.toList.map(toType(_, loc)) ++ vars.toList.map(toType(_, loc)) ++ rest.map(toType(_, loc))
+      val ts = csts.toList.map(fromTerm(_, loc)) ++ vars.toList.map(fromTerm(_, loc)) ++ rest.map(fromTerm(_, loc))
       Type.mkIntersection(ts, loc)
-    case Term.Or(ts) => Type.mkUnion(ts.map(toType(_, loc)), loc)
+    case Term.Or(ts) => Type.mkUnion(ts.map(fromTerm(_, loc)), loc)
   }
-
-  private def fromType(t: Type, env: Bimap[Type.Var, Int], renv: RigidityEnv): Term = Type.eraseTopAliases(t) match {
-    case t: Type.Var => env.getForward(t) match {
-      case None => throw InternalCompilerException(s"Unexpected unbound variable: '$t'.", t.loc)
-      case Some(x) => renv.get(t.sym) match {
-        case Rigidity.Flexible => Term.Var(x)
-        case Rigidity.Rigid => Term.Cst(x)
-      }
-    }
-    case Type.Cst(TypeConstructor.Effect(sym), _) => throw InternalCompilerException("Not yet", t.loc) // TODO
-    case Type.Pure => Term.True
-    case Type.Univ => Term.False
-    case Type.Apply(Type.Cst(TypeConstructor.Complement, _), tpe1, _) => Term.mkNot(fromType(tpe1, env, renv))
-    case Type.Apply(Type.Apply(Type.Cst(TypeConstructor.Union, _), tpe1, _), tpe2, _) => Term.mkAnd(fromType(tpe1, env, renv), fromType(tpe2, env, renv))
-    case Type.Apply(Type.Apply(Type.Cst(TypeConstructor.Intersection, _), tpe1, _), tpe2, _) => Term.mkOr(fromType(tpe1, env, renv), fromType(tpe2, env, renv))
-    case Type.Cst(TypeConstructor.Error(_), _) => Term.True
-    case _ => throw InternalCompilerException(s"Unexpected type: '$t'.", t.loc)
-  }
-
 
 }
