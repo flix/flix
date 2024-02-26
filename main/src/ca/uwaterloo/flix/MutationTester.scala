8db99998aaf579a1e6fd24441fe271d5485afa79
au701790 <au701790@uni.au.dk>
2024-02-26 15:16:07 +0100

    refactored mutateSop and added cases to mutateExpr

--- main/src/ca/uwaterloo/flix/MutationTester.scala
+++ main/src/ca/uwaterloo/flix/MutationTester.scala
@@ -25,6 +25,8 @@ import ca.uwaterloo.flix.util._
 import org.json4s.reflect.fail
 
 import java.io.File
+import scala.::
+import scala.util.Random
 
 
 object MutationTester {
@@ -41,7 +43,7 @@ object MutationTester {
             |    pub def fake(): Int32 = 99
             |}
             |mod Test {
-            |    pub def one(): Int32 = 1
+            |    pub def one(): Int32 = 0 + 1
             |    pub def ite(b: Bool): Int32 = if(b) 1 else 2
             |    pub def m(i: Int32): Int32 = match i {
             |       case 1 => 100
@@ -66,7 +68,8 @@ object MutationTester {
         val start = System.nanoTime()
         val root1 = mutateRoot(root, testee)
         val end = System.nanoTime() - start
-        println("time to generate mutations: " + end.toString)
+        val timeSec = end.toFloat / 1_000_000_000.0
+        println(s"time to generate mutations: $timeSec")
         runMutations(flix, root, root1)
         /**
         val result = root1.map(r => flix.codeGen(r).unsafeGet)
@@ -80,11 +83,15 @@ object MutationTester {
         var survivorCount = 0
         var mutantCounter = 0
         mutatedDefs.foreach(mut => {
+            val defName = mut._1.toString
+            val mutationAmount = mut._2.length
+            println(s"testing $defName with $mutationAmount mutations")
             val defs = root.defs
-            mutantCounter += mut._2.length
             mut._2.foreach(mDef => {
+                mutantCounter += 1
                 val start = System.nanoTime()
                 val n = defs + (mut._1 -> mDef)
+                println(s"mutation: $mDef")
                 val newRoot = root.copy(defs = n)
                 val cRes = flix.codeGen(newRoot).unsafeGet
                 val testResults = cRes.getTests.values.forall(c =>
@@ -122,6 +129,8 @@ object MutationTester {
             case (s, fun) =>
                 if (defSyms.contains(s)) {
                     val mutExps = mutateExpr(fun.exp)
+                    println(fun.exp)
+                    println(mutExps)
                     val mutDefs = mutExps.map(mexp => fun.copy(exp = mexp))
                     Some(d._1 -> mutDefs)
                 } else None
@@ -129,84 +138,154 @@ object MutationTester {
         })
     }
 
-    private def mutateExpr(e: TypedAst.Expr): List[TypedAst.Expr] =
-        e match {
-            case Expr.Cst(cst, tpe, loc) =>
-                mutateCst(cst).map(m => Expr.Cst(m, tpe, loc))
-            case Expr.Binary(sop, exp1, exp2, typ, eff, loc) =>
-                val mut1 = Expr.Binary(mutateSop(sop), exp1, exp2, typ, eff, loc)
-                val mut2 = mutateExpr(exp1).map(m => Expr.Binary(sop, m, exp2, typ, eff, loc))
-                val mut3 = mutateExpr(exp2).map(m => Expr.Binary(sop, exp1, m, typ, eff, loc))
-                mut1::mut2:::mut3
-            case original@Expr.IfThenElse(exp1, exp2, exp3,_, _, _) =>
-                val mut1 = mutateExpr(exp1).map(m => original.copy(exp1 = m))
-                val mut2 = mutateExpr(exp2).map(m => original.copy(exp2 = m))
-                val mut3 = mutateExpr(exp3).map(m => original.copy(exp3 = m))
-                mut1:::mut2:::mut3
-            // var doesn't contain a subtree, and we don't mutate them so we don't need to return them
-            case Expr.Var(_, _, _) => Nil
-            case original@Expr.Match(_, rules, _, _, _) =>
-                rules.map(r => mutateMatchrule(r)).map(m => original.copy(rules = m))
+    /**
+        // var doesn't contain a subtree, and we don't mutate them so we don't need to return them
+        case Expr.Var(_, _, _) => Nil
+      */
+    private def mutateExpr(e: TypedAst.Expr): List[TypedAst.Expr] = e match {
+        case Expr.Cst(cst, tpe, loc) =>
+            mutateCst(cst).map(m => Expr.Cst(m, tpe, loc))
+        case Expr.Var(_, _, _) => Nil
+        case Expr.Def(sym, tpe, loc) => Nil
+        case Expr.Sig(sym, tpe, loc) => Nil
+        case Expr.Hole(sym, tpe, loc) => Nil
+        case Expr.HoleWithExp(exp, tpe, eff, loc) => Nil
+        case Expr.OpenAs(symUse, exp, tpe, loc) => Nil
+        case Expr.Use(sym, alias, exp, loc) => Nil
+        case original@Expr.Lambda(fparam, exp, tpe, loc) =>
+            mutateExpr(exp).map(m => original.copy(exp = m))
 
-            case e => List(e)
-        }
+        case original@Expr.Apply(exp, exps, tpe, eff, loc) =>
+            def helper(expList: List[Expr], index: Int) : List[Expr]= (expList, index) match {
+                case (x::Nil, 0) => mutateExpr(x) ::: Nil
+                case (x::xs, 0) => mutateExpr(x) ::: xs
+                case (x::xs, n) =>  x::helper(xs, n-1)
+                case (_ ,_) => Nil
+            }
+            var list: List[List[Expr]] = Nil
+            for (i <- 0 until exps.length)
+                list = helper(exps, i) :: list
+            list.map(m => original.copy(exps = m))
 
+        case original@Expr.Unary(sop, exp, tpe, eff, loc) =>
+            val mut1 = Expr.Unary(sop, original, tpe, eff, loc)
+            mut1 :: mutateExpr(exp).map(m => original.copy(exp = m))
 
+        case original@Expr.Binary(sop, exp1, exp2, _, _, _) =>
+            val mut1 = mutateSop(sop).map(m => original.copy(sop = m))
+            val mut2 = mutateExpr(exp1).map(m => original.copy(exp1 = m))
+            val mut3 = mutateExpr(exp2).map(m => original.copy(exp2 = m))
+            mut1:::mut2:::mut3
+
+        case original@Expr.Let(sym, mod, exp1, exp2, tpe, eff, loc) =>
+            mutateExpr(exp2).map(m => original.copy(exp2 = m))
+        case Expr.LetRec(sym, ann, mod, exp1, exp2, tpe, eff, loc) => Nil
+        case Expr.Region(tpe, loc) => Nil
+        case Expr.Scope(sym, regionVar, exp, tpe, eff, loc) => Nil
+        case original@Expr.IfThenElse(exp1, exp2, exp3, _, _, _) =>
+            val mut1 = mutateExpr(exp1).map(m => original.copy(exp1 = m))
+            val mut2 = mutateExpr(exp2).map(m => original.copy(exp2 = m))
+            val mut3 = mutateExpr(exp3).map(m => original.copy(exp3 = m))
+            mut1:::mut2:::mut3
+        case Expr.Stm(exp1, exp2, tpe, eff, loc) => Nil
+        case Expr.Discard(exp, eff, loc) => Nil
+        case original@Expr.Match(_, rules, _, _, _) =>
+            // refactor to permutation
+            val permutations = rules.permutations
+            val shuffle = permutations.map(p => original.copy(rules = p)).toList
+            val ruleMute = rules.map(r => mutateMatchrule(r)).map(m => original.copy(rules = m))
+            shuffle:::ruleMute
+        case Expr.TypeMatch(exp, rules, tpe, eff, loc) => Nil
+        case Expr.RestrictableChoose(star, exp, rules, tpe, eff, loc) => Nil
+        case Expr.Tag(sym, exp, tpe, eff, loc) => Nil
+        case Expr.RestrictableTag(sym, exp, tpe, eff, loc) => Nil
+        case original@Expr.Tuple(elms, _, _, _) =>
+            val mutateElms = elms.map(e => mutateExpr(e))
+            mutateElms.map(m => original.copy(elms = m))
+        case Expr.RecordEmpty(tpe, loc) => Nil
+        case Expr.RecordSelect(exp, label, tpe, eff, loc) => Nil
+        case Expr.RecordExtend(label, exp1, exp2, tpe, eff, loc) => Nil
+        case Expr.RecordRestrict(label, exp, tpe, eff, loc) => Nil
+        case Expr.ArrayLit(exps, exp, tpe, eff, loc) => Nil
+        case Expr.ArrayNew(exp1, exp2, exp3, tpe, eff, loc) => Nil
+        case Expr.ArrayLoad(exp1, exp2, _, _, _) => Nil
+
+        case Expr.ArrayLength(exp, eff, loc) => Nil
+        case Expr.ArrayStore(exp1, exp2, exp3, eff, loc) => Nil
+        case Expr.VectorLit(exps, tpe, eff, loc) => Nil
+        case Expr.VectorLoad(exp1, exp2, tpe, eff, loc) => Nil
+        case Expr.VectorLength(exp, loc) => Nil
+        case Expr.Ref(exp1, exp2, tpe, eff, loc) => Nil
+        case Expr.Deref(exp, tpe, eff, loc) => Nil
+        case Expr.Assign(exp1, exp2, tpe, eff, loc) => Nil
+        case Expr.Ascribe(exp, tpe, eff, loc) => Nil
+        case Expr.InstanceOf(exp, clazz, loc) => Nil
+        case Expr.CheckedCast(cast, exp, tpe, eff, loc) => Nil
+        case Expr.UncheckedCast(exp, declaredType, declaredEff, tpe, eff, loc) => Nil
+        case Expr.UncheckedMaskingCast(exp, tpe, eff, loc) => Nil
+        case Expr.Without(exp, effUse, tpe, eff, loc) => Nil
+        case Expr.TryCatch(exp, rules, tpe, eff, loc) => Nil
+        case Expr.TryWith(exp, effUse, rules, tpe, eff, loc) => Nil
+        case Expr.Do(op, exps, tpe, eff, loc) => Nil
+        case Expr.InvokeConstructor(constructor, exps, tpe, eff, loc) => Nil
+        case Expr.InvokeMethod(method, exp, exps, tpe, eff, loc) => Nil
+        case Expr.InvokeStaticMethod(method, exps, tpe, eff, loc) => Nil
+        case Expr.GetField(field, exp, tpe, eff, loc) => Nil
+        case Expr.PutField(field, exp1, exp2, tpe, eff, loc) => Nil
+        case Expr.GetStaticField(field, tpe, eff, loc) => Nil
+        case Expr.PutStaticField(field, exp, tpe, eff, loc) => Nil
+        case Expr.NewObject(name, clazz, tpe, eff, methods, loc) => Nil
+        case Expr.NewChannel(exp1, exp2, tpe, eff, loc) => Nil
+        case Expr.GetChannel(exp, tpe, eff, loc) => Nil
+        case Expr.PutChannel(exp1, exp2, tpe, eff, loc) => Nil
+        case Expr.SelectChannel(rules, default, tpe, eff, loc) => Nil
+        case Expr.Spawn(exp1, exp2, tpe, eff, loc) => Nil
+        case Expr.ParYield(frags, exp, tpe, eff, loc) => Nil
+        case Expr.Lazy(exp, tpe, loc) => Nil
+        case Expr.Force(exp, tpe, eff, loc) => Nil
+        case Expr.FixpointConstraintSet(cs, tpe, loc) => Nil
+        case Expr.FixpointLambda(pparams, exp, tpe, eff, loc) => Nil
+        case Expr.FixpointMerge(exp1, exp2, tpe, eff, loc) => Nil
+        case Expr.FixpointSolve(exp, tpe, eff, loc) => Nil
+        case Expr.FixpointFilter(pred, exp, tpe, eff, loc) => Nil
+        case Expr.FixpointInject(exp, pred, tpe, eff, loc) => Nil
+        case Expr.FixpointProject(pred, exp, tpe, eff, loc) => Nil
+        case Expr.Error(m, tpe, eff) => Nil
+    }
+
+//    private def mutateAndPrepend[A](original: A, mutatee: A, func: (A => List[A]), copyFunc: (A => A), list: List[A]): List[A] = {
+//        func(mutatee).foldLeft(list)((acc, m) => copyFunc(m)::acc)
+ //   }
     private def mutateMatchrule(mr: TypedAst.MatchRule): List[TypedAst.MatchRule] = {
-        val mutExps = mutateExpr(mr.exp).map(m => mr.copy(exp = m))
-        mutatePattern(mr.pat).foldLeft(mutExps)((acc, m) => mr.copy(pat = m)::acc)
+        mutateExpr(mr.exp).map(m => mr.copy(exp = m))
 
     }
     private def mutatePattern(pattern: TypedAst.Pattern): List[TypedAst.Pattern] = {
         pattern match {
             case original@TypedAst.Pattern.Cst(cst, _, _) => mutateCst(cst).map(m => original.copy(m))
-            case e => List(e)
+            case e => Nil
         }
     }
-    private def mutateSop(sop: SemanticOp): SemanticOp = {
-        sop match {
+    private def mutateSop(sop: SemanticOp): List[SemanticOp] = {
+        def helper(sop: SemanticOp): List[SemanticOp] = sop match {
             case op: SemanticOp.BoolOp => op match {
-                case BoolOp.Not => BoolOp.Not
-                case BoolOp.And => BoolOp.Or
-                case BoolOp.Or => BoolOp.And
-                case BoolOp.Eq => BoolOp.Neq
-                case BoolOp.Neq => BoolOp.Eq
+                case BoolOp.Not => BoolOp.Not :: Nil
+                case _ => BoolOp.Or :: BoolOp.And :: BoolOp.Neq :: BoolOp.Eq :: Nil
             }
             case op: SemanticOp.CharOp => op match {
-                case CharOp.Eq => CharOp.Neq
-                case CharOp.Neq => CharOp.Eq
-                case CharOp.Lt => CharOp.Le
-                case CharOp.Le => CharOp.Lt
-                case CharOp.Gt => CharOp.Ge
-                case CharOp.Ge => CharOp.Gt
+                case _ => CharOp.Neq :: CharOp.Eq :: CharOp.Neq :: CharOp.Ge :: CharOp.Le :: CharOp.Lt :: CharOp.Gt :: Nil
             }
-            case op: SemanticOp.Float32Op => op match {
-                case Float32Op.Neg => Float32Op.Neg
-                case Float32Op.Add => Float32Op.Sub
-                case Float32Op.Sub => Float32Op.Add
-                case Float32Op.Mul => Float32Op.Div
-                case Float32Op.Div => Float32Op.Mul
-                case Float32Op.Exp => Float32Op.Mul
-                case Float32Op.Eq => Float32Op.Neq
-                case Float32Op.Neq => Float32Op.Eq
-                case Float32Op.Lt => Float32Op.Le
-                case Float32Op.Le => Float32Op.Lt
-                case Float32Op.Gt => Float32Op.Ge
-                case Float32Op.Ge => Float32Op.Gt
+            case op: SemanticOp.Float32Op => op match{
+                case Float32Op.Neg => Float32Op.Neg :: Nil
+                case Float32Op.Add | Float32Op.Sub | Float32Op.Mul |
+                  Float32Op.Sub | Float32Op.Div | Float32Op.Exp =>
+                    Float32Op.Add :: Float32Op.Sub :: Float32Op.Mul :: Float32Op.Sub :: Float32Op.Div :: Float32Op.Exp :: Nil
+                case _ => Float32Op.Eq :: Float32Op.Neq :: Float32Op.Lt :: Float32Op.Le :: Float32Op.Gt :: Float32Op.Ge :: Nil
             }
             case op: SemanticOp.Float64Op => op match {
-                case Float64Op.Neg => Float64Op.Neg
-                case Float64Op.Add => Float64Op.Sub
-                case Float64Op.Sub => Float64Op.Add
-                case Float64Op.Mul => Float64Op.Div
-                case Float64Op.Div => Float64Op.Mul
-                case Float64Op.Exp =>Float64Op.Mul
-                case Float64Op.Eq => Float64Op.Neq
-                case Float64Op.Neq =>Float64Op.Eq
-                case Float64Op.Lt => Float64Op.Le
-                case Float64Op.Le => Float64Op.Lt
-                case Float64Op.Gt => Float64Op.Ge
-                case Float64Op.Ge => Float64Op.Gt
+                case Float64Op.Neg | Float64Op.Add | Float64Op.Sub | Float64Op.Mul | Float64Op.Div | Float64Op.Exp =>
+                    Float64Op.Neq :: Float64Op.Add :: Float64Op.Sub :: Float64Op.Mul :: Float64Op.Div :: Float64Op.Exp :: Nil
+                case _ => Float64Op.Eq :: Float64Op.Neq :: Float64Op.Lt :: Float64Op.Le :: Float64Op.Gt :: Float64Op.Ge :: Nil
             }
             case op: SemanticOp.Int8Op => op match {
                 case Int8Op.Neg =>Int8Op.Neg
@@ -230,46 +309,20 @@ object MutationTester {
                 case Int8Op.Ge => Int8Op.Gt
             }
             case op: SemanticOp.Int16Op => op match {
-                case Int16Op.Neg =>Int16Op.Neg
-                case Int16Op.Not =>Int16Op.Not
-                case Int16Op.Add =>Int16Op.Sub
-                case Int16Op.Sub =>Int16Op.Add
-                case Int16Op.Mul =>Int16Op.Div
-                case Int16Op.Div =>Int16Op.Mul
-                case Int16Op.Rem =>Int16Op.Div
-                case Int16Op.Exp =>Int16Op.Mul
-                case Int16Op.And =>Int16Op.Or
-                case Int16Op.Or => Int16Op.And
-                case Int16Op.Xor =>Int16Op.Or
-                case Int16Op.Shl =>Int16Op.Shr
-                case Int16Op.Shr =>Int16Op.Shl
-                case Int16Op.Eq => Int16Op.Neq
-                case Int16Op.Neq =>Int16Op.Eq
-                case Int16Op.Lt => Int16Op.Le
-                case Int16Op.Le => Int16Op.Lt
-                case Int16Op.Gt => Int16Op.Ge
-                case Int16Op.Ge => Int16Op.Gt
+                case Int16Op.Neg => Int16Op.Neg :: Nil
+                case Int16Op.Not => Int16Op.Not :: Nil
+                case Int16Op.Add | Int16Op.Sub | Int16Op.Mul | Int16Op.Div | Int16Op.Rem | Int16Op.Exp | Int16Op.Shr | Int16Op.Shl =>
+                    Int16Op.Sub :: Int16Op.Sub :: Int16Op.Mul :: Int16Op.Div :: Int16Op.Rem :: Int16Op.Exp :: Int16Op.Shr :: Int16Op.Shl :: Nil
+                case Int16Op.And | Int16Op.Or | Int16Op.Xor => Int16Op.And :: Int16Op.Or :: Int16Op.Xor :: Nil
+                case _ => Int16Op.Eq :: Int16Op.Neq :: Int16Op.Lt :: Int16Op.Le :: Int16Op.Gt :: Int16Op.Ge :: Nil
             }
             case op: SemanticOp.Int32Op => op match {
-                case Int32Op.Neg =>Int32Op.Neg
-                case Int32Op.Not =>Int32Op.Not
-                case Int32Op.Add =>Int32Op.Sub
-                case Int32Op.Sub =>Int32Op.Add
-                case Int32Op.Mul =>Int32Op.Div
-                case Int32Op.Div =>Int32Op.Mul
-                case Int32Op.Rem =>Int32Op.Div
-                case Int32Op.Exp =>Int32Op.Mul
-                case Int32Op.And =>Int32Op.Or
-                case Int32Op.Or => Int32Op.And
-                case Int32Op.Xor =>Int32Op.Or
-                case Int32Op.Shl =>Int32Op.Shr
-                case Int32Op.Shr =>Int32Op.Shl
-                case Int32Op.Eq => Int32Op.Neq
-                case Int32Op.Neq =>Int32Op.Eq
-                case Int32Op.Lt => Int32Op.Le
-                case Int32Op.Le => Int32Op.Lt
-                case Int32Op.Gt => Int32Op.Ge
-                case Int32Op.Ge => Int32Op.Gt
+                case Int32Op.Neg => Int32Op.Neg :: Nil
+                case Int32Op.Not => Int32Op.Not :: Nil
+                case Int32Op.Add | Int32Op.Sub | Int32Op.Mul | Int32Op.Div | Int32Op.Rem | Int32Op.Exp | Int32Op.Shr | Int32Op.Shl =>
+                    Int32Op.Add :: Int32Op.Sub :: Int32Op.Mul :: Int32Op.Div :: Int32Op.Rem :: Int32Op.Exp :: Int32Op.Shr :: Int32Op.Shl :: Nil
+                case Int32Op.And | Int32Op.Or | Int32Op.Xor => Int32Op.And :: Int32Op.Or :: Int32Op.Xor :: Nil
+                case _ => Int32Op.Eq :: Int32Op.Neq :: Int32Op.Lt :: Int32Op.Le :: Int32Op.Gt :: Int32Op.Ge :: Nil
             }
             case op: SemanticOp.Int64Op => op match {
                 case Int64Op.Neg =>Int64Op.Neg
@@ -296,6 +349,7 @@ object MutationTester {
                 case StringOp.Concat => StringOp.Concat
             }
         }
+        helper(sop).filter(e => e != sop)
     }
     private def mutateCst(cst: Ast.Constant): List[Ast.Constant] = {
         cst match {
