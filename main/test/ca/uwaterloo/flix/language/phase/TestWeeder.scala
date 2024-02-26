aa2e670f4320e10ed314b4514492ad900d6fc75b
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2024-02-26 11:18:51 +0100

    Reorder tests

--- main/test/ca/uwaterloo/flix/language/phase/TestWeeder.scala
+++ main/test/ca/uwaterloo/flix/language/phase/TestWeeder.scala
@@ -32,6 +32,19 @@ class TestWeeder extends AnyFunSuite with TestUtils {
     expectError[WeederError.DuplicateAnnotation](result)
   }
 
+  test("DuplicateAnnotation.02") {
+    val input =
+      """
+        |def f(): Int32 = {
+        | @Tailrec @Tailrec
+        | def g(i) = if (i <= 0) 0 else g(i - 1);
+        | g(10)
+        |}
+        |""".stripMargin
+    val result = compile(input, Options.TestWithLibNix)
+    expectError[WeederError.DuplicateAnnotation](result)
+  }
+
   test("DuplicateFormalParam.01") {
     val input = "def f(x: Int32, x: Int32): Int32 = 42"
     val result = compile(input, Options.TestWithLibNix)
@@ -60,19 +73,6 @@ class TestWeeder extends AnyFunSuite with TestUtils {
     val input = "def f(): (Int32, Int32, Int32) -> Int32 = (x, y, x) -> x"
     val result = compile(input, Options.TestWithLibNix)
     expectError[WeederError.DuplicateFormalParam](result)
-  }
-
-  test("DuplicateInnerFunctionAnnotation.01") {
-    val input =
-      """
-        |def f(): Int32 = {
-        | @Tailrec @Tailrec
-        | def g(i) = if (i <= 0) 0 else g(i - 1);
-        | g(10)
-        |}
-        |""".stripMargin
-    val result = compile(input, Options.TestWithLibNix)
-    expectError[WeederError.DuplicateAnnotation](result)
   }
 
   test("DuplicateTag.01") {
