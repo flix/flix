1843f7d5f5511176ff17f44f687ccaaeceaa4826
JonathanStarup <32037926+JonathanStarup@users.noreply.github.com>
2024-02-26 17:46:28 +0100

    work

--- main/src/ca/uwaterloo/flix/language/phase/Tailrec.scala
+++ main/src/ca/uwaterloo/flix/language/phase/Tailrec.scala
@@ -24,8 +24,11 @@ import ca.uwaterloo.flix.util.ParOps
 /**
   * The Tailrec phase identifies function calls that are in tail recursive position.
   *
-  * Specifically, it replaces `ApplyRef` AST nodes with `ApplyTail` AST nodes
-  * when the `ApplyRef` node calls the same function and occurs in tail position.
+  * Specifically, it replaces [[Expr.ApplyDef]] AST nodes with [[Expr.ApplySelfTail]] AST nodes
+  * when the [[Expr.ApplyDef]] node calls the enclosing function and occurs in tail position.
+  *
+  * For correctness it is assumed that all calls in the given AST have [[Ast.CallType.NonTailCall]]
+  * and there is no [[Expr.ApplySelfTail]] nodes present.
   */
 object Tailrec {
 
@@ -45,44 +48,38 @@ object Tailrec {
     /**
       * Introduces tail recursive calls in the given expression `exp0`.
       *
-      * Replaces every `ApplyRef`, which calls the same function and occurs in tail position, with `ApplyTail`.
+      * Replaces every [[Expr.ApplyDef]], which calls the enclosing function and occurs in tail position, with [[Expr.ApplySelfTail]].
       */
     def visitExp(exp0: Expr): Expr = exp0 match {
-      /*
-       * Let: The body expression is in tail position.
-       * (The value expression is *not* in tail position).
-       */
       case Expr.Let(sym, exp1, exp2, tpe, purity, loc) =>
+        // The body expression is in tail position.
+        // (The value expression is *not* in tail position).
         val e2 = visitExp(exp2)
         Expr.Let(sym, exp1, e2, tpe, purity, loc)
 
-      /*
-       * If-Then-Else: Consequent and alternative are both in tail position.
-       * (The condition is *not* in tail position).
-       */
+      case Expr.LetRec(varSym, index, defSym, exp1, exp2, tpe, purity, loc) =>
+        // The body expression is in tail position.
+        // (The value expression is *not* in tail position).
+        val e2 = visitExp(exp2)
+        Expr.LetRec(varSym, index, defSym, exp1, e2, tpe, purity, loc)
+
       case Expr.IfThenElse(exp1, exp2, exp3, tpe, purity, loc) =>
+        // Consequent and alternative are both in tail position.
+        // (The condition is *not* in tail position).
         val e2 = visitExp(exp2)
         val e3 = visitExp(exp3)
         Expr.IfThenElse(exp1, e2, e3, tpe, purity, loc)
 
-      /*
-       * Branch: Each branch is in tail position.
-       */
       case Expr.Branch(e0, br0, tpe, purity, loc) =>
+        // Each branch is in tail position.
         val br = br0 map {
           case (sym, exp) => sym -> visitExp(exp)
         }
         Expr.Branch(e0, br, tpe, purity, loc)
 
-      /*
-       * ApplyClo.
-       */
       case Expr.ApplyClo(exp, exps, _, tpe, purity, loc) =>
         Expr.ApplyClo(exp, exps, Ast.CallType.TailCall, tpe, purity, loc)
 
-      /*
-       * ApplyDef.
-       */
       case Expr.ApplyDef(sym, exps, _, tpe, purity, loc) =>
         // Check whether this is a self recursive call.
         if (defn.sym != sym) {
@@ -93,10 +90,18 @@ object Tailrec {
           Expr.ApplySelfTail(sym, exps, tpe, purity, loc)
         }
 
-      /*
-       * Other expression: No calls in tail position.
-       */
-      case _ => exp0
+      // Non-tail expressions
+      case Expr.ApplyAtomic(_, _, _, _, _) => exp0
+      case Expr.ApplySelfTail(_, _, _, _, _) => exp0
+      case Expr.Cst(_, _, _) => exp0
+      case Expr.Do(_, _, _, _, _) => exp0
+      case Expr.JumpTo(_, _, _, _) => exp0
+      case Expr.NewObject(_, _, _, _, _, _) => exp0
+      case Expr.Scope(_, _, _, _, _) => exp0
+      case Expr.Stmt(_, _, _, _, _) => exp0
+      case Expr.TryCatch(_, _, _, _, _) => exp0
+      case Expr.TryWith(_, _, _, _, _, _) => exp0
+      case Expr.Var(_, _, _) => exp0
     }
 
     defn.copy(exp = visitExp(defn.exp))
