58f3cc7cc9b18c308e74942994396e02d74d8c8a
Magnus Madsen <magnusm@cs.au.dk>
2024-02-28 15:10:53 +0100

    feat: add FastBoolUnification

--- main/src/ca/uwaterloo/flix/language/phase/unification/FastBoolUnification.scala
+++ main/src/ca/uwaterloo/flix/language/phase/unification/FastBoolUnification.scala
@@ -52,7 +52,7 @@ object FastBoolUnification {
   /**
     * Internal formatter. Used for debugging.
     */
-  private val formatter: Formatter = Formatter.AnsiTerminalFormatter
+  private val formatter: Formatter = Formatter.NoFormatter
 
   /**
     * Attempts to solve all the given unification equations `l`.
@@ -252,13 +252,19 @@ object FastBoolUnification {
 
     // TODO: Could start from empty subst and then use ++ later.
 
+    var currentEqns = l
     var currentSubst = subst0
     var rest = ListBuffer.empty[Equation]
 
-    for (eqn <- l) {
+    while(currentEqns.nonEmpty) {
+      val eqn = currentEqns.head
+      currentEqns = currentEqns.tail
       eqn match {
         case Equation(Term.Var(x), Term.Var(y)) =>
-          currentSubst = currentSubst.extended(x, Term.Var(y))
+          val singleton = BoolSubstitution.singleton(x, Term.Var(y))
+
+          currentEqns = singleton(currentEqns)
+          currentSubst = singleton @@ currentSubst
         case _ => rest += eqn
       }
     }
@@ -755,7 +761,10 @@ object FastBoolUnification {
       case None => BoolSubstitution(m + (x -> t))
       case Some(t1) =>
         // Note: If t == t1 we can just return the same substitution.
-        if (t == t1) this else throw ConflictException(t, t1)
+        if (t == t1)
+          this
+        else
+          throw ConflictException(t, t1)
     }
 
     /**
@@ -1170,8 +1179,15 @@ object FastBoolUnification {
     Var(55261) ~ Var(112585)
   )
 
+  private def Boxable_lift1(): List[Equation] = List(
+    Var(56474) ~ True,
+    Var(56476) ~ (Var(113308) & Var(56474)),
+    Var(56478) ~ Var(56476)
+  )
+
+
   def main(args: Array[String]): Unit = {
-    //solveAll(FixpointInterpreter_evalTerm()).get
+    solveAll(FixpointInterpreter_evalTerm()).get
     //solveAll(Array_copyOfRange()).get
     //solveAll(FixpointAstDatalog_toString299997()).get
     //solveAll(Nec_zipWithA()).get
@@ -1181,7 +1197,8 @@ object FastBoolUnification {
     //solveAll(FixpointAstDatalog_predSymsOf29898()).get
     //solveAll(Iterator_toArray()).get
     //solveAll(Files_append()).get
-    solveAll(Iterator_next()).get
+    //solveAll(Iterator_next()).get
+    //solveAll(Boxable_lift1()).get
   }
 
 }
