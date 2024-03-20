6fdffc9fa3ce508ea442b9e657790edf581d6291
JonathanStarup <32037926+JonathanStarup@users.noreply.github.com>
2024-03-20 12:35:16 +0100

    owrk

--- main/src/ca/uwaterloo/flix/language/phase/Verifier.scala
+++ main/src/ca/uwaterloo/flix/language/phase/Verifier.scala
@@ -27,7 +27,7 @@ import ca.uwaterloo.flix.util.InternalCompilerException
 object Verifier {
 
   def run(root: Root)(implicit flix: Flix): Root = flix.phase("Verifier") {
-    if (true) {
+    if (flix.options.xverify) {
       root.defs.values.foreach(d => visitDef(d)(root))
     }
     root
