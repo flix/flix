896562ed90df6845569e0e57d9b842f5afc987f9
Stephen Tetley <stephen.tetley@gmail.com>
2024-04-01 15:47:46 +0100

    chore: rename class to trait in Desugar and others (issue #6591) (#7527)

--- main/src/ca/uwaterloo/flix/language/phase/extra/CodeHinter.scala
+++ main/src/ca/uwaterloo/flix/language/phase/extra/CodeHinter.scala
@@ -29,10 +29,10 @@ object CodeHinter {
     * Returns a collection of code quality hints for the given AST `root`.
     */
   def run(root: TypedAst.Root, sources: Set[String])(implicit flix: Flix, index: Index): List[CodeHint] = {
-    val classHints = root.classes.values.flatMap(visitClass(_)(root, index)).toList
+    val traitHints = root.classes.values.flatMap(visitTrait(_)(root, index)).toList
     val defsHints = root.defs.values.flatMap(visitDef(_)(root, flix)).toList
     val enumsHints = root.enums.values.flatMap(visitEnum(_)(root, index)).toList
-    (classHints ++ defsHints ++ enumsHints).filter(include(_, sources))
+    (traitHints ++ defsHints ++ enumsHints).filter(include(_, sources))
   }
 
   /**
@@ -56,13 +56,13 @@ object CodeHinter {
   }
 
   /**
-    * Computes code quality hints for the given class `typeclass`.
+    * Computes code quality hints for the given trait `trt`.
     */
-  private def visitClass(typeclass: TypedAst.Class)(implicit root: Root, index: Index): List[CodeHint] = {
-    val uses = index.usesOf(typeclass.sym)
-    val isDeprecated = typeclass.ann.isDeprecated
+  private def visitTrait(trt: TypedAst.Class)(implicit root: Root, index: Index): List[CodeHint] = {
+    val uses = index.usesOf(trt.sym)
+    val isDeprecated = trt.ann.isDeprecated
     val deprecated = if (isDeprecated) uses.map(CodeHint.Deprecated) else Nil
-    val isExperimental = typeclass.ann.isExperimental
+    val isExperimental = trt.ann.isExperimental
     val experimental = if (isExperimental) uses.map(CodeHint.Experimental) else Nil
     (deprecated ++ experimental).toList
   }
