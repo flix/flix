527004d8d4f42c1bdb1e9cb66837a541917bfdf9
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2024-04-08 14:45:37 +0200

    Add test

--- main/test/ca/uwaterloo/flix/util/TestFormatter.scala
+++ main/test/ca/uwaterloo/flix/util/TestFormatter.scala
@@ -6,6 +6,27 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class TestFormatter extends AnyFunSuite {
 
+  test("ExplicitLineCall.01") {
+    def message(): String = {
+      import Formatter._
+      val message: MessageKind = MessageKind("Test Error")
+      val source: Ast.Source = SourceLocation.Unknown.source
+      val formatter: Formatter = NoFormatter
+      val s: String =
+        s""">> Bad thing happened
+           |
+           |""".stripMargin
+      formatter.addErrorLine(message, source, s)
+    }
+
+    val expected =
+      s"""-- Test Error -------------------------------------------------- <unknown>
+         |>> Bad thing happened
+         |
+         |""".stripMargin
+
+    assertResult(expected)(message())
+  }
 
   test("ImplicitLineCall.01") {
     def message(): String = {
@@ -13,9 +34,11 @@ class TestFormatter extends AnyFunSuite {
       implicit val message: MessageKind = MessageKind("Test Error")
       implicit val source: Ast.Source = SourceLocation.Unknown.source
       val formatter: Formatter = NoFormatter
-      s""">> Bad thing happened
-         |
-         |""".stripMargin
+      implicit val s: String =
+        s""">> Bad thing happened
+           |
+           |""".stripMargin
+      s
     }
 
     val expected =
