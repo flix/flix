c5aad050656215c261f898bde2e2b5b4977cf37b
Samuel S <kongsamuel@hotmail.dk>
2024-04-15 11:51:00 +0200

    deleted print statement

--- main/src/ca/uwaterloo/flix/MutationTester.scala
+++ main/src/ca/uwaterloo/flix/MutationTester.scala
@@ -175,7 +175,6 @@ object MutationTester {
     private def compileAndTestMutant(mDef: TypedAst.Def, mut: (Symbol.DefnSym, List[TypedAst.Def]), testKit: TestKit): TestRes = {
         val defs = testKit.root.defs
         val n = defs + (mut._1 -> mDef)
-        println(mDef)
         val newRoot = testKit.root.copy(defs = n)
         val cRes = testKit.flix.codeGen(newRoot).unsafeGet
         val testsFromTester = cRes.getTests.filter { case (s, _) => s.namespace.head.equals(testKit.testModule) }.toList
