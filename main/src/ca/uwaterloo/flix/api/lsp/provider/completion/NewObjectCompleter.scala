16338eb5e4fa962338a44689fc0237780e68015b
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2024-04-21 16:35:11 +0200

    WIP add Completer

--- main/src/ca/uwaterloo/flix/api/lsp/provider/completion/NewObjectCompleter.scala
+++ main/src/ca/uwaterloo/flix/api/lsp/provider/completion/NewObjectCompleter.scala
@@ -2,8 +2,8 @@ package ca.uwaterloo.flix.api.lsp.provider.completion
 
 import ca.uwaterloo.flix.api.Flix
 import ca.uwaterloo.flix.api.lsp.Index
-import ca.uwaterloo.flix.api.lsp.provider.completion.Completion.NewObjectCompletion
-import ca.uwaterloo.flix.language.ast.TypedAst
+import ca.uwaterloo.flix.api.lsp.provider.completion.Completion.{MatchCompletion, NewObjectCompletion}
+import ca.uwaterloo.flix.language.ast.{Ast, TypedAst}
 
 object NewObjectCompleter extends Completer {
 
@@ -11,27 +11,30 @@ object NewObjectCompleter extends Completer {
     * Returns a List of Completion for completer.
     */
   override def getCompletions(context: CompletionContext)(implicit flix: Flix, index: Index, root: TypedAst.Root, delta: DeltaContext): Iterable[Completion] = {
-    val regex = raw"\s*new\s+(?:.*\s+)*(.*)".r
-    context.prefix match {
-      case regex(clazz) =>
-        val path = clazz.split('.').toList
-        // Get completions for if we are currently typing the next package/class and if we have just finished typing a package
-        javaClassCompletionsFromPrefix(path)(root) ++ javaClassCompletionsFromPrefix(path.dropRight(1))(root)
-      case _ => Nil
+    val regex = raw"\s*n?e?w?\s+(?:.*\s+)*(.*)".r
+    if (!regex.matches(context.prefix)) {
+      Nil
+    } else {
+      val wordPattern = "n?e?w?".r
+      val currentWordIsNew = wordPattern.matches(context.word)
+
+      root.uses.foldLeft(List.empty[MatchCompletion]) {
+        case (acc, (_, useOrImport)) => newObjectCompletion(useOrImport, currentWordIsNew) ::: acc
+      }
     }
   }
 
-  /**
-    * Gets completions from a java path prefix
-    */
-  private def javaClassCompletionsFromPrefix(prefix: List[String])(implicit root: TypedAst.Root): Iterable[NewObjectCompletion] = {
-    root.names(prefix).map(clazz => {
-      val label = prefix match {
-        case Nil => clazz
-        case v => v.mkString("", ".", s".$clazz")
+  private def newObjectCompletion(useOrImports: List[Ast.UseOrImport], currentWordIsNew: Boolean): List[NewObjectCompletion] = {
+    useOrImports.foldLeft(List.empty[NewObjectCompletion]) {
+      case (acc, x) => x match {
+        case Ast.UseOrImport.Use(_, _, _) => acc
+        case Ast.UseOrImport.Import(clazz, alias, loc) => ???
       }
-      Completion.NewObjectCompletion(label)
-    })
+    }
   }
 
+  private def helper(): Unit = ???
+  /*
+
+   */
 }
