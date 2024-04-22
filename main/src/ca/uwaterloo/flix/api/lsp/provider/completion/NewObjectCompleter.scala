79bcbc4952b294a8d6980d64edda81bb1a7a186a
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2024-04-22 11:00:30 +0200

    Add debug code

--- main/src/ca/uwaterloo/flix/api/lsp/provider/completion/NewObjectCompleter.scala
+++ main/src/ca/uwaterloo/flix/api/lsp/provider/completion/NewObjectCompleter.scala
@@ -13,10 +13,11 @@ object NewObjectCompleter extends Completer {
     */
   override def getCompletions(context: CompletionContext)(implicit flix: Flix, index: Index, root: TypedAst.Root, delta: DeltaContext): Iterable[Completion] = {
     println("NewObjectCompleter.getCompletions fired!")
-    println("Checking dummy code")
+    println("Checking debug code")
     val regex = raw"\s*new\s+(?:.*\s+)*(.*)".r
     val check = context.prefix match {
       case regex(clazz) =>
+        println("Entered good path")
         val path = clazz.split('.').toList
         // Get completions for if we are currently typing the next package/class and if we have just finished typing a package
         javaClassCompletionsFromPrefix(path)(root) ++ javaClassCompletionsFromPrefix(path.dropRight(1))(root)
