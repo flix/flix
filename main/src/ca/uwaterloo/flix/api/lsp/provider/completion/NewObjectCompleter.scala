9cb65efb0123cf357e0636be9cb2cf34a34e39d5
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2024-04-28 15:50:30 +0200

    Remove debug prints

--- main/src/ca/uwaterloo/flix/api/lsp/provider/completion/NewObjectCompleter.scala
+++ main/src/ca/uwaterloo/flix/api/lsp/provider/completion/NewObjectCompleter.scala
@@ -18,7 +18,7 @@ object NewObjectCompleter extends Completer {
         val path = clazz.replaceFirst("##", "").split('.').toList
         // Get completions for if we are currently typing the next package/class and if we have just finished typing a package
         val classNames = javaClassCompletionsFromPrefix(path)(root) ++ javaClassCompletionsFromPrefix(path.dropRight(1))(root)
-        val results = classNames.map { c =>
+        classNames.map { c =>
             try {
               Some(Class.forName(c.replaceAll("\\[L", "")))
             } catch {
@@ -28,8 +28,6 @@ object NewObjectCompleter extends Completer {
           .map(c => c.flatMap(newObjectCompletion))
           .filter(_.isDefined)
           .map(_.get)
-        println(s"results: $results")
-        results
       case _ => Nil
     }
   }
