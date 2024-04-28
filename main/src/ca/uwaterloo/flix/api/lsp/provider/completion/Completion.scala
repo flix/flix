e4c35ddda0889e888682e754be43d0fd3d0f8ccd
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2024-04-28 14:37:00 +0200

    Update CompletionItem

--- main/src/ca/uwaterloo/flix/api/lsp/provider/completion/Completion.scala
+++ main/src/ca/uwaterloo/flix/api/lsp/provider/completion/Completion.scala
@@ -133,7 +133,7 @@ sealed trait Completion {
         textEdit = TextEdit(context.range, completion),
         documentation = None,
         insertTextFormat = InsertTextFormat.Snippet,
-        kind = CompletionItemKind.Snippet)
+        kind = CompletionItemKind.Interface)
     case Completion.SnippetCompletion(name, snippet, documentation) =>
       CompletionItem(label = name,
         sortText = Priority.snippet(name),
