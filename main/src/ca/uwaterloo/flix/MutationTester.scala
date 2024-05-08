f4a137090891b43a6bb56ca632f7031d6e367fd5
au701790 <au701790@uni.au.dk>
2024-05-08 11:34:14 +0200

    added data collection

--- main/src/ca/uwaterloo/flix/MutationTester.scala
+++ main/src/ca/uwaterloo/flix/MutationTester.scala
@@ -20,7 +20,7 @@ import ca.uwaterloo.flix.runtime.TestFn
 import ca.uwaterloo.flix.api.Flix
 import ca.uwaterloo.flix.language.ast.Ast.Constant
 import ca.uwaterloo.flix.language.ast.{Symbol, Type, TypedAst}
-import ca.uwaterloo.flix.language.ast.TypedAst.{Expr, MutationType, Root}
+import ca.uwaterloo.flix.language.ast.TypedAst.{Expr, MutationType, Root, empty}
 import dev.flix.runtime.Global
 
 import java.time.LocalDateTime
@@ -28,6 +28,7 @@ import java.time.format.DateTimeFormatter
 import ca.uwaterloo.flix.language.phase.jvm.BackendObjType
 
 import java.io.{File, FileWriter}
+import scala.collection.immutable.HashMap
 import scala.util.Random
 
 ///
@@ -195,37 +196,122 @@ object MutationTester {
         val amountOfMutants = mutatedDefs.map(m => m._2.length).sum
         val f = DateTimeFormatter.ofPattern("yyyy-MM-dd: HH:mm")
         // (survivorCount, startTime, tempTime for progress updates, date for progress updates, amount of mutants currently tested)
-        val localAcc = (0, 0, 0, totalStartTime.toDouble, temp, 0)
-        val (totalSurvivorCount, totalUnknowns, equivalents, _, _, _) = mutatedDefs.foldLeft(localAcc)((acc, mut) => {
+        val emptyList: List[(MutationType, TestRes)] = Nil
+        val localAcc = (0, 0, 0, totalStartTime.toDouble, temp, 0, emptyList)
+        val (totalSurvivorCount, totalUnknowns, equivalents, _, _, _, mOperatorResults) = mutatedDefs.foldLeft(localAcc)((acc, mut) => {
             val kit = TestKit(flix, root, testModule)
             println(s"testing ${mut._2.length} mutations in: ${mut._1}")
             testMutantsAndUpdateProgress(acc, mut, kit, f)
         })
-        val totalEndTime = System.nanoTime() - totalStartTime
-        println(s"mutation score: ${(amountOfMutants - totalSurvivorCount - totalUnknowns -equivalents).toFloat/(amountOfMutants - totalUnknowns - equivalents).toFloat}")
-        println(s"There where $totalSurvivorCount surviving mutations, out of $amountOfMutants mutations")
-        println(s"There where $equivalents equivalent mutations, out of $amountOfMutants mutations")
-        println(s"There where $totalUnknowns mutations that did not finish terminating, out of $amountOfMutants mutations")
-        val nano = 1_000_000_000.0
-        val ifMutants = amountOfMutants > 0
-        val average = if (ifMutants) (totalEndTime / nano) / amountOfMutants else 0
-        println(s"Average time to test a mutant:  $average seconds")
-        val time = if (ifMutants) totalEndTime.toFloat / nano else 0
-        println(s"Total time to test all mutants: $time seconds")
+      reportResults(totalStartTime, amountOfMutants, totalSurvivorCount, totalUnknowns, equivalents)
+      collectData(mOperatorResults)
     }
 
-    private def testMutantsAndUpdateProgress(acc: (Int, Int, Int, Double, Long, Int), mut: (Symbol.DefnSym, List[MutatedDef]), testKit: TestKit, f: DateTimeFormatter) = {
+  private def collectData(operatorResults: List[(MutationType, TestRes)]): Unit = {
+   val sortedData = sortData(operatorResults)
+   writeDataToFile(sortedData)
+  }
+  private def writeDataToFile(stringToPoints: Map[String, DataPoints]): Unit = {
+    val stringToWrite = stringToPoints.foldLeft("") {
+      case (acc, (operator, dPoints)) =>
+        s"$acc\n$operator:${dPoints.total}:${dPoints.killed}:${dPoints.surviving}:${dPoints.unknown}:${dPoints.equivalent}"
+    }
+
+    val fileWriter = new FileWriter(new File(s"MutStats.txt"))
+    fileWriter.write(stringToWrite)
+    fileWriter.close()
+  }
+
+  private case class DataPoints(total: Int, killed: Int, surviving: Int, unknown: Int, equivalent: Int)
+  private def sortData(tuples: List[(MutationType, TestRes)]): Map[String, DataPoints] = {
+    def updateDataPoints(dataPoints: DataPoints, testRes: TestRes): DataPoints = testRes match {
+      case TestRes.MutantKilled => dataPoints.copy(total = dataPoints.total + 1, killed = dataPoints.killed + 1)
+      case TestRes.MutantSurvived => dataPoints.copy(total = dataPoints.total + 1, killed = dataPoints.surviving + 1)
+      case TestRes.Unknown => dataPoints.copy(total = dataPoints.total + 1, killed = dataPoints.unknown + 1)
+      case TestRes.Equivalent => dataPoints.copy(total = dataPoints.total + 1, killed = dataPoints.equivalent + 1)
+    }
+    def helper(opRes: (MutationType, TestRes), hMap: HashMap[String, DataPoints])  = opRes._1 match {
+      case MutationType.CstMut(_) =>
+        val dPoints = hMap.apply("CstMut")
+        val newDPoints = updateDataPoints(dPoints, opRes._2)
+        hMap.updated("CstMut", newDPoints)
+      case MutationType.SigMut(_) =>
+        val dPoints = hMap.apply("SigMut")
+        val newDPoints = updateDataPoints(dPoints, opRes._2)
+        hMap.updated("SigMut", newDPoints)
+      case MutationType.IfMut(_) =>
+        val dPoints = hMap.apply("IfMut")
+        val newDPoints = updateDataPoints(dPoints, opRes._2)
+        hMap.updated("IfMut", newDPoints)
+      case MutationType.CompMut(_) =>
+        val dPoints = hMap.apply("CompMut")
+        val newDPoints = updateDataPoints(dPoints, opRes._2)
+        hMap.updated("CompMut", newDPoints)
+      case MutationType.CaseSwitch(_, _) =>
+        val dPoints = hMap.apply("CaseSwitch")
+        val newDPoints = updateDataPoints(dPoints, opRes._2)
+        hMap.updated("CaseSwitch", newDPoints)
+      case MutationType.VarMut(_, _) =>
+        val dPoints = hMap.apply("VarMut")
+        val newDPoints = updateDataPoints(dPoints, opRes._2)
+        hMap.updated("VarMut", newDPoints)
+      case MutationType.CaseDeletion(_) =>
+        val dPoints = hMap.apply("CaseDeletion")
+        val newDPoints = updateDataPoints(dPoints, opRes._2)
+        hMap.updated("CaseDeletion", newDPoints)
+      case MutationType.RecordSelectMut(_) =>
+        val dPoints = hMap.apply("RecordSelectMut")
+        val newDPoints = updateDataPoints(dPoints, opRes._2)
+        hMap.updated("RecordSelectMut", newDPoints)
+      case MutationType.ListMut() =>
+        val dPoints = hMap.apply("ListMut")
+        val newDPoints = updateDataPoints(dPoints, opRes._2)
+        hMap.updated("ListMut", newDPoints)
+    }
+    val emptyMap: HashMap[String, DataPoints] = HashMap.empty
+    val emptyDP = DataPoints(0,0,0,0,0)
+    val m1 =  emptyMap.updated("CstMut", emptyDP)
+    val m2 =  m1.updated("ListMut", emptyDP)
+    val m3 =  m2.updated("RecordSelectMut", emptyDP)
+    val m4 =  m3.updated("CaseDeletion", emptyDP)
+    val m5 =  m4.updated("VarMut", emptyDP)
+    val m6 =  m5.updated("CaseSwitch", emptyDP)
+    val m7 =  m6.updated("CompMut", emptyDP)
+    val m8 =  m7.updated("IfMut", emptyDP)
+    val m9 =  m8.updated("SigMut", emptyDP)
+
+
+    tuples.foldLeft(m9)((acc, t) => helper(t, acc))
+  }
+
+
+  private def reportResults(totalStartTime: Long, amountOfMutants: Int, totalSurvivorCount: Int, totalUnknowns: Int, equivalents: Int): Unit = {
+
+    val totalEndTime = System.nanoTime() - totalStartTime
+    println(s"mutation score: ${(amountOfMutants - totalSurvivorCount - totalUnknowns -equivalents).toFloat/(amountOfMutants - totalUnknowns - equivalents).toFloat}")
+    println(s"There where $totalSurvivorCount surviving mutations, out of $amountOfMutants mutations")
+    println(s"There where $equivalents equivalent mutations, out of $amountOfMutants mutations")
+    println(s"There where $totalUnknowns mutations that did not finish terminating, out of $amountOfMutants mutations")
+    val nano = 1_000_000_000.0
+    val ifMutants = amountOfMutants > 0
+    val average = if (ifMutants) (totalEndTime / nano) / amountOfMutants else 0
+    println(s"Average time to test a mutant:  $average seconds")
+    val time = if (ifMutants) totalEndTime.toFloat / nano else 0
+    println(s"Total time to test all mutants: $time seconds")
+  }
+
+    private def testMutantsAndUpdateProgress(acc: (Int, Int, Int, Double, Long, Int, List[(MutationType, TestRes)]), mut: (Symbol.DefnSym, List[MutatedDef]), testKit: TestKit, f: DateTimeFormatter) = {
         mut._2.foldLeft(acc)((acc2, mDef) => {
-            val (survivorCount, unknownCount, eQCount, time, accTemp, mAmount) = acc2
+            val (survivorCount, unknownCount, eQCount, time, accTemp, mAmount, mTypeResults) = acc2
             val mutationAmount = mAmount + 1
             val start = System.nanoTime()
-            val testResults = compileAndTestMutant(mDef.df, mut, testKit)
+            val testResult = compileAndTestMutant(mDef.df, mut, testKit)
             val nano = 1_000_000_000
             val newTime = time + (System.nanoTime() - start).toDouble / nano
-            val newSurvivorCount = if (testResults.equals(TestRes.MutantSurvived))  survivorCount + 1 else survivorCount
-            val newUnknownCount = if (testResults.equals(TestRes.Unknown))  unknownCount + 1 else unknownCount
-            val newEQCount = if (testResults.equals(TestRes.Equivalent))  eQCount + 1 else eQCount
-            if (testResults.equals(TestRes.MutantSurvived)) {
+            val newSurvivorCount = if (testResult.equals(TestRes.MutantSurvived))  survivorCount + 1 else survivorCount
+            val newUnknownCount = if (testResult.equals(TestRes.Unknown))  unknownCount + 1 else unknownCount
+            val newEQCount = if (testResult.equals(TestRes.Equivalent))  eQCount + 1 else eQCount
+            if (testResult.equals(TestRes.MutantSurvived)) {
               // println(MutationReporter.reportNonKilledMutation(mDef.exp))
               println(testKit.flix.getFormatter.code(mDef.df.exp.loc, printMutation(mDef.mutType)))
 
@@ -235,7 +321,7 @@ object MutationTester {
           val now = LocalDateTime.now()
             val message = s"[${f.format(now)}] Mutants: $mutationAmount, Killed: ${mutationAmount - survivorCount - unknownCount}, Survived: $survivorCount, Unknown: $unknownCount"
             val newTemp = progressUpdate(message, accTemp)
-            (newSurvivorCount, newUnknownCount, newEQCount, newTime, newTemp, mutationAmount)
+            (newSurvivorCount, newUnknownCount, newEQCount, newTime, newTemp, mutationAmount, (mDef.mutType, testResult) :: mTypeResults)
             //val sym = mDef.sym.toString
             //println(s"mutation in $sym survived")
         })
