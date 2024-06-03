7d79b03787088bf457fb8596b52e9adcb1a00538
herlufb <herlufbaggesen13@gmail.com>
2024-06-03 08:24:48 +0200

    test: illegal empty singleton enum

--- main/test/ca/uwaterloo/flix/language/phase/TestParser.scala
+++ main/test/ca/uwaterloo/flix/language/phase/TestParser.scala
@@ -938,6 +938,15 @@ class TestParserHappy extends AnyFunSuite with TestUtils {
     expectError[WeederError.IllegalEnum](result)
   }
 
+  test("IllegalEnum.03") {
+    val input =
+      """
+        |enum Foo()
+        |""".stripMargin
+    val result = compile(input, Options.TestWithLibNix)
+    expectError[ParseError](result)
+  }
+
   // TODO: unignore with Parser2
   ignore("IllegalModuleName.01") {
     val input =
