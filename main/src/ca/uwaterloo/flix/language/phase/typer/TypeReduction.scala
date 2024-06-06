9823b073b5040d896493a946a66c21d12d80e312
Chanattan Sok <zentsugo@gmail.com>
2024-06-06 17:36:01 +0200

    short fix

--- main/src/ca/uwaterloo/flix/language/phase/typer/TypeReduction.scala
+++ main/src/ca/uwaterloo/flix/language/phase/typer/TypeReduction.scala
@@ -157,7 +157,7 @@ object TypeReduction {
       case 1 =>
         val tpe = Type.Cst(TypeConstructor.JvmMethod(candidateMethods.head), loc)
         JavaResolutionResult.Resolved(tpe)
-      case 2 => JavaResolutionResult.AmbiguousMethod(candidateMethods.toList) // For now, method not found either if there is an ambiguity or no method found
+      case _ => JavaResolutionResult.AmbiguousMethod(candidateMethods.toList) // For now, method not found either if there is an ambiguity or no method found
     }
   }
 
