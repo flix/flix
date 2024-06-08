230e28b84c628592a53877228364930f5e283bf3
Andreas Stenbæk Larsen <astenbaek@cs.au.dk>
2024-06-08 10:58:53 +0200

    added a test file for Permission to ensure calling ofString after toString is the identity

--- main/src/ca/uwaterloo/flix/tools/pkg/Permission.scala
+++ main/src/ca/uwaterloo/flix/tools/pkg/Permission.scala
@@ -4,16 +4,22 @@ sealed trait Permission
 
 object Permission {
 
-  case object JavaInterop extends Permission
+  case object JavaInterop extends Permission {
+    override def toString: String = "java-interop"
+  }
 
-  case object UnsafeCast extends Permission
+  case object UnsafeCast extends Permission {
+    override def toString: String = "unsafe-cast"
+  }
 
-  case object Effect extends Permission
+  case object Effect extends Permission {
+    override def toString: String = "effect"
+  }
 
   def ofString(s: String): Option[Permission] = s match {
     case "java-interop" => Some(JavaInterop)
-    case "unsafe-cast"  => Some(UnsafeCast)
-    case "effect"       => Some(Effect)
-    case _              => None
+    case "unsafe-cast" => Some(UnsafeCast)
+    case "effect" => Some(Effect)
+    case _ => None
   }
 }
