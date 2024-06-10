682af83caeee16dbab8f0fa7ffb32083d6afbdc0
Chanattan <zentsugo@gmail.com>
2024-06-10 15:33:08 +0200

    refactor: switch from `Hash` symbol to `Currency` symbol for `InvokeMethod2` (#7858)

--- main/src/ca/uwaterloo/flix/language/phase/Lexer.scala
+++ main/src/ca/uwaterloo/flix/language/phase/Lexer.scala
@@ -329,6 +329,7 @@ object Lexer {
       case _ if isMatch("#{") => TokenKind.HashCurlyL
       case _ if isMatch("#(") => TokenKind.HashParenL
       case '#' => TokenKind.Hash
+      case '¤' => TokenKind.Currency
       case _ if isMatch("//") => acceptLineOrDocComment()
       case _ if isMatch("/*") => acceptBlockComment()
       case '/' => TokenKind.Slash
