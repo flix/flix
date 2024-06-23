896ba6e983c98772c97dc421fa3d029ce5f2ca82
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2024-06-23 15:36:47 +0200

    remove debug

--- main/src/ca/uwaterloo/flix/api/lsp/provider/completion/LabelCompleter.scala
+++ main/src/ca/uwaterloo/flix/api/lsp/provider/completion/LabelCompleter.scala
@@ -27,39 +27,21 @@ object LabelCompleter extends Completer {
     */
   override def getCompletions(context: CompletionContext)(implicit flix: Flix, index: Index, root: TypedAst.Root): Iterable[LabelCompletion] = {
     // Do not get label completions if we are importing or using.
-    println("Entered LabelCompleter")
     if (context.prefix.contains("import") || context.prefix.contains("use")) {
-      println("BAD: In import or use context")
       return Nil
     }
 
-    println("Not in import or use context")
-
     val regex = raw"(.*)#.*".r
 
-    println(s"context.word is             : ${if (context.word.isBlank) "<the empty string>" else context.word}")
-    println(s"context.prefix is           : ${if (context.prefix.isBlank) "<the empty string>" else context.prefix}")
-    println(s"regex matches context.word  : ${regex.matches(context.word)}")
-    println(s"regex matches context.prefix: ${regex.matches(context.prefix)}")
-
-    val matchWord = context.word
-
-    println(s"match word is: $matchWord")
-
-    matchWord match {
+    context.word match {
       case regex(prefix) if isFirstCharLowerCase(prefix) =>
-        println("Entered good suggestion path")
         index.labelDefs.m.concat(index.labelUses.m)
           .filter { case (_, locs) => locs.exists(loc => loc.source.name == context.uri) }
           .map {
             case (label, _) =>
               Completion.LabelCompletion(label, prefix)
           }
-      case regex(prefix) =>
-        println(s"case regex(prefix) => $prefix")
-        Nil
       case _ =>
-        println("Entered Nil suggestion path")
         Nil
     }
   }
