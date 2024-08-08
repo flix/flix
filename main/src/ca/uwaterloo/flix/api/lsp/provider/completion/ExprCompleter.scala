261171ed5d2c1f0d0cc4deeedc3aa1f1c3afa6e7
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2024-08-08 20:05:41 +0200

    feat: add NewObjectCompleter to ExprCompleter

--- main/src/ca/uwaterloo/flix/api/lsp/provider/completion/ExprCompleter.scala
+++ main/src/ca/uwaterloo/flix/api/lsp/provider/completion/ExprCompleter.scala
@@ -31,6 +31,7 @@ object ExprCompleter extends Completer {
       EnumTagCompleter.getCompletions(context) ++
       SnippetCompleter.getCompletions(context) ++
       EnumCompleter.getCompletions(context) ++
-      ModuleCompleter.getCompletions(context)
+      ModuleCompleter.getCompletions(context) ++
+      NewObjectCompleter.getCompletions(context)
   }
 }
