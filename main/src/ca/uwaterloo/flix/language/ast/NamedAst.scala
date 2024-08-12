5fa4a697756a2c38d61e168368a8383dc655d040
Matthew Lutze <mlutze@users.noreply.github.com>
2024-08-12 10:06:47 +0200

    chore: remove TODO (#8290)

--- main/src/ca/uwaterloo/flix/language/ast/NamedAst.scala
+++ main/src/ca/uwaterloo/flix/language/ast/NamedAst.scala
@@ -420,7 +420,6 @@ object NamedAst {
   case class SelectChannelRule(sym: Symbol.VarSym, chan: Expr, exp: Expr)
 
   sealed trait TypeParam {
-    // TODO redundant? #8207
     def name: Name.Ident
 
     def sym: Symbol.UnkindedTypeVarSym
