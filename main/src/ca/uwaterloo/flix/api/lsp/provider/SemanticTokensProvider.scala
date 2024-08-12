1241a11934bdebf2ff9395488309d90492174987
Joseph Tan <66340795+dghosef@users.noreply.github.com>
2024-08-12 17:15:16 +0200

    feat: added `struct` support to `SemanticTokenProvider` (#8296)

--- main/src/ca/uwaterloo/flix/api/lsp/provider/SemanticTokensProvider.scala
+++ main/src/ca/uwaterloo/flix/api/lsp/provider/SemanticTokensProvider.scala
@@ -416,9 +416,19 @@ object SemanticTokensProvider {
     case Expr.ArrayLength(exp, _, _) =>
       visitExp(exp)
 
-    case Expr.StructNew(sym, fields, region, tpe, eff, loc) => throw new RuntimeException("JOE TBD")
-    case Expr.StructGet(sym, exp, field, tpe, eff, loc) => throw new RuntimeException("JOE TBD")
-    case Expr.StructPut(sym, exp1, field, exp2, tpe, eff, loc) => throw new RuntimeException("JOE TBD")
+    case Expr.StructNew(sym, fields, region, _, _, _) =>
+      val (names, exps) = fields.unzip
+      val ts = names.map(name => SemanticToken(SemanticTokenType.Property, Nil, name.loc))
+      val t = SemanticToken(SemanticTokenType.Type, Nil, sym.loc)
+      visitExps(exps) ++ visitExp(region) ++ ts ++ Iterator(t)
+
+    case Expr.StructGet(_, exp, field, _, _, _) =>
+      val t = SemanticToken(SemanticTokenType.Property, Nil, field.loc)
+      visitExp(exp) ++ Iterator(t)
+
+    case Expr.StructPut(_, exp1, field, exp2, _, _, _) =>
+      val t = SemanticToken(SemanticTokenType.Property, Nil, field.loc)
+      visitExp(exp1) ++ visitExp(exp2) ++ Iterator(t)
 
     case Expr.VectorLit(exps, _, _, _) =>
       visitExps(exps)
