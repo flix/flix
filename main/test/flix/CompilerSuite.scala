f4f24104d201790089d1f7de1a5e7e72b587c746
Joseph Tan <jdtan638@gmail.com>
2024-08-13 17:15:09 +0200

    _

--- main/test/flix/CompilerSuite.scala
+++ main/test/flix/CompilerSuite.scala
@@ -5,6 +5,9 @@ import ca.uwaterloo.flix.util.{FlixSuite, Options}
 class CompilerSuite extends FlixSuite(incremental = true) {
   implicit val options: Options = Options.TestWithLibAll
 
-  mkTestDir("main/test/flix/")
+  mkTest("/Users/dghosef/flix/main/test/ca/uwaterloo/flix/library/Test.Dec.Struct.flix")
+  mkTest("/Users/dghosef/flix/main/test/ca/uwaterloo/flix/library/Test.Exp.Struct.Put.flix")
+  mkTest("/Users/dghosef/flix/main/test/ca/uwaterloo/flix/library/Test.Exp.Struct.Get.flix")
+  mkTest("/Users/dghosef/flix/main/test/ca/uwaterloo/flix/library/Test.Exp.Struct.New.flix")
 
 }
