ff936af08b94eded4ec2cda0152b70e2ed70077f
jonathanStarup <32037926+JonathanStarup@users.noreply.github.com>
2024-08-27 15:30:27 +0200

    crime

--- main/src/ca/uwaterloo/flix/language/phase/typer/ConstraintSolver.scala
+++ main/src/ca/uwaterloo/flix/language/phase/typer/ConstraintSolver.scala
@@ -21,7 +21,6 @@ import ca.uwaterloo.flix.language.ast.{Ast, Kind, KindedAst, RigidityEnv, Source
 import ca.uwaterloo.flix.language.errors.TypeError
 import ca.uwaterloo.flix.language.phase.typer.TypeConstraint.Provenance
 import ca.uwaterloo.flix.language.phase.typer.TypeReduction.{JavaConstructorResolutionResult, JavaMethodResolutionResult}
-import ca.uwaterloo.flix.language.phase.unification.Unification.getUnderOrOverAppliedError
 import ca.uwaterloo.flix.language.phase.unification._
 import ca.uwaterloo.flix.util.Result.Err
 import ca.uwaterloo.flix.util.collection.{ListMap, ListOps}
@@ -739,8 +738,10 @@ object ConstraintSolver {
 
     case (UnificationError.MismatchedTypes(baseType1, baseType2), Provenance.Match(type1, type2, loc)) =>
       (baseType1.typeConstructor, baseType2.typeConstructor) match {
-        case (Some(TypeConstructor.Arrow(_)), _) => getUnderOrOverAppliedError(baseType1, baseType2, type1, type2, RigidityEnv.empty, loc)
-        case (_, Some(TypeConstructor.Arrow(_))) => getUnderOrOverAppliedError(baseType2, baseType1, type2, type1, RigidityEnv.empty, loc)
+        case (Some(TypeConstructor.Arrow(_)), _) =>
+          TypeError.MismatchedTypes(baseType1, baseType2, type1, type2, RigidityEnv.empty, loc)
+        case (_, Some(TypeConstructor.Arrow(_))) =>
+          TypeError.MismatchedTypes(baseType2, baseType1, type2, type1, RigidityEnv.empty, loc)
         case _ => TypeError.MismatchedTypes(baseType1, baseType2, type1, type2, RigidityEnv.empty, loc)
       }
 
