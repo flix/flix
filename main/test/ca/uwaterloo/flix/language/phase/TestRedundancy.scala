4eca960a64a49239fe237e7b2ca65668e9d6ad52
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2024-08-30 08:50:24 +0200

    Revert "test: update test"
    This reverts commit 2a691abf107a5e3ad8cffa5fa4dad41531378d37.

--- main/test/ca/uwaterloo/flix/language/phase/TestRedundancy.scala
+++ main/test/ca/uwaterloo/flix/language/phase/TestRedundancy.scala
@@ -1087,12 +1087,9 @@ class TestRedundancy extends AnyFunSuite with TestUtils {
   test("UnusedFormalParam.NewObject.01") {
     val input =
       """
-        |import java.lang.Comparable
-        |import java.lang.Object
-        |
-        |def f(): Comparable \ IO =
-        |   new Comparable {
-        |     def compareTo(x: Object, _y: Object): Int32 =
+        |def f(): ##java.lang.Comparable \ IO =
+        |   new ##java.lang.Comparable {
+        |     def compareTo(x: ##java.lang.Object, _y: ##java.lang.Object): Int32 =
         |       0
         |   }
         """.stripMargin
