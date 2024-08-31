6cf14fcfb049bb64b2aa48063d339d065f5547f1
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2024-08-31 21:29:45 +0200

    fix: handle error kinds

--- main/src/ca/uwaterloo/flix/language/ast/Type.scala
+++ main/src/ca/uwaterloo/flix/language/ast/Type.scala
@@ -492,7 +492,7 @@ object Type {
     lazy val kind: Kind = {
       tpe1.kind match {
         case Kind.Arrow(_, k2) => k2
-        case _ => throw InternalCompilerException(s"Illegal kind: '${tpe1.kind}' of type '$tpe1'.", loc)
+        case _ => Kind.Error
       }
     }
 
