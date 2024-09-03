df914a85e95d8dd4d9d09c493bdc85065e7cf7a9
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2024-09-03 10:02:56 +0200

    chore: scaffolding for base effects

--- main/src/ca/uwaterloo/flix/language/phase/typer/BaseEffects.scala
+++ main/src/ca/uwaterloo/flix/language/phase/typer/BaseEffects.scala
@@ -17,20 +17,38 @@ package ca.uwaterloo.flix.language.phase.typer
 
 import ca.uwaterloo.flix.language.ast.{SourceLocation, Symbol, Type, TypeConstructor}
 
+import java.lang.reflect.Method
+
 object BaseEffects {
 
   /**
-   * A pre-computed map from classes to effects.
-   */
-  private val m: Map[String, Set[Symbol.EffectSym]] = Map(
+    * A pre-computed map from classes to effects.
+    */
+  private val classEffs: Map[String, Set[Symbol.EffectSym]] = Map(
     classOf[java.lang.ProcessBuilder].getName -> Set(Symbol.Exec)
   )
 
   /**
-   * Returns the base effect set of the given Java `clazz`.
-   */
-  def of(clazz: Class[?], loc: SourceLocation): Type = {
-    val effs = m.getOrElse(clazz.getName, Set.empty).toList
+    * A pre-computed map from methods to effects.
+    */
+  private val methodEffs: Map[Method, Set[Symbol.EffectSym]] = Map(
+    classOf[java.lang.System].getMethod("currentTimeMillis") -> Set(Symbol.Clock)
+  )
+
+  /**
+    * Returns the effects of the given Java class `c`.
+    */
+  def of(c: Class[?], loc: SourceLocation): Type = {
+    val effs = classEffs.getOrElse(c.getName, Set.empty).toList
+    val tpes = effs.map(sym => Type.Cst(TypeConstructor.Effect(sym), loc))
+    Type.mkUnion(tpes, loc)
+  }
+
+  /**
+    * Returns the effects of the given Java method `m`.
+    */
+  def of(m: Method, loc: SourceLocation): Type = {
+    val effs = methodEffs.getOrElse(m, Set.empty).toList
     val tpes = effs.map(sym => Type.Cst(TypeConstructor.Effect(sym), loc))
     Type.mkUnion(tpes, loc)
   }
