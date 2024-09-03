54e0728234fd9c9835168a5140b92b0f23af5fd2
Alexander Dybdahl Troelsen <202008975@post.au.dk>
2024-09-03 18:13:56 +0200

    simplified `KeywordCompleters.Decl`

--- main/src/ca/uwaterloo/flix/api/lsp/provider/completion/KeywordCompleters.scala
+++ main/src/ca/uwaterloo/flix/api/lsp/provider/completion/KeywordCompleters.scala
@@ -70,78 +70,20 @@ object KeywordCompleters {
       * A completer for declaration keywords. These are keywords that denote a declaration.
       */
     object Decl {
-      val DEF       = "def"
-      val PUB       = "pub"
-      val ENUM      = "enum"
-      val TYPE      = "type"
-      val INSTANCE  = "instance"
-      val MOD       = "mod"
-      val EFF       = "eff"
-      val STRUCT    = "struct"
-      val SEALED    = "sealed"
-      val TRAIT     = "trait"
-      val IMPORT    = "import"
-
-      val DEF_ENTRY       = (DEF      , Priority.highest)
-      val PUB_ENTRY       = (PUB      , Priority.higher)
-      val ENUM_ENTRY      = (ENUM     , Priority.high)
-      val TYPE_ENTRY      = (TYPE     , Priority.high)
-      val INSTANCE_ENTRY  = (INSTANCE , Priority.high)
-      val MOD_ENTRY       = (MOD      , Priority.low)
-      val EFF_ENTRY       = (EFF      , Priority.lower)
-      val STRUCT_ENTRY    = (STRUCT   , Priority.lower)
-      val SEALED_ENTRY    = (SEALED   , Priority.lowest)
-      val TRAIT_ENTRY     = (TRAIT    , Priority.lowest)
-      val IMPORT_ENTRY    = (IMPORT   , Priority.lowest)
-
-      def getCompletions(context: CompletionContext)(implicit flix: Flix, index: Index, root: TypedAst.Root): Iterable[Completion] = {
-        context.previousWord match {
-          case PUB => PubDecl.getCompletions(context)
-          case SEALED => SealedDecl.getCompletions(context)
-          case _ => PriDecl.getCompletions(context) ++ DeclMod.getCompletions(context)
-        }
-      }
-
-      object DeclMod {
-        def getCompletions(context: CompletionContext)(implicit flix: Flix, index: Index, root: TypedAst.Root): Iterable[Completion] =
-          List(
-            PUB_ENTRY,
-            SEALED_ENTRY,
-          ) map { case (name, priority) => Completion.KeywordCompletion(name, priority(name)) }
-      }
-
-      object SealedDecl {
-        def getCompletions(context: CompletionContext)(implicit flix: Flix, index: Index, root: TypedAst.Root): Iterable[Completion] =
-          List(
-            TRAIT_ENTRY,
-          ) map { case (name, priority) => Completion.KeywordCompletion(name, priority(name)) }
-      }
-
-      object PubDecl {
-        def getCompletions(context: CompletionContext)(implicit flix: Flix, index: Index, root: TypedAst.Root): Iterable[Completion] =
-          List(
-            DEF_ENTRY,
-            ENUM_ENTRY,
-            TYPE_ENTRY,
-            EFF_ENTRY,
-            STRUCT_ENTRY,
-          ) map { case (name, priority) => Completion.KeywordCompletion(name, priority(name)) }
-      }
-
-      object PriDecl {
-        def getCompletions(context: CompletionContext)(implicit flix: Flix, index: Index, root: TypedAst.Root): Iterable[Completion] =
-          List(
-            DEF_ENTRY,
-            ENUM_ENTRY,
-            TYPE_ENTRY,
-            INSTANCE_ENTRY,
-            MOD_ENTRY,
-            EFF_ENTRY,
-            STRUCT_ENTRY,
-            TRAIT_ENTRY,
-            IMPORT_ENTRY,
-          ) map { case (name, priority) => Completion.KeywordCompletion(name, priority(name)) }
-      }
+      def getCompletions(context: CompletionContext)(implicit flix: Flix, index: Index, root: TypedAst.Root): Iterable[Completion] =
+        List(
+          ("def"      , Priority.highest),   
+          ("pub"      , Priority.higher),
+          ("enum"     , Priority.high),
+          ("type"     , Priority.high),
+          ("instance" , Priority.high),
+          ("mod"      , Priority.low),
+          ("eff"      , Priority.lower),
+          ("struct"   , Priority.lower),
+          ("sealed"   , Priority.lowest),
+          ("trait"    , Priority.lowest),
+          ("import"   , Priority.lowest),
+        ) map { case (name, priority) => Completion.KeywordCompletion(name, priority(name)) }
     }
 
     /**
