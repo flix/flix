74bf10623ed208388f2818bad09f83c94a258de4
Alexander Dybdahl Troelsen <202008975@post.au.dk>
2024-09-03 21:58:12 +0200

    fix: removed redundant args

--- main/src/ca/uwaterloo/flix/api/lsp/provider/CompletionProvider.scala
+++ main/src/ca/uwaterloo/flix/api/lsp/provider/CompletionProvider.scala
@@ -119,10 +119,10 @@ object CompletionProvider {
       //
       // Declarations.
       //
-      case SyntacticContext.Decl.Trait => KeywordCompleter.getTraitKeywords(context)
-      case SyntacticContext.Decl.Enum => KeywordCompleter.getEnumKeywords(context)
+      case SyntacticContext.Decl.Trait => KeywordCompleter.getTraitKeywords
+      case SyntacticContext.Decl.Enum => KeywordCompleter.getEnumKeywords
       case SyntacticContext.Decl.Instance => InstanceCompleter.getCompletions(context)
-      case _: SyntacticContext.Decl => KeywordCompleter.getDeclKeywords(context) ++ SnippetCompleter.getCompletions(context)
+      case _: SyntacticContext.Decl => KeywordCompleter.getDeclKeywords ++ SnippetCompleter.getCompletions(context)
 
       //
       // Imports.
@@ -157,7 +157,7 @@ object CompletionProvider {
       // Fallthrough.
       //
       case SyntacticContext.Unknown =>
-        KeywordCompleter.getOtherKeywords(context) ++ SnippetCompleter.getCompletions(context)
+        KeywordCompleter.getOtherKeywords ++ SnippetCompleter.getCompletions(context)
     }
   }
 
