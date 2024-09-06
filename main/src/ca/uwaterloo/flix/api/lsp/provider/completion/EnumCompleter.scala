d2bb6c462869027a8583b8d0b2b2dbf35e188fdb
Alexander Dybdahl Troelsen <202008975@post.au.dk>
2024-09-06 03:07:24 +0200

    removed remnants of priority boosting

--- main/src/ca/uwaterloo/flix/api/lsp/provider/completion/EnumCompleter.scala
+++ main/src/ca/uwaterloo/flix/api/lsp/provider/completion/EnumCompleter.scala
@@ -17,7 +17,7 @@ package ca.uwaterloo.flix.api.lsp.provider.completion
 
 import ca.uwaterloo.flix.api.Flix
 import ca.uwaterloo.flix.api.lsp.provider.completion.Completion.EnumCompletion
-import ca.uwaterloo.flix.api.lsp.provider.completion.TypeCompleter.{formatTParams, formatTParamsSnippet, getInternalPriority, priorityBoostForTypes}
+import ca.uwaterloo.flix.api.lsp.provider.completion.TypeCompleter.{formatTParams, formatTParamsSnippet, getInternalPriority}
 import ca.uwaterloo.flix.api.lsp.{Index, TextEdit}
 import ca.uwaterloo.flix.language.ast.Symbol.EnumSym
 import ca.uwaterloo.flix.language.ast.{Symbol, TypedAst}
@@ -60,7 +60,7 @@ object EnumCompleter {
     val sym = decl.sym
     val name = decl.sym.name
     val internalPriority = getInternalPriority(decl.loc, decl.sym.namespace)(ctx)
-    Completion.EnumCompletion(sym, formatTParams(decl.tparams), priorityBoostForTypes(internalPriority)(ctx),
+    Completion.EnumCompletion(sym, formatTParams(decl.tparams), internalPriority,
       TextEdit(ctx.range, s"${sym.toString}${formatTParamsSnippet(decl.tparams)}"), Some(decl.doc.text))
   }
 
