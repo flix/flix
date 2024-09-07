a0471c984e89c66768ded07e7e2f79a347bb7662
Holger Dal Mogensen <61235930+sockmaster27@users.noreply.github.com>
2024-09-07 10:32:40 +0200

    refactor: move `Polarity` from `Ast` to `shared` (#8576)

--- main/src/ca/uwaterloo/flix/language/ast/DesugaredAst.scala
+++ main/src/ca/uwaterloo/flix/language/ast/DesugaredAst.scala
@@ -17,7 +17,7 @@
 package ca.uwaterloo.flix.language.ast
 
 import ca.uwaterloo.flix.language.CompilationMessage
-import ca.uwaterloo.flix.language.ast.shared.{Denotation, Fixity, Source}
+import ca.uwaterloo.flix.language.ast.shared.{Denotation, Fixity, Polarity, Source}
 import ca.uwaterloo.flix.util.collection.MultiMap
 
 object DesugaredAst {
@@ -292,7 +292,7 @@ object DesugaredAst {
 
     object Body {
 
-      case class Atom(pred: Name.Pred, den: Denotation, polarity: Ast.Polarity, fixity: Fixity, terms: List[Pattern], loc: SourceLocation) extends Predicate.Body
+      case class Atom(pred: Name.Pred, den: Denotation, polarity: Polarity, fixity: Fixity, terms: List[Pattern], loc: SourceLocation) extends Predicate.Body
 
       case class Functional(idents: List[Name.Ident], exp: Expr, loc: SourceLocation) extends Predicate.Body
 
