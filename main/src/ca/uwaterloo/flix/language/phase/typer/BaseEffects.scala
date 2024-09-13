2be565b57540808b2bd3a285de3a20d5809d1431
Magnus Madsen <magnusm@cs.au.dk>
2024-09-13 12:58:59 +0200

    feat: add `Type.JvmToEff`

--- main/src/ca/uwaterloo/flix/language/phase/typer/BaseEffects.scala
+++ main/src/ca/uwaterloo/flix/language/phase/typer/BaseEffects.scala
@@ -17,39 +17,70 @@ package ca.uwaterloo.flix.language.phase.typer
 
 import ca.uwaterloo.flix.language.ast.{SourceLocation, Symbol, Type, TypeConstructor}
 
-import java.lang.reflect.{Constructor, Field, Method}
+import java.lang.reflect.{Constructor, Method}
 
 object BaseEffects {
 
   /**
-    * A pre-computed map from constructors to effects.
-    */
+   * A pre-computed map from constructors to effects.
+   */
   private val constructorEffs: Map[Constructor[?], Set[Symbol.EffectSym]] = Map.empty ++
     classOf[java.net.URL].getConstructors.map(c => (c, Set(Symbol.Net)))
 
   /**
-    * A pre-computed map from methods to effects.
-    */
+   * A pre-computed map from methods to effects.
+   */
   private val methodEffs: Map[Method, Set[Symbol.EffectSym]] = Map(
     classOf[java.lang.System].getMethod("currentTimeMillis") -> Set(Symbol.Time)
   )
 
   /**
-    * Returns the base effects of calling the given constructor `c`.
-    */
+   * A pre-computed map from classes to effects.
+   *
+   * If there is are specific effect(s) for a constructor or method then we use the effects for the entire class.
+   */
+  private val classEffs: Map[Class[?], Set[Symbol.EffectSym]] = Map(
+    classOf[java.lang.ProcessBuilder] -> Set(Symbol.Net)
+  )
+
+  /**
+   * Returns the base effects of calling the given constructor `c`.
+   */
   def getConstructorEffs(c: Constructor[?], loc: SourceLocation): Type = constructorEffs.get(c) match {
-    case None => Type.IO
+    case None =>
+      // Case 1: No effects for the constructor. Try the class map.
+      classEffs.get(c.getDeclaringClass) match {
+        case None =>
+          // Case 1.1: We use the IO effect by default.
+          Type.IO
+        case Some(effs) =>
+          // Case 1.2: We use the class effects.
+          val tpes = effs.toList.map(sym => Type.Cst(TypeConstructor.Effect(sym), loc))
+          Type.mkUnion(tpes, loc)
+      }
     case Some(effs) =>
+      // Case 2: We found the effects for the constructor.
       val tpes = effs.toList.map(sym => Type.Cst(TypeConstructor.Effect(sym), loc))
       Type.mkUnion(tpes, loc)
   }
 
   /**
-    * Returns the base effects of calling the given method `m`.
-    */
+   * Returns the base effects of calling the given method `m`.
+   */
   def getMethodEffs(m: Method, loc: SourceLocation): Type = methodEffs.get(m) match {
-    case None => Type.IO
+    case None =>
+      // Case 1: No effects for the constructor. Try the class map.
+      classEffs.get(m.getDeclaringClass) match {
+        case None =>
+          // Case 1.1: We use the IO effect by default.
+          Type.IO
+        case Some(effs) =>
+          // Case 1.2: We use the class effects.
+          val tpes = effs.toList.map(sym => Type.Cst(TypeConstructor.Effect(sym), loc))
+          Type.mkUnion(tpes, loc)
+      }
     case Some(effs) =>
+      // Case 2: We found the effects for the method.
       val tpes = effs.toList.map(sym => Type.Cst(TypeConstructor.Effect(sym), loc))
       Type.mkUnion(tpes, loc)
   }
