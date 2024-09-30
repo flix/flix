ed5ec0cc3bcf7d8aa3a36ca91aa2c7f1c837da23
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2024-09-30 21:16:07 +0200

    feat: impl Indexer

--- main/src/ca/uwaterloo/flix/api/lsp/Indexer.scala
+++ main/src/ca/uwaterloo/flix/api/lsp/Indexer.scala
@@ -244,6 +244,10 @@ object Indexer {
       val parent = Entity.Exp(exp0)
       visitExps(exps) ++ Index.occurrenceOf(exp0) ++ Index.useOf(sym, loc, parent)
 
+    case Expr.ApplySig(Ast.SigSymUse(sym, loc1), exps, _, _, _, loc2) =>
+      val parent = Entity.Exp(exp0)
+      visitExps(exps) ++ Index.occurrenceOf(exp0) ++ Index.useOf(sym, loc1, parent) ++ Index.useOf(sym.trt, loc1)
+
     case Expr.Unary(_, exp, _, _, _) =>
       visitExp(exp) ++ Index.occurrenceOf(exp0)
 
