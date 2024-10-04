fcff5afb2ace44f8dba0e842e29c0e26bd90d6c4
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2024-10-04 12:47:01 +0200

    feat: add ICE to prevent LocalDefSymUse abuse

--- main/src/ca/uwaterloo/flix/language/ast/Ast.scala
+++ main/src/ca/uwaterloo/flix/language/ast/Ast.scala
@@ -272,7 +272,9 @@ object Ast {
   /**
     * Represents a use of a LocalDef sym.
     */
-  case class LocalDefSymUse(sym: Symbol.VarSym, loc: SourceLocation)
+  case class LocalDefSymUse(sym: Symbol.VarSym, loc: SourceLocation) {
+    if (sym.boundBy != BoundBy.LocalDef) throw ca.uwaterloo.flix.util.InternalCompilerException(s"$sym is not a local def", loc)
+  }
 
   /**
     * Represents a use of a class sym.
