e92c6d89892cbdcd93e81af7efb852730d65a627
Alexander Dybdahl Troelsen <202008975@post.au.dk>
2024-10-07 13:48:24 +0200

    docs

--- main/src/ca/uwaterloo/flix/api/lsp/provider/Visitor.scala
+++ main/src/ca/uwaterloo/flix/api/lsp/provider/Visitor.scala
@@ -109,10 +109,19 @@ object Visitor {
     def accept(loc: SourceLocation): Boolean;
   }
 
+  /**
+    * Acceptor that accepts all AST nodes.
+    */
   case object AllAcceptor extends Acceptor {
     def accept(loc: SourceLocation): Boolean = true
   }
 
+  /**
+    * Acceptor that accepts all AST nodes whose `SourceLocation` is within
+    * the file given by the path `uri`.
+    *
+    * @param uri  the path of the file that an AST node `SourceLocation` must be within to be accepted.
+    */
   case class FileAcceptor(uri: String) extends Acceptor {
     def accept(loc: SourceLocation): Boolean = uri == loc.source.name
   }
