2bdaa5ce843eea105a4d2e1953ce1eefcf9ba4c4
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2024-10-09 19:03:41 +0200

    refactor: remove unused imports

--- main/src/ca/uwaterloo/flix/language/ast/OccurrenceAst1.scala
+++ main/src/ca/uwaterloo/flix/language/ast/OccurrenceAst1.scala
@@ -18,7 +18,7 @@
 package ca.uwaterloo.flix.language.ast
 
 import ca.uwaterloo.flix.language.ast.Ast.EliminatedBy
-import ca.uwaterloo.flix.language.ast.shared.{Annotations, Constant, Denotation, Doc, Fixity, Modifiers, Polarity, Source}
+import ca.uwaterloo.flix.language.ast.shared.{Annotations, Constant, Doc, Modifiers, Source}
 import ca.uwaterloo.flix.language.phase.Monomorpher
 
 object OccurrenceAst1 {
