e7d611eaa88d5936b012aa7d85af30bb2d899ce3
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2024-10-11 10:22:33 +0200

    fix: do not minimize types with errors

--- main/src/ca/uwaterloo/flix/language/phase/unification/TypeMinimization.scala
+++ main/src/ca/uwaterloo/flix/language/phase/unification/TypeMinimization.scala
@@ -15,8 +15,6 @@
  */
 package ca.uwaterloo.flix.language.phase.unification
 
-import ca.uwaterloo.flix.api.Flix
-import ca.uwaterloo.flix.language.ast.Type.hasError
 import ca.uwaterloo.flix.language.ast.{Ast, Kind, Scheme, Type}
 import ca.uwaterloo.flix.language.phase.unification.BoolFormula.{fromBoolType, fromEffType, toType}
 import ca.uwaterloo.flix.language.phase.unification.BoolFormulaTable.minimizeFormula
@@ -35,7 +33,7 @@ object TypeMinimization {
   def minimizeType(t: Type): Type = t.kind match {
     case Kind.Eff =>
       // We don't minimize if there is an error.
-      if (hasError(t)) t else minimizeBoolAlg(t)
+      if (Type.hasError(t)) t else minimizeBoolAlg(t)
     case Kind.Bool => minimizeBoolAlg(t)
     case _ => t match {
       case tpe: Type.Var => tpe
