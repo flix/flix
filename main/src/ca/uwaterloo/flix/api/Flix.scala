b810c1b72a9739121f4abecb5cc11c48d0079a2c
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2024-10-14 11:10:25 +0200

    refactor: use call phase in phaseNew

--- main/src/ca/uwaterloo/flix/api/Flix.scala
+++ main/src/ca/uwaterloo/flix/api/Flix.scala
@@ -618,31 +618,14 @@ class Flix {
   /**
     * Enters the phase with the given name.
     */
-  def phaseNew[A, B](phase: String)(f: => (A, B))(implicit d: Debug[A]): (A, B) = {
-    // Initialize the phase time object.
-    currentPhase = PhaseTime(phase, 0)
-
-    if (options.progress) {
-      progressBar.observe(currentPhase.phase, "", sample = false)
+  def phaseNew[A, B](phaseName: String)(f: => (A, B))(implicit d: Debug[A]): (A, B) = {
+    implicit object DebugValue extends Debug[(A, B)] {
+      override def emit(phase: String, tuple: (A, B))(implicit flix: Flix): Unit = {
+        val (a, _) = tuple
+        d.emit(phase, a)
+      }
     }
-
-    // Measure the execution time.
-    val t = System.nanoTime()
-    val (root, errs) = f
-    val e = System.nanoTime() - t
-
-    // Update the phase time.
-    currentPhase = currentPhase.copy(time = e)
-
-    // And add it to the list of executed phases.
-    phaseTimers += currentPhase
-
-    if (this.options.xprintphases) {
-      d.emit(phase, root)(this)
-    }
-
-    // Return the result computed by the phase.
-    (root, errs)
+    phase(phaseName)(f)
   }
 
   /**
