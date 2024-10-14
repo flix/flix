0f28388d1dcfcab27a8c730251a21c5860cb313f
Holger Dal Mogensen <61235930+sockmaster27@users.noreply.github.com>
2024-10-14 16:25:06 +0200

    chore: rename `debug` to `dbg` (#8894)

--- main/src/ca/uwaterloo/flix/language/phase/Lexer.scala
+++ main/src/ca/uwaterloo/flix/language/phase/Lexer.scala
@@ -402,9 +402,9 @@ object Lexer {
       case _ if isKeyword("checked_ecast") => TokenKind.KeywordCheckedECast
       case _ if isKeyword("choose*") => TokenKind.KeywordChooseStar
       case _ if isKeyword("choose") => TokenKind.KeywordChoose
-      case _ if isKeyword("debug!!") => TokenKind.KeywordDebugBangBang
-      case _ if isKeyword("debug!") => TokenKind.KeywordDebugBang
-      case _ if isKeyword("debug") => TokenKind.KeywordDebug
+      case _ if isKeyword("dbg!!") => TokenKind.KeywordDebugBangBang
+      case _ if isKeyword("dbg!") => TokenKind.KeywordDebugBang
+      case _ if isKeyword("dbg") => TokenKind.KeywordDebug
       case _ if isKeyword("def") => TokenKind.KeywordDef
       case _ if isKeyword("discard") => TokenKind.KeywordDiscard
       case _ if isKeyword("do") => TokenKind.KeywordDo
