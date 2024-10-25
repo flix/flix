566d14924ec78fc80d1ab38bd71953ef7c470c52
JonathanStarup <32037926+JonathanStarup@users.noreply.github.com>
2024-10-25 13:29:21 +0200

    work

--- main/src/ca/uwaterloo/flix/language/phase/typer/ConstraintGen.scala
+++ main/src/ca/uwaterloo/flix/language/phase/typer/ConstraintGen.scala
@@ -138,15 +138,15 @@ object ConstraintGen {
         // SUB-EFFECTING: Check if sub-effecting is enabled for lambda expressions.
         val shouldSubeffect = {
           val enabled = flix.options.xsubeffecting.contains(Subeffecting.Lambdas)
-          val redundant = exp match {
+          val useless = exp match {
             case Expr.Ascribe(_, _, Some(Type.Pure), _, _) => true
             case _ => false
           }
-          val hasCheckedCast = exp match {
+          val redundant = exp match {
             case Expr.CheckedCast(CheckedCastType.EffectCast, _, _, _, _) => true
             case _ => false
           }
-          enabled && !redundant && !hasCheckedCast
+          enabled && !useless && !redundant
         }
         val eff = if (shouldSubeffect) Type.mkUnion(eff0, Type.freshVar(Kind.Eff, loc), loc) else eff0
         val resTpe = Type.mkArrowWithEffect(fparam.tpe, eff, tpe, loc)
@@ -384,15 +384,15 @@ object ConstraintGen {
         // SUB-EFFECTING: Check if sub-effecting is enabled for lambda expressions (which include local defs).
         val shouldSubeffect = {
           val enabled = flix.options.xsubeffecting.contains(Subeffecting.Lambdas)
-          val redundant = exp1 match {
+          val useless = exp1 match {
             case Expr.Ascribe(_, _, Some(Type.Pure), _, _) => true
             case _ => false
           }
-          val hasCheckedCast = exp1 match {
+          val redundant = exp1 match {
             case Expr.CheckedCast(CheckedCastType.EffectCast, _, _, _, _) => true
             case _ => false
           }
-          enabled && !redundant && !hasCheckedCast
+          enabled && !useless && !redundant
         }
         val defEff = if (shouldSubeffect) Type.mkUnion(eff1, Type.freshVar(Kind.Eff, loc), loc) else eff1
         val defTpe = Type.mkUncurriedArrowWithEffect(fparams.map(_.tpe), defEff, tpe1, sym.loc)
