e9de23ae6a064dd9932fecc01bf067ab3e42bab3
Alexander Dybdahl Troelsen <202008975@post.au.dk>
2024-11-05 19:04:28 +0100

    updated copyright header

--- main/src/ca/uwaterloo/flix/api/lsp/provider/HighlightProvider.scala
+++ main/src/ca/uwaterloo/flix/api/lsp/provider/HighlightProvider.scala
@@ -1,5 +1,6 @@
 /*
- * Copyright 2024 Magnus Madsen, Alexander Dybdahl Troelsen
+ * Copyright 2020 Magnus Madsen,
+ *           2024 Alexander Dybdahl Troelsen
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
