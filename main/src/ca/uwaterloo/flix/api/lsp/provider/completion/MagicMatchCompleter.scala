cef9ada3205c4ea1852987b04160a178edf9a11c
stormckey <chenhaogao123@gmail.com>
2024-11-08 16:54:30 +0800

    align => for different cases

--- main/src/ca/uwaterloo/flix/api/lsp/provider/completion/MagicMatchCompleter.scala
+++ main/src/ca/uwaterloo/flix/api/lsp/provider/completion/MagicMatchCompleter.scala
@@ -39,39 +39,61 @@ object MagicMatchCompleter {
   def getCompletions(err: TypeError.FieldNotFound)(implicit root: TypedAst.Root): Iterable[Completion] = {
     for {
       sym <- getEnumSym(err.tpe)
-      ident <- err.base.text
+      baseExp <- err.base.text
     } yield {
-      val name = s"$ident.match"
+      val name = s"$baseExp.match"
       val range = sourceLocation2Range(err.loc)
       val casesString = generateCasesString(root.enums(sym).cases)
-      val snippet = s"match $ident {\n$casesString}"
+      val snippet = s"match $baseExp {\n$casesString}"
       Completion.MagicMatchCompletion(name, range, snippet, "Expand to a full match expression.")
+    }
+  }
+
+  /**
+   * Returns the length of the case String in the following form:
+   *  Unit: case Red => ???
+   *  Tuple: case Red(_elem1, _elem2) => ???
+   *  Normal: case Red(_elem) => ???
+   */
+
+  private def getCaseLength(cas: TypedAst.Case): Int = {
+    cas.tpe.typeConstructor match {
+      case Some(TypeConstructor.Unit) => cas.sym.toString.length
+      case Some(TypeConstructor.Tuple(arity)) => cas.sym.toString.length + arity * 7 + 1
+      case _ => cas.sym.toString.length + 7
     }
   }
 
   /**
     * Generates the string representation of the cases of an enum.
     */
+
   private def generateCasesString(cases: Map[Symbol.CaseSym, TypedAst.Case]): String = {
+    val maxCaseLength = cases.values.map(getCaseLength).max
+    println(maxCaseLength)
     cases.toList.sortBy(_._1.loc).foldLeft(("", 1))({
       case ((acc, z), (sym, cas)) =>
-        val (str, k) = cas.tpe.typeConstructor match {
-          case Some(TypeConstructor.Unit) => (s"$sym => $${${z + 1}:???}", z + 1)
-          case Some(TypeConstructor.Tuple(arity)) => (List.range(1, arity + 1)
-            .map(elem => s"$${${elem + z}:_elem$elem}")
-            .mkString(s"$sym(", ", ", s") => $${${arity + z + 1}:???}"), z + arity + 1)
-          case _ => (s"$sym($${${z + 1}:_elem}) => $${${z + 2}:???}", z + 2)
+        val (lhs, rhs, k) = cas.tpe.typeConstructor match {
+          case Some(TypeConstructor.Unit) =>
+            (s"$sym", s"$${${z + 1}:???}", z + 1)
+          case Some(TypeConstructor.Tuple(arity)) =>
+            val elements = List.range(1, arity + 1).map { elem =>
+              s"$${${elem + z}:_elem$elem}"
+            }.mkString(", ")
+            (s"$sym($elements)", s"$${${arity + z + 1}:???}", z + arity + 1)
+          case _ =>
+            (s"$sym($${${z + 1}:_elem})", s"$${${z + 2}:???}", z + 2)
         }
-        (acc + "    case " + str + "\n", k)
+        val paddedLhs = lhs.padTo(maxCaseLength, ' ')
+        (acc + s"    case $paddedLhs => $rhs\n", k)
     })._1
   }
-
 
   /**
    * Converts a [[SourceLocation]] to an [[Range]].
    */
 
-  def sourceLocation2Range(loc: SourceLocation): Range = {
+  private def sourceLocation2Range(loc: SourceLocation): Range = {
     Range(sourcePosition2Position(loc.sp1), sourcePosition2Position(loc.sp2))
   }
 
@@ -79,12 +101,13 @@ object MagicMatchCompleter {
    * Converts a [[SourcePosition]] to a [[Position]].
    */
 
-  def sourcePosition2Position(pos: SourcePosition): Position =
+  private def sourcePosition2Position(pos: SourcePosition): Position =
     Position(pos.line, pos.col)
 
   /**
    * Returns the enum symbol of the given type, if it is an enum.
    */
+
   private def getEnumSym(tpe: Type): Option[Symbol.EnumSym] = tpe.typeConstructor match {
     case Some(TypeConstructor.Enum(sym, _)) => Some(sym)
     case _ => None
