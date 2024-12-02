05c5137ded81617fc9a68420d354489591a478d8
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2024-12-02 02:47:23 +0100

    feat: add skeleton for `LspServer` (#9411)

---
+++ main/src/ca/uwaterloo/flix/api/lsp/LspServer.scala
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2024 Magnus Madsen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ca.uwaterloo.flix.api.lsp
+
+import org.eclipse.lsp4j.*
+import org.eclipse.lsp4j.launch.LSPLauncher
+import org.eclipse.lsp4j.services.{TextDocumentService, WorkspaceService}
+
+import java.util.concurrent.CompletableFuture
+
+object LspServer {
+
+  def run(): Unit = {
+    val in = System.in
+    val out = System.out
+
+    val server = new FlixLanguageServer
+    LSPLauncher.createServerLauncher(server, in, out).startListening
+  }
+
+  private class FlixLanguageServer extends org.eclipse.lsp4j.services.LanguageServer {
+
+    private val flixTextDocumentService = new FlixTextDocumentService
+    private val flixWorkspaceService = new FlixWorkspaceService
+
+    override def initialize(initializeParams: InitializeParams): CompletableFuture[InitializeResult] = {
+      System.err.println(s"initialize: $initializeParams")
+
+      val capabilities = new ServerCapabilities
+      capabilities.setHoverProvider(true)
+      CompletableFuture.completedFuture(new InitializeResult(capabilities))
+    }
+
+    override def shutdown(): CompletableFuture[AnyRef] = {
+      System.err.println("shutdown")
+      CompletableFuture.completedFuture(new Object)
+    }
+
+    override def exit(): Unit = {
+      System.err.println("exit")
+    }
+
+    override def getTextDocumentService: TextDocumentService = flixTextDocumentService
+
+    override def getWorkspaceService: WorkspaceService = flixWorkspaceService
+  }
+
+  private class FlixTextDocumentService extends TextDocumentService {
+    override def didOpen(didOpenTextDocumentParams: DidOpenTextDocumentParams): Unit = {
+      System.err.println(s"didOpen: $didOpenTextDocumentParams")
+    }
+
+    override def didChange(didChangeTextDocumentParams: DidChangeTextDocumentParams): Unit = {
+      System.err.println(s"didChange: $didChangeTextDocumentParams")
+    }
+
+    override def didClose(didCloseTextDocumentParams: DidCloseTextDocumentParams): Unit = {
+      System.err.println(s"didCloseTextDocumentParams: $didCloseTextDocumentParams")
+    }
+
+    override def didSave(didSaveTextDocumentParams: DidSaveTextDocumentParams): Unit = {
+      System.err.println(s"didSaveTextDocumentParams: $didSaveTextDocumentParams")
+    }
+
+    override def hover(params: HoverParams): CompletableFuture[Hover] = {
+      System.err.println(s"hover: $params")
+
+      val h = new Hover(new MarkupContent("plaintext", "Hello World from Hover!"))
+      CompletableFuture.completedFuture(h)
+    }
+  }
+
+  private class FlixWorkspaceService extends WorkspaceService {
+    override def didChangeConfiguration(didChangeConfigurationParams: DidChangeConfigurationParams): Unit = {
+      System.err.println(s"didChangeConfiguration: $didChangeConfigurationParams")
+    }
+
+    override def didChangeWatchedFiles(didChangeWatchedFilesParams: DidChangeWatchedFilesParams): Unit = {
+      System.err.println(s"didChangeWatchedFiles: $didChangeWatchedFilesParams")
+    }
+  }
+
+}
