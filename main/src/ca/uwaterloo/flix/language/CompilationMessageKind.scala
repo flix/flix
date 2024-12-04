3a3c0deaecffe81988923957bb6f93100e4b99df
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2024-12-04 21:27:49 +0100

    feat: add toString methods

--- main/src/ca/uwaterloo/flix/language/CompilationMessageKind.scala
+++ main/src/ca/uwaterloo/flix/language/CompilationMessageKind.scala
@@ -4,34 +4,56 @@ sealed trait CompilationMessageKind
 
 object CompilationMessageKind {
 
-  case object ParseError extends CompilationMessageKind
+  case object ParseError extends CompilationMessageKind {
+    override def toString: String = "Parse Error"
+  }
 
-  case object WeederError extends CompilationMessageKind
+  case object WeederError extends CompilationMessageKind {
+    override def toString: String = "Syntax Error"
+  }
 
-  case object NameError extends CompilationMessageKind
+  case object NameError extends CompilationMessageKind {
+    override def toString: String = "Name Error"
+  }
 
-  case object ResolutionError extends CompilationMessageKind
+  case object ResolutionError extends CompilationMessageKind {
+    override def toString: String = "Resolution Error"
+  }
 
-  case object KindError extends CompilationMessageKind
+  case object KindError extends CompilationMessageKind {
+    override def toString: String = "Kind Error"
+  }
 
-  case object DerivationError extends CompilationMessageKind
+  case object DerivationError extends CompilationMessageKind {
+    override def toString: String = "Derivation Error"
+  }
 
-  case object TypeError extends CompilationMessageKind
+  case object TypeError extends CompilationMessageKind {
+    override def toString: String = "Type Error"
+  }
 
-  case object RegionError extends CompilationMessageKind
+  case object EntryPointError extends CompilationMessageKind {
+    override def toString: String = "Entry Point Error"
+  }
 
-  case object EntryPointError extends CompilationMessageKind
+  case object InstanceError extends CompilationMessageKind {
+    override def toString: String = "Instance Error"
+  }
 
-  case object InstanceError extends CompilationMessageKind
+  case object StratificationError extends CompilationMessageKind {
+    override def toString: String = "Stratification Error"
+  }
 
-  case object PredDepError extends CompilationMessageKind
+  case object PatternMatchError extends CompilationMessageKind {
+    override def toString: String = "Pattern Match"
+  }
 
-  case object StratificationError extends CompilationMessageKind
+  case object RedundancyError extends CompilationMessageKind {
+    override def toString: String = "Redundancy Error"
+  }
 
-  case object PatternMatchError extends CompilationMessageKind
-
-  case object RedundancyError extends CompilationMessageKind
-
-  case object SafetyError extends CompilationMessageKind
+  case object SafetyError extends CompilationMessageKind {
+    override def toString: String = "Safety Error"
+  }
 
 }
