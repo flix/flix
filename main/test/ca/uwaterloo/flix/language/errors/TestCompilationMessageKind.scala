625f30eb8ceccaa35c932138312c45d2df6752c9
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2024-12-08 16:10:10 +0100

    test: add test

--- main/test/ca/uwaterloo/flix/language/errors/TestCompilationMessageKind.scala
+++ main/test/ca/uwaterloo/flix/language/errors/TestCompilationMessageKind.scala
@@ -26,16 +26,24 @@ class TestCompilationMessageKind extends AnyFunSuite with TestUtils {
     WeederError,
   )
 
-  test("TestParserErrorDoesNotDependOnSctx.01") {
+  private val Cartesian: List[(CompilationMessageKind, CompilationMessageKind)] = All.flatMap(e1 => All.map(e2 => (e1, e2)))
+
+  test("TestParserErrorOrderDoesNotDependOnSctx.01") {
     val e1 = ParseError(SyntacticContext.Unknown)
     val e2 = ParseError(SyntacticContext.Use)
     assert(e1.compare(e2) == 0)
   }
 
-  test("TestParserErrorDoesNotDependOnSctx.02") {
+  test("TestParserErrorOrderDoesNotDependOnSctx.02") {
     val e1 = ParseError(SyntacticContext.Unknown)
     val e2 = ParseError(SyntacticContext.Use)
     assert(e2.compare(e1) == 0)
+  }
+
+  test("TestOrderIsSameAsIntegers") {
+    val numbers = 1 to All.length
+    val cartesianNumbers = numbers.flatMap(i => numbers.map(j => (i, j))).toList
+    assert(Cartesian.zip(cartesianNumbers).forall { case ((e1, e2), (i, j)) => e1.compare(e2) == i.compare(j) })
   }
 
 }
