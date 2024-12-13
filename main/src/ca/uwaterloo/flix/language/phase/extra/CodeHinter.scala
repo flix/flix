2a6b008ae0e8ab6eab7d4fd5dbebf1b682f9d115
Alexander Dybdahl Troelsen <202008975@post.au.dk>
2024-12-13 22:38:27 +0100

    Separation of use finding and hint construction

--- main/src/ca/uwaterloo/flix/language/phase/extra/CodeHinter.scala
+++ main/src/ca/uwaterloo/flix/language/phase/extra/CodeHinter.scala
@@ -16,16 +16,13 @@
  */
 package ca.uwaterloo.flix.language.phase.extra
 
-import ca.uwaterloo.flix.api.Flix
 import ca.uwaterloo.flix.api.lsp.acceptors.AllAcceptor
-import ca.uwaterloo.flix.api.lsp.{Consumer, Index, Visitor}
-import ca.uwaterloo.flix.language.ast.TypedAst.Predicate.{Body, Head}
+import ca.uwaterloo.flix.api.lsp.{Consumer, Visitor}
 import ca.uwaterloo.flix.language.ast.TypedAst.*
-import ca.uwaterloo.flix.language.ast.shared.{Annotation, Annotations, SymUse, TraitConstraint}
-import ca.uwaterloo.flix.language.ast.shared.SymUse.DefSymUse
-import ca.uwaterloo.flix.language.ast.{Ast, SourceLocation, Symbol, Type, TypeConstructor, TypedAst}
+import ca.uwaterloo.flix.language.ast.shared.SymUse.{DefSymUse, TraitSymUse}
+import ca.uwaterloo.flix.language.ast.shared.{Annotations, SymUse, TraitConstraint}
+import ca.uwaterloo.flix.language.ast.{SourceLocation, Symbol, Type, TypeConstructor, TypedAst}
 import ca.uwaterloo.flix.language.errors.CodeHint
-import ca.uwaterloo.flix.util.collection.MultiMap
 
 object CodeHinter {
 
@@ -33,36 +30,48 @@ object CodeHinter {
     * Returns a collection of code quality hints for the given AST `root`.
     */
   def run(root: TypedAst.Root, sources: Set[String]): List[CodeHint] = {
-    var hints: List[CodeHint] = Nil
+    val uses = getUses(root)
 
-    object HintConsumer extends Consumer {
-      override def consumeTraitSymUse(symUse: SymUse.TraitSymUse): Unit = {
-        hints = hints ++ considerTrait(root, symUse.sym, symUse.loc)
-      }
-      override def consumeTraitConstraintHead(tcHead: TraitConstraint.Head): Unit = {
-        hints = hints ++ considerTrait(root, tcHead.sym, tcHead.loc)
-      }
-      override def consumeDefSymUse(sym: DefSymUse): Unit = {
-        hints = hints ++ considerDef(root, sym.sym, sym.loc)
-      }
+    val traitHints = uses.traitUses.flatMap(considerTrait(root))
+    val defHints = uses.defUses.flatMap(considerDef(root))
+    val enumHints = uses.enumUses.flatMap{case (sym, loc) => considerEnum(root, sym, loc)}
+    val defCalls = uses.defCalls.flatMap{case (sym, exps) => considerDefCall(root, sym, exps)}
+
+    val hints = traitHints ++ defHints ++ enumHints ++ defCalls
+
+    hints.filter(include(_, sources))
+  }
+
+  private def considerDefCall(root: Root, sym: Symbol.DefnSym, exps: List[Expr]): List[CodeHint] = {
+    exps.flatMap(e => checkEffect(sym, e.tpe, e.loc)(root))
+  }
+
+  private case class Uses(traitUses: List[TraitSymUse], enumUses: List[(Symbol.EnumSym, SourceLocation)], defUses: List[DefSymUse], defCalls: List[(Symbol.DefnSym, List[Expr])])
+
+  private def getUses(root: Root): Uses = {
+    var traitUses: List[TraitSymUse] = Nil
+    var defUses: List[DefSymUse] = Nil
+    var enumUses: List[(Symbol.EnumSym, SourceLocation)] = Nil
+    var defCalls: List[(Symbol.DefnSym, List[Expr])] = Nil
+
+    object UseConsumer extends Consumer {
+      override def consumeTraitSymUse(symUse: TraitSymUse): Unit = traitUses = symUse :: traitUses
+      override def consumeTraitConstraintHead(tcHead: TraitConstraint.Head): Unit = traitUses = TraitSymUse(tcHead.sym, tcHead.loc) :: traitUses
+      override def consumeDefSymUse(sym: DefSymUse): Unit = defUses = sym :: defUses
       override def consumeExpr(exp: Expr): Unit = exp match {
-        case TypedAst.Expr.ApplyDef(symUse, exps, _, _, _, _) =>
-          hints = hints ++ exps.flatMap(e => checkEffect(symUse.sym, e.tpe, e.loc)(root))
+        case TypedAst.Expr.ApplyDef(symUse, exps, _, _, _, _) => defCalls = (symUse.sym, exps) :: defCalls
         case _ => ()
       }
       override def consumeType(tpe: Type): Unit = tpe match {
-        case Type.Cst(TypeConstructor.Enum(sym, _), loc) =>
-          hints = hints ++ considerEnum(root, sym, loc)
+        case Type.Cst(TypeConstructor.Enum(sym, _), loc) => enumUses = (sym, loc) :: enumUses
         case _ => ()
       }
-      override def consumeCaseSymUse(sym: SymUse.CaseSymUse): Unit = {
-        hints = hints ++ considerEnum(root, sym.sym.enumSym, sym.loc)
-      }
+      override def consumeCaseSymUse(sym: SymUse.CaseSymUse): Unit = enumUses = (sym.sym.enumSym, sym.loc) :: enumUses
     }
 
-    Visitor.visitRoot(root, HintConsumer, AllAcceptor)
+    Visitor.visitRoot(root, UseConsumer, AllAcceptor)
 
-    hints.filter(include(_, sources))
+    Uses(traitUses, enumUses, defUses, defCalls)
   }
 
   /**
@@ -73,10 +82,10 @@ object CodeHinter {
     * @param loc  The [[SourceLocation]] for the occurrence of `sym`.
     * @return     A collection of code quality hints.
     */
-  private def considerTrait(root: Root, sym: Symbol.TraitSym, loc: SourceLocation): List[CodeHint] = {
-    val trt = root.traits(sym)
+  private def considerTrait(root: Root)(symUse: SymUse.TraitSymUse): List[CodeHint] = {
+    val trt = root.traits(symUse.sym)
     val ann = trt.ann
-    checkDeprecated(ann, loc) ++ checkExperimental(ann, loc)
+    checkDeprecated(ann, symUse.loc) ++ checkExperimental(ann, symUse.loc)
   }
 
   /**
@@ -89,13 +98,13 @@ object CodeHinter {
     * @param loc  The [[SourceLocation]] for the occurrence of the `sym`.
     * @return     A collection of code quality hints
     */
-  private def considerDef(root: Root, sym: Symbol.DefnSym, loc: SourceLocation): List[CodeHint] = {
-    val defn = root.defs(sym)
+  private def considerDef(root: Root)(symUse: SymUse.DefSymUse): List[CodeHint] = {
+    val defn = root.defs(symUse.sym)
     val ann = defn.spec.ann
-    checkDeprecated(ann, loc) ++
-      checkExperimental(ann, loc) ++
-      checkLazy(ann, loc) ++
-      checkParallel(ann, loc)
+    checkDeprecated(ann, symUse.loc) ++
+      checkExperimental(ann, symUse.loc) ++
+      checkLazy(ann, symUse.loc) ++
+      checkParallel(ann, symUse.loc)
   }
 
   /**
