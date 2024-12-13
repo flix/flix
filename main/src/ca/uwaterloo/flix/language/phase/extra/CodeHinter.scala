1d5eb2c4620c16b2741f7da2f51ed4289bd69979
Alexander Dybdahl Troelsen <202008975@post.au.dk>
2024-12-13 21:09:57 +0100

    copyright header

--- main/src/ca/uwaterloo/flix/language/phase/extra/CodeHinter.scala
+++ main/src/ca/uwaterloo/flix/language/phase/extra/CodeHinter.scala
@@ -1,5 +1,6 @@
 /*
  * Copyright 2021 Magnus Madsen
+ * Copyright 2024 Alexander Dybdahl Troelsen
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
