246baf295537bbb559fc707707f76971016fab90
stormckey <chenhaogao123@gmail.com>
2024-12-20 16:31:07 +0100

    another algorithm for op, sig and tag

--- main/src/ca/uwaterloo/flix/api/lsp/provider/CompletionProvider.scala
+++ main/src/ca/uwaterloo/flix/api/lsp/provider/CompletionProvider.scala
@@ -18,6 +18,7 @@ package ca.uwaterloo.flix.api.lsp.provider
 import ca.uwaterloo.flix.api.Flix
 import ca.uwaterloo.flix.api.lsp.*
 import ca.uwaterloo.flix.api.lsp.provider.completion.*
+import ca.uwaterloo.flix.api.lsp.provider.completion.CompletionUtils.getNamespaceAndIdentFromQName
 import ca.uwaterloo.flix.api.lsp.provider.completion.semantic.{GetStaticFieldCompleter, InvokeStaticMethodCompleter}
 import ca.uwaterloo.flix.api.lsp.provider.completion.syntactic.{ExprSnippetCompleter, KeywordCompleter}
 import ca.uwaterloo.flix.language.CompilationMessage
@@ -46,9 +47,13 @@ object CompletionProvider {
   def autoComplete(uri: String, pos: Position, source: String, currentErrors: List[CompilationMessage])(implicit flix: Flix, root: TypedAst.Root): JObject = {
     val completionItems = getCompletionContext(source, uri, pos, currentErrors).map {ctx =>
       errorsAt(ctx.uri, ctx.pos, currentErrors).flatMap({
-        case err: WeederError.UnqualifiedUse => UseCompleter.getCompletions(err, ctx.uri)
+        case err: WeederError.UnqualifiedUse =>
+          val (namespace, ident) = getNamespaceAndIdentFromQName(err.qn, err.loc)
+          UseCompleter.getCompletions(ctx.uri, namespace, ident)
         case WeederError.UndefinedAnnotation(_, _) => KeywordCompleter.getModKeywords ++ ExprSnippetCompleter.getCompletions()
-        case err: ResolutionError.UndefinedUse => UseCompleter.getCompletions(err, ctx.uri)
+        case err: ResolutionError.UndefinedUse =>
+          val (namespace, ident) = getNamespaceAndIdentFromQName(err.qn, err.loc)
+          UseCompleter.getCompletions(ctx.uri, namespace, ident)
         case ResolutionError.UndefinedTag(_, _, _, _) => ModuleCompleter.getCompletions(ctx) ++ EnumTagCompleter.getCompletions(ctx)
         case err: ResolutionError.UndefinedName => AutoImportCompleter.getCompletions(err) ++ LocalScopeCompleter.getCompletions(err) ++ AutoUseCompleter.getCompletions(err) ++ ExprCompleter.getCompletions(ctx)
         case err: ResolutionError.UndefinedType => AutoImportCompleter.getCompletions(err) ++ LocalScopeCompleter.getCompletions(err) ++ AutoUseCompleter.getCompletions(err) ++ EffSymCompleter.getCompletions(err) ++ TypeCompleter.getCompletions(ctx)
