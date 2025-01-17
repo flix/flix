2766b991049825b9edb4ef4498dcb693687957d8
Magnus Madsen <magnusm@cs.au.dk>
2025-01-17 15:52:55 +0100

    test: add `testSelectRandom05`

--- main/test/ca/uwaterloo/flix/language/phase/unification/set/TestSetUnification.scala
+++ main/test/ca/uwaterloo/flix/language/phase/unification/set/TestSetUnification.scala
@@ -18,7 +18,7 @@ package ca.uwaterloo.flix.language.phase.unification.set
 
 import ca.uwaterloo.flix.TestUtils
 import ca.uwaterloo.flix.language.phase.unification.set.EquationGenerator.mkEq
-import ca.uwaterloo.flix.language.phase.unification.set.SetFormula.{Cst, Empty, Union, Var}
+import ca.uwaterloo.flix.language.phase.unification.set.SetFormula.*
 import ca.uwaterloo.flix.language.phase.unification.set.SetUnification.SolverListener
 import ca.uwaterloo.flix.util.TwoList
 import org.scalatest.funsuite.AnyFunSuite
@@ -28,7 +28,7 @@ class TestSetUnification extends AnyFunSuite with TestUtils {
   implicit val opts: SetUnification.Options = SetUnification.Options.default
   implicit val listener: SolverListener = SolverListener.DoNothing
 
-  test("Iterator.mapWithIndex") {
+  test("testSelectRandom05") {
     val input = List(
       mkEq(Var(0), Cst(21)),
       mkEq(Var(9), Var(10)),
@@ -54,6 +54,33 @@ class TestSetUnification extends AnyFunSuite with TestUtils {
       mkEq(Var(9), Cst(21))
     )
     val (eqs, _) = SetUnification.solve(input)
+    assert(eqs.isEmpty)
+  }
+
+  test("testSelectRandom05") {
+    val input = List(
+      mkEq(Cst(1), mkInter(mkUnionAll(List(Cst(1), Cst(2), Cst(3), Cst(4), Cst(5), Cst(6), Cst(7), Cst(8), Cst(9))), Var(317))),
+      mkEq(Cst(1), mkInter(mkUnionAll(List(Cst(1), Cst(2), Cst(3), Cst(4), Cst(5), Cst(6), Cst(7), Cst(8), Cst(9))), Var(320))),
+      mkEq(Cst(1), mkInter(mkUnionAll(List(Cst(1), Cst(2), Cst(3), Cst(4), Cst(5), Cst(6), Cst(7), Cst(8), Cst(9))), Var(323))),
+      mkEq(Cst(1), mkInter(mkUnionAll(List(Cst(1), Cst(2), Cst(3), Cst(4), Cst(5), Cst(6), Cst(7), Cst(8), Cst(9))), Var(326))),
+      mkEq(Cst(1), mkInter(mkUnionAll(List(Cst(1), Cst(2), Cst(3), Cst(4), Cst(5), Cst(6), Cst(7), Cst(8), Cst(9))), Var(329))),
+      mkEq(Cst(1), mkInter(mkUnionAll(List(Cst(1), Cst(2), Cst(3), Cst(4), Cst(5), Cst(6), Cst(7), Cst(8), Cst(9))), Var(332))),
+      mkEq(Cst(1), mkInter(mkUnionAll(List(Cst(1), Cst(2), Cst(3), Cst(4), Cst(5), Cst(6), Cst(7), Cst(8), Cst(9))), Var(335))),
+      mkEq(Cst(1), mkInter(mkUnionAll(List(Cst(1), Cst(2), Cst(3), Cst(4), Cst(5), Cst(6), Cst(7), Cst(8), Cst(9))), Var(338))),
+      mkEq(Cst(1), mkInter(mkUnionAll(List(Cst(1), Cst(2), Cst(3), Cst(4), Cst(5), Cst(6), Cst(7), Cst(8), Cst(9))), Var(341))),
+      mkEq(Cst(1), mkInter(mkUnionAll(List(Cst(1), Cst(2), Cst(3), Cst(4), Cst(5), Cst(6), Cst(7), Cst(8), Cst(9))), Var(344))),
+      mkEq(Cst(1), mkInter(mkUnionAll(List(Cst(1), Cst(2), Cst(3), Cst(4), Cst(5), Cst(6), Cst(7), Cst(8), Cst(9))), Var(347))),
+      mkEq(Cst(1), mkInter(mkUnionAll(List(Cst(1), Cst(2), Cst(3), Cst(4), Cst(5), Cst(6), Cst(7), Cst(8), Cst(9))), Var(350))),
+      mkEq(Cst(1), mkInter(mkUnionAll(List(Cst(1), Cst(2), Cst(3), Cst(4), Cst(5), Cst(6), Cst(7), Cst(8), Cst(9))), Var(353))),
+      mkEq(Cst(1), mkInter(mkUnionAll(List(Cst(1), Cst(2), Cst(3), Cst(4), Cst(5), Cst(6), Cst(7), Cst(8), Cst(9))), Var(356))),
+      mkEq(Cst(1), mkInter(mkUnionAll(List(Cst(1), Cst(2), Cst(3), Cst(4), Cst(5), Cst(6), Cst(7), Cst(8), Cst(9))), Var(359))),
+      mkEq(Cst(1), mkInter(mkUnionAll(List(Cst(1), Cst(2), Cst(3), Cst(4), Cst(5), Cst(6), Cst(7), Cst(8), Cst(9))), Var(362))),
+      mkEq(mkUnion(Cst(1), Cst(8)), mkInter(mkUnionAll(List(Cst(1), Cst(2), Cst(3), Cst(4), Cst(5), Cst(6), Cst(7), Cst(8), Cst(9))), Var(365))),
+      mkEq(mkUnion(Cst(1), Cst(8)), mkInter(mkUnionAll(List(Cst(1), Cst(2), Cst(3), Cst(4), Cst(5), Cst(6), Cst(7), Cst(8), Cst(9))), Var(371))),
+      mkEq(Cst(1), mkInter(mkUnionAll(List(Cst(1), Cst(2), Cst(3), Cst(4), Cst(5), Cst(6), Cst(7), Cst(8), Cst(9))), Var(385))),
+      mkEq(mkUnion(Cst(1), Cst(8)), mkInter(mkUnionAll(List(Cst(1), Cst(2), Cst(3), Cst(4), Cst(5), Cst(6), Cst(7), Cst(8), Cst(9))), Var(453)))
+    )
+    val (eqs, s) = SetUnification.solve(input)
     assert(eqs.isEmpty)
   }
 
