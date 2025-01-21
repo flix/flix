33c849f8b20fd565594e50c4168fb1da7a851b31
stormckey <chenhaogao123@gmail.com>
2025-01-21 13:30:37 +0100

    change the architect

--- main/src/ca/uwaterloo/flix/api/lsp/LspServer.scala
+++ main/src/ca/uwaterloo/flix/api/lsp/LspServer.scala
@@ -23,7 +23,7 @@ import ca.uwaterloo.flix.util.Formatter.NoFormatter
 import ca.uwaterloo.flix.util.{Options, StreamOps}
 import org.eclipse.lsp4j.*
 import org.eclipse.lsp4j.launch.LSPLauncher
-import org.eclipse.lsp4j.services.{LanguageServer, TextDocumentService, WorkspaceService}
+import org.eclipse.lsp4j.services.{LanguageClient, LanguageClientAware, LanguageServer, TextDocumentService, WorkspaceService}
 
 import java.nio.file.{Files, Paths}
 import java.util.concurrent.CompletableFuture
@@ -35,34 +35,53 @@ object LspServer {
     val out = System.out
     val server = new FlixLanguageServer(o)
     System.err.println(s"Starting Default LSP Server...")
-    LSPLauncher.createServerLauncher(server, in, out).startListening.get()
+    val launcher = LSPLauncher.createServerLauncher(server, in, out)
+    server.connect(launcher.getRemoteProxy)
+    launcher.startListening().get()
     System.err.println(s"LSP Server Terminated.")
   }
 
-  private class FlixLanguageServer(o: Options) extends LanguageServer {
+  private class FlixLanguageServer(o: Options) extends LanguageServer with LanguageClientAware {
+    /**
+      * The Flix instance (the same instance is used for incremental compilation).
+      */
+    val flix: Flix = new Flix().setFormatter(NoFormatter).setOptions(o)
 
-    private val flixTextDocumentService = new FlixTextDocumentService(o)
-    private val flixWorkspaceService = new FlixWorkspaceService
+    /**
+      * A map from source URIs to source code.
+      */
+    val sources: mutable.Map[String, String] = mutable.Map.empty
+
+    /**
+      * The current AST root. The root is null until the source code is compiled.
+      */
+    var root: Root = TypedAst.empty
+
+    var flixLanguageClient: LanguageClient = _
+
+    private var clientCapabilities: ClientCapabilities = _;
+
+    private val flixTextDocumentService = new FlixTextDocumentService(this)
+    private val flixWorkspaceService = new FlixWorkspaceService(this)
 
     override def initialize(initializeParams: InitializeParams): CompletableFuture[InitializeResult] = {
       System.err.println(s"initialize: $initializeParams")
 
-      val capabilities = new ServerCapabilities
-      capabilities.setHoverProvider(true)
-      if (initializeParams.getWorkspaceFolders == null || initializeParams.getWorkspaceFolders.isEmpty) {
-        System.err.println("No workspace folders.")
-        return CompletableFuture.failedFuture(new RuntimeException("No workspace folders"))
+      clientCapabilities = initializeParams.getCapabilities
+
+      val serverCapabilities = new ServerCapabilities
+      serverCapabilities.setHoverProvider(true)
+      serverCapabilities.setTextDocumentSync(TextDocumentSyncKind.Full)
+
+      try {
+        loadMain(initializeParams)
+      } catch {
+        case ex: Throwable =>
+          System.err.println(s"Failed to initialize the LSP: ${ex.getMessage}")
+          return CompletableFuture.failedFuture(ex)
       }
-      val mainPath = Paths.get(initializeParams.getWorkspaceFolders.get(0).getName, "Main.flix")
-      val mainUri = mainPath.toString
-      if (Files.exists(mainPath)) {
-        val mainSrc = StreamOps.readAll(Files.newInputStream(mainPath))
-        flixTextDocumentService.addSourceCode(mainUri, mainSrc)
-        CompletableFuture.completedFuture(new InitializeResult(capabilities))
-      } else {
-        System.err.println(s"Main file not found: $mainPath")
-        CompletableFuture.failedFuture(new RuntimeException("Main file not found"))
-      }
+
+      CompletableFuture.completedFuture(new InitializeResult(serverCapabilities))
     }
 
     override def shutdown(): CompletableFuture[AnyRef] = {
@@ -74,27 +93,38 @@ object LspServer {
       System.err.println("exit")
     }
 
+    override def connect(client: LanguageClient): Unit = {
+      System.err.println("connect to the client")
+      flixLanguageClient = client
+    }
+
     override def getTextDocumentService: TextDocumentService = flixTextDocumentService
 
     override def getWorkspaceService: WorkspaceService = flixWorkspaceService
+
+    private def loadMain(initializeParams: InitializeParams): Unit = {
+      if (initializeParams.getWorkspaceFolders == null || initializeParams.getWorkspaceFolders.isEmpty) {
+        System.err.println("No workspace folders.")
+        throw new RuntimeException("No workspace folders")
+      }
+      val mainPath = Paths.get(initializeParams.getWorkspaceFolders.get(0).getName, "Main.flix")
+      val mainUri = mainPath.toString
+      if (Files.exists(mainPath)) {
+        val mainSrc = StreamOps.readAll(Files.newInputStream(mainPath))
+        addSourceCode(mainUri, mainSrc)
+      } else {
+        System.err.println(s"Main file not found: $mainPath")
+        throw new RuntimeException("Main file not found")
+      }
+    }
+
+    private def addSourceCode(uri: String, src: String): Unit = {
+      flix.addSourceCode(uri, src)(SecurityContext.AllPermissions)
+      sources.put(uri, src)
+    }
   }
 
-  private class FlixTextDocumentService(o: Options) extends TextDocumentService {
-    /**
-      * The Flix instance (the same instance is used for incremental compilation).
-      */
-    private val flix: Flix = new Flix().setFormatter(NoFormatter).setOptions(o)
-
-    /**
-      * A map from source URIs to source code.
-      */
-    private val sources: mutable.Map[String, String] = mutable.Map.empty
-
-    /**
-      * The current AST root. The root is null until the source code is compiled.
-      */
-    private var root: Root = TypedAst.empty
-
+  private class FlixTextDocumentService(flixLanguageServer: FlixLanguageServer) extends TextDocumentService {
     override def didOpen(didOpenTextDocumentParams: DidOpenTextDocumentParams): Unit = {
       System.err.println(s"didOpen: $didOpenTextDocumentParams")
     }
@@ -117,14 +147,9 @@ object LspServer {
       val h = new Hover(new MarkupContent("plaintext", "Hello World from Hover!"))
       CompletableFuture.completedFuture(h)
     }
-
-    def addSourceCode(uri: String, src: String): Unit = {
-      flix.addSourceCode(uri, src)(SecurityContext.AllPermissions)
-      sources.put(uri, src)
-    }
   }
 
-  private class FlixWorkspaceService extends WorkspaceService {
+  private class FlixWorkspaceService(flixLanguageServer: FlixLanguageServer) extends WorkspaceService {
     override def didChangeConfiguration(didChangeConfigurationParams: DidChangeConfigurationParams): Unit = {
       System.err.println(s"didChangeConfiguration: $didChangeConfigurationParams")
     }
