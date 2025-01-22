8bdcc159369d665a01339800bacd2d16c644d06e
stormckey <chenhaogao123@gmail.com>
2025-01-22 15:50:02 +0800

    feat: load main for the standalone lsp when initializing (#9734)

--- main/src/ca/uwaterloo/flix/api/lsp/LspServer.scala
+++ main/src/ca/uwaterloo/flix/api/lsp/LspServer.scala
@@ -15,35 +15,80 @@
  */
 package ca.uwaterloo.flix.api.lsp
 
+import ca.uwaterloo.flix.api.Flix
+import ca.uwaterloo.flix.language.ast.TypedAst
+import ca.uwaterloo.flix.language.ast.TypedAst.Root
+import ca.uwaterloo.flix.language.ast.shared.SecurityContext
+import ca.uwaterloo.flix.util.Formatter.NoFormatter
 import ca.uwaterloo.flix.util.Options
 import org.eclipse.lsp4j.*
 import org.eclipse.lsp4j.launch.LSPLauncher
-import org.eclipse.lsp4j.services.{LanguageServer, TextDocumentService, WorkspaceService}
+import org.eclipse.lsp4j.services.{LanguageClient, LanguageClientAware, LanguageServer, TextDocumentService, WorkspaceService}
 
 import java.util.concurrent.CompletableFuture
+import scala.collection.mutable
 
 object LspServer {
-
   def run(o: Options): Unit = {
-    val in = System.in
-    val out = System.out
-    val server = new FlixLanguageServer
     System.err.println(s"Starting Default LSP Server...")
-    LSPLauncher.createServerLauncher(server, in, out).startListening.get()
+
+    val server = new FlixLanguageServer(o)
+    val launcher = LSPLauncher.createServerLauncher(server, System.in, System.out)
+    val client = launcher.getRemoteProxy
+    server.connect(client)
+    launcher.startListening().get()
+
     System.err.println(s"LSP Server Terminated.")
   }
 
-  private class FlixLanguageServer extends LanguageServer {
+  private class FlixLanguageServer(o: Options) extends LanguageServer with LanguageClientAware {
+    /**
+      * The Flix instance (the same instance is used for incremental compilation).
+      */
+    val flix: Flix = new Flix().setFormatter(NoFormatter).setOptions(o)
 
-    private val flixTextDocumentService = new FlixTextDocumentService
-    private val flixWorkspaceService = new FlixWorkspaceService
+    /**
+      * A map from source URIs to source code.
+      */
+    val sources: mutable.Map[String, String] = mutable.Map.empty
 
+    /**
+      * The current AST root. The root is null until the source code is compiled.
+      */
+    var root: Root = TypedAst.empty
+
+    /**
+      * The proxy to the language client.
+      * Used to send messages to the client.
+      */
+    var flixLanguageClient: LanguageClient = _
+
+    /**
+      * The client capabilities.
+      * Will be set during the initialization.
+      */
+    private var clientCapabilities: ClientCapabilities = _
+
+    private val flixTextDocumentService = new FlixTextDocumentService(this, flixLanguageClient)
+    private val flixWorkspaceService = new FlixWorkspaceService(this, flixLanguageClient)
+
+    /**
+      * Initializes the language server.
+      *
+      * During the initialization, we should:
+      * - Store the client capabilities.
+      * - Return the server capabilities.
+      */
     override def initialize(initializeParams: InitializeParams): CompletableFuture[InitializeResult] = {
       System.err.println(s"initialize: $initializeParams")
 
-      val capabilities = new ServerCapabilities
-      capabilities.setHoverProvider(true)
-      CompletableFuture.completedFuture(new InitializeResult(capabilities))
+      clientCapabilities = initializeParams.getCapabilities
+
+      val serverCapabilities = new ServerCapabilities
+      serverCapabilities.setHoverProvider(true)
+      serverCapabilities.setTextDocumentSync(TextDocumentSyncKind.Full)
+
+      CompletableFuture.completedFuture(new InitializeResult(serverCapabilities))
     }
 
     override def shutdown(): CompletableFuture[AnyRef] = {
@@ -55,14 +100,35 @@ object LspServer {
       System.err.println("exit")
     }
 
+    override def connect(client: LanguageClient): Unit = {
+      System.err.println("connect to the client")
+      flixLanguageClient = client
+    }
+
     override def getTextDocumentService: TextDocumentService = flixTextDocumentService
 
     override def getWorkspaceService: WorkspaceService = flixWorkspaceService
+
+    /**
+      * Adds the given source code to the Flix instance.
+      */
+    def addSourceCode(uri: String, src: String): Unit = {
+      flix.addSourceCode(uri, src)(SecurityContext.AllPermissions)
+      sources.put(uri, src)
+    }
   }
 
-  private class FlixTextDocumentService extends TextDocumentService {
+  private class FlixTextDocumentService(flixLanguageServer: FlixLanguageServer, flixLanguageClient: LanguageClient) extends TextDocumentService {
+    /**
+      * Called when a text document is opened.
+      * If the document is a Flix source file, we add the source code to the Flix instance.
+      */
     override def didOpen(didOpenTextDocumentParams: DidOpenTextDocumentParams): Unit = {
       System.err.println(s"didOpen: $didOpenTextDocumentParams")
+      val textDocument = didOpenTextDocumentParams.getTextDocument
+      if (textDocument.getLanguageId == "flix") {
+        flixLanguageServer.addSourceCode(textDocument.getUri, textDocument.getText)
+      }
     }
 
     override def didChange(didChangeTextDocumentParams: DidChangeTextDocumentParams): Unit = {
@@ -77,6 +143,11 @@ object LspServer {
       System.err.println(s"didSaveTextDocumentParams: $didSaveTextDocumentParams")
     }
 
+    /**
+      * Returns the hover information for the given position in the given document.
+      *
+      * Now a mock implementation that just returns a simple greeting.
+      */
     override def hover(params: HoverParams): CompletableFuture[Hover] = {
       System.err.println(s"hover: $params")
 
@@ -85,7 +156,7 @@ object LspServer {
     }
   }
 
-  private class FlixWorkspaceService extends WorkspaceService {
+  private class FlixWorkspaceService(flixLanguageServer: FlixLanguageServer, flixLanguageClient: LanguageClient) extends WorkspaceService {
     override def didChangeConfiguration(didChangeConfigurationParams: DidChangeConfigurationParams): Unit = {
       System.err.println(s"didChangeConfiguration: $didChangeConfigurationParams")
     }
