8fbdb9ae6744be64313cf634ab6a8b54a630c8b3
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2025-01-23 15:30:22 +0100

    refactor: add helper functions

--- main/test/ca/uwaterloo/flix/api/EffectLockSuite.scala
+++ main/test/ca/uwaterloo/flix/api/EffectLockSuite.scala
@@ -1,5 +1,6 @@
 package ca.uwaterloo.flix.api
 
+import ca.uwaterloo.flix.language.ast.TypedAst
 import ca.uwaterloo.flix.language.ast.shared.SecurityContext
 import ca.uwaterloo.flix.util.Options
 import org.scalatest.funsuite.AnyFunSuite
@@ -20,10 +21,20 @@ class EffectLockSuite extends AnyFunSuite {
         |
       """.stripMargin
     }
-    implicit val sctx: SecurityContext = SecurityContext.AllPermissions
-    implicit val flix: Flix = new Flix().setOptions(Options.TestWithLibNix).addSourceCode("<test>", input)
-    val result = flix.effectLock().get.defs.keys.map(_.text)
+    val result = reachableDefs(checkEffectLock(input))
     val expected = Set("main", "f", "g")
     assert(result == expected)
+  }
+
+  private def checkEffectLock(input: String): Option[TypedAst.Root] = {
+    implicit val sctx: SecurityContext = SecurityContext.AllPermissions
+    implicit val flix: Flix = new Flix().setOptions(Options.TestWithLibNix).addSourceCode("<test>", input)
+    val result = flix.effectLock()
+    result
+  }
+
+  private def reachableDefs(root: Option[TypedAst.Root]): Set[String] = root match {
+    case None => Set.empty
+    case Some(r) => r.defs.keys.map(_.text).toSet
   }
 }
