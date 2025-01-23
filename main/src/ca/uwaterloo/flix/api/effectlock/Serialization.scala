ec45908d245418f333be28784a97409360c863d1
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2025-01-23 17:29:24 +0100

    feat: add serializable types

--- main/src/ca/uwaterloo/flix/api/effectlock/Serialization.scala
+++ main/src/ca/uwaterloo/flix/api/effectlock/Serialization.scala
@@ -16,12 +16,135 @@ object Serialization {
 
   case class SerializableFunction(name: String, tpe: SerializableType) // TODO: Maybe not String for name field
 
-  sealed trait SerializableType {
-    def kind: SerializableKind
-  }
+  sealed trait SerializableType
 
   object SerializableType {
-    case class Var(sym: SerializableSymbol.KindedTypeVarSym)
+    case class Var(sym: SerializableSymbol.KindedTypeVarSym) extends SerializableType
+
+    case class Cst(tc: SerializableTypeConstructor) extends SerializableType
+
+    case class Apply(tpe1: SerializableType, tpe2: SerializableType) extends SerializableType
+
+    // case class Alias(symUse: TypeAliasSymUse, args: List[SerializableType], tpe: SerializableType) extends Type
+
+    // case class AssocType(symUse: AssocTypeSymUse, arg: SerializableType, kind: SerializableKind) extends SerializableType
+
+    case class JvmToType(tpe: SerializableType) extends SerializableType
+
+    case class JvmToEff(tpe: SerializableType) extends SerializableType
+
+    case class UnresolvedJvmType(member: JvmMember) extends SerializableType
+
+    sealed trait JvmMember
+
+    object JvmMember {
+    }
+
+  }
+
+  sealed trait SerializableTypeConstructor
+
+  object SerializableTypeConstructor {
+    case object Void extends SerializableTypeConstructor
+
+    case object Unit extends SerializableTypeConstructor
+
+    case object Null extends SerializableTypeConstructor
+
+    case object Bool extends SerializableTypeConstructor
+
+    case object Char extends SerializableTypeConstructor
+
+    case object Float32 extends SerializableTypeConstructor
+
+    case object Float64 extends SerializableTypeConstructor
+
+    case object BigDecimal extends SerializableTypeConstructor
+
+    case object Int8 extends SerializableTypeConstructor
+
+    case object Int16 extends SerializableTypeConstructor
+
+    case object Int32 extends SerializableTypeConstructor
+
+    case object Int64 extends SerializableTypeConstructor
+
+    case object BigInt extends SerializableTypeConstructor
+
+    case object Str extends SerializableTypeConstructor
+
+    case object Regex extends SerializableTypeConstructor
+
+    case class Arrow(arity: Int) extends SerializableTypeConstructor
+
+    case class ArrowWithoutEffect(arity: Int) extends SerializableTypeConstructor
+
+    case object RecordRowEmpty extends SerializableTypeConstructor
+
+    // case class RecordRowExtend(label: SerializableName.Label) extends SerializableTypeConstructor
+
+    case object Record extends SerializableTypeConstructor
+
+    case object SchemaRowEmpty extends SerializableTypeConstructor
+
+    // case class SchemaRowExtend(pred: SerializableName.Pred) extends SerializableTypeConstructor
+
+    case object Schema extends SerializableTypeConstructor
+
+    case object Lazy extends SerializableTypeConstructor
+
+    // case class Enum(sym: Symbol.EnumSym, kind: SerializableKind) extends SerializableTypeConstructor
+    // case class Struct(sym: Symbol.StructSym, kind: SerializableKind) extends SerializableTypeConstructor
+    // case class RestrictableEnum(sym: Symbol.RestrictableEnumSym, kind: SerializableKind) extends SerializableTypeConstructor
+    // case class Native(clazz: Class[?]) extends SerializableTypeConstructor
+    // case class JvmConstructor(constructor: Constructor[?]) extends SerializableTypeConstructor
+    // case class JvmMethod(method: Method) extends SerializableTypeConstructor
+    // case class JvmField(field: Field) extends SerializableTypeConstructor
+
+    case object Array extends SerializableTypeConstructor
+
+    case object Vector extends SerializableTypeConstructor
+
+    case class Tuple(l: Int) extends SerializableTypeConstructor
+
+    case object Relation extends SerializableTypeConstructor
+
+    case object Lattice extends SerializableTypeConstructor
+
+    case object True extends SerializableTypeConstructor
+
+    case object False extends SerializableTypeConstructor
+
+    case object Not extends SerializableTypeConstructor
+
+    case object And extends SerializableTypeConstructor
+
+    case object Or extends SerializableTypeConstructor
+
+    case object Pure extends SerializableTypeConstructor
+
+    case object Univ extends SerializableTypeConstructor
+
+    case object Complement extends SerializableTypeConstructor
+
+    case object Union extends SerializableTypeConstructor
+
+    case object Intersection extends SerializableTypeConstructor
+
+    case object Difference extends SerializableTypeConstructor
+
+    case object SymmetricDiff extends SerializableTypeConstructor
+
+    // case class Effect(sym: Symbol.EffectSym) extends SerializableTypeConstructor
+    // case class CaseComplement(sym: Symbol.RestrictableEnumSym) extends SerializableTypeConstructor
+    // case class CaseUnion(sym: Symbol.RestrictableEnumSym) extends SerializableTypeConstructor
+    // case class CaseIntersection(sym: Symbol.RestrictableEnumSym) extends SerializableTypeConstructor
+    // case class CaseSet(syms: SortedSet[Symbol.RestrictableCaseSym], enumSym: Symbol.RestrictableEnumSym) extends SerializableTypeConstructor
+
+    case object RegionToStar extends SerializableTypeConstructor
+
+    case class Error(id: Int, kind: SerializableKind) extends SerializableTypeConstructor
+
   }
 
   sealed trait SerializableKind
@@ -57,7 +180,7 @@ object Serialization {
   sealed trait SerializableName
 
   object SerializableName {
-    case class Ident(name: String)
+    case class Ident(name: String) extends SerializableName
   }
 
   sealed trait SerializableSymbol
