1b14b3b7d5f48b5e8d314d1a8fcbe58d9dcb98d4
stormckey <chenhaogao123@gmail.com>
2025-01-27 19:26:48 +0800

    feat: add partition and merge api to ChangeSet (#9760)

--- main/src/ca/uwaterloo/flix/language/ast/ChangeSet.scala
+++ main/src/ca/uwaterloo/flix/language/ast/ChangeSet.scala
@@ -80,6 +80,23 @@ sealed trait ChangeSet {
           (stale + (k -> v), fresh)
       }
   }
+
+
+  /**
+    * Updates the stale part of the map with the given function `f`.
+    */
+  def updateStaleValues[K <: Sourceable, V](newMap: Map[K, V], oldMap: Map[K, V], f: Map[K, V] => Map[K, V]): Map[K, V] = {
+    val (stale, fresh) = partition(newMap, oldMap)
+    fresh ++ stale
+  }
+
+  /**
+    * Updates the stale part of the list map with the given function `f`.
+    */
+  def updateStaleValueLists[K, V <: Sourceable](newMap: ListMap[K, V], oldMap: ListMap[K, V], f: ListMap[K, V] => ListMap[K, V]): ListMap[K, V] = {
+    val (stale, fresh) = partitionOnValues(newMap, oldMap)
+    fresh ++ f(stale)
+  }
 }
 
 object ChangeSet {
