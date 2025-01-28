f5d24cd0cebc5a1111b443a41ab4404d3d0fb97d
stormckey <chenhaogao123@gmail.com>
2025-01-28 15:30:22 +0100

    feat: make PatMatch incremental

--- main/src/ca/uwaterloo/flix/api/Flix.scala
+++ main/src/ca/uwaterloo/flix/api/Flix.scala
@@ -586,7 +586,7 @@ class Flix {
             val (afterStratifier, stratificationErrors) = Stratifier.run(afterPredDeps)
             errors ++= stratificationErrors
 
-            val (afterPatMatch, patMatchErrors) = PatMatch.run(afterStratifier)
+            val (afterPatMatch, patMatchErrors) = PatMatch.run(afterStratifier, cachedTyperAst, changeSet)
             errors ++= patMatchErrors
 
             val (afterRedundancy, redundancyErrors) = Redundancy.run(afterPatMatch)
