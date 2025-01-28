5340d9b7adf3f7cf0ce25b9860e3ba2015830e1c
Matthew Lutze <mlutze@users.noreply.github.com>
2025-01-28 15:14:00 +0100

    fix thing that IntelliJ broke

--- main/src/ca/uwaterloo/flix/language/ast/Scheme.scala
+++ main/src/ca/uwaterloo/flix/language/ast/Scheme.scala
@@ -20,6 +20,7 @@ import ca.uwaterloo.flix.api.Flix
 import ca.uwaterloo.flix.language.ast.shared.*
 import ca.uwaterloo.flix.language.fmt.{FormatOptions, FormatScheme}
 import ca.uwaterloo.flix.language.phase.typer.*
+import ca.uwaterloo.flix.language.phase.typer.TypeConstraint2.Provenance
 import ca.uwaterloo.flix.language.phase.unification.{EqualityEnvironment, Substitution, TraitEnv}
 import ca.uwaterloo.flix.util.InternalCompilerException
 import ca.uwaterloo.flix.util.collection.ListMap
