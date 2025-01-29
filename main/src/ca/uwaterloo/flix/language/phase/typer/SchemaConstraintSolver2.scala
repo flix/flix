953c92248bf42f7017a0773abe7c480f236c0b6b
Matthew Lutze <mlutze@users.noreply.github.com>
2025-01-29 09:22:21 +0100

    refactor: consolidate TypeConstraint and TypeConstraint2

--- main/src/ca/uwaterloo/flix/language/phase/typer/SchemaConstraintSolver2.scala
+++ main/src/ca/uwaterloo/flix/language/phase/typer/SchemaConstraintSolver2.scala
@@ -17,8 +17,8 @@ package ca.uwaterloo.flix.language.phase.typer
 
 import ca.uwaterloo.flix.api.Flix
 import ca.uwaterloo.flix.language.ast.shared.Scope
-import ca.uwaterloo.flix.language.ast.{Kind, Name, RigidityEnv, SourceLocation, Symbol, Type, TypeConstructor}
-import ca.uwaterloo.flix.language.phase.typer.TypeConstraint2.Provenance
+import ca.uwaterloo.flix.language.ast.{Kind, Name, RigidityEnv, Symbol, Type, TypeConstructor}
+import ca.uwaterloo.flix.language.phase.typer.TypeConstraint.Provenance
 import ca.uwaterloo.flix.language.phase.unification.Substitution
 import ca.uwaterloo.flix.util.InternalCompilerException
 
@@ -30,7 +30,7 @@ object SchemaConstraintSolver2 {
   /**
     * Unifies the two given schema row types.
     */
-  def solve(tpe1: Type, tpe2: Type, scope: Scope, renv: RigidityEnv, prov: Provenance, loc: SourceLocation)(implicit progress: Progress, flix: Flix): (List[TypeConstraint2], Substitution) = (tpe1, tpe2) match {
+  def solve(tpe1: Type, tpe2: Type, scope: Scope, renv: RigidityEnv, prov: Provenance)(implicit progress: Progress, flix: Flix): (List[TypeConstraint], Substitution) = (tpe1, tpe2) match {
 
     // ----------
     // ρ ~ ρ => ∅
@@ -58,7 +58,7 @@ object SchemaConstraintSolver2 {
     // ( ℓ : τ₁  | ρ₁ ) ~ ( ℓ : τ₂  | ρ₂ )  =>  { τ₁ ~ τ₂, ρ₁ ~ ρ₂ }
     case (Type.Apply(Type.Apply(Type.Cst(TypeConstructor.SchemaRowExtend(label1), _), t1, _), rest1, _), Type.Apply(Type.Apply(Type.Cst(TypeConstructor.SchemaRowExtend(label2), _), t2, _), rest2, _)) if label1 == label2 =>
       progress.markProgress()
-      (List(TypeConstraint2.Equality(t1, t2, prov, loc), TypeConstraint2.Equality(rest1, rest2, prov, loc)), Substitution.empty)
+      (List(TypeConstraint.Equality(t1, t2, prov), TypeConstraint.Equality(rest1, rest2, prov)), Substitution.empty)
 
     // If labels do not match, then we pivot the right row to make them match.
     //
@@ -69,16 +69,16 @@ object SchemaConstraintSolver2 {
       pivot(r2, label, t1, r1.typeVars.map(_.sym))(scope, renv, flix) match {
         case Some((Type.Apply(Type.Apply(_, t3, _), rest3, _), subst)) =>
           progress.markProgress()
-          (List(TypeConstraint2.Equality(t1, t3, prov, loc), TypeConstraint2.Equality(rest1, rest3, prov, loc)), subst)
+          (List(TypeConstraint.Equality(t1, t3, prov), TypeConstraint.Equality(rest1, rest3, prov)), subst)
 
         case None =>
-          (List(TypeConstraint2.Equality(tpe1, tpe2, prov, loc)), Substitution.empty)
+          (List(TypeConstraint.Equality(tpe1, tpe2, prov)), Substitution.empty)
 
         case Some((t, _)) => throw InternalCompilerException("unexpected result from pivot: " + t, t.loc)
       }
 
     // If nothing matches, we give up and return the constraints as we got them.
-    case _ => (List(TypeConstraint2.Equality(tpe1, tpe2, prov, loc)), Substitution.empty)
+    case _ => (List(TypeConstraint.Equality(tpe1, tpe2, prov)), Substitution.empty)
   }
 
   /**
