9eed801a4741734f18e4e8c06ec08babd3412795
Matthew Lutze <mlutze@users.noreply.github.com>
2025-01-30 13:13:06 +0100

    chore: remove dead code from EqualityEnvironment

--- main/src/ca/uwaterloo/flix/language/phase/unification/EqualityEnvironment.scala
+++ main/src/ca/uwaterloo/flix/language/phase/unification/EqualityEnvironment.scala
@@ -15,15 +15,11 @@
  */
 package ca.uwaterloo.flix.language.phase.unification
 
-import ca.uwaterloo.flix.api.Flix
 import ca.uwaterloo.flix.language.ast.shared.*
-import ca.uwaterloo.flix.language.ast.shared.SymUse.AssocTypeSymUse
-import ca.uwaterloo.flix.language.ast.{Kind, RigidityEnv, SourceLocation, Symbol, Type}
-import ca.uwaterloo.flix.util.collection.ListMap
-import ca.uwaterloo.flix.util.{InternalCompilerException, Result, Validation}
+import ca.uwaterloo.flix.language.ast.{SourceLocation, Type}
+import ca.uwaterloo.flix.util.InternalCompilerException
 
 object EqualityEnvironment {
-
   /**
     * Converts the given EqualityConstraint into a BroadEqualityConstraint.
     */
@@ -39,105 +35,5 @@ object EqualityEnvironment {
   def broaden(econstr: EqualityConstraint): BroadEqualityConstraint = econstr match {
     case EqualityConstraint(cst, tpe1, tpe2, loc) =>
       BroadEqualityConstraint(Type.AssocType(cst, tpe1, tpe2.kind, loc), tpe2)
-  }
-
-  /**
-    * Reduces the associated type in the equality environment.
-    *
-    * Only performs one reduction step. The result may itself contain associated types.
-    */
-  def reduceAssocTypeStep(symUse: AssocTypeSymUse, arg: Type, eqEnv: ListMap[Symbol.AssocTypeSym, AssocTypeDef])(implicit scope: Scope, flix: Flix): Result[Type, UnificationError] = {
-    val renv = arg.typeVars.map(_.sym).foldLeft(RigidityEnv.empty)(_.markRigid(_))
-    val insts = eqEnv(symUse.sym)
-    insts.iterator.flatMap { // TODO ASSOC-TYPES generalize this pattern (also in monomorph)
-      inst =>
-        Unification.unifyTypesIgnoreLeftoverAssocs(arg, inst.arg, renv, eqEnv).map {
-          case subst => subst(inst.ret) // TODO ASSOC-TYPES consider econstrs
-        }
-    }.nextOption() match {
-      case None => Result.Err(UnificationError.IrreducibleAssocType(symUse.sym, arg))
-      case Some(t) => Result.Ok(t)
-    }
-  }
-
-  /**
-    * Fully reduces the given associated type.
-    */
-  def reduceAssocType(symUse: AssocTypeSymUse, arg: Type, eqEnv: ListMap[Symbol.AssocTypeSym, AssocTypeDef])(implicit scope: Scope, flix: Flix): Result[Type, UnificationError] = {
-    for {
-      tpe <- reduceAssocTypeStep(symUse, arg, eqEnv)
-      res <- reduceType(tpe, eqEnv)
-    } yield res
-  }
-
-  /**
-    * Reduces associated types in the equality environment.
-    */
-  def reduceType(t0: Type, eqEnv: ListMap[Symbol.AssocTypeSym, AssocTypeDef])(implicit scope: Scope, flix: Flix): Result[Type, UnificationError] = {
-    // TODO ASSOC-TYPE require that AssocTypeDefs which themselves include assoc types are supported by tconstrs
-    def visit(t: Type): Result[Type, UnificationError] = t match {
-      case t: Type.Var => Result.Ok(t)
-      case t: Type.Cst => Result.Ok(t)
-      case Type.Apply(tpe1, tpe2, loc) =>
-        visit(tpe1)
-        for {
-          t1 <- visit(tpe1)
-          t2 <- visit(tpe2)
-        } yield Type.Apply(t1, t2, loc)
-      case Type.Alias(cst, args0, tpe0, loc) =>
-        for {
-          args <- Result.traverse(args0)(visit)
-          tpe <- visit(tpe0)
-        } yield Type.Alias(cst, args, tpe, loc)
-      case Type.AssocType(cst, arg0, kind, loc) =>
-        for {
-          arg <- visit(arg0)
-          res0 <- reduceAssocTypeStep(cst, arg, eqEnv)
-          res <- visit(res0)
-        } yield res
-      case Type.JvmToType(tpe, loc) =>
-        for {
-          t1 <- visit(tpe)
-        } yield Type.JvmToType(t1, loc)
-      case Type.JvmToEff(tpe, loc) =>
-        for {
-          t1 <- visit(tpe)
-        } yield Type.JvmToEff(t1, loc)
-      case Type.UnresolvedJvmType(member0, loc) =>
-        for {
-          member <- traverse(member0)(visit)
-        } yield Type.UnresolvedJvmType(member, loc)
-    }
-
-    visit(t0)
-  }
-
-  /**
-    * Transforms `mem` by executing `f` on all the types in `this`.
-    *
-    * If `f` returns `Err` for any call, this function returns `Err`.
-    */
-  // TODO CONSTR-SOLVER-2 remove this after we migrate to the new constraint solver
-  private def traverse[E](mem: Type.JvmMember)(f: Type => Result[Type, E]): Result[Type.JvmMember, E] = mem match {
-    case Type.JvmMember.JvmConstructor(clazz, tpes0) =>
-      for {
-        tpes <- Result.traverse(tpes0)(f)
-      } yield Type.JvmMember.JvmConstructor(clazz, tpes)
-
-    case Type.JvmMember.JvmField(base, tpe0, name) =>
-      for {
-        tpe <- f(tpe0)
-      } yield Type.JvmMember.JvmField(base, tpe, name)
-
-    case Type.JvmMember.JvmMethod(tpe0, name, tpes0) =>
-      for {
-        tpe <- f(tpe0)
-        tpes <- Result.traverse(tpes0)(f)
-      } yield Type.JvmMember.JvmMethod(tpe, name, tpes)
-
-    case Type.JvmMember.JvmStaticMethod(clazz, name, tpes0) =>
-      for {
-        tpes <- Result.traverse(tpes0)(f)
-      } yield Type.JvmMember.JvmStaticMethod(clazz, name, tpes)
   }
 }
