d5e276196b0598384931ae17bd0d23ce3feea93b
Matthew Lutze <mlutze@users.noreply.github.com>
2025-01-31 08:27:10 +0100

    refactor: return Option from CaseSetUnification

--- main/test/ca/uwaterloo/flix/language/phase/unification/TestCaseSetUnification.scala
+++ main/test/ca/uwaterloo/flix/language/phase/unification/TestCaseSetUnification.scala
@@ -20,7 +20,6 @@ import ca.uwaterloo.flix.TestUtils
 import ca.uwaterloo.flix.api.Flix
 import ca.uwaterloo.flix.language.ast.shared.{Scope, VarText}
 import ca.uwaterloo.flix.language.ast.{Kind, Name, RigidityEnv, SourceLocation, Symbol, Type, TypeConstructor}
-import ca.uwaterloo.flix.util.Result
 import org.scalatest.funsuite.AnyFunSuite
 
 import scala.collection.immutable.SortedSet
@@ -410,16 +409,11 @@ class TestCaseSetUnification extends AnyFunSuite with TestUtils {
   }
 
   private def assertUnifies(tpe1: Type, tpe2: Type, renv: RigidityEnv, enumSym: Symbol.RestrictableEnumSym): Unit = {
-    assert(isOk(CaseSetUnification.unify(tpe1, tpe2, renv, enumSym.universe, enumSym)))
+    assert(CaseSetUnification.unify(tpe1, tpe2, renv, enumSym.universe, enumSym).isDefined)
   }
 
   private def assertDoesNotUnify(tpe1: Type, tpe2: Type, renv: RigidityEnv, enumSym: Symbol.RestrictableEnumSym): Unit = {
-    assert(!isOk(CaseSetUnification.unify(tpe1, tpe2, renv, enumSym.universe, enumSym)))
-  }
-
-  private def isOk[T, E](r: Result[T, E]) = r match {
-    case Result.Ok(_) => true
-    case Result.Err(_) => false
+    assert(CaseSetUnification.unify(tpe1, tpe2, renv, enumSym.universe, enumSym).isEmpty)
   }
 
 }
