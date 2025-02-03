eb2cc24a42df030cbbbe4acea99f6bd33c112e13
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2025-02-03 12:17:51 +0100

    refactor: remove option

--- main/src/ca/uwaterloo/flix/api/Bootstrap.scala
+++ main/src/ca/uwaterloo/flix/api/Bootstrap.scala
@@ -850,52 +850,46 @@ class Bootstrap(val projectPath: Path, apiKey: Option[String]) {
     out.println(s"Reading from file: ${path.getFileName.toString}")
     // TODO: Add error handling
     val json = Files.readString(path) // TODO: Check that it is readable and exists. Add this as method to FileOps
-    val deserde = Serialization.deserialize(json)
+    val Some(deserde) = Serialization.deserialize(json)
     validateLibs(flix, out, deserde)
   }
 
-  private def validateLibs(flix: Flix, out: PrintStream, deserde: Option[Map[Library, NamedTypeSchemes]]): Validation[Unit, BootstrapError] = deserde match {
-    case Some(lockedSignatures) =>
-      out.println("Lock file read. Checking upgrade safety")
-      Validation.flatMapN(check(flix)) {
-        case root =>
-          val result = Validation.traverse(root.defs) {
-            case (sym, defn) =>
-              sym.loc.sp1.source.input match {
-                case Input.Unknown | Input.PkgFile(_, _) | Input.Text(_, _, _) | Input.TxtFile(_, _) => Validation.Success(())
-                case Input.FileInPackage(path, _, _, _) =>
-                  val name = path.getFileName.toString
-                  lockedSignatures.get(name) match {
-                    case None => Validation.Success(())
-                    case Some(signatures) =>
-                      val matchingSigs = signatures.find {
-                        case (sym1, _) =>
-                          out.println(s"debug from json: $sym1")
-                          sym.namespace == sym1.namespace && sym.text == sym1.text
-                      }
-                      matchingSigs match {
-                        case None => Validation.Success(())
-                        case Some((_, originalScheme)) =>
-                          val newScheme = defn.spec.declaredScheme
-                          val isSafe = EffectLock.isSafe2(originalScheme, newScheme)
+  private def validateLibs(flix: Flix, out: PrintStream, lockedSignatures: Map[Library, NamedTypeSchemes]): Validation[Unit, BootstrapError] = {
+    out.println("Lock file read. Checking upgrade safety")
+    Validation.flatMapN(check(flix)) {
+      case root =>
+        val result = Validation.traverse(root.defs) {
+          case (sym, defn) =>
+            sym.loc.sp1.source.input match {
+              case Input.Unknown | Input.PkgFile(_, _) | Input.Text(_, _, _) | Input.TxtFile(_, _) => Validation.Success(())
+              case Input.FileInPackage(path, _, _, _) =>
+                val name = path.getFileName.toString
+                lockedSignatures.get(name) match {
+                  case None => Validation.Success(())
+                  case Some(signatures) =>
+                    val matchingSigs = signatures.find {
+                      case (sym1, _) =>
+                        out.println(s"debug from json: $sym1")
+                        sym.namespace == sym1.namespace && sym.text == sym1.text
+                    }
+                    matchingSigs match {
+                      case None => Validation.Success(())
+                      case Some((_, originalScheme)) =>
+                        val newScheme = defn.spec.declaredScheme
+                        val isSafe = EffectLock.isSafe2(originalScheme, newScheme)
 
-                          if (isSafe) {
-                            out.println("Upgrade is valid")
-                            Validation.Success(())
-                          } else {
-                            out.println(s"$sym is a bad upgrade")
-                            Validation.Failure(BootstrapError.EffectUpgradeError(sym, originalScheme, newScheme))
-                          }
-                      }
-                  }
-              }
-          }
-          Validation.mapN(result)(_ => ())
-      }
-
-    case None =>
-      val error = BootstrapError.FileError("malformed effect lock file")
-      Validation.Failure(error)
-    // TODO: Add error handling (maybe return Validation or Result in Serialization.deserialize)
+                        if (isSafe) {
+                          out.println("Upgrade is valid")
+                          Validation.Success(())
+                        } else {
+                          out.println(s"$sym is a bad upgrade")
+                          Validation.Failure(BootstrapError.EffectUpgradeError(sym, originalScheme, newScheme))
+                        }
+                    }
+                }
+            }
+        }
+        Validation.mapN(result)(_ => ())
+    }
   }
 }
