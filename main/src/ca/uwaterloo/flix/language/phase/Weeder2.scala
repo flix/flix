3dba2213149c546d1687c668d616e8839a859369
stormckey <chenhaogao123@gmail.com>
2025-02-04 19:23:10 +0100

    more precise loc

--- main/src/ca/uwaterloo/flix/language/phase/Weeder2.scala
+++ main/src/ca/uwaterloo/flix/language/phase/Weeder2.scala
@@ -1784,23 +1784,11 @@ object Weeder2 {
       expect(tree, TreeKind.Expr.TryCatchRuleFragment)
       mapN(pickNameIdent(tree), pickQName(tree), pickExpr(tree)) {
         case (ident, qname, expr) if qname.isUnqualified => CatchRule(ident, qname.ident, expr)
-        case _ =>
-          val error = IllegalQualifiedName(tree.loc)
+        case (_, qname, _) =>
+          val error = IllegalQualifiedName(qname.loc)
           sctx.errors.add(error)
           CatchRule(Name.Ident("error", tree.loc), Name.Ident("error", tree.loc), Expr.Error(error))
       }
-//      pickAll(TreeKind.Ident, tree).map(tokenToIdent) match {
-//        case ident :: className :: Nil =>
-//          println("first case")
-//          mapN(pickExpr(tree)){
-//            expr => CatchRule(ident, className, expr)
-//          }
-//        case _ =>
-//          println("second case")
-//          val error = Malformed(NamedTokenSet.CatchRule, SyntacticContext.Expr.OtherExpr, loc = tree.loc)
-//          sctx.errors.add(error)
-//          Validation.Success(CatchRule(Name.Ident("error", tree.loc), Name.Ident("error", tree.loc), Expr.Error(error)))
-//      }
     }
 
     private def visitTryWithBody(tree: Tree)(implicit sctx: SharedContext): Validation[Expr, CompilationMessage] = {
