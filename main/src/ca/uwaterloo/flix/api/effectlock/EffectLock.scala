749c96b8cb07e55108ee67b20cf0896bae23924c
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2025-02-04 15:52:33 +0100

    feat: add handling of covariant effect downgrade

--- main/src/ca/uwaterloo/flix/api/effectlock/EffectLock.scala
+++ main/src/ca/uwaterloo/flix/api/effectlock/EffectLock.scala
@@ -41,59 +41,27 @@ object EffectLock {
     *
     */
   private def isSubset(tpe1: Type, tpe2: Type): Boolean = {
-    // 1. Types match t1 -> t2
-    val isMatchingArgs = tpe1.arrowArgTypes == tpe2.arrowArgTypes
-    val isMatchingResult = tpe1.arrowResultType == tpe2.arrowResultType
-
     // TODO: What about type variables? Alpha equivalence
+    // 1. Types match t1 -> t2
+    (tpe1.typeConstructor, tpe2.typeConstructor) match {
+      case (Some(TypeConstructor.Arrow(_)), Some(TypeConstructor.Arrow(_))) => ()
+      case _ => return false
+    }
+    val isMatchingArgs = tpe1.arrowArgTypes == tpe2.arrowArgTypes
+    val tpe1Res = tpe1.arrowResultType
+    val tpe2Res = tpe2.arrowResultType
+    val isMatchingResultTypes = (tpe1.arrowResultType.typeConstructor, tpe2.arrowResultType.typeConstructor) match {
+      case (Some(TypeConstructor.Arrow(_)), Some(TypeConstructor.Arrow(_))) => isSubset(tpe1Res, tpe2Res)
+      case (t1, t2) => t1 == t2
+    }
+
 
     // 2. Boolean unification of effects phi + phi' = phi'
     val sc1Effs = tpe1.arrowEffectType
     val sc2Effs = tpe2.arrowEffectType
     val left = Type.mkUnion(sc1Effs, sc2Effs, sc1Effs.loc)
     implicit val flix: Flix = new Flix().setOptions(Options.Default)
-    val (solution, subst) = EffUnification3.unifyAll(List((left, sc2Effs, sc2Effs.loc)), Scope.Top, RigidityEnv.empty)
-    println("new")
-    println(s"sc1.base.arrowArgTypes: ${tpe1.arrowArgTypes}")
-    println(s"sc1.base.arrowResultType: ${tpe1.arrowResultType}")
-    println(s"sc1.base.arrowEffectType: ${tpe1.arrowEffectType}")
-    println("old")
-    println(s"sc2.base.arrowArgTypes: ${tpe2.arrowArgTypes}")
-    println(s"sc2.base.arrowResultType: ${tpe2.arrowResultType}")
-    println(s"sc2.base.arrowEffectType: ${tpe2.arrowEffectType}")
-    println("---------------")
-    println(solution)
-    isMatchingArgs && isMatchingResult && solution.isEmpty
-  }
-
-
-  private def monomorphicDowngrade(sc1: Scheme, sc2: Scheme): Boolean = {
-    val noQuantifiers = sc1.quantifiers.isEmpty && sc2.quantifiers.isEmpty
-    val sameMonomorphicType = checkSameMonomorphicType(sc1.base, sc2.base)
-    val subsetOfEffects1 = sc1.base.arrowEffectType.effects.subsetOf(sc2.base.arrowEffectType.effects)
-    val subsetOfEffects2 = sc1.base.arrowResultType.effects.subsetOf(sc2.base.arrowResultType.effects)
-    noQuantifiers && sameMonomorphicType && subsetOfEffects1 && subsetOfEffects2
-  }
-
-  private def checkSameMonomorphicType(tpe1: Type, tpe2: Type): Boolean = (tpe1, tpe2) match {
-    // Ignore effect cases
-    case (Type.Cst(TypeConstructor.Effect(_), _), _) => true
-    case (Type.Cst(TypeConstructor.Pure, _), _) => true
-    case (_, Type.Cst(TypeConstructor.Effect(_), _)) => true
-    case (_, Type.Cst(TypeConstructor.Pure, _)) => true
-
-    case (Type.Cst(tc1, _), Type.Cst(tc2, _)) =>
-      tc1 == tc2
-
-    case (Type.Apply(tpe11, tpe12, _), Type.Apply(tpe21, tpe22, _)) =>
-      checkSameMonomorphicType(tpe11, tpe21) && checkSameMonomorphicType(tpe12, tpe22)
-
-    case (Type.Alias(symUse1, args1, tpe1, _), Type.Alias(symUse2, args2, tpe2, _)) =>
-      args1.zip(args2).forall {
-        case (t1, t2) => checkSameMonomorphicType(t1, t2)
-      } && checkSameMonomorphicType(tpe1, tpe2)
-
-    case (Type.AssocType(symUse1, arg1, kind1, _), Type.AssocType(symUse2, arg2, kind2, _)) => ???
-    case _ => false
+    val (unsolvedConstraints, _) = EffUnification3.unifyAll(List((left, sc2Effs, sc2Effs.loc)), Scope.Top, RigidityEnv.empty)
+    isMatchingArgs && isMatchingResultTypes && unsolvedConstraints.isEmpty
   }
 }
