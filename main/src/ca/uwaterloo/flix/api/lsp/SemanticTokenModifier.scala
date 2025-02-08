ed89defb76683e10873ea2e7533d42808bfa4ad1
stormckey <chenhaogao123@gmail.com>
2025-02-08 17:25:10 +0800

    feat: use the full SemanticToken legend (#9896)

--- main/src/ca/uwaterloo/flix/api/lsp/SemanticTokenModifier.scala
+++ main/src/ca/uwaterloo/flix/api/lsp/SemanticTokenModifier.scala
@@ -27,6 +27,14 @@ sealed trait SemanticTokenModifier {
   def toInt: Int = this match {
     case SemanticTokenModifier.Declaration => 0
     case SemanticTokenModifier.Definition => 1
+    case SemanticTokenModifier.Readonly => 2
+    case SemanticTokenModifier.Static => 3
+    case SemanticTokenModifier.Deprecated => 4
+    case SemanticTokenModifier.Abstract => 5
+    case SemanticTokenModifier.Async => 6
+    case SemanticTokenModifier.Modification => 7
+    case SemanticTokenModifier.Documentation => 8
+    case SemanticTokenModifier.DefaultLibrary => 9
   }
 }
 
@@ -34,5 +42,33 @@ object SemanticTokenModifier {
   case object Declaration extends SemanticTokenModifier
 
   case object Definition extends SemanticTokenModifier
-}
 
+  case object Readonly extends SemanticTokenModifier
+
+  case object Static extends SemanticTokenModifier
+
+  case object Deprecated extends SemanticTokenModifier
+
+  case object Abstract extends SemanticTokenModifier
+
+  case object Async extends SemanticTokenModifier
+
+  case object Modification extends SemanticTokenModifier
+
+  case object Documentation extends SemanticTokenModifier
+
+  case object DefaultLibrary extends SemanticTokenModifier
+
+  def getWholeList: List[String] = List(
+    "declaration",
+    "definition",
+    "readonly",
+    "static",
+    "deprecated",
+    "abstract",
+    "async",
+    "modification",
+    "documentation",
+    "defaultLibrary"
+  )
+}
