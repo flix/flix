fa276bf968a9fc1dc0d40a36b8a23f474440d829
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2025-02-10 15:32:04 +0100

    feat: add TrustValidation

--- main/src/ca/uwaterloo/flix/api/effectlock/TrustValidation.scala
+++ main/src/ca/uwaterloo/flix/api/effectlock/TrustValidation.scala
@@ -1,6 +1,14 @@
 package ca.uwaterloo.flix.api.effectlock
 
+import ca.uwaterloo.flix.api.Flix
+import ca.uwaterloo.flix.language.ast.TypedAst
+
 object TrustValidation {
 
+  sealed trait TrustError
+
+  def run(root: TypedAst.Root)(implicit flix: Flix): List[TrustError] = {
+    List.empty
+  }
 
 }
