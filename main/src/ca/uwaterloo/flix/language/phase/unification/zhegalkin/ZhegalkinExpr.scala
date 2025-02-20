38a31ec3fb34134be3403bae85a917d5274233b0
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2025-02-20 21:48:47 +0100

    feat: towards supporting timeouts for `Zhegalkin`

--- main/src/ca/uwaterloo/flix/language/phase/unification/zhegalkin/ZhegalkinExpr.scala
+++ main/src/ca/uwaterloo/flix/language/phase/unification/zhegalkin/ZhegalkinExpr.scala
@@ -126,7 +126,11 @@ object ZhegalkinExpr {
           seen.add(t)
         }
       }
+
       val allTermsNonDup = seen.toList
+      if (allTermsNonDup.length > 1000) {
+        throw new TooComplex("Number of Zhegalkin terms exceed: 1000. Inference too complex.")
+      }
 
       // Merge coefficients: (c1 ∩ x1 ∩ x2) ⊕ (c2 ∩ x1 ∩ x2) = (c1 ∩ c2) ∩ x1 ∩ x2
       val termsGroupedByVarSet = allTermsNonDup.groupBy(_.vars).toList
