2a65085ec66e6c0702c2bfb2494841b4c0856719
stormckey <chenhaogao123@gmail.com>
2025-02-26 20:30:03 +0100

    chore: remove unused SyntacticContext

--- main/src/ca/uwaterloo/flix/language/ast/shared/SyntacticContext.scala
+++ main/src/ca/uwaterloo/flix/language/ast/shared/SyntacticContext.scala
@@ -34,8 +34,6 @@ object SyntacticContext {
 
     case object Effect extends Decl
 
-    case object Instance extends Decl
-
     case object Module extends Decl
 
     case object Struct extends Decl
@@ -50,32 +48,8 @@ object SyntacticContext {
   object Expr {
     case object Constraint extends Expr
 
-    case object New extends Expr
-
     case object OtherExpr extends Expr
   }
-
-  case object Import extends SyntacticContext
-
-  sealed trait Pat extends SyntacticContext
-
-  object Pat {
-    case object OtherPat extends Pat
-  }
-
-  sealed trait Type extends SyntacticContext
-
-  object Type {
-    case object Eff extends Type
-
-    case object OtherType extends Type
-  }
-
-  case object Use extends SyntacticContext
-
-  case object WithClause extends SyntacticContext
-
-  case object WithHandler extends SyntacticContext
 
   case object Unknown extends SyntacticContext
 
