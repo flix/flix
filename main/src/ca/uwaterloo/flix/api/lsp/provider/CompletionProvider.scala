1968d8d5904325a9951e6b401e314c9478f89f36
stormckey <chenhaogao123@gmail.com>
2025-03-14 01:36:55 +0800

    refactor: unwrap KindCompleter (#10114)

--- main/src/ca/uwaterloo/flix/api/lsp/provider/CompletionProvider.scala
+++ main/src/ca/uwaterloo/flix/api/lsp/provider/CompletionProvider.scala
@@ -103,7 +103,7 @@ object CompletionProvider {
         case err: ResolutionError.UndefinedEffect => EffectCompleter.getCompletions(err)
         case err: ResolutionError.UndefinedJvmImport => ImportCompleter.getCompletions(err.name, Range.from(err.loc))
         case err: ResolutionError.UndefinedJvmStaticField => GetStaticFieldCompleter.getCompletions(err) ++ InvokeStaticMethodCompleter.getCompletions(err)
-        case err: ResolutionError.UndefinedKind => KindCompleter.getCompletions(err)
+        case err: ResolutionError.UndefinedKind => KindCompleter.getCompletions(err.qn.ident.name, Range.from(err.loc))
         case err: ResolutionError.UndefinedOp => OpCompleter.getCompletions(err)
         case err: ResolutionError.UndefinedStructField => StructFieldCompleter.getCompletions(err, root)
         case err: ResolutionError.UndefinedTrait => TraitCompleter.getCompletions(err)
