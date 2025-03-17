b293daa5a535908062796922e5395b8402f1c0e8
stormckey <chenhaogao123@gmail.com>
2025-03-17 13:34:37 +0100

    feat: introduce UndefinedNameContext

--- main/src/ca/uwaterloo/flix/api/lsp/provider/CompletionProvider.scala
+++ main/src/ca/uwaterloo/flix/api/lsp/provider/CompletionProvider.scala
@@ -17,12 +17,18 @@ package ca.uwaterloo.flix.api.lsp.provider
 
 import ca.uwaterloo.flix.api.Flix
 import ca.uwaterloo.flix.api.lsp.*
+import ca.uwaterloo.flix.api.lsp.acceptors.InsideAcceptor
+import ca.uwaterloo.flix.api.lsp.consumers.StackConsumer
+import ca.uwaterloo.flix.api.lsp.provider.CompletionProvider.{getStack, getUndefinedNameContext}
 import ca.uwaterloo.flix.api.lsp.provider.completion.*
 import ca.uwaterloo.flix.api.lsp.provider.completion.semantic.{GetStaticFieldCompleter, InvokeStaticMethodCompleter}
 import ca.uwaterloo.flix.api.lsp.provider.completion.syntactic.{ExprSnippetCompleter, KeywordCompleter}
 import ca.uwaterloo.flix.language.CompilationMessage
+import ca.uwaterloo.flix.language.ast.TypedAst
 import ca.uwaterloo.flix.language.ast.TypedAst.Root
+import ca.uwaterloo.flix.language.ast.shared.SymUse.DefSymUse
 import ca.uwaterloo.flix.language.ast.shared.{SyntacticContext, TraitUsageKind}
+import ca.uwaterloo.flix.language.ast.shared.SyntacticContext
 import ca.uwaterloo.flix.language.errors.{ParseError, ResolutionError, TypeError, WeederError}
 
 /**
@@ -41,16 +47,18 @@ import ca.uwaterloo.flix.language.errors.{ParseError, ResolutionError, TypeError
 object CompletionProvider {
 
   def autoComplete(uri: String, pos: Position, currentErrors: List[CompilationMessage])(implicit root: Root, flix: Flix): CompletionList = {
-      val items = getCompletions(uri, pos, currentErrors)(root, flix).map(_.toCompletionItem)
+      val stack = getStack(uri, pos)
+      val undefinedNameContext: UndefinedNameContext = getUndefinedNameContext(stack)
+      val items = getCompletions(uri, pos, currentErrors, stack)(root, flix).map(_.toCompletionItem(undefinedNameContext))
       CompletionList(isIncomplete = true, items)
   }
 
   /**
     * Returns all completions in the given `uri` at the given position `pos`.
     */
-  private def getCompletions(uri: String, pos: Position, currentErrors: List[CompilationMessage])(implicit root: Root, flix: Flix): List[Completion] = {
+  private def getCompletions(uri: String, pos: Position, currentErrors: List[CompilationMessage], stack: List[AnyRef])(implicit root: Root, flix: Flix): List[Completion] = {
     if (currentErrors.isEmpty)
-      HoleCompleter.getHoleCompletion(uri, pos).toList
+      HoleCompleter.getHoleCompletion(uri, pos, root).toList
     else
       errorsAt(uri, pos, currentErrors).flatMap {
         case err: WeederError.UndefinedAnnotation => KeywordCompleter.getModKeywords(Range.from(err.loc))
@@ -140,6 +148,48 @@ object CompletionProvider {
 
       case SyntacticContext.Unknown => Nil
     }
+  }
+
+  /**
+    * Returns the undefined name context from the stack.
+    *
+    * Transfer:
+    *   Start -- Malformed --> Start
+    *   Start -- UndefinedName --> CanBeApplied
+    *   CanBeApplied -- ApplyClo --> CanBeApplied
+    *   CanBeApplied -- ApplyDef --> Done
+    *   AnyState -- Anything --> Done
+    */
+  private def getUndefinedNameContext(stack: List[AnyRef]): UndefinedNameContext = {
+    stack.foldLeft((UndefinedNameContextState.Start: UndefinedNameContextState, UndefinedNameContext(None, pipelined = None))) {
+      case ((UndefinedNameContextState.Start, ctx), TypedAst.Expr.Error(_: ParseError.Malformed, _, _)) => (UndefinedNameContextState.Start, ctx)
+      case ((UndefinedNameContextState.Start, ctx), TypedAst.Expr.Error(_: ResolutionError.UndefinedName, _, _)) => (UndefinedNameContextState.CanBeApplied, ctx)
+      case ((UndefinedNameContextState.CanBeApplied, ctx), _: TypedAst.Expr.ApplyClo) => (UndefinedNameContextState.CanBeApplied, ctx.copy(applied = Some(ctx.applied.getOrElse(0) + 1)))
+      case ((UndefinedNameContextState.CanBeApplied, ctx), TypedAst.Expr.ApplyDef(DefSymUse(sym, _), _, _, _, _, _)) =>
+        sym.text match {
+          case "|>" | "!>" => (UndefinedNameContextState.Done, ctx.copy(pipelined = Some(1)))
+          case "||>" => (UndefinedNameContextState.Done, ctx.copy(pipelined = Some(2)))
+          case _ => (UndefinedNameContextState.Done, ctx)
+        }
+      case ((_, ctx), _) => (UndefinedNameContextState.Done, ctx) // Jump to the final state in other cases
+    }._2
+  }
+
+  /**
+    * Returns the stack of AST nodes at the given position.
+    *
+    * Every AST node that includes the given position is pushed onto the stack.
+    * Represented as a list of AnyRef. The head of the list is the innermost AST node.
+    */
+  private def getStack(uri: String, pos: Position)(implicit root: Root): List[AnyRef] = {
+    val stack = StackConsumer()
+
+    if (pos.character >= 2) {
+      val leftPos = Position(pos.line, pos.character - 1)
+      Visitor.visitRoot(root, stack, InsideAcceptor(uri, leftPos))
+    }
+
+    stack.getStack
   }
 
   /**
