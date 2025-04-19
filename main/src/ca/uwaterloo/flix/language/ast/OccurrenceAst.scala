5e5f3881f1abadfa866547672ff0a0664ec41b96
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2025-04-19 11:42:00 +0200

    docs: improve documentation

--- main/src/ca/uwaterloo/flix/language/ast/OccurrenceAst.scala
+++ main/src/ca/uwaterloo/flix/language/ast/OccurrenceAst.scala
@@ -143,47 +143,50 @@ object OccurrenceAst {
 
   case class ParYieldFragment(pat: Pattern, exp: Expr, loc: SourceLocation)
 
+  /**
+    * Represents occurrence information of binders, i.e., how a binder occurs in the program.
+    */
   sealed trait Occur
 
   object Occur {
 
     /**
-      * Represents a variable that is not used in an expression.
+      * Represents a binder that is not used in an expression.
       */
     case object Dead extends Occur
 
     /**
-      * Represents a variable that occurs exactly once in an expression.
+      * Represents a binder that occurs exactly once in an expression.
       */
     case object Once extends Occur
 
     /**
-      * Represents a variable that occurs exactly once in the body of a lambda abstraction.
+      * Represents a binder that occurs exactly once and that occurrence is in the body of a lambda abstraction.
       */
     case object OnceInLambda extends Occur
 
     /**
-      * Represents a variable that occurs exactly once in the body of a local def.
+      * Represents a binder that occurs exactly once and that occurrence is in the body of a local def.
       */
     case object OnceInLocalDef extends Occur
 
     /**
-      * Represents a variable that occurs in expressions more than once.
-      */
-    case object Many extends Occur
-
-    /**
-      * Represents a variable that occurs in more than one branch, e.g., match cases, but never inside a lambda abstraction or local def.
+      * Represents a binder that occurs in more than one branch, e.g., match cases, but never inside a lambda abstraction or local def.
       */
     case object ManyBranch extends Occur
 
     /**
-      * Represents a variable that we explicitly do not want to inline.
+      * Represents a binder that occurs more than once (including lambdas, local defs, branches).
+      */
+    case object Many extends Occur
+
+    /**
+      * Represents a binder that we explicitly do not want to inline.
       */
     case object DontInline extends Occur
 
     /**
-      * Represents a function that should not be inlined and excluded from any rewriting internally.
+      * Represents a binder that should not be inlined and excluded from any rewriting internally.
       * This is because casts are dangerous.
       */
     case object Dangerous extends Occur
