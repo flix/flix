aa4f6e23e86fc2aa7a72ed0224bf89fca9e2e423
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2025-04-19 11:48:59 +0200

    docs: improve documentation

--- main/src/ca/uwaterloo/flix/language/ast/OccurrenceAst.scala
+++ main/src/ca/uwaterloo/flix/language/ast/OccurrenceAst.scala
@@ -193,11 +193,14 @@ object OccurrenceAst {
   }
 
   /**
-    * `OccurDef` contains information that indicates whether a def should be inlined.
-    * A def is `isDirectCall` if the expression consist of a single (non-self) call.
-    * `occur` represents the number of times a def is references in the entire program.
-    * `size` denotes the cumulative weight of each expression in the body of the def.
+    * A [[DefContext]] contains various pieces information on a function that are relevant for making an inlining decision.
+    *
+    * @param occur           the occurrence information of the function (as described by [[Occur]]).
+    * @param size            the number of subexpressions in the function.
+    * @param localDefs       the number of local defs defined in a function.
+    * @param isDirectCall    true if the outermost expression of the body is a function call.
+    * @param isSelfRecursive true if the function symbol occurs in the body of the function being defined.
     */
-  case class DefContext(isDirectCall: Boolean, occur: Occur, size: Int, localDefs: Int, isSelfRecursive: Boolean)
+  case class DefContext(occur: Occur, size: Int, localDefs: Int, isDirectCall: Boolean, isSelfRecursive: Boolean)
 
 }
