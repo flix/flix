708c13ce99b0d811e3b9fd4c8e8c953e2fd1c5f4
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2025-04-20 12:12:31 +0200

    docs: new year

--- main/src/ca/uwaterloo/flix/language/ast/OccurrenceAst.scala
+++ main/src/ca/uwaterloo/flix/language/ast/OccurrenceAst.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 Jakob Schneider Villumsen
+ * Copyright 2025 Jakob Schneider Villumsen
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
