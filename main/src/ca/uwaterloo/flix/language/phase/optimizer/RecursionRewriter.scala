5c5364f7706477eb002aa6e7f8b05134a41e2d9e
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2025-04-24 15:56:46 +0200

    feat: add rewriteExp

--- main/src/ca/uwaterloo/flix/language/phase/optimizer/RecursionRewriter.scala
+++ main/src/ca/uwaterloo/flix/language/phase/optimizer/RecursionRewriter.scala
@@ -18,9 +18,10 @@ package ca.uwaterloo.flix.language.phase.optimizer
 
 import ca.uwaterloo.flix.api.Flix
 import ca.uwaterloo.flix.language.ast.MonoAst.Expr
-import ca.uwaterloo.flix.language.ast.{MonoAst, Symbol}
+import ca.uwaterloo.flix.language.ast.shared.{BoundBy, Scope}
+import ca.uwaterloo.flix.language.ast.{MonoAst, SourceLocation, Symbol}
 import ca.uwaterloo.flix.language.dbg.AstPrinter.DebugMonoAst
-import ca.uwaterloo.flix.util.ParOps
+import ca.uwaterloo.flix.util.{InternalCompilerException, ParOps}
 
 import scala.collection.mutable
 
@@ -45,9 +46,11 @@ object RecursionRewriter {
     }
 
     // 2. Rewrite eligible functions
-
     // 2.1 Create a substitution from the function symbol and alive parameters to fresh symbols (maybe this can be created during step 2)
     // 2.2 Copy the function body, visit and apply the substitution and rewrite nodes. Any ApplyDef expr becomes an ApplyLocalDef expr.
+    val freshLocalDefSym = Symbol.freshVarSym(defn.sym.text, BoundBy.LocalDef, defn.sym.loc)(Scope.Top, flix)
+    val subst = Subst.from(defn.sym, freshLocalDefSym, ctx.alive.toMap)
+    val localDefLoop = rewriteExp(defn.exp)(subst, defn.spec.fparams)
     // 2.3 Replace the original function body with a LocalDef declaration that has the body from 3.2, followed by an ApplyLocalDef expr.
     ???
   }
@@ -167,6 +170,136 @@ object RecursionRewriter {
       }
   }
 
+  private def rewriteExp(expr0: MonoAst.Expr)(implicit subst: Subst, fparams0: List[MonoAst.FormalParam]): MonoAst.Expr = expr0 match {
+    case Expr.Cst(_, _, _) =>
+      expr0
+
+    case Expr.Var(sym, tpe, loc) =>
+      Expr.Var(subst(sym), tpe, loc)
+
+    case Expr.Lambda(fparam, exp, tpe, loc) =>
+      val e = rewriteExp(exp)
+      Expr.Lambda(fparam, e, tpe, loc)
+
+    case Expr.ApplyAtomic(op, exps, tpe, eff, loc) =>
+      val es = exps.map(rewriteExp)
+      Expr.ApplyAtomic(op, es, tpe, eff, loc)
+
+    case Expr.ApplyClo(exp1, exp2, tpe, eff, loc) =>
+      val e1 = rewriteExp(exp1)
+      val e2 = rewriteExp(exp2)
+      Expr.ApplyClo(e1, e2, tpe, eff, loc)
+
+    case Expr.ApplyDef(sym, exps, itpe, tpe, eff, loc) =>
+      subst(sym) match {
+        case None => // It is not a recursive call
+          Expr.ApplyDef(sym, exps, itpe, tpe, eff, loc)
+
+        case Some(localDefSym) =>
+          val es = exps.zipWithIndex.filter {
+            case (_, i) => subst.isAlive(i, fparams0)
+          }.map {
+            case (e, _) => rewriteExp(e)
+          }
+          Expr.ApplyLocalDef(localDefSym, es, itpe, eff, loc)
+      }
+
+    case Expr.ApplyLocalDef(sym, exps, tpe, eff, loc) =>
+      val es = exps.map(rewriteExp)
+      Expr.ApplyLocalDef(sym, es, tpe, eff, loc)
+
+    case Expr.Let(sym, exp1, exp2, tpe, eff, loc) =>
+      val e1 = rewriteExp(exp1)
+      val e2 = rewriteExp(exp2)
+      Expr.Let(sym, e1, e2, tpe, eff, loc)
+
+    case Expr.LocalDef(sym, fparams, exp1, exp2, tpe, eff, loc) =>
+      val e1 = rewriteExp(exp1)
+      val e2 = rewriteExp(exp2)
+      Expr.LocalDef(sym, fparams, e1, e2, tpe, eff, loc)
+
+    case Expr.Scope(sym, regSym, exp, tpe, eff, loc) =>
+      val e = rewriteExp(exp)
+      Expr.Scope(sym, regSym, e, tpe, eff, loc)
+
+    case Expr.IfThenElse(exp1, exp2, exp3, tpe, eff, loc) =>
+      val e1 = rewriteExp(exp1)
+      val e2 = rewriteExp(exp2)
+      val e3 = rewriteExp(exp3)
+      Expr.IfThenElse(e1, e2, e3, tpe, eff, loc)
+
+    case Expr.Stm(exp1, exp2, tpe, eff, loc) =>
+      val e1 = rewriteExp(exp1)
+      val e2 = rewriteExp(exp2)
+      Expr.Stm(e1, e2, tpe, eff, loc)
+
+    case Expr.Discard(exp, eff, loc) =>
+      val e = rewriteExp(exp)
+      Expr.Discard(e, eff, loc)
+
+    case Expr.Match(exp1, rules, tpe, eff, loc) =>
+      val e1 = rewriteExp(exp1)
+      val rs = rules.map {
+        case MonoAst.MatchRule(pat, guard, exp2) =>
+          val g = guard.map(rewriteExp)
+          val e2 = rewriteExp(exp2)
+          MonoAst.MatchRule(pat, g, e2)
+      }
+      Expr.Match(e1, rs, tpe, eff, loc)
+
+    case Expr.VectorLit(exps, tpe, eff, loc) =>
+      val es = exps.map(rewriteExp)
+      Expr.VectorLit(es, tpe, eff, loc)
+
+    case Expr.VectorLoad(exp1, exp2, tpe, eff, loc) =>
+      val e1 = rewriteExp(exp1)
+      val e2 = rewriteExp(exp2)
+      Expr.VectorLoad(e1, e2, tpe, eff, loc)
+
+    case Expr.VectorLength(exp, loc) =>
+      val e = rewriteExp(exp)
+      Expr.VectorLength(e, loc)
+
+    case Expr.Ascribe(exp, tpe, eff, loc) =>
+      val e = rewriteExp(exp)
+      Expr.Ascribe(e, tpe, eff, loc)
+
+    case Expr.Cast(exp, declaredType, declaredEff, tpe, eff, loc) =>
+      val e = rewriteExp(exp)
+      Expr.Cast(e, declaredType, declaredEff, tpe, eff, loc)
+
+    case Expr.TryCatch(exp1, rules, tpe, eff, loc) =>
+      val e1 = rewriteExp(exp1)
+      val rs = rules.map {
+        case MonoAst.CatchRule(sym, clazz, exp2) =>
+          val e2 = rewriteExp(exp2)
+          MonoAst.CatchRule(sym, clazz, e2)
+      }
+      Expr.TryCatch(e1, rs, tpe, eff, loc)
+
+    case Expr.RunWith(exp1, effUse, rules, tpe, eff, loc) =>
+      val e1 = rewriteExp(exp1)
+      val rs = rules.map {
+        case MonoAst.HandlerRule(op, fparams, exp2) =>
+          val e2 = rewriteExp(exp2)
+          MonoAst.HandlerRule(op, fparams, e2)
+      }
+      Expr.RunWith(e1, effUse, rs, tpe, eff, loc)
+
+    case Expr.Do(op, exps, tpe, eff, loc) =>
+      val es = exps.map(rewriteExp)
+      Expr.Do(op, es, tpe, eff, loc)
+
+    case Expr.NewObject(name, clazz, tpe, eff1, methods, loc1) =>
+      val ms = methods.map {
+        case MonoAst.JvmMethod(ident, fparams, exp, retTpe, eff2, loc2) =>
+          val e = rewriteExp(exp)
+          MonoAst.JvmMethod(ident, fparams, e, retTpe, eff2, loc2)
+      }
+      Expr.NewObject(name, clazz, tpe, eff1, ms, loc1)
+
+  }
+
   /** Returns `false` if `expr` is a var with same symbol as `fparam`. Returns `true` otherwise. */
   private def aliveParam(expr: MonoAst.Expr, fparam: MonoAst.FormalParam): Boolean = expr match {
     case Expr.Var(sym, _, _) => sym != fparam.sym
@@ -180,5 +313,38 @@ object RecursionRewriter {
   }
 
   private case class LocalContext(alive: mutable.HashMap[Symbol.VarSym, Symbol.VarSym])
+
+  private object Subst {
+    def from(old: Symbol.DefnSym, fresh: Symbol.VarSym, vars: Map[Symbol.VarSym, Symbol.VarSym]): Subst = Subst(old, fresh, vars)
+  }
+
+  private case class Subst(private val old: Symbol.DefnSym, private val fresh: Symbol.VarSym, private val vars: Map[Symbol.VarSym, Symbol.VarSym]) {
+
+    def apply(sym: Symbol.DefnSym): Option[Symbol.VarSym] = {
+      if (sym == old)
+        Some(fresh)
+      else
+        None
+    }
+
+    def apply(sym: Symbol.VarSym): Symbol.VarSym = vars.get(sym) match {
+      case Some(freshSym) => freshSym
+      case None => sym
+    }
+
+    /**
+      * Returns `true` if the fparam at index `i` is alive.
+      *
+      * @param i       The index of the fparam of which to check aliveness.
+      * @param fparams The list of [[MonoAst.FormalParam]] to traverse.
+      */
+    def isAlive(i: Int, fparams: List[MonoAst.FormalParam]): Boolean = {
+      fparams.drop(i) match {
+        case fp :: _ => vars.contains(fp.sym)
+        case Nil => throw InternalCompilerException("unexpected empty fparam", SourceLocation.Unknown)
+      }
+    }
+
+  }
 
 }
