ca17f1631ad71ab6e3a4889ffed7d7579295dd7d
stormckey <chenhaogao123@gmail.com>
2025-04-24 15:43:16 +0200

    use the error of unexpectedToken

--- main/src/ca/uwaterloo/flix/language/phase/Weeder2.scala
+++ main/src/ca/uwaterloo/flix/language/phase/Weeder2.scala
@@ -1276,7 +1276,7 @@ object Weeder2 {
               val error = UnexpectedToken(NamedTokenSet.FromKinds(Set(TokenKind.KeywordElse)), actual = None, SyntacticContext.Expr.OtherExpr, hint = Some("the else-branch is required in Flix."), tree.loc)
               sctx.errors.add(error)
               // This Expr.Error is just a placeholder.
-              (condExpr, thenExpr) => Expr.IfThenElse(condExpr, thenExpr, Expr.Error(MisplacedComments(SyntacticContext.Unknown, tree.loc)), tree.loc)
+              (condExpr, thenExpr) => Expr.IfThenElse(condExpr, thenExpr, Expr.Error(error), tree.loc)
             }
         }
       }
