4cd815dd6804465b5af00cfceaf869935a16f892
jonathanStarup <32037926+JonathanStarup@users.noreply.github.com>
2025-05-04 23:03:07 +0200

    work

--- main/test/ca/uwaterloo/flix/verifier/TokenVerifier.scala
+++ main/test/ca/uwaterloo/flix/verifier/TokenVerifier.scala
@@ -23,7 +23,8 @@ import ca.uwaterloo.flix.util.{Formatter, InternalCompilerException, ParOps}
 
 /**
   * Invariants:
-  *   - I-Eof: The last token is [[TokenKind.Eof]] and no other token is.
+  *   - I-Eof-A: The last token is [[TokenKind.Eof]] and no other token is.
+  *   - I-Eof-B: No token before the last is [[TokenKind.Eof]].
   *   - I-Src: All tokens have a [[Source]] consistent with the source map.
   *   - I-Ranges: Offset- and position ranges must end after they are started.
   *   - I-Bounds: Offset ranges must be inside bounds.
@@ -44,83 +45,70 @@ object TokenVerifier {
   def verify(m: Map[Source, Array[Token]])(implicit flix: Flix): Unit = {
     ParOps.parMap(m) {
       case (src, tokens) =>
-        checkEndOfFile(src, tokens)
-        checkSrc(src, tokens)
-        checkRanges(tokens)
-        checkBounds(src, tokens)
-        checkOffsetOrder(tokens)
-        checkPositionOrder(tokens)
+        val tokensLength = tokens.length
+        checkEndOfFileA(src, tokens)
+        var prev: Token = null
+        for ((token, i) <- tokens.iterator.zipWithIndex) {
+          checkEndOfFileB(token, i == tokensLength)
+          checkSrc(src, token)
+          checkRange(token)
+          checkBounds(src, token)
+          if (prev != null) {
+            checkOffsetOrder(prev, token)
+            checkPositionOrder(prev, token)
+          }
+          prev = token
+        }
     }
   }
 
-  /** Checks I-Eef. */
-  private def checkEndOfFile(src: Source, tokens: Array[Token]): Unit = {
+  /** Checks I-Eef-A. */
+  private def checkEndOfFileA(src: Source, tokens: Array[Token]): Unit = {
     tokens.lastOption match {
       case None => emptyTokens(src)
       case Some(last) =>
         if (last.kind != TokenKind.Eof) missingEof(last)
-        tokens
-          .iterator
-          .zipWithIndex
-          .filter { case (token, i) => token.kind == TokenKind.Eof && i != tokens.length - 1 }
-          .foreach { case (token, _) => unexpectedEof(token) }
     }
   }
 
+  /** Checks I-Eef-B. */
+  private def checkEndOfFileB(token: Token, isLast: Boolean): Unit =
+    if (token.kind == TokenKind.Eof && !isLast) unexpectedEof(token)
+
   /** Checks I-Src. */
-  private def checkSrc(src: Source, tokens: Array[Token]): Unit = {
-    tokens
-      .iterator
-      .filter(_.src != src)
-      .foreach(wrongSource(src, _))
-  }
+  private def checkSrc(src: Source, token: Token): Unit =
+    if (token.src != src) wrongSource(src, token)
 
   /** Checks I-Ranges. */
-  private def checkRanges(tokens: Array[Token]): Unit = {
-    tokens
-      .iterator
-      .filter(token => token.start > token.end)
-      .foreach(wrongOffsetRange)
-    tokens
-      .iterator
-      .filter(token => {
-        if (token.sp1.lineOneIndexed == token.sp2.lineOneIndexed) {
-          token.sp1.colOneIndexed > token.sp2.colOneIndexed
-        } else token.sp1.lineOneIndexed > token.sp2.lineOneIndexed
-      })
-      .foreach(wrongPositionRange)
+  private def checkRange(token: Token): Unit = {
+    if (token.start > token.end) wrongOffsetRange(token)
+    if (!isBefore(token.sp1, token.sp2)) wrongPositionRange(token)
+  }
+
+  /** Returns true if `sp1` is before of the same as `sp2`, ignoring [[SourcePosition.source]].  */
+  private def isBefore(sp1: SourcePosition, sp2: SourcePosition): Boolean = {
+    if (sp1.lineOneIndexed == sp2.lineOneIndexed) {
+      sp1.colOneIndexed <= sp2.colOneIndexed
+    } else sp1.lineOneIndexed <= sp2.lineOneIndexed
   }
 
   /** Checks I-Bounds. */
-  private def checkBounds(src: Source, tokens: Array[Token]): Unit = {
+  private def checkBounds(src: Source, token: Token): Unit = {
     // Tokens end offsets are exclusive and offsets are zero-indexed.
     def outOfBounds(i: Int): Boolean = i < 0 || src.data.length < i
-
-    tokens
-      .iterator
-      .filter(token => outOfBounds(token.start) || outOfBounds(token.end))
-      .foreach(outOfBoundOffset)
+    if (outOfBounds(token.start) || outOfBounds(token.end)) outOfBoundOffset(token)
   }
 
   /** Checks I-Offset. */
-  private def checkOffsetOrder(tokens: Array[Token]): Unit = {
-    tokens.iterator
-      .zip(tokens.iterator.drop(1))
-      .filter { case (one, two) => one.end > two.start }
-      .foreach { case (one, two) => outOfOrderOffsets(one, two) }
+  private def checkOffsetOrder(left: Token, right: Token): Unit = {
+    // Tokens end offsets are exclusive.
+    if (left.end > right.start) outOfOrderOffsets(left, right)
   }
 
   /** Checks I-Pos. */
-  private def checkPositionOrder(tokens: Array[Token]): Unit = {
-    tokens.iterator
-      .zip(tokens.iterator.drop(1))
-      .filter { case (one, two) =>
-        if (one.sp2.lineOneIndexed == two.sp1.lineOneIndexed) {
-          // Token end positions are exclusive.
-          one.sp2.colOneIndexed > two.sp1.colOneIndexed
-        } else one.sp2.lineOneIndexed > two.sp1.lineOneIndexed
-      }
-      .foreach { case (one, two) => outOfOrderPositions(one, two) }
+  private def checkPositionOrder(left: Token, right: Token): Unit = {
+    // Token end positions are exclusive.
+    if (!isBefore(left.sp2, right.sp1)) outOfOrderPositions(left, right)
   }
 
   private def missingEof(found: Token): Nothing = {
