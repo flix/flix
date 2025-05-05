02cac25c35c03abd28e48030be6033e149bd024e
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2025-05-05 14:25:21 +0200

    refactor: add isRecursive

--- main/src/ca/uwaterloo/flix/language/phase/optimizer/Inliner.scala
+++ main/src/ca/uwaterloo/flix/language/phase/optimizer/Inliner.scala
@@ -525,9 +525,14 @@ object Inliner {
       Expr.ApplyDef(sym, es, itpe, tpe, eff, loc)
   }
 
-  /** Returns true if `defn` is not recursive and is either a higher-order function or is a direct call to another function. */
+  /** Returns `true` if `defn` is not recursive and is either a higher-order function or is a direct call to another function. */
   private def shouldInlineDef(defn: OccurrenceAst.Def)(implicit sym0: Symbol.DefnSym): Boolean = {
-    defn.sym != sym0 && (isDirectCall(defn) || isHigherOrder(defn))
+    isRecursive(defn, sym0) && (isDirectCall(defn) || isHigherOrder(defn))
+  }
+
+  /** Returns `true` if `defn.sym` is equal to `sym0` */
+  private def isRecursive(defn: OccurrenceAst.Def, sym0: Symbol.DefnSym) = {
+    defn.sym != sym0
   }
 
   /** Returns `true` if at least one formal parameter of `defn` has an arrow type. */
