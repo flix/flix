fb08a12ecbed18a2da88f33e925e05086e96d9fc
jonathanStarup <32037926+JonathanStarup@users.noreply.github.com>
2025-05-06 22:22:27 +0200

    work

--- main/src/ca/uwaterloo/flix/language/dbg/printer/MonoAstPrinter.scala
+++ main/src/ca/uwaterloo/flix/language/dbg/printer/MonoAstPrinter.scala
@@ -42,7 +42,7 @@ object MonoAstPrinter {
     case Expr.VectorLoad(exp1, exp2, _, _, _) => DocAst.Expr.VectorLoad(print(exp1), print(exp2))
     case Expr.VectorLength(exp, _) => DocAst.Expr.VectorLength(print(exp))
     case Expr.Ascribe(exp, tpe, _, _) => DocAst.Expr.AscriptionTpe(print(exp), TypePrinter.print(tpe))
-    case Expr.Cast(exp, tpe, _, _) => DocAst.Expr.Cast(print(exp), TypePrinter.print(tpe))
+    case Expr.Cast(exp, tpe, eff, _) => DocAst.Expr.UncheckedCast(print(exp), Some(TypePrinter.print(tpe)), Some(TypePrinter.print(eff)))
     case Expr.TryCatch(exp, rules, _, _, _) => DocAst.Expr.TryCatch(print(exp), rules.map(printCatchRule))
     case Expr.RunWith(exp, effUse, rules, _, _, _) => DocAst.Expr.RunWithHandler(print(exp), effUse.sym, rules.map(printHandlerRule))
     case Expr.Do(op, exps, _, _, _) => DocAst.Expr.Do(op.sym, exps.map(print))
