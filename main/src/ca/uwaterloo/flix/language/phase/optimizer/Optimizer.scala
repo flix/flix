a6fb7c3f7bd9ff9e3c76f9f8695c0effa073fa44
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2025-05-07 15:13:21 +0200

    feat: remove OccurrenceAst

--- main/src/ca/uwaterloo/flix/language/phase/optimizer/Optimizer.scala
+++ main/src/ca/uwaterloo/flix/language/phase/optimizer/Optimizer.scala
@@ -1,9 +1,8 @@
 package ca.uwaterloo.flix.language.phase.optimizer
 
 import ca.uwaterloo.flix.api.Flix
-import ca.uwaterloo.flix.language.ast.{MonoAst, OccurrenceAst}
+import ca.uwaterloo.flix.language.ast.MonoAst
 import ca.uwaterloo.flix.language.dbg.AstPrinter.DebugMonoAst
-import ca.uwaterloo.flix.util.ParOps
 
 object Optimizer {
 
