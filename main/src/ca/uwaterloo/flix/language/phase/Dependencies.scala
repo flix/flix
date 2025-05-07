baee12bca7ef819622bb39b081f96f24de35a4fd
Magnus Madsen <magnusm@cs.au.dk>
2025-05-07 10:57:14 +0200

    feat: add support for extensible variants to `TypedAst`

--- main/src/ca/uwaterloo/flix/language/phase/Dependencies.scala
+++ main/src/ca/uwaterloo/flix/language/phase/Dependencies.scala
@@ -235,6 +235,15 @@ object Dependencies {
       visitType(tpe)
       visitType(eff)
 
+    case Expr.ExtensibleMatch(exp1, bnd1, exp2, bnd2, exp3, tpe, eff, _) =>
+      visitExp(exp1)
+      visitBinder(bnd1)
+      visitExp(exp2)
+      visitBinder(bnd2)
+      visitExp(exp3)
+      visitType(tpe)
+      visitType(eff)
+
     case Expr.Tag(sym, exps, tpe, eff, _) =>
       visitSymUse(sym)
       exps.foreach(visitExp)
@@ -243,6 +252,11 @@ object Dependencies {
 
     case Expr.RestrictableTag(sym, exps, tpe, eff, _) =>
       visitSymUse(sym)
+      exps.foreach(visitExp)
+      visitType(tpe)
+      visitType(eff)
+
+    case Expr.ExtensibleTag(_, exps, tpe, eff, _) =>
       exps.foreach(visitExp)
       visitType(tpe)
       visitType(eff)
