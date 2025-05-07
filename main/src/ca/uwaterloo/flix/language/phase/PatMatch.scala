30549640ab2119545775ce223f2d5f143f7e1c23
Magnus Madsen <magnusm@cs.au.dk>
2025-05-07 10:53:00 +0200

    feat: add support for extensible variants to `TypedAst`

--- main/src/ca/uwaterloo/flix/language/phase/PatMatch.scala
+++ main/src/ca/uwaterloo/flix/language/phase/PatMatch.scala
@@ -187,9 +187,16 @@ object PatMatch {
         visitExp(exp)
         rules.foreach(r => visitExp(r.exp))
 
+      case Expr.ExtensibleMatch(exp1, _, exp2, _, exp3, _, _, _) =>
+        visitExp(exp1)
+        visitExp(exp2)
+        visitExp(exp3)
+
       case Expr.Tag(_, exps, _, _, _) => exps.foreach(visitExp)
 
       case Expr.RestrictableTag(_, exps, _, _, _) => exps.foreach(visitExp)
+
+      case Expr.ExtensibleTag(_, exps, _, _, _) => exps.foreach(visitExp)
 
       case Expr.Tuple(elms, _, _, _) => elms.foreach(visitExp)
 
--- main/src/ca/uwaterloo/flix/language/phase/PredDeps.scala
+++ main/src/ca/uwaterloo/flix/language/phase/PredDeps.scala
@@ -172,10 +172,18 @@ object PredDeps {
       visitExp(exp)
       rules.foreach{ case RestrictableChooseRule(_, body) => visitExp(body) }
 
+    case Expr.ExtensibleMatch(exp1, _, exp2, _, exp3, _, _, _) =>
+      visitExp(exp1)
+      visitExp(exp2)
+      visitExp(exp3)
+
     case Expr.Tag(_, exps, _, _, _) =>
       exps.foreach(visitExp)
 
     case Expr.RestrictableTag(_, exps, _, _, _) =>
+      exps.foreach(visitExp)
+
+    case Expr.ExtensibleTag(_, exps, _, _, _) =>
       exps.foreach(visitExp)
 
     case Expr.Tuple(elms, _, _, _) =>
