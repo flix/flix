bf6fb4a6261002db10ada3d9f432c99f1e08a596
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2025-05-16 21:36:32 +0200

    feat: wip add structure

--- main/src/ca/uwaterloo/flix/util/CyclicalGraph.scala
+++ main/src/ca/uwaterloo/flix/util/CyclicalGraph.scala
@@ -16,6 +16,43 @@
 
 package ca.uwaterloo.flix.util
 
+import ca.uwaterloo.flix.util.CyclicalGraph.Vertex
+
+case class CyclicalGraph[T](vertices: List[Vertex[T]])
+
 object CyclicalGraph {
+
+  sealed trait Vertex[T] {
+
+    /**
+      *
+      * @return
+      */
+    def in: List[T]
+
+    /**
+      *
+      * @return
+      */
+    def out: List[T]
+  }
+
+  /**
+    *
+    * @param in
+    * @param out
+    * @tparam T
+    */
+  case class Singleton[T](in: List[T], out: List[T]) extends Vertex[T]
+
+  /**
+    * Represents a strongly connected component.
+    *
+    * @param cycle
+    * @param in
+    * @param out
+    * @tparam T
+    */
+  case class SCC[T](cycle: List[T], in: List[T], out: List[T]) extends Vertex[T]
 
 }
