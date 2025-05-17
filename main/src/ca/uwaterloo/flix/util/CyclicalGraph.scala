57c6dd762e26a0fe570cc72944e5ecc0d2738cf6
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2025-05-17 12:42:13 +0200

    feat: add scc

--- main/src/ca/uwaterloo/flix/util/CyclicalGraph.scala
+++ main/src/ca/uwaterloo/flix/util/CyclicalGraph.scala
@@ -16,7 +16,10 @@
 
 package ca.uwaterloo.flix.util
 
+import ca.uwaterloo.flix.language.ast.SourceLocation
 import ca.uwaterloo.flix.util.CyclicalGraph.Vertex
+
+import scala.collection.mutable
 
 /**
   * Represents a graph that may contain cycles.
@@ -30,8 +33,7 @@ object CyclicalGraph {
   sealed trait Vertex[T] {
 
     /**
-      *
-      * @return the list of outgoing edges.
+      * Returns the list of outgoing edges.
       */
     def out: List[T]
 
@@ -42,7 +44,7 @@ object CyclicalGraph {
     *
     * @param out the list of outgoing edges.
     */
-  case class Singleton[T](out: List[T]) extends Vertex[T]
+  case class Singleton[T](value: T, out: List[T]) extends Vertex[T]
 
   /**
     * Represents a strongly connected component.
@@ -50,6 +52,95 @@ object CyclicalGraph {
     * @param cycle the list of vertices that form the cycle.
     * @param out   the list of outgoing edges.
     */
-  case class SCC[T](cycle: List[T], out: List[T]) extends Vertex[T]
+  case class SCC[T](cycle: List[Singleton[T]]) extends Vertex[T] {
+    def out: List[T] = {
+      cycle.flatMap(singleton => singleton.out.filterNot(cycle.contains))
+    }
+  }
 
+  def from[T](graph: Map[T, List[T]]): CyclicalGraph[T] = {
+    val vertices = graph.map { case (k, vs) => Singleton(k, vs) }.toList
+    CyclicalGraph(vertices)
+  }
+
+  def invert[T](graph: CyclicalGraph[T]): CyclicalGraph[T] = {
+    from(invert(toMap(graph)))
+  }
+
+  def scc[T](graph: CyclicalGraph[T]): CyclicalGraph[T] = {
+    scc(toMap(graph))
+  }
+
+  def toMap[T](graph: CyclicalGraph[T]): Map[T, List[T]] = {
+    graph.vertices.flatMap {
+      case Singleton(value, out) => Map(value -> out)
+      case SCC(cycle) => cycle.map(v => v.value -> v.out)
+    }.toMap
+  }
+
+  private def invert[T](graph: Map[T, List[T]]): Map[T, List[T]] = {
+    val result: mutable.Map[T, List[T]] = mutable.Map.empty
+    ???
+  }
+
+  private def scc[T](graph: Map[T, List[T]]): CyclicalGraph[T] = {
+    if (graph.isEmpty) {
+      throw InternalCompilerException("unexpected empty graph", SourceLocation.Unknown)
+    }
+
+    val visited: mutable.ArrayBuffer[T] = mutable.ArrayBuffer.empty
+    val discoveryTimes: mutable.Map[T, Int] = mutable.Map.empty
+    val finishingTimes: mutable.Map[T, Int] = mutable.Map.empty
+    var time = 0
+
+    def visit(u: T, g: Map[T, List[T]]): Unit = {
+      time += 1
+      discoveryTimes.put(u, time)
+      visited.addOne(u)
+      g(u).foreach {
+        v =>
+          if (!visited.contains(v)) {
+
+            visit(v, g)
+          }
+      }
+      time += 1
+      finishingTimes.put(u, time)
+    }
+
+    graph.keys.foreach(u => if (!visited.contains(u)) visit(u, graph))
+    visited.clear()
+    time = 0
+
+    val inverted = invert(graph)
+    val sortedByFinishingTimes = inverted.keys.map(u => (u, discoveryTimes(u), finishingTimes(u)))
+      .toArray.sortInPlaceBy { case (_, _, f) => f }
+
+    discoveryTimes.clear()
+    finishingTimes.clear()
+
+    sortedByFinishingTimes.reverse.foreach { case (u, _, _) => if (!visited.contains(u)) visit(u, inverted) }
+
+    val timesStack = mutable.Stack.from(sortedByFinishingTimes)
+    var result = List.empty[List[T]]
+    var (u, d, f) = timesStack.pop()
+    var cycle = List(u)
+    while (timesStack.nonEmpty) {
+      val (v, d1, f1) = timesStack.pop()
+      if (d < d1 && f1 < f) {
+        cycle = v :: cycle
+      } else {
+        result = cycle :: result
+        cycle = List(v)
+      }
+      u = v
+      d = d1
+      f = f1
+    }
+
+    CyclicalGraph(result.map {
+      case value :: Nil => Singleton(value, graph(value))
+      case l => SCC(l.reverse.map(value => Singleton(value, graph(value))))
+    })
+  }
 }
