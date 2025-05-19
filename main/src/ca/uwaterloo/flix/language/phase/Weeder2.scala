152ff316557ce69d0dd4468f9f7c3799202d5bb0
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2025-05-19 16:22:20 +0200

    feat: emit error on more than two variables

--- main/src/ca/uwaterloo/flix/language/phase/Weeder2.scala
+++ main/src/ca/uwaterloo/flix/language/phase/Weeder2.scala
@@ -1573,13 +1573,15 @@ object Weeder2 {
       val exprs = pickAll(TreeKind.Expr.Expr, tree)
       flatMapN(Patterns.pickExtensiblePattern(tree), traverse(exprs)(visitExpr)) {
         // case pattern => expr
-        case ((qname, pats), expr :: Nil) =>
+        case ((qname, pats), expr :: Nil) if pats.length == 2 =>
           Validation.Success(ExtMatchRule(qname, pats, expr, tree.loc))
-        // case pattern if expr => expr
-        case ((qname, pats), _ :: expr :: Nil) =>
-          val error = Malformed(NamedTokenSet.MatchRule, SyntacticContext.Expr.OtherExpr, loc = tree.loc)
+
+        // At the moment we only allow to variables in ExtMatch
+        case ((qname, pats), expr :: Nil) =>
+          val error = WeederError.MismatchedArity(2, pats.length, tree.loc)
           sctx.errors.add(error)
           Validation.Success(ExtMatchRule(qname, pats, expr, tree.loc))
+
         // Fall back on Expr.Error. Parser has reported an error here.
         case ((qname, _), _) =>
           val error = Malformed(NamedTokenSet.MatchRule, SyntacticContext.Expr.OtherExpr, loc = tree.loc)
