6d9337a2e2436f94ae00d00f6bba04e2c4b6709a
CadeMichael <cade.lueker@gmail.com>
2025-05-20 09:41:02 +0200

    remove default test as it is breaking

--- main/src/ca/uwaterloo/flix/util/Options.scala
+++ main/src/ca/uwaterloo/flix/util/Options.scala
@@ -54,7 +54,7 @@ object Options {
   /**
     * Default test options.
     */
-  val DefaultTest: Options = Default.copy(lib = LibLevel.All, progress = false, xnodeprecated = true, xchaosMonkey = true)
+  val DefaultTest: Options = Default.copy(lib = LibLevel.All, progress = false, xnodeprecated = true)
 
   /**
     * Default test options with the standard library.
