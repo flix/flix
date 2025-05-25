bdafdf169793d0af70ef9115f5f27c6c1b759b41
jonathanStarup <32037926+JonathanStarup@users.noreply.github.com>
2025-05-25 19:07:37 +0200

    work

--- main/src/ca/uwaterloo/flix/language/phase/jvm/JvmName.scala
+++ main/src/ca/uwaterloo/flix/language/phase/jvm/JvmName.scala
@@ -86,7 +86,6 @@ object JvmName {
     * result in the string `"Tuple2$Obj$Int32$Obj"`.
     */
   def mkClassName(prefix: String, args: List[String]): String = {
-    // TODO: Should delimiter always be included?
     val cPrefix = mangle(prefix)
     if (args.isEmpty) s"$cPrefix${Flix.Delimiter}"
     else s"$cPrefix${Flix.Delimiter}${args.map(mangle).mkString(Flix.Delimiter)}"
@@ -101,7 +100,6 @@ object JvmName {
   /**
     * Performs name mangling on the given string `s` to avoid issues with special characters.
     */
-  // TODO: Magnus: Use this in appropriate places.
   def mangle(s: String): String = s.
     replace("+", Flix.Delimiter + "plus").
     replace("-", Flix.Delimiter + "minus").
