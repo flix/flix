1dee9b3f0ef138bd19ea65ece5613f619fda2669
jonathanStarup <32037926+JonathanStarup@users.noreply.github.com>
2025-06-06 23:13:18 +0200

    work

--- main/src/ca/uwaterloo/flix/language/phase/jvm/GenExpression.scala
+++ main/src/ca/uwaterloo/flix/language/phase/jvm/GenExpression.scala
@@ -23,7 +23,7 @@ import ca.uwaterloo.flix.language.ast.SemanticOp.*
 import ca.uwaterloo.flix.language.ast.shared.{Constant, ExpPosition}
 import ca.uwaterloo.flix.language.ast.{MonoType, *}
 import ca.uwaterloo.flix.language.phase.jvm.BytecodeInstructions.{InstructionSet, MethodEnricher}
-import ca.uwaterloo.flix.language.phase.jvm.ClassMaker.{ConstructorMethod, InstanceField}
+import ca.uwaterloo.flix.language.phase.jvm.ClassMaker
 import ca.uwaterloo.flix.language.phase.jvm.JvmName.MethodDescriptor
 import ca.uwaterloo.flix.language.phase.jvm.JvmName.MethodDescriptor.mkDescriptor
 import ca.uwaterloo.flix.util.InternalCompilerException
@@ -1499,12 +1499,12 @@ object GenExpression {
 
       NEW(className) ~
         DUP() ~
-        INVOKESPECIAL(ConstructorMethod(className, Nil)) ~
+        INVOKESPECIAL(ClassMaker.ConstructorMethod(className, Nil)) ~
         // For each method, compile the closure which implements the body of that method and store it in a field.
         composeN(for ((e, i) <- exps.zipWithIndex) yield {
           DUP() ~
             pushExpr(e) ~
-            PUTFIELD(InstanceField(className, s"clo$i", JvmOps.getClosureAbstractClassType(e.tpe).toTpe))
+            PUTFIELD(ClassMaker.InstanceField(className, s"clo$i", JvmOps.getClosureAbstractClassType(e.tpe).toTpe))
         })
     })
 
