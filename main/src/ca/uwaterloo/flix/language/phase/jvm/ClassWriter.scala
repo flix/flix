3474d50f9bc4b1ff227e53703aba07ab453a7c8a
jonathanStarup <32037926+JonathanStarup@users.noreply.github.com>
2025-06-07 12:03:12 +0200

    work

--- main/src/ca/uwaterloo/flix/language/phase/jvm/ClassWriter.scala
+++ main/src/ca/uwaterloo/flix/language/phase/jvm/ClassWriter.scala
@@ -1,5 +1,6 @@
 /*
- * Copyright 2025 Jonathan Lindegaard Starup
+ * Copyright 2017 Magnus Madsen
+ * Copyright 2021 Jonathan Lindegaard Starup
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
