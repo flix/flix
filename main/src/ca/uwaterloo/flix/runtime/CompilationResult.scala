d7deba40a77e17ed48cefb10d9e836ebe703ad93
jonathanStarup <32037926+JonathanStarup@users.noreply.github.com>
2025-06-07 21:33:13 +0200

    work

--- main/src/ca/uwaterloo/flix/runtime/CompilationResult.scala
+++ main/src/ca/uwaterloo/flix/runtime/CompilationResult.scala
@@ -23,7 +23,9 @@ import ca.uwaterloo.flix.language.ast.*
   * A class representing the result of a compilation.
   *
   * @param root     the abstract syntax tree of the program.
+  * @param main     the reflected main function, if present.
   * @param defs     the definitions in the program.
+  * @param totalTime the total compilation time, excluding class writing/loading.
   * @param codeSize the number of bytes the compiler generated.
   */
 class CompilationResult(root: Root,
