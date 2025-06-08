922d8141324d633cb33c29d055fb1190bb2e268d
gaganchandan <gaganchandan29@gmail.com>
2025-06-08 05:49:19 +0200

    feat: inlay hints to show effects

--- main/src/ca/uwaterloo/flix/api/lsp/provider/InlayHintProvider.scala
+++ main/src/ca/uwaterloo/flix/api/lsp/provider/InlayHintProvider.scala
@@ -15,13 +15,33 @@
  */
 package ca.uwaterloo.flix.api.lsp.provider
 
-import ca.uwaterloo.flix.api.lsp.{InlayHint, Range}
+import ca.uwaterloo.flix.api.lsp.acceptors.{FileAcceptor}
+import ca.uwaterloo.flix.language.ast.shared.SymUse
+import ca.uwaterloo.flix.language.ast.TypedAst.{Expr, Root}
+import ca.uwaterloo.flix.api.lsp.{Consumer, InlayHint, InlayHintKind, Position, Range}
+import ca.uwaterloo.flix.api.lsp.Visitor
 
 object InlayHintProvider {
+  def getInlayHints(uri: String, range: Range)(implicit root: Root): List[InlayHint] = {
+    var inlayHints: List[InlayHint] = Nil
+    object opSymUseConsumer extends Consumer {
+      override def consumeOpSymUse(opSymUse: SymUse.OpSymUse): Unit = {
+        val line = opSymUse.loc.beginLine
+        var hint = InlayHint(
+          position = Position(line, 100),
+          label = opSymUse.sym.eff.name,
+          kind = Some(InlayHintKind.Type),
+          textEdits = List.empty,
+          tooltip = s"Effect: ${opSymUse.sym.eff.name}",
+          paddingLeft = true,
+          paddingRight = true
+        )
+      inlayHints = hint :: inlayHints
+      }
+    }
 
-  def getInlayHints(uri: String, range: Range): List[InlayHint] = {
-    // We currently do not support any inlay hints.
-    Nil
+    Visitor.visitRoot(root, opSymUseConsumer, FileAcceptor(uri))
+
+    inlayHints
   }
-
 }
