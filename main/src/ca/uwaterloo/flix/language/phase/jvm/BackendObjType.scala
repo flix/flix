fe652541329c9e3b2349914641569e9072e4ef54
JonathanStarup <32037926+JonathanStarup@users.noreply.github.com>
2025-06-09 12:28:50 +0200

    work

--- main/src/ca/uwaterloo/flix/language/phase/jvm/BackendObjType.scala
+++ main/src/ca/uwaterloo/flix/language/phase/jvm/BackendObjType.scala
@@ -77,7 +77,6 @@ sealed trait BackendObjType {
     case BackendObjType.Thread => JvmName(JavaLang, "Thread")
     case BackendObjType.ThreadBuilderOfVirtual => JvmName(JavaLang, "Thread$Builder$OfVirtual")
     case BackendObjType.ThreadUncaughtExceptionHandler => JvmName(JavaLang, "Thread$UncaughtExceptionHandler")
-    case BackendObjType.ReentrantLock => JvmName.ReentrantLock
     // Effects Runtime
     case BackendObjType.Result => JvmName(DevFlixRuntime, mkClassName("Result"))
     case BackendObjType.Value => JvmName(DevFlixRuntime, mkClassName("Value"))
@@ -178,7 +177,7 @@ object BackendObjType {
 
     def ValueField: InstanceField = InstanceField(this.jvmName, "value", tpe)
 
-    private def LockField: InstanceField = InstanceField(this.jvmName, "lock", ReentrantLock.toTpe)
+    private def LockField: InstanceField = InstanceField(this.jvmName, "lock", JvmName.ReentrantLock.toTpe)
 
     def Constructor: ConstructorMethod = ConstructorMethod(this.jvmName, List(BackendType.Object))
 
@@ -194,7 +193,7 @@ object BackendObjType {
         PUTFIELD(ExpField)
         // this.lock = new ReentrantLock()
         thisLoad()
-        NEW(ReentrantLock.jvmName)
+        NEW(JvmName.ReentrantLock)
         DUP()
         INVOKESPECIAL(ReentrantLock.Constructor)
         PUTFIELD(LockField)
@@ -1767,16 +1766,6 @@ object BackendObjType {
 
     def UncaughtExceptionMethod: InstanceMethod = InstanceMethod(this.jvmName, "uncaughtException",
       mkDescriptor(Thread.toTpe, JvmName.Throwable.toTpe)(VoidableType.Void))
-  }
-
-  case object ReentrantLock extends BackendObjType {
-
-    def Constructor: ConstructorMethod = ConstructorMethod(this.jvmName, Nil)
-
-    def UnlockMethod: InstanceMethod = InstanceMethod(this.jvmName, "unlock", MethodDescriptor.NothingToVoid)
-
-    def LockInterruptiblyMethod: InstanceMethod = InstanceMethod(this.jvmName, "lockInterruptibly", MethodDescriptor.NothingToVoid)
-
   }
 
   case object Result extends BackendObjType {
--- main/src/ca/uwaterloo/flix/language/phase/jvm/ClassMaker.scala
+++ main/src/ca/uwaterloo/flix/language/phase/jvm/ClassMaker.scala
@@ -296,4 +296,14 @@ object ClassMaker {
 
   }
 
+  object ReentrantLock {
+
+    def Constructor: ConstructorMethod = ConstructorMethod(JvmName.ReentrantLock, Nil)
+
+    def UnlockMethod: InstanceMethod = InstanceMethod(JvmName.ReentrantLock, "unlock", MethodDescriptor.NothingToVoid)
+
+    def LockInterruptiblyMethod: InstanceMethod = InstanceMethod(JvmName.ReentrantLock, "lockInterruptibly", MethodDescriptor.NothingToVoid)
+
+  }
+
 }
