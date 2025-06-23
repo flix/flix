6a2e50525a5befbfe14e9dbae214dc2ab9049716
jonathanStarup <32037926+JonathanStarup@users.noreply.github.com>
2025-06-23 23:09:27 +0200

    work

--- main/src/ca/uwaterloo/flix/language/phase/TypeSimplifier.scala
+++ main/src/ca/uwaterloo/flix/language/phase/TypeSimplifier.scala
@@ -31,11 +31,8 @@ object TypeSimplifier {
     *
     * This function should never crash and works for any type, no matter how ill-kinded.
     */
-  def simplify(tpe: Type): Type = {
-    // This might throw [[InternalCompilerException]] since it reuses unification infrastructure
-    // but since the error is so imprecise we cannot handle it here.
-    simplifyInternal(tpe)
-  }
+  def simplify(tpe: Type): Type =
+    visitType(tpe)
 
   /** Simplifies types inside `tc` with [[simplify]]. */
   def simplifyConstraint(tc: TypeConstraint): TypeConstraint = tc match {
@@ -49,34 +46,33 @@ object TypeSimplifier {
       TypeConstraint.Conflicted(simplify(tpe1), simplify(tpe2), prov)
   }
 
-  /**
-    * Simplifies all effects in `tpe0`.
-    *
-    * Might throw [[InternalCompilerException]] for non-well-kinded effects because of [[EffUnification3.simplify]].
-    */
-  private def simplifyInternal(tpe0: Type): Type = tpe0 match {
-    case t if t.kind == Kind.Eff => EffUnification3.simplify(t)
+  /** Simplifies all effects in `tpe0`. */
+  private def visitType(tpe0: Type): Type = tpe0 match {
+    case t if t.kind == Kind.Eff =>
+      // This might throw [[InternalCompilerException]] since it reuses unification infrastructure
+      // but since the error is so imprecise we cannot handle it here.
+      EffUnification3.simplify(t)
     case t: Type.Var => t
     case t: Type.Cst => t
     case t@Type.Apply(tpe1, tpe2, loc) =>
-      val t1 = simplifyInternal(tpe1)
-      val t2 = simplifyInternal(tpe2)
+      val t1 = visitType(tpe1)
+      val t2 = visitType(tpe2)
       t.renew(t1, t2, loc)
     case Type.Alias(symUse, args, tpe, loc) =>
-      val as = args.map(simplifyInternal)
-      val t = simplifyInternal(tpe)
+      val as = args.map(visitType)
+      val t = visitType(tpe)
       Type.Alias(symUse, as, t, loc)
     case Type.AssocType(symUse, arg, kind, loc) =>
-      val a = simplifyInternal(arg)
+      val a = visitType(arg)
       Type.AssocType(symUse, a, kind, loc)
     case Type.JvmToType(tpe, loc) =>
-      val t = simplifyInternal(tpe)
+      val t = visitType(tpe)
       Type.JvmToType(t, loc)
     case Type.JvmToEff(tpe, loc) =>
-      val t = simplifyInternal(tpe)
+      val t = visitType(tpe)
       Type.JvmToEff(t, loc)
     case Type.UnresolvedJvmType(member, loc) =>
-      val m = member.map(simplifyInternal)
+      val m = member.map(visitType)
       Type.UnresolvedJvmType(m, loc)
   }
 
