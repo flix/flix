1e21e4cf2976897dc3018942c9204428acfe213e
JonathanStarup <32037926+JonathanStarup@users.noreply.github.com>
2025-06-25 12:00:33 +0200

    work

--- main/test/ca/uwaterloo/flix/language/TestTypeSimplifier.scala
+++ main/test/ca/uwaterloo/flix/language/TestTypeSimplifier.scala
@@ -23,7 +23,7 @@ import ca.uwaterloo.flix.language.ast.{SourceLocation, Type, TypedAst}
 import ca.uwaterloo.flix.language.phase.TypeSimplifier
 import ca.uwaterloo.flix.language.phase.typer.ConstraintSolver2
 import ca.uwaterloo.flix.language.phase.unification.EqualityEnv
-import ca.uwaterloo.flix.util.Options
+import ca.uwaterloo.flix.util.{InternalCompilerException, Options}
 import org.scalatest.funsuite.AnyFunSuite
 
 import scala.collection.mutable
@@ -31,14 +31,29 @@ import scala.collection.mutable
 class TestTypeSimplifier extends AnyFunSuite with TestUtils {
 
   test("TypeCheckWithSimplifier") {
+    // Check that for all types, they are equivalent to their simplified type.
+
+    // `TypeSimplifier.simplify` does not reduce associated types, so eqEnv can be left empty without invalidating equality.
+    val eqEnv: EqualityEnv = EqualityEnv.empty
+    // The flix object is largely unused when calling equivalent.
+    val flix: Flix = new Flix().setOptions(Options.DefaultTest)
+
+    for (tpe <- getSampleTypes) {
+      val simplifiedType = TypeSimplifier.simplify(tpe)
+      assert(ConstraintSolver2.equivalent(tpe, simplifiedType)(eqEnv, flix), s"\n$tpe\ndoes not unify with\n$simplifiedType")
+    }
+  }
+
+  /** Returns a list of types to use for testing. */
+  private def getSampleTypes: List[Type] = {
+    // To find sample types, check the standard library and pick of the types in the AST.
+
     implicit val flix: Flix = new Flix().setOptions(Options.DefaultTest)
-    // Run the frontend to have some types to work on and to get the EqualityEnv.
+
     val root = flix.check() match {
       case (Some(r), Nil) => r
-      case (_, _) => fail("Standard library does not compile")
+      case (_, _) => throw InternalCompilerException("Standard library does not compile", SourceLocation.Unknown)
     }
-
-    implicit val eqEnv: EqualityEnv = root.eqEnv
 
     // Collect all the types of the AST. Use `consumeExpr` to have both compiler generated and user written types.
     val types = mutable.ArrayBuffer.empty[Type]
@@ -49,13 +64,7 @@ class TestTypeSimplifier extends AnyFunSuite with TestUtils {
       },
       (_: SourceLocation) => true
     )
-
-    // Check that for all types, they are equivalent to their simplified type.
-    for (tpe <- types) {
-      val simplifiedType = TypeSimplifier.simplify(tpe)
-      assert(ConstraintSolver2.equivalent(tpe, simplifiedType), s"\n$tpe\ndoes not unify with\n$simplifiedType")
-    }
-
+    types.toList
   }
 
 }
