62af4750fd28e67737b6cb4a6d7a8b50125ce067
gaganchandan <gaganchandan29@gmail.com>
2025-07-07 12:29:40 +0200

    refactor

--- main/src/ca/uwaterloo/flix/api/lsp/provider/InlayHintProvider.scala
+++ main/src/ca/uwaterloo/flix/api/lsp/provider/InlayHintProvider.scala
@@ -15,6 +15,7 @@
  */
 
 package ca.uwaterloo.flix.api.lsp.provider
+
 import ca.uwaterloo.flix.api.lsp.acceptors.{FileAcceptor}
 import ca.uwaterloo.flix.language.ast.shared.SymUse
 import ca.uwaterloo.flix.language.ast.TypedAst.{Expr, Root}
@@ -24,34 +25,59 @@ import ca.uwaterloo.flix.api.lsp.{Consumer, InlayHint, InlayHintKind, Position, 
 import ca.uwaterloo.flix.api.lsp.Visitor
 import scala.collection.immutable.Map
 
+/**
+  * Provides inlay hints for effects in the Flix language.
+  */
 object InlayHintProvider {
+
+  /**
+    * Whether to enable effect hints.
+    */
   private val EnableEffectHints: Boolean = false
 
+  /**
+    * Returns a list of inlay hints for the given URI and range.
+    *
+    * @param uri   The URI of the file.
+    * @param range The range within the file to get inlay hints for.
+    * @param root  The root of the typed AST.
+    * @return A list of inlay hints.
+    */
   def getInlayHints(uri: String, range: Range)(implicit root: Root): List[InlayHint] = {
-    var inlayHints: List[InlayHint] = Nil
-    if(EnableEffectHints) {
-      val effects: Set[(Type, SourceLocation)] = (getOpSymUses(uri) ++ getDefSymUses(uri)).filter {
+    if (EnableEffectHints) {
+      val effects: List[(Type, SourceLocation)] = (getEffectsFromOpSymUses(uri) ++ getEffectsFromDefSymUses(uri)).filter {
         case (eff, loc) => eff match {
           case Type.Pure => false
           case _ => true
         }
       }
-      var locationToEffectsMap: Map[SourceLocation, Set[Type]] = Map.empty
-      effects.foreach { case (eff, loc) =>
-        locationToEffectsMap = locationToEffectsMap.updated(loc, locationToEffectsMap.getOrElse(loc, Set.empty) + eff)
+
+      /**
+        * Map from position to effects.
+        * The position is the end of the effect expression, and the effects are all the effects that appear on that line.
+        * For example, if there are two effects on the same line, they will be combined into a single hint.
+        */
+      val positionToEffectsMap: Map[Position, List[Type]] = effects.foldLeft(Map.empty[Position, List[Type]]) {
+        case (acc, (eff, loc)) =>
+          val position = Position(loc.endLine, loc.source.getLine(loc.endLine).length + 2)
+          acc.updated(position, eff :: acc.getOrElse(position, List.empty))
       }
-      inlayHints = mkHintsFromEffects(locationToEffectsMap)
+      mkHintsFromEffects(positionToEffectsMap)
+    } else {
+      List.empty[InlayHint]
     }
-    inlayHints
   }
 
-  private def getOpSymUses(uri: String)(implicit root: Root): Set[(Type, SourceLocation)] = {
-    var opSymUses: Set[(Type, SourceLocation)] = Set.empty
+  /**
+    * Returns a list of effects from effect operations.
+    */
+  private def getEffectsFromOpSymUses(uri: String)(implicit root: Root): List[(Type, SourceLocation)] = {
+    var opSymUses: List[(Type, SourceLocation)] = List.empty
     object opSymUseConsumer extends Consumer {
       override def consumeExpr(expr: Expr): Unit = {
         expr match {
           case Expr.Do(_, _, _, eff, loc) =>
-            opSymUses += ((eff, loc))
+            opSymUses = ((eff, loc)) :: opSymUses
           case _ => ()
         }
       }
@@ -60,13 +86,16 @@ object InlayHintProvider {
     opSymUses
   }
 
-  private def getDefSymUses(uri: String)(implicit root: Root): Set[(Type, SourceLocation)] = {
-    var defSymUses: Set[(Type, SourceLocation)] = Set.empty
+  /**
+    * Returns a list of effects from uses of definitions.
+    */
+  private def getEffectsFromDefSymUses(uri: String)(implicit root: Root): List[(Type, SourceLocation)] = {
+    var defSymUses: List[(Type, SourceLocation)] = List.empty
     object defSymUseConsumer extends Consumer {
       override def consumeExpr(expr: Expr): Unit = {
         expr match {
           case Expr.ApplyDef(_, _, _, _, eff, loc) =>
-            defSymUses += ((eff, loc))
+            defSymUses = ((eff, loc)) :: defSymUses
           case _ => ()
         }
       }
@@ -75,19 +104,24 @@ object InlayHintProvider {
     defSymUses
   }
 
-  private def mkHintsFromEffects(locationToEffectsMap: Map[SourceLocation, Set[Type]]): List[InlayHint] = {
-    locationToEffectsMap.map {
-      case (loc, effs) =>
-        mkHint(effs, loc)
+  /**
+    * Creates a list of inlay hints from the effects mapped to their positions.
+    */
+  private def mkHintsFromEffects(positionToEffectsMap: Map[Position, List[Type]]): List[InlayHint] = {
+    positionToEffectsMap.map {
+      case (pos, effs) =>
+        mkHint(effs, pos)
     }.toList
   }
 
-  // Creates an inlay hint combining all effects for a given line.
-  // For example, if the effects are ef1 and ef2, the hint will be { ef1 + ef2 }.
-  private def mkHint(effs: Set[Type], loc: SourceLocation): InlayHint = {
+  /**
+    * Creates an inlay hint combining all effects for a given line.
+    * For example, if the effects are ef1 and ef2, the hint will be { ef1 + ef2 }.
+    */
+  private def mkHint(effs: List[Type], pos: Position): InlayHint = {
     val effectString: String = effs.mkString(" + ")
     InlayHint(
-      position = Position(loc.endLine, loc.endCol),
+      position = pos,
       label = s"{ $effectString }",
       kind = Some(InlayHintKind.Type),
       textEdits = List.empty,
