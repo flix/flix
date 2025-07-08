05c264f245355009585d1544e5750b5c3380c0a7
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2025-07-08 14:35:18 +0200

    add PatMatch

--- main/src/ca/uwaterloo/flix/language/phase/PatMatch.scala
+++ main/src/ca/uwaterloo/flix/language/phase/PatMatch.scala
@@ -189,10 +189,10 @@ object PatMatch {
         visitExp(exp)
         rules.foreach(r => visitExp(r.exp))
 
-      case Expr.ExtensibleMatch(_, exp1, _, exp2, _, exp3, _, _, _) =>
-        visitExp(exp1)
-        visitExp(exp2)
-        visitExp(exp3)
+      case Expr.ExtensibleMatch(expr, rules, _, _, _) =>
+        // Exhaustiveness does not make sense for extensible variants.
+        visitExp(expr)
+        rules.foreach(r => visitExp(r.exp))
 
       case Expr.Tag(_, exps, _, _, _) => exps.foreach(visitExp)
 
