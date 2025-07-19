7d02d5e77277a6546d676f5e00a1d2d7bfbc6b50
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2025-07-19 13:30:22 +0100

    feat: update FormatType

--- main/src/ca/uwaterloo/flix/language/fmt/FormatType.scala
+++ main/src/ca/uwaterloo/flix/language/fmt/FormatType.scala
@@ -304,19 +304,19 @@ object FormatType {
       case DisplayType.SchemaConstructor(arg) => s"#{ ${visit(arg, Mode.Type)} }"
       case DisplayType.ExtSchema(fields) =>
         val fieldString = fields.map(visitSchemaFieldType).mkString(", ")
-        s"#|| $fieldString ||"
+        s"#| $fieldString |#"
       case DisplayType.ExtSchemaExtend(fields, rest) =>
         val fieldString = fields.map(visitSchemaFieldType).mkString(", ")
         val restString = visit(rest, Mode.Type)
-        s"#|| $fieldString | $restString ||"
+        s"#| $fieldString | $restString |#"
       case DisplayType.ExtSchemaRow(fields) =>
         val fieldString = fields.map(visitSchemaFieldType).mkString(", ")
-        s"#|( $fieldString )|"
+        s"#|( $fieldString )|#"
       case DisplayType.ExtSchemaRowExtend(fields, rest) =>
         val fieldString = fields.map(visitSchemaFieldType).mkString(", ")
         val restString = visit(rest, Mode.Type)
-        s"#|( $fieldString | $restString )|"
-      case DisplayType.ExtSchemaConstructor(arg) => s"#|| ${visit(arg, Mode.Type)} ||"
+        s"#|( $fieldString | $restString )|#"
+      case DisplayType.ExtSchemaConstructor(arg) => s"#| ${visit(arg, Mode.Type)} |#"
       case DisplayType.Not(tpe) => s"not ${delimit(tpe, mode)}"
       case DisplayType.And(tpes) =>
         val strings = tpes.map(delimit(_, mode))
