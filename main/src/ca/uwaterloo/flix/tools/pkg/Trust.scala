e063f98b82ef2a3106928c1862c5a3129ec3fa43
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2025-10-06 14:38:18 +0200

    add copyright

--- main/src/ca/uwaterloo/flix/tools/pkg/Trust.scala
+++ main/src/ca/uwaterloo/flix/tools/pkg/Trust.scala
@@ -1,5 +1,6 @@
 /*
  * Copyright 2024 Andreas Stenbæk Larsen
+ * Copyright 2025 Jakob Schneider Villumsen
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
