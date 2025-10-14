f751a4e9532b4486bbdfbb3c5ebf7695779744e0
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2025-10-14 11:29:30 +0200

    run flix package manager in suite

--- main/test/ca/uwaterloo/flix/tools/pkg/PackageManagerSuite.scala
+++ main/test/ca/uwaterloo/flix/tools/pkg/PackageManagerSuite.scala
@@ -5,5 +5,6 @@ import org.scalatest.Suites
 class PackageManagerSuite extends Suites(
   new TestBootstrap,
   new TestTrust,
-  new TestManifestParser
+  new TestManifestParser,
+  new TestFlixPackageManager
 )
