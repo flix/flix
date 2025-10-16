13f715402bfca368ea757e315c7460c281805cc4
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2025-10-16 14:37:59 +0200

    fix: wip build graph

--- main/src/ca/uwaterloo/flix/tools/pkg/FlixPackageManager.scala
+++ main/src/ca/uwaterloo/flix/tools/pkg/FlixPackageManager.scala
@@ -35,8 +35,8 @@ object FlixPackageManager {
     */
   def findTransitiveDependencies(manifest: Manifest, path: Path, apiKey: Option[String])(implicit formatter: Formatter, out: PrintStream): Result[List[Manifest], PackageError] = {
     out.println("Resolving Flix dependencies...")
-    implicit val depGraph: mutable.Map[Manifest, FlixDependency] = mutable.Map.empty
-    implicit val trust: mutable.Map[FlixDependency, Trust] = mutable.Map.empty
+    implicit val parentsOf: mutable.Map[Manifest, List[Manifest]] = mutable.Map.empty
+    implicit val trust: mutable.Map[Manifest, Trust] = mutable.Map.empty
     findTransitiveDependenciesRec(manifest, path, List(manifest), apiKey)
   }
 
@@ -148,7 +148,7 @@ object FlixPackageManager {
     * parses them to manifests. Returns the list of manifests.
     * `res` is the list of Manifests found so far to avoid duplicates.
     */
-  private def findTransitiveDependenciesRec(manifest: Manifest, path: Path, res: List[Manifest], apiKey: Option[String])(implicit depGraph: mutable.Map[Manifest, FlixDependency], trust: mutable.Map[FlixDependency, Trust], formatter: Formatter, out: PrintStream): Result[List[Manifest], PackageError] = {
+  private def findTransitiveDependenciesRec(manifest: Manifest, path: Path, res: List[Manifest], apiKey: Option[String])(implicit parentsOf: mutable.Map[Manifest, List[Manifest]], manifestToTrust: mutable.Map[Manifest, Trust], formatter: Formatter, out: PrintStream): Result[List[Manifest], PackageError] = {
     // find Flix dependencies of the current manifest
     val flixDeps = findFlixDependencies(manifest)
 
@@ -159,24 +159,21 @@ object FlixPackageManager {
         install(depName, dep.version, "toml", path, apiKey).map(p => (p, dep))
       }
 
-      //parse the manifests
-      transitiveManifests <- traverse(tomlPaths) { case (p, d) => parseManifest(p).map(m => (m, d)) }
-
-    } yield {
-      // Enforce that new dependencies respect transitive trust level
-      Result.sequence(transitiveManifests.flatMap {
-        case (tm, origDep) =>
-          val t = trust.getOrElse(origDep, origDep.trust).combine(origDep.trust)
-          trust.put(origDep, t)
-          depGraph.put(tm, origDep)
-          tm.dependencies.map(checkTrust(origDep, t, _))
-      }) match {
-        case Err(e) => return Err(e)
-        case Ok(_) => // Continue
+      // parse the manifests
+      transitiveManifests <- traverse(tomlPaths) { case (p, d) =>
+        parseManifest(p).map { m =>
+          manifestToTrust.put(m, d.trust)
+          m
+        }
       }
 
-      //remove duplicates
-      val newManifests = transitiveManifests.map(_._1).filter(m => !res.contains(m))
+    } yield {
+      for (m <- transitiveManifests) {
+        parentsOf.put(m, manifest :: parentsOf.getOrElse(m, List.empty))
+      }
+
+      // remove duplicates
+      val newManifests = transitiveManifests.filter(m => !res.contains(m))
       var newRes = res ++ newManifests
 
       // do recursive calls for all dependencies
@@ -191,13 +188,13 @@ object FlixPackageManager {
   }
 
   private def checkTrust(origDep: FlixDependency, t: Trust, dep: Dependency): Result[Unit, PackageError] = dep match {
-    case FlixDependency(_, _, _, _, t1) if t1.lessThan(t.combine(t1)) =>
+    case FlixDependency(_, _, _, _, t1) if t.lessThanEq(t.lub(t1)) =>
       Err(PackageError.TrustError(origDep, t))
 
-    case Dependency.MavenDependency(_, _, _) if t.lessThan(Trust.TrustJavaClass) =>
+    case Dependency.MavenDependency(_, _, _) if t.lessThanEq(Trust.TrustJavaClass) =>
       Err(PackageError.TrustError(origDep, t))
 
-    case Dependency.JarDependency(_, _) if t.lessThan(Trust.TrustJavaClass) =>
+    case Dependency.JarDependency(_, _) if t.lessThanEq(Trust.TrustJavaClass) =>
       Err(PackageError.TrustError(origDep, t))
 
     case _ => Ok(())
