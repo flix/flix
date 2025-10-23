f3f1a36b43e08740510db4194d53579ab8006703
Jonathan Lindegaard Starup <32037926+JonathanStarup@users.noreply.github.com>
2025-10-23 13:35:31 +0200

    feat: add `ErasedAst` (#11944)

--- main/src/ca/uwaterloo/flix/language/ast/ErasedAst.scala
+++ main/src/ca/uwaterloo/flix/language/ast/ErasedAst.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 Magnus Madsen
+ * Copyright 2025 Jonathan Lindegaard Starup
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,11 +18,9 @@ package ca.uwaterloo.flix.language.ast
 
 import ca.uwaterloo.flix.language.ast.Purity.Pure
 import ca.uwaterloo.flix.language.ast.shared.SymUse.{EffSymUse, OpSymUse}
-import ca.uwaterloo.flix.language.ast.shared.{Annotations, Constant, ExpPosition, Modifiers, Source}
+import ca.uwaterloo.flix.language.ast.shared.*
 
-import java.lang.reflect.Method
-
-object ReducedAst {
+object ErasedAst {
 
   case class Root(defs: Map[Symbol.DefnSym, Def],
                   enums: Map[Symbol.EnumSym, Enum],
@@ -32,7 +30,9 @@ object ReducedAst {
                   entryPoints: Set[Symbol.DefnSym],
                   sources: Map[Source, SourceLocation])
 
-  case class Def(ann: Annotations, mod: Modifiers, sym: Symbol.DefnSym, cparams: List[FormalParam], fparams: List[FormalParam], exp: Expr, tpe: SimpleType, unboxedType: UnboxedType, loc: SourceLocation)
+  case class Def(ann: Annotations, mod: Modifiers, sym: Symbol.DefnSym, cparams: List[FormalParam], fparams: List[FormalParam], exp: Expr, tpe: SimpleType, unboxedType: UnboxedType, loc: SourceLocation) {
+    val arrowType: SimpleType.Arrow = SimpleType.mkArrow(fparams.map(_.tpe), tpe)
+  }
 
   /** Remember the unboxed return type for test function generation. */
   case class UnboxedType(tpe: SimpleType)
@@ -57,6 +57,7 @@ object ReducedAst {
 
     case class Cst(cst: Constant, loc: SourceLocation) extends Expr {
       def tpe: SimpleType = cst.tpe
+
       def purity: Purity = Pure
     }
 
@@ -83,12 +84,14 @@ object ReducedAst {
     case class Let(sym: Symbol.VarSym, exp1: Expr, exp2: Expr, loc: SourceLocation) extends Expr {
       // Note: We use an implicit representation of type and purity to aid correctness and to save memory.
       def tpe: SimpleType = exp2.tpe
+
       def purity: Purity = Purity.combine(exp1.purity, exp2.purity)
     }
 
     case class Stmt(exp1: Expr, exp2: Expr, loc: SourceLocation) extends Expr {
       // Note: We use an implicit representation of type and purity to aid correctness and to save memory.
       def tpe: SimpleType = exp2.tpe
+
       def purity: Purity = Purity.combine(exp1.purity, exp2.purity)
     }
 
