d64b790400fe2a0390bd6b21660ebacd577022d1
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2025-11-03 20:07:50 +0100

    update test

--- main/test/ca/uwaterloo/flix/tools/pkg/TestFlixPackageManager.scala
+++ main/test/ca/uwaterloo/flix/tools/pkg/TestFlixPackageManager.scala
@@ -698,17 +698,23 @@ class TestFlixPackageManager extends AnyFunSuite with BeforeAndAfter {
   }
 
   test("mismatched-versions") {
-    val deps = List(
+    val toml = mkToml(List(
       """
         |"github:jaschdoc/flix-test-pkg-mismatched-versions" = "0.1.0"
         |""".stripMargin
-    )
-    val (forbidden, message) = checkForbidden(deps, Main)
+    ))
+    val manifest = ManifestParser.parse(toml, null) match {
+      case Ok(m) => m
+      case Err(e) => fail(e.message(formatter))
+    }
 
-    if (forbidden) {
-      succeed
-    } else {
-      fail(message + System.lineSeparator() + "expected error with mismatched versions (required '0.1.0', declared '0.2.0'")
+    val path = Files.createTempDirectory("")
+    throttle {
+      FlixPackageManager.findTransitiveDependencies(manifest, path, None)
+    } match {
+      case Ok(_) => fail("expected error, got success")
+      case Err(_: PackageError.MismatchedVersions) => succeed
+      case Err(e) => fail(e.message(formatter))
     }
   }
 
@@ -718,30 +724,18 @@ class TestFlixPackageManager extends AnyFunSuite with BeforeAndAfter {
     */
   private def checkForbidden(deps: List[String], main: String): (Boolean, String) = {
     val path = Files.createTempDirectory("")
-    val toml =
-      s"""
-         |[package]
-         |name = "test"
-         |description = "test"
-         |version = "0.1.0"
-         |flix = "0.65.0"
-         |authors = ["flix"]
-         |
-         |[dependencies]
-         |${deps.mkString(System.lineSeparator())}
-         |""".stripMargin
+    val toml = mkToml(deps)
 
     val manifest = ManifestParser.parse(toml, null) match {
       case Ok(m) => m
-      case Err(e) => return (false, e.message(formatter))
+      case Err(e) => fail(e.message(formatter))
     }
 
     val allManifests = throttle {
-      FlixPackageManager.findTransitiveDependencies(manifest, path, None) match {
-        case Ok(ms) => ms
-        case Err(e: PackageError.MismatchedVersions) => return (true, e.message(formatter))
-        case Err(e) => return (false, e.message(formatter))
-      }
+      FlixPackageManager.findTransitiveDependencies(manifest, path, None)
+    } match {
+      case Ok(ms) => ms
+      case Err(e) => fail(e.message(formatter))
     }
 
     val manifestsWithSecurity = FlixPackageManager.resolveSecurityLevels(allManifests)
@@ -752,10 +746,10 @@ class TestFlixPackageManager extends AnyFunSuite with BeforeAndAfter {
     }
 
     val pkgs = throttle {
-      FlixPackageManager.installAll(manifestsWithSecurity, path, None) match {
-        case Ok(ps) => ps
-        case Err(e) => return (false, e.message(formatter))
-      }
+      FlixPackageManager.installAll(manifestsWithSecurity, path, None)
+    } match {
+      case Ok(ps) => ps
+      case Err(e) => fail(e.message(formatter))
     }
 
     val flix = new Flix()
@@ -772,5 +766,19 @@ class TestFlixPackageManager extends AnyFunSuite with BeforeAndAfter {
     }
 
     (forbidden, flix.mkMessages(errors).mkString(System.lineSeparator()))
+  }
+
+  private def mkToml(deps: List[String]): String = {
+    s"""
+       |[package]
+       |name = "test"
+       |description = "test"
+       |version = "0.1.0"
+       |flix = "0.65.0"
+       |authors = ["flix"]
+       |
+       |[dependencies]
+       |${deps.mkString(System.lineSeparator())}
+       |""".stripMargin
   }
 }
