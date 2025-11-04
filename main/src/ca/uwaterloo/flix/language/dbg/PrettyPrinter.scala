8d15641d0ba057ffdb6ab2ac84fc92329d535b69
gwydd <gwydd12@proton.me>
2025-11-04 17:15:43 +0100

    feat: add `PrettyPrinter`
    - implements prettyPrint function that takes a SyntaxTree.Root and
      returns a reconstructed source code
    - simply concatenates the strings without formatting

---
+++ main/src/ca/uwaterloo/flix/language/dbg/PrettyPrinter.scala
@@ -0,0 +1,32 @@
+package ca.uwaterloo.flix.language.dbg
+
+import ca.uwaterloo.flix.language.ast.{SyntaxTree, Token}
+
+object PrettyPrinter {
+
+  /**
+    * Returns a "pretty-printed" string representation of the given syntax tree `root`.
+    *
+    * @param root the syntax tree root
+    * @return the "pretty-printed" string representation
+    */
+  def prettyPrint(root: SyntaxTree.Root): String = {
+    root.units.values.map(renderTree).mkString
+  }
+
+  private def renderTree(tree: SyntaxTree.Tree): String = {
+    val sb = new StringBuilder
+
+    def visit(t: SyntaxTree.Tree): Unit = {
+      val SyntaxTree.Tree(_, children, _) = t
+      children.foreach {
+        case token: Token => sb.append(token.text)
+        case childTree: SyntaxTree.Tree => visit(childTree)
+        case _ => ()
+      }
+    }
+
+    visit(tree)
+    sb.toString()
+  }
+}
