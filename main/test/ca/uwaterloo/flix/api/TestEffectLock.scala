00120dbd4cf560b5dc2938c4d62d0dae8b35d86c
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2025-11-09 15:58:41 +0100

    add tests

--- main/test/ca/uwaterloo/flix/api/TestEffectLock.scala
+++ main/test/ca/uwaterloo/flix/api/TestEffectLock.scala
@@ -24,4 +24,38 @@ class TestEffectLock extends AnyFunSuite with TestUtils {
     assert(expected == actual)
   }
 
+  test("serialization.02") {
+    val input =
+      """
+        |@Test
+        |pub def main(x: Int32): Int32 = x + x
+        |""".stripMargin
+
+    val tpe = serialization.Apply(serialization.Apply(serialization.Apply(serialization.Cst(serialization.Arrow(2)), serialization.Cst(serialization.Pure)), serialization.Cst(serialization.Int32)), serialization.Cst(serialization.Int32))
+    val scheme = serialization.SScheme(List.empty, tpe)
+    val expected = serialization.SDef(List.empty, "main", scheme, "<test>")
+
+    val (Some(root), _) = check(input, Options.TestWithLibMin)
+    val defs = root.entryPoints.flatMap(root.defs.get).toList
+    val actual = Serialize.serializeDef(defs.head)
+    assert(expected == actual)
+  }
+
+  test("serialization.03") {
+    val input =
+      """
+        |@Test
+        |pub def main(_: Int32): Unit = ()
+        |""".stripMargin
+
+    val tpe = serialization.Apply(serialization.Apply(serialization.Apply(serialization.Cst(serialization.Arrow(2)), serialization.Cst(serialization.Pure)), serialization.Cst(serialization.Int32)), serialization.Cst(serialization.Unit))
+    val scheme = serialization.SScheme(List.empty, tpe)
+    val expected = serialization.SDef(List.empty, "main", scheme, "<test>")
+
+    val (Some(root), _) = check(input, Options.TestWithLibMin)
+    val defs = root.entryPoints.flatMap(root.defs.get).toList
+    val actual = Serialize.serializeDef(defs.head)
+    assert(expected == actual)
+  }
+
 }
