bde2ff66dcb10a5f6e7686bb84965acb7a501e22
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2025-11-09 16:11:23 +0100

    add test

--- main/test/ca/uwaterloo/flix/api/TestEffectLock.scala
+++ main/test/ca/uwaterloo/flix/api/TestEffectLock.scala
@@ -1,8 +1,7 @@
 package ca.uwaterloo.flix.api
 
 import ca.uwaterloo.flix.TestUtils
-import ca.uwaterloo.flix.api.effectlock.serialization
-import ca.uwaterloo.flix.api.effectlock.serialization.Serialize
+import ca.uwaterloo.flix.api.effectlock.serialization.*
 import ca.uwaterloo.flix.util.Options
 import org.scalatest.funsuite.AnyFunSuite
 
@@ -11,68 +10,81 @@ class TestEffectLock extends AnyFunSuite with TestUtils {
   test("serialization.01") {
     val input =
       """
-        |pub def main(): Unit = ()
+        |pub def fun(): Unit = ()
         |""".stripMargin
 
-    val tpe = serialization.Apply(serialization.Apply(serialization.Apply(serialization.Cst(serialization.Arrow(2)), serialization.Cst(serialization.Pure)), serialization.Cst(serialization.Unit)), serialization.Cst(serialization.Unit))
-    val scheme = serialization.SScheme(List.empty, tpe)
-    val expected = serialization.SDef(List.empty, "main", scheme, "<test>")
+    val tpe = Apply(Apply(Apply(Cst(Arrow(2)), Cst(Pure)), Cst(Unit)), Cst(Unit))
+    val scheme = SScheme(List.empty, tpe)
+    val expected = SDef(List.empty, "fun", scheme, "<test>")
 
     val (Some(root), _) = check(input, Options.TestWithLibNix)
-    val defs = root.entryPoints.flatMap(root.defs.get).toList
+    val defs = root.defs.keys.flatMap(root.defs.get)
     val actual = Serialize.serializeDef(defs.head)
-    assert(expected == actual)
+    assert(actual == expected)
   }
 
   test("serialization.02") {
     val input =
       """
-        |@Test
-        |pub def main(x: Int32): Int32 = x + x
+        |pub def fun(x: Int32): Int32 = x + x
         |""".stripMargin
 
-    val tpe = serialization.Apply(serialization.Apply(serialization.Apply(serialization.Cst(serialization.Arrow(2)), serialization.Cst(serialization.Pure)), serialization.Cst(serialization.Int32)), serialization.Cst(serialization.Int32))
-    val scheme = serialization.SScheme(List.empty, tpe)
-    val expected = serialization.SDef(List.empty, "main", scheme, "<test>")
+    val tpe = Apply(Apply(Apply(Cst(Arrow(2)), Cst(Pure)), Cst(Int32)), Cst(Int32))
+    val scheme = SScheme(List.empty, tpe)
+    val expected = SDef(List.empty, "fun", scheme, "<test>")
 
-    val (Some(root), _) = check(input, Options.TestWithLibMin)
-    val defs = root.entryPoints.flatMap(root.defs.get).toList
+    val (Some(root), _) = check(input, Options.TestWithLibNix)
+    val defs = root.defs.keys.flatMap(root.defs.get)
     val actual = Serialize.serializeDef(defs.head)
-    assert(expected == actual)
+    assert(actual == expected)
   }
 
   test("serialization.03") {
     val input =
       """
-        |@Test
-        |pub def main(_: Int32): Unit = ()
+        |pub def toUnit(_: Int32): Unit = ()
         |""".stripMargin
 
-    val tpe = serialization.Apply(serialization.Apply(serialization.Apply(serialization.Cst(serialization.Arrow(2)), serialization.Cst(serialization.Pure)), serialization.Cst(serialization.Int32)), serialization.Cst(serialization.Unit))
-    val scheme = serialization.SScheme(List.empty, tpe)
-    val expected = serialization.SDef(List.empty, "main", scheme, "<test>")
+    val tpe = Apply(Apply(Apply(Cst(Arrow(2)), Cst(Pure)), Cst(Int32)), Cst(Unit))
+    val scheme = SScheme(List.empty, tpe)
+    val expected = SDef(List.empty, "toUnit", scheme, "<test>")
 
-    val (Some(root), _) = check(input, Options.TestWithLibMin)
-    val defs = root.entryPoints.flatMap(root.defs.get).toList
+    val (Some(root), _) = check(input, Options.TestWithLibNix)
+    val defs = root.defs.keys.flatMap(root.defs.get)
     val actual = Serialize.serializeDef(defs.head)
-    assert(expected == actual)
+    assert(actual == expected)
   }
 
   test("serialization.04") {
     val input =
       """
-        |@Test
-        |pub def main(): Int32 = 42
+        |pub def answer(): Int32 = 42
         |""".stripMargin
 
-    val tpe = serialization.Apply(serialization.Apply(serialization.Apply(serialization.Cst(serialization.Arrow(2)), serialization.Cst(serialization.Pure)), serialization.Cst(serialization.Unit)), serialization.Cst(serialization.Int32))
-    val scheme = serialization.SScheme(List.empty, tpe)
-    val expected = serialization.SDef(List.empty, "main", scheme, "<test>")
+    val tpe = Apply(Apply(Apply(Cst(Arrow(2)), Cst(Pure)), Cst(Unit)), Cst(Int32))
+    val scheme = SScheme(List.empty, tpe)
+    val expected = SDef(List.empty, "answer", scheme, "<test>")
 
-    val (Some(root), _) = check(input, Options.TestWithLibMin)
-    val defs = root.entryPoints.flatMap(root.defs.get).toList
+    val (Some(root), _) = check(input, Options.TestWithLibNix)
+    val defs = root.defs.keys.flatMap(root.defs.get)
     val actual = Serialize.serializeDef(defs.head)
-    assert(expected == actual)
+    assert(actual == expected)
+  }
+
+  test("serialization.05") {
+    val input =
+      """
+        |pub def toInt32(f: a -> Int32, x: a): Int32 = f(x)
+        |""".stripMargin
+
+    val tpe = Apply(Apply(Apply(Apply(Cst(Arrow(3)), Cst(Pure)), Apply(Apply(Apply(Cst(Arrow(2)), Cst(Pure)), Var(VarSym(0, Text("a"), StarKind))), Cst(Int32))), Var(VarSym(0, Text("a"), StarKind))), Cst(Int32))
+    val scheme = SScheme(List(VarSym(0, Text("a"), StarKind)), tpe)
+    val expected = SDef(List(), "toInt32", scheme, "<test>")
+
+    val (Some(root), _) = check(input, Options.TestWithLibNix)
+    val defs = root.defs.keys.flatMap(root.defs.get)
+    val actual = Serialize.serializeDef(defs.head)
+    assert(actual == expected)
   }
 
 }
