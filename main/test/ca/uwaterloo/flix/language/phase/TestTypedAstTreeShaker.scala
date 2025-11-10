bdf3326bbca5ae7e234d8e21306ad6dc9e1172d4
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2025-11-10 16:58:55 +0100

    add tests

--- main/test/ca/uwaterloo/flix/language/phase/TestTypedAstTreeShaker.scala
+++ main/test/ca/uwaterloo/flix/language/phase/TestTypedAstTreeShaker.scala
@@ -30,6 +30,113 @@ class TestTypedAstTreeShaker extends AnyFunSuite with TestUtils {
     expectReachable(input, expected)
   }
 
+  test("Reachable.03") {
+    val input =
+      """
+        |def main(): Unit =
+        |    A.f()
+        |
+        |mod A {
+        |    pub def f(): Unit = ()
+        |}
+        |
+        |def f(): Unit = g()
+        |
+        |def g(): Unit = ()
+        |""".stripMargin
+    val expected = Set("main", "A.f")
+    expectReachable(input, expected)
+  }
+
+  test("Reachable.04") {
+    val input =
+      """
+        |def main(): Unit =
+        |    A.f()
+        |
+        |mod A {
+        |    pub def f(): Unit = B.g()
+        |}
+        |
+        |mod B {
+        |    pub def g(): Unit = ()
+        |}
+        |""".stripMargin
+    val expected = Set("main", "A.f", "B.g")
+    expectReachable(input, expected)
+  }
+
+  test("Reachable.05") {
+    val input =
+      """
+        |@Test
+        |def test01(): Unit = ()
+        |""".stripMargin
+    val expected = Set("test01")
+    expectReachable(input, expected)
+  }
+
+  test("Reachable.06") {
+    val input =
+      """
+        |@Test
+        |def test01(): Unit = main()
+        |
+        |def main(): Unit = A.f()
+        |
+        |mod A {
+        |    pub def f(): Unit = B.g()
+        |}
+        |
+        |mod B {
+        |    pub def g(): Unit = ()
+        |}
+        |
+        |""".stripMargin
+    val expected = Set("test01", "main", "A.f", "B.g")
+    expectReachable(input, expected)
+  }
+
+  test("Reachable.08") {
+    val input =
+      """
+        |@Test
+        |def test01(): Unit = C.f()
+
+        |mod C {
+        |    pub def f(): Unit = ()
+        |}
+        |
+        |""".stripMargin
+    val expected = Set("test01", "C.f")
+    expectReachable(input, expected)
+  }
+
+  test("Reachable.09") {
+    val input =
+      """
+        |@Test
+        |def test01(): Unit = C.f()
+        |
+        |def main(): Unit = A.f()
+        |
+        |mod A {
+        |    pub def f(): Unit = B.g()
+        |}
+        |
+        |mod B {
+        |    pub def g(): Unit = ()
+        |}
+        |
+        |mod C {
+        |    pub def f(): Unit = ()
+        |}
+        |
+        |""".stripMargin
+    val expected = Set("test01", "main", "A.f", "B.g", "C.f")
+    expectReachable(input, expected)
+  }
+
   /** Asserts that the program contains exactly the functions `expected` after running [[Flix.treeshake]] */
   private def expectReachable(input: String, expected: Set[String], options: Options = Options.TestWithLibNix): Unit = {
     val (optRoot, errors) = check(input, options)
