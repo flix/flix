09ce06f6f04fa4ead2759a4fdcbd52d77f466822
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2025-11-10 11:55:28 +0100

    update error msg

--- main/src/ca/uwaterloo/flix/api/Bootstrap.scala
+++ main/src/ca/uwaterloo/flix/api/Bootstrap.scala
@@ -462,7 +462,7 @@ class Bootstrap(val projectPath: Path, apiKey: Option[String]) {
     // Delete files paths
     for (file <- files) {
       FileOps.delete(file) match {
-        case Err(e) => return Err(BootstrapError.FileError(s"exception in deletion: ${e.getMessage}"))
+        case Err(e) => return Err(BootstrapError.FileError(s"exception in deletion: ${e.getMessage}\n$e"))
         case Ok(_) => ()
       }
     }
@@ -476,7 +476,7 @@ class Bootstrap(val projectPath: Path, apiKey: Option[String]) {
       }
 
       FileOps.delete(dir) match {
-        case Err(e) => return Err(BootstrapError.FileError(s"exception in deletion: ${e.getMessage}"))
+        case Err(e) => return Err(BootstrapError.FileError(s"exception in deletion: ${e.getMessage}\n$e"))
         case Ok(_) => ()
       }
     }
