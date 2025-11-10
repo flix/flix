4750ff52d6d7636d1449201dc8d78619b54f680b
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2025-11-10 15:39:49 +0100

    feat: add TypedAstTreeShaker

---
+++ main/src/ca/uwaterloo/flix/language/phase/TypedAstTreeShaker.scala
@@ -0,0 +1,339 @@
+/*
+ * Copyright 2017 Magnus Madsen, 2022 Jakob Schneider Villumsen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ca.uwaterloo.flix.language.phase
+
+import ca.uwaterloo.flix.api.Flix
+import ca.uwaterloo.flix.language.ast.TypedAst.*
+import ca.uwaterloo.flix.language.ast.Symbol
+import ca.uwaterloo.flix.language.ast.shared.SymUse
+import ca.uwaterloo.flix.language.dbg.AstPrinter.DebugTypedAst
+import ca.uwaterloo.flix.util.ParOps
+
+/**
+  * The Tree Shaking phase removes all unused function definitions.
+  *
+  * A function is considered reachable if it:
+  *   - Is an entry point (main / test / export).
+  *   - Appears in a function which itself is reachable.
+  *   - Is an instance of a trait whose signature(s) appear in a reachable function.
+  */
+object TypedAstTreeShaker {
+
+  /** Performs tree shaking on `root`. */
+  def run(root: Root)(implicit flix: Flix): Root = flix.phase("TypedAstTreeShaker") {
+    val initReach: Set[ReachableSym] = root.entryPoints.map(ReachableSym.DefnSym.apply)
+
+    val loweringTargets: Set[ReachableSym] = root.defs.filter {
+      case (_, defn) => defn.spec.ann.isLoweringTarget
+    }.keys.map(ReachableSym.DefnSym.apply).toSet
+
+    // Compute the symbols that are transitively reachable.
+    val allReachable = ParOps.parReach(initReach ++ loweringTargets, visitSym(_, root))
+
+    // Filter the reachable definitions.
+    val reachableDefs = root.defs.filter {
+      case (sym, _) => allReachable.contains(ReachableSym.DefnSym(sym))
+    }
+
+    root.copy(defs = reachableDefs)
+  }
+
+  /** Returns the symbols reachable from `sym`. */
+  private def visitSym(sym: ReachableSym, root: Root): Set[ReachableSym] = sym match {
+    case ReachableSym.DefnSym(defnSym) =>
+      val defn = root.defs(defnSym)
+      visitExp(defn.exp)
+
+    case ReachableSym.SigSym(sigSym) =>
+      val sig = root.sigs(sigSym)
+      Set(ReachableSym.TraitSym(sig.sym.trt)) ++
+        sig.exp.map(visitExp).getOrElse(Set.empty)
+
+    case ReachableSym.TraitSym(traitSym) =>
+      root.instances(traitSym).foldLeft(Set.empty[ReachableSym]) {
+        case (acc, s) => visitExps(s.defs.map(_.exp)) ++ acc
+      }
+  }
+
+  /** Returns the symbols reachable from `e0`. */
+  private def visitExp(e0: Expr): Set[ReachableSym] = e0 match {
+    case Expr.Cst(_, _, _) =>
+      Set.empty
+
+    case Expr.Var(_, _, _) =>
+      Set.empty
+
+    case Expr.Hole(_, _, _, _, _) =>
+      Set.empty
+
+    case Expr.HoleWithExp(exp, _, _, _, _) =>
+      visitExp(exp)
+
+    case Expr.OpenAs(_, exp, _, _) =>
+      visitExp(exp)
+
+    case Expr.Use(_, _, exp, _) =>
+      visitExp(exp)
+
+    case Expr.Lambda(_, exp, _, _) =>
+      visitExp(exp)
+
+    case Expr.ApplyClo(exp1, exp2, _, _, _) =>
+      visitExp(exp1) ++ visitExp(exp2)
+
+    case Expr.ApplyDef(SymUse.DefSymUse(sym, _), exps, _, _, _, _, _) =>
+      Set(ReachableSym.DefnSym(sym)) ++ visitExps(exps)
+
+    case Expr.ApplyLocalDef(_, exps, _, _, _, _) =>
+      visitExps(exps)
+
+    case Expr.ApplyOp(_, exps, _, _, _) =>
+      visitExps(exps)
+
+    case Expr.ApplySig(SymUse.SigSymUse(sym, _), exps, _, _, _, _, _, _) =>
+      Set(ReachableSym.SigSym(sym)) ++ visitExps(exps)
+
+    case Expr.Unary(_, exp, _, _, _) =>
+      visitExp(exp)
+
+    case Expr.Binary(_, exp1, exp2, _, _, _) =>
+      visitExp(exp1) ++ visitExp(exp2)
+
+    case Expr.Let(_, exp1, exp2, _, _, _) =>
+      visitExp(exp1) ++ visitExp(exp2)
+
+    case Expr.LocalDef(_, _, exp1, exp2, _, _, _) =>
+      visitExp(exp1) ++ visitExp(exp2)
+
+    case Expr.Region(_, _, exp, _, _, _) =>
+      visitExp(exp)
+
+    case Expr.IfThenElse(exp1, exp2, exp3, _, _, _) =>
+      visitExp(exp1) ++ visitExp(exp2) ++ visitExp(exp3)
+
+    case Expr.Stm(exp1, exp2, _, _, _) =>
+      visitExp(exp1) ++ visitExp(exp2)
+
+    case Expr.Discard(exp, _, _) =>
+      visitExp(exp)
+
+    case Expr.Match(exp, rules, _, _, _) =>
+      visitExp(exp) ++ visitExps(rules.map(_.exp)) ++ visitExps(rules.flatMap(_.guard))
+
+    case Expr.ExtMatch(exp, rules, _, _, _) =>
+      visitExp(exp) ++ visitExps(rules.map(_.exp))
+
+    case Expr.TypeMatch(exp, rules, _, _, _) =>
+      visitExp(exp) ++ visitExps(rules.map(_.exp))
+
+    case Expr.RestrictableChoose(_, exp, rules, _, _, _) =>
+      visitExp(exp) ++ visitExps(rules.map(r => r.exp))
+
+    case Expr.Tag(_, exps, _, _, _) =>
+      visitExps(exps)
+
+    case Expr.RestrictableTag(_, exps, _, _, _) =>
+      visitExps(exps)
+
+    case Expr.ExtTag(_, exps, _, _, _) =>
+      visitExps(exps)
+
+    case Expr.Tuple(exps, _, _, _) =>
+      visitExps(exps)
+
+    case Expr.RecordSelect(exp, _, _, _, _) =>
+      visitExp(exp)
+
+    case Expr.RecordExtend(_, exp1, exp2, _, _, _) =>
+      visitExp(exp1) ++ visitExp(exp2)
+
+    case Expr.RecordRestrict(_, exp, _, _, _) =>
+      visitExp(exp)
+
+    case Expr.ArrayLit(exps, exp, _, _, _) =>
+      visitExps(exps) ++ visitExp(exp)
+
+    case Expr.ArrayNew(exp1, exp2, exp3, _, _, _) =>
+      visitExp(exp1) ++ visitExp(exp2) ++ visitExp(exp3)
+
+    case Expr.ArrayLoad(exp1, exp2, _, _, _) =>
+      visitExp(exp1) ++ visitExp(exp2)
+
+    case Expr.ArrayLength(exp, _, _) =>
+      visitExp(exp)
+
+    case Expr.ArrayStore(exp1, exp2, exp3, _, _) =>
+      visitExp(exp1) ++ visitExp(exp2) ++ visitExp(exp3)
+
+    case Expr.StructNew(_, fields, exp, _, _, _) =>
+      visitExps(fields.map { case (_, e) => e }) ++ exp.map(visitExp).getOrElse(Set.empty)
+
+    case Expr.StructGet(exp, _, _, _, _) =>
+      visitExp(exp)
+
+    case Expr.StructPut(exp1, _, exp2, _, _, _) =>
+      visitExp(exp1) ++ visitExp(exp2)
+
+    case Expr.VectorLit(exps, _, _, _) =>
+      visitExps(exps)
+
+    case Expr.VectorLoad(exp1, exp2, _, _, _) =>
+      visitExp(exp1) ++ visitExp(exp2)
+
+    case Expr.VectorLength(exp, _) =>
+      visitExp(exp)
+
+    case Expr.Ascribe(exp, _, _, _, _, _) =>
+      visitExp(exp)
+
+    case Expr.InstanceOf(exp, _, _) =>
+      visitExp(exp)
+
+    case Expr.CheckedCast(_, exp, _, _, _) =>
+      visitExp(exp)
+
+    case Expr.UncheckedCast(exp, _, _, _, _, _) =>
+      visitExp(exp)
+
+    case Expr.Unsafe(exp, _, _, _, _) =>
+      visitExp(exp)
+
+    case Expr.Without(exp, _, _, _, _) =>
+      visitExp(exp)
+
+    case Expr.TryCatch(exp, rules, _, _, _) =>
+      visitExp(exp) ++ visitExps(rules.map(_.exp))
+
+    case Expr.Throw(exp, _, _, _) =>
+      visitExp(exp)
+
+    case Expr.Handler(_, rules, _, _, _, _, _) =>
+      visitExps(rules.map(r => r.exp))
+
+    case Expr.RunWith(exp1, exp2, _, _, _) =>
+      visitExp(exp1) ++ visitExp(exp2)
+
+    case Expr.InvokeConstructor(_, exps, _, _, _) =>
+      visitExps(exps)
+
+    case Expr.InvokeMethod(_, exp, exps, _, _, _) =>
+      visitExp(exp) ++ visitExps(exps)
+
+    case Expr.InvokeStaticMethod(_, exps, _, _, _) =>
+      visitExps(exps)
+
+    case Expr.GetField(_, exp, _, _, _) =>
+      visitExp(exp)
+
+    case Expr.PutField(_, exp1, exp2, _, _, _) =>
+      visitExp(exp1) ++ visitExp(exp2)
+
+    case Expr.GetStaticField(_, _, _, _) =>
+      Set.empty
+
+    case Expr.PutStaticField(_, exp, _, _, _) =>
+      visitExp(exp)
+
+    case Expr.NewObject(_, _, _, _, methods, _) =>
+      visitExps(methods.map(_.exp))
+
+    case Expr.NewChannel(exp, _, _, _) =>
+      visitExp(exp)
+
+    case Expr.GetChannel(exp, _, _, _) =>
+      visitExp(exp)
+
+    case Expr.PutChannel(exp1, exp2, _, _, _) =>
+      visitExp(exp1) ++ visitExp(exp2)
+
+    case Expr.SelectChannel(selects, optExp, _, _, _) =>
+      visitExps(selects.map(_.exp)) ++ visitExps(selects.map(_.chan)) ++ optExp.map(visitExp).getOrElse(Set.empty)
+
+    case Expr.Spawn(exp1, exp2, _, _, _) =>
+      visitExp(exp1) ++ visitExp(exp2)
+
+    case Expr.ParYield(frags, exp, _, _, _) =>
+      visitExps(frags.map(f => f.exp)) ++ visitExp(exp)
+
+    case Expr.Lazy(exp, _, _) =>
+      visitExp(exp)
+
+    case Expr.Force(exp, _, _, _) =>
+      visitExp(exp)
+
+    case Expr.FixpointConstraintSet(constrs, _, _) =>
+      constrs.flatMap(visitFixpointConstraint).toSet
+
+    case Expr.FixpointLambda(_, exp, _, _, _) =>
+      visitExp(exp)
+
+    case Expr.FixpointMerge(exp1, exp2, _, _, _) =>
+      visitExp(exp1) ++ visitExp(exp2)
+
+    case Expr.FixpointQueryWithProvenance(exps, select, _, _, _, _) =>
+      visitExps(exps) ++ visitPredicateHead(select)
+
+    case Expr.FixpointQueryWithSelect(exps1, exp, exps2, from, exps3, _, _, _, _) =>
+      visitExps(exps1) ++ visitExp(exp) ++ visitExps(exps2) ++ from.flatMap(visitPredicateBody) ++ visitExps(exps3)
+
+    case Expr.FixpointSolveWithProject(exps, _, _, _, _, _) =>
+      visitExps(exps)
+
+    case Expr.FixpointInjectInto(exps, _, _, _, _) =>
+      visitExps(exps)
+
+    case Expr.Error(_, _, _) =>
+      Set.empty
+
+  }
+
+  /** Returns the symbols reachable from `exps`. */
+  private def visitExps(exps: List[Expr]): Set[ReachableSym] =
+    exps.map(visitExp).fold(Set())(_ ++ _)
+
+  /** Returns the symbols reachable from `head0`. */
+  private def visitFixpointConstraint(constr0: Constraint): Set[ReachableSym] = constr0 match {
+    case Constraint(_, head, body, _) =>
+      visitPredicateHead(head) ++ body.flatMap(visitPredicateBody)
+  }
+
+  /** Returns the symbols reachable from `head0`. */
+  private def visitPredicateHead(head0: Predicate.Head): Set[ReachableSym] = head0 match {
+    case Predicate.Head.Atom(_, _, exps, _, _) => visitExps(exps)
+  }
+
+  /** Returns the symbols reachable from `body0`. */
+  private def visitPredicateBody(body0: Predicate.Body): Set[ReachableSym] = body0 match {
+    case Predicate.Body.Atom(_, _, _, _, _, _, _) => Set.empty
+    case Predicate.Body.Functional(_, exp, _) => visitExp(exp)
+    case Predicate.Body.Guard(exp, _) => visitExp(exp)
+  }
+
+  /** Reachable symbols (defs, traits, sigs). */
+  private sealed trait ReachableSym
+
+  private object ReachableSym {
+
+    case class DefnSym(sym: Symbol.DefnSym) extends ReachableSym
+
+    case class TraitSym(sym: Symbol.TraitSym) extends ReachableSym
+
+    case class SigSym(sym: Symbol.SigSym) extends ReachableSym
+
+  }
+
+}
