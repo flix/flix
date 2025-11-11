4fef3291a8ca0a2e36062a99428c3eb65a2f9119
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2025-11-11 15:19:35 +0100

    add EffectUpgrade

---
+++ main/src/ca/uwaterloo/flix/api/effectlock/EffectUpgrade.scala
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2025 Jakob Schneider Villumsen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ca.uwaterloo.flix.api.effectlock
+
+import ca.uwaterloo.flix.api.Flix
+import ca.uwaterloo.flix.language.ast.Scheme
+
+object EffectUpgrade {
+
+  def isSafe(sc01: Scheme, sc02: Scheme)(implicit flix: Flix): Boolean = {
+    val sc1 = alpha(sc01)
+    val sc2 = alpha(sc02)
+    isGeneralizable(sc1, sc2) || isSubset(sc1, sc2)
+  }
+
+  private def isGeneralizable(sc01: Scheme, sc02: Scheme)(implicit flix: Flix): Boolean = ???
+
+  private def isSubset(sc01: Scheme, sc02: Scheme)(implicit flix: Flix): Boolean = ???
+
+  private def alpha(sc0: Scheme): Scheme = ???
+
+}
