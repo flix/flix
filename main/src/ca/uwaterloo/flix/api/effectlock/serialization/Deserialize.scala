cea7fa608e45cf65cb90a63aadf80818eac6d90e
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2025-11-13 10:53:24 +0100

    add functions

--- main/src/ca/uwaterloo/flix/api/effectlock/serialization/Deserialize.scala
+++ main/src/ca/uwaterloo/flix/api/effectlock/serialization/Deserialize.scala
@@ -1,16 +1,50 @@
 package ca.uwaterloo.flix.api.effectlock.serialization
 
-import ca.uwaterloo.flix.language.ast.{Scheme, Symbol}
+import ca.uwaterloo.flix.api.Flix
+import ca.uwaterloo.flix.language.ast.shared.{Scope, VarText}
+import ca.uwaterloo.flix.language.ast.{Kind, Scheme, SourceLocation, Symbol}
 
 object Deserialize {
 
-  def deserializeDef(defn0: SDef): (Symbol.DefnSym, Scheme) = defn0 match {
+  def deserializeDef(defn0: SDef)(implicit flix: Flix): (Symbol.DefnSym, Scheme) = defn0 match {
     case SDef(namespace, text, scheme, source) => // source unused?
       val sym = Symbol.mkDefnSym(namespace.mkString("", ".", s".$text"))
       val sc = deserializeScheme(scheme)
       sym -> sc
   }
 
-  private def deserializeScheme(sc0: SScheme): Scheme = ???
+  private def deserializeScheme(sc0: SScheme)(implicit flix: Flix): Scheme = sc0 match {
+    case SScheme(quantifiers, tconstrs, econstrs, base) =>
+      val qs = quantifiers.map(deserializeKindedTypeVarSym)
+      Scheme(qs, ???, ???, ???)
+  }
+
+  private def deserializeKind(kind0: SKind): Kind = kind0 match {
+    case WildKind => Kind.Wild
+    case WildCaseSetKind => Kind.WildCaseSet
+    case StarKind => Kind.Star
+    case EffKind => Kind.Eff
+    case BoolKind => Kind.Bool
+    case RecordRowKind => Kind.RecordRow
+    case SchemaRowKind => Kind.SchemaRow
+    case PredicateKind => Kind.Predicate
+    case JvmKind => Kind.Jvm
+    case CaseSetKind(sym) => Kind.CaseSet(deserializeRestrictableEnumSym(sym))
+    case ArrowKind(k1, k2) => Kind.Arrow(deserializeKind(k1), deserializeKind(k2))
+  }
+
+  private def deserializeKindedTypeVarSym(sym0: VarSym)(implicit flix: Flix): Symbol.KindedTypeVarSym = sym0 match {
+    case VarSym(text, kind) =>
+      val t = deserializeVarText(text)
+      val k = deserializeKind(kind)
+      Symbol.freshKindedTypeVarSym(t, k, isSlack = false, SourceLocation.Unknown)(Scope.Top, flix)
+  }
+
+  private def deserializeRestrictableEnumSym(sym0: RestrictableEnumSym): Symbol.RestrictableEnumSym = ???
+
+  private def deserializeVarText(text0: SVarText): VarText = text0 match {
+    case Absent => VarText.Absent
+    case Text(s) => VarText.SourceText(s)
+  }
 
 }
