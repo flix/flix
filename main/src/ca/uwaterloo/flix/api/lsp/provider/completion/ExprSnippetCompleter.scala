5630090c7dd7b15016809fec7d8878770a47bdab
NeonOxide <neoexdeus@gmail.com>
2025-11-13 13:37:25 +0100

    Added wildcards for VsCode to fill. Also added extra annotation completion.

--- main/src/ca/uwaterloo/flix/api/lsp/provider/completion/ExprSnippetCompleter.scala
+++ main/src/ca/uwaterloo/flix/api/lsp/provider/completion/ExprSnippetCompleter.scala
@@ -18,12 +18,14 @@ package ca.uwaterloo.flix.api.lsp.provider.completion
 import ca.uwaterloo.flix.api.lsp.Range
 
 object ExprSnippetCompleter {
+  val DefaultHandlerSnippetGenerator =  (label, range) =>
+    Completion.SnippetCompletion(label, range, Priority.Low(0),
+      "@DefaultHandler\npub def runWithIO(f: Unit -> a \\ ef): a \\ (ef - ${1:Eff}) + IO = \n    run {\n        f()\n    } with ${0:?HandlerForEff}",
+      "snippet for a default handler for Eff")
 
   def getCompletions(range: Range): Iterable[Completion] = List(
     // NB: Please keep the list alphabetically sorted.
-    Completion.SnippetCompletion("default handler", range, Priority.Low(0),
-      "@DefaultHandler\npub def runWithIO(f: Unit -> a \\ ef): a \\ (ef - Eff) + IO = \n    run {\n        f()\n    } with ?HandlerForEff",
-      "snippet for a default handler for Eff"),
+    DefaultHandlerSnippetGenerator("default handler", range),
     Completion.SnippetCompletion("main", range, Priority.High(0),
       "def main(): Unit \\ IO = \n    println(\"Hello World!\")",
       "snippet for Hello World Program"),
