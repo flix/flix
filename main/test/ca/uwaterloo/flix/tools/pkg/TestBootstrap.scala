a42f465e5599f7c37a64bacaa4a846215ec693ca
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2025-11-21 15:03:09 +0100

    use mkFlixInstance

--- main/test/ca/uwaterloo/flix/tools/pkg/TestBootstrap.scala
+++ main/test/ca/uwaterloo/flix/tools/pkg/TestBootstrap.scala
@@ -40,7 +40,7 @@ class TestBootstrap extends AnyFunSuite {
     val p = Files.createTempDirectory(ProjectPrefix)
     Bootstrap.init(p)(System.out)
     val b = Bootstrap.bootstrap(p, None)(Formatter.getDefault, System.out).unsafeGet
-    val flix = new Flix
+    val flix = mkFlixInstance
     b.build(flix)
     b.buildJar(flix)(Formatter.getDefault)
 
@@ -54,7 +54,7 @@ class TestBootstrap extends AnyFunSuite {
     val p = Files.createTempDirectory(ProjectPrefix)
     Bootstrap.init(p)(System.out)
     val b = Bootstrap.bootstrap(p, None)(Formatter.getDefault, System.out).unsafeGet
-    val flix = new Flix
+    val flix = mkFlixInstance
     b.build(flix)
     b.buildJar(flix)(Formatter.getDefault)
 
