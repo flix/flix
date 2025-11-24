9315b1f55e839700b1d960b7c2bc2b8842e83e84
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2025-11-24 12:31:48 +0100

    experiment: update sorting

--- main/src/ca/uwaterloo/flix/util/FileOps.scala
+++ main/src/ca/uwaterloo/flix/util/FileOps.scala
@@ -288,17 +288,8 @@ object FileOps {
   def sortPlatformIndependently(prefix: Path, paths: List[Path]): List[(Path, String)] = {
     require(paths.forall(_.startsWith(prefix)), "All paths in 'paths' must start with 'prefix'.")
     paths.map { path =>
-      (path, convertPathToRelativeFileName(prefix, path))
+      (path, prefix.relativize(path).toString)
     }.sortBy(_._2)
-  }
-
-  /**
-    * @param prefix the root directory to compute a relative path from the given path
-    * @param path   the path to be converted to a relative path based on the given root directory
-    * @return relative file name separated by slashes, like `path/to/file.ext`
-    */
-  private def convertPathToRelativeFileName(prefix: Path, path: Path): String = {
-    prefix.relativize(path).toString.replace('\\', '/')
   }
 
 }
