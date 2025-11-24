88cd17091ac55e5d220db6392bce9bafc76ae885
Jakob Schneider <60233376+jaschdoc@users.noreply.github.com>
2025-11-24 18:46:05 +0100

    refactor: reuse semver parsing (#12078)
    Co-authored-by: Magnus Madsen <magnus-madsen@users.noreply.github.com>

--- main/src/ca/uwaterloo/flix/tools/pkg/github/GitHub.scala
+++ main/src/ca/uwaterloo/flix/tools/pkg/github/GitHub.scala
@@ -312,11 +312,14 @@ object GitHub {
     *
     * * `v2.3.4`
     */
-  private def parseSemVer(string: String): SemVer = {
-    val semVer = """v(\d+)\.(\d+)\.(\d+)""".r
-    string match {
-      case semVer(major, minor, patch) => SemVer(major.toInt, minor.toInt, patch.toInt)
-      case _ => throw new RuntimeException(s"Invalid semantic version: $string")
+  private def parseSemVer(str: String): SemVer = {
+    val (v, num) = str.splitAt(1)
+    if (v != "v") {
+      throw new RuntimeException(s"Invalid semantic version: $str")
+    }
+    SemVer.ofString(num) match {
+      case Some(semver) => semver
+      case _ => throw new RuntimeException(s"Invalid semantic version: $str")
     }
   }
 }
