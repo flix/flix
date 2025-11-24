9eee542d29befbde26ecd10c79a0c6f6ca884b69
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2025-11-24 16:19:31 +0100

    add impl

--- main/src/ca/uwaterloo/flix/api/effectlock/serialization/Deserialize.scala
+++ main/src/ca/uwaterloo/flix/api/effectlock/serialization/Deserialize.scala
@@ -1,8 +1,12 @@
 package ca.uwaterloo.flix.api.effectlock.serialization
 
 import ca.uwaterloo.flix.api.Flix
-import ca.uwaterloo.flix.language.ast.shared.{Scope, VarText}
-import ca.uwaterloo.flix.language.ast.{Kind, Name, Scheme, SourceLocation, Symbol}
+import ca.uwaterloo.flix.language.ast.shared.SymUse.{AssocTypeSymUse, TraitSymUse, TypeAliasSymUse}
+import ca.uwaterloo.flix.language.ast.shared.{Scope, TraitConstraint, VarText}
+import ca.uwaterloo.flix.language.ast.{Kind, Name, Scheme, SourceLocation, Symbol, Type, TypeConstructor}
+
+import java.lang.reflect.{Constructor, Field, Method}
+import scala.collection.immutable.SortedSet
 
 object Deserialize {
 
@@ -16,7 +20,80 @@ object Deserialize {
   private def deserializeScheme(sc0: SScheme)(implicit flix: Flix): Scheme = sc0 match {
     case SScheme(quantifiers, tconstrs, econstrs, base) =>
       val qs = quantifiers.map(deserializeKindedTypeVarSym)
-      Scheme(qs, ???, ???, ???)
+      Scheme(qs, tconstrs.map(deserializeTraitConstraint), ???, deserializeType(base))
+  }
+
+  private def deserializeType(tpe0: SType)(implicit flix: Flix): Type = tpe0 match {
+    case Var(sym) => Type.Var(deserializeKindedTypeVarSym(sym), SourceLocation.Unknown)
+    case Cst(tc) => Type.Cst(deserializeTypeConstructor(tc), SourceLocation.Unknown)
+    case Apply(tpe1, tpe2) => Type.Apply(deserializeType(tpe1), deserializeType(tpe2), SourceLocation.Unknown)
+    case Alias(symUse, args, tpe) => Type.Alias(deserializeTypeAliasSymUse(symUse), args.map(deserializeType), deserializeType(tpe), SourceLocation.Unknown)
+    case AssocType(symUse, arg, kind) => Type.AssocType(deserializeAssocTypeSymUse(symUse), deserializeType(arg), deserializeKind(kind), SourceLocation.Unknown)
+  }
+
+  private def deserializeTypeConstructor(tc0: STC)(implicit flix: Flix): TypeConstructor = tc0 match {
+    case Void => TypeConstructor.Void
+    case AnyType => TypeConstructor.AnyType
+    case Unit => TypeConstructor.Unit
+    case Null => TypeConstructor.Null
+    case Bool => TypeConstructor.Bool
+    case Char => TypeConstructor.Char
+    case Float32 => TypeConstructor.Float32
+    case Float64 => TypeConstructor.Float64
+    case BigDecimal => TypeConstructor.BigDecimal
+    case Int8 => TypeConstructor.Int8
+    case Int16 => TypeConstructor.Int16
+    case Int32 => TypeConstructor.Int32
+    case Int64 => TypeConstructor.Int64
+    case BigInt => TypeConstructor.BigInt
+    case Str => TypeConstructor.Str
+    case Regex => TypeConstructor.Regex
+    case Arrow(arity) => TypeConstructor.Arrow(arity)
+    case ArrowWithoutEffect(arity) => TypeConstructor.ArrowWithoutEffect(arity)
+    case RecordRowEmpty => TypeConstructor.RecordRowEmpty
+    case RecordRowExtend(label) => TypeConstructor.RecordRowExtend(deserializeLabel(label))
+    case Record => TypeConstructor.Record
+    case Extensible => TypeConstructor.Extensible
+    case SchemaRowEmpty => TypeConstructor.SchemaRowEmpty
+    case SchemaRowExtend(pred) => TypeConstructor.SchemaRowExtend(deserializePred(pred))
+    case Schema => TypeConstructor.Schema
+    case Sender => TypeConstructor.Sender
+    case Receiver => TypeConstructor.Receiver
+    case Lazy => TypeConstructor.Lazy
+    case Enum(sym, kind) => TypeConstructor.Enum(deserializeEnumSym(sym), deserializeKind(kind))
+    case Struct(sym, kind) => TypeConstructor.Struct(deserializeStructSym(sym), deserializeKind(kind))
+    case RestrictableEnum(sym, kind) => TypeConstructor.RestrictableEnum(deserializeRestrictableEnumSym(sym), deserializeKind(kind))
+    case Native(clazz) => TypeConstructor.Native(deserializeJvmClass(clazz))
+    case JvmConstructor(constructor) => TypeConstructor.JvmConstructor(deserializeJvmConstructor(constructor))
+    case JvmMethod(method) => TypeConstructor.JvmMethod(deserializeJvmMethod(method))
+    case JvmField(field) => TypeConstructor.JvmField(deserializeJvmField(field))
+    case Array => TypeConstructor.Array
+    case ArrayWithoutRegion => TypeConstructor.ArrayWithoutRegion
+    case Vector => TypeConstructor.Vector
+    case Tuple(arity) => TypeConstructor.Tuple(arity)
+    case Relation(arity) => TypeConstructor.Relation(arity)
+    case Lattice(arity) => TypeConstructor.Lattice(arity)
+    case True => TypeConstructor.True
+    case False => TypeConstructor.False
+    case Not => TypeConstructor.Not
+    case And => TypeConstructor.And
+    case Or => TypeConstructor.Or
+    case Pure => TypeConstructor.Pure
+    case Univ => TypeConstructor.Univ
+    case Complement => TypeConstructor.Complement
+    case Union => TypeConstructor.Union
+    case Intersection => TypeConstructor.Intersection
+    case Difference => TypeConstructor.Difference
+    case SymmetricDiff => TypeConstructor.SymmetricDiff
+    case Effect(sym, kind) => TypeConstructor.Effect(deserializeEffectSym(sym), deserializeKind(kind))
+    case CaseComplement(sym) => TypeConstructor.CaseComplement(deserializeRestrictableEnumSym(sym))
+    case CaseUnion(sym) => TypeConstructor.CaseUnion(deserializeRestrictableEnumSym(sym))
+    case CaseIntersection(sym) => TypeConstructor.CaseIntersection(deserializeRestrictableEnumSym(sym))
+    case CaseSymmetricDiff(sym) => TypeConstructor.CaseSymmetricDiff(deserializeRestrictableEnumSym(sym))
+    case CaseSet(syms, enumSym) => TypeConstructor.CaseSet(SortedSet.from(syms.map(deserializeRestrictableCaseSym)), deserializeRestrictableEnumSym(enumSym))
+    case Region(sym) => TypeConstructor.Region(deserializeRegionSym(sym))
+    case RegionToStar => TypeConstructor.RegionToStar
+    case RegionWithoutRegion => TypeConstructor.RegionWithoutRegion
   }
 
   private def deserializeKind(kind0: SKind): Kind = kind0 match {
@@ -33,6 +110,41 @@ object Deserialize {
     case ArrowKind(k1, k2) => Kind.Arrow(deserializeKind(k1), deserializeKind(k2))
   }
 
+  private def deserializeAssocTypeSym(sym0: AssocTypeSym): Symbol.AssocTypeSym = sym0 match {
+    case AssocTypeSym(trt, name) =>
+      Symbol.mkAssocTypeSym(deserializeTraitSym(trt), deserializeIdent(name))
+  }
+
+  private def deserializeAssocTypeSymUse(symUse0: AssocTypeSym): AssocTypeSymUse = {
+    AssocTypeSymUse(deserializeAssocTypeSym(symUse0), SourceLocation.Unknown)
+  }
+
+  private def deserializeEffectSym(sym0: EffSym): Symbol.EffSym = sym0 match {
+    case EffSym(namespace, name) =>
+      Symbol.mkEffSym(deserializeNamespace(namespace), deserializeIdent(name))
+  }
+
+  private def deserializeEnumSym(sym0: EnumSym): Symbol.EnumSym = sym0 match {
+    case EnumSym(namespace, text) =>
+      Symbol.mkEnumSym(deserializeNamespace(namespace), deserializeIdent(text))
+  }
+
+  private def deserializeJvmClass(clazz0: String): Class[?] = {
+    Class.forName(clazz0)
+  }
+
+  private def deserializeJvmConstructor(constructor0: String): Constructor[?] = {
+    ???
+  }
+
+  private def deserializeJvmMethod(method0: String): Method = {
+    ???
+  }
+
+  private def deserializeJvmField(field0: String): Field = {
+    ???
+  }
+
   private def deserializeKindedTypeVarSym(sym0: VarSym)(implicit flix: Flix): Symbol.KindedTypeVarSym = sym0 match {
     case VarSym(text, kind) =>
       val t = deserializeVarText(text)
@@ -40,9 +152,46 @@ object Deserialize {
       Symbol.freshKindedTypeVarSym(t, k, isSlack = false, SourceLocation.Unknown)(Scope.Top, flix)
   }
 
+  private def deserializeLabel(label0: String): Name.Label = {
+    Name.Label(label0, SourceLocation.Unknown)
+  }
+
+  private def deserializePred(pred0: String): Name.Pred = {
+    Name.Pred(pred0, SourceLocation.Unknown)
+  }
+
+  private def deserializeRegionSym(sym0: RegionSym)(implicit flix: Flix): Symbol.RegionSym = sym0 match {
+    case RegionSym(text) =>
+      Symbol.freshRegionSym(deserializeIdent(text))
+  }
+
   private def deserializeRestrictableEnumSym(sym0: RestrictableEnumSym): Symbol.RestrictableEnumSym = sym0 match {
     case RestrictableEnumSym(ns, name, cases) =>
-      new Symbol.RestrictableEnumSym(ns, name, cases.map(deserializeIdent), SourceLocation.Unknown)
+      Symbol.mkRestrictableEnumSym(deserializeNamespace(ns), deserializeIdent(name), cases.map(deserializeIdent))
+  }
+
+  private def deserializeRestrictableCaseSym(sym0: RestrictableCaseSym): Symbol.RestrictableCaseSym = sym0 match {
+    case RestrictableCaseSym(enumSym, name) =>
+      Symbol.mkRestrictableCaseSym(deserializeRestrictableEnumSym(enumSym), deserializeIdent(name))
+  }
+
+  private def deserializeStructSym(sym0: StructSym): Symbol.StructSym = sym0 match {
+    case StructSym(namespace, text) =>
+      Symbol.mkStructSym(deserializeNamespace(namespace), deserializeIdent(text))
+  }
+
+  private def deserializeTraitSym(sym0: TraitSym): Symbol.TraitSym = sym0 match {
+    case TraitSym(namespace, name) =>
+      Symbol.mkTraitSym(deserializeNamespace(namespace), deserializeIdent(name))
+  }
+
+  private def deserializeTypeAliasSym(sym0: TypeAliasSym): Symbol.TypeAliasSym = sym0 match {
+    case TypeAliasSym(namespace, name) =>
+      Symbol.mkTypeAliasSym(deserializeNamespace(namespace), deserializeIdent(name))
+  }
+
+  private def deserializeTypeAliasSymUse(symUse0: TypeAliasSym): TypeAliasSymUse = {
+    TypeAliasSymUse(deserializeTypeAliasSym(symUse0), SourceLocation.Unknown)
   }
 
   private def deserializeVarText(text0: SVarText): VarText = text0 match {
@@ -52,6 +201,15 @@ object Deserialize {
 
   private def deserializeIdent(ident0: String): Name.Ident = {
     Name.Ident(ident0, SourceLocation.Unknown)
+  }
+
+  private def deserializeNamespace(ns0: List[String]): Name.NName = {
+    Name.NName(ns0.map(deserializeIdent), SourceLocation.Unknown)
+  }
+
+  private def deserializeTraitConstraint(traitConstr0: TraitConstr)(implicit flix: Flix): TraitConstraint = traitConstr0 match {
+    case TraitConstr(sym, tpe) =>
+      TraitConstraint(TraitSymUse(deserializeTraitSym(sym), SourceLocation.Unknown), deserializeType(tpe), SourceLocation.Unknown)
   }
 
 }
