42701e23665e9789ef4245871cf1fc7f26591ed4
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2025-11-27 19:10:31 +0100

    remove imports

--- main/test/ca/uwaterloo/flix/api/effectlock/TestEffectLock.scala
+++ main/test/ca/uwaterloo/flix/api/effectlock/TestEffectLock.scala
@@ -1,8 +1,6 @@
 package ca.uwaterloo.flix.api.effectlock
 
 import ca.uwaterloo.flix.TestUtils
-import ca.uwaterloo.flix.api.effectlock.serialization.*
-import ca.uwaterloo.flix.util.Options
 import org.scalatest.funsuite.AnyFunSuite
 
 class TestEffectLock extends AnyFunSuite with TestUtils {
