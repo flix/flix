99a930909f2d643dd06637a19a75701aeb25f117
gwydd <gwydd12@proton.me>
2025-11-28 09:24:28 +0100

    feat: implement spacing between adjacent tokens
    - Some comment blocks result in no formatting.

--- main/src/ca/uwaterloo/flix/api/lsp/Formatter.scala
+++ main/src/ca/uwaterloo/flix/api/lsp/Formatter.scala
@@ -15,11 +15,9 @@
  */
 package ca.uwaterloo.flix.api.lsp
 
+import ca.uwaterloo.flix.language.ast.SyntaxTree.TreeKind.CommentList
 import ca.uwaterloo.flix.language.ast.SyntaxTree.TreeKind.Expr.Binary
 import ca.uwaterloo.flix.language.ast.{SyntaxTree, Token}
-import ca.uwaterloo.flix.language.ast.SyntaxTree.TreeKind.Operator
-
-import scala.collection.mutable.ListBuffer
 
 /**
   * A formatter for Flix syntax trees.
@@ -69,64 +67,43 @@ object Formatter {
     * @return a list of text edits to apply for formatting
     */
   private def formatBinaryExpression(tree: SyntaxTree.Tree): List[TextEdit] = {
-    val edits = ListBuffer.empty[TextEdit]
-    val operatorTree = tree.children.collectFirst {
-      case t: SyntaxTree.Tree if t.kind == Operator => t
-    }
+    val childBoundaries = tree.children.flatMap {
+      case t: SyntaxTree.Tree =>
+        for {
+          first <- getFirstToken(t)
+          last <- getLastToken(t)
+        } yield (first, last)
+      case tok: Token =>
+        Some((tok, tok))
+    }.toList
 
-    operatorTree.foreach { opTree =>
-      val opToken = collectTokens(opTree).headOption
-
-      opToken.foreach { op =>
-        val leftToken = tree.children.headOption.flatMap {
-          case t: SyntaxTree.Tree => getRightmostToken(t)
-          case token: Token => Some(token)
-        }
-
-        val rightToken = tree.children.lastOption.flatMap {
-          case t: SyntaxTree.Tree => getLeftmostToken(t)
-          case token: Token => Some(token)
-        }
-
-        (leftToken, rightToken) match {
-          case (Some(left), Some(right)) =>
-            val beforeRange = Range(
-              Position(left.sp2.lineOneIndexed, left.sp2.colOneIndexed),
-              Position(op.sp1.lineOneIndexed, op.sp1.colOneIndexed)
-            )
-            val afterRange = Range(
-              Position(op.sp2.lineOneIndexed, op.sp2.colOneIndexed),
-              Position(right.sp1.lineOneIndexed, right.sp1.colOneIndexed)
-            )
-
-            edits += TextEdit(beforeRange, " ")
-            edits += TextEdit(afterRange, " ")
-          case _ => // In case tokens are missing we do nothing.
-        }
+    val children = childBoundaries
+      .sliding(2)
+      .collect {
+        case List((_, prevLast), (nextFirst, _)) =>
+          TextEdit(
+            Range(
+              Position(prevLast.sp2.lineOneIndexed, prevLast.sp2.colOneIndexed),
+              Position(nextFirst.sp1.lineOneIndexed, nextFirst.sp1.colOneIndexed)
+            ),
+            " "
+          )
       }
-    }
-
-    edits.toList
+    children.toList
   }
 
-  private def getLeftmostToken(tree: SyntaxTree.Tree): Option[Token] = {
+  private def getFirstToken(tree: SyntaxTree.Tree): Option[Token] = {
     tree.children.headOption.flatMap {
-      case token: Token => Some(token)
-      case t: SyntaxTree.Tree => getLeftmostToken(t)
+      case t: SyntaxTree.Tree => getFirstToken(t)
+      case tok: Token => Some(tok)
     }
   }
 
-  private def getRightmostToken(tree: SyntaxTree.Tree): Option[Token] = {
+  private def getLastToken(tree: SyntaxTree.Tree): Option[Token] = {
     tree.children.lastOption.flatMap {
-      case token: Token => Some(token)
-      case t: SyntaxTree.Tree => getRightmostToken(t)
+      case t: SyntaxTree.Tree => getLastToken(t)
+      case tok: Token => Some(tok)
     }
-  }
-
-  private def collectTokens(child: SyntaxTree.Child): Array[Token] = child match {
-    case token: Token => Array(token)
-    case tree: SyntaxTree.Tree =>
-      tree.children.flatMap(collectTokens)
   }
 
   private def findTreeBasedOnUri(root: SyntaxTree.Root, uri: String): Option[SyntaxTree.Tree] = {
