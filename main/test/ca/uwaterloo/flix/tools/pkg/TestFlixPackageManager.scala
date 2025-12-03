ddf6f4301a0989015cec0f951e6e2f9bf5fcc813
Jakob Schneider <60233376+jaschdoc@users.noreply.github.com>
2025-12-03 09:06:48 +0100

    test: add missing GitHub token to tests (#12112)

--- main/test/ca/uwaterloo/flix/tools/pkg/TestFlixPackageManager.scala
+++ main/test/ca/uwaterloo/flix/tools/pkg/TestFlixPackageManager.scala
@@ -694,7 +694,7 @@ class TestFlixPackageManager extends AnyFunSuite with BeforeAndAfter {
       case Err(e) => fail(e.message(formatter))
     }
 
-    val allManifests = FlixPackageManager.findTransitiveDependencies(manifest, path, None) match {
+    val allManifests = FlixPackageManager.findTransitiveDependencies(manifest, path, PkgTestUtils.gitHubToken) match {
       case Ok(ms) => ms
       case Err(e) => fail(e.message(formatter))
     }
