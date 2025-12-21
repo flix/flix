fb3bc3c51d91738ac6f29922c0bc248161bc1f92
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2025-12-21 16:35:02 +0100

    style: format `TestListMap`

--- main/test/ca/uwaterloo/flix/util/TestListMap.scala
+++ main/test/ca/uwaterloo/flix/util/TestListMap.scala
@@ -36,13 +36,13 @@ class TestListMap extends AnyFunSuite {
   }
 
   test("ListMap.from.02") {
-    val lm = ListMap.from((1, 2) :: (2, 3) :: (1, 3) ::  Nil)
+    val lm = ListMap.from((1, 2) :: (2, 3) :: (1, 3) :: Nil)
     assert(lm.m == Map(1 -> List(3, 2), 2 -> List(3)))
   }
 
   test("ListMap.map.01") {
     val lm = ListMap(1 -> List(1, 2), 2 -> List(3, 4))
-    val res = lm.map{
+    val res = lm.map {
       case (k, v) => k + v
     }
     assert(res == Iterable(2, 3, 5, 6))
@@ -50,7 +50,7 @@ class TestListMap extends AnyFunSuite {
 
   test("ListMap.flatMap.01") {
     val lm = ListMap(1 -> List(1, 2), 2 -> List(3, 4))
-    val res = lm.flatMap{
+    val res = lm.flatMap {
       case (k, v) => List(k + v)
     }
     assert(res == Iterable(2, 3, 5, 6))
@@ -78,7 +78,7 @@ class TestListMap extends AnyFunSuite {
 
   test("ListMap.++.01") {
     val lm = ListMap(1 -> List(1, 2), 2 -> List(3, 4))
-    val res = lm ++ (1 -> List(5,6))
+    val res = lm ++ (1 -> List(5, 6))
     assert(res == ListMap(1 -> List(5, 6, 1, 2), 2 -> List(3, 4)))
   }
 
