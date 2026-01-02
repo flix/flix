b5f3deebda6dee33db7afe4c557028e52b93f3b6
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2026-01-02 22:57:45 +0100

    add debug print

--- main/src/ca/uwaterloo/flix/api/Bootstrap.scala
+++ main/src/ca/uwaterloo/flix/api/Bootstrap.scala
@@ -465,7 +465,10 @@ class Bootstrap(val projectPath: Path, apiKey: Option[String]) {
         case (f, UseGraph.UsedSym.DefnSym(g)) => f.toString -> g.loc
         case (f, UseGraph.UsedSym.SigSym(g)) => f.toString -> g.loc
       })
-      UseGraph.computeGraph(root).m.foreach { case (k, _) => println(k) }
+      UseGraph.computeGraph(root).m.keys.toList.sortBy {
+        case UseGraph.UsedSym.DefnSym(sym) => sym.toString
+        case UseGraph.UsedSym.SigSym(sym) => sym.toString
+      }.foreach { case (k, _) => println(k) }
       println(s"Use graph (filtered): ${UseGraph.computeGraph(root).filter { case (src, _) => src.toString == "main" }}")
       println(s"Inverted use graph (filterd): ${useGraph.filter { case (src, _) => src == "Upgr.entrypoint" }}")
       println(s"Use graph: ${UseGraph.computeGraph(root)}")
