c7349eaac0af895d63fc18aebd0e127a347944ff
sommer <au701790@uni.au.dk>
2026-01-27 13:29:51 +0100

    Using format

--- main/src/ca/uwaterloo/flix/language/phase/typer/Debug.scala
+++ main/src/ca/uwaterloo/flix/language/phase/typer/Debug.scala
@@ -144,7 +144,7 @@ object Debug {
       val edges = nested.map { child => s"${constraintId(constr)} -> ${constraintId(child)};" }
       (header :: children ::: edges).mkString("\n")
     case TypeConstraint.Conflicted(tpe1, tpe2, _) => s"""${constraintId(constr)} [label = "$tpe1 ≁ $tpe2"];"""
-    case TypeConstraint.EffConflicted(_) => "" //TODO eff errors
+    case TypeConstraint.EffConflicted(effErr) => format(effErr.toString)
   }
 
   /**
