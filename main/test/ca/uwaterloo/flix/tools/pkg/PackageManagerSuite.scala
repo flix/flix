4c6afd32b9713901b786444a23e9d7474784d92f
Magnus Madsen <magnusm@cs.au.dk>
2026-02-23 22:48:40 +0100

    feat: gradle -> mill

--- main/test/ca/uwaterloo/flix/tools/pkg/PackageManagerSuite.scala
+++ main/test/ca/uwaterloo/flix/tools/pkg/PackageManagerSuite.scala
@@ -1,7 +1,9 @@
 package ca.uwaterloo.flix.tools.pkg
 
+import org.scalatest.DoNotDiscover
 import org.scalatest.Suites
 
+@DoNotDiscover
 class PackageManagerSuite extends Suites(
   new TestBootstrap,
   new TestManifestParser,
