c73f8af44dd02839e64eb898c88a72ffc36ddfd4
gwydd <gwydd12@proton.me>
2026-04-03 10:29:47 +0200

    feat: improve Doc for Wadler-Lindig & PrettyPrinter
    - implement the most basic traversal of the tree
    - implement the most basic wadler lindig doc

--- main/src/ca/uwaterloo/flix/tools/fmt/Doc.scala
+++ main/src/ca/uwaterloo/flix/tools/fmt/Doc.scala
@@ -1,118 +1,60 @@
 package ca.uwaterloo.flix.tools.fmt
 
 sealed trait Doc {
-  def <>(that: Doc): Doc = Doc.Concat(this, that)
-  def <+>(that: Doc): Doc = this <> Doc.space <> that
+  def <>(right: Doc): Doc = Doc.Concat(this, right)
+  def <+>(right: Doc): Doc = this <> Doc.space <> right
+  def <|>(right: Doc): Doc = this <> Doc.line <> right
 }
 
 object Doc {
 
   case object Empty extends Doc
-  case class Text(s: String) extends Doc
+  case class Text(str: String) extends Doc
   case class Concat(left: Doc, right: Doc) extends Doc
-  case class Nest(indent: Int, doc: Doc) extends Doc
-  case object Line extends Doc
-  case object SoftLine extends Doc
+  case class Line() extends Doc
+  case class Nest(level: Int, doc: Doc) extends Doc
   case class Group(doc: Doc) extends Doc
 
-  def empty: Doc = Empty
-  def text(s: String): Doc = Text(s)
+  def text(str: String): Doc = Text(str)
+  def line: Doc = Line()
   def space: Doc = Text(" ")
-  def line: Doc = Line
-  def softline: Doc = SoftLine
-  def group(doc: Doc): Doc = Group(doc)
-  def nest(indent: Int, doc: Doc): Doc = Nest(indent, doc)
+  def empty: Doc = Empty
 
+  private sealed trait SimpleDoc
+  private case object SNil extends SimpleDoc
+  private case class SText(str: String, rest: SimpleDoc) extends SimpleDoc
+  private case class SLine(indent: Int, rest: SimpleDoc) extends SimpleDoc
 
-  sealed trait Mode
-  object Mode {
-    case object Flat extends Mode
-    case object Break extends Mode
+  def pretty(doc: Doc): String = {
+    val resolved = best(0, List((0, doc)))
+    layout(resolved)
   }
 
-  def pretty(doc: Doc, width: Int = 80): String = {
-    val out = new StringBuilder
-    val stack = scala.collection.mutable.Stack[(Int, Mode, Doc)]()
-    stack.push((0, Mode.Break, doc))
-
-    var col = 0
-
-    while (stack.nonEmpty) {
-      stack.pop() match {
-
-        case (_, _, Empty) =>
-
-        case (_, _, Text(s)) =>
-          out.append(s)
-          col += s.length
-
-        case (indent, mode, SoftLine) =>
-          if (mode == Mode.Flat) {
-            out.append(" ")
-            col += 1
-          } else {
-            out.append("\n")
-            out.append(" " * indent)
-            col = indent
-          }
-
-        case (indent, _, Line) =>
-          out.append("\n")
-          out.append(" " * indent)
-          col = indent
-
-        case (indent, mode, Concat(l, r)) =>
-          stack.push((indent, mode, r))
-          stack.push((indent, mode, l))
-
-        case (indent, mode, Nest(i, d)) =>
-          stack.push((indent + i, mode, d))
-
-        case (indent, mode, Group(d)) =>
-          val trial = (indent, Mode.Flat, d) :: stack.toList
-          if (fits(width - col, trial)) {
-            stack.push((indent, Mode.Flat, d))
-          } else {
-            stack.push((indent, Mode.Break, d))
-          }
+  private def layout(sdoc: SimpleDoc): String = {
+    val sb = new StringBuilder
+    var current = sdoc
+    while (current != SNil) {
+      current match {
+        case SText(s, rest) =>
+          sb.append(s)
+          current = rest
+        case SLine(indent, rest) =>
+          sb.append('\n')
+          sb.append(" " * indent)
+          current = rest
+        case SNil =>
       }
     }
-
-    out.toString()
+    sb.toString()
   }
 
-
-  private def fits(width: Int, stack: List[(Int, Mode, Doc)]): Boolean = {
-    var w = width
-    val localStack = scala.collection.mutable.Stack.from(stack)
-
-    while (w >= 0 && localStack.nonEmpty) {
-      localStack.pop() match {
-
-        case (_, _, Empty) =>
-
-        case (_, _, Text(s)) =>
-          w -= s.length
-
-        case (_, mode, SoftLine) =>
-          if (mode == Mode.Flat) w -= 1
-          else return true
-
-        case (_, _, Line) =>
-          return true
-
-        case (i, m, Concat(l, r)) =>
-          localStack.push((i, m, r))
-          localStack.push((i, m, l))
-
-        case (i, m, Nest(j, d)) =>
-          localStack.push((i + j, m, d))
-
-        case (i, _, Group(d)) =>
-          localStack.push((i, Mode.Flat, d))
-      }
-    }
-
-    w >= 0
+  private def best(k: Int, docs: List[(Int, Doc)]): SimpleDoc = docs match {
+    case Nil                      => SNil
+    case (_, Empty) :: z          => best(k, z)
+    case (i, Concat(x, y)) :: z  => best(k, (i, x) :: (i, y) :: z)
+    case (i, Nest(j, x)) :: z    => best(k, (i + j, x) :: z)
+    case (i, Text(s)) :: z       => SText(s, best(k + s.length, z))
+    case (i, Line()) :: z        => SLine(i, best(i, z))
+    case (i, Group(x)) :: z      => best(k, (i, x) :: z)
   }
 }
--- main/src/ca/uwaterloo/flix/tools/fmt/PrettyPrinter.scala
+++ main/src/ca/uwaterloo/flix/tools/fmt/PrettyPrinter.scala
@@ -1,79 +1,35 @@
 package ca.uwaterloo.flix.tools.fmt
 
 import ca.uwaterloo.flix.language.ast.SyntaxTree
-import ca.uwaterloo.flix.language.ast.SyntaxTree.TreeKind
+import ca.uwaterloo.flix.language.ast.SyntaxTree.{Tree, TreeKind}
 import ca.uwaterloo.flix.language.ast.Token
-import ca.uwaterloo.flix.tools.fmt.Doc.{empty, line, pretty, space, text}
-import ca.uwaterloo.flix.tools.fmt.rules.FormatterModule
+import ca.uwaterloo.flix.tools.fmt.Doc._
 
-/**
-  * TODO: Think about nesting like Binary expression?
-  */
 object PrettyPrinter {
 
-  /**
-    * The complete rule registry, composed of all [[FormatterModule]] instances
-    * declared in [[FormatterRegistry]].
-    */
-  private val rules: PartialFunction[SyntaxTree.Tree, Doc] =
-    FormatterRegistry.modules
-      .map(_.rules)
-      .reduce(_ orElse _)
-  /**
-    * Formats the given syntax tree into a string by applying predefined formatting rules.
-    * The method traverses the class hierarchy of the trees kind to find the most specific
-    * applicable formatting rule. If no specific rule is found we fallback to preserving
-    * the original formatting.
-    *
-    * @param tree the syntax tree to format
-    * @return a string representing the formatted source code of the given syntax tree
-    */
-  def format(tree: SyntaxTree.Tree): String =
-    pretty(treeToDoc(tree))
-
-  /**
-    * Transforms a syntax tree into a document by applying the most specific formatting rule
-    * available for the tree's kind. If no specific rule is found, it reconstructs the document.
-    * @param tree the syntax tree to transform into a document
-    * @return a document representing the formatted structure of the given syntax tree
-    */
-  private[tools] def treeToDoc(tree: SyntaxTree.Tree): Doc =
-    rules.lift(tree).getOrElse(reconstruct(tree))
-
-  private[tools] def reconstruct(tree: SyntaxTree.Tree): Doc = {
-    val (doc, _) = tree.children.foldLeft((empty, Option.empty[Token])) {
-      case ((acc, p), token: Token) =>
-        val gap = p.map(gapBetween(_, token)).getOrElse(empty)
-        (acc <> gap <> text(token.text), Some(token))
-
-      case ((acc, p), child: SyntaxTree.Tree) =>
-        val firstToken = collectTokens(child).headOption
-        val gap = (p, firstToken) match {
-          case (Some(prev), Some(f)) => gapBetween(prev, f)
-          case _                     => empty
-        }
-        val childDoc  = gap <> treeToDoc(child)
-        val lastToken = collectTokens(child).lastOption
-        (acc <> childDoc, lastToken orElse p)
-
-      case (state, _) =>
-        state
-    }
-    doc
+  def format(tree: Tree): String = {
+    val doc = traverse(tree)
+    println("Generated Doc:")
+    println(doc)
+    Doc.pretty(doc)
   }
 
-  private def gapBetween(prev: Token, curr: Token): Doc = {
-    val gap      = prev.src.data.slice(prev.endIndex, curr.startIndex)
-    val newlines = gap.count(_ == '\n')
-    val spaces   = gap.reverseIterator.takeWhile(_ == ' ').length
-
-    Iterator.fill(newlines)(line).foldLeft(empty)(_ <> _) <>
-      Iterator.fill(spaces)(space).foldLeft(empty)(_ <> _)
+  def traverse(tree: Tree): Doc = tree.kind match {
+    case TreeKind.Expr.Binary => prettyBinary(tree)
+    case _                    => prettyFallback(tree)
   }
 
-  private[tools] def collectTokens(tree: SyntaxTree.Tree): List[Token] =
-    tree.children.toList.flatMap {
-      case t: Token           => List(t)
-      case c: SyntaxTree.Tree => collectTokens(c)
-    }
+  private def prettyBinary(tree: Tree): Doc = {
+    val parts = tree.children.map(prettyChild)
+    parts.reduceLeftOption(_ <+> _).getOrElse(empty)
+  }
+
+  private def prettyFallback(tree: Tree): Doc = {
+    tree.children.map(prettyChild).reduceLeftOption(_ <> _).getOrElse(empty)
+  }
+
+  private def prettyChild(child: SyntaxTree.Child): Doc = child match {
+    case token: Token => text(token.text)
+    case tree: Tree   => traverse(tree)
+  }
 }
