fbd1b4c778f5c3913a46ff697cbfc96dd674ee9d
Simon220902 <simon.lykke.andersen@gmail.com>
2026-08-04 10:22:07 +0200

    fix: use FlowConstraint type and remove stray reachable mention in comment

--- main/src/ca/uwaterloo/flix/language/phase/monomorph2/NonMonomorphizableCheck.scala
+++ main/src/ca/uwaterloo/flix/language/phase/monomorph2/NonMonomorphizableCheck.scala
@@ -66,11 +66,8 @@ object NonMonomorphizableCheck {
   private case class Edge(src: Vertex, dst: Vertex, growing: Boolean)
 
   // TODO Make it a proper compiler error-message
-  /**
-    * Checks whether `flows` contains a reachable growing cycle and throws
-    * [[InternalCompilerException]] if so.
-    */
-  def checkMonomorphizable(flows: List[Flow]): Unit = {
+  /** Checks whether `flows` contains a growing cycle and throws [[InternalCompilerException]] if so. */
+  def checkMonomorphizable(flows: List[FlowConstraint]): Unit = {
     val edges = for {
       FlowConstraint(Instantiation(args), dst) <- flows
       (arg, i) <- args.zipWithIndex
