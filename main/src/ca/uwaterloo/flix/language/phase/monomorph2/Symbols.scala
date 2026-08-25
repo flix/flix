c40f3374aa2e3787880b1808716de3b9bce8fb48
Simon Lykke Andersen <simon.lykke.andersen@gmail.com>
2026-08-25 13:51:37 +0000

    chore: use "Flix Authors" copyright attribution in monomorph2 (#13145)
    Co-authored-by: Magnus Madsen <magnus-madsen@users.noreply.github.com>

--- main/src/ca/uwaterloo/flix/language/phase/monomorph2/Symbols.scala
+++ main/src/ca/uwaterloo/flix/language/phase/monomorph2/Symbols.scala
@@ -1,6 +1,5 @@
 /*
- * Copyright 2021 Magnus Madsen
- *                Casper Dalgaard Nielsen
+ * Copyright 2026 Flix Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
