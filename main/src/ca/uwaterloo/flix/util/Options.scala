088e47a9ebdf4daa50402f0ab031453f132fb2e1
Simon Lykke Andersen <simon.lykke.andersen@gmail.com>
2026-08-26 14:58:18 +0000

    chore: compiler test running with new monomorphization pipeline (#13153)
    Co-authored-by: Magnus Madsen <magnus-madsen@users.noreply.github.com>

--- main/src/ca/uwaterloo/flix/util/Options.scala
+++ main/src/ca/uwaterloo/flix/util/Options.scala
@@ -19,6 +19,9 @@ package ca.uwaterloo.flix.util
 import ca.uwaterloo.flix.language.ast.Symbol
 
 object Options {
+  // Enable constraint-based monomorphization if "XNEWMONO" env var is set.
+  private def EnableMono2: Boolean = sys.env.get("XNEWMONO").contains("1")
+
   /**
     * Default options.
     */
@@ -38,7 +41,7 @@ object Options {
     xnodeprecated = false,
     xsummary = false,
     xsubeffecting = Set.empty,
-    xnewmono = false,
+    xnewmono = EnableMono2,
     XPerfN = None,
     XPerfFrontend = false,
     XPerfPar = false,
