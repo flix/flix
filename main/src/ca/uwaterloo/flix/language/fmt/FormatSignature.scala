a95cf44907fbdeb1251ee9fd84c34d85ab3fd92d
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2026-09-20 21:50:46 +0200

    feat: report a changed signature as the declaration it is written as (#13377)

--- main/src/ca/uwaterloo/flix/language/fmt/FormatSignature.scala
+++ main/src/ca/uwaterloo/flix/language/fmt/FormatSignature.scala
@@ -16,6 +16,7 @@
 package ca.uwaterloo.flix.language.fmt
 
 import ca.uwaterloo.flix.api.Flix
+import ca.uwaterloo.flix.language.ast.shared.{EqualityConstraint, TraitConstraint}
 import ca.uwaterloo.flix.language.ast.{Type, TypeConstructor, TypedAst}
 import ca.uwaterloo.flix.util.collection.Nel
 
@@ -46,20 +47,36 @@ object FormatSignature {
     * Returns a markdown string for the given `name` and `spec`.
     */
   private def formatSpec(name: String, spec: TypedAst.Spec)(implicit flix: Flix): String = {
-    s"def $name(${formatFormalParams(spec.fparams)}): ${formatResultTypeAndEff(spec.retTpe, spec.eff)}"
+    formatSpecWithOptions(name, spec, flix.getFormatOptions)
+  }
 
+  /**
+    * Returns a string for the given `name` and `spec`, written as the declaration would be, e.g.
+    * `def sell(x: a, n: Int32): Int32 \ IO with ToString[a]`.
+    *
+    * The constraints are part of the signature: what a declaration requires of the types it is
+    * given is as much a part of how it may be called as the types themselves.
+    */
+  def formatSpecWithOptions(name: String, spec: TypedAst.Spec, fmt: FormatOptions): String = {
+    val params = formatFormalParams(spec.fparams, fmt)
+    val result = formatResultTypeAndEff(spec.retTpe, spec.eff, fmt)
+    // N.B.: The constraints are taken from the declared scheme. `Spec` holds the equality
+    // constraints as `TypedAst.EqualityConstraint`, which does not name the associated type, so
+    // there is not enough in one to write it out.
+    val sc = spec.declaredScheme
+    s"def $name($params): $result${formatConstraints(sc.tconstrs, sc.econstrs, fmt)}"
   }
 
   /**
     * Returns a formatted string of the formal parameters.
     */
-  private def formatFormalParams(fparams0: Nel[TypedAst.FormalParam])(implicit flix: Flix): String = fparams0 match {
+  private def formatFormalParams(fparams0: Nel[TypedAst.FormalParam], fmt: FormatOptions): String = fparams0 match {
     // Case 1: Single Unit type parameter. This gets sugared into a nullary function: `foo()`
     case Nel(fparam, Nil) if fparam.tpe == Type.Unit => ""
     // Case 2: Some list of parameters. Format each and join them: `foo(x: Int32, y: Bool)`
     case fparams =>
       val formattedArgs = fparams.map {
-        case TypedAst.FormalParam(bnd, tpe, _, _, _) => s"${bnd.sym.text}: ${FormatType.formatType(tpe)}"
+        case TypedAst.FormalParam(bnd, tpe, _, _, _) => s"${bnd.sym.text}: ${FormatType.formatTypeWithOptions(tpe, fmt)}"
       }
       formattedArgs.mkString(", ")
 
@@ -68,9 +85,29 @@ object FormatSignature {
   /**
     * Returns a formatted string of the result type and effect.
     */
-  private def formatResultTypeAndEff(tpe: Type, eff: Type)(implicit flix: Flix): String = eff match {
-    case Type.Cst(TypeConstructor.Pure, _) => FormatType.formatType(tpe)
-    case Type.Cst(TypeConstructor.Univ, _) => s"${FormatType.formatType(tpe)} \\ IO"
-    case otherEff => s"${FormatType.formatType(tpe)} \\ ${FormatType.formatType(otherEff)}"
+  private def formatResultTypeAndEff(tpe: Type, eff: Type, fmt: FormatOptions): String = eff match {
+    case Type.Cst(TypeConstructor.Pure, _) => FormatType.formatTypeWithOptions(tpe, fmt)
+    case Type.Cst(TypeConstructor.Univ, _) => s"${FormatType.formatTypeWithOptions(tpe, fmt)} \\ IO"
+    case otherEff => s"${FormatType.formatTypeWithOptions(tpe, fmt)} \\ ${FormatType.formatTypeWithOptions(otherEff, fmt)}"
+  }
+
+  /**
+    * Returns a formatted string of the trait and equality constraints, or the empty string if
+    * there are none of either.
+    */
+  private def formatConstraints(tconstrs: List[TraitConstraint], econstrs: List[EqualityConstraint], fmt: FormatOptions): String = {
+    val tconstrPart =
+      if (tconstrs.isEmpty)
+        ""
+      else
+        " with " + tconstrs.map(FormatTraitConstraint.formatTraitConstraintWithOptions(_, fmt)).mkString(", ")
+
+    val econstrPart =
+      if (econstrs.isEmpty)
+        ""
+      else
+        " where " + econstrs.map(FormatEqualityConstraint.formatEqualityConstraintWithOptions(_, fmt)).mkString(", ")
+
+    tconstrPart + econstrPart
   }
 }
